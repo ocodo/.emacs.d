1c3f6be78f431d9cae1fa2404fa8ed2c27039ad4
jasonm23 <jasonm23@gmail.com>
2010-08-09 21:14:27 +0000

    additional language mode hooks for hideshow
    git-svn-id: http://fluxusproject.org/svn/dotfiles/.emacs.d@8 9e7839e2-c8cb-4cfb-b91e-3acec954afc1

--- plugins.el
+++ plugins.el
@@ -9,12 +9,9 @@
 (require 'dropins)
 (require 'autopair) 
 (autopair-global-mode)
-
 (require 'darkroom-mode)
-
 (require 'delsel) 
 (delete-selection-mode t)
-
 (require 'gist)
 (require 'ido)
 (require 'movetext)
@@ -25,23 +22,36 @@
 (require 'tabbar-extension)
 (require 'kill-buffer-without-confirm)
 
+;; Hideshow settings (code folding)
 (require 'hideshow)
-(require 'hideshowvis)
+
+;; we use a version of hideshowvis which has
+;; arrow bitmaps in the fringe, and switches
+;; on the hidden number of lines view.
+;; http://gist.github.com/514946
+(require 'hideshowvis) 
 
 (add-to-list 'hs-special-modes-alist
 	     '(ruby-mode
 	       "\\(def\\|do\\|{\\)" "\\(end\\|end\\|}\\)" "#"
-	       (lambda (arg) (ruby-end-of-block)) nil))
+	       (lambda (arg) (ruby-end-of-block)) nil)
+	     )
 
 (dolist (hook (list 'emacs-lisp-mode-hook
 		    'lisp-mode-hook
 		    'ruby-mode-hook
+		    'perl-mode-hook
+		    'php-mode-hook
+		    'python-mode-hook
+		    'lua-mode-hook
 		    'c-mode-hook
 		    'java-mode-hook
 		    'js-mode-hook
 		    'c++-mode-hook))
   (add-hook hook 'hideshowvis-enable))
 
+;; Start the emacs server.
 (server-start nil)
 
+;; Setup global key bindings.
 (load "keybindings")
