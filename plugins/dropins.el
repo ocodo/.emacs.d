81981a733abd7e9724001b5ab860a6cfa4cb31e7
jasonm23 <jasonm23@gmail.com>
2010-08-10 12:17:36 +0000

    Dropins.el now supports .elc's (must be provided with .el to load) Added .elc's for color-themes and most plugins.
    git-svn-id: http://fluxusproject.org/svn/dotfiles/.emacs.d@12 9e7839e2-c8cb-4cfb-b91e-3acec954afc1

--- plugins/dropins.el
+++ plugins/dropins.el
@@ -4,13 +4,17 @@
 ;; by placing them in a named folder.  This is similar to the site-lisp
 ;; folder in the Emacs directory, however, Dropins defaults to a folder 
 ;; within the user HONE.
-;; 
-;; Just place your dropin libraries in a folder and use
-;; M-x customize-group RET dropins to point to that folder.
-;;  
-;; NOTE that ELPA http://tromey.com/elpa/install.html would 
-;; be the best way to install libraries. But libraries must be 
-;; packaged specifically for ELPA to work.
+;;
+;; ~/.emacs.d/dropins/
+;;
+;; is the default Library dropin folder, place your 
+;; dropin .el libraries in here, also place .elc's in this 
+;; folder but a corresponding .el must be available.
+;; Otherwise it won't load.
+;;
+;; note that ELPA http://tromey.com/elpa/install.html would 
+;; be the best way to install libraries. But they must be
+;; packaged properly.
 ;; 
 ;; Dropins is really intended for small extensions, for example
 ;; color-themes or binding setups.
@@ -22,7 +26,9 @@
 )
 
 (defcustom dropin-folder "~/.emacs.d/dropins" 
-  "Library dropin folder, place your dropin .el libraries in this folder. 
+  "Library dropin folder, place your dropin .el libraries 
+   in this folder. You can also place .elc's in this folder
+   but if there is not corresponding .el it will not load.
    
   note that ELPA http://tromey.com/elpa/install.html would 
   be the best way to install libraries. But they must be 
@@ -33,15 +39,23 @@
 )
 
 (defvar dropin-folder 'dropin-folder
-  "Library dropin folder, place your dropin .el libraries in this folder. note that ELPA http://tromey.com/elpa/install.html would be the best way to install libraries. But they must be packaged properly.")
+  "Library dropin folder, place your dropin .el libraries 
+   in this folder. You can also place .elc's in this folder
+   but if there is not corresponding .el it will not load.
+
+   note that ELPA http://tromey.com/elpa/install.html would 
+   be the best way to install libraries. But they must be
+   packaged properly.")
 
 (defun load-dropins () ""
   (interactive)
     (loop for entry in (directory-files dropin-folder t)
 	  when (equal ".el" (substring entry -3))
-	  do (load-library entry)
+	  ;; do regexp match and grab result substring
+	  do (load-library (car (split-string (car (reverse (split-string entry "/" ))) "\\.")))
 	  )
     )
 
 (load-dropins)
 (provide 'dropins)
+
