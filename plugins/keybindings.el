c2f0139f4bab661bee6076f8b3a8542e44b881f7
jasonm23 <jasonm23@9e7839e2-c8cb-4cfb-b91e-3acec954afc1>
2010-08-24 08:19:05 +0000

    Added - color-theme-tilak cua mode on (the awesomeness!) dropped things surpassed by cua mode... rect mark, delete-selection etc. customized cua mode features.
    git-svn-id: http://fluxusproject.org/svn/dotfiles/.emacs.d@16 9e7839e2-c8cb-4cfb-b91e-3acec954afc1

--- plugins/keybindings.el
+++ plugins/keybindings.el
@@ -1,28 +1,10 @@
-;; C-z undo 
-(global-set-key [(control z)] 'undo)
 ;; C-n find-file
 (global-set-key [(control n)] 'find-file)
 
-;;
-;; Rect-mark key bindings.
-;;
-(global-set-key [(S-down-mouse-1)] 'rm-set-mark)
-(global-set-key (kbd "C-x r C-SPC") 'rm-set-mark)
-(global-set-key (kbd "C-x r C-x") 'rm-exchange-point-and-mark)
-(global-set-key (kbd "C-x r C-w") 'rm-kill-region)
-(global-set-key (kbd "C-x r M-w") 'rm-kill-ring-save)
+;; Move between buffers with M-[ and M-]
 
-(autoload 'rm-set-mark "rect-mark"
-  "Set mark for rectangle." t)
-
-(autoload 'rm-exchange-point-and-mark "rect-mark"
-  "Exchange point and mark for rectangle." t)
-
-(autoload 'rm-kill-region "rect-mark"
-  "Kill a rectangular region and save it in the kill ring." t)
-
-(autoload 'rm-kill-ring-save "rect-mark"
-  "Copy a rectangular region to the kill ring." t)
+(global-set-key (kbd "M-]") 'next-buffer)
+(global-set-key (kbd "M-[") 'previous-buffer)
 
 ;; Darkroom additional bindings
-(global-set-key (quote [M-S-return]) 'darkroom-mode)
+(global-set-key '[M-S-return] 'darkroom-mode)
