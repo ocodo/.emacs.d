0aa1d4115b1aea65d53b98545b42df792d576ac3
jasonm23 <jasonm23@gmail.com>
2010-11-29 21:46:08 +0000

    many updates, see rev list
    git-svn-id: http://fluxusproject.org/svn/dotfiles/.emacs.d@30 9e7839e2-c8cb-4cfb-b91e-3acec954afc1

--- plugins/sockpuppet.el
+++ plugins/sockpuppet.el
@@ -1,5 +1,5 @@
 ;;; sockpuppet.el - Emacs client interface for sockpuppet chat server
-(defconst sockpuppet-version "0.2")
+(defconst sockpuppet-version "0.8.0")
 
 ;; Copyright (c)2010 Jasonm23. (by)(nc)(sa) Some rights reserved.
 ;; Author: Jasonm23 <jasonm23@gmail.com>
@@ -22,7 +22,7 @@
 ;;
 ;; Commentary:
 ;;
-;; Sockpuppet Mode
+;; Sockpuppet chat client mode
 
 (require 'easymenu)
 
@@ -32,7 +32,7 @@
   "Sockpuppet connection options"
   :group 'sockpuppet)
 
-(defcustom sockpuppet-nick "Jason" 
+(defcustom sockpuppet-nick "GorgaR!" 
   "Nickname to use on sockpuppet"
   :group 'sockpuppet)
 
@@ -41,7 +41,7 @@
   :type 'color
   :group 'sockpuppet)
 
-(defcustom sockpuppet-host "10.1.6.107" 
+(defcustom sockpuppet-host "10.1.6.110" 
   "Sockpuppet host ip"
   :group 'sockpuppet)
 
@@ -58,6 +58,10 @@
 
 (defvar sockpuppet-is-online nil)
 
+(defvar sockpuppet-color-match-regexp  
+  "^0x\\([0-9A-Fa-f]\\{6\\}\\)§"
+  "Regexp for matching message color headers")
+
 ;;; Functions ==============================================
 
 (defun sockpuppet-connect ()
@@ -72,6 +76,8 @@
      (height . 45))))
     (select-frame sock-puppet-frame)
     (switch-to-buffer "sock-buffer")
+    (set-buffer-process-coding-system 'utf-8-dos 'utf-8-dos)
+    (setq buffer-read-only 1)
     (sockpuppet-mode)
     (setq sockpuppet-is-online t)
     (sockpuppet-send-message (concat "/nick " sockpuppet-nick))
@@ -85,11 +91,12 @@
   "End the sockpuppet session and kill it's buffer and frame"
   (interactive)
   (when (eq sockpuppet-is-online t)
+    (setq sockpuppet-is-online nil)
     (delete-process sockpuppet-process)
+    (kill-buffer (get-buffer "sock-buffer"))
     (select-frame sock-puppet-frame)
     (delete-frame)
-    (kill-buffer "sock-buffer")
-    (setq sockpuppet-is-online nil)
+    (message "Disconnected from sock-puppet...")
     ))
 
 (defun sockpuppet-send-message (message)
@@ -112,12 +119,65 @@
   (interactive)
   (message (format "SockPuppet for Emacs v%s" sockpuppet-version)))
 
+(defun entry-color-block (beg end len)
+  "Tidy up the message color string."
+  (when (eq sockpuppet-is-online t)
+  (linum-mode 0)
+  (let (in-message new-message color match-overlay)
+    (setq in-message (buffer-substring-no-properties beg end))
+    (setq new-message 
+	  (replace-regexp-in-string sockpuppet-color-match-regexp "" in-message))
+    (setq color (concat "#" (substring-no-properties in-message 2 8)))
+    (if (not (eq nil (color-values color)))
+	(progn
+	  (replace-string in-message new-message nil beg end)
+	  (setq  match-overlay (make-overlay (match-beginning 0) (match-end 0)))
+	  (overlay-put match-overlay 'face `((:background ,color)
+					     (:foreground ,(if (> 128.0 (color-luminance color))
+						   "white" "black"))))) 
+      nil))))
+
+(defun color-luminance (color)
+  "Calculate the luminance of a color string (e.g. \"#ffaa00\", \"blue\")."
+  (let* ((values (color-values color))
+	 (r (car values))
+	 (g (cadr values))
+	 (b (caddr values)))
+    (floor  (+ (* .2126 r) (* .7152 g) (* .0722 b)) 256 )))
+
+(defun customize-sockpuppet ()
+  "Customize sockpuppet."
+  (interactive)
+  (customize-group-other-window 'sockpuppet))
+
+(defun reduce-text ()
+  "reduce sockpuppet text"
+  (interactive)
+  (text-scale-decrease 1))
+
+(defun enlarge-text ()
+  "reduce sockpuppet text"
+  (interactive)
+  (text-scale-increase 1))
+
+(defun google ()
+  "Googles a query or region if any."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
+    (if mark-active
+        (buffer-substring (region-beginning) (region-end))
+      (read-string "Query: ")))))
+
 ;;; Keymap ================================================================
 
 (defvar sockpuppet-mode-map
   (let ((sockpuppet-mode-map (make-keymap)))
     ;; Element insertion
     (define-key sockpuppet-mode-map "\C-m" 'sockpuppet-send-message)
+    (define-key sockpuppet-mode-map '[(mouse-3)] 'browse-url-at-mouse)
+    (define-key sockpuppet-mode-map (kbd "C-7") 'google)
     sockpuppet-mode-map)
   "Keymap for Markdown major mode.")
 
@@ -129,14 +189,25 @@
     ["Send Message" sockpuppet-send-message]
     ["Send Region" sockpuppet-send-region]
     "---"
+    ["Enlarge text" enlarge-text]
+    ["Reduce text" reduce-text]
+    "---"
+    ["Google Region" google]
+    "---"
+    ["Customize Sockpuppet" customize-sockpuppet ]
+    "---"
     ["Version" sockpuppet-show-version]))
 
 ;;; Sockpuppet Mode =======================================================
 
 (define-derived-mode sockpuppet-mode fundamental-mode "SockPuppet"
   "Major mode for using sockpuppet buffers"
+  (add-hook 'after-change-functions 'entry-color-block nil 1)
   (easy-menu-add sockpuppet-mode-menu sockpuppet-mode-map)
-  (global-set-key "\M-p" 'sockpuppet-send-region))
+  (global-set-key "\M-p" 'sockpuppet-send-region)
+  (text-scale-adjust -1)
+  (linum-mode 0)
+)
 
-(provide 'sock-puppet)
+(provide 'sockpuppet)
 ;; end-of-sockpuppet.el
