44348c7394d331ecd24d2c6b4f04b7ccc59d6adb
jasonm23 <jasonm23@gmail.com>
2012-06-02 05:39:16 +1000

    transitioning to el-get

---
+++ el-get/.loaddefs.el~
@@ -0,0 +1,12 @@
+;;; .loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+(provide '.loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; .loaddefs.el ends here
--- init.el
+++ init.el
@@ -1,8 +1,25 @@
-;; Mac Cocoa Emacs.app & NTEmacs/w32 
 ;; init.el 
+(progn (cd "~/.emacs.d") (normal-top-level-add-subdirs-to-load-path))
 
-;; Load plugins...
-(load "~/.emacs.d/plugins.el")
+(require 'color-theme)
+(require 'dropins)
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get") (unless (require 'el-get nil t) (with-current-buffer (url-retrieve-synchronously "https://raw.github.com/dimitri/el-get/master/el-get-install.el") (goto-char (point-max)) (eval-print-last-sexp))) (el-get 'sync)
+
+(require 'resize-window)
+(require 'frame-play)
+(require 'ido)
+(require 'gist)
+(require 'move-text)
+(require 'markdown-mode)
+(require 'textile-mode)
+(require 'rainbow-mode)
+(require 'scroll-bell-fix)
+(require 'squeeze-view)
+(require 'kill-buffer-without-confirm)
+(require 'xfrp_find_replace_pairs)
+(require 'haml-mode)
+(require 'scss-mode)
 
 ;; turn off menubar - uncomment
 ;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
@@ -13,16 +30,125 @@
 (require 'yasnippet-bundle)
 (yas/load-directory "~/.emacs.d/snippets" )
 
-;; Load additional snippets
-;; (yas/load-directory "~/.emacs.d/snippets")
-;; Periodically re-create the yasnippet-bundle
-;; with M-x yas/compile-bundle
-;;
-;; You will need to migrate additional snippets
-;; into the main snippets folder before
-;; compiling the bundle.
-;;
-;; see http://yasnippet.googlecode.com/svn/trunk/doc/snippet-organization.html
+;; We default, with customize, to MacBook settings 
+  (set-face-attribute 'default nil
+		      :family "Monaco"
+		      :height 130)
+;; ...and override here.
+
+(put 'erase-buffer 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+
+;; Load a nice theme...
+;;(defun color-theme-tq2323 () (interactive) (color-theme-install '(color-theme-tq2323 ((background-color . "#000407") (foreground-color . "#ddfffc") (background-mode . dark) (border-color . "#052e2d") (cursor-color . "#0d6d6c") (mouse-color . "#323232")) (fringe ((t (:background "#052e2d")))) (mode-line ((t (:foreground "#9cf6f4" :background "#0b2c2d")))) (region ((t (:background "#0c5f59")))) (font-lock-builtin-face ((t (:foreground "#0b8e87")))) (font-lock-comment-face ((t (:foreground "#265f59")))) (font-lock-function-name-face ((t (:foreground "#60c3be")))) (font-lock-keyword-face ((t (:foreground "#0abda7")))) (font-lock-string-face ((t (:foreground "#aaedee")))) (font-lock-type-face ((t (:foreground"#1f8e8a")))) (font-lock-constant-face ((t (:foreground "#1ae9d7")))) (font-lock-variable-name-face ((t (:foreground "#0ebeb8")))) (minibuffer-prompt ((t (:foreground "#00faf2" :bold t)))) (font-lock-warning-face ((t (:foreground "red" :bold t)))))))
+;;(color-theme-tq2323)
+;; this is now external... 
+
+(require 'ls-lisp)
+(setq ls-lisp-use-insert-directory-program nil)
+(setq ls-lisp-verbosity nil)
+
+(defadvice ls-lisp-format (around my-ls-lisp-format 
+  (file-name file-attr file-size switches time-index now))
+  "Advice definition which removes unnecessary information
+during file listing in dired. For such purposes 
+`ls-lisp-verbosity' customized variable can be used, but 
+even if it is equal to nil dired will display file 
+permissions field like \"drwxrwxrwx\".\. So here we just 
+get full control to what dired shows and leave only those 
+fields which we need."
+  (progn
+    ad-do-it
+    (setq ad-return-value 
+          (concat (substring ad-return-value 0 1)
+                  " "
+                  (substring ad-return-value 29 )))))
+(ad-activate 'ls-lisp-format t)
+
+(when (window-system)
+  (require 'darkroom-mode)
+
+  (require 'hideshowvis)
+  (add-to-list 'hs-special-modes-alist
+	       '(ruby-mode
+		 "\\(def\\|do\\|{\\)"
+		 "\\(end\\|\\)}"
+		 "#"
+		 (lambda (arg) (ruby-end-of-block)) nil))
+  (add-to-list 'hs-special-modes-alist
+	       '(css-mode "{" "}" "/[*/]" nil nil))
+  (dolist (hook (list 'emacs-lisp-mode-hook
+		      'lisp-mode-hook
+		      'ruby-mode-hook
+		      'perl-mode-hook
+		      'php-mode-hook
+		      'python-mode-hook
+		      'lua-mode-hook
+		      'c-mode-hook
+		      'java-mode-hook
+		      'js-mode-hook
+		      'css-mode-hook
+		      'c++-mode-hook))
+    (add-hook hook 'hideshowvis-enable)))
+
+
+;; Setup global key bindings.
+;; C-n find-file
+(global-set-key [(control n)] 'find-file)
+
+;; Move between buffers with M-[ and M-]
+
+(global-set-key (kbd "M-]") 'next-buffer)
+(global-set-key (kbd "M-[") 'previous-buffer)
+
+(global-set-key (kbd "s-]") 'next-buffer)
+(global-set-key (kbd "s-[") 'previous-buffer)
+
+(global-set-key (kbd "<s-right>") 'next-buffer)
+(global-set-key (kbd "<s-left>") 'previous-buffer)
+
+(global-set-key (kbd "s-/" ) 'dabbrev-expand)
+
+(global-set-key (kbd "s-b") 'switch-to-buffer)
+
+(global-set-key (kbd "s--") 'text-scale-decrease) 
+(global-set-key (kbd "s-=") 'text-scale-increase) 
+
+(global-set-key (kbd "s-n") 'find-file)
+
+(global-set-key (kbd "s-1") 'delete-other-windows)
+(global-set-key (kbd "s-2") 'split-window-horizontally)
+(global-set-key (kbd "s-3") 'split-window-vertically)
+(global-set-key (kbd "s-4") 'delete-other-windows-vertically)
+(global-set-key (kbd "s-5") 'delete-window)
+
+
+(global-set-key (kbd "C-M-,") 'shrink-window-horizontally)
+(global-set-key (kbd "C-M-.") 'enlarge-window-horizontally)
+
+(global-set-key (kbd "s-`") 'other-window)
+(global-set-key (kbd "s-~") 'other-frame)
+
+;; completion at point
+(global-set-key [(control tab)] 'completion-at-point)
+(global-set-key (kbd "<s-return>" ) 'completion-at-point)
+
+;; Darkroom additional bindings
+(global-set-key '[M-S-return] 'darkroom-mode)
+(global-set-key '[M-s-return] 'darkroom-mode)
+
+;; Linum mode toggle
+(global-set-key (kbd "s-0") 'linum-mode)
+
+;; Dired mode additions
+
+(eval-after-load 'dired
+  '(define-key dired-mode-map [mouse-3] 'dired-maybe-insert-subdir))
+
+;; Start the emacs server.
+(server-start nil)
 
 ;; Customize stuff... 
 (custom-set-variables
@@ -82,7 +208,9 @@ mouse-3: Remove current window from display")))))
  '(yas/global-mode t nil (yasnippet))
  '(yas/prompt-functions (quote (yas/dropdown-prompt yas/x-prompt yas/completing-prompt yas/ido-prompt yas/no-prompt)))
  '(yas/triggers-in-field t)
- '(yas/wrap-around-region t))
+ '(yas/wrap-around-region t)
+)
+
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -133,33 +261,7 @@ mouse-3: Remove current window from display")))))
  '(vhdl-speedbar-library-face ((((class color) (background dark)) (:inherit speedbar-button-face :foreground "Orchid1"))))
  '(vhdl-speedbar-package-face ((((class color) (background dark)) (:inherit speedbar-button-face :foreground "Grey80"))))
  '(vhdl-speedbar-package-selected-face ((((class color) (background dark)) (:inherit speedbar-button-face :foreground "Grey80" :underline t))))
- '(vhdl-speedbar-subprogram-face ((((class color) (background dark)) (:inherit speedbar-button-face :foreground "BurlyWood2")))))
-
-
-;; We default, with customize, to MacBook settings 
-  (set-face-attribute 'default nil
-		      :family "Monaco"
-		      :height 130)
-;; ...and override here.
-
-;; Windows specific font settings...
-(when 
-    (eq system-type 'windows-nt) 
-  (set-face-attribute 'default nil
-                      ;; :family "DejaVu Sans Mono"
-                      :family "Monaco"
-                      :height 100)
-  )
-
-(put 'erase-buffer 'disabled nil)
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
-
-;; Load a nice theme...
-(defun color-theme-tq2323 () (interactive) (color-theme-install '(color-theme-tq2323 ((background-color . "#000407") (foreground-color . "#ddfffc") (background-mode . dark) (border-color . "#052e2d") (cursor-color . "#0d6d6c") (mouse-color . "#323232")) (fringe ((t (:background "#052e2d")))) (mode-line ((t (:foreground "#9cf6f4" :background "#0b2c2d")))) (region ((t (:background "#0c5f59")))) (font-lock-builtin-face ((t (:foreground "#0b8e87")))) (font-lock-comment-face ((t (:foreground "#265f59")))) (font-lock-function-name-face ((t (:foreground "#60c3be")))) (font-lock-keyword-face ((t (:foreground "#0abda7")))) (font-lock-string-face ((t (:foreground "#aaedee")))) (font-lock-type-face ((t (:foreground"#1f8e8a")))) (font-lock-constant-face ((t (:foreground "#1ae9d7")))) (font-lock-variable-name-face ((t (:foreground "#0ebeb8")))) (minibuffer-prompt ((t (:foreground "#00faf2" :bold t)))) (font-lock-warning-face ((t (:foreground "red" :bold t)))))))
-
+ '(vhdl-speedbar-subprogram-face ((((class color) (background dark)) (:inherit speedbar-button-face :foreground "BurlyWood2"))))
+)
 
 (color-theme-tq2323)
-
-(setq mouse-wheel-scroll-amount '(0.01))
