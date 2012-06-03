b37493ed7e29593a7b4bb71c1800d2d12247d9cf
jasonm23 <jasonm23@gmail.com>
2012-06-03 16:27:57 +1000

    pruning

--- elpa/inflections-1.0/inflections-autoloads.el
+++
@@ -1,13 +0,0 @@
-;;; inflections-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-(provide 'inflections-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; inflections-autoloads.el ends here
