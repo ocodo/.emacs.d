f77e4cfbd4f85742019a6e605b1e6198ab708464
jasonm23 <jasonm23@gmail.com>
2012-06-19 01:22:05 +1000

    roll-back to before el-get / package installs.

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-install-save-confirm nil)
+ '(blink-cursor-mode nil)
  '(color-theme-illegal-faces "^w3-")
  '(color-theme-is-cumulative t)
  '(color-theme-mode-hook nil)
