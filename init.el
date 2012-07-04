e6f12e054e38b6b5381ab863f662ddcd13953da3
jasonm23 <jasonm23@gmail.com>
2012-07-03 23:03:56 -0700

    modificatons for terminal mode

--- init.el
+++ init.el
@@ -3,13 +3,21 @@
 ;;   / _ \ / __/ _ \ / _` |/ _ \   / _ \ '_ ` _ \ / _` |/ __/ __|  / _` |/ _ \| __|
 ;;  | (_) | (_| (_) | (_| | (_) | |  __/ | | | | | (_| | (__\__ \ | (_| | (_) | |_ 
 ;;   \___/ \___\___/ \__,_|\___/   \___|_| |_| |_|\__,_|\___|___/  \__,_|\___/ \__|
-;;                                                                                 
+;;                                          
 
-(setq exec-path (append exec-path '("/Developer/usr/bin") ))
-;; init.el 
+;; A little hack to find Git on systems that only have XCode installed
+;; versions of Git. (git is now required for many Emacs things now.)
+(when (eq system-type 'darwin)
+  (when (file-exists-p "/Developer/usr/bin")
+    (setq exec-path (append exec-path '("/Developer/usr/bin"))))
+  (when (file-exists-p "/Applications/Xcode.app/Contents/Developer/usr/bin")
+    (setq exec-path (append exec-path '("/Applications/Xcode.app/Contents/Developer/usr/bin"))))
+  )
+
 (require 'cl)
 
-;; dirty, but cheap way to get .emacs.d subfolders into the load path.
+;; dirty, but cheap way to get .emacs.d subfolders into the load path,
+;; and then return us to the user home directory, for find-file etc.
 (progn (cd "~/.emacs.d/") (normal-top-level-add-subdirs-to-load-path) (cd "~"))
 
 ;; turn off toolbar 
@@ -18,7 +26,8 @@
 ;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
 ;; assume you'd prefer y / n to typing yes / no
-;; with power comes responsibility.
+;; with power comes responsibility. 
+;; Comment out for standard yes/no behaviour.
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (put 'erase-buffer     'disabled nil)
@@ -27,8 +36,18 @@
 (put 'narrow-to-region 'disabled nil)
 
 ;; Modes init (things that need more than just a require.) 
+(when (string-match "Emacs 24" (version))
+  (message "Running Emacs 24")
+  ;; Emacs 24 Specific stuff...
+  (require 'init-elpa)
+)
+
+(when (string-match "Emacs 23" (version))
+ (message "Running Emacs 23")
+ ;; Emacs 23 Specific stuff...
+)
+
 (require 'init-el-get)
-(require 'init-elpa)
 (require 'init-yasnippet)
 (require 'init-dired)
 (require 'init-hideshowvis)
@@ -75,21 +94,26 @@
 (require 'hexrgb)                      ;; hexrgb functions
 
 
+(when (window-system)
+
+;; window system stuff (only tested on Emacs.app Mac OS X.)
+
 ;;; modern/fancy modeline modification, seems to need a reload after
 ;;; the theme changes - it's a bit experimental / unstable.
-(require 'powerline)
-;; Notes on Powerline:
-;; rounded radius / chamfer size
-;; 
-;; This is as good a place as any to make a note about the powerline
-;; bitmaps I've added, as of this moment I've placed a few fixed size
-;; bitmap images into the powerline.el however, I think it would be a
-;; lot nicer to be able to specify the rounded corner or chamfer
-;; radius.
-;;
-;; But I'll need a couple of geometric functions and I'm short on time
-;; to do this now, so this is a note to bug me in the future.
-;; 
+  (require 'powerline)
+  ;; Notes on Powerline:
+  ;; rounded radius / chamfer size
+  ;; 
+  ;; This is as good a place as any to make a note about the powerline
+  ;; bitmaps I've added, as of this moment I've placed a few fixed size
+  ;; bitmap images into the powerline.el however, I think it would be a
+  ;; lot nicer to be able to specify the rounded corner or chamfer
+  ;; radius.
+  ;;
+  ;; But I'll need a couple of geometric functions and I'm short on time
+  ;; to do this now, so this is a note to bug me in the future.
+  ;; 
+)
 
 (setq custom-file "~/.emacs.d/custom/custom.el") ;; Customize stuff goes in custom.el
 (load-file        "~/.emacs.d/custom/custom.el") ;; <- load customizations...
@@ -101,13 +125,46 @@
 ;; Anything that needs to run after custom.
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 
-;; Quick access to default font face for fickle f-users, like me.
-(set-face-font 'default "Monaco")
 
-;;(set-face-font 'default "Inconsolata")
-;;(set-face-font 'default "Menlo")
-;;(set-face-font 'default "Bitstream Vera Sans Mono")
-;;(set-face-font 'default "Consolas")
-;;(set-face-font 'default "Droid Sans Mono")
+;; Default Font for different window systems
+(when (window-system)
 
-(require 'handy-functions)
+  ;; Mac OS X
+  (when (eq system-type 'darwin)
+    (set-face-font 'default "Monaco")
+    )
+
+  ;; Windows whatever...
+  (when (eq system-type 'windows-nt)
+    (set-face-font 'default "Consolas")
+    )
+
+  ;; GNU Linux (Droid or Vera)
+  (when (eq system-type 'gnu/linux)
+    ;; (set-face-font 'default "Droid Sans Mono") ;; for quick swapping.
+    (set-face-font 'default "Bitstream Vera Sans Mono")
+    )
+
+  ;; Quick access for some alternatives..
+  ;;(set-face-font 'default "Inconsolata")
+  ;;(set-face-font 'default "Monaco")
+  ;;(set-face-font 'default "Menlo")
+  ;;(set-face-font 'default "Bitstream Vera Sans Mono")
+  ;;(set-face-font 'default "Consolas")
+  ;;(set-face-font 'default "Droid Sans Mono")
+)
+
+(require 'handy-functions) ;; my lab area for little defuns
+
+(unless (window-system)
+  (message "running on the terminal")
+  ;; Do your terminal specific stuff.
+  (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+  (load-theme 'Jadedragon)
+  ;; Works with Emacs 23 & 24, earlier
+  ;; versions... go fish.
+  ;; Not 100% terminal
+  ;; compatible, but much nicer than
+  ;; angry-fruit-salad
+)
