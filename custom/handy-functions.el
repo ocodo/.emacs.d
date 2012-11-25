668c8cf3858fabd8cf66304d7084abf3a4e59d97
jasonm23 <jasonm23@gmail.com>
2012-11-25 20:40:37 +1100

    added a couple of helpers for generating random numbers in ranges, specifically a random Radian, or a number in a range of numbers, also added a convenience function for converting degrees to radians. (I guess I should add one for going back.)

--- custom/handy-functions.el
+++ custom/handy-functions.el
@@ -1,4 +1,21 @@
 ;; Handy functions, add little helpers in here.
+(defun insert-random-in-range (start end)         
+  (interactive "nRange start:\nnRange end:")
+  (insert (format "%i" (random-in-range start end))))
+
+(defun insert-random-radian () 
+  "insert a radian value from 0 to 6.28318 (2PI)"
+  (interactive)
+  (insert (format "%s" (* (/ float-pi 180) (random 361)))))
+
+(defun random-in-range (start end) 
+  (random t)
+  (+ start (random (+ 1 (- end start)))))
+
+(defun insert-radians-for-degrees (degrees)
+  "Insert radians for degrees"
+  (interactive "ndegrees:")
+  (insert (format "%s" (* (/ float-pi 180) degrees))))
 
 (defun now-is ()
     "Display current time."
