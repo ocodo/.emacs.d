ae9673b200df5a9f2ea12a69048415bb4caa2e69
Jasonm23 <jasonm23@gmail.com>
2013-03-20 00:05:21 +1100

    Update:
    * soothe-theme to v0.3.8

--- elpa/soothe-theme-0.3.8/soothe-theme-autoloads.el
+++ elpa/soothe-theme-0.3.8/soothe-theme-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 
 
-;;;### (autoloads nil "soothe-theme" "soothe-theme.el" (20808 19656
+;;;### (autoloads nil "soothe-theme" "soothe-theme.el" (20808 24674
 ;;;;;;  0 0))
 ;;; Generated autoloads from soothe-theme.el
 
@@ -11,7 +11,7 @@
 
 ;;;***
 
-;;;### (autoloads nil nil ("soothe-theme-pkg.el") (20808 19657 23944
+;;;### (autoloads nil nil ("soothe-theme-pkg.el") (20808 24674 833057
 ;;;;;;  0))
 
 ;;;***
