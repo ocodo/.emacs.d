fddc9159e86bdc93b4921ce367e9882ab3ea7dce
jasonm23 <jasonm23@gmail.com>
2013-07-13 21:37:30 +1000

    Installed Remember-theme extension

---
+++ elpa/remember-theme-0.1.0/remember-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; remember-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "remember-theme" "remember-theme.el" (20961
+;;;;;;  15301))
+;;; Generated autoloads from remember-theme.el
+
+(when load-file-name (add-hook 'after-init-hook 'remember-theme-load) (add-hook 'kill-emacs-hook 'remember-theme-save))
+
+;;;***
+
+;;;### (autoloads nil nil ("remember-theme-pkg.el") (20961 15301
+;;;;;;  275510))
+
+;;;***
+
+(provide 'remember-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; remember-theme-autoloads.el ends here
