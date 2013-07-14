027a04cf32a149a100852b0db28ad06e81c2d4fb
Jasonm23 <jasonm23@gmail.com>
2013-07-14 15:32:47 +1000

    Update packages

---
+++ elpa/clues-theme-20130713.205/clues-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clues-theme" "20130713.205" "Emacs 24 theme which is ok I suppose [github]" (quote nil))
---
+++ elpa/grizzl-20130713.824/grizzl-pkg.el
@@ -0,0 +1,2 @@
+(define-package "grizzl" "20130713.824" "Fuzzy Search Library & Completing Read [github]" 'nil)
+
