a30b13eab1be20ab514b9645c36cc5d9a8f4f081
jasonm23 <jasonm23@gmail.com>
2013-07-14 15:32:47 +1000

    Update packages

---
+++ elpa/clues-theme-20130713.205/clues-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; clues-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "clues-theme" "clues-theme.el" (20962 14210
+;;;;;;  0 0))
+;;; Generated autoloads from clues-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("clues-theme-pkg.el") (20962 14210 699439
+;;;;;;  0))
+
+;;;***
+
+(provide 'clues-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; clues-theme-autoloads.el ends here
---
+++ elpa/magit-20130713.1802/magit-pkg.el
@@ -0,0 +1,3 @@
+(define-package "magit" "20130713.1802" "Control Git from Emacs. [github]"
+  '((cl-lib "0.2")))
+
