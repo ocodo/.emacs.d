4aa628b361daaded3135d786116069c3e9f94c16
Jasonm23 <jasonm23@gmail.com>
2013-07-15 10:17:36 +1000

    update packages

--- elpa/butler-20130528.2136/butler-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "butler" "20130528.2136" "Client for Jenkins [github]"
-  '((web "0.3.7")))
-
--- elpa/clues-theme-20130713.205/clues-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "clues-theme" "20130713.205" "Emacs 24 theme which is ok I suppose [github]" (quote nil))
