cdbde24d0224dc8ed330ee237475d2cc2aab6ec8
jasonm23 <jasonm23@gmail.com>
2013-07-15 17:11:31 +1000

    Update packages

---
+++ elpa/assemblage-theme-20130715.620/assemblage-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; assemblage-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "assemblage-theme" "assemblage-theme.el" (20963
+;;;;;;  41110))
+;;; Generated autoloads from assemblage-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("assemblage-theme-pkg.el") (20963 41110
+;;;;;;  388917))
+
+;;;***
+
+(provide 'assemblage-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; assemblage-theme-autoloads.el ends here
