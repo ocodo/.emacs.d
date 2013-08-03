684826f14864c1a454cca2a1c6c87abfb3c0b3bf
Jasonm23 <jasonm23@gmail.com>
2013-08-03 23:03:27 +1000

    update packages

--- elpa/flymake-python-pyflakes-20130512.1844/flymake-python-pyflakes-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flymake-python-pyflakes" "20130512.1844" "A flymake handler for python-mode files using pyflakes (or flake8) [github]" (quote ((flymake-easy "0.8"))))
---
+++ elpa/flymake-python-pyflakes-20130730.131/flymake-python-pyflakes-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flymake-python-pyflakes" "20130730.131" "A flymake handler for python-mode files using pyflakes (or flake8) [github]" (quote ((flymake-easy "0.8"))))
