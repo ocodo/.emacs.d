b2087d8c3acf2f29370276c6b93f630d1c514e3c
jasonm23 <jasonm23@gmail.com>
2013-08-05 23:30:08 +1000

    updated latest packages

---
+++ elpa/helm-20130804.1901/helm-pkg.el
@@ -0,0 +1,2 @@
+(define-package "helm" "20130804.1901" "Helm is an Emacs incremental and narrowing framework [github]" 'nil)
+
---
+++ elpa/magit-20130804.315/magit-pkg.el
@@ -0,0 +1,2 @@
+(define-package "magit" "20130804.315" "control Git from Emacs [github]" 'nil)
+
