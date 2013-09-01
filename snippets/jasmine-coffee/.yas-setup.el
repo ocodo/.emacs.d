9648a21b743655cf206aeee24f3ebf58bcec2a94
Jasonm23 <jasonm23@gmail.com>
2013-09-01 14:15:59 +1000

    add rails snippets and a couple of extras, defface snippet v0.1

--- snippets/jasmine-coffee/.yas-setup.el
+++ snippets/jasmine-coffee/.yas-setup.el
@@ -2,12 +2,12 @@
   "Grab the previous matches of regexp and return the contents of
   the n match group (first group match if no n arg is specified)"
   (save-excursion
-  (unless n
-    (setq n 1))
-  (when (numberp n)
-    (re-search-backward-lax-whitespace rexp)
-    (when  (= (+ 2 (* n 2)) (length (match-data)))
-      (match-string-no-properties n)))))
+    (unless n
+      (setq n 1))
+    (when (numberp n)
+      (when (re-search-backward-lax-whitespace rexp)
+        (when  (= (+ 2 (* n 2)) (length (match-data)))
+          (match-string-no-properties n))))))
 
 (defun jasmine-coffee-ng/before-each-module-name ()
   "Find the name of a module included by a previous beforeEach"
