c6677d381849624b8e92f5bd3d6148994ceca267
jason <jason@gitlab.(none)>
2013-09-27 00:59:49 +0000

    updated

--- elpa/icicles-20130916.1701/icicles-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "icicles" "20130916.1701" "Minibuffer input completion and cycling. [wiki]" 'nil)
-
--- elpa/rspec-mode-20130810.1844/rspec-mode-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "rspec-mode" "20130810.1844" "Enhance ruby-mode for RSpec [github]"
-  '((ruby-mode "1.0")))
-
