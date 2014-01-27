131535896731d694cb1ae5ecb3e821cb681892d1
Tomek Maszkowski <tomazy@go2.pl>
2014-01-27 15:32:51 +0800

    Add exuberant ctags support
    Update projectile-rails init

---
+++ elpa/ctags-20110911.304/ctags-autoloads.el
@@ -0,0 +1,18 @@
+;;; ctags-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("ctags-pkg.el" "ctags.el") (21221 58032
+;;;;;;  175330 0))
+
+;;;***
+
+(provide 'ctags-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ctags-autoloads.el ends here
