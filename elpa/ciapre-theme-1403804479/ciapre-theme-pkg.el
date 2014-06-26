1a2e085e0eb7ede420816a605c595ff8f281a7cd
Jasonm23 <jasonm23@gmail.com>
2014-06-27 02:27:47 +0800

    Add auto-converted .tmThemes

---
+++ elpa/ciapre-theme-1403804479/ciapre-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ciapre-theme" "1403804479" "an Emacs 24 theme based on Ciapre (tmTheme)" (quote ((emacs "24.0"))))
---
+++ elpa/tech49-theme-1403804527/tech49-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "tech49-theme" "1403804527" "an Emacs 24 theme based on Tech49 (tmTheme)" (quote ((emacs "24.0"))))
