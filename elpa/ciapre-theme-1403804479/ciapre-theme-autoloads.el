78f46652af889a596ea8a60c7970adc9b15984f3
Jasonm23 <jasonm23@gmail.com>
2014-06-27 19:13:59 +0800

    scrub generated themes, prepare to install new ones

--- elpa/ciapre-theme-1403804479/ciapre-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; ciapre-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "ciapre-theme" "ciapre-theme.el" (21420 23470
-;;;;;;  0 0))
-;;; Generated autoloads from ciapre-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("ciapre-theme-pkg.el") (21420 23471 37693
-;;;;;;  0))
-
-;;;***
-
-(provide 'ciapre-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ciapre-theme-autoloads.el ends here
--- elpa/tech49-theme-1403804527/tech49-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "tech49-theme" "1403804527" "an Emacs 24 theme based on Tech49 (tmTheme)" (quote ((emacs "24.0"))))
