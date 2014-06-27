5a5dafeb731834651985e6e005fcd408a2a97ab9
jasonm23 <jasonm23@gmail.com>
2014-06-27 19:13:59 +0800

    scrub generated themes, prepare to install new ones

--- elpa/bone-a-kite-theme-1403805165/bone-a-kite-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bone-a-kite-theme" "1403805165" "an Emacs 24 theme based on Bone-a-kite (tmTheme)" (quote ((emacs "24.0"))))
--- elpa/ciapre-black-theme-1403804505/ciapre-black-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; ciapre-black-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "ciapre-black-theme" "ciapre-black-theme.el"
-;;;;;;  (21420 23479 0 0))
-;;; Generated autoloads from ciapre-black-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("ciapre-black-theme-pkg.el") (21420 23479
-;;;;;;  657388 0))
-
-;;;***
-
-(provide 'ciapre-black-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ciapre-black-theme-autoloads.el ends here
