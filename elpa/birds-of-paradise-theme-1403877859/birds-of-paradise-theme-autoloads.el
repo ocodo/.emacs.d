ec3aaff60b94e0bf2a670aa20f9ae00bf1f2aeb2
Jason Milkins <jasonm23@gmail.com>
2014-06-28 12:09:44 +0800

    Remove bop theme

--- elpa/birds-of-paradise-theme-1403877859/birds-of-paradise-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; birds-of-paradise-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "birds-of-paradise-theme" "birds-of-paradise-theme.el"
-;;;;;;  (21421 31642 0 0))
-;;; Generated autoloads from birds-of-paradise-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("birds-of-paradise-theme-pkg.el") (21421
-;;;;;;  31642 367330 0))
-
-;;;***
-
-(provide 'birds-of-paradise-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; birds-of-paradise-theme-autoloads.el ends here
--- elpa/birds-of-paradise-theme-1403877859/birds-of-paradise-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "birds-of-paradise-theme" "1403877859" "an Emacs 24 theme based on Birds of Paradise (tmTheme)" (quote ((emacs "24.0"))))
