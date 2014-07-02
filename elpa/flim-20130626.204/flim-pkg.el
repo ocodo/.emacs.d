1defb0f1eefc42d38a09c3275314332bc7a7c711
jasonm23 <jasonm23@gmail.com>
2014-07-02 14:06:12 +0800

    add Wunderlust

---
+++ elpa/flim-20130626.204/flim-pkg.el
@@ -0,0 +1,2 @@
+(define-package "flim" "20130626.204" "A library to provide basic features about message representation or encoding."
+  '((apel "10.8")))
---
+++ elpa/semi-20130719.2051/semi-autoloads.el
@@ -0,0 +1,13 @@
+;;; semi-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+(provide 'semi-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; semi-autoloads.el ends here
