5e713a07c1cf444a45ef97c047f2578f682d2b2d
Jason Milkins <pair+jason@neo.com>
2014-07-02 14:06:12 +0800

    add Wunderlust

---
+++ elpa/apel-20130823.2314/apel-pkg.el
@@ -0,0 +1,1 @@
+(define-package "apel" "20130823.2314" "APEL (A Portable Emacs Library) provides support for portable Emacs Lisp programs" 'nil)
---
+++ elpa/semi-20130719.2051/semi-pkg.el
@@ -0,0 +1,2 @@
+(define-package "semi" "20130719.2051" "A library to provide MIME features."
+  '((flim "1.14.2")))
