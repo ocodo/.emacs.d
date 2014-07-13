b50025295f50822f69cbbf2fd96ee02764affc64
jasonm23 <jasonm23@gmail.com>
2014-07-14 00:22:18 +0800

    Kill personally maintained dired details, dirtree and edit-server. update init-dired.

--- modes-init/init-dired.el
+++ modes-init/init-dired.el
@@ -1,20 +1,18 @@
 ;; Dired mode init
 (require 'dired-details+)
 
-(defun dired-narrow-window ()
-  "make the current dired mode window 30 chars wide"
-  (interactive)
-  (adjust-window-trailing-edge (selected-window) (- 30 (window-width)) t))
+(setq dired-details-initially-hide t)
 
 (add-hook 'dired-mode-hook
-    '(lambda()
+    '(lambda ()
          (visual-line-mode 0) ;; unwrap lines.
-         (linum-mode 0) ;; turn off line numbers.
-         (auto-revert-mode) ;; auto-refresh dired
-         (define-key dired-mode-map [mouse-3] 'dired-maybe-insert-subdir)
-       (define-key dired-mode-map (kbd "C-{") 'dired-narrow-window)
+         (linum-mode 0)       ;; turn off line numbers.
+         (auto-revert-mode)   ;; auto-refresh dired
          )
       )
+
+(add-hook 'dired-after-readin-hook
+          #'(lambda () (dired-details-activate)))
 
 (autoload 'dirtree "dirtree" "Add directory to tree view" t)
 
