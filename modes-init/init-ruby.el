8fdbf71e88770033aef29098f9620d7d3720aa2a
Jasonm23 <jasonm23@gmail.com>
2014-07-16 21:29:17 +0800

    fix ruby init error

--- modes-init/init-ruby.el
+++ modes-init/init-ruby.el
@@ -10,11 +10,11 @@
   (rvm-activate-corresponding-ruby))
 ;; (add-hook 'ruby-mode-hook 'company-robe)
 
-(add-hook 'ruby-mode-hook #'(lambda()
+(add-hook 'ruby-mode-hook (lambda()
                               (define-key ruby-mode-map (kbd "C-c #") 'ruby-make-interpolated-string-at-point-or-region)
                               (define-key ruby-mode-map (kbd "C-c :") 'ruby-toggle-symbol-name)
                               (define-key ruby-mode-map (kbd "C-c {") 'ruby-toggle-block)
                               (define-key ruby-mode-map (kbd "C-c +") 'ruby-toggle-hash-syntax)
-                              )
+                              ))
 
 (provide 'init-ruby)
