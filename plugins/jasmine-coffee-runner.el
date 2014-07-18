9fa47f035276b7f5f0092ed2e0dd2534817b26cb
jasonm23 <jasonm23@gmail.com>
2014-07-18 12:10:15 +0800

    v0.1.0 of jasmine-coffee-runnner.el

--- plugins/jasmine-coffee-runner.el
+++ plugins/jasmine-coffee-runner.el
@@ -1,33 +1,38 @@
 ;;; jasmine-coffee-runner --- Simple test launcher for Jasmine (in coffeescript).
+;;; Author: Jason Milkins
+;;; Version: 201407181200
 ;;; Commentary:
 ;;
-;;  Launch a Jasmine coffeescript spec at the cursor position,
-;;  effectively we just generate the required spec URL and browse
-;;  it, unbound, just run:
+;;  This file is not a part of Emacs
+;;
+;;  This launches a Jasmine coffeescript spec above the point.
 ;;
 ;;    M-x jasmine-coffee/verify-single
 ;;
 ;;  Example.
 ;;
-;;  create/browse this url: localhost:3000/jasmine?spec=mockup%20is%20the%20one%20we%20want
 ;;  When our cursor is at the point noted below...
 ;;
-;;  describe "mockup", ->
+;;  describe "base level", ->
 ;;
-;;    describe "we skip this", ->
+;;    describe "one level deep", ->
 ;;
-;;      describe "we also skip this", ->
+;;      describe "we skip this", ->
 ;;
 ;;        # oh just ignore me...
-;;        it "could be anything up here, we don't care", ->
+;;        it "we skip this it definition", ->
 ;;          expect(nothing).toBeHappening()
 ;;
-;;    it "is the one we want... ", ->
-;;      expect(something).toBe somethingElse
-;;      # My cursor is here...
+;;      it "is the one we #want...", ->
+;;        expect(something).toBe somethingElse
+;;        # My cursor is here...
+;;        # note, we must replace # in the spec name.
 ;;
-;;    it "is a red herring", ->
-;;      expect(thing).toBe defined
+;;  expect:
+;;    jasmine-coffee/base-url +
+;;    "base%20level%20one%20level%20deep%20is%20the%20one%20we%20%23want..."
+;;
+;;; Licence: GPL3
 ;;
 ;;; Code:
 
@@ -36,65 +41,50 @@
   "Base URL for our Jasmine spec runner.")
 
 (defvar jasmine-coffee/it-regexp
-  "it(? ?[\"']\\(.*\\)\\(?:[\"'],\\) ?)?"
+  (rx
+   "it"
+   (any " " "(")
+   (zero-or-more " ")
+   (any "'" "\"")
+   (group (zero-or-more not-newline))
+   (any "'" "\"")
+   (? ")")
+   (zero-or-more " ")
+   ","
+   )
   "Regexp to find a jasmine coffee-mode `it'.")
 
 (defvar jasmine-coffee/describe-regexp
-  "describe(? ?[\"']\\(.*\\)\\(?:[\"'],\\) ?)?"
+  (rx
+   "describe"
+   (any " " "(")
+   (zero-or-more " ")
+   (any "'" "\"")
+   (group (zero-or-more not-newline))
+   (any "'" "\"")
+   (? ")")
+   (zero-or-more " ")
+   ","
+   )
   "Regexp to find a jasmine coffee-mode `describe'.")
-
-(defun jasmine-coffee/get-previous-describe-marker-and-description ()
-  "Find previous Jasmine `describe', return start marker and description string."
-  (save-excursion
-    (with-demoted-errors
-      (re-search-backward jasmine-coffee/describe-regexp 0 t )
-      (destructuring-bind (start end description-start description-end) (match-data)
-        (list start (buffer-substring-no-properties description-start description-end))))))
-
-(defun jasmine-coffee/get-previous-it-marker-and-description ()
-  "Find the nearest Jasmine `it' definition, return start marker and description string."
-  (save-excursion
-    (with-demoted-errors
-    (re-search-backward jasmine-coffee/it-regexp)
-    (destructuring-bind (start end description-start description-end) (match-data)
-      (list start (buffer-substring-no-properties description-start description-end))))))
-
-(defun jasmine-coffee/get-column-from-marker (marker)
-  "Get the column number at the MARKER position."
-  (save-excursion
-    (goto-char (marker-position marker))
-    (current-column)))
-
-(defun jasmine-coffee/get-previous-describes-outdented-from-column (marker column)
-  "Go to the MARKER and `re-search-backward' for all previous 'describe-regexp'.
-Filter and keep only where column is less than COLUMN."
-  (save-excursion
-    (goto-char (marker-position marker))
-    (destructuring-bind
-        (new-marker string)
-        (jasmine-coffee/get-previous-describe-marker-and-description )
-
-      (when (< (jasmine-coffee/get-column-from-marker new-marker) column)
-          (setq description string))
-      (list new-marker description))
-    ))
 
 (defun jasmine-coffee/verify-single ()
   "Compose the Spec URL launch a browser and run the spec at the cursor point."
   (interactive)
-  (save-excursion
-    ;; Get the previous it
-    (let* ((start-column 0) (spec-string ""))
-      (destructuring-bind
-        (marker string)
-        (jasmine-coffee/get-previous-it-marker-and-description)
-        (setq start-column (jasmine-coffee/get-column-from-marker marker))
+  (let* ((start-column 0) (spec-string ""))
+    (save-excursion
+      (re-search-backward jasmine-coffee/it-regexp)
+      (setq start-column (current-column))
+      (setq spec-string (match-string-no-properties 1))
 
-        ;; TODO: Get all previous describes.
-
-        )
-
-    )))
+      (while
+          (re-search-backward jasmine-coffee/describe-regexp 0 t)
+        (when (< (current-column) start-column)
+          (setq start-column (current-column))
+          (setq spec-string (format "%s %s" (match-string 1) spec-string)))))
+    (setq spec-string (replace-regexp-in-string "#" "%23" spec-string))
+    (browse-url (url-encode-url (concat jasmine-coffee/base-url spec-string)))))
 
 (provide 'jasmine-coffee-runner)
+
 ;;; jasmine-coffee-runner.el ends here
