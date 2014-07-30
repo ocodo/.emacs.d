17a565a1c27cedc91873e91e2e69d5c691b9f38d
Jason Milkins <jasonm23@gmail.com>
2014-07-30 13:20:33 +0800

    Jasmine-coffee additional features - Var to jset/jlet - Move line/region up to describe/beforeEach - Browse describe/group - Browse it/single

--- modes-init/init-coffee.el
+++ modes-init/init-coffee.el
@@ -1,9 +1,8 @@
-(require 'jasmine-coffee-runner)
-(eval-after-load 'coffee-mode
-  (lambda ()
-    (define-key coffee-mode-map (kbd "C-c C-n") 'flymake-goto-next-error)
-    (define-key coffee-mode-map (kbd "C-c C-p") 'flymake-goto-prev-error)
-    (define-key coffee-mode-map (kbd "C-c C-,") 'coffee-indent-shift-left)
-    (define-key coffee-mode-map (kbd "C-c C-.") 'coffee-indent-shift-right)))
+(require 'jasmine-coffee)
+
+(define-key coffee-mode-map (kbd "C-c C-n") 'flymake-goto-next-error)
+(define-key coffee-mode-map (kbd "C-c C-p") 'flymake-goto-prev-error)
+(define-key coffee-mode-map (kbd "C-c C-,") 'coffee-indent-shift-left)
+(define-key coffee-mode-map (kbd "C-c C-.") 'coffee-indent-shift-right)
 
 (provide 'init-coffee)
