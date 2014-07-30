45d9038ac92725470375b56399a1312ceabac2be
Jason Milkins <jasonm23@gmail.com>
2014-07-30 13:28:45 +0800

    Fix init-coffee

--- modes-init/init-coffee.el
+++ modes-init/init-coffee.el
@@ -1,8 +1,11 @@
 (require 'jasmine-coffee)
 
-(define-key coffee-mode-map (kbd "C-c C-n") 'flymake-goto-next-error)
-(define-key coffee-mode-map (kbd "C-c C-p") 'flymake-goto-prev-error)
-(define-key coffee-mode-map (kbd "C-c C-,") 'coffee-indent-shift-left)
-(define-key coffee-mode-map (kbd "C-c C-.") 'coffee-indent-shift-right)
+(eval-after-load 'coffee-mode
+  (lambda ()
+    (define-key coffee-mode-map (kbd "C-c C-n") 'flymake-goto-next-error)
+    (define-key coffee-mode-map (kbd "C-c C-p") 'flymake-goto-prev-error)
+    (define-key coffee-mode-map (kbd "C-c C-,") 'coffee-indent-shift-left)
+    (define-key coffee-mode-map (kbd "C-c C-.") 'coffee-indent-shift-right)
+    ))
 
 (provide 'init-coffee)
