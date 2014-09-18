193ad267fb61977e34347ea9b7946491ed7cbbc9
Jason Milkins <jasonm23@gmail.com>
2014-09-18 06:26:38 +0000

    Remove morning pages. To be replaced by org-daypage

--- elpa/mpages-20140915.1338/mpages-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; mpages-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("mpages.el") (21528 64989 455771 105000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; mpages-autoloads.el ends here
