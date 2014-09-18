54ccf152ed2e3461dab46c6eb2e9f34cbf1188fe
Jason Milkins <jasonm23@gmail.com>
2014-09-18 06:16:18 +0000

    Added a few dark themes

---
+++ elpa/darkburn-theme-20140510.1449/darkburn-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "darkburn-theme" "20140510.1449" "A not-so-low contrast color theme for Emacs." 'nil)
---
+++ elpa/darkmine-theme-20140829.714/darkmine-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "darkmine-theme" "20140829.714" "Yet another emacs dark color theme." 'nil)
