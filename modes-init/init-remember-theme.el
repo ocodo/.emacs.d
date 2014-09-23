56f32ce9cd194ac9e9b3bc6054614c8c0cc846af
Jia Hen <jia@opsmanager.com>
2014-09-23 14:45:31 +0800

    Remove linum height from remember theme hook

--- modes-init/init-remember-theme.el
+++ modes-init/init-remember-theme.el
@@ -2,7 +2,6 @@
 (add-hook 'remember-theme-after-load-hook
           (lambda ()
             (set-face-attribute 'default nil :height 180)
-            (set-face-attribute 'linum nil :height 110)
             (armitp-mode-line)
             ))
 
