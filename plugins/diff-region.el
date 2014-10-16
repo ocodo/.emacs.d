27e45aabe01defcaeff5e5fe4440d10587fc5ee6
jasonm23 <jasonm23@gmail.com>
2014-10-16 11:18:10 +0800

    Add diff-region and fix spyOn andCallThrough snippet

---
+++ plugins/diff-region.el
@@ -0,0 +1,26 @@
+;;;###autoload
+(defun diff-region ()
+  "Select a region to compare."
+  (interactive)
+  (when (use-region-p)  ; there is a region
+        (let (buf)
+          (setq buf (get-buffer-create "*Diff-regionA*"))
+          (save-current-buffer
+            (set-buffer buf)
+            (erase-buffer))
+          (append-to-buffer buf (region-beginning) (region-end))))
+  (message "Now select other region to compare and run `diff-region-now`"))
+
+;;;###autoload
+(defun diff-region-now ()
+  "Compare current region with region already selected by `diff-region`."
+  (interactive)
+  (when (use-region-p)
+        (let (bufa bufb)
+          (setq bufa (get-buffer-create "*Diff-regionA*"))
+          (setq bufb (get-buffer-create "*Diff-regionB*"))
+          (save-current-buffer
+            (set-buffer bufb)
+            (erase-buffer))
+          (append-to-buffer bufb (region-beginning) (region-end))
+          (ediff-buffers bufa bufb))))
