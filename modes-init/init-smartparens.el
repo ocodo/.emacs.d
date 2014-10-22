df042919d613dd6b7cd26821b24c8cf6f1dec3af
Jason Milkins <jason@opsmanager.com>
2014-10-22 12:27:20 +0800

    Temporary fix for cua-replace-region in smart-parens

--- modes-init/init-smartparens.el
+++ modes-init/init-smartparens.el
@@ -4,4 +4,16 @@
 (sp-pair "<%=" "%>")
 (sp-pair "<%" "%>")
 
+(unless (fboundp 'cua-replace-region)
+  (defun cua-replace-region ()
+    "Replace the active region with the character you type."
+    (interactive)
+    (let ((not-empty (and cua-delete-selection (cua-delete-region))))
+      (unless (eq this-original-command this-command)
+        (let ((overwrite-mode
+               (and overwrite-mode
+                    not-empty
+                    (not (eq this-original-command 'self-insert-command)))))
+          (cua--fallback))))))
+
 (provide 'init-smartparens)
