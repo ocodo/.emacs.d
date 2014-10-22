4c12125496163632afb1191ce1c4eaf8b3971010
Jason Milkins <jason@opsmanager.com>
2014-10-22 12:29:39 +0800

    Remove helm-anything

--- elpa/helm-anything-20131217.1832/helm-anything-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; helm-anything-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (helm-anything-resume) "helm-anything" "helm-anything.el"
-;;;;;;  (21356 52954 0 0))
-;;; Generated autoloads from helm-anything.el
-
-(autoload 'helm-anything-resume "helm-anything" "\
-Resurrect previously invoked `helm' or `anything'.
-Called with a prefix arg, allow choosing among all existing
-helm/anything buffers.  i.e choose among various helm/anything sessions.
-Called from lisp, you can specify a buffer-name as a string with PARG.
-
-\(fn PARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("helm-anything-pkg.el") (21356 52954 250530
-;;;;;;  0))
-
-;;;***
-
-(provide 'helm-anything-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; helm-anything-autoloads.el ends here
