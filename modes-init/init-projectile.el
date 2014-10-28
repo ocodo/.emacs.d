848e5cd45128e6d58a0069d0e22f534fd3a31663
jasonm23 <jasonm23@gmail.com>
2014-10-29 01:24:32 +0800

    Open last projectile project function added

--- modes-init/init-projectile.el
+++ modes-init/init-projectile.el
@@ -1,1 +1,8 @@
 (projectile-global-mode t)
+
+(defun projectile-open-last-known-project ()
+    "Open the root folder of the last known project."
+    (interactive)
+    (find-file (car (projectile-load-known-projects))))
+
+(provide 'init-projectile)
