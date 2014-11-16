111a346d5cdb375ed4e2d05d531937baa300c1ef
Jasonm23 <jasonm23@gmail.com>
2014-11-16 16:32:26 +0800

    Ditch Darcula theme

--- elpa/darcula-theme-20141114.312/darcula-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "darcula-theme" "20141114.312" "Inspired by IntelliJ's Darcula theme" 'nil :url "https://github.com/fommil/darcula-theme-emacs" :keywords '("faces"))
