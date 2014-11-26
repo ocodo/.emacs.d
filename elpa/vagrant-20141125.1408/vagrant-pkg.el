082f1027a8127256344ba09a9b75193b67adc18c
Jason Milkins <jasonm23@gmail.com>
2014-11-26 16:39:09 +0000

    Update packages - inc. gruvbox theme update

--- elpa/vagrant-20141125.1408/vagrant-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "vagrant" "20141125.1408" "Manage a vagrant box from emacs" 'nil :url "https://github.com/ottbot/vagrant.el" :keywords '("vagrant" "chef"))
