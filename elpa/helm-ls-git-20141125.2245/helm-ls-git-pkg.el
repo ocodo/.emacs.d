d089bf210791ae22ce9e7fe18830146334ee8753
jasonm23 <jasonm23@gmail.com>
2014-11-26 16:39:09 +0000

    Update packages - inc. gruvbox theme update

---
+++ elpa/helm-ls-git-20141125.2245/helm-ls-git-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-ls-git" "20141125.2245" "list git files." '((helm "1.5")))
---
+++ elpa/vagrant-20141125.1959/vagrant-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vagrant" "20141125.1959" "Manage a vagrant box from emacs" 'nil :url "https://github.com/ottbot/vagrant.el" :keywords '("vagrant" "chef"))
