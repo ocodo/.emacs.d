97c69e4e835f2aa3d95552b8688d3a47290bc618
Jason Milkins <jasonm23@gmail.com>
2014-12-12 01:14:07 +0000

    Removed Marmalade and e6h from the list of ELPA registries

--- modes-init/init-elpa.el
+++ modes-init/init-elpa.el
@@ -5,13 +5,9 @@
   (require 'package)
 
   (setq package-archives
-        '(
-          ("melpa"        . "http://melpa.milkbox.net/packages/")
+        '(("melpa"        . "http://melpa.milkbox.net/packages/")
           ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-          ("marmalade"    . "http://marmalade-repo.org/packages/")
-          ("e6h"          . "http://www.e6h.org/packages/") ;; Wanderlust
-          ("gnu"          . "http://elpa.gnu.org/packages/")
-          ))
+          ("gnu"          . "http://elpa.gnu.org/packages/")))
 
   (package-initialize)) ;; init elpa packages
 
