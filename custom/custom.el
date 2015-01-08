e9367f80789f2cea082226ff94d3e96973f13689
Jason Milkins <jasonm23@gmail.com>
2015-01-08 17:27:29 +0800

    Regenerate custom.el - change markdown command to kramdown GFM

--- custom/custom.el
+++ custom/custom.el
@@ -4,8 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#1F1611" "#660000" "#144212" "#EFC232" "#5798AE" "#BE73FD" "#93C1BC" "#E6E1DC"])
- '(ansi-term-color-vector [unspecified "#1F1611" "#660000" "#144212" "#EFC232" "#5798AE" "#BE73FD" "#93C1BC" "#E6E1DC"])
+ '(ansi-color-names-vector
+   ["#1F1611" "#660000" "#144212" "#EFC232" "#5798AE" "#BE73FD" "#93C1BC" "#E6E1DC"])
+ '(ansi-term-color-vector
+   [unspecified "#1F1611" "#660000" "#144212" "#EFC232" "#5798AE" "#BE73FD" "#93C1BC" "#E6E1DC"])
  '(auto-install-save-confirm nil)
  '(blink-cursor-mode nil)
  '(column-number-mode t)
@@ -21,7 +23,12 @@
  '(ediff-diff3-program "diff3")
  '(fci-rule-character-color "#202020")
  '(fci-rule-color "#202020")
- '(fiplr-ignored-globs (quote ((directories (".git" ".svn" ".hg" ".bzr" "tmp" "log")) (files (".#*" "*~" "*.so" "*.jpg" "*.png" "*.gif" "*.pdf" "*.gz" "*.zip" ".DS_Store" ".gitkeep")))))
+ '(fiplr-ignored-globs
+   (quote
+    ((directories
+      (".git" ".svn" ".hg" ".bzr" "tmp" "log"))
+     (files
+      (".#*" "*~" "*.so" "*.jpg" "*.png" "*.gif" "*.pdf" "*.gz" "*.zip" ".DS_Store" ".gitkeep")))))
  '(glasses-original-separator "-")
  '(glasses-separate-parentheses-p nil)
  '(glasses-separator "-")
@@ -29,7 +36,9 @@
  '(global-auto-revert-mode t)
  '(global-hl-line-mode nil)
  '(global-undo-tree-mode t)
- '(grep-find-ignored-directories (quote ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "tmp" "log" "vendor")))
+ '(grep-find-ignored-directories
+   (quote
+    ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "tmp" "log" "vendor")))
  '(indent-tabs-mode nil)
  '(indicate-buffer-boundaries (quote right))
  '(inhibit-startup-screen t)
@@ -44,9 +53,12 @@
  '(linum-eager t)
  '(lua-indent-level 2)
  '(make-backup-files nil)
- '(markdown-command "markdown")
+ '(markdown-command "kramdown -i GFM")
  '(markdown-css-path "markdown.css")
- '(newsticker-url-list (quote (("Hacker News" "https://news.ycombinator.com/rss" nil nil nil) ("Emacs Reddit" "http://www.reddit.com/r/emacs.rss" nil nil nil))))
+ '(newsticker-url-list
+   (quote
+    (("Hacker News" "https://news.ycombinator.com/rss" nil nil nil)
+     ("Emacs Reddit" "http://www.reddit.com/r/emacs.rss" nil nil nil))))
  '(paradox-automatically-star t)
  '(paradox-github-token t)
  '(recentf-max-menu-items 30)
@@ -54,7 +66,35 @@
  '(recentf-menu-title "Recent Files...")
  '(recentf-mode t)
  '(rinari-rails-env "development")
- '(safe-local-variable-values (quote ((eval progn (message "Setting project specific key bindings") (global-set-key [24 down] (quote duplicate-current-line-or-region)) (global-set-key "i" (quote iedit-mode))) (rainbow-mode . 1) (rainbow-mode . t) (eval when (fboundp (quote rainbow-mode)) (rainbow-mode 1)) (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t))))
+ '(safe-local-variable-values
+   (quote
+    ((eval progn
+           (message "Setting project specific key bindings")
+           (global-set-key
+            [24 down]
+            (quote duplicate-current-line-or-region))
+           (global-set-key "i"
+                           (quote iedit-mode)))
+     (rainbow-mode . 1)
+     (rainbow-mode . t)
+     (eval when
+           (fboundp
+            (quote rainbow-mode))
+           (rainbow-mode 1))
+     (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
+           (add-hook
+            (quote write-contents-functions)
+            (lambda nil
+              (delete-trailing-whitespace)
+              nil))
+           (require
+            (quote whitespace))
+           "Sometimes the mode needs to be toggled off and on."
+           (whitespace-mode 0)
+           (whitespace-mode 1))
+     (whitespace-line-column . 80)
+     (whitespace-style face trailing lines-tail)
+     (require-final-newline . t))))
  '(scroll-bar-mode nil)
  '(scss-compile-at-save nil)
  '(send-mail-function (quote sendmail-send-it))
@@ -65,18 +105,29 @@
  '(visible-bell t)
  '(whitespace- nil)
  '(window-left-margin 0)
- '(yas-prompt-functions (quote (yas-dropdown-prompt yas/x-prompt yas/completing-prompt yas/ido-prompt yas/no-prompt)))
- '(zoneinfo-style-world-list (quote (("Australia/Sydney" "Sydney") ("Europe/London" "London") ("Asia/Bangkok" "Bangkok") ("America/Vancouver" "Vancouver") ("America/Los_Angeles" "San Fransisco") ("America/New_York" "New York") ("Asia/Shanghai" "Shanghai") ("Asia/Tokyo" "Tokyo")))))
+ '(yas-prompt-functions
+   (quote
+    (yas-dropdown-prompt yas/x-prompt yas/completing-prompt yas/ido-prompt yas/no-prompt)))
+ '(zoneinfo-style-world-list
+   (quote
+    (("Australia/Sydney" "Sydney")
+     ("Europe/London" "London")
+     ("Asia/Bangkok" "Bangkok")
+     ("America/Vancouver" "Vancouver")
+     ("America/Los_Angeles" "San Fransisco")
+     ("America/New_York" "New York")
+     ("Asia/Shanghai" "Shanghai")
+     ("Asia/Tokyo" "Tokyo")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "Helvetica Neue"))) t)
- '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 2.0))) t)
- '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.9))) t)
- '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.8))) t)
- '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.7))) t)
- '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 1.6))) t)
- '(markdown-header-face-6 ((t (:inherit markdown-header-face :height 1.5))) t)
+ '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "Helvetica Neue"))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 2.0))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.9))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.8))))
+ '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.7))))
+ '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 1.6))))
+ '(markdown-header-face-6 ((t (:inherit markdown-header-face :height 1.5))))
  '(stripe-highlight ((t (:background "#181818"))) t))
