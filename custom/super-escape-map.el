dfe489347c425577bd4befb6983fbff63e4ab701
Jason Milkins <jasonm23@gmail.com>
2015-01-18 22:58:09 +0800

    Fix super-escape-map

--- custom/super-escape-map.el
+++ custom/super-escape-map.el
@@ -1,6 +1,7 @@
 (defvar super-escape-map (make-keymap)
   "Custom Keymap for user bindings prefixed by s-ESC.")
-(define-key global-map (kbd "s-ESC") 'super-escape-map)
+(fset 'super-escape-map super-escape-map)
+(define-key global-map [s-escape] 'super-escape-map)
 
 (define-key super-escape-map "o" 'open-opsmanager)
 (define-key super-escape-map "v" 'jasmine-coffee/verify-suite)
