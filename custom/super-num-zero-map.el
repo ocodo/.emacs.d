5402b25dabd165a45b8bbfdfe320cbbd36d5c0ac
jasonm23 <jasonm23@gmail.com>
2015-01-19 10:11:25 +0800

    Move super-escape-map to super-num-zero-map

---
+++ custom/super-num-zero-map.el
@@ -0,0 +1,10 @@
+(defvar super-num-zero-map (make-keymap)
+  "Custom Keymap for user bindings prefixed by s-kp-0.")
+(fset 'super-num-zero-map super-num-zero-map)
+(define-key global-map [s-kp-0] 'super-num-zero-map)
+
+(define-key super-num-zero-map "o" 'open-opsmanager)
+(define-key super-num-zero-map "v" 'jasmine-coffee/verify-suite)
+(define-key super-num-zero-map "r" 'reload-current-chrome-tab-osx)
+
+(provide 'super-num-zero-map)
