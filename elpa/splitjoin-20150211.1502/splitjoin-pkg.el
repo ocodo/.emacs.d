cf068a5f3eaa908776d965f0b2d6e2d303c3e633
jasonm23 <jasonm23@gmail.com>
2015-02-12 01:08:02 +0000

    added splitjoin

---
+++ elpa/splitjoin-20150211.1502/splitjoin-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "splitjoin" "20150211.1502" "Transition between multiline and single-line code" '((cl-lib "0.5")) :url "https://github.com/syohex/emacs-splitjoin")
