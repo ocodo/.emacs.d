defb6565205d6630ee61ff1d3cfe53b5b8e95457
Jason Milkins <jasonm23@gmail.com>
2015-03-07 13:04:30 +0000

    Upgrade packages

--- elpa/golden-ratio-20141208.844/golden-ratio-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "golden-ratio" "20141208.844" "Automatic resizing of Emacs windows to the golden ratio" 'nil :keywords '("window" "resizing"))
---
+++ elpa/lusty-explorer-20150306.1915/lusty-explorer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lusty-explorer" "20150306.1915" "Dynamic filesystem explorer and buffer switcher" 'nil :keywords '("convenience" "files" "matching"))
