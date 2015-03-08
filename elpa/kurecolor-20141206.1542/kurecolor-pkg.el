6c0a84ef67829e64597d22259864553f99e88d72
Jason Milkins <jasonm23@gmail.com>
2015-03-08 13:12:23 +0000

    Update packages

--- elpa/kurecolor-20141206.1542/kurecolor-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "kurecolor" "20141206.1542" "color editing goodies for Emacs" '((emacs "24.1") (s "1.0")))
---
+++ elpa/spinner-1.0/spinner-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "spinner" "1.0" "Add spinners and progress-bars to the mode-line for ongoing operations" '((cl-lib "0.5")) :url "https://github.com/Bruce-Connor/spinner.el" :keywords '("processes" "mode-line"))
