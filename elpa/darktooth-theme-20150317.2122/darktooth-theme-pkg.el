b63812fc945588d85a3c3109e3d3e41b4c9ce824
Jasonm23 <jasonm23@gmail.com>
2015-03-19 00:18:13 +0800

    Added Darktooth

---
+++ elpa/darktooth-theme-20150317.2122/darktooth-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "darktooth-theme" "20150317.2122" "colour theme for Emacs, a remix of gruvbox" 'nil :url "http://github.com/emacsfodder/emacs-darktooth-theme")
