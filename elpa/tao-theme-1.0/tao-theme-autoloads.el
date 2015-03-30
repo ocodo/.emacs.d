b719c05bf47436c1dcd0b5a460be919f9b7305c5
jasonm23 <jasonm23@gmail.com>
2015-03-31 01:22:56 +0800

    Add Tao Theme

---
+++ elpa/tao-theme-1.0/tao-theme-autoloads.el
@@ -0,0 +1,20 @@
+;;; tao-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "tao-theme" "tao-theme.el" (21785 34299 0 0))
+;;; Generated autoloads from tao-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(add-to-list 'safe-local-eval-forms '(when (require 'rainbow-mode nil t) (rainbow-mode 1)))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; tao-theme-autoloads.el ends here
