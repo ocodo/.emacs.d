0c0848f52b7aad18844aa6c334df5f6e7577599a
Jason Milkins <jason@opsmanager.com>
2015-04-04 14:06:41 +0800

    Support for anzu and swiper

--- modes-init/init-projectile.el
+++ modes-init/init-projectile.el
@@ -5,4 +5,9 @@
     (interactive)
     (find-file (car (projectile-load-known-projects))))
 
+;; Projectile find file - flx based
+(global-set-key (kbd "M-P")                 'projectile-find-file)
+(global-set-key (kbd "M-p")                 'projectile-find-file)
+(global-set-key (kbd "C-x p")               'projectile-find-file)
+
 (provide 'init-projectile)
