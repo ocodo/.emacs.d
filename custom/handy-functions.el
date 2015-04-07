0882332db17dce927a24f9d17266adf9ff344abb
jasonm23 <jasonm23@gmail.com>
2015-04-07 08:24:16 +0800

    Add ruby-selected-to-try-call

--- custom/handy-functions.el
+++ custom/handy-functions.el
@@ -432,6 +432,9 @@ the buffer."
   (if (s-matches? "^:.*" s)
       (s-replace ":" "" s)
     (ruby-prepend-colon s)))
+    
+;; Saved macro to replace selection with try(:selection)
+(fset 'ruby-selected-to-try-call [?\C-w ?t ?r ?y ?\( ?: ?\C-y right])    
 
 (defun pcre-regexp-from-list-of-words (words)
   "Insert a pcre regexp to match a list of WORDS."
