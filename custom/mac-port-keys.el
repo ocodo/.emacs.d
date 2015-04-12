533b62be9be80eb0fbd229bc3a6ed5a1e8af213a
jasonm23 <jasonm23@gmail.com>
2015-04-12 12:45:19 +0700

    Stage 2 of Company mode trial - enable elisp, robe, yasnippet backends.
    - Also map Mac Fn to Hyper

--- custom/mac-port-keys.el
+++ custom/mac-port-keys.el
@@ -20,8 +20,7 @@
    mac-control-modifier  'control
    mac-option-modifier   'meta
    mac-command-modifier  'super
-   ;; mac-function-modifier 'hyper ;; use with some keyboards.
-   ;; TOOD: Setup a toggle.
+   mac-function-modifier 'hyper
    ))
 
 ;; Fullscreen mode toggle in emacs mac. This invokes the Kiosk mode
--- modes-init/init-company.el
+++ modes-init/init-company.el
@@ -1,4 +1,15 @@
-(global-company-mode 1)
-(company-quickhelp-mode 1)
+;;; Commentary
+;;; Code:
+
+(eval-after-load "company-mode"
+  (progn
+    (global-company-mode 1)
+    (company-quickhelp-mode 1)
+    (dolist (backend (list 'company-robe
+                           'company-elisp
+                           'company-yasnippet))
+      (add-to-list 'company-backends backend))))
 
 (provide 'init-company)
+
+;;; init-company ends here
