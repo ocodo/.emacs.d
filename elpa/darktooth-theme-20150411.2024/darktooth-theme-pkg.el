0a3b33987dca7fabb230a3269bb1b13ec6249082
jasonm23 <jasonm23@gmail.com>
2015-04-12 05:49:20 +0000

    Update packages

---
+++ elpa/darktooth-theme-20150411.2024/darktooth-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "darktooth-theme" "20150411.2024" "colour theme for Emacs, a remix of gruvbox" 'nil :url "http://github.com/emacsfodder/emacs-theme-darktooth")
--- elpa/github-browse-file-20141102.1040/github-browse-file-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "github-browse-file" "20141102.1040" "View the file you're editing on GitHub" '((cl-lib "0.5")))
