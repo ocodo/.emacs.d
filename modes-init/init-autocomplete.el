c8cabff3fc8f3b704b5b5acb5b22c8ecaaaac549
jasonm23 <jasonm23@gmail.com>
2015-04-20 15:58:17 +0000

    Add filename completion to default autocomplete sources

--- modes-init/init-autocomplete.el
+++ modes-init/init-autocomplete.el
@@ -4,6 +4,10 @@
 (require 'dropdown-list)
 
 (ac-config-default)
+
 (setf ac-delay 0.25)
+
+(defun ac-common-setup ()
+  (add-to-list 'ac-sources 'ac-source-filename))
 
 (provide 'init-autocomplete)
