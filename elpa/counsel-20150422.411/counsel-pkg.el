07191f2a0a7a47d3b4bc632e1682f57ca13a4852
Jason Milkins <jason@opsmanager.com>
2015-04-23 02:31:13 +0800

    Switching to SVG modeline, turning off Armit P Modeline Hack
    - Also adding Counsel, Ivy functions
    - Updating Darktooth theme

---
+++ elpa/counsel-20150422.411/counsel-pkg.el
@@ -0,0 +1,1 @@
+(define-package "counsel" "20150422.411" "Various completion functions using Ivy" '((emacs "24.1") (swiper "0.3.0")) :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
