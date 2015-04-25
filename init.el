ec630195ea683efed09360b1e2c15ad151c50ac3
Jason Milkins <jason@opsmanager.com>
2015-04-25 18:44:23 +0800

    Remove requires in favor of load-library

--- init.el
+++ init.el
@@ -7,12 +7,12 @@
 ;;   \___/ \___\___/ \__,_|\___/   \___|_| |_| |_|\__,_|\___|___/  \__,_|\___/ \__|
 ;;
 ;;; Code:
-(require 'cl)
+(load-library "cl")
 
 (let ((default-directory user-emacs-directory))
   (add-to-list 'load-path (expand-file-name "init-helpers")))
 
-(require 'init-helpers)
+(load-library "init-helpers")
 (init-set-custom)
 (manage-toolbar-and-menubar)
 (manage-history)
@@ -25,74 +25,74 @@
 
 ;; Explicit Requires ...
 (dolist (lib '(init-elpa
-             cl
-             cl-lib
-             s
-             f
-             dash
-             handy-functions
-             custom-keys
-             ag
-             armitp-mode-line
-             diff-region
-             highlight-indentation
-             hyde-autoloads
-             kurecolor
-             iedit
-             js2-refactor
-             kill-buffer-without-confirm
-             mac-frame-adjust
-             multiple-cursors
-             opl-coffee
-             resize-window
-             scroll-bell-fix
-             squeeze-view
-             switch-window
-             xterm-256-to-hex
-             super-num-zero-map))
+               cl
+               cl-lib
+               s
+               f
+               dash
+               handy-functions
+               custom-keys
+               ag
+               armitp-mode-line
+               diff-region
+               highlight-indentation
+               hyde-autoloads
+               kurecolor
+               iedit
+               js2-refactor
+               kill-buffer-without-confirm
+               mac-frame-adjust
+               multiple-cursors
+               opl-coffee
+               resize-window
+               scroll-bell-fix
+               squeeze-view
+               switch-window
+               xterm-256-to-hex
+               super-num-zero-map))
   (load-library (symbol-name lib)))
 
 ;; mode inits
 (dolist (init '(exec-path
-             asciidoc
-             autocomplete
-             anzu
-             buffer-clean
-             codenotes
-             coffee
-             ;; company ;; Sorry, no thank you company mode, still not comparably useful vs AC mode.
-             dired
-             elpa-themes
-             emacs-daemon
-             eww
-             flycheck
-             guide
-             git-gutter
-             haml
-             hideshowvis
-             json-mode
-             ido
-             ispell
-             markdown
-             misc-settings
-             moonscript
-             multi-web-mode
-             nxml
-             pivotal
-             projectile
-             projectile-rails
-             rainbow
-             rainbow-delimiters
-             ruby
-             rvm
-             remember-theme
-             sh
-             smartparens
-             smooth-scroll
-             swiper
-             winner
-             yaml
-             yasnippet))
+                asciidoc
+                autocomplete
+                anzu
+                buffer-clean
+                codenotes
+                coffee
+                ;; company ;; Sorry, no thank you company mode, still not comparably useful vs AC mode.
+                dired
+                elpa-themes
+                emacs-daemon
+                eww
+                flycheck
+                guide
+                git-gutter
+                haml
+                hideshowvis
+                json-mode
+                ido
+                ispell
+                markdown
+                misc-settings
+                moonscript
+                multi-web-mode
+                nxml
+                pivotal
+                projectile
+                projectile-rails
+                rainbow
+                rainbow-delimiters
+                ruby
+                rvm
+                remember-theme
+                sh
+                smartparens
+                smooth-scroll
+                swiper
+                winner
+                yaml
+                yasnippet))
   (load-mode-init (symbol-name init)))
 
 ;; When GUI (hopefully svg is available!)
