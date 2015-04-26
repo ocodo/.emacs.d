b92cd04df6bdaea6df5f12a68675e9729aa75cda
Jason Milkins <jasonm23@gmail.com>
2015-04-26 11:08:59 +0000

    Update packages

---
+++ elpa/helm-ls-git-20150425.553/helm-ls-git-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-ls-git" "20150425.553" "list git files." '((helm "1.5")))
---
+++ elpa/yagist-20150425.551/yagist-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yagist" "20150425.551" "Yet Another Emacs integration for gist.github.com" '((cl-lib "0.3")) :url "https://github.com/mhayashi1120/yagist.el" :keywords '("tools"))
