3ed227ba8197b5f169d5cdf08599614c4da6c2f3
jasonm23 <jasonm23@gmail.com>
2015-05-08 01:04:21 +0800

    Add coffee-compile-region as C-c C-r in coffee mode map

--- modes-init/init-coffee.el
+++ modes-init/init-coffee.el
@@ -4,6 +4,7 @@
   (lambda ()
     (define-key coffee-mode-map (kbd "C-c C-n") 'flymake-goto-next-error)
     (define-key coffee-mode-map (kbd "C-c C-p") 'flymake-goto-prev-error)
+    (define-key coffee-mode-map (kbd "C-c C-r") 'coffee-compile-region)
     (define-key coffee-mode-map (kbd "C-c C-,") 'coffee-indent-shift-left)
     (define-key coffee-mode-map (kbd "C-c C-.") 'coffee-indent-shift-right)
     ))
