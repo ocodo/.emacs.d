4ec713ddbe0008b68da5509c36289c9177527599
jasonm23 <jasonm23@gmail.com>
2015-05-20 02:49:26 +0000

    Update packages - add LaunchCtl for Emacs

---
+++ elpa/async-20150518.1113/async-pkg.el
@@ -0,0 +1,4 @@
+(define-package "async" "20150518.1113" "Asynchronous processing in Emacs" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/launchctl-20150518.609/launchctl-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "launchctl" "20150518.609" "Interface to launchctl on Mac OS X." '((emacs "24.1")) :url "http://github.com/pekingduck/launchctl-el" :keywords '("tools" "convenience"))
