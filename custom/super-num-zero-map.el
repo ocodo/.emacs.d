ff25cb8b345addae59127af6d65428882bc38593
jasonm23 <jasonm23@gmail.com>
2015-05-30 16:15:32 +0800

    [TODO] : convert the super num zero map to a hydra

--- custom/super-num-zero-map.el
+++ custom/super-num-zero-map.el
@@ -1,8 +1,9 @@
+;; TODO : Replace with a defhydra...?
+
 (defvar super-num-zero-map (make-keymap)
   "Custom Keymap for user bindings prefixed by s-kp-0.")
 (fset 'super-num-zero-map super-num-zero-map)
 (define-key global-map [s-kp-0] 'super-num-zero-map)
-
 (define-key super-num-zero-map "o" 'open-opsmanager)
 (define-key super-num-zero-map "v" 'jasmine-coffee/verify-suite)
 (define-key super-num-zero-map "r" 'reload-current-chrome-tab-osx)
