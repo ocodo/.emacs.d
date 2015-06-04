7315f901149e87f919bd2d26cc289e3b20da3de3
jasonm23 <jasonm23@gmail.com>
2015-06-04 09:50:14 +0800

    Remove occur follow

--- elpa/occur-follow-1.0/occur-follow-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package #("occur-follow" 0 12 (fontified t face font-lock-comment-face)) "1.0" "a simple addition to occur key map to add instant highlighting of occurences in context" 'nil)
