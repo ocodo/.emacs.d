5f59a5bc87bbded4419424b26aae87a9f4dd779b
jasonm23 <jasonm23@gmail.com>
2015-06-18 10:40:16 +0800

    Add more avy bindings

--- available-inits/init-avy.el
+++ available-inits/init-avy.el
@@ -1,1 +1,5 @@
 (define-key isearch-mode-map "\M-ss" 'avy-isearch)
+(global-set-key "\M-gw" 'avy-goto-word-0)
+(global-set-key "\M-g\M-l" 'avy-goto-line)
+(global-set-key "\M-g\M-c" 'avy-goto-char)
+(global-set-key "\M-g\M-i" 'avy-goto-char-in-line)
