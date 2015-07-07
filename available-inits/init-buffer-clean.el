f663197001e2811fa5ec02b6d80e092c8b59ce6f
jasonm23 <jasonm23@gmail.com>
2015-07-07 08:53:13 +0800

    Fix buffer clean (on file save) so that Makefiles are not untabified

--- available-inits/init-buffer-clean.el
+++ available-inits/init-buffer-clean.el
@@ -1,10 +1,21 @@
-;; Safe cleanup, probably a package?
+;;; init-buffer-clean --- Safe cleanup, probably a package?
+;;; Commentary:
+;;  Unless the buffer is a Makefile untabify it.
+;;
+;;  For every buffer, kill trailing whitespace and set the buffer file
+;;  coding as utf8.
+;;
+;;; Code:
+
 (defun safe-buffer-cleanup ()
-  "Clean whitespace, kill tabs, set to UTF8"
-  (untabify (point-min) (point-max))
+  "Clean whitespace, kill tabs, set to UTF8."
+  (unless (eq (with-current-buffer (current-buffer) major-mode) 'makefile-bsdmake-mode)
+    (untabify (point-min) (point-max)))
   (delete-trailing-whitespace)
   (set-buffer-file-coding-system 'utf-8))
 
 (add-hook 'before-save-hook 'safe-buffer-cleanup)
 
 (provide 'init-buffer-clean)
+
+;;; init-buffer-clean.el ends here
