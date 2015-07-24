90454158513858378ae7502195cb950ad30abdaa
jasonm23 <jasonm23@gmail.com>
2015-07-24 06:35:18 +0000

    [Updating hackernews] version: 20150723.1601 - removing version: 20150216.218

--- elpa/hackernews-20150723.1601/hackernews-autoloads.el
+++ elpa/hackernews-20150723.1601/hackernews-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "hackernews" "hackernews.el" (21729 61874 307401
+;;;### (autoloads nil "hackernews" "hackernews.el" (21937 41113 839401
 ;;;;;;  0))
 ;;; Generated autoloads from hackernews.el
 
@@ -14,8 +14,8 @@ The entry point of our client
 
 ;;;***
 
-;;;### (autoloads nil nil ("hackernews-pkg.el") (21729 61874 316906
-;;;;;;  915000))
+;;;### (autoloads nil nil ("hackernews-pkg.el") (21937 41113 855563
+;;;;;;  242000))
 
 ;;;***
 
