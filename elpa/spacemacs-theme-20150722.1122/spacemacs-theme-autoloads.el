b0fb63e2592e72f89317f92f89b935ef2c86ee2e
jasonm23 <jasonm23@gmail.com>
2015-07-24 06:37:03 +0000

    [Updating spacemacs-theme] version: 20150722.1122 - removing version: 20150708.757

--- elpa/spacemacs-theme-20150722.1122/spacemacs-theme-autoloads.el
+++ elpa/spacemacs-theme-20150722.1122/spacemacs-theme-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "spacemacs-common" "spacemacs-common.el" (21920
-;;;;;;  42575 811401 0))
+;;;### (autoloads nil "spacemacs-common" "spacemacs-common.el" (21937
+;;;;;;  40974 835401 0))
 ;;; Generated autoloads from spacemacs-common.el
 
 (when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -12,7 +12,7 @@
 ;;;***
 
 ;;;### (autoloads nil nil ("spacemacs-dark-theme.el" "spacemacs-light-theme.el"
-;;;;;;  "spacemacs-theme-pkg.el") (21920 42575 824930 843000))
+;;;;;;  "spacemacs-theme-pkg.el") (21937 40974 851087 174000))
 
 ;;;***
 
--- elpa/spacemacs-theme-20150722.1122/spacemacs-theme-pkg.el
+++ elpa/spacemacs-theme-20150722.1122/spacemacs-theme-pkg.el
@@ -1,4 +1,4 @@
-(define-package "spacemacs-theme" "20150708.757" "Color theme with a dark and light versions" 'nil :keywords
+(define-package "spacemacs-theme" "20150722.1122" "Color theme with a dark and light versions" 'nil :keywords
   '("color" "theme")
   :url "https://github.com/nashamri/spacemacs-theme")
 ;; Local Variables:
