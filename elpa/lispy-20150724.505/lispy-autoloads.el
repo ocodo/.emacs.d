f172c098ab0f47d4ab0d26e94eb5d20280a43e8b
jasonm23 <jasonm23@gmail.com>
2015-07-25 06:39:49 +0000

    [Updating lispy] version: 20150724.505 [removing old version: 20150721.850]

--- elpa/lispy-20150724.505/lispy-autoloads.el
+++ elpa/lispy-20150724.505/lispy-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "lispy" "lispy.el" (21937 41048 483401 0))
+;;;### (autoloads nil "lispy" "lispy.el" (21939 8247 819401 0))
 ;;; Generated autoloads from lispy.el
 
 (autoload 'lispy-mode "lispy" "\
@@ -34,7 +34,7 @@ backward through lists, which is useful to move into special.
 
 ;;;### (autoloads nil nil ("elpa.el" "le-clojure.el" "le-lisp.el"
 ;;;;;;  "le-scheme.el" "lispy-inline.el" "lispy-pkg.el" "lispy-tags.el")
-;;;;;;  (21937 41048 503221 402000))
+;;;;;;  (21939 8247 832092 689000))
 
 ;;;***
 
--- elpa/lispy-20150724.505/lispy-pkg.el
+++ elpa/lispy-20150724.505/lispy-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lispy" "20150721.850" "vi-like Paredit"
+(define-package "lispy" "20150724.505" "vi-like Paredit"
   '((emacs "24.1")
     (ace-window "0.9.0")
     (iedit "0.97")
