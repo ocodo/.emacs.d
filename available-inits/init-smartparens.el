0f9002a1ce96026af6a26f0a1b72160cdcc444af
jasonm23 <jasonm23@gmail.com>
2015-07-26 12:41:44 +0800

    fix smartparens for lisp

--- available-inits/init-smartparens.el
+++ available-inits/init-smartparens.el
@@ -23,8 +23,20 @@
 
 ;; Major mode specific pairs
 
-;; Emacs lisp
-(sp-local-pair 'emacs-lisp-mode "`" "'")
+;; lisp
+(sp-with-modes sp--lisp-modes
+  ;; disable ', it's the quote character!
+  (sp-local-pair "'" nil :actions nil)
+  ;; also only use the pseudo-quote inside strings where it serve as
+  ;; hyperlink.
+  (sp-local-pair "`" "'" :when '(sp-in-string-p sp-in-comment-p))
+  (sp-local-pair "`" nil
+                 :skip-match (lambda (ms mb me)
+                               (cond
+                                ((equal ms "'")
+                                 (or (sp--org-skip-markup ms mb me)
+                                     (not (sp-point-in-string-or-comment))))
+                                (t (not (sp-point-in-string-or-comment)))))))
 
 ;; Ruby
 (sp-local-pair 'ruby-mode "|" "|" :wrap "C-c |")
