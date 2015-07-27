dd08d10f776870d2dd37784b08b7d24be3eb0aa5
jasonm23 <jasonm23@gmail.com>
2015-07-27 08:57:35 +0800

    [Updating swiper] version: 20150726.624 [removing old version: 20150724.739]

--- elpa/swiper-20150726.624/swiper-pkg.el
+++ elpa/swiper-20150726.624/swiper-pkg.el
@@ -1,4 +1,4 @@
-(define-package "swiper" "20150724.739" "Isearch with an overview. Oh, man!"
+(define-package "swiper" "20150726.624" "Isearch with an overview. Oh, man!"
   '((emacs "24.1"))
   :url "https://github.com/abo-abo/swiper" :keywords
   '("matching"))
