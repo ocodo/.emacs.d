ed6d3c6c092247273862ff93c7b9bb60f80f111a
jasonm23 <jasonm23@gmail.com>
2015-07-28 08:03:38 +0000

    [Updating darktooth-theme] version: 20150727.711 [removing old version: 20150726.1023]

--- elpa/darktooth-theme-20150726.1023/darktooth-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "darktooth-theme" "20150726.1023" "Color theme for Emacs, when soothe and gruvbox collide" 'nil :url "http://github.com/emacsfodder/emacs-theme-darktooth")
--- elpa/darktooth-theme-20150727.711/darktooth-theme-autoloads.el
+++ elpa/darktooth-theme-20150727.711/darktooth-theme-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "darktooth-theme" "darktooth-theme.el" (21941
-;;;;;;  33140 0 0))
+;;;### (autoloads nil "darktooth-theme" "darktooth-theme.el" (21943
+;;;;;;  8334 583401 0))
 ;;; Generated autoloads from darktooth-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
