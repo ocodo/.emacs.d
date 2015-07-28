57c4dae1ed2d6894ce0a1cccd5f798d2bc01f73b
jasonm23 <jasonm23@gmail.com>
2015-07-28 09:17:52 +0000

    [Updating haskell-mode] version: 20150727.354 [removing old version: 20150726.712]

--- elpa/haskell-mode-20150726.712/haskell-mode-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "haskell-mode" "20150726.712" "A Haskell editing mode"
-  '((cl-lib "0.5")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/haskell-mode-20150727.354/haskell-mode-pkg.el
@@ -0,0 +1,5 @@
+(define-package "haskell-mode" "20150727.354" "A Haskell editing mode"
+  '((cl-lib "0.5")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
