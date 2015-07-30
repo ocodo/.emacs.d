d81388e2dd87248ad6639138aaff28247918c3f3
jasonm23 <jasonm23@gmail.com>
2015-07-30 22:52:00 +0000

    [Updating flycheck] version: 20150730.101 [removing old version: 20150728.1414]

--- elpa/flycheck-20150730.101/flycheck-autoloads.el
+++ elpa/flycheck-20150730.101/flycheck-autoloads.el
@@ -3,7 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "flycheck" "flycheck.el" (21944 12757 0 0))
+;;;### (autoloads nil "flycheck" "flycheck.el" (21946 43601 211401
+;;;;;;  0))
 ;;; Generated autoloads from flycheck.el
 
 (autoload 'flycheck-info "flycheck" "\
@@ -104,7 +105,7 @@ The following PROPERTIES constitute an error level:
 
 \(fn LEVEL &rest PROPERTIES)" nil nil)
 
-(put 'flycheck-define-error-level 'lisp-indent-function '1)
+(function-put 'flycheck-define-error-level 'lisp-indent-function '1)
 
 (autoload 'flycheck-define-command-checker "flycheck" "\
 Define SYMBOL as syntax checker which runs a command.
@@ -177,9 +178,9 @@ default `:verify' function of command checkers.
 
 \(fn SYMBOL DOCSTRING &rest PROPERTIES)" nil nil)
 
-(put 'flycheck-define-command-checker 'lisp-indent-function '1)
+(function-put 'flycheck-define-command-checker 'lisp-indent-function '1)
 
-(put 'flycheck-define-command-checker 'doc-string-elt '2)
+(function-put 'flycheck-define-command-checker 'doc-string-elt '2)
 
 (autoload 'flycheck-def-config-file-var "flycheck" "\
 Define SYMBOL as config file variable for CHECKER, with default FILE-NAME.
@@ -196,7 +197,7 @@ argument to `flycheck-define-checker'.
 
 \(fn SYMBOL CHECKER &optional FILE-NAME &rest CUSTOM-ARGS)" nil t)
 
-(put 'flycheck-def-config-file-var 'lisp-indent-function '3)
+(function-put 'flycheck-def-config-file-var 'lisp-indent-function '3)
 
 (autoload 'flycheck-def-option-var "flycheck" "\
 Define SYMBOL as option variable with INIT-VALUE for CHECKER.
@@ -212,14 +213,14 @@ Use this together with the `option', `option-list' and
 
 \(fn SYMBOL INIT-VALUE CHECKER DOCSTRING &rest CUSTOM-ARGS)" nil t)
 
-(put 'flycheck-def-option-var 'lisp-indent-function '3)
+(function-put 'flycheck-def-option-var 'lisp-indent-function '3)
 
-(put 'flycheck-def-option-var 'doc-string-elt '4)
+(function-put 'flycheck-def-option-var 'doc-string-elt '4)
 
 ;;;***
 
-;;;### (autoloads nil nil ("flycheck-ert.el" "flycheck-pkg.el") (21944
-;;;;;;  12757 140315 0))
+;;;### (autoloads nil nil ("flycheck-ert.el" "flycheck-pkg.el") (21946
+;;;;;;  43601 229731 121000))
 
 ;;;***
 
