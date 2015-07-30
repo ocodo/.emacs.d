f8f27b7b7d5968a9724207da0c50ecbf7ab17ee6
jasonm23 <jasonm23@gmail.com>
2015-07-30 22:52:05 +0000

    [Updating helm-ag] version: 20150729.1953 [removing old version: 20150728.1612]

--- elpa/helm-ag-20150728.1612/helm-ag-autoloads.el
+++ elpa/helm-ag-20150728.1612/helm-ag-autoloads.el
@@ -3,7 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "helm-ag" "helm-ag.el" (21944 12753 0 0))
+;;;### (autoloads nil "helm-ag" "helm-ag.el" (21946 43586 915401
+;;;;;;  0))
 ;;; Generated autoloads from helm-ag.el
 
 (autoload 'helm-ag-pop-stack "helm-ag" "\
--- elpa/helm-ag-20150728.1612/helm-ag-pkg.el
+++ elpa/helm-ag-20150728.1612/helm-ag-pkg.el
@@ -1,1 +1,2 @@
-(define-package "helm-ag" "20150728.1612" "the silver searcher with helm interface" '((helm "1.5.6") (cl-lib "0.5")) :url "https://github.com/syohex/emacs-helm-ag")
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-ag" "20150729.1953" "the silver searcher with helm interface" '((helm "1.5.6") (cl-lib "0.5")) :url "https://github.com/syohex/emacs-helm-ag")
