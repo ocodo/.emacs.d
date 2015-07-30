88a044788c987602d28134895f58b289c8cde19b
jasonm23 <jasonm23@gmail.com>
2015-07-30 22:52:10 +0000

    [Updating icicles] version: 20150729.1905 [removing old version: 20150723.237]

--- elpa/icicles-20150729.1905/icicles-autoloads.el
+++ elpa/icicles-20150729.1905/icicles-autoloads.el
@@ -3,7 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "icicles" "icicles.el" (21937 41061 39401 0))
+;;;### (autoloads nil "icicles" "icicles.el" (21946 43564 891401
+;;;;;;  0))
 ;;; Generated autoloads from icicles.el
  (autoload 'icy-mode    "icicles" "Toggle Icicle mode - see `icicle-mode'." t nil)
  (autoload 'icicle-mode "icicles"
@@ -28,7 +29,7 @@
 ;;;### (autoloads nil nil ("icicles-chg.el" "icicles-cmd1.el" "icicles-cmd2.el"
 ;;;;;;  "icicles-doc1.el" "icicles-doc2.el" "icicles-face.el" "icicles-fn.el"
 ;;;;;;  "icicles-mac.el" "icicles-mcmd.el" "icicles-mode.el" "icicles-opt.el"
-;;;;;;  "icicles-pkg.el" "icicles-var.el") (21937 41061 170924 697000))
+;;;;;;  "icicles-pkg.el" "icicles-var.el") (21946 43564 974417 515000))
 
 ;;;***
 
