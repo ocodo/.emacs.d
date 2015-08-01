4e3c93219b21748336d94fd8c854447724a17bbe
jasonm23 <jasonm23@gmail.com>
2015-08-01 06:34:54 +0000

    remove arjen grey

--- elpa/arjen-grey-theme-20150726.1057/arjen-grey-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; arjen-grey-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "arjen-grey-theme" "arjen-grey-theme.el" (21948
-;;;;;;  19785 47401 0))
-;;; Generated autoloads from arjen-grey-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; arjen-grey-theme-autoloads.el ends here
--- elpa/arjen-grey-theme-20150726.1057/arjen-grey-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "arjen-grey-theme" "20150731.645" "A soothing dark grey theme" 'nil :url "https://github.com/credmp/arjen-grey" :keywords '("faces"))
