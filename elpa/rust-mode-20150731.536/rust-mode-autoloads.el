56939ad79bedcd3b3401c20bd1a12b5c61cadfe6
jasonm23 <jasonm23@gmail.com>
2015-08-01 06:37:16 +0000

    [Updating rust-mode] version: 20150731.536 [removing old version: 20150720.7]

--- elpa/rust-mode-20150731.536/rust-mode-autoloads.el
+++ elpa/rust-mode-20150731.536/rust-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "rust-mode" "rust-mode.el" (21937 40978 599401
+;;;### (autoloads nil "rust-mode" "rust-mode.el" (21948 19752 83401
 ;;;;;;  0))
 ;;; Generated autoloads from rust-mode.el
 
---
+++ elpa/rust-mode-20150731.536/rust-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rust-mode" "20150731.536" "A major emacs mode for editing Rust source code" 'nil :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
