e1a9705108da8aa6f8737c1b27b3cbcc3662339e
jasonm23 <jasonm23@gmail.com>
2015-08-05 23:52:50 +0000

    [Removing common-lisp-snippets version: 20150601.52]

--- elpa/common-lisp-snippets-20150601.52/common-lisp-snippets-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; common-lisp-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "common-lisp-snippets" "common-lisp-snippets.el"
-;;;;;;  (21868 32278 667401 0))
-;;; Generated autoloads from common-lisp-snippets.el
-
-(autoload 'common-lisp-snippets-initialize "common-lisp-snippets" "\
-Initialize Common Lisp snippets, so Yasnippet can see them.
-
-\(fn)" nil nil)
-
-(eval-after-load 'yasnippet '(common-lisp-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("common-lisp-snippets-pkg.el") (21868
-;;;;;;  32278 698992 689000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; common-lisp-snippets-autoloads.el ends here
