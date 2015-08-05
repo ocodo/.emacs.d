d0849300a717fd89b1cfa853c997e3ad747ae7d2
jasonm23 <jasonm23@gmail.com>
2015-08-05 23:43:02 +0000

    [Adding reveal-in-osx-finder version: 20150802.957]

---
+++ elpa/reveal-in-osx-finder-20150802.957/reveal-in-osx-finder-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "reveal-in-osx-finder" "20150802.957" "Reveal file associated with buffer in OS X Finder" 'nil :url "https://github.com/kaz-yos/reveal-in-osx-finder" :keywords '("os x" "finder"))
