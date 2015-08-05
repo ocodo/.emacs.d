fe0907fae03ff7276f972e3cc7adb2dda5d9fd83
jasonm23 <jasonm23@gmail.com>
2015-08-05 23:52:48 +0000

    [Updating color-theme-sanityinc-tomorrow version: 20150803.1419][removing old version: 20150710.1903]

--- elpa/color-theme-sanityinc-tomorrow-20150803.1419/color-theme-sanityinc-tomorrow-autoloads.el
+++ elpa/color-theme-sanityinc-tomorrow-20150803.1419/color-theme-sanityinc-tomorrow-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "color-theme-sanityinc-tomorrow" "color-theme-sanityinc-tomorrow.el"
-;;;;;;  (21920 42673 743401 0))
+;;;;;;  (21954 41103 327401 0))
 ;;; Generated autoloads from color-theme-sanityinc-tomorrow.el
 
 (when (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -39,8 +39,8 @@ Apply the tomorrow blue theme.
 ;;;### (autoloads nil nil ("color-theme-sanityinc-tomorrow-pkg.el"
 ;;;;;;  "sanityinc-tomorrow-blue-theme.el" "sanityinc-tomorrow-bright-theme.el"
 ;;;;;;  "sanityinc-tomorrow-day-theme.el" "sanityinc-tomorrow-eighties-theme.el"
-;;;;;;  "sanityinc-tomorrow-night-theme.el") (21920 42673 768324
-;;;;;;  336000))
+;;;;;;  "sanityinc-tomorrow-night-theme.el") (21954 41103 354351
+;;;;;;  477000))
 
 ;;;***
 
---
+++ elpa/color-theme-sanityinc-tomorrow-20150803.1419/color-theme-sanityinc-tomorrow-pkg.el
@@ -0,0 +1,4 @@
+(define-package "color-theme-sanityinc-tomorrow" "20150803.1419" "A version of Chris Kempson's various Tomorrow themes" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
