00e283270190f510d2e8484e36708a0216109436
jasonm23 <jasonm23@gmail.com>
2015-08-05 23:42:59 +0000

    [Adding 0blayout version: 20150803.1350]

---
+++ elpa/0blayout-20150803.1350/0blayout-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "0blayout" "20150803.1350" "Layout grouping with ease" 'nil :url "https://github.com/etu/0blayout" :keywords '("convenience" "window-management"))
