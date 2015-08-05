ef269f4e30a40ceb1a9b316afcf2a4896f642393
jasonm23 <jasonm23@gmail.com>
2015-08-05 23:43:01 +0000

    [Adding ob-typescript version: 20150804.530]

---
+++ elpa/ob-typescript-20150804.530/ob-typescript-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ob-typescript" "20150804.530" "org-babel functions for typescript evaluation" '((emacs "24") (org "8.0")) :url "https://github.com/lurdan/ob-typescript" :keywords '("literate programming" "reproducible research" "typescript"))
