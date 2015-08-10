99049bcfcb40bde87609076af8ccacb6434587c0
jasonm23 <jasonm23@gmail.com>
2015-08-10 13:41:29 +0000

    [Updating with-editor version: 20150808.1054][removing old version: 20150710.252]

--- elpa/with-editor-20150808.1054/with-editor-autoloads.el
+++ elpa/with-editor-20150808.1054/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (21920 42566 772649 793000))
+;;;;;;  (21960 42652 770848 961000))
 
 ;;;***
 
--- elpa/with-editor-20150808.1054/with-editor-pkg.el
+++ elpa/with-editor-20150808.1054/with-editor-pkg.el
@@ -1,6 +1,7 @@
-(define-package "with-editor" "20150710.252" "Use the Emacsclient as $EDITOR"
+(define-package "with-editor" "20150808.1054" "Use the Emacsclient as $EDITOR"
   '((emacs "24.4")
-    (dash "2.10.0"))
+    (async "20150807")
+    (dash "2.11.0"))
   :url "https://github.com/magit/magit" :keywords
   '("tools"))
 ;; Local Variables:
