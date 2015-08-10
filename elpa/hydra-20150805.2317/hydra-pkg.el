51107b520dd20e6eec2719c32bd53e7d52195736
jasonm23 <jasonm23@gmail.com>
2015-08-10 13:40:48 +0000

    [Updating hydra version: 20150805.2317][removing old version: 20150805.845]

--- elpa/hydra-20150805.2317/hydra-pkg.el
+++ elpa/hydra-20150805.2317/hydra-pkg.el
@@ -1,4 +1,4 @@
-(define-package "hydra" "20150805.845" "Make bindings that stick around."
+(define-package "hydra" "20150805.2317" "Make bindings that stick around."
   '((cl-lib "0.5"))
   :url "https://github.com/abo-abo/hydra" :keywords
   '("bindings"))
