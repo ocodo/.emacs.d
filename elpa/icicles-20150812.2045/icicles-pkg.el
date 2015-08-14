8955e2c7a0467b121b92e29a71adb693ccdb4bcc
jasonm23 <jasonm23@gmail.com>
2015-08-14 10:51:08 +0000

    [Updating icicles version: 20150812.2045][removing old version: 20150804.1406]

--- elpa/icicles-20150812.2045/icicles-pkg.el
+++ elpa/icicles-20150812.2045/icicles-pkg.el
@@ -1,4 +1,4 @@
-(define-package "icicles" "20150804.1406" "Minibuffer input completion and cycling." 'nil :url "http://www.emacswiki.org/icicles.el" :keywords
+(define-package "icicles" "20150812.2045" "Minibuffer input completion and cycling." 'nil :url "http://www.emacswiki.org/icicles.el" :keywords
   '("extensions" "help" "abbrev" "local" "minibuffer" "projects" "keys" "apropos" "completion" "matching" "regexp" "command"))
 ;; Local Variables:
 ;; no-byte-compile: t
