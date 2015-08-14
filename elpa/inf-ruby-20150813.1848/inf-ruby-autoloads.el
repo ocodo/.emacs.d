9fb4e510220605ba12a32d1e8f364d57a52fd8ba
jasonm23 <jasonm23@gmail.com>
2015-08-14 10:51:11 +0000

    [Updating inf-ruby version: 20150813.1848][removing old version: 20150728.1635]

--- elpa/inf-ruby-20150813.1848/inf-ruby-autoloads.el
+++ elpa/inf-ruby-20150813.1848/inf-ruby-autoloads.el
@@ -3,7 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "inf-ruby" "inf-ruby.el" (21944 12752 0 0))
+;;;### (autoloads nil "inf-ruby" "inf-ruby.el" (21965 50210 143401
+;;;;;;  0))
 ;;; Generated autoloads from inf-ruby.el
 
 (defvar ruby-source-modes '(ruby-mode enh-ruby-mode) "\
---
+++ elpa/inf-ruby-20150813.1848/inf-ruby-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "inf-ruby" "20150813.1848" "Run a Ruby process in a buffer" 'nil :url "http://github.com/nonsequitur/inf-ruby" :keywords '("languages" "ruby"))
