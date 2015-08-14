8adb695714e13626055a7897948f09211cf440cb
jasonm23 <jasonm23@gmail.com>
2015-08-14 10:51:17 +0000

    [Adding kite-mini version: 20150811.1129]

---
+++ elpa/kite-mini-20150811.1129/kite-mini-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kite-mini" "20150811.1129" "Remotely evaluate JavaScript in the WebKit debugger" '((dash "2.11.0") (websocket "1.5")) :url "https://github.com/tungd/kite-mini.el" :keywords '("webkit"))
