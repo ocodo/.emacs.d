1ce4d817e143881ea128acf5d7139cf00d11116d
jasonm23 <jasonm23@gmail.com>
2015-08-23 08:24:03 +0000

    [Adding string-utils version: 20140508.1341]

---
+++ elpa/string-utils-20140508.1341/string-utils-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "string-utils" "20140508.1341" "String-manipulation utilities" '((list-utils "0.4.2")) :url "http://github.com/rolandwalker/string-utils" :keywords '("extensions"))
