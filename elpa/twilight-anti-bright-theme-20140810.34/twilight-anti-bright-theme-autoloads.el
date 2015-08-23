c631cd1f5c3b2fa2319cb3659a96a18c782aba32
jasonm23 <jasonm23@gmail.com>
2015-08-23 08:24:22 +0000

    [Adding twilight-anti-bright-theme version: 20140810.34]

---
+++ elpa/twilight-anti-bright-theme-20140810.34/twilight-anti-bright-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; twilight-anti-bright-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "twilight-anti-bright-theme" "twilight-anti-bright-theme.el"
+;;;;;;  (21975 62419 579401 0))
+;;; Generated autoloads from twilight-anti-bright-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; twilight-anti-bright-theme-autoloads.el ends here
---
+++ elpa/twilight-anti-bright-theme-20140810.34/twilight-anti-bright-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "twilight-anti-bright-theme" "20140810.34" "A soothing Emacs 24 light-on-dark theme" 'nil :url "https://github.com/jimeh/twilight-anti-bright-theme.el" :keywords '("themes"))
