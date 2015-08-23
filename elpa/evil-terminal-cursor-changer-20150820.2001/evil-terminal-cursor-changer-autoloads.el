5c58159e095be8c3651c3c41e9ec3070ba56039c
jasonm23 <jasonm23@gmail.com>
2015-08-23 08:22:53 +0000

    [Updating evil-terminal-cursor-changer version: 20150820.2001][removing old version: 20150812.1003]

--- elpa/evil-terminal-cursor-changer-20150820.2001/evil-terminal-cursor-changer-autoloads.el
+++ elpa/evil-terminal-cursor-changer-20150820.2001/evil-terminal-cursor-changer-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("evil-terminal-cursor-changer.el") (21965
-;;;;;;  50248 411804 831000))
+;;;### (autoloads nil nil ("evil-terminal-cursor-changer.el") (21975
+;;;;;;  62524 235171 596000))
 
 ;;;***
 
