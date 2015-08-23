ae052f33d9acbc985e41fb65b0e4d3bee7fe953a
jasonm23 <jasonm23@gmail.com>
2015-08-23 08:24:14 +0000

    [Adding tern-auto-complete version: 20150611.639]

---
+++ elpa/tern-auto-complete-20150611.639/tern-auto-complete-autoloads.el
@@ -0,0 +1,22 @@
+;;; tern-auto-complete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "tern-auto-complete" "tern-auto-complete.el"
+;;;;;;  (21975 62433 3401 0))
+;;; Generated autoloads from tern-auto-complete.el
+
+(autoload 'tern-ac-setup "tern-auto-complete" "\
+Setup auto-complete for tern-mode.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; tern-auto-complete-autoloads.el ends here
---
+++ elpa/tern-auto-complete-20150611.639/tern-auto-complete-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tern-auto-complete" "20150611.639" "Tern Completion by auto-complete.el" '((tern "0.0.1") (auto-complete "1.4") (cl-lib "0.5") (emacs "24")))
