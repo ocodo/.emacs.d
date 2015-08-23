396e4bc36acba2ce88378e1a1016e634c650698e
jasonm23 <jasonm23@gmail.com>
2015-08-23 08:24:18 +0000

    [Adding togetherly version: 20150820.138]

---
+++ elpa/togetherly-20150820.138/togetherly-autoloads.el
@@ -0,0 +1,15 @@
+;;; togetherly-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("togetherly.el") (21975 62251 684226 855000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; togetherly-autoloads.el ends here
