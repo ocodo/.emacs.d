1d88218c19ac13779f6aecd406473a7d42a1121d
jasonm23 <jasonm23@gmail.com>
2015-08-23 08:23:47 +0000

    [Adding org-ac version: 20140302.413]

---
+++ elpa/org-ac-20140302.413/org-ac-autoloads.el
@@ -0,0 +1,26 @@
+;;; org-ac-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "org-ac" "org-ac.el" (21975 62452 703401 0))
+;;; Generated autoloads from org-ac.el
+
+(autoload 'org-ac/setup-current-buffer "org-ac" "\
+Do setup for using org-ac in current buffer.
+
+\(fn)" t nil)
+
+(autoload 'org-ac/config-default "org-ac" "\
+Do setting recommemded configuration.
+
+\(fn)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; org-ac-autoloads.el ends here
---
+++ elpa/org-ac-20140302.413/org-ac-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-ac" "20140302.413" "Some auto-complete sources for org-mode" '((auto-complete-pcmp "0.0.1") (log4e "0.2.0") (yaxception "0.1")) :url "https://github.com/aki2o/org-ac" :keywords '("org" "completion"))
