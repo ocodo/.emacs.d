72046a354819b40e2eb206e5745c0ebadd352a19
jasonm23 <jasonm23@gmail.com>
2015-08-23 08:24:35 +0000

    [Adding use-package version: 20150817.2257]

---
+++ elpa/use-package-20150817.2257/use-package-autoloads.el
@@ -0,0 +1,15 @@
+;;; use-package-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("use-package.el") (21975 62925 94130 19000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; use-package-autoloads.el ends here
