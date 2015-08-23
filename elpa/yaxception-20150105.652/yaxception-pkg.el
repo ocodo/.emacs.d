7310c6de51857357f5f71d17576687b8c7eacf0f
jasonm23 <jasonm23@gmail.com>
2015-08-23 08:24:49 +0000

    [Adding yaxception version: 20150105.652]

---
+++ elpa/yaxception-20150105.652/yaxception-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yaxception" "20150105.652" "Provide framework about exception like Java for Elisp" 'nil :url "https://github.com/aki2o/yaxception" :keywords '("exception" "error" "signal"))
