716b5d0ac1eca9fe3fac8c381cd56e1c768832bd
jasonm23 <jasonm23@gmail.com>
2015-08-23 08:22:09 +0000

    [Updating ample-theme version: 20150814.1101][removing old version: 20150812.537]

---
+++ elpa/ample-theme-20150814.1101/ample-theme-pkg.el
@@ -0,0 +1,5 @@
+(define-package "ample-theme" "20150814.1101" "Calm Dark Theme for Emacs" 'nil :url "https://github.com/jordonbiondo/ample-theme" :keywords
+  '("theme" "dark"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
