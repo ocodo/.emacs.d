047e2e816859c0b96a07003bffd1639de8afc06f
jasonm23 <jasonm23@gmail.com>
2015-08-23 08:24:20 +0000

    [Adding tss version: 20141228.2257]

---
+++ elpa/tss-20141228.2257/tss-pkg.el
@@ -0,0 +1,10 @@
+(define-package "tss" "20141228.2257" "provide a interface for auto-complete.el/flymake.el on typescript-mode."
+  '((auto-complete "1.4.0")
+    (json-mode "1.1.0")
+    (log4e "0.2.0")
+    (yaxception "0.1"))
+  :url "https://github.com/aki2o/emacs-tss" :keywords
+  '("typescript" "completion"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
