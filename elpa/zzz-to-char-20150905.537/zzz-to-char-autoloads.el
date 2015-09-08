d21e1ecfa4bea86b801e48ff6c32b37d059121a7
jasonm23 <jasonm23@gmail.com>
2015-09-08 07:13:58 +0000

    [Adding zzz-to-char version: 20150905.537]

---
+++ elpa/zzz-to-char-20150905.537/zzz-to-char-autoloads.el
@@ -0,0 +1,33 @@
+;;; zzz-to-char-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "zzz-to-char" "zzz-to-char.el" (21998 35123
+;;;;;;  159401 0))
+;;; Generated autoloads from zzz-to-char.el
+
+(autoload 'zzz-to-char "zzz-to-char" "\
+Kill text between the point and the character CHAR.
+
+This command is similar to `zap-to-char', it kills target
+character too.
+
+\(fn CHAR)" t nil)
+
+(autoload 'zzz-up-to-char "zzz-to-char" "\
+Kill text between the point and the character CHAR.
+
+This command is similar to `zap-up-to-char', it doesn't kill
+target character.
+
+\(fn CHAR)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; zzz-to-char-autoloads.el ends here
