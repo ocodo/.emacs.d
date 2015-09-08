aab45b7aef993387121e3fb419f91fb8536646fb
jasonm23 <jasonm23@gmail.com>
2015-09-08 07:11:53 +0000

    [Updating js3-mode version: 20150902.949][removing old version: 20140805.1529]

--- elpa/js3-mode-20140805.1529/js3-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "js3-mode" "20140805.1529" "An improved JavaScript editing mode" 'nil)
---
+++ elpa/js3-mode-20150902.949/js3-mode-pkg.el
@@ -0,0 +1,5 @@
+(define-package "js3-mode" "20150902.949" "An improved JavaScript editing mode" 'nil :keywords
+  '("javascript" "languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
