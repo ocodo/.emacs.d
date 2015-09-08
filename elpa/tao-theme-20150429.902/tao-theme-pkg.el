ea0fe0e0ada7fbce3454dfd40178e955c395b075
jasonm23 <jasonm23@gmail.com>
2015-09-08 07:13:16 +0000

    [Updating tao-theme version: 20150822.937][removing old version: 20150429.902]

--- elpa/tao-theme-20150429.902/tao-theme-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "tao-theme" "20150429.902" "Tao, light & dark themes for Emacs with greyscale palettes generated from the golden mean." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
