a060ce61e1d108c8a2310a53620eb87bd3675f02
jasonm23 <jasonm23@gmail.com>
2015-09-08 07:09:12 +0000

    [Updating anaconda-mode version: 20150907.422][removing old version: 20150626.441]

--- elpa/anaconda-mode-20150907.422/anaconda-mode-autoloads.el
+++ elpa/anaconda-mode-20150907.422/anaconda-mode-autoloads.el
@@ -3,19 +3,9 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "anaconda-mode" "anaconda-mode.el" (21906 2373
-;;;;;;  235401 0))
+;;;### (autoloads nil "anaconda-mode" "anaconda-mode.el" (21998 34902
+;;;;;;  267401 0))
 ;;; Generated autoloads from anaconda-mode.el
-
-(autoload 'anaconda-mode-remote "anaconda-mode" "\
-Suggest anaconda_mode.py running on HOST at PORT.
-
-\(fn HOST PORT)" t nil)
-
-(autoload 'anaconda-mode-local "anaconda-mode" "\
-Suggest anaconda_mode.py running on localhost.
-
-\(fn)" t nil)
 
 (autoload 'anaconda-mode "anaconda-mode" "\
 Code navigation, documentation lookup and completion for Python.
@@ -23,11 +13,6 @@ Code navigation, documentation lookup and completion for Python.
 \\{anaconda-mode-map}
 
 \(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("anaconda-mode-pkg.el") (21906 2373 253974
-;;;;;;  853000))
 
 ;;;***
 
