b6da5d14b64fe7abcb9b7a881c0845d91ddb93f6
jasonm23 <jasonm23@gmail.com>
2015-09-17 05:23:52 +0000

    [Updating telephone-line version: 20150910.1004][removing old version: 20150902.957]

--- elpa/telephone-line-20150910.1004/telephone-line-pkg.el
+++ elpa/telephone-line-20150910.1004/telephone-line-pkg.el
@@ -1,8 +1,7 @@
-(define-package "telephone-line" "20150902.957" "Rewrite of Powerline"
+(define-package "telephone-line" "20150910.1004" "Rewrite of Powerline"
   '((emacs "24.3")
     (cl-lib "0.5")
     (memoize "1.0.1")
-    (names "0.5")
     (s "1.9.0")
     (seq "1.8"))
   :url "https://github.com/dbordak/telephone-line" :keywords
