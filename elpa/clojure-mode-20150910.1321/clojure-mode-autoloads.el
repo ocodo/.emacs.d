2c680aca421910b93e0956759068348353ccbb81
jasonm23 <jasonm23@gmail.com>
2015-09-17 05:20:26 +0000

    [Updating clojure-mode version: 20150910.1321][removing old version: 20150804.1311]

--- elpa/clojure-mode-20150910.1321/clojure-mode-autoloads.el
+++ elpa/clojure-mode-20150910.1321/clojure-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "clojure-mode" "clojure-mode.el" (21954 41107
-;;;;;;  759401 0))
+;;;### (autoloads nil "clojure-mode" "clojure-mode.el" (22010 16168
+;;;;;;  853140 0))
 ;;; Generated autoloads from clojure-mode.el
 
 (autoload 'clojure-mode "clojure-mode" "\
---
+++ elpa/clojure-mode-20150910.1321/clojure-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clojure-mode" "20150910.1321" "Major mode for Clojure code" '((emacs "24.3")) :url "http://github.com/clojure-emacs/clojure-mode" :keywords '("languages" "clojure" "clojurescript" "lisp"))
