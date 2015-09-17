ddbe6ec6c82d1063d6c64bc483ace90436216dc6
jasonm23 <jasonm23@gmail.com>
2015-09-17 05:22:14 +0000

    [Updating ido-vertical-mode version: 20150914.1142][removing old version: 20150724.2337]

--- elpa/ido-vertical-mode-20150914.1142/ido-vertical-mode-autoloads.el
+++ elpa/ido-vertical-mode-20150914.1142/ido-vertical-mode-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "ido-vertical-mode" "ido-vertical-mode.el"
-;;;;;;  (21940 26149 279401 0))
+;;;;;;  (22010 16078 953140 0))
 ;;; Generated autoloads from ido-vertical-mode.el
 
 (defvar ido-vertical-mode nil "\
