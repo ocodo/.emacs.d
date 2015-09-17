23e93f0e28679270e752439e33259e9944fe2dc3
jasonm23 <jasonm23@gmail.com>
2015-09-17 05:22:59 +0000

    [Updating material-theme version: 20150914.1231][removing old version: 20150819.1314]

--- elpa/material-theme-20150914.1231/material-theme-autoloads.el
+++ elpa/material-theme-20150914.1231/material-theme-autoloads.el
@@ -4,23 +4,23 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "material-light-theme" "material-light-theme.el"
-;;;;;;  (21975 62458 187401 0))
+;;;;;;  (22010 16044 873140 0))
 ;;; Generated autoloads from material-light-theme.el
 
 (when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
 
 ;;;***
 
-;;;### (autoloads nil "material-theme" "material-theme.el" (21975
-;;;;;;  62458 183401 0))
+;;;### (autoloads nil "material-theme" "material-theme.el" (22010
+;;;;;;  16044 869140 0))
 ;;; Generated autoloads from material-theme.el
 
 (when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
 
 ;;;***
 
-;;;### (autoloads nil nil ("material-theme-pkg.el") (21975 62458
-;;;;;;  202906 406000))
+;;;### (autoloads nil nil ("material-theme-pkg.el") (22010 16044
+;;;;;;  888823 815000))
 
 ;;;***
 
