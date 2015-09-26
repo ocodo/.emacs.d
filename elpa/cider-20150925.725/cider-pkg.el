2ef413605aed604c3dd59370aa5793094e21ad58
jasonm23 <jasonm23@gmail.com>
2015-09-26 01:46:19 +0000

    [Updating cider version: 20150925.725][removing old version: 20150916.1453]

---
+++ elpa/cider-20150925.725/cider-pkg.el
@@ -0,0 +1,12 @@
+(define-package "cider" "20150925.725" "Clojure Interactive Development Environment that Rocks"
+  '((clojure-mode "4.2.0")
+    (dash "2.11.0")
+    (pkg-info "0.4")
+    (emacs "24.3")
+    (queue "0.1.1")
+    (spinner "1.4"))
+  :url "http://www.github.com/clojure-emacs/cider" :keywords
+  '("languages" "clojure" "cider"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
