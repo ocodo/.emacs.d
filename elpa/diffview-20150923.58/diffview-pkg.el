b61d791b306261cc2f0a72011ea53942f68a6cd0
jasonm23 <jasonm23@gmail.com>
2015-09-26 01:46:33 +0000

    [Adding diffview version: 20150923.58]

---
+++ elpa/diffview-20150923.58/diffview-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "diffview" "20150923.58" "View diffs in side-by-side format" 'nil :url "https://github.com/mgalgs/diffview-mode" :keywords '("convenience" "diff"))
