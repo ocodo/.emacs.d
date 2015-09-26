04c17624a81e9926d2e29c2eeceef54be52e65b9
jasonm23 <jasonm23@gmail.com>
2015-09-26 01:46:08 +0000

    [Adding annoying-arrows-mode version: 20130515.1000]

---
+++ elpa/annoying-arrows-mode-20130515.1000/annoying-arrows-mode-autoloads.el
@@ -0,0 +1,43 @@
+;;; annoying-arrows-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "annoying-arrows-mode" "annoying-arrows-mode.el"
+;;;;;;  (22021 61741 861140 0))
+;;; Generated autoloads from annoying-arrows-mode.el
+
+(autoload 'annoying-arrows-mode "annoying-arrows-mode" "\
+Annoying-Arrows emacs minor mode.
+
+\(fn &optional ARG)" t nil)
+
+(defvar global-annoying-arrows-mode nil "\
+Non-nil if Global-Annoying-Arrows mode is enabled.
+See the command `global-annoying-arrows-mode' for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `global-annoying-arrows-mode'.")
+
+(custom-autoload 'global-annoying-arrows-mode "annoying-arrows-mode" nil)
+
+(autoload 'global-annoying-arrows-mode "annoying-arrows-mode" "\
+Toggle Annoying-Arrows mode in all buffers.
+With prefix ARG, enable Global-Annoying-Arrows mode if ARG is positive;
+otherwise, disable it.  If called from Lisp, enable the mode if
+ARG is omitted or nil.
+
+Annoying-Arrows mode is enabled in all buffers where
+`annoying-arrows-mode' would do it.
+See `annoying-arrows-mode' for more information on Annoying-Arrows mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; annoying-arrows-mode-autoloads.el ends here
---
+++ elpa/annoying-arrows-mode-20130515.1000/annoying-arrows-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "annoying-arrows-mode" "20130515.1000" "Ring the bell if using arrows too much" 'nil :keywords '("learning"))
