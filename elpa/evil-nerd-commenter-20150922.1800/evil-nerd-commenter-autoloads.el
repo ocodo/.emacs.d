aaeb26bcff325ff5f54109bb81d38b17be530f29
jasonm23 <jasonm23@gmail.com>
2015-09-26 01:46:51 +0000

    [Updating evil-nerd-commenter version: 20150922.1800][removing old version: 20150701.1655]

--- elpa/evil-nerd-commenter-20150922.1800/evil-nerd-commenter-autoloads.el
+++ elpa/evil-nerd-commenter-20150922.1800/evil-nerd-commenter-autoloads.el
@@ -4,81 +4,88 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "evil-nerd-commenter" "evil-nerd-commenter.el"
-;;;;;;  (21911 65093 959401 0))
+;;;;;;  (22021 61712 621140 0))
 ;;; Generated autoloads from evil-nerd-commenter.el
 
 (autoload 'evilnc-comment-or-uncomment-paragraphs "evil-nerd-commenter" "\
-Comment or uncomment paragraph(s). A paragraph is a continuation non-empty lines.
+Comment or uncomment NUM paragraph(s).
+A paragraph is a continuation non-empty lines.
 Paragraphs are separated by empty lines.
 
 \(fn &optional NUM)" t nil)
 
 (autoload 'evilnc-comment-or-uncomment-to-the-line "evil-nerd-commenter" "\
-Comment or uncomment from the current line to the LINENUM line
+Comment or uncomment from current line to LINENUM line.
 
 \(fn &optional LINENUM)" t nil)
 
 (autoload 'evilnc-quick-comment-or-uncomment-to-the-line "evil-nerd-commenter" "\
-Comment or uncomment to line number by specifying its last digit(s)
-For exmaple, you can use 'C-u 53 M-x evilnc-quick-comment-or-uncomment-to-the-line'
-or 'C-u 3 M-x evilnc-quick-comment-or-uncomment-to-the-line' to comment to the line 6453
+Comment/uncomment to line number by last digit(s) whose value is UNITS.
+For exmaple, you can use either \\<M-53>\\[evilnc-quick-comment-or-uncomment-to-the-line] or \\<M-3>\\[evilnc-quick-comment-or-uncomment-to-the-line] to comment to the line 6453
 
 \(fn &optional UNITS)" t nil)
 
 (autoload 'evilnc-toggle-invert-comment-line-by-line "evil-nerd-commenter" "\
-Please note this command may NOT work on complex evil text objects
+Please note this command may NOT work on complex evil text objects.
 
 \(fn)" t nil)
 
 (autoload 'evilnc-toggle-comment-empty-lines "evil-nerd-commenter" "\
-
+Toggle the flag which decide wether empty line will be commented.
 
 \(fn)" t nil)
 
 (autoload 'evilnc-comment-or-uncomment-lines "evil-nerd-commenter" "\
-Comment or uncomment NUM lines. NUM could be negative.
-   Case 1: If no region selected, comment/uncomment on current line. if NUM>1, comment/uncomment
-   extra N-1 lines from next line
-   Case 2: If a region selected, the region is expand to make sure the region contain
-   whole lines. Then we comment/uncomment the expanded region. NUM is ignored.
-   Case 3: If a region in ONE line is selected, we comment/uncomment that region.
-   In this case, CORRECT comment syntax will be used for C++/Java/Javascript.
+Comment or uncomment NUM lines.  NUM could be negative.
+
+Case 1: If no region selected, comment/uncomment on current line.
+If NUM>1, comment/uncomment extra N-1 lines from next line.
+
+Case 2: Selected region is expanded to make it contain whole lines.
+Then we comment/uncomment the expanded region.  NUM is ignored.
+
+Case 3: If a region inside of ONE line is selected,
+we comment/uncomment that region.
+CORRECT comment syntax will be used for C++/Java/Javascript.
 
 \(fn &optional NUM)" t nil)
 
 (autoload 'evilnc-copy-and-comment-lines "evil-nerd-commenter" "\
-Copy and paste NUM lines. Then comment the original lines. NUM could be negative.
-   Case 1: If no region selected, operate on current line. if NUM>1, comment/uncomment
-   extra N-1 lines from next line
-   Case 2: If a region selected, the region is expand to make sure the region contain
-   whole lines. Then we operate the expanded region. NUM is ignored.
+Copy&paste NUM lines and comment out original lines.
+NUM could be negative.
+
+Case 1: If no region selected, operate on current line.
+if NUM>1, comment/uncomment extra N-1 lines from next line
+
+Case 2: Selected region is expanded to make it contain whole lines.
+Then we operate the expanded region.  NUM is ignored.
 
 \(fn &optional NUM)" t nil)
 
 (autoload 'evilnc-copy-to-line "evil-nerd-commenter" "\
-Copy from the current line to the LINENUM line, for non-evil user only
+Copy from the current line to LINENUM line.  For non-evil user only.
 
 \(fn &optional LINENUM)" t nil)
 
 (autoload 'evilnc-kill-to-line "evil-nerd-commenter" "\
-Kill from the current line to the LINENUM line, for non-evil user only
+Kill from the current line to the LINENUM line.  For non-evil user only.
 
 \(fn &optional LINENUM)" t nil)
 
 (autoload 'evilnc-version "evil-nerd-commenter" "\
-
+The version number.
 
 \(fn)" t nil)
 
 (autoload 'evilnc-default-hotkeys "evil-nerd-commenter" "\
-Set the hotkeys of evil-nerd-comment
+Set the hotkeys of evil-nerd-comment.
 
 \(fn)" t nil)
 
 ;;;***
 
 ;;;### (autoloads nil nil ("evil-nerd-commenter-operator.el" "evil-nerd-commenter-pkg.el")
-;;;;;;  (21911 65093 971222 86000))
+;;;;;;  (22021 61712 633336 501000))
 
 ;;;***
 
---
+++ elpa/evil-nerd-commenter-20150922.1800/evil-nerd-commenter-pkg.el
@@ -0,0 +1,4 @@
+(define-package "evil-nerd-commenter" "20150922.1800" "Comment/uncomment lines efficiently. Like Nerd Commenter in Vim" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
