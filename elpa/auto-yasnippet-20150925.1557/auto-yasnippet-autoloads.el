a3f2d7222bd9fd38854cdab56f2683a11d845eba
jasonm23 <jasonm23@gmail.com>
2015-09-27 11:00:13 +0000

    [Updating auto-yasnippet version: 20150925.1557][removing old version: 20150717.451]

--- elpa/auto-yasnippet-20150925.1557/auto-yasnippet-autoloads.el
+++ elpa/auto-yasnippet-20150925.1557/auto-yasnippet-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "auto-yasnippet" "auto-yasnippet.el" (21937
-;;;;;;  41171 607401 0))
+;;;### (autoloads nil "auto-yasnippet" "auto-yasnippet.el" (22023
+;;;;;;  52177 657140 0))
 ;;; Generated autoloads from auto-yasnippet.el
 
 (autoload 'aya-create-one-line "auto-yasnippet" "\
