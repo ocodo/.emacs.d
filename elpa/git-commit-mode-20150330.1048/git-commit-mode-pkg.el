8c7506d8ac8e3845eb980879930cbf17ffe17e12
jasonm23 <jasonm23@gmail.com>
2015-10-03 12:49:48 +0800

    [Removing git-commit-mode version: 20150330.1048]

--- elpa/git-commit-mode-20150330.1048/git-commit-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit-mode" "20150330.1048" "Major mode for editing git commit messages" 'nil :url "https://github.com/magit/git-modes" :keywords '("convenience" "vc" "git"))
