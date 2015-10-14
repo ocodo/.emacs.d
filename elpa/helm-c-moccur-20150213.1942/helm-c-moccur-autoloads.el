e86dcf3ba799b1030981a6c640234dec286b91d9
jasonm23 <jasonm23@gmail.com>
2015-10-14 03:23:15 +0000

    [Adding helm-c-moccur version: 20150213.1942]

---
+++ elpa/helm-c-moccur-20150213.1942/helm-c-moccur-autoloads.el
@@ -0,0 +1,16 @@
+;;; helm-c-moccur-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("helm-c-moccur.el") (22045 51596 685845
+;;;;;;  358000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-c-moccur-autoloads.el ends here
---
+++ elpa/helm-c-moccur-20150213.1942/helm-c-moccur-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-c-moccur" "20150213.1942" "helm source for color-moccur.el" '((helm "20120811") (color-moccur "2.71")) :keywords '("convenience" "emulation"))
