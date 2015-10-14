a9665226cff2a172fb78cdfe8406d5268531ee15
jasonm23 <jasonm23@gmail.com>
2015-10-14 03:22:42 +0000

    [Updating git-commit version: 20151002.1417][removing old version: 20150903.1204]

--- elpa/git-commit-20151002.1417/git-commit-autoloads.el
+++ elpa/git-commit-20151002.1417/git-commit-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "git-commit" "git-commit.el" (21998 34821 879401
+;;;### (autoloads nil "git-commit" "git-commit.el" (22045 48040 429140
 ;;;;;;  0))
 ;;; Generated autoloads from git-commit.el
 
