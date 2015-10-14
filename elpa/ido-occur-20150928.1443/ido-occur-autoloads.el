de4de49cf868f78df52805858b697c75e7bc3b47
jasonm23 <jasonm23@gmail.com>
2015-10-14 03:23:55 +0000

    [Adding ido-occur version: 20150928.1443]

---
+++ elpa/ido-occur-20150928.1443/ido-occur-autoloads.el
@@ -0,0 +1,22 @@
+;;; ido-occur-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "ido-occur" "ido-occur.el" (22045 51592 261140
+;;;;;;  0))
+;;; Generated autoloads from ido-occur.el
+
+(autoload 'ido-occur "ido-occur" "\
+Yet another `occur' with `ido'.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ido-occur-autoloads.el ends here
---
+++ elpa/ido-occur-20150928.1443/ido-occur-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ido-occur" "20150928.1443" "Yet another `occur' with `ido'." '((ido-vertical-mode "1.0.0") (dash "2.11.0")) :url "https://github.com/danil/ido-occur" :keywords '("inner" "buffer" "search"))
