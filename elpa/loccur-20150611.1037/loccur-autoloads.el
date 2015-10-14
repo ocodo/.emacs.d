42dcd506e16cde7036d1261a4d4a9c946c7b21cd
jasonm23 <jasonm23@gmail.com>
2015-10-14 03:24:29 +0000

    [Adding loccur version: 20150611.1037]

---
+++ elpa/loccur-20150611.1037/loccur-autoloads.el
@@ -0,0 +1,15 @@
+;;; loccur-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("loccur.el") (22045 51588 683255 409000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; loccur-autoloads.el ends here
---
+++ elpa/loccur-20150611.1037/loccur-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "loccur" "20150611.1037" "Performs an occur-like folding in current buffer." 'nil :url "https://github.com/fourier/loccur" :keywords '("matching"))
