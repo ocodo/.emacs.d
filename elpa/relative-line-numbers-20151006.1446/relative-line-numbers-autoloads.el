b5c1104ff74b029e7c12b3a52bcee422022c8d49
jasonm23 <jasonm23@gmail.com>
2015-10-14 03:25:30 +0000

    [Updating relative-line-numbers version: 20151006.1446][removing old version: 20150624.714]

--- elpa/relative-line-numbers-20151006.1446/relative-line-numbers-autoloads.el
+++ elpa/relative-line-numbers-20151006.1446/relative-line-numbers-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "relative-line-numbers" "relative-line-numbers.el"
-;;;;;;  (21906 2249 595401 0))
+;;;;;;  (22045 47872 57140 0))
 ;;; Generated autoloads from relative-line-numbers.el
 
 (autoload 'relative-line-numbers-mode "relative-line-numbers" "\
--- elpa/relative-line-numbers-20151006.1446/relative-line-numbers.el
+++ elpa/relative-line-numbers-20151006.1446/relative-line-numbers.el
@@ -2,8 +2,8 @@
 
 ;; Author: Fanael Linithien <fanael4@gmail.com>
 ;; URL: https://github.com/Fanael/relative-line-numbers
-;; Package-Version: 20150624.714
-;; Version: 0.3.1
+;; Package-Version: 20151006.1446
+;; Version: 0.3.2
 ;; Package-Requires: ((emacs "24"))
 
 ;; This file is NOT part of GNU Emacs.
@@ -61,9 +61,9 @@ Recommended functions are:
  - `forward-line': invisible lines will be counted, counting follows physical
    lines,
  - `forward-visible-line': invisible lines will NOT be counted, counting follows
-   physical lines,
- - `vertical-motion': invisible lines will NOT be counted, counting follows
-   VISUAL lines."
+   physical lines.
+
+Using other functions is possible, but only at one's own risk."
   :type 'function
   :group 'relative-line-numbers)
 
