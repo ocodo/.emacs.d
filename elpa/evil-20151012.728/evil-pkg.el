ba76d421b59eb6749c310a1893943709e624f3ee
jasonm23 <jasonm23@gmail.com>
2015-10-14 03:22:19 +0000

    [Updating evil version: 20151012.728][removing old version: 20150928.737]

--- elpa/evil-20151012.728/evil-pkg.el
+++ elpa/evil-20151012.728/evil-pkg.el
@@ -1,4 +1,4 @@
-(define-package "evil" "20150928.737" "Extensible Vi layer for Emacs."
+(define-package "evil" "20151012.728" "Extensible Vi layer for Emacs."
   '((undo-tree "0.6.3")
     (goto-chg "1.6")))
 ;; Local Variables:
