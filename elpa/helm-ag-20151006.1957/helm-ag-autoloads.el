7a3f0b8f7e682ce560a62eb600d9cb0330b475fd
jasonm23 <jasonm23@gmail.com>
2015-10-14 03:23:12 +0000

    [Updating helm-ag version: 20151006.1957][removing old version: 20150924.731]

--- elpa/helm-ag-20151006.1957/helm-ag-autoloads.el
+++ elpa/helm-ag-20151006.1957/helm-ag-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "helm-ag" "helm-ag.el" (22021 61685 861140
+;;;### (autoloads nil "helm-ag" "helm-ag.el" (22045 48004 957140
 ;;;;;;  0))
 ;;; Generated autoloads from helm-ag.el
 
