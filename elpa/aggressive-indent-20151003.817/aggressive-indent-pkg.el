6b56e22d951c28ecdc40a133af38a523da95febd
jasonm23 <jasonm23@gmail.com>
2015-10-15 03:58:28 +0000

    [Removing aggressive-indent version: 20151003.817]

--- elpa/aggressive-indent-20151003.817/aggressive-indent-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "aggressive-indent" "20151003.817" "Minor mode to aggressively keep your code always indented" '((emacs "24.1") (cl-lib "0.5")) :url "http://github.com/Malabarba/aggressive-indent-mode" :keywords '("indent" "lisp" "maint" "tools"))
