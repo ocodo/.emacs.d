8fcc671573ddb69150b371463fcb79509b821982
jasonm23 <jasonm23@gmail.com>
2015-10-15 03:39:42 +0000

    [Updating smeargle version: 20151013.2242][removing old version: 20141110.1919]

---
+++ elpa/smeargle-20151013.2242/smeargle-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smeargle" "20151013.2242" "Highlighting region by last updated time" '((cl-lib "0.5") (emacs "24")) :url "https://github.com/syohex/emacs-smeargle")
