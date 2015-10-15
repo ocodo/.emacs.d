b5265d26ccb744bdd020051c996589c305a216fb
jasonm23 <jasonm23@gmail.com>
2015-10-15 03:39:39 +0000

    [Updating linum-relative version: 20151013.2114][removing old version: 20150927.24]

---
+++ elpa/linum-relative-20151013.2114/linum-relative-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "linum-relative" "20151013.2114" "display relative line number in emacs." 'nil :url "http://github.com/coldnew/linum-relative" :keywords '("converience"))
