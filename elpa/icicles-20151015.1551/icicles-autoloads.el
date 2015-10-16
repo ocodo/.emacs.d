f33a16a21c9397ddc16af4055adc8935b4da5c55
jasonm23 <jasonm23@gmail.com>
2015-10-16 14:45:32 +0000

    [Updating icicles version: 20151015.1551][removing old version: 20151011.937]

--- elpa/icicles-20151015.1551/icicles-autoloads.el
+++ elpa/icicles-20151015.1551/icicles-autoloads.el
@@ -3,8 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "icicles" "icicles.el" (22045 47971 513140
-;;;;;;  0))
+;;;### (autoloads nil "icicles" "icicles.el" (22049 3314 194811 0))
 ;;; Generated autoloads from icicles.el
  (autoload 'icy-mode    "icicles" "Toggle Icicle mode - see `icicle-mode'." t nil)
  (autoload 'icicle-mode "icicles"
@@ -29,7 +28,7 @@
 ;;;### (autoloads nil nil ("icicles-chg.el" "icicles-cmd1.el" "icicles-cmd2.el"
 ;;;;;;  "icicles-doc1.el" "icicles-doc2.el" "icicles-face.el" "icicles-fn.el"
 ;;;;;;  "icicles-mac.el" "icicles-mcmd.el" "icicles-mode.el" "icicles-opt.el"
-;;;;;;  "icicles-pkg.el" "icicles-var.el") (22045 47971 607889 175000))
+;;;;;;  "icicles-pkg.el" "icicles-var.el") (22049 3314 276464 485000))
 
 ;;;***
 
--- elpa/icicles-20151015.1551/icicles-pkg.el
+++ elpa/icicles-20151015.1551/icicles-pkg.el
@@ -1,4 +1,4 @@
-(define-package "icicles" "20151011.937" "Minibuffer input completion and cycling." 'nil :url "http://www.emacswiki.org/icicles.el" :keywords
+(define-package "icicles" "20151015.1551" "Minibuffer input completion and cycling." 'nil :url "http://www.emacswiki.org/icicles.el" :keywords
   '("extensions" "help" "abbrev" "local" "minibuffer" "projects" "keys" "apropos" "completion" "matching" "regexp" "command"))
 ;; Local Variables:
 ;; no-byte-compile: t
