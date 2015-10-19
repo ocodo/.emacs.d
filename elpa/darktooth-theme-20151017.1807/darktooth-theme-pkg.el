536eaace9c3d9265100cabdeac46c457eb1fb9b9
jasonm23 <jasonm23@gmail.com>
2015-10-19 01:56:31 +0000

    [Updating darktooth-theme version: 20151017.1807][removing old version: 20151015.1732]

---
+++ elpa/darktooth-theme-20151017.1807/darktooth-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "darktooth-theme" "20151017.1807" "Color theme for Emacs, when soothe and gruvbox collide" 'nil :url "http://github.com/emacsfodder/emacs-theme-darktooth")
