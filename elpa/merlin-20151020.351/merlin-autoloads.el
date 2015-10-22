bbc23f7d88975fe36eb2b01a187e39f8e8a5be1f
jasonm23 <jasonm23@gmail.com>
2015-10-22 03:55:56 +0000

    [Updating merlin version: 20151020.351][removing old version: 20151001.2338]

--- elpa/merlin-20151020.351/merlin-autoloads.el
+++ elpa/merlin-20151020.351/merlin-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "merlin" "merlin.el" (22030 34870 829140 0))
+;;;### (autoloads nil "merlin" "merlin.el" (22056 24034 234811 0))
 ;;; Generated autoloads from merlin.el
 
 (autoload 'merlin-mode "merlin" "\
@@ -18,8 +18,8 @@ Short cuts:
 ;;;***
 
 ;;;### (autoloads nil nil ("merlin-ac.el" "merlin-cap.el" "merlin-company.el"
-;;;;;;  "merlin-compat.el" "merlin-iedit.el" "merlin-pkg.el") (22030
-;;;;;;  34870 847787 562000))
+;;;;;;  "merlin-compat.el" "merlin-iedit.el" "merlin-pkg.el") (22056
+;;;;;;  24034 256075 783000))
 
 ;;;***
 
