60764f3a8a9e3d6e855d995437dfc6a9e07b0016
jasonm23 <jasonm23@gmail.com>
2015-10-22 03:55:50 +0000

    [Updating direx version: 20151020.2107][removing old version: 20150805.143]

--- elpa/direx-20150805.143/direx-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "direx" "20150805.143" "Simple Directory Explorer" 'nil :keywords
-  '("convenience"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/direx-20151020.2107/direx-pkg.el
@@ -0,0 +1,5 @@
+(define-package "direx" "20151020.2107" "Simple Directory Explorer" 'nil :keywords
+  '("convenience"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
