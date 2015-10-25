239ec6c6532a49404cecd8c0e3da3ae969887d4a
jason <jason@noreply.org>
2015-10-25 01:48:17 -0400

    [Updating ycmd version: 20151023.1419][removing old version: 20151020.328]

--- elpa/ycmd-20151023.1419/ycmd-autoloads.el
+++ elpa/ycmd-20151023.1419/ycmd-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "ycmd" "ycmd.el" (22056 24014 398811 0))
+;;;### (autoloads nil "ycmd" "ycmd.el" (22060 27294 996236 316000))
 ;;; Generated autoloads from ycmd.el
 
 (autoload 'ycmd-mode "ycmd" "\
@@ -51,7 +51,7 @@ See `ycmd-mode' for more information on Ycmd mode.
 ;;;***
 
 ;;;### (autoloads nil nil ("ycmd-next-error.el" "ycmd-pkg.el" "ycmd-request-deferred.el"
-;;;;;;  "ycmd-request.el") (22056 24014 478029 547000))
+;;;;;;  "ycmd-request.el") (22060 27295 40236 311000))
 
 ;;;***
 
