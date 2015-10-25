033859d07851cc1a93f687ae1bb15b31d193f551
jason <jason@noreply.org>
2015-10-25 01:47:42 -0400

    [Updating git-commit version: 20151022.933][removing old version: 20151020.855]

---
+++ elpa/git-commit-20151022.933/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20151022.933" "Edit Git commit messages" '((emacs "24.4") (dash "20151021.113") (with-editor "20151022")) :url "https://github.com/magit/magit" :keywords '("git" "tools" "vc"))
--- elpa/git-commit-20151022.933/git-commit.el
+++ elpa/git-commit-20151022.933/git-commit.el
@@ -11,9 +11,9 @@
 ;;	Marius Vollmer <marius.vollmer@gmail.com>
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 
-;; Package-Requires: ((emacs "24.4") (dash "2.11.0") (with-editor "20150903"))
+;; Package-Requires: ((emacs "24.4") (dash "20151021.113") (with-editor "20151022"))
 ;; Keywords: git tools vc
-;; Package-Version: 20151020.855
+;; Package-Version: 20151022.933
 ;; Homepage: https://github.com/magit/magit
 
 ;; This file is not part of GNU Emacs.
