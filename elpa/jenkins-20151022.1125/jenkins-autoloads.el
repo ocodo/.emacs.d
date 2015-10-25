5e8cf65e9e5fef4d2fd31e632e86e8275039b559
jason <jason@noreply.org>
2015-10-25 01:48:18 -0400

    [Adding jenkins version: 20151022.1125]

---
+++ elpa/jenkins-20151022.1125/jenkins-autoloads.el
@@ -0,0 +1,22 @@
+;;; jenkins-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "jenkins" "jenkins.el" (22060 27050 752225
+;;;;;;  271000))
+;;; Generated autoloads from jenkins.el
+
+(autoload 'jenkins "jenkins" "\
+Initialize jenkins buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; jenkins-autoloads.el ends here
---
+++ elpa/jenkins-20151022.1125/jenkins-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "jenkins" "20151022.1125" "Minimalistic Jenkins client for Emacs" '((dash "2.12") (emacs "24.3") (json "1.4")) :keywords '("jenkins" "convenience"))
