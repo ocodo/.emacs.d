19ab84538254d010bfa1a21291cf08e77eb0e6b7
jason <jason@noreply.org>
2015-10-25 01:47:30 -0400

    [Updating cider version: 20151022.28][removing old version: 20151020.646]

---
+++ elpa/cider-20151022.28/cider-pkg.el
@@ -0,0 +1,12 @@
+(define-package "cider" "20151022.28" "Clojure Interactive Development Environment that Rocks"
+  '((clojure-mode "5.0.0")
+    (pkg-info "0.4")
+    (emacs "24.3")
+    (queue "0.1.1")
+    (spinner "1.4")
+    (seq "1.9"))
+  :url "http://www.github.com/clojure-emacs/cider" :keywords
+  '("languages" "clojure" "cider"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
