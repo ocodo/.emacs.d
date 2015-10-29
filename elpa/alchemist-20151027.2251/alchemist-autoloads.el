285c7576c16daf1ebc4643c975e68cd6d24fa23e
JasonM23 <jasonm23@gmail.com>
2015-10-29 08:28:03 -0400

    [Updating alchemist version: 20151027.2251][removing old version: 20151020.137]

--- elpa/alchemist-20151027.2251/alchemist-autoloads.el
+++ elpa/alchemist-20151027.2251/alchemist-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "alchemist" "alchemist.el" (22056 24104 10811
-;;;;;;  0))
+;;;### (autoloads nil "alchemist" "alchemist.el" (22066 4260 861223
+;;;;;;  185000))
 ;;; Generated autoloads from alchemist.el
 
 (autoload 'alchemist-mode "alchemist" "\
@@ -17,8 +17,8 @@ Key bindings:
 
 ;;;***
 
-;;;### (autoloads nil "alchemist-iex" "alchemist-iex.el" (22056 24104
-;;;;;;  46811 0))
+;;;### (autoloads nil "alchemist-iex" "alchemist-iex.el" (22066 4260
+;;;;;;  909223 209000))
 ;;; Generated autoloads from alchemist-iex.el
 
 (defalias 'run-elixir 'alchemist-iex-run)
@@ -39,7 +39,7 @@ Show the IEx buffer if an IEx process is already run.
 ;;;***
 
 ;;;### (autoloads nil "alchemist-phoenix" "alchemist-phoenix.el"
-;;;;;;  (22056 24104 38811 0))
+;;;;;;  (22066 4260 897223 203000))
 ;;; Generated autoloads from alchemist-phoenix.el
 
 (autoload 'alchemist-phoenix-project-p "alchemist-phoenix" "\
@@ -66,7 +66,7 @@ The following commands are available:
 ;;;***
 
 ;;;### (autoloads nil "alchemist-refcard" "alchemist-refcard.el"
-;;;;;;  (22056 24104 18811 0))
+;;;;;;  (22066 4260 869223 189000))
 ;;; Generated autoloads from alchemist-refcard.el
 
 (autoload 'alchemist-refcard "alchemist-refcard" "\
@@ -77,7 +77,7 @@ Generate an Alchemist refcard of all the features.
 ;;;***
 
 ;;;### (autoloads nil "alchemist-test-mode" "alchemist-test-mode.el"
-;;;;;;  (22056 24104 58811 0))
+;;;;;;  (22066 4260 921223 215000))
 ;;; Generated autoloads from alchemist-test-mode.el
 
 (autoload 'alchemist-test-mode "alchemist-test-mode" "\
@@ -105,7 +105,7 @@ The following commands are available:
 ;;;;;;  "alchemist-macroexpand.el" "alchemist-message.el" "alchemist-mix.el"
 ;;;;;;  "alchemist-pkg.el" "alchemist-project.el" "alchemist-report.el"
 ;;;;;;  "alchemist-scope.el" "alchemist-server.el" "alchemist-utils.el")
-;;;;;;  (22056 24104 65598 841000))
+;;;;;;  (22066 4260 921223 215000))
 
 ;;;***
 
