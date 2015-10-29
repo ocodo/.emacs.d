0b3fe7c7c6f3f0d749521767cff51061e2023c43
JasonM23 <jasonm23@gmail.com>
2015-10-29 08:28:09 -0400

    [Updating flx version: 20151028.1546][removing old version: 20140921.739]

--- elpa/flx-20151028.1546/flx-autoloads.el
+++ elpa/flx-20151028.1546/flx-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("flx.el") (21536 48401 972407 152000))
+;;;### (autoloads nil nil ("flx.el") (22066 4215 325199 812000))
 
 ;;;***
 
---
+++ elpa/flx-20151028.1546/flx-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flx" "20151028.1546" "fuzzy matching with good sorting" '((cl-lib "0.3")) :url "https://github.com/lewang/flx")
