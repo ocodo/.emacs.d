56cb44e5877d2ca9962c024721d6003c550b6ef7
JasonM23 <jasonm23@gmail.com>
2015-10-29 08:28:25 -0400

    [Adding go-rename version: 20151027.638]

---
+++ elpa/go-rename-20151027.638/go-rename-autoloads.el
@@ -0,0 +1,24 @@
+;;; go-rename-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "go-rename" "go-rename.el" (22066 4197 221190
+;;;;;;  112000))
+;;; Generated autoloads from go-rename.el
+
+(autoload 'go-rename "go-rename" "\
+Rename the entity denoted by the identifier at point, using
+the `gorename' tool. With FORCE, call `gorename' with the
+`-force' flag.
+
+\(fn NEW-NAME &optional FORCE)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; go-rename-autoloads.el ends here
