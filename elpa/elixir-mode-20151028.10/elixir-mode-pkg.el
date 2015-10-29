f4fbef6fc15d53cd0085bcc83302664a679c541a
JasonM23 <jasonm23@gmail.com>
2015-10-29 08:28:06 -0400

    [Updating elixir-mode version: 20151028.10][removing old version: 20151024.855]

--- elpa/elixir-mode-20151028.10/elixir-mode-pkg.el
+++ elpa/elixir-mode-20151028.10/elixir-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "elixir-mode" "20151024.855" "Major mode for editing Elixir files"
+(define-package "elixir-mode" "20151028.10" "Major mode for editing Elixir files"
   '((emacs "24")
     (pkg-info "0.4"))
   :url "https://github.com/elixir-lang/emacs-elixir" :keywords
