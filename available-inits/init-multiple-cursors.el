83f405e27f9552e7f838f94f8d0ebc0bfb65e274
jasonm23 <jasonm23@gmail.com>
2015-10-31 18:57:15 +0800

    Add multiple cursors use-package

--- available-inits/init-multiple-cursors.el
+++ available-inits/init-multiple-cursors.el
@@ -1,4 +1,17 @@
+;;; init-multiple-cursors --- initialize multiple cursors
+;;; Commentary:
+;;; Code:
+(require 'use-package)
+
 (use-package multiple-cursors
-  :bind (("C->" . mc/mark-next-like-this)
-         ("C-<" . mc/mark-all-like-this)
-         ("C-c C-<" . mc/mark-previous-like-this)))
+  :bind
+  (("C->" . mc/mark-next-like-this)
+   ("C-<" . mc/mark-all-like-this)
+   ("C-c C-<" . mc/mark-previous-like-this))
+
+  :init
+  (progn
+    (define-key cua--rectangle-keymap (kbd "M-U") 'mc/cua-rectangle-to-multiple-cursors)))
+
+(provide 'init-multiple-cursors)
+;;; init-multiple-cursors.el ends here
