60913cc7739346b0c7c75085fd8a36e0049b2d79
JasonM23 <jasonm23@gmail.com>
2015-10-31 06:28:52 -0400

    [Updating lispy version: 20151030.808][removing old version: 20151029.544]

--- elpa/lispy-20151030.808/lispy-autoloads.el
+++ elpa/lispy-20151030.808/lispy-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "lispy" "lispy.el" (22067 3467 155645 547000))
+;;;### (autoloads nil "lispy" "lispy.el" (22068 38780 866169 457000))
 ;;; Generated autoloads from lispy.el
 
 (autoload 'lispy-mode "lispy" "\
@@ -34,7 +34,7 @@ backward through lists, which is useful to move into special.
 
 ;;;### (autoloads nil nil ("elpa.el" "le-clojure.el" "le-lisp.el"
 ;;;;;;  "le-scheme.el" "lispy-inline.el" "lispy-pkg.el" "lispy-tags.el")
-;;;;;;  (22067 3467 155645 547000))
+;;;;;;  (22068 38780 882560 495000))
 
 ;;;***
 
