ce5cb22019d00affb723cf911d0b4c0a22c8a670
jasonm23 <jasonm23@gmail.com>
2015-11-01 14:37:02 +0800

    [Adding scad-preview version: 20150818.624]

---
+++ elpa/scad-preview-20150818.624/scad-preview-autoloads.el
@@ -0,0 +1,22 @@
+;;; scad-preview-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "scad-preview" "scad-preview.el" (22069 45700
+;;;;;;  0 0))
+;;; Generated autoloads from scad-preview.el
+
+(autoload 'scad-preview-mode "scad-preview" "\
+Preview SCAD models in real-time within Emacs.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; scad-preview-autoloads.el ends here
---
+++ elpa/scad-preview-20150818.624/scad-preview-pkg.el
@@ -0,0 +1,1 @@
+(define-package "scad-preview" "20150818.624" "Preview SCAD models in real-time within Emacs" '((scad-mode "91.0")) :url "http://hins11.yu-yake.com/")
