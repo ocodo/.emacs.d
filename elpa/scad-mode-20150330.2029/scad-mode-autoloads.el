f54782989f2b6bcda1157c53562259f44a1849ba
jasonm23 <jasonm23@gmail.com>
2015-11-01 14:37:02 +0800

    [Adding scad-mode version: 20150330.2029]

---
+++ elpa/scad-mode-20150330.2029/scad-mode-autoloads.el
@@ -0,0 +1,36 @@
+;;; scad-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "scad-mode" "scad-mode.el" (22069 45701 0 0))
+;;; Generated autoloads from scad-mode.el
+
+(add-to-list 'auto-mode-alist '("\\.scad$" . scad-mode))
+
+(autoload 'scad-mode "scad-mode" "\
+Major mode for editing OpenSCAD code.
+
+To see what version of CC Mode you are running, enter `\\[c-version]'.
+
+The hook `c-mode-common-hook' is run with no args at mode
+initialization, then `scad-mode-hook'.
+
+Key bindings:
+\\{scad-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("scad-mode-pkg.el") (22069 45701 417738
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; scad-mode-autoloads.el ends here
