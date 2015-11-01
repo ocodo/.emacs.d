2b26bb50a22baa4da0b434f662da73e0829203b8
jasonm23 <jasonm23@gmail.com>
2015-11-01 14:37:02 +0800

    [Adding pastelmac-theme version: 20151030.1936]

---
+++ elpa/pastelmac-theme-20151030.1936/pastelmac-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "pastelmac-theme" "20151030.1936" "a soothing theme with a pastel color palette" '((emacs "24.1")) :url "https://github.com/bmastenbrook/pastelmac-theme-el" :keywords '("themes"))
