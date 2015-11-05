af420f956359d57e53a070e4376e291f9a9745da
jasonm23 <jasonm23@gmail.com>
2015-11-05 17:26:45 +0800

    Haml mode to use-package

--- available-inits/init-haml.el
+++ available-inits/init-haml.el
@@ -1,3 +1,10 @@
-;; init-haml
-(add-to-list 'auto-mode-alist '("\\.hamlc" . haml-mode))
+;;; init-haml --- initialize haml
+;;; Commentary:
+;;; Code:
+(require 'use-package)
+
+(use-package haml
+   :mode (("\\.hamlc" . haml-mode)))
+
 (provide 'init-haml)
+;;; init-haml ends here
