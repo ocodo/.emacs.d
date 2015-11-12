957320817e1c652f524f0f9bae900e00a8b7be87
JasonM23 <jasonm23@gmail.com>
2015-11-11 22:56:20 -0500

    [Updating avy version: 20151111.450][removing old version: 20151026.59]

--- elpa/avy-20151111.450/avy-autoloads.el
+++ elpa/avy-20151111.450/avy-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "avy" "avy.el" (22066 4257 913221 714000))
+;;;### (autoloads nil "avy" "avy.el" (22084 2017 912928 372000))
 ;;; Generated autoloads from avy.el
 
 (autoload 'avy-goto-char "avy" "\
---
+++ elpa/avy-20151111.450/avy-pkg.el
@@ -0,0 +1,1 @@
+(define-package "avy" "20151111.450" "set-based completion" '((emacs "24.1") (cl-lib "0.5")) :url "https://github.com/abo-abo/avy" :keywords '("point" "location"))
