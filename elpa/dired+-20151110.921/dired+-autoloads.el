62f001d6a4bd0878944a2fa1a4c12918d8f65ab5
JasonM23 <jasonm23@gmail.com>
2015-11-11 23:06:27 -0500

    [Updating dired+ version: 20151110.921][removing old version: 20151002.1015]

--- elpa/dired+-20151110.921/dired+-autoloads.el
+++ elpa/dired+-20151110.921/dired+-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "dired+" "dired+.el" (22045 48075 265140 0))
+;;;### (autoloads nil "dired+" "dired+.el" (22084 1985 344925 897000))
 ;;; Generated autoloads from dired+.el
 
 (defvar diff-switches "-c" "\
