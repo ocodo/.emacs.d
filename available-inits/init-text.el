0082806bfd3bfedc45e8c3df0bc8070a7e4a4b7c
jasonm23 <jasonm23@gmail.com>
2015-11-13 16:42:40 +0800

    Add C-c m to Text-Mode map - switch to markdown-mode

---
+++ available-inits/init-text.el
@@ -0,0 +1,12 @@
+;;; init-text --- initialize text-mode
+;;; Commentary:
+;;;
+;;; Note text-mode is a bit weird, in that you cannot require it (it's already loaded) so use-package isn't needed.
+;;;
+;;; Code:
+(require 'bind-key)
+
+(bind-key "C-c m" 'markdown-mode text-mode-map)
+
+(provide 'init-text)
+;;; init-text ends here
