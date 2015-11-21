1bb018560bb98a45afdb4156a95259c830973160
JasonM23 <jasonm23@gmail.com>
2015-11-20 21:33:12 -0500

    [Updating evil-nerd-commenter version: 20151111.2326][removing old version: 20151111.1842]

--- elpa/evil-nerd-commenter-20151111.2326/evil-nerd-commenter-autoloads.el
+++ elpa/evil-nerd-commenter-20151111.2326/evil-nerd-commenter-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "evil-nerd-commenter" "evil-nerd-commenter.el"
-;;;;;;  (22084 1974 128925 127000))
+;;;;;;  (22095 53627 745805 371000))
 ;;; Generated autoloads from evil-nerd-commenter.el
 
 (autoload 'evilnc-comment-or-uncomment-paragraphs "evil-nerd-commenter" "\
@@ -85,7 +85,7 @@ Set the hotkeys of evil-nerd-comment.
 ;;;***
 
 ;;;### (autoloads nil nil ("evil-nerd-commenter-operator.el" "evil-nerd-commenter-pkg.el")
-;;;;;;  (22084 1974 139007 888000))
+;;;;;;  (22095 53627 758235 155000))
 
 ;;;***
 
