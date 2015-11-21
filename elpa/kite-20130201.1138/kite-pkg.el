9aa843f4ef28ffcd2184188cbab4e72a7f6e1639
JasonM23 <jasonm23@gmail.com>
2015-11-20 21:46:51 -0500

    [Removing kite version: 20130201.1138]

--- elpa/kite-20130201.1138/kite-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "kite" "20130201.1138" "WebKit inspector front-end"
-  '((json "1.2")
-    (websocket "0.93.1"))
-  :keywords
-  '("tools"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
