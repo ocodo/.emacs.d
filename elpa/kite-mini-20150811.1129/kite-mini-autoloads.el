f875eccd33c7aad7591e76bdaed3b45d6e9dac71
JasonM23 <jasonm23@gmail.com>
2015-11-20 21:55:36 -0500

    [Adding kite-mini version: 20150811.1129]

---
+++ elpa/kite-mini-20150811.1129/kite-mini-autoloads.el
@@ -0,0 +1,32 @@
+;;; kite-mini-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "kite-mini" "kite-mini.el" (22095 56587 815887
+;;;;;;  404000))
+;;; Generated autoloads from kite-mini.el
+
+(autoload 'turn-on-kite-mini-mode "kite-mini" "\
+Turn on Kite Mini mode.
+
+\(fn)" nil nil)
+
+(autoload 'turn-off-kite-mini-mode "kite-mini" "\
+Turn off Kite Mini mode.
+
+\(fn)" nil nil)
+
+(autoload 'kite-mini-mode "kite-mini" "\
+Minor mode for interact with WebKit remote debugging API.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; kite-mini-autoloads.el ends here
---
+++ elpa/kite-mini-20150811.1129/kite-mini-pkg.el
@@ -0,0 +1,1 @@
+(define-package "kite-mini" "20150811.1129" "Remotely evaluate JavaScript in the WebKit debugger" '((dash "2.11.0") (websocket "1.5")) :url "https://github.com/tungd/kite-mini.el" :keywords '("webkit"))
