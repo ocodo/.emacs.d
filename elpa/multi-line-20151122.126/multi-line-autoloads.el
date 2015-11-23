e0f4b572fc70ab525c919d67ec775471f060082b
JasonM23 <jasonm23@gmail.com>
2015-11-22 20:24:14 -0500

    [Adding multi-line version: 20151122.126]

---
+++ elpa/multi-line-20151122.126/multi-line-autoloads.el
@@ -0,0 +1,29 @@
+;;; multi-line-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "multi-line" "multi-line.el" (22098 25093 249099
+;;;;;;  181000))
+;;; Generated autoloads from multi-line.el
+
+(autoload 'multi-line "multi-line" "\
+Multi-line the statement at point.
+
+When ARG is provided single-line the statement at point instead.
+
+\(fn ARG)" t nil)
+
+(autoload 'multi-line-single-line "multi-line" "\
+Single-line the statement at point.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; multi-line-autoloads.el ends here
---
+++ elpa/multi-line-20151122.126/multi-line-pkg.el
@@ -0,0 +1,1 @@
+(define-package "multi-line" "20151122.126" "multi-line statements" '((emacs "24")) :url "https://github.com/IvanMalison/multi-line" :keywords '("multi" "line" "length" "whitespace" "programming"))
