e1e6107e3b792a073ecac235d39b8d9c461ec80a
JasonM23 <jasonm23@gmail.com>
2015-11-22 20:24:04 -0500

    [Updating darktooth-theme version: 20151121.1722][removing old version: 20151118.2251]

--- elpa/darktooth-theme-20151118.2251/darktooth-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "darktooth-theme" "20151118.2251" "Color theme for Emacs, when soothe and gruvbox collide" 'nil :url "http://github.com/emacsfodder/emacs-theme-darktooth")
---
+++ elpa/darktooth-theme-20151121.1722/darktooth-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "darktooth-theme" "20151121.1722" "Color theme for Emacs, when soothe and gruvbox collide" 'nil :url "http://github.com/emacsfodder/emacs-theme-darktooth")
