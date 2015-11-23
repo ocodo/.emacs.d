655438eef7a87755d56b25d79ee9e62a9489fb46
JasonM23 <jasonm23@gmail.com>
2015-11-22 20:24:14 -0500

    [Adding flycheck-css-colorguard version: 20151122.147]

---
+++ elpa/flycheck-css-colorguard-20151122.147/flycheck-css-colorguard-autoloads.el
@@ -0,0 +1,16 @@
+;;; flycheck-css-colorguard-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("flycheck-css-colorguard.el") (22098 25094
+;;;;;;  833869 923000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-css-colorguard-autoloads.el ends here
