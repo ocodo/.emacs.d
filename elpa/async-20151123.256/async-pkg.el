6227185e40ff0bee91224dcedae1816122a4f949
JasonM23 <jasonm23@gmail.com>
2015-11-30 14:41:38 -0500

    [Updating async version: 20151123.256][removing old version: 20151120.228]

--- elpa/async-20151123.256/async-pkg.el
+++ elpa/async-20151123.256/async-pkg.el
@@ -1,4 +1,4 @@
-(define-package "async" "20151120.228" "Asynchronous processing in Emacs"
+(define-package "async" "20151123.256" "Asynchronous processing in Emacs"
   '((emacs "24")
     (cl-lib "0.5"))
   :url "https://github.com/jwiegley/emacs-async")
