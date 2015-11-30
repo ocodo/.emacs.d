df7530a5106aefdf0bf20c2818218cbe3b8300c1
JasonM23 <jasonm23@gmail.com>
2015-11-30 14:42:13 -0500

    [Updating with-editor version: 20151126.323][removing old version: 20151111.418]

--- elpa/with-editor-20151126.323/with-editor-autoloads.el
+++ elpa/with-editor-20151126.323/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22084 1794 644458 744000))
+;;;;;;  (22108 42276 749346 584000))
 
 ;;;***
 
