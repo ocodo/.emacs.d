9f056a5743b97a20906df90723db4d9054d778c2
JasonM23 <jasonm23@gmail.com>
2015-11-30 14:42:06 -0500

    [Updating paradox version: 20151130.233][removing old version: 20151110.501]

--- elpa/paradox-20151130.233/paradox-autoloads.el
+++ elpa/paradox-20151130.233/paradox-autoloads.el
@@ -3,7 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "paradox" "paradox.el" (22084 1836 428919 618000))
+;;;### (autoloads nil "paradox" "paradox.el" (22108 42312 481171
+;;;;;;  844000))
 ;;; Generated autoloads from paradox.el
 
 (autoload 'paradox-list-packages "paradox" "\
@@ -62,7 +63,7 @@ empty.  Passing a non-nil REFRESH argument forces this update.
 
 ;;;### (autoloads nil nil ("paradox-commit-list.el" "paradox-core.el"
 ;;;;;;  "paradox-execute.el" "paradox-github.el" "paradox-menu.el"
-;;;;;;  "paradox-pkg.el") (22084 1836 444125 891000))
+;;;;;;  "paradox-pkg.el") (22108 42312 504039 848000))
 
 ;;;***
 
