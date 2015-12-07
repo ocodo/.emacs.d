996d3c8c275b366de6f3fe40ac319293d66862c9
jasonm23 <jasonm23@gmail.com>
2015-12-07 17:04:59 +0800

    Add fatness flipper to coffee mode map C-c C-\

--- available-inits/init-coffee.el
+++ available-inits/init-coffee.el
@@ -8,12 +8,22 @@
   :config
   (progn
     (use-package jasmine-coffee)
+
+    (defun coffee-flip-that-fatness ()
+      "Flip fatness of a coffee function def syntax arrow."
+      (interactive)
+      (save-excursion
+        (search-backward-regexp "[-=]>")
+        (when (looking-at "=") (kill-char 1) (insert "-"))
+        (when (looking-at "-") (kill-char 1) (insert "="))))
+
     (bind-keys :map coffee-mode-map
-               ("C-c C-n" . flymake-goto-next-error)
-               ("C-c C-p" . flymake-goto-prev-error)
-               ("C-c C-r" . coffee-compile-region)
-               ("C-c C-," . coffee-indent-shift-left)
-               ("C-c C-." . coffee-indent-shift-right))))
+               ("C-c C-\\" . coffee-flip-that-fatness)
+               ("C-c C-n"  . flymake-goto-next-error)
+               ("C-c C-p"  . flymake-goto-prev-error)
+               ("C-c C-r"  . coffee-compile-region)
+               ("C-c C-,"  . coffee-indent-shift-left)
+               ("C-c C-."  . coffee-indent-shift-right))))
 
 (provide 'init-coffee)
 ;;; init-coffee ends here
