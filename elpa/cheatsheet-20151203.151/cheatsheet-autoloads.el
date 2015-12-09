6f2e4bc98b5b86bbb63108e35aa91669c8eca052
JasonM23 <jasonm23@gmail.com>
2015-12-09 08:43:55 -0500

    [Adding cheatsheet version: 20151203.151]

---
+++ elpa/cheatsheet-20151203.151/cheatsheet-autoloads.el
@@ -0,0 +1,27 @@
+;;; cheatsheet-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "cheatsheet" "cheatsheet.el" (22120 11794 737787
+;;;;;;  445000))
+;;; Generated autoloads from cheatsheet.el
+
+(autoload 'cheatsheet-add "cheatsheet" "\
+Add CHEAT to cheatsheet.
+
+\(fn &rest CHEAT)" nil nil)
+
+(autoload 'cheatsheet-show "cheatsheet" "\
+Create buffer and show cheatsheet.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; cheatsheet-autoloads.el ends here
