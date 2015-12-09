552597b600b05a0d96ea2a0e441c419836078911
JasonM23 <jasonm23@gmail.com>
2015-12-09 08:43:55 -0500

    [Adding borland-blue-theme version: 20151206.1038]

---
+++ elpa/borland-blue-theme-20151206.1038/borland-blue-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "borland-blue-theme" "20151206.1038" "Blue/yellow theme based on old DOS Borland/Turbo C IDE" '((emacs "24.1")) :url "http://github.com/fourier/borland-blue-theme" :keywords '("themes"))
