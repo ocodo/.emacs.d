ef7beaf4869fc6d17da545415704846504917494
JasonM23 <jasonm23@gmail.com>
2015-12-09 08:44:18 -0500

    [Updating hydra version: 20151208.124][removing old version: 20151030.1203]

--- elpa/hydra-20151208.124/hydra-autoloads.el
+++ elpa/hydra-20151208.124/hydra-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "hydra" "hydra.el" (22068 38803 446139 947000))
+;;;### (autoloads nil "hydra" "hydra.el" (22120 11688 948800 178000))
 ;;; Generated autoloads from hydra.el
 
 (autoload 'defhydra "hydra" "\
@@ -63,7 +63,7 @@ result of `defhydra'.
 ;;;***
 
 ;;;### (autoloads nil nil ("hydra-examples.el" "hydra-ox.el" "hydra-pkg.el"
-;;;;;;  "lv.el") (22068 38803 458897 590000))
+;;;;;;  "lv.el") (22120 11688 968707 206000))
 
 ;;;***
 
--- elpa/hydra-20151208.124/hydra-pkg.el
+++ elpa/hydra-20151208.124/hydra-pkg.el
@@ -1,4 +1,4 @@
-(define-package "hydra" "20151030.1203" "Make bindings that stick around."
+(define-package "hydra" "20151208.124" "Make bindings that stick around."
   '((cl-lib "0.5"))
   :url "https://github.com/abo-abo/hydra" :keywords
   '("bindings"))
