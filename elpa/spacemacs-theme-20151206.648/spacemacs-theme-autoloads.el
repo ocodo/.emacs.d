5c3551f0839fe2604d8139b67b7cdff6349aec98
JasonM23 <jasonm23@gmail.com>
2015-12-09 08:44:31 -0500

    [Updating spacemacs-theme version: 20151206.648][removing old version: 20151125.1035]

--- elpa/spacemacs-theme-20151206.648/spacemacs-theme-autoloads.el
+++ elpa/spacemacs-theme-20151206.648/spacemacs-theme-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "spacemacs-common" "spacemacs-common.el" (22108
-;;;;;;  42297 281174 649000))
+;;;### (autoloads nil "spacemacs-common" "spacemacs-common.el" (22120
+;;;;;;  11620 952059 126000))
 ;;; Generated autoloads from spacemacs-common.el
 
 (when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -12,7 +12,7 @@
 ;;;***
 
 ;;;### (autoloads nil nil ("spacemacs-dark-theme.el" "spacemacs-light-theme.el"
-;;;;;;  "spacemacs-theme-pkg.el") (22108 42297 292294 506000))
+;;;;;;  "spacemacs-theme-pkg.el") (22120 11620 965145 812000))
 
 ;;;***
 
