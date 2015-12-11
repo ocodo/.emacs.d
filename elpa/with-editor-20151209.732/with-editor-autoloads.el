ce6f70e00cc56b453144736896e693e1c768e86c
JasonM23 <jasonm23@gmail.com>
2015-12-10 23:07:54 -0500

    [Updating with-editor version: 20151209.732][removing old version: 20151126.323]

--- elpa/with-editor-20151209.732/with-editor-autoloads.el
+++ elpa/with-editor-20151209.732/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22108 42276 749346 584000))
+;;;;;;  (22122 19296 869645 508000))
 
 ;;;***
 
--- elpa/with-editor-20151209.732/with-editor-pkg.el
+++ elpa/with-editor-20151209.732/with-editor-pkg.el
@@ -1,4 +1,4 @@
-(define-package "with-editor" "20151126.323" "Use the Emacsclient as $EDITOR"
+(define-package "with-editor" "20151209.732" "Use the Emacsclient as $EDITOR"
   '((emacs "24.4")
     (async "20150909.2257")
     (dash "20151021.113"))
