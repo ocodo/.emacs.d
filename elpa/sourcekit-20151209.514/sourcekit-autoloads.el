d57d4c0e69d5d88c1e04a4a9bafbece71a221ff3
JasonM23 <jasonm23@gmail.com>
2015-12-10 23:08:24 -0500

    [Adding sourcekit version: 20151209.514]

---
+++ elpa/sourcekit-20151209.514/sourcekit-autoloads.el
@@ -0,0 +1,15 @@
+;;; sourcekit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("sourcekit.el") (22122 19367 815206 698000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sourcekit-autoloads.el ends here
---
+++ elpa/sourcekit-20151209.514/sourcekit-pkg.el
@@ -0,0 +1,1 @@
+(define-package "sourcekit" "20151209.514" "Library to interact with sourcekittendaemon" '((emacs "24.3") (dash "2.12.1") (dash-functional "1.2.0")) :url "https://github.com/nathankot/company-sourcekit" :keywords '("tools" "processes"))
