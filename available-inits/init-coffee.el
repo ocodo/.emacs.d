747583d36e960ba1c09a5e0fbf1367f2b9e58ec3
jasonm23 <jasonm23@gmail.com>
2015-12-14 10:50:13 +0800

    Temporarily disable coffee-mode at face prior to debugging

--- available-inits/init-coffee.el
+++ available-inits/init-coffee.el
@@ -9,20 +9,20 @@
   (progn
     (use-package jasmine-coffee)
 
-    (defface coffee-at-face
-      '((t (:foreground "#FF6600")))
-      "Coffee-Mode face for the @ (this) sigil.")
+    ;; (defface coffee-at-face
+    ;;   '((t (:foreground "#FF6600")))
+    ;;   "Coffee-Mode face for the @ (this) sigil.")
 
-    (let (at-face)
-      (if (boundp 'coffee-at-face)
-          (setq at-face coffee-at-face)
-        (setq at-face font-lock-string-face))
+    ;; (let (at-face)
+    ;;   (if (boundp 'coffee-at-face)
+    ;;       (setq at-face coffee-at-face)
+    ;;     (setq at-face font-lock-string-face))
 
-      (font-lock-add-keywords
-       'coffee-mode
-       '(("\\(@\\)\\([_[:word:]]+\\|\\<this\\)\\>"
-          (1 at-face)
-          (2 font-lock-variable-name-face)))))
+    ;;   (font-lock-add-keywords
+    ;;    'coffee-mode
+    ;;    '(("\\(@\\)\\([_[:word:]]+\\|\\<this\\)\\>"
+    ;;       (1 at-face)
+    ;;       (2 font-lock-variable-name-face)))))
 
     (bind-keys :map coffee-mode-map
                ("C-c C-\\" . coffee-toggle-fatness)
