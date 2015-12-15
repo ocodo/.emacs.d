b9014cc693e8276dd4b5074696b6e0067e968217
JasonM23 <jasonm23@gmail.com>
2015-12-15 04:24:21 -0500

    [Removing smex version: 20150822.1146]

--- elpa/smex-20150822.1146/smex-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smex" "20150822.1146" "M-x interface with Ido-style fuzzy matching." '((emacs "24")) :url "http://github.com/nonsequitur/smex/" :keywords '("convenience" "usability"))
