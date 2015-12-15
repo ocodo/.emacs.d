e5787d61b7c9f2e8e93b74fbcab325a950e47ba7
JasonM23 <jasonm23@gmail.com>
2015-12-15 04:16:14 -0500

    [Adding toggle version: 20151210.1527]

---
+++ elpa/toggle-20151210.1527/toggle-autoloads.el
@@ -0,0 +1,28 @@
+;;; toggle-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "toggle" "toggle.el" (22127 55783 622790 535000))
+;;; Generated autoloads from toggle.el
+
+(autoload 'toggle-style "toggle" "\
+
+
+\(fn NAME)" t nil)
+
+(autoload 'toggle-buffer "toggle" "\
+Opens a related file to the current buffer using matching rules.
+Matches the current buffer against rules in toggle-mappings. If a
+match is found, switches to that buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; toggle-autoloads.el ends here
---
+++ elpa/toggle-20151210.1527/toggle-pkg.el
@@ -0,0 +1,1 @@
+(define-package "toggle" "20151210.1527" "quickly open corresponding file (eg test vs impl)." 'nil :keywords '("files" "extensions" "convenience"))
