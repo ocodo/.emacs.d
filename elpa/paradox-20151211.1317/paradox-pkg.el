aab2071abcdcb8474a4328759cd1ecb188bee173
JasonM23 <jasonm23@gmail.com>
2015-12-15 04:16:12 -0500

    [Adding paradox version: 20151211.1317]

---
+++ elpa/paradox-20151211.1317/paradox-pkg.el
@@ -0,0 +1,13 @@
+(define-package "paradox" "20151211.1317" "A modern Packages Menu. Colored, with package ratings, and customizable."
+  '((emacs "24.4")
+    (seq "1.7")
+    (cl-lib "0.5")
+    (json "1.3")
+    (let-alist "1.0.3")
+    (spinner "1.4")
+    (hydra "0.13.2"))
+  :url "https://github.com/Malabarba/paradox" :keywords
+  '("package" "packages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
