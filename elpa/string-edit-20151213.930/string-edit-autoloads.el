880521f632bb5275c7ee8f28ed462044150ef2d6
jasonm23 <jasonm23@gmail.com>
2015-12-16 17:02:46 +0800

    [Adding string-edit version: 20151213.930]

---
+++ elpa/string-edit-20151213.930/string-edit-autoloads.el
@@ -0,0 +1,22 @@
+;;; string-edit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "string-edit" "string-edit.el" (22129 1810
+;;;;;;  0 0))
+;;; Generated autoloads from string-edit.el
+
+(autoload 'string-edit-at-point "string-edit" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; string-edit-autoloads.el ends here
---
+++ elpa/string-edit-20151213.930/string-edit-pkg.el
@@ -0,0 +1,1 @@
+(define-package "string-edit" "20151213.930" "Avoid escape nightmares by editing string in separate buffer" '((dash "1.2.0")))
