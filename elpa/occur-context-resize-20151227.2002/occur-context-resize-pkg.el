563528dd96682c7ef03e51e86fcaeef5a0c9e3b2
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:47 -0500

    [Adding occur-context-resize version: 20151227.2002]

---
+++ elpa/occur-context-resize-20151227.2002/occur-context-resize-pkg.el
@@ -0,0 +1,1 @@
+(define-package "occur-context-resize" "20151227.2002" "dynamically resize context around matches in occur-mode" 'nil :url "https://github.com/dgtized/occur-context-resize.el" :keywords '("matching"))
