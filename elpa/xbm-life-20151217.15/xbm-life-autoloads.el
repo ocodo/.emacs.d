860dc7bccb7834cfcfea413f89247378f53a3f52
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:54 -0500

    [Adding xbm-life version: 20151217.15]

---
+++ elpa/xbm-life-20151217.15/xbm-life-autoloads.el
@@ -0,0 +1,24 @@
+;;; xbm-life-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "xbm-life" "xbm-life.el" (22145 57043 406125
+;;;;;;  263000))
+;;; Generated autoloads from xbm-life.el
+
+(autoload 'xbm-life "xbm-life" "\
+Launch a XBM demo of Conway's Game of Life.
+Use ARG as prefix argument to create a buffer with a different
+name.
+
+\(fn ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; xbm-life-autoloads.el ends here
---
+++ elpa/xbm-life-20151217.15/xbm-life-pkg.el
@@ -0,0 +1,1 @@
+(define-package "xbm-life" "20151217.15" "A XBM version of Conway's Game of Life" 'nil :url "https://github.com/wasamasa/xbm-life" :keywords '("games"))
