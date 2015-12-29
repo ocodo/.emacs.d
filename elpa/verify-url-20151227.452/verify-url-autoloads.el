8296334d2ae9140705cba9648379e5d6f4edf49f
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:53 -0500

    [Adding verify-url version: 20151227.452]

---
+++ elpa/verify-url-20151227.452/verify-url-autoloads.el
@@ -0,0 +1,22 @@
+;;; verify-url-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "verify-url" "verify-url.el" (22145 57676 570875
+;;;;;;  932000))
+;;; Generated autoloads from verify-url.el
+
+(autoload 'verify-url "verify-url" "\
+find out invalid urls in buffer or region
+
+\(fn &optional START END)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; verify-url-autoloads.el ends here
---
+++ elpa/verify-url-20151227.452/verify-url-pkg.el
@@ -0,0 +1,1 @@
+(define-package "verify-url" "20151227.452" "No description available." 'nil)
