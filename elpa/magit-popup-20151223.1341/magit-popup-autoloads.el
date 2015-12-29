c17371f1de2c8a8617754f795ea556a63794ce08
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:46 -0500

    [Adding magit-popup version: 20151223.1341]

---
+++ elpa/magit-popup-20151223.1341/magit-popup-autoloads.el
@@ -0,0 +1,16 @@
+;;; magit-popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
+;;;;;;  (22145 57131 640026 348000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magit-popup-autoloads.el ends here
