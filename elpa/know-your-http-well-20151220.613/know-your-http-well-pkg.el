31b7a4882e075d88fb753d8c574008cc3b6be193
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:44 -0500

    [Adding know-your-http-well version: 20151220.613]

---
+++ elpa/know-your-http-well-20151220.613/know-your-http-well-pkg.el
@@ -0,0 +1,4 @@
+(define-package "know-your-http-well" "20151220.613" "Look up the meaning of HTTP headers, methods, relations, status codes" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
