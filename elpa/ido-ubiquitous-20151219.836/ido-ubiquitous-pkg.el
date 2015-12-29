2819400c6f65fc5128735d3a29c36fea9fcb17cd
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:43 -0500

    [Adding ido-ubiquitous version: 20151219.836]

---
+++ elpa/ido-ubiquitous-20151219.836/ido-ubiquitous-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ido-ubiquitous" "20151219.836" "Use ido (nearly) everywhere." '((emacs "24.1") (ido-completing-read+ "3.10") (cl-lib "0.5") (s "0")) :url "https://github.com/DarwinAwardWinner/ido-ubiquitous" :keywords '("convenience" "completion" "ido"))
