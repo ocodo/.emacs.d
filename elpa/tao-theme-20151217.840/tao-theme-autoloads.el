21fbd1bd80f46516ded6074f60db36825907a9d2
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:52 -0500

    [Adding tao-theme version: 20151217.840]

---
+++ elpa/tao-theme-20151217.840/tao-theme-autoloads.el
@@ -0,0 +1,32 @@
+;;; tao-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "tao-yang-theme" "tao-yang-theme.el" (22145
+;;;;;;  57058 670148 510000))
+;;; Generated autoloads from tao-yang-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "tao-yin-theme" "tao-yin-theme.el" (22145 57058
+;;;;;;  690148 541000))
+;;; Generated autoloads from tao-yin-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("tao-theme-pkg.el" "tao-yinyang-common.el")
+;;;;;;  (22145 57058 701184 66000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; tao-theme-autoloads.el ends here
