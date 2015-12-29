d0e5d777ab02a01f825741fecb0c87aa4643ab9e
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:45 -0500

    [Adding lentic version: 20151223.114]

---
+++ elpa/lentic-20151223.114/lentic-pkg.el
@@ -0,0 +1,9 @@
+(define-package "lentic" "20151223.114" "One buffer as a view of another"
+  '((emacs "24.4")
+    (m-buffer "0.13")
+    (dash "2.5.0")
+    (f "0.17.2")
+    (s "1.9.0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
