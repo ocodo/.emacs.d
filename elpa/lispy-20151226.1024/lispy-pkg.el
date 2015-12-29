41a0a02bcf58c6e46a5438f6c5435c13fe50e5aa
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:45 -0500

    [Adding lispy version: 20151226.1024]

---
+++ elpa/lispy-20151226.1024/lispy-pkg.el
@@ -0,0 +1,8 @@
+(define-package "lispy" "20151226.1024" "vi-like Paredit"
+  '((emacs "24.1")
+    (ace-window "0.9.0")
+    (iedit "0.97")
+    (swiper "0.4.0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
