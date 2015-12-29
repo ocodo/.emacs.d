22c6ce9f6e95a874e66d5894ef93a90932b90e18
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:54 -0500

    [Adding with-editor version: 20151223.1341]

---
+++ elpa/with-editor-20151223.1341/with-editor-pkg.el
@@ -0,0 +1,9 @@
+(define-package "with-editor" "20151223.1341" "Use the Emacsclient as $EDITOR"
+  '((emacs "24.4")
+    (async "20150909.2257")
+    (dash "20151021.113"))
+  :url "https://github.com/magit/magit" :keywords
+  '("tools"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
