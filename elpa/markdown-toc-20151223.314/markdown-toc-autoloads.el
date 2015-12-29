a73af94a4a811735d72fd0c24067bc35aa73b727
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:47 -0500

    [Adding markdown-toc version: 20151223.314]

---
+++ elpa/markdown-toc-20151223.314/markdown-toc-autoloads.el
@@ -0,0 +1,34 @@
+;;; markdown-toc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "markdown-toc" "markdown-toc.el" (22145 57126
+;;;;;;  870248 98000))
+;;; Generated autoloads from markdown-toc.el
+
+(autoload 'markdown-toc-version "markdown-toc" "\
+Markdown-toc version.
+
+\(fn)" t nil)
+
+(autoload 'markdown-toc-generate-toc "markdown-toc" "\
+Generate a TOC for markdown file at current point.
+Deletes any previous TOC.
+If called interactively with prefix arg REPLACE-TOC-P, replaces previous TOC.
+
+\(fn &optional REPLACE-TOC-P)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("markdown-toc-pkg.el") (22145 57126 881411
+;;;;;;  995000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; markdown-toc-autoloads.el ends here
---
+++ elpa/markdown-toc-20151223.314/markdown-toc-pkg.el
@@ -0,0 +1,7 @@
+(define-package "markdown-toc" "20151223.314" "A simple TOC generator for markdown file"
+  '((s "1.9.0")
+    (dash "2.11.0")
+    (markdown-mode "2.0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
