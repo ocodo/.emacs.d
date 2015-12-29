5dff715a73025690324db331df395610093bf0a5
JasonM23 <jasonm23@gmail.com>
2015-12-28 20:28:45 -0500

    [Adding loccur version: 20151227.2013]

---
+++ elpa/loccur-20151227.2013/loccur-autoloads.el
@@ -0,0 +1,23 @@
+;;; loccur-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "loccur" "loccur.el" (22145 57136 758261 991000))
+;;; Generated autoloads from loccur.el
+
+(autoload 'loccur-mode "loccur" "\
+Minor mode for navigating through the file.
+Hides all lines without matches like `occur' does, but without opening
+a new window.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; loccur-autoloads.el ends here
