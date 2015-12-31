0306411451c502f914a74279cadaac27cd0b4769
JasonM23 <jasonm23@gmail.com>
2015-12-30 21:39:42 -0500

    [Removing ox-twbs version: 20151025.1048]

--- elpa/ox-twbs-20151025.1048/ox-twbs-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ox-twbs" "20151025.1048" "Bootstrap compatible HTML Back-End for Org" 'nil :url "https://github.com/marsmining/ox-twbs" :keywords '("org" "html" "publish" "twitter" "bootstrap"))
