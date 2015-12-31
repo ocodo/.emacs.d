57f0f06831b014c62ae4f6b3d620f84d587f052c
JasonM23 <jasonm23@gmail.com>
2015-12-30 21:39:35 -0500

    [Removing ido-ubiquitous version: 20151130.1616]

--- elpa/ido-ubiquitous-20151130.1616/ido-ubiquitous-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ido-ubiquitous" "20151130.1616" "Use ido (nearly) everywhere." '((emacs "24.1") (ido-completing-read+ "3.9") (cl-lib "0.5") (s "0")) :url "https://github.com/DarwinAwardWinner/ido-ubiquitous" :keywords '("convenience" "completion" "ido"))
