31b04e28b00880a0bac836a70bdd8d6a9b504145
JasonM23 <jasonm23@gmail.com>
2015-12-30 21:39:40 -0500

    [Removing markdown-toc version: 20150715.914]

--- elpa/markdown-toc-20150715.914/markdown-toc-autoloads.el
+++
@@ -1,34 +0,0 @@
-;;; markdown-toc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "markdown-toc" "markdown-toc.el" (21937 41043
-;;;;;;  763401 0))
-;;; Generated autoloads from markdown-toc.el
-
-(autoload 'markdown-toc-version "markdown-toc" "\
-Markdown-toc version.
-
-\(fn)" t nil)
-
-(autoload 'markdown-toc-generate-toc "markdown-toc" "\
-Generate a TOC for markdown file at current point.
-Deletes any previous TOC.
-If called interactively with prefix arg REPLACE-TOC-P, replaces previous TOC.
-
-\(fn &optional REPLACE-TOC-P)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("markdown-toc-pkg.el") (21937 41043 776320
-;;;;;;  673000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; markdown-toc-autoloads.el ends here
