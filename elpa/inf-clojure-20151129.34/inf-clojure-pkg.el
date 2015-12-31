bfc330794970c329db0ac6ffa5f43b3ef6604277
JasonM23 <jasonm23@gmail.com>
2015-12-30 21:39:36 -0500

    [Removing inf-clojure version: 20151129.34]

--- elpa/inf-clojure-20151129.34/inf-clojure-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "inf-clojure" "20151129.34" "an inferior-clojure mode" '((emacs "24.1") (clojure-mode "5.0")) :url "http://github.com/clojure-emacs/inf-clojure" :keywords '("processes" "clojure"))
