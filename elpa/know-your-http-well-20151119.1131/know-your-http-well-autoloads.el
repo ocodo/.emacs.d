387a49786057271112847cdc62c9276bbb0ef23a
JasonM23 <jasonm23@gmail.com>
2015-12-30 21:39:38 -0500

    [Removing know-your-http-well version: 20151119.1131]

--- elpa/know-your-http-well-20151119.1131/know-your-http-well-autoloads.el
+++
@@ -1,60 +0,0 @@
-;;; know-your-http-well-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "http-headers" "http-headers.el" (22095 53559
-;;;;;;  225793 295000))
-;;; Generated autoloads from http-headers.el
-
-(autoload 'http-header "http-headers" "\
-Display the meaning of an HTTP header
-
-\(fn HEADER)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "http-methods" "http-methods.el" (22095 53559
-;;;;;;  241793 297000))
-;;; Generated autoloads from http-methods.el
-
-(autoload 'http-method "http-methods" "\
-Display the meaning of an HTTP method
-
-\(fn METHOD)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "http-relations" "http-relations.el" (22095
-;;;;;;  53559 237793 296000))
-;;; Generated autoloads from http-relations.el
-
-(autoload 'http-relation "http-relations" "\
-Display the meaning of an HTTP relation
-
-\(fn RELATION)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "http-status-codes" "http-status-codes.el"
-;;;;;;  (22095 53559 245793 297000))
-;;; Generated autoloads from http-status-codes.el
-
-(autoload 'http-status-code "http-status-codes" "\
-Display the meaning of an HTTP status code or phrase
-
-\(fn STATUS)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("know-your-http-well-pkg.el" "know-your-http-well.el")
-;;;;;;  (22095 53559 265019 100000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; know-your-http-well-autoloads.el ends here
--- elpa/know-your-http-well-20151119.1131/know-your-http-well-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "know-your-http-well" "20151119.1131" "Look up the meaning of HTTP headers, methods, relations, status codes" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
