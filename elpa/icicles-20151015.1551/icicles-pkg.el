62c2db43628696775cacd033c303552893ee7ebd
JasonM23 <jasonm23@gmail.com>
2015-12-30 21:39:32 -0500

    [Removing icicles version: 20151015.1551]

--- elpa/icicles-20151015.1551/icicles-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "icicles" "20151015.1551" "Minibuffer input completion and cycling." 'nil :url "http://www.emacswiki.org/icicles.el" :keywords
-  '("extensions" "help" "abbrev" "local" "minibuffer" "projects" "keys" "apropos" "completion" "matching" "regexp" "command"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
