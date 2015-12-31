912fceee5a947ba606102e7ab0f57930f77019c2
JasonM23 <jasonm23@gmail.com>
2015-12-30 21:39:44 -0500

    [Removing popup version: 20151125.542]

--- elpa/popup-20151125.542/popup-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("popup.el") (22108 42307 728410 907000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; popup-autoloads.el ends here
--- elpa/popup-20151125.542/popup-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "popup" "20151125.542" "Visual Popup User Interface" '((cl-lib "0.5")) :keywords '("lisp"))
