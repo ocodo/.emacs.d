ec1f7ba4f7014724f7658d1c9f1f501530c3ae5c
JasonM23 <jasonm23@gmail.com>
2015-12-30 21:39:38 -0500

    [Removing lispy version: 20151214.237]

--- elpa/lispy-20151214.237/lispy-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "lispy" "20151214.237" "vi-like Paredit"
-  '((emacs "24.1")
-    (ace-window "0.9.0")
-    (iedit "0.97")
-    (swiper "0.4.0")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
