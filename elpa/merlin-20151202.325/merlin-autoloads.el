fe4fff2c3b4b8aeacef1e3da9dc0bb1e01045354
JasonM23 <jasonm23@gmail.com>
2015-12-30 21:39:41 -0500

    [Removing merlin version: 20151202.325]

--- elpa/merlin-20151202.325/merlin-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; merlin-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "merlin" "merlin.el" (22120 11654 472435 836000))
-;;; Generated autoloads from merlin.el
-
-(autoload 'merlin-mode "merlin" "\
-Minor mode for interacting with a merlin process.
-Runs a merlin process in the background and perform queries on it.
-
-Short cuts:
-\\{merlin-mode-map}
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("merlin-ac.el" "merlin-cap.el" "merlin-company.el"
-;;;;;;  "merlin-compat.el" "merlin-iedit.el" "merlin-pkg.el") (22120
-;;;;;;  11654 498127 800000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; merlin-autoloads.el ends here
