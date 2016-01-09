4d04fe3658b3faa039195e3ccdb7610255e8939d
JasonM23 <jasonm23@gmail.com>
2016-01-09 00:40:57 -0500

    [Updating anzu version: 20160104.2255][removing old version: 20151015.405]

--- elpa/anzu-20160104.2255/anzu-autoloads.el
+++ elpa/anzu-20160104.2255/anzu-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "anzu" "anzu.el" (22049 3362 634811 0))
+;;;### (autoloads nil "anzu" "anzu.el" (22160 39071 197794 193000))
 ;;; Generated autoloads from anzu.el
 
 (autoload 'anzu-mode "anzu" "\
