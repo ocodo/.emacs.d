0c1c08a20197ece63b734945de9afee44a65ac06
JasonM23 <jasonm23@gmail.com>
2016-01-09 00:41:33 -0500

    [Updating org-projectile version: 20160101.1550][removing old version: 20150902.2156]

--- elpa/org-projectile-20150902.2156/org-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-projectile" "20150902.2156" "Repository todo management for org-mode" '((projectile "0.11.0") (dash "2.10.0")) :url "https://github.com/IvanMalison/org-projectile" :keywords '("org" "projectile" "todo"))
---
+++ elpa/org-projectile-20160101.1550/org-projectile-pkg.el
@@ -0,0 +1,1 @@
+(define-package "org-projectile" "20160101.1550" "Repository todo management for org-mode" '((projectile "0.11.0") (dash "2.10.0")) :url "https://github.com/IvanMalison/org-projectile" :keywords '("org" "projectile" "todo"))
