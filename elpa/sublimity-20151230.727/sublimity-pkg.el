50cc13b6d1010b49e3b22883260a07bfcd5308e8
JasonM23 <jasonm23@gmail.com>
2016-01-09 00:41:38 -0500

    [Updating sublimity version: 20151230.727][removing old version: 20150918.1217]

---
+++ elpa/sublimity-20151230.727/sublimity-pkg.el
@@ -0,0 +1,4 @@
+(define-package "sublimity" "20151230.727" "smooth-scrolling, minimap and distraction-free mode" 'nil :url "http://hins11.yu-yake.com/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
