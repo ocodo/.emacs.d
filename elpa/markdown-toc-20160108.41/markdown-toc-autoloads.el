75e16ea1b4d0c00d0f14a7b96a714fbdfe75cb69
JasonM23 <jasonm23@gmail.com>
2016-01-09 00:41:29 -0500

    [Updating markdown-toc version: 20160108.41][removing old version: 20151223.314]

--- elpa/markdown-toc-20160108.41/markdown-toc-autoloads.el
+++ elpa/markdown-toc-20160108.41/markdown-toc-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "markdown-toc" "markdown-toc.el" (22145 57126
-;;;;;;  870248 98000))
+;;;### (autoloads nil "markdown-toc" "markdown-toc.el" (22160 38842
+;;;;;;  196669 632000))
 ;;; Generated autoloads from markdown-toc.el
 
 (autoload 'markdown-toc-version "markdown-toc" "\
@@ -21,8 +21,8 @@ If called interactively with prefix arg REPLACE-TOC-P, replaces previous TOC.
 
 ;;;***
 
-;;;### (autoloads nil nil ("markdown-toc-pkg.el") (22145 57126 881411
-;;;;;;  995000))
+;;;### (autoloads nil nil ("markdown-toc-pkg.el") (22160 38842 205455
+;;;;;;  519000))
 
 ;;;***
 
