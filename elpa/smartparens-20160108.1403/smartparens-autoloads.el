d483c878db7dc94327492dde66c24b296889cd83
JasonM23 <jasonm23@gmail.com>
2016-01-09 00:41:38 -0500

    [Updating smartparens version: 20160108.1403][removing old version: 20151226.1556]

--- elpa/smartparens-20160108.1403/smartparens-autoloads.el
+++ elpa/smartparens-20160108.1403/smartparens-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "smartparens" "smartparens.el" (22145 57065
-;;;;;;  466158 744000))
+;;;### (autoloads nil "smartparens" "smartparens.el" (22160 38770
+;;;;;;  652223 698000))
 ;;; Generated autoloads from smartparens.el
 
 (autoload 'sp-cheat-sheet "smartparens" "\
@@ -182,7 +182,7 @@ Turn off `show-smartparens-mode'.
 ;;;;;;  "smartparens-html.el" "smartparens-latex.el" "smartparens-lua.el"
 ;;;;;;  "smartparens-pkg.el" "smartparens-python.el" "smartparens-racket.el"
 ;;;;;;  "smartparens-ruby.el" "smartparens-rust.el" "smartparens-scala.el")
-;;;;;;  (22145 57065 507521 333000))
+;;;;;;  (22160 38770 687586 867000))
 
 ;;;***
 
--- elpa/smartparens-20160108.1403/smartparens.el
+++ elpa/smartparens-20160108.1403/smartparens.el
@@ -1,6 +1,6 @@
 ;;; smartparens.el --- Automatic insertion, wrapping and paredit-like navigation with user defined pairs.
 
-;; Copyright (C) 2012-2015 Matus Goljer
+;; Copyright (C) 2012-2016 Matus Goljer
 
 ;; Author: Matus Goljer <matus.goljer@gmail.com>
 ;; Maintainer: Matus Goljer <matus.goljer@gmail.com>
@@ -3761,11 +3761,12 @@ The expressions considered are those delimited by pairs on
                                      (let ((in-comment (sp-point-in-comment))
                                            (in-string (sp-point-in-string)))
                                        (save-excursion
-                                         (backward-char)
-                                         (cond
-                                          (in-comment (and in-comment (sp-point-in-comment)))
-                                          ((and (not in-comment) (sp-point-in-comment)) t)
-                                          ((or in-comment in-string)))))
+                                         (unless (= (point) (point-min))
+                                           (backward-char)
+                                           (cond
+                                            (in-comment (and in-comment (sp-point-in-comment)))
+                                            ((and (not in-comment) (sp-point-in-comment)) t)
+                                            ((or in-comment in-string))))))
                                    (sp-point-in-string-or-comment)))
            (string-bounds (and in-string-or-comment (sp--get-string-or-comment-bounds)))
            (fw-bound (if in-string-or-comment (cdr string-bounds) (point-max)))
@@ -3775,12 +3776,27 @@ The expressions considered are those delimited by pairs on
       (while (and (not done)
                   (sp--search-and-save-match
                    search-fn
-                   (sp--get-allowed-regexp)
+                   ;; #556 The regexp we use here might exclude or
+                   ;; include extra pairs in case the next match is in
+                   ;; a different context.  There's no way to know
+                   ;; beforehand where we land, so we need to consider
+                   ;; *all* pairs in the search and then re-check with
+                   ;; a regexp based on the context of the found pair
+                   (sp--get-allowed-regexp
+                    ;; use all the pairs!
+                    (sp--get-pair-list))
                    (if back bw-bound fw-bound)
                    r mb me ms))
         ;; search for the first opening pair.  Here, only consider tags
         ;; that are allowed in the current context.
-        (unless (sp--skip-match-p ms mb me :global-skip global-skip-fn)
+        (unless (or (not (save-excursion
+                           (if back
+                               (progn
+                                 (goto-char me)
+                                 (sp--looking-back-p (sp--get-allowed-regexp)))
+                             (goto-char mb)
+                             (sp--looking-at-p (sp--get-allowed-regexp)))))
+                    (sp--skip-match-p ms mb me :global-skip global-skip-fn))
           ;; if the point originally wasn't inside of a string or comment
           ;; but now is, jump out of the string/comment and only search
           ;; the code.  This ensures that the comments and strings are
@@ -5995,6 +6011,9 @@ If ARG is raw prefix \\[universal-argument], extend all the way to the end of th
 
 If both the current expression and the expression to be slurped
 are strings, they are joined together.
+
+See also `sp-slurp-hybrid-sexp' which is similar but handles
+C-style syntax better.
 
 Examples:
 
