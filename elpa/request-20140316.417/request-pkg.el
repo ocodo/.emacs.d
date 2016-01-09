fb29901c4f97dd88f7115b3c3f8f0e22f07505f5
JasonM23 <jasonm23@gmail.com>
2016-01-09 00:41:35 -0500

    [Updating request version: 20160108.33][removing old version: 20140316.417]

--- elpa/request-20140316.417/request-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "request" "20140316.417" "Compatible layer for URL request in Emacs" (quote nil))
--- elpa/request-20160108.33/request-autoloads.el
+++ elpa/request-20160108.33/request-autoloads.el
@@ -1,18 +1,15 @@
 ;;; request-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("request-pkg.el" "request.el") (21286
-;;;;;;  24834 295290 0))
+;;;### (autoloads nil nil ("request.el") (22160 38784 815844 379000))
 
 ;;;***
 
-(provide 'request-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; request-autoloads.el ends here
