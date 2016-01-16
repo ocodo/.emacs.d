8ad3a1835b6d90fc4658ebf628397f416cb57489
JasonM23 <jasonm23@gmail.com>
2016-01-15 21:08:39 -0500

    [Updating buttercup version: 20160109.13][removing old version: 20151223.606]

--- elpa/buttercup-20151223.606/buttercup-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "buttercup" "20151223.606" "Behavior-Driven Emacs Lisp Testing" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/buttercup-20160109.13/buttercup-autoloads.el
+++ elpa/buttercup-20160109.13/buttercup-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "buttercup" "buttercup.el" (22145 57654 146854
-;;;;;;  441000))
+;;;### (autoloads nil "buttercup" "buttercup.el" (22168 62007 335531
+;;;;;;  764000))
 ;;; Generated autoloads from buttercup.el
 
 (autoload 'buttercup-run-at-point "buttercup" "\
@@ -28,7 +28,7 @@ current directory.
 ;;;***
 
 ;;;### (autoloads nil nil ("buttercup-compat.el" "buttercup-pkg.el")
-;;;;;;  (22145 57654 170245 78000))
+;;;;;;  (22168 62007 355082 93000))
 
 ;;;***
 
