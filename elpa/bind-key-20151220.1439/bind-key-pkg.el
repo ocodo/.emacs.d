6eac31c735dfe038e558b00d9d32a0fd2f7ea4bc
JasonM23 <jasonm23@gmail.com>
2016-01-15 21:08:39 -0500

    [Updating bind-key version: 20160112.758][removing old version: 20151220.1439]

--- elpa/bind-key-20151220.1439/bind-key-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bind-key" "20151220.1439" "A simple way to manage personal keybindings" 'nil :url "https://github.com/jwiegley/use-package" :keywords '("keys" "keybinding" "config" "dotemacs"))
