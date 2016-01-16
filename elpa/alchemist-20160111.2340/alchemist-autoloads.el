ee200f4e6b6fbd753556248ad57ef42dfbf0d39a
JasonM23 <jasonm23@gmail.com>
2016-01-15 21:08:37 -0500

    [Updating alchemist version: 20160111.2340][removing old version: 20160106.505]

--- elpa/alchemist-20160111.2340/alchemist-autoloads.el
+++ elpa/alchemist-20160111.2340/alchemist-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "alchemist" "alchemist.el" (22160 39077 97818
-;;;;;;  181000))
+;;;### (autoloads nil "alchemist" "alchemist.el" (22168 62014 151534
+;;;;;;  621000))
 ;;; Generated autoloads from alchemist.el
 
 (autoload 'alchemist-mode "alchemist" "\
@@ -17,8 +17,8 @@ Key bindings:
 
 ;;;***
 
-;;;### (autoloads nil "alchemist-iex" "alchemist-iex.el" (22160 39077
-;;;;;;  149818 392000))
+;;;### (autoloads nil "alchemist-iex" "alchemist-iex.el" (22168 62014
+;;;;;;  211534 647000))
 ;;; Generated autoloads from alchemist-iex.el
 
 (defalias 'run-elixir 'alchemist-iex-run)
@@ -39,11 +39,11 @@ Show the IEx buffer if an IEx process is already run.
 ;;;***
 
 ;;;### (autoloads nil "alchemist-phoenix" "alchemist-phoenix.el"
-;;;;;;  (22160 39077 137818 343000))
+;;;;;;  (22168 62014 203534 643000))
 ;;; Generated autoloads from alchemist-phoenix.el
 
 (autoload 'alchemist-phoenix-project-p "alchemist-phoenix" "\
-Return non-nil if `default- is inside an Phoenix project.
+Return non-nil if `default-directory' is inside an Phoenix project.
 
 \(fn)" nil nil)
 
@@ -66,7 +66,7 @@ The following commands are available:
 ;;;***
 
 ;;;### (autoloads nil "alchemist-refcard" "alchemist-refcard.el"
-;;;;;;  (22160 39077 105818 214000))
+;;;;;;  (22168 62014 163534 627000))
 ;;; Generated autoloads from alchemist-refcard.el
 
 (autoload 'alchemist-refcard "alchemist-refcard" "\
@@ -77,7 +77,7 @@ Generate an Alchemist refcard of all the features.
 ;;;***
 
 ;;;### (autoloads nil "alchemist-test-mode" "alchemist-test-mode.el"
-;;;;;;  (22160 39077 157818 424000))
+;;;;;;  (22168 62014 227534 653000))
 ;;; Generated autoloads from alchemist-test-mode.el
 
 (autoload 'alchemist-test-mode "alchemist-test-mode" "\
@@ -105,7 +105,7 @@ The following commands are available:
 ;;;;;;  "alchemist-key.el" "alchemist-macroexpand.el" "alchemist-message.el"
 ;;;;;;  "alchemist-mix.el" "alchemist-pkg.el" "alchemist-project.el"
 ;;;;;;  "alchemist-report.el" "alchemist-scope.el" "alchemist-server.el"
-;;;;;;  "alchemist-utils.el") (22160 39077 166240 443000))
+;;;;;;  "alchemist-utils.el") (22168 62014 240623 341000))
 
 ;;;***
 
--- elpa/alchemist-20160111.2340/alchemist-pkg.el
+++ elpa/alchemist-20160111.2340/alchemist-pkg.el
@@ -1,4 +1,4 @@
-(define-package "alchemist" "20160106.505" "Elixir tooling integration into Emacs"
+(define-package "alchemist" "20160111.2340" "Elixir tooling integration into Emacs"
   '((elixir-mode "2.2.5")
     (dash "2.11.0")
     (emacs "24.4")
