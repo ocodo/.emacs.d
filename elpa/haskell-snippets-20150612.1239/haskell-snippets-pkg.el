27694867167e912c4d34b2b60a2ff1e24a982eec
Jason Milkins <jasonm23@gmail.com>
2016-01-21 13:01:12 +0800

    [Adding haskell-snippets version: 20150612.1239]

---
+++ elpa/haskell-snippets-20150612.1239/haskell-snippets-pkg.el
@@ -0,0 +1,7 @@
+(define-package "haskell-snippets" "20150612.1239" "Yasnippets for Haskell"
+  '((yasnippet "0.8.0"))
+  :url "https://github.com/haskell/haskell-snippets" :keywords
+  '("snippets" "haskell"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
