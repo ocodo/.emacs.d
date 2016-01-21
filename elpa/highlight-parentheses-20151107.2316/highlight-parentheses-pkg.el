21381a620dadefd1e6364f75afd0f5872878e948
Jason Milkins <jasonm23@gmail.com>
2016-01-21 13:01:14 +0800

    [Adding highlight-parentheses version: 20151107.2316]

---
+++ elpa/highlight-parentheses-20151107.2316/highlight-parentheses-pkg.el
@@ -0,0 +1,1 @@
+(define-package "highlight-parentheses" "20151107.2316" "highlight surrounding parentheses" 'nil :url "https://github.com/tsdh/highlight-parentheses.el" :keywords '("faces" "matching"))
