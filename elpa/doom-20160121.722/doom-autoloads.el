589792458b0c6c5810cd51136f487d88235f2e52
JasonM23 <jasonm23@gmail.com>
2016-01-23 03:05:43 -0500

    [Adding doom version: 20160121.722]

---
+++ elpa/doom-20160121.722/doom-autoloads.el
@@ -0,0 +1,15 @@
+;;; doom-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("doom.el") (22179 12983 971333 34000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; doom-autoloads.el ends here
