0d96bbdd3b77b9c23e3b31fcb85558d73d5ff1d1
JasonM23 <jasonm23@gmail.com>
2016-02-08 07:17:20 -0500

    [Updating markdown-toc version: 20160207.858][removing old version: 20160108.41]

--- elpa/markdown-toc-20160207.858/markdown-toc-pkg.el
+++ elpa/markdown-toc-20160207.858/markdown-toc-pkg.el
@@ -1,4 +1,4 @@
-(define-package "markdown-toc" "20160108.41" "A simple TOC generator for markdown file"
+(define-package "markdown-toc" "20160207.858" "A simple TOC generator for markdown file"
   '((s "1.9.0")
     (dash "2.11.0")
     (markdown-mode "2.0")))
