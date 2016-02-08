f0f35d6557c51eeb1fa052eeb39d95069cc7044a
JasonM23 <jasonm23@gmail.com>
2016-02-08 07:17:43 -0500

    [Updating zenburn-theme version: 20160204.1216][removing old version: 20160117.1119]

--- elpa/zenburn-theme-20160117.1119/zenburn-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "zenburn-theme" "20160117.1119" "A low contrast color theme for Emacs." 'nil :url "http://github.com/bbatsov/zenburn-emacs")
---
+++ elpa/zenburn-theme-20160204.1216/zenburn-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "zenburn-theme" "20160204.1216" "A low contrast color theme for Emacs." 'nil :url "http://github.com/bbatsov/zenburn-emacs")
