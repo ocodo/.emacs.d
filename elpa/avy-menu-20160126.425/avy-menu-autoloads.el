83871892e56557177d078fac4e0c715d1c270f83
JasonM23 <jasonm23@gmail.com>
2016-02-08 07:17:43 -0500

    [Adding avy-menu version: 20160126.425]

---
+++ elpa/avy-menu-20160126.425/avy-menu-autoloads.el
@@ -0,0 +1,37 @@
+;;; avy-menu-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "avy-menu" "avy-menu.el" (22200 34272 452459
+;;;;;;  567000))
+;;; Generated autoloads from avy-menu.el
+
+(autoload 'avy-menu "avy-menu" "\
+Show popup menu in a temporary window and return user's selection.
+
+BUFFER-OR-NAME specifies name of the buffer (or buffer itself)
+that hosts menu options.  MENU itself should be a list of the
+form (TITLE PANE1 PANE2 …), where each pane is a list of
+form (TITLE ITEM1 ITEM2 …).  Each item is normally a cons
+cell (STRING . VALUE), but a string can appear as an item — that
+makes a non-selectable item in the menu.  Also, empty strings
+start new sub-section.
+
+If SHOW-PANE-HEADER is not NIL, show pane headers (titles),
+otherwise hide them.
+
+Returned value is VALUE if user has selected something and NIL if
+he has cancelled the whole menu or pressed key that does not
+correspond to any available option.
+
+\(fn BUFFER-OR-NAME MENU &optional SHOW-PANE-HEADER)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; avy-menu-autoloads.el ends here
---
+++ elpa/avy-menu-20160126.425/avy-menu-pkg.el
@@ -0,0 +1,1 @@
+(define-package "avy-menu" "20160126.425" "Library providing avy-powered popup menu" '((emacs "24.3") (avy "0.3.0")) :url "https://github.com/mrkkrp/avy-menu" :keywords '("popup" "menu"))
