d7505f87c36d971af73866d9189d98640451ffbe
JasonM23 <jasonm23@gmail.com>
2016-02-08 07:17:07 -0500

    [Updating fancy-narrow version: 20160124.403][removing old version: 20151021.333]

--- elpa/fancy-narrow-20151021.333/fancy-narrow-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "fancy-narrow" "20151021.333" "narrow-to-region with more eye candy." 'nil :url "http://github.com/Bruce-Connor/fancy-narrow" :keywords '("faces" "convenience"))
---
+++ elpa/fancy-narrow-20160124.403/fancy-narrow-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fancy-narrow" "20160124.403" "narrow-to-region with more eye candy." 'nil :url "http://github.com/Bruce-Connor/fancy-narrow" :keywords '("faces" "convenience"))
