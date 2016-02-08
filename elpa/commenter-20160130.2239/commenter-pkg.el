73f83a5521dec218d3b6735e44241450f5cdffb8
JasonM23 <jasonm23@gmail.com>
2016-02-08 07:17:44 -0500

    [Adding commenter version: 20160130.2239]

---
+++ elpa/commenter-20160130.2239/commenter-pkg.el
@@ -0,0 +1,1 @@
+(define-package "commenter" "20160130.2239" "multiline-comment support package" '((emacs "24.4") (let-alist "1.0.1")) :url "https://github.com/yuutayamada/commenter" :keywords '("comment"))
