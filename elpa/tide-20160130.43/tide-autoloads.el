9e85c01657c350ee882414b76a91b578e818ecaf
JasonM23 <jasonm23@gmail.com>
2016-02-08 07:17:29 -0500

    [Updating tide version: 20160130.43][removing old version: 20151201.45]

--- elpa/tide-20160130.43/tide-autoloads.el
+++ elpa/tide-20160130.43/tide-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "tide" "tide.el" (22120 11612 987966 199000))
+;;;### (autoloads nil "tide" "tide.el" (22200 34334 232682 801000))
 ;;; Generated autoloads from tide.el
 
 (autoload 'company-tide "tide" "\
@@ -25,7 +25,7 @@ Minor mode for Typescript Interactive Development Environment.
 
 ;;;***
 
-;;;### (autoloads nil nil ("tide-pkg.el") (22120 11613 2881 354000))
+;;;### (autoloads nil nil ("tide-pkg.el") (22200 34334 240069 10000))
 
 ;;;***
 
