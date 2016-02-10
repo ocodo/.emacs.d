424f2ee6f0166ffb08aaa8f89372f9c155077d2d
Jason Milkins <jasonm23@gmail.com>
2016-02-10 21:39:58 +0800

    Remove extended function key bindings and replace f16 with select-themes

--- custom/custom-mac-fn-keys.el
+++ custom/custom-mac-fn-keys.el
@@ -9,10 +9,7 @@
   (global-set-key (kbd "<f13>") 'overwrite-mode )
   (global-set-key (kbd "<f14>") 'ibuffer )
   (global-set-key (kbd "<f15>") 'magit-status )
-  (global-set-key (kbd "<f16>") 'load-theme )
-  (global-set-key (kbd "<f17>") 'disable-theme )
-  (global-set-key (kbd "<f18>") 'smartparens-global-mode )
-  (global-set-key (kbd "<f19>") 'package-list-packages )
+  (global-set-key (kbd "<f16>") 'select-themes )
 
   )
 
