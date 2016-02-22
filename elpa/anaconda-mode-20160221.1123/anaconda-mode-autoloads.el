199222eb46be6149d1433d88e8221c5d6c4c6b7b
JasonM23 <jasonm23@gmail.com>
2016-02-21 20:50:53 -0500

    [Updating anaconda-mode version: 20160221.1123][removing old version: 20150922.537]

--- elpa/anaconda-mode-20160221.1123/anaconda-mode-autoloads.el
+++ elpa/anaconda-mode-20160221.1123/anaconda-mode-autoloads.el
@@ -3,14 +3,19 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "anaconda-mode" "anaconda-mode.el" (22021 61743
-;;;;;;  329140 0))
+;;;### (autoloads nil "anaconda-mode" "anaconda-mode.el" (22218 26533
+;;;;;;  742365 70000))
 ;;; Generated autoloads from anaconda-mode.el
 
 (autoload 'anaconda-mode "anaconda-mode" "\
 Code navigation, documentation lookup and completion for Python.
 
 \\{anaconda-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'anaconda-eldoc-mode "anaconda-mode" "\
+Toggle echo area display of Python objects at point.
 
 \(fn &optional ARG)" t nil)
 
