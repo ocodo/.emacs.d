08aee10575cdcad71a80e1cdd4035907ac185851
JasonM23 <jasonm23@gmail.com>
2016-02-21 20:51:43 -0500

    [Updating ycmd version: 20160214.2344][removing old version: 20160205.2350]

--- elpa/ycmd-20160214.2344/ycmd-pkg.el
+++ elpa/ycmd-20160214.2344/ycmd-pkg.el
@@ -1,9 +1,10 @@
-(define-package "ycmd" "20160205.2350" "emacs bindings to the ycmd completion server"
+(define-package "ycmd" "20160214.2344" "emacs bindings to the ycmd completion server"
   '((emacs "24")
     (f "0.17.1")
     (dash "1.2.0")
     (deferred "0.3.2")
-    (popup "0.5.0"))
+    (popup "0.5.0")
+    (cl-lib "0.5"))
   :url "https://github.com/abingham/emacs-ycmd")
 ;; Local Variables:
 ;; no-byte-compile: t
