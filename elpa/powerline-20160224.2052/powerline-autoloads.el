e29e80c12511d2cfd3215256e16147281637dcff
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:45:07 -0400

    [Updating powerline version: 20160224.2052][removing old version: 20151008.1449]

--- elpa/powerline-20160224.2052/powerline-autoloads.el
+++ elpa/powerline-20160224.2052/powerline-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "powerline" "powerline.el" (22045 47879 73140
-;;;;;;  0))
+;;;### (autoloads nil "powerline" "powerline.el" (22246 12184 866565
+;;;;;;  677000))
 ;;; Generated autoloads from powerline.el
 
 (autoload 'powerline-hud "powerline" "\
@@ -47,8 +47,8 @@ Return empty space using FACE and leaving RESERVE space on the right.
 
 ;;;***
 
-;;;### (autoloads nil "powerline-themes" "powerline-themes.el" (22045
-;;;;;;  47879 61140 0))
+;;;### (autoloads nil "powerline-themes" "powerline-themes.el" (22246
+;;;;;;  12184 846565 694000))
 ;;; Generated autoloads from powerline-themes.el
 
 (autoload 'powerline-default-theme "powerline-themes" "\
@@ -74,7 +74,7 @@ Setup a nano-like mode-line.
 ;;;***
 
 ;;;### (autoloads nil nil ("powerline-pkg.el" "powerline-separators.el")
-;;;;;;  (22045 47879 82880 79000))
+;;;;;;  (22246 12184 877006 956000))
 
 ;;;***
 
--- elpa/powerline-20160224.2052/powerline-pkg.el
+++ elpa/powerline-20160224.2052/powerline-pkg.el
@@ -1,4 +1,4 @@
-(define-package "powerline" "20151008.1449" "Rewrite of Powerline"
+(define-package "powerline" "20160224.2052" "Rewrite of Powerline"
   '((cl-lib "0.2"))
   :url "http://github.com/milkypostman/powerline/" :keywords
   '("mode-line"))
