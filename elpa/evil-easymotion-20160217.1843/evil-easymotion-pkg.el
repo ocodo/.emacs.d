8066cdc35218b3968aa4e9a9f64f70431056bf47
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:44:32 -0400

    [Updating evil-easymotion version: 20160310.1922][removing old version: 20160217.1843]

--- elpa/evil-easymotion-20160217.1843/evil-easymotion-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-easymotion" "20160217.1843" "A port of vim's easymotion to emacs" '((emacs "24") (avy "0.3.0")) :url "https://github.com/pythonnut/evil-easymotion" :keywords '("convenience" "evil"))
--- elpa/evil-easymotion-20160310.1922/evil-easymotion-autoloads.el
+++ elpa/evil-easymotion-20160310.1922/evil-easymotion-autoloads.el
@@ -3,14 +3,9 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "evil-easymotion" "evil-easymotion.el" (22218
-;;;;;;  26467 42524 765000))
+;;;### (autoloads nil "evil-easymotion" "evil-easymotion.el" (22246
+;;;;;;  12438 281707 13000))
 ;;; Generated autoloads from evil-easymotion.el
-
-(autoload 'evilem-define "evil-easymotion" "\
-Automatically create and bind an evil motion
-
-\(fn KEY MOTION &optional PRE-HOOK POST-HOOK VARS)" nil t)
 
 (autoload 'evilem-default-keybindings "evil-easymotion" "\
 Define easymotions for all motions evil defines by default
