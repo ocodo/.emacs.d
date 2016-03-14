c836ae5ba9c2d43adedcdf525048b13726b0565c
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:45:40 -0400

    [Adding zenity-color-picker version: 20160302.354]

---
+++ elpa/zenity-color-picker-20160302.354/zenity-color-picker-pkg.el
@@ -0,0 +1,1 @@
+(define-package "zenity-color-picker" "20160302.354" "Insert and adjust colors using Zenity" '((emacs "24.4")) :url "https://bitbucket.org/Soft/zenity-color-picker.el" :keywords '("colors"))
