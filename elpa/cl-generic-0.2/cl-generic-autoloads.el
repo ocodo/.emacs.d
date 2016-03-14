537b64925c537cb3cbfcb8123f274538c866c98a
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:45:37 -0400

    [Adding cl-generic version: 0.2]

---
+++ elpa/cl-generic-0.2/cl-generic-autoloads.el
@@ -0,0 +1,20 @@
+;;; cl-generic-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "cl-generic" "cl-generic.el" (22246 12152 382592
+;;;;;;  157000))
+;;; Generated autoloads from cl-generic.el
+ (let ((d (file-name-directory #$)))
+  (when (member d load-path)
+    (setq load-path (append (remove d load-path) (list d)))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; cl-generic-autoloads.el ends here
