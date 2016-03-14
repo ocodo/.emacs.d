510fe8d8e96a95c96b4fc6c0bbbd0b67b1683287
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:45:14 -0400

    [Updating tao-theme version: 20160221.2103][removing old version: 20151217.840]

--- elpa/tao-theme-20151217.840/tao-theme-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "tao-theme" "20151217.840" "Tao, light & dark themes for Emacs with greyscale palettes generated from the golden mean." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/tao-theme-20160221.2103/tao-theme-autoloads.el
+++ elpa/tao-theme-20160221.2103/tao-theme-autoloads.el
@@ -3,16 +3,16 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "tao-yang-theme" "tao-yang-theme.el" (22145
-;;;;;;  57058 670148 510000))
+;;;### (autoloads nil "tao-yang-theme" "tao-yang-theme.el" (22246
+;;;;;;  12156 822588 581000))
 ;;; Generated autoloads from tao-yang-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
 
 ;;;***
 
-;;;### (autoloads nil "tao-yin-theme" "tao-yin-theme.el" (22145 57058
-;;;;;;  690148 541000))
+;;;### (autoloads nil "tao-yin-theme" "tao-yin-theme.el" (22246 12156
+;;;;;;  842588 565000))
 ;;; Generated autoloads from tao-yin-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -20,7 +20,7 @@
 ;;;***
 
 ;;;### (autoloads nil nil ("tao-theme-pkg.el" "tao-yinyang-common.el")
-;;;;;;  (22145 57058 701184 66000))
+;;;;;;  (22246 12156 853601 394000))
 
 ;;;***
 
