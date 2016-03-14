0623652b22c421a641bd6aeddbf04c26dc74e430
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:45:37 -0400

    [Updating zenburn-theme version: 20160302.58][removing old version: 20160204.1216]

--- elpa/zenburn-theme-20160204.1216/zenburn-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "zenburn-theme" "20160204.1216" "A low contrast color theme for Emacs." 'nil :url "http://github.com/bbatsov/zenburn-emacs")
--- elpa/zenburn-theme-20160302.58/zenburn-theme-autoloads.el
+++ elpa/zenburn-theme-20160302.58/zenburn-theme-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (22200 34295
-;;;;;;  900544 622000))
+;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (22246 12085
+;;;;;;  666643 984000))
 ;;; Generated autoloads from zenburn-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
