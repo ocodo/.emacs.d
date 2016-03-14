3b50a8e47d5af4424cf4c77cbf94f721e0c406b3
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:45:38 -0400

    [Adding harvest version: 20160304.1100]

---
+++ elpa/harvest-20160304.1100/harvest-pkg.el
@@ -0,0 +1,1 @@
+(define-package "harvest" "20160304.1100" "Harvest integration" '((swiper "0.7.0") (hydra "0.13.0") (s "1.11.0")) :url "https://github.com/kostajh/harvest.el" :keywords '("harvest"))
