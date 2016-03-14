fbf75b3b80c42b3851d7539ca1ed506831546618
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:45:38 -0400

    [Adding grails version: 20160313.445]

---
+++ elpa/grails-20160313.445/grails-autoloads.el
@@ -0,0 +1,26 @@
+;;; grails-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "grails" "grails.el" (22246 12071 902654 215000))
+;;; Generated autoloads from grails.el
+
+(autoload 'grails "grails" "\
+Grails minor mode.
+     With no argument, this command toggles the mode.
+     Non-null prefix argument turns on the mode.
+     Null prefix argument turns off the mode.
+     When Grails minor mode is enabled you have some
+     shortcut to fast navigate a Grails project.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; grails-autoloads.el ends here
---
+++ elpa/grails-20160313.445/grails-pkg.el
@@ -0,0 +1,1 @@
+(define-package "grails" "20160313.445" "Minor mode for Grails projects" '((emacs "24")) :url "https://github.com/lifeisfoo/emacs-grails")
