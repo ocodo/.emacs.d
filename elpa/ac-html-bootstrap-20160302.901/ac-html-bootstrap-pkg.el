4bc20c488c8b322655ab7e6562e5567bcb185e98
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:44:12 -0400

    [Updating ac-html-bootstrap version: 20160302.901][removing old version: 20151222.1312]

---
+++ elpa/ac-html-bootstrap-20160302.901/ac-html-bootstrap-pkg.el
@@ -0,0 +1,7 @@
+(define-package "ac-html-bootstrap" "20160302.901" "auto complete bootstrap3/fontawesome classes for `ac-html' and `company-web'"
+  '((web-completion-data "0.1"))
+  :url "https://github.com/osv/ac-html-bootstrap" :keywords
+  '("html" "auto-complete" "bootstrap" "cssx"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
