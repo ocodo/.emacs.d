23d993b63a56940e16bd9277b2c9219aacfd2829
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:44:55 -0400

    [Updating js2-mode version: 20160305.452][removing old version: 20160124.1132]

--- elpa/js2-mode-20160305.452/js2-mode-pkg.el
+++ elpa/js2-mode-20160305.452/js2-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "js2-mode" "20160124.1132" "Improved JavaScript editing mode"
+(define-package "js2-mode" "20160305.452" "Improved JavaScript editing mode"
   '((emacs "24.1")
     (cl-lib "0.5"))
   :url "https://github.com/mooz/js2-mode/" :keywords
