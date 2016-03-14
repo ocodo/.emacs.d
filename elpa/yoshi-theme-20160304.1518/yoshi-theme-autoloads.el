9d84a23f1274354125b543608a798b58d89492e7
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:45:40 -0400

    [Adding yoshi-theme version: 20160304.1518]

---
+++ elpa/yoshi-theme-20160304.1518/yoshi-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; yoshi-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "yoshi-theme" "yoshi-theme.el" (22246 12063
+;;;;;;  114660 662000))
+;;; Generated autoloads from yoshi-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; yoshi-theme-autoloads.el ends here
---
+++ elpa/yoshi-theme-20160304.1518/yoshi-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "yoshi-theme" "20160304.1518" "Theme named after my cat" 'nil :url "http://projects.ryuslash.org/yoshi-theme/" :keywords '("faces"))
