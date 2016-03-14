18e5eb866e0ee0c5bd4056d5baca61d926be2a65
JasonM23 <jasonm23@gmail.com>
2016-03-14 02:45:39 -0400

    [Adding sed-mode version: 1.0]

---
+++ elpa/sed-mode-1.0/sed-mode-autoloads.el
@@ -0,0 +1,24 @@
+;;; sed-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "sed-mode" "sed-mode.el" (22246 12066 166658
+;;;;;;  430000))
+;;; Generated autoloads from sed-mode.el
+ (add-to-list 'auto-mode-alist '("\\.sed\\'" . sed-mode))
+ (add-to-list 'interpreter-mode-alist '("sed" . sed-mode))
+
+(autoload 'sed-mode "sed-mode" "\
+Sed editing mode.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sed-mode-autoloads.el ends here
---
+++ elpa/sed-mode-1.0/sed-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "sed-mode" "1.0" "Major mode to edit sed scripts" 'nil :url "http://elpa.gnu.org/packages/sed-mode.html" :keywords nil)
