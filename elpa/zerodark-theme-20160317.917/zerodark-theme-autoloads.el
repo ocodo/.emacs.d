e04474da2272fd59aa88f665192dc10b19aa4d4d
JasonM23 <jasonm23@gmail.com>
2016-03-19 05:24:23 -0400

    [Adding zerodark-theme version: 20160317.917]

---
+++ elpa/zerodark-theme-20160317.917/zerodark-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; zerodark-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "zerodark-theme" "zerodark-theme.el" (22253
+;;;;;;  3163 433949 721000))
+;;; Generated autoloads from zerodark-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; zerodark-theme-autoloads.el ends here
