a86a0f36660178cc6446883829c0fb7eb8731c70
JasonM23 <jasonm23@gmail.com>
2016-03-19 05:24:18 -0400

    [Updating point-stack version: 20141225.2110][removing old version: 20140102.1223]

--- elpa/point-stack-20140102.1223/point-stack-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "point-stack" "20140102.1223" "A forward/back stack for point" (quote nil))
---
+++ elpa/point-stack-20141225.2110/point-stack-pkg.el
@@ -0,0 +1,1 @@
+(define-package "point-stack" "20141225.2110" "Back and forward navigation through buffer locations" 'nil :stars '(#("6" 0 1 (font-lock-face paradox-star-face))))
