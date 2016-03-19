a463caa201d0bac7a78d5640fa9da1cd4087bb58
JasonM23 <jasonm23@gmail.com>
2016-03-19 05:24:20 -0400

    [Adding transmission version: 20160314.1231]

---
+++ elpa/transmission-20160314.1231/transmission-autoloads.el
@@ -0,0 +1,28 @@
+;;; transmission-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "transmission" "transmission.el" (22253 3201
+;;;;;;  173861 237000))
+;;; Generated autoloads from transmission.el
+
+(autoload 'transmission-add "transmission" "\
+Add TORRENT by filename, URL, magnet link, or info hash.
+When called with a prefix, prompt for DIRECTORY.
+
+\(fn TORRENT &optional DIRECTORY)" t nil)
+
+(autoload 'transmission "transmission" "\
+Open a `transmission-mode' buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; transmission-autoloads.el ends here
