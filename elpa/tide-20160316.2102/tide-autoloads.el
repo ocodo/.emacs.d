8e8a304818812d511472a1218d9e31baa0843f59
JasonM23 <jasonm23@gmail.com>
2016-03-19 05:24:20 -0400

    [Adding tide version: 20160316.2102]

---
+++ elpa/tide-20160316.2102/tide-autoloads.el
@@ -0,0 +1,37 @@
+;;; tide-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "tide" "tide.el" (22253 3208 725843 959000))
+;;; Generated autoloads from tide.el
+
+(autoload 'company-tide "tide" "\
+
+
+\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
+
+(autoload 'tide-setup "tide" "\
+Setup `tide-mode' in current buffer.
+
+\(fn)" t nil)
+
+(autoload 'tide-mode "tide" "\
+Minor mode for Typescript Interactive Development Environment.
+
+\\{tide-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("tide-pkg.el") (22253 3208 731330 201000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; tide-autoloads.el ends here
