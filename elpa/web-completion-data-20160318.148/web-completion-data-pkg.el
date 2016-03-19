dbebe83efc845a04a97281c871843abcdfbc1998
JasonM23 <jasonm23@gmail.com>
2016-03-19 05:24:21 -0400

    [Adding web-completion-data version: 20160318.148]

---
+++ elpa/web-completion-data-20160318.148/web-completion-data-pkg.el
@@ -0,0 +1,5 @@
+(define-package "web-completion-data" "20160318.148" "Shared completion data for ac-html and company-web" 'nil :url "https://github.com/osv/web-completion-data" :keywords
+  '("html" "auto-complete" "company"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
