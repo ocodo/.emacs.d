5a9934115aef3ccb425954db2992944d25ecd05b
JasonM23 <jasonm23@gmail.com>
2016-04-01 22:12:39 -0400

    [Updating ag version: 20160321.1606][removing old version: 20160227.1022]

--- elpa/ag-20160321.1606/ag-autoloads.el
+++ elpa/ag-20160321.1606/ag-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "ag" "ag.el" (22246 12532 77334 126000))
+;;;### (autoloads nil "ag" "ag.el" (22271 10683 299402 137000))
 ;;; Generated autoloads from ag.el
 
 (autoload 'ag "ag" "\
