0664351842f00d6dba032dc26be22e099d5d859f
JasonM23 <jasonm23@gmail.com>
2016-04-01 22:12:44 -0400

    [Updating auto-complete version: 20160329.2321][removing old version: 20160310.2248]

--- elpa/auto-complete-20160329.2321/auto-complete-pkg.el
+++ elpa/auto-complete-20160329.2321/auto-complete-pkg.el
@@ -1,4 +1,4 @@
-(define-package "auto-complete" "20160310.2248" "Auto Completion for GNU Emacs"
+(define-package "auto-complete" "20160329.2321" "Auto Completion for GNU Emacs"
   '((popup "0.5.0")
     (cl-lib "0.5")))
 ;; Local Variables:
