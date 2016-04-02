3d144c9b547726adfe6536885302fee2d3685f6e
JasonM23 <jasonm23@gmail.com>
2016-04-01 22:12:56 -0400

    [Updating git-commit version: 20160329.858][removing old version: 20160130.649]

--- elpa/git-commit-20160329.858/git-commit.el
+++ elpa/git-commit-20160329.858/git-commit.el
@@ -11,9 +11,9 @@
 ;;	Marius Vollmer <marius.vollmer@gmail.com>
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 
-;; Package-Requires: ((emacs "24.4") (dash "20151021.113") (with-editor "20160128.1201"))
+;; Package-Requires: ((emacs "24.4") (dash "20151021.113") (with-editor "20160223.115"))
 ;; Keywords: git tools vc
-;; Package-Version: 20160130.649
+;; Package-Version: 20160329.858
 ;; Homepage: https://github.com/magit/magit
 
 ;; This file is not part of GNU Emacs.
