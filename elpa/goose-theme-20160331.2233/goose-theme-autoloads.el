ed3e7b0dff5702fa959aaaec4ad9dfd086b2daa2
JasonM23 <jasonm23@gmail.com>
2016-04-01 22:02:03 -0400

    [Adding goose-theme version: 20160331.2233]

---
+++ elpa/goose-theme-20160331.2233/goose-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; goose-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "goose-theme" "goose-theme.el" (22271 10199
+;;;;;;  240461 550000))
+;;; Generated autoloads from goose-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; goose-theme-autoloads.el ends here
---
+++ elpa/goose-theme-20160331.2233/goose-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "goose-theme" "20160331.2233" "A gray color theme" '((emacs "24.1")) :stars '("") :url "https://github.com/thwg/goose-theme")
