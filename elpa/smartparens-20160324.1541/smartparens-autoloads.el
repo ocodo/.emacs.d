1d75fc95554f8bad66fb561d7ebd3db82b643d04
JasonM23 <jasonm23@gmail.com>
2016-04-01 22:13:20 -0400

    [Updating smartparens version: 20160324.1541][removing old version: 20160225.705]

--- elpa/smartparens-20160324.1541/smartparens-autoloads.el
+++ elpa/smartparens-20160324.1541/smartparens-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "smartparens" "smartparens.el" (22246 12167
-;;;;;;  538579 899000))
+;;;### (autoloads nil "smartparens" "smartparens.el" (22271 10390
+;;;;;;  517900 161000))
 ;;; Generated autoloads from smartparens.el
 
 (autoload 'sp-cheat-sheet "smartparens" "\
@@ -182,7 +182,7 @@ Turn off `show-smartparens-mode'.
 ;;;;;;  "smartparens-html.el" "smartparens-latex.el" "smartparens-lua.el"
 ;;;;;;  "smartparens-pkg.el" "smartparens-python.el" "smartparens-racket.el"
 ;;;;;;  "smartparens-ruby.el" "smartparens-rust.el" "smartparens-scala.el")
-;;;;;;  (22246 12167 575676 448000))
+;;;;;;  (22271 10390 569652 494000))
 
 ;;;***
 
--- elpa/smartparens-20160324.1541/smartparens.el
+++ elpa/smartparens-20160324.1541/smartparens.el
@@ -497,6 +497,8 @@ Symbol is defined as a chunk of text recognized by
                                              python-mode
                                              coffee-mode
                                              js2-mode
+                                             asm-mode
+                                             makefile-gmake-mode
                                              )
   "List of modes that should not reindent after kill."
   :type '(repeat symbol)
