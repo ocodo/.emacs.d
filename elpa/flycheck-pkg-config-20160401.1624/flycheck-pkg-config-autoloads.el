b7c51a54f3a5d9112e722ef1959d65f1b2b76103
JasonM23 <jasonm23@gmail.com>
2016-04-02 21:26:24 -0400

    [Adding flycheck-pkg-config version: 20160401.1624]

---
+++ elpa/flycheck-pkg-config-20160401.1624/flycheck-pkg-config-autoloads.el
@@ -0,0 +1,23 @@
+;;; flycheck-pkg-config-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "flycheck-pkg-config" "flycheck-pkg-config.el"
+;;;;;;  (22272 28959 222966 329000))
+;;; Generated autoloads from flycheck-pkg-config.el
+
+(autoload 'flycheck-pkg-config "flycheck-pkg-config" "\
+Configure flycheck to use additional includes
+when checking the current buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-pkg-config-autoloads.el ends here
