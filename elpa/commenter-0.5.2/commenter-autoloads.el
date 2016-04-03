4e4901bddfb1b038011010539607c249b8b2ac53
JasonM23 <jasonm23@gmail.com>
2016-04-03 01:31:09 -0400

    [Adding commenter version: 0.5.2]

---
+++ elpa/commenter-0.5.2/commenter-autoloads.el
@@ -0,0 +1,22 @@
+;;; commenter-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "commenter" "commenter.el" (22272 30045 248522
+;;;;;;  691000))
+;;; Generated autoloads from commenter.el
+
+(autoload 'commenter-setup "commenter" "\
+Add advises to ‘comment-normalize-vars’.
+
+\(fn)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; commenter-autoloads.el ends here
