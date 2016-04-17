50b1f9da1c1eba2a7cb762a978b439c12dd1cfb3
JasonM23 <jasonm23@gmail.com>
2016-04-17 04:49:22 -0400

    [Updating visual-regexp-steroids version: 20160409.241][removing old version: 20150411.416]

--- elpa/visual-regexp-steroids-20160409.241/visual-regexp-steroids-pkg.el
+++ elpa/visual-regexp-steroids-20160409.241/visual-regexp-steroids-pkg.el
@@ -1,5 +1,5 @@
-(define-package "visual-regexp-steroids" "20150411.416" "Extends visual-regexp to support other regexp engines"
-  '((visual-regexp "0.8"))
+(define-package "visual-regexp-steroids" "20160409.241" "Extends visual-regexp to support other regexp engines"
+  '((visual-regexp "0.9"))
   :url "https://github.com/benma/visual-regexp-steroids.el/" :keywords
   '("external" "foreign" "regexp" "replace" "python" "visual" "feedback"))
 ;; Local Variables:
