98ba2eeed6a97968e4f676d8e28ba88f58d42819
JasonM23 <jasonm23@gmail.com>
2016-04-17 04:32:24 -0400

    [Adding typit version: 20160407.549]

---
+++ elpa/typit-20160407.549/typit-pkg.el
@@ -0,0 +1,9 @@
+(define-package "typit" "20160407.549" "Typing game similar to tests on 10 fast fingers"
+  '((emacs "24.4")
+    (f "0.18")
+    (mmt "0.1.1"))
+  :url "https://github.com/mrkkrp/typit" :keywords
+  '("games"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
