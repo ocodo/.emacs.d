4cd9c4f29be0bcb682af616e954e25b95d2a62a8
JasonM23 <jasonm23@gmail.com>
2016-04-17 04:32:24 -0400

    [Adding ten-hundred-mode version: 20160409.551]

---
+++ elpa/ten-hundred-mode-20160409.551/ten-hundred-mode-autoloads.el
@@ -0,0 +1,32 @@
+;;; ten-hundred-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "ten-hundred-mode" "ten-hundred-mode.el" (22291
+;;;;;;  18945 759753 943000))
+;;; Generated autoloads from ten-hundred-mode.el
+
+(autoload 'ten-hundred-mode "ten-hundred-mode" "\
+This makes you write with only the ten hundred most usual
+words that everyone uses every day. If you try to use any other
+words, the computer takes them away for you, so you can try
+again. So you always use easy words that everyone can
+understand, and no one has trouble figuring out what you want
+to say.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ten-hundred-mode-pkg.el") (22291 18945
+;;;;;;  766106 385000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ten-hundred-mode-autoloads.el ends here
