734745df702e7df185ae1198412b444da8f3888b
JasonM23 <jasonm23@gmail.com>
2016-04-17 04:32:23 -0400

    [Adding restclient-helm version: 20160407.249]

---
+++ elpa/restclient-helm-20160407.249/restclient-helm-autoloads.el
@@ -0,0 +1,22 @@
+;;; restclient-helm-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "restclient-helm" "restclient-helm.el" (22291
+;;;;;;  18947 251760 325000))
+;;; Generated autoloads from restclient-helm.el
+
+(autoload 'helm-restclient "restclient-helm" "\
+Helm for Restclient.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; restclient-helm-autoloads.el ends here
---
+++ elpa/restclient-helm-20160407.249/restclient-helm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "restclient-helm" "20160407.249" "helm interface for restclient.el" '((restclient "0") (helm "1.9.4")) :stars '("") :keywords '("http" "helm"))
