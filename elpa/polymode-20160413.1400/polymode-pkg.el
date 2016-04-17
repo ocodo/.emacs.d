6aaf4ce497dfa1dff8a7f657f3541044a64737e3
JasonM23 <jasonm23@gmail.com>
2016-04-17 04:49:12 -0400

    [Updating polymode version: 20160413.1400][removing old version: 20160320.1740]

--- elpa/polymode-20160413.1400/polymode-pkg.el
+++ elpa/polymode-20160413.1400/polymode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "polymode" "20160320.1740" "Versatile multiple modes with extensive literate programming support"
+(define-package "polymode" "20160413.1400" "Versatile multiple modes with extensive literate programming support"
   '((emacs "24"))
   :url "https://github.com/vitoshka/polymode")
 ;; Local Variables:
