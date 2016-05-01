8e267ab051df624d9cb68d2d4c6a5e8c3b4ac26e
JasonM23 <jasonm23@gmail.com>
2016-05-01 02:38:46 -0400

    [Updating clojure-mode version: 20160430.1414][removing old version: 20160414.545]

--- elpa/clojure-mode-20160430.1414/clojure-mode-autoloads.el
+++ elpa/clojure-mode-20160430.1414/clojure-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "clojure-mode" "clojure-mode.el" (22291 19274
-;;;;;;  44793 908000))
+;;;### (autoloads nil "clojure-mode" "clojure-mode.el" (22309 39037
+;;;;;;  140140 350000))
 ;;; Generated autoloads from clojure-mode.el
 
 (autoload 'clojure-mode "clojure-mode" "\
