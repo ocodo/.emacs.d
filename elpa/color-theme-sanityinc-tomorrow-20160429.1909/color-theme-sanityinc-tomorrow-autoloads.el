3c25a3298a6ccabb4af63efc5a0b86e20ce81c33
JasonM23 <jasonm23@gmail.com>
2016-05-01 02:38:48 -0400

    [Updating color-theme-sanityinc-tomorrow version: 20160429.1909][removing old version: 20160413.150]

--- elpa/color-theme-sanityinc-tomorrow-20160429.1909/color-theme-sanityinc-tomorrow-autoloads.el
+++ elpa/color-theme-sanityinc-tomorrow-20160429.1909/color-theme-sanityinc-tomorrow-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "color-theme-sanityinc-tomorrow" "color-theme-sanityinc-tomorrow.el"
-;;;;;;  (22291 19271 364787 814000))
+;;;;;;  (22309 39029 964129 697000))
 ;;; Generated autoloads from color-theme-sanityinc-tomorrow.el
 
 (when (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -39,8 +39,8 @@ Apply the tomorrow blue theme.
 ;;;### (autoloads nil nil ("color-theme-sanityinc-tomorrow-pkg.el"
 ;;;;;;  "sanityinc-tomorrow-blue-theme.el" "sanityinc-tomorrow-bright-theme.el"
 ;;;;;;  "sanityinc-tomorrow-day-theme.el" "sanityinc-tomorrow-eighties-theme.el"
-;;;;;;  "sanityinc-tomorrow-night-theme.el") (22291 19271 372884
-;;;;;;  17000))
+;;;;;;  "sanityinc-tomorrow-night-theme.el") (22309 39029 988015
+;;;;;;  404000))
 
 ;;;***
 
