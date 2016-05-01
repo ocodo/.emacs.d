6a9d6c19cdd4c4951521c1cf43df654b8a78642e
JasonM23 <jasonm23@gmail.com>
2016-05-01 02:39:15 -0400

    [Updating rust-mode version: 20160425.2119][removing old version: 20160412.1243]

--- elpa/rust-mode-20160425.2119/rust-mode-autoloads.el
+++ elpa/rust-mode-20160425.2119/rust-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "rust-mode" "rust-mode.el" (22291 19079 492258
-;;;;;;  287000))
+;;;### (autoloads nil "rust-mode" "rust-mode.el" (22309 38837 779826
+;;;;;;  274000))
 ;;; Generated autoloads from rust-mode.el
 
 (autoload 'rust-mode "rust-mode" "\
