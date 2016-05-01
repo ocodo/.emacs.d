a37b1c2d0b765f5dcfa72b6fc741a15d871c3c87
jasonm23 <jasonm23@gmail.com>
2016-05-01 16:02:23 +0800

    [Adding github-issues version: 20120425.1535]

---
+++ elpa/github-issues-20120425.1535/github-issues-autoloads.el
@@ -0,0 +1,16 @@
+;;; github-issues-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("github-issues.el") (22309 47056 864479
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; github-issues-autoloads.el ends here
---
+++ elpa/github-issues-20120425.1535/github-issues-pkg.el
@@ -0,0 +1,1 @@
+(define-package "github-issues" "20120425.1535" "Emacs utility functions and modes for managing GitHub projects' issues" 'nil :url "http://inkel.github.com/github-issues.el/" :keywords '("github" "issues"))
