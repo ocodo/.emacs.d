d5457c410a1cbac532c2e0d98c321d0b40b457a7
JasonM23 <jasonm23@gmail.com>
2016-05-01 02:39:33 -0400

    [Adding elisp-format version: 20090830.340]

---
+++ elpa/elisp-format-20090830.340/elisp-format-autoloads.el
@@ -0,0 +1,16 @@
+;;; elisp-format-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("elisp-format.el") (22309 38730 895265
+;;;;;;  400000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; elisp-format-autoloads.el ends here
---
+++ elpa/elisp-format-20090830.340/elisp-format-pkg.el
@@ -0,0 +1,1 @@
+(define-package "elisp-format" "20090830.340" "Format elisp code" 'nil :url "http://www.emacswiki.org/emacs/download/elisp-format.el")
