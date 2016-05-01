68e04f375830b71af87bd318ccea42f8d6fab449
JasonM23 <jasonm23@gmail.com>
2016-05-01 02:39:34 -0400

    [Adding shift-number version: 20160419.1257]

---
+++ elpa/shift-number-20160419.1257/shift-number-autoloads.el
@@ -0,0 +1,27 @@
+;;; shift-number-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "shift-number" "shift-number.el" (22309 38726
+;;;;;;  879631 648000))
+;;; Generated autoloads from shift-number.el
+
+(autoload 'shift-number-up "shift-number" "\
+Increase the number at point (or on the current line) by ARG.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'shift-number-down "shift-number" "\
+Decrease the number at point (or on the current line) by ARG.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; shift-number-autoloads.el ends here
---
+++ elpa/shift-number-20160419.1257/shift-number-pkg.el
@@ -0,0 +1,1 @@
+(define-package "shift-number" "20160419.1257" "Increase/decrease the number at point" 'nil :url "https://github.com/alezost/shift-number.el" :keywords '("convenience"))
