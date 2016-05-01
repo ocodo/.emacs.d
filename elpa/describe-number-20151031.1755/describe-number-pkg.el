8b081d439ee1efa921eb34191fed5cfed9217ada
jasonm23 <jasonm23@gmail.com>
2016-05-01 16:00:15 +0800

    [Adding describe-number version: 20151031.1755]

---
+++ elpa/describe-number-20151031.1755/describe-number-pkg.el
@@ -0,0 +1,1 @@
+(define-package "describe-number" "20151031.1755" "Describe arbitrarily large number at point." '((yabin "1.1")) :url "https://github.com/netromdk/describe-number" :keywords '("describe" "value" "help"))
