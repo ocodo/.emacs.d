e683d177bec64a5e0b9974297637249493dcee96
JasonM23 <jasonm23@gmail.com>
2016-05-07 23:03:51 -0400

    [Adding fzf version: 20151129.633]

---
+++ elpa/fzf-20151129.633/fzf-autoloads.el
@@ -0,0 +1,26 @@
+;;; fzf-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "fzf" "fzf.el" (22318 43690 316313 621000))
+;;; Generated autoloads from fzf.el
+
+(autoload 'fzf "fzf" "\
+Starts a fzf session.
+
+\(fn)" t nil)
+
+(autoload 'fzf-directory "fzf" "\
+Starts a fzf session at the specified directory.
+
+\(fn DIRECTORY)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; fzf-autoloads.el ends here
---
+++ elpa/fzf-20151129.633/fzf-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fzf" "20151129.633" "A front-end for fzf." '((emacs "24.4")) :url "https://github.com/bling/fzf.el" :keywords '("fzf" "fuzzy" "search"))
