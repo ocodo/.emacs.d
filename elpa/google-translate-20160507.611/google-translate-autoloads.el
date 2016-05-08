8e7ca376c81ac5ba585870f950ef71eef77183ce
JasonM23 <jasonm23@gmail.com>
2016-05-07 23:01:41 -0400

    [Updating google-translate version: 20160507.611][removing old version: 20160111.654]

--- elpa/google-translate-20160507.611/google-translate-autoloads.el
+++ elpa/google-translate-20160507.611/google-translate-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "google-translate-default-ui" "google-translate-default-ui.el"
-;;;;;;  (22168 61950 699508 299000))
+;;;;;;  (22318 43584 912795 643000))
 ;;; Generated autoloads from google-translate-default-ui.el
 
 (autoload 'google-translate-query-translate "google-translate-default-ui" "\
@@ -64,7 +64,7 @@ reverse direction.
 ;;;***
 
 ;;;### (autoloads nil "google-translate-smooth-ui" "google-translate-smooth-ui.el"
-;;;;;;  (22168 61950 715508 306000))
+;;;;;;  (22318 43584 920795 606000))
 ;;; Generated autoloads from google-translate-smooth-ui.el
 
 (autoload 'google-translate-smooth-translate "google-translate-smooth-ui" "\
@@ -93,7 +93,7 @@ one respectively.
 
 ;;;### (autoloads nil nil ("google-translate-core-ui.el" "google-translate-core.el"
 ;;;;;;  "google-translate-pkg.el" "google-translate-tk.el" "google-translate.el")
-;;;;;;  (22168 61950 737417 337000))
+;;;;;;  (22318 43584 938732 614000))
 
 ;;;***
 
---
+++ elpa/google-translate-20160507.611/google-translate-pkg.el
@@ -0,0 +1,4 @@
+(define-package "google-translate" "20160507.611" "Emacs interface to Google Translate." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
