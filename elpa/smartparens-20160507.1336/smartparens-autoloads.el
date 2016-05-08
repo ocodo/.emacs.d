96bbe2e24cf02b3d5f00f4f66516f14616131651
JasonM23 <jasonm23@gmail.com>
2016-05-07 23:01:55 -0400

    [Updating smartparens version: 20160507.1336][removing old version: 20160411.719]

--- elpa/smartparens-20160507.1336/smartparens-autoloads.el
+++ elpa/smartparens-20160507.1336/smartparens-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "smartparens" "smartparens.el" (22291 19071
-;;;;;;  196230 663000))
+;;;### (autoloads nil "smartparens" "smartparens.el" (22318 43469
+;;;;;;  933340 242000))
 ;;; Generated autoloads from smartparens.el
 
 (autoload 'sp-cheat-sheet "smartparens" "\
@@ -182,7 +182,7 @@ Turn off `show-smartparens-mode'.
 ;;;;;;  "smartparens-html.el" "smartparens-latex.el" "smartparens-lua.el"
 ;;;;;;  "smartparens-pkg.el" "smartparens-python.el" "smartparens-racket.el"
 ;;;;;;  "smartparens-ruby.el" "smartparens-rust.el" "smartparens-scala.el")
-;;;;;;  (22291 19071 204594 173000))
+;;;;;;  (22318 43469 953703 566000))
 
 ;;;***
 
