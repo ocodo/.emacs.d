47d07ad405de327f2523c9a6d6342e19be2fc252
JasonM23 <jasonm23@gmail.com>
2016-05-12 19:45:49 -0400

    [Updating helm-core version: 20160511.622][removing old version: 20160504.2359]

--- elpa/helm-core-20160511.622/helm-core-autoloads.el
+++ elpa/helm-core-20160511.622/helm-core-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "helm" "helm.el" (22318 43566 616882 431000))
+;;;### (autoloads nil "helm" "helm.el" (22325 5477 295438 323000))
 ;;; Generated autoloads from helm.el
 
 (autoload 'helm-define-multi-key "helm" "\
@@ -190,7 +190,7 @@ Enable/disable helm debugging from outside of helm session.
 ;;;***
 
 ;;;### (autoloads nil nil ("helm-core-pkg.el" "helm-lib.el" "helm-multi-match.el"
-;;;;;;  "helm-source.el") (22318 43566 627193 825000))
+;;;;;;  "helm-source.el") (22325 5477 306120 949000))
 
 ;;;***
 
--- elpa/helm-core-20160511.622/helm-core-pkg.el
+++ elpa/helm-core-20160511.622/helm-core-pkg.el
@@ -1,4 +1,4 @@
-(define-package "helm-core" "20160504.2359" "Development files for Helm"
+(define-package "helm-core" "20160511.622" "Development files for Helm"
   '((emacs "24.3")
     (async "1.8"))
   :url "https://emacs-helm.github.io/helm/")
