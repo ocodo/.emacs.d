7444b831a67642186a92942de65359b8628d4208
JasonM23 <jasonm23@gmail.com>
2016-05-12 02:18:21 -0400

    [Updating dired+ version: 20160510.1649][removing old version: 20160429.2053]

--- elpa/dired+-20160510.1649/dired+-autoloads.el
+++ elpa/dired+-20160510.1649/dired+-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "dired+" "dired+.el" (22309 39015 860108 634000))
+;;;### (autoloads nil "dired+" "dired+.el" (22323 65260 203330 932000))
 ;;; Generated autoloads from dired+.el
 
 (defvar diff-switches "-c" "\
@@ -1210,6 +1210,17 @@ To set the behavior as a preference (default behavior), put this in
 your ~/.emacs, where VALUE is 1 to reuse or -1 to not reuse:
 
  (diredp-toggle-find-file-reuse-dir VALUE)
+
+Note: This affects only these commands:
+
+  `dired-find-file'
+  `diredp-mouse-find-file'
+
+It does not affect the corresponding `-other-window' commands.  Note
+too that, by default, mouse clicks to open files or directories open
+in another window: command `diredp-mouse-find-file-other-window', not
+`diredp-mouse-find-file'.  If you want a mouse click to reuse a
+directory then bind `mouse-2' to `diredp-mouse-find-file' instead.
 
 \(fn FORCE-P)" t nil)
 
--- elpa/dired+-20160510.1649/dired+.el
+++ elpa/dired+-20160510.1649/dired+.el
@@ -7,11 +7,11 @@
 ;; Copyright (C) 1999-2016, Drew Adams, all rights reserved.
 ;; Created: Fri Mar 19 15:58:58 1999
 ;; Version: 2013.07.23
-;; Package-Version: 20160429.2053
+;; Package-Version: 20160510.1649
 ;; Package-Requires: ()
-;; Last-Updated: Fri Apr 29 20:59:23 2016 (-0700)
+;; Last-Updated: Tue May 10 16:55:53 2016 (-0700)
 ;;           By: dradams
-;;     Update #: 9487
+;;     Update #: 9492
 ;; URL: http://www.emacswiki.org/dired+.el
 ;; Doc URL: http://www.emacswiki.org/DiredPlus
 ;; Keywords: unix, mouse, directories, diredp, dired
@@ -318,6 +318,9 @@
 ;;  -----------------
 ;;
 ;;  Starting with Emacs 24.4, listing details are hidden by default.
+;;  Note that this is different from the vanilla Emacs behavior, which
+;;  is to show details by default.
+;;
 ;;  Use `(' anytime to toggle this hiding.  You can use option
 ;;  `diredp-hide-details-initially-flag' to change the default/initial
 ;;  state.  See also option `diredp-hide-details-propagate-flag'.
@@ -7535,7 +7538,18 @@ A prefix arg specifies directly whether or not to reuse.
 To set the behavior as a preference (default behavior), put this in
 your ~/.emacs, where VALUE is 1 to reuse or -1 to not reuse:
 
- (diredp-toggle-find-file-reuse-dir VALUE)"
+ (diredp-toggle-find-file-reuse-dir VALUE)
+
+Note: This affects only these commands:
+
+  `dired-find-file'
+  `diredp-mouse-find-file'
+
+It does not affect the corresponding `-other-window' commands.  Note
+too that, by default, mouse clicks to open files or directories open
+in another window: command `diredp-mouse-find-file-other-window', not
+`diredp-mouse-find-file'.  If you want a mouse click to reuse a
+directory then bind `mouse-2' to `diredp-mouse-find-file' instead."
   (interactive "P")
   (if force-p                           ; Force.
       (if (natnump (prefix-numeric-value force-p))
