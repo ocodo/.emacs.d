7b9d6eea4fd6cb75cc4ee097285d12fb6aa95c03
JasonM23 <jasonm23@gmail.com>
2016-05-12 02:18:36 -0400

    [Updating org-projectile version: 20160511.1513][removing old version: 20160324.759]

--- elpa/org-projectile-20160324.759/org-projectile-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org-projectile" "20160324.759" "Repository todo management for org-mode" '((projectile "0.11.0") (dash "2.10.0")) :stars '(#("48" 0 2 (font-lock-face paradox-star-face))) :url "https://github.com/IvanMalison/org-projectile" :keywords '("org" "projectile" "todo"))
