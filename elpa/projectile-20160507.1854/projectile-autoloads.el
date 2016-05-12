e964dce13470e1f49a558c8809894de0a6946f14
JasonM23 <jasonm23@gmail.com>
2016-05-12 02:18:36 -0400

    [Updating projectile version: 20160507.1854][removing old version: 20160420.1508]

--- elpa/projectile-20160507.1854/projectile-autoloads.el
+++ elpa/projectile-20160507.1854/projectile-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "projectile" "projectile.el" (22309 38849 35845
-;;;;;;  134000))
+;;;### (autoloads nil "projectile" "projectile.el" (22323 65147 415481
+;;;;;;  572000))
 ;;; Generated autoloads from projectile.el
 
 (autoload 'projectile-version "projectile" "\
--- elpa/projectile-20160507.1854/projectile.el
+++ elpa/projectile-20160507.1854/projectile.el
@@ -4,7 +4,7 @@
 
 ;; Author: Bozhidar Batsov <bozhidar@batsov.com>
 ;; URL: https://github.com/bbatsov/projectile
-;; Package-Version: 20160420.1508
+;; Package-Version: 20160507.1854
 ;; Keywords: project, convenience
 ;; Version: 0.14.0-cvs
 ;; Package-Requires: ((dash "2.11.0") (pkg-info "0.4"))
@@ -1830,7 +1830,7 @@ a COMPILE-CMD, a TEST-CMD, and a RUN-CMD."
 (projectile-register-project-type 'haskell-stack '("stack.yaml") "stack build" "stack build --test")
 (projectile-register-project-type 'haskell-cabal #'projectile-cabal "cabal build" "cabal test")
 (projectile-register-project-type 'rust-cargo '("Cargo.toml") "cargo build" "cargo test")
-(projectile-register-project-type 'r '("DESCRIPTION") "R CMD INSTALL ." (concat "R CMD check -o " temporary-file-directory " ."))
+(projectile-register-project-type 'r '("DESCRIPTION") "R CMD INSTALL --with-keep.source ." (concat "R CMD check -o " temporary-file-directory " ."))
 (projectile-register-project-type 'go #'projectile-go "go build ./..." "go test ./...")
 
 (defun projectile-cabal ()
