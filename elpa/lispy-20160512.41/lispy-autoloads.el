8e33589a935f13fbd52fe351c64157182b4ff08c
JasonM23 <jasonm23@gmail.com>
2016-05-12 19:45:49 -0400

    [Updating lispy version: 20160512.41][removing old version: 20160511.621]

--- elpa/lispy-20160512.41/lispy-autoloads.el
+++ elpa/lispy-20160512.41/lispy-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "lispy" "lispy.el" (22323 65196 391414 667000))
+;;;### (autoloads nil "lispy" "lispy.el" (22325 5464 119289 85000))
 ;;; Generated autoloads from lispy.el
 
 (autoload 'lispy-mode "lispy" "\
@@ -34,7 +34,7 @@ backward through lists, which is useful to move into special.
 
 ;;;### (autoloads nil nil ("elpa.el" "le-clojure.el" "le-hy.el" "le-julia.el"
 ;;;;;;  "le-lisp.el" "le-python.el" "le-scheme.el" "lispy-inline.el"
-;;;;;;  "lispy-pkg.el" "lispy-tags.el") (22323 65196 408822 62000))
+;;;;;;  "lispy-pkg.el" "lispy-tags.el") (22325 5464 134840 802000))
 
 ;;;***
 
--- elpa/lispy-20160512.41/lispy-pkg.el
+++ elpa/lispy-20160512.41/lispy-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lispy" "20160511.621" "vi-like Paredit"
+(define-package "lispy" "20160512.41" "vi-like Paredit"
   '((emacs "24.1")
     (ace-window "0.9.0")
     (iedit "0.9.9")
