bc49285b944330e87e18e81d921203616ad566fd
JasonM23 <jasonm23@gmail.com>
2016-05-22 20:26:13 -0400

    [Updating hydra version: 20160518.721][removing old version: 20160511.152]

--- elpa/hydra-20160518.721/hydra-autoloads.el
+++ elpa/hydra-20160518.721/hydra-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "hydra" "hydra.el" (22323 65222 979379 328000))
+;;;### (autoloads nil "hydra" "hydra.el" (22338 19838 199383 423000))
 ;;; Generated autoloads from hydra.el
 
 (autoload 'defhydra "hydra" "\
@@ -63,7 +63,7 @@ result of `defhydra'.
 ;;;***
 
 ;;;### (autoloads nil nil ("hydra-examples.el" "hydra-ox.el" "hydra-pkg.el"
-;;;;;;  "lv.el") (22323 65222 992626 452000))
+;;;;;;  "lv.el") (22338 19838 213339 624000))
 
 ;;;***
 
