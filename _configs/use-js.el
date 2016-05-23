d9615db493769e34af82508cd1fc0a79abe53362
Pair <pair@pivotal.io>
2016-05-23 13:41:16 +0800

    Add jsx and json helpers for eslint

--- _configs/use-js.el
+++ _configs/use-js.el
@@ -1,4 +1,32 @@
 ;; use-js-mode
 (dolist (pattern '("\\.jsx\\'"))
   (add-to-list 'auto-mode-alist (cons pattern 'js2-jsx-mode)))
+
+;; js2-jsx mode + eslint
+
+(defun use-eslint-js2 ()
+  (interactive)
+  ;; disable jshint since we prefer eslint checking
+  (setq-default flycheck-disabled-checkers
+                (append flycheck-disabled-checkers
+                        '(javascript-jshint)))
+
+  (setq sgml-basic-offset 2
+        js2-basic-offset 2)
+
+  ;; use eslint with web-mode for jsx files
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+
+  ;; customize flycheck temp file prefix
+  (setq-default flycheck-temp-prefix ".flycheck")
+
+  ;; disable json-jsonlist checking for json files
+  (setq-default flycheck-disabled-checkers
+                (append flycheck-disabled-checkers
+                        '(json-jsonlist)))
+
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize))
+)
+
 (provide 'use-js)
--- _configs/use-json-mode.el
+++ _configs/use-json-mode.el
@@ -1,4 +1,11 @@
 ;; use-json-mode
-(dolist (pattern '("\\.jshintrc\\'" "\\.jslint\\'"))
+(dolist (pattern '(
+                   "\\.jshintrc\\'"
+
+                   "\\.jslint\\'"
+
+                   "\\.eslintrc\\'"
+
+                   ))
   (add-to-list 'auto-mode-alist (cons pattern 'json-mode)))
 (provide 'use-json-mode)
