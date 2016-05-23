6be6c8f4248fb6cfac8233ab1d92e09079c460cf
JasonM23 <jasonm23@gmail.com>
2016-05-22 20:26:05 -0400

    [Updating flycheck version: 20160519.303][removing old version: 20160511.1347]

--- elpa/flycheck-20160519.303/flycheck-autoloads.el
+++ elpa/flycheck-20160519.303/flycheck-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "flycheck" "flycheck.el" (22323 65243 51353
-;;;;;;  81000))
+;;;### (autoloads nil "flycheck" "flycheck.el" (22338 19885 183304
+;;;;;;  895000))
 ;;; Generated autoloads from flycheck.el
 
 (autoload 'flycheck-manual "flycheck" "\
@@ -227,7 +227,7 @@ Use this together with the `option', `option-list' and
 ;;;***
 
 ;;;### (autoloads nil nil ("flycheck-buttercup.el" "flycheck-ert.el"
-;;;;;;  "flycheck-pkg.el") (22323 65243 59299 602000))
+;;;;;;  "flycheck-pkg.el") (22338 19885 191010 331000))
 
 ;;;***
 
