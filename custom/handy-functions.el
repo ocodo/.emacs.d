902a00c0f601c750f9cb7d8cd769f2eea924201e
Jasonm23 <Jasonm23@gmail.com>
2016-06-03 17:45:23 +0800

    add phils multioccur to handy funcs

--- custom/handy-functions.el
+++ custom/handy-functions.el
@@ -837,6 +837,13 @@ Use negative prefix P to go backward."
     (goto-char begin)
     (insert table)))
 
+(defun my-multi-occur-in-matching-buffers (regexp &optional allbufs)
+  "Show all lines matching REGEXP in all buffers."
+  (interactive (occur-read-primary-args))
+  (multi-occur-in-matching-buffers ".*" regexp))
+
+(global-set-key (kbd "M-s-/") 'my-multi-occur-in-matching-buffers)
+
 ;; Key bindings
 
 (global-set-key (kbd "C-c M-+")   'increase-default-font-height)
