79b64a9b75fc41a2de709d8d0623ad18c77a7c12
JasonM23 <jasonm23@gmail.com>
2016-06-13 02:57:19 -0400

    [Updating kill-or-bury-alive version: 20160531.908][removing old version: 20160128.109]

--- elpa/kill-or-bury-alive-20160531.908/kill-or-bury-alive.el
+++ elpa/kill-or-bury-alive-20160531.908/kill-or-bury-alive.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Mark Karpov <markkarpov@openmailbox.org>
 ;; URL: https://github.com/mrkkrp/kill-or-bury-alive
-;; Package-Version: 20160128.109
+;; Package-Version: 20160531.908
 ;; Version: 0.1.2
 ;; Package-Requires: ((emacs "24.4") (cl-lib "0.5"))
 ;; Keywords: buffer, killing, convenience
@@ -29,17 +29,17 @@
 ;; Have you ever killed some buffer that you might want to leave alive?
 ;; Motivation for killing is usually “get out of my way for now”, and
 ;; killing may be not the best choice in many cases unless your RAM is
-;; very-very limited. This package allows to teach Emacs which buffers we
+;; very-very limited.  This package allows to teach Emacs which buffers we
 ;; want to kill and which ones we prefer to bury alive.
 ;;
 ;; When we really want to kill a buffer, it turns out that not all buffers
-;; would like to die the same way. The package allows to specify *how* to
-;; kill various kinds of buffers. This may be especially useful when you're
+;; would like to die the same way.  The package allows to specify *how* to
+;; kill various kinds of buffers.  This may be especially useful when you're
 ;; working with some buffer that has an associated process, for example.
 ;;
 ;; But sometimes you may want to get rid of most buffers and bring Emacs to
-;; some more or less virgin state. You probably don't want to kill scratch
-;; buffer and maybe ERC-related buffers too. You can specify which buffers
+;; some more or less virgin state.  You probably don't want to kill scratch
+;; buffer and maybe ERC-related buffers too.  You can specify which buffers
 ;; to purge.
 
 ;;; Code:
