fbb0ee522204aee4a329fa49c618582b778f7ab1
JasonM23 <jasonm23@gmail.com>
2016-06-13 02:57:45 -0400

    [Adding counsel-osx-app version: 20160525.305]

---
+++ elpa/counsel-osx-app-20160525.305/counsel-osx-app-autoloads.el
@@ -0,0 +1,22 @@
+;;; counsel-osx-app-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "counsel-osx-app" "counsel-osx-app.el" (22366
+;;;;;;  21221 816457 411000))
+;;; Generated autoloads from counsel-osx-app.el
+
+(autoload 'counsel-osx-app "counsel-osx-app" "\
+Launch an application via ivy interface.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; counsel-osx-app-autoloads.el ends here
