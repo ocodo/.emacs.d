3ff7dc5b4def2e1699e3286041de1917e2bc75bb
JasonM23 <jasonm23@gmail.com>
2016-06-13 02:57:46 -0400

    [Adding trr version: 20160602.923]

---
+++ elpa/trr-20160602.923/trr-autoloads.el
@@ -0,0 +1,27 @@
+;;; trr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "trr" "trr.el" (22366 21215 472448 654000))
+;;; Generated autoloads from trr.el
+
+(autoload 'trr "trr" "\
+Start trr.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("trr-files.el" "trr-graphs.el" "trr-menus.el"
+;;;;;;  "trr-mesg.el" "trr-pkg.el" "trr-sess.el") (22366 21215 500912
+;;;;;;  262000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; trr-autoloads.el ends here
---
+++ elpa/trr-20160602.923/trr-pkg.el
@@ -0,0 +1,5 @@
+(define-package "trr" "20160602.923" "a type-writing training program on GNU Emacs." 'nil :keywords
+  '("games" "faces"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
