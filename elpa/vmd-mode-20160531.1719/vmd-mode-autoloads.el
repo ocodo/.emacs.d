9435506a1c3a3385dc96784128b6467ca587d64e
JasonM23 <jasonm23@gmail.com>
2016-06-13 02:57:46 -0400

    [Adding vmd-mode version: 20160531.1719]

---
+++ elpa/vmd-mode-20160531.1719/vmd-mode-autoloads.el
@@ -0,0 +1,22 @@
+;;; vmd-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "vmd-mode" "vmd-mode.el" (22366 21211 916443
+;;;;;;  739000))
+;;; Generated autoloads from vmd-mode.el
+
+(autoload 'vmd-mode "vmd-mode" "\
+Live Markdown preview with `vmd'.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; vmd-mode-autoloads.el ends here
---
+++ elpa/vmd-mode-20160531.1719/vmd-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "vmd-mode" "20160531.1719" "Fast Github-flavored Markdown preview using a vmd subprocess." '((emacs "24.3")) :url "https://github.com/blak3mill3r/vmd-mode" :keywords '("markdown" "preview" "live" "vmd"))
