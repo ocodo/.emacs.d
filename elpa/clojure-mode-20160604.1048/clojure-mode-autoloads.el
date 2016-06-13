a4cfb008b6c6c120a03bcdcc2632275dfe24233e
JasonM23 <jasonm23@gmail.com>
2016-06-13 02:56:59 -0400

    [Updating clojure-mode version: 20160604.1048][removing old version: 20160521.1109]

--- elpa/clojure-mode-20160604.1048/clojure-mode-autoloads.el
+++ elpa/clojure-mode-20160604.1048/clojure-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "clojure-mode" "clojure-mode.el" (22338 19921
-;;;;;;  111955 754000))
+;;;### (autoloads nil "clojure-mode" "clojure-mode.el" (22366 21718
+;;;;;;  808671 652000))
 ;;; Generated autoloads from clojure-mode.el
 
 (autoload 'clojure-mode "clojure-mode" "\
@@ -41,6 +41,44 @@ Fully thread the form at point using ->>.
 When BUT-LAST is passed the last expression is not threaded.
 
 \(fn BUT-LAST)" t nil)
+
+(autoload 'clojure-cycle-privacy "clojure-mode" "\
+Make public the current private def, or vice-versa.
+See: https://github.com/clojure-emacs/clj-refactor.el/wiki/cljr-cycle-privacy
+
+\(fn)" t nil)
+
+(autoload 'clojure-convert-collection-to-list "clojure-mode" "\
+Convert collection at (point) to list.
+
+\(fn)" t nil)
+
+(autoload 'clojure-convert-collection-to-quoted-list "clojure-mode" "\
+Convert collection at (point) to quoted list.
+
+\(fn)" t nil)
+
+(autoload 'clojure-convert-collection-to-map "clojure-mode" "\
+Convert collection at (point) to map.
+
+\(fn)" t nil)
+
+(autoload 'clojure-convert-collection-to-vector "clojure-mode" "\
+Convert collection at (point) to vector.
+
+\(fn)" t nil)
+
+(autoload 'clojure-convert-collection-to-set "clojure-mode" "\
+Convert collection at (point) to set.
+
+\(fn)" t nil)
+
+(autoload 'clojure-cycle-if "clojure-mode" "\
+Change a surrounding if to if-not, or vice-versa.
+
+See: https://github.com/clojure-emacs/clj-refactor.el/wiki/cljr-cycle-if
+
+\(fn)" t nil)
 
 (autoload 'clojurescript-mode "clojure-mode" "\
 Major mode for editing ClojureScript code.
