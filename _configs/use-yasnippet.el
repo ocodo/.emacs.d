575b5d6d1fef20e27807fbd68f65daf7f93ee5c5
jasonm23 <jasonm23@gmail.com>
2016-06-14 23:11:52 +0800

    Ido prompting default for yas, add es6 import yasnippet

--- _configs/use-yasnippet.el
+++ _configs/use-yasnippet.el
@@ -1,3 +1,11 @@
 ;; use-yasnippet
 (yas-global-mode t)
+
+(setq yas-prompt-functions '(yas-ido-prompt
+                             yas-dropdown-prompt
+                             yas/x-prompt
+                             yas/completing-prompt
+                             yas/no-prompt))
+
+
 (provide 'use-yasnippet)
