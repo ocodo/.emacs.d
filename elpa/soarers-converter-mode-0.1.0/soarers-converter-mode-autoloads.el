1030a591cb6e5591dbd284d83930e5079af3f009
jasonm23 <jasonm23@gmail.com>
2016-06-18 19:03:27 +0800

    Add soarers-coverter-mode via package install file

---
+++ elpa/soarers-converter-mode-0.1.0/soarers-converter-mode-autoloads.el
@@ -0,0 +1,24 @@
+;;; soarers-converter-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "soarers-converter-mode" "soarers-converter-mode.el"
+;;;;;;  (22373 10855 0 0))
+;;; Generated autoloads from soarers-converter-mode.el
+
+(autoload 'soarers-converter-mode "soarers-converter-mode" "\
+Major mode for editing soarers converter configuration files
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.sc\\'" . soarers-converter-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; soarers-converter-mode-autoloads.el ends here
---
+++ elpa/soarers-converter-mode-0.1.0/soarers-converter-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "soarers-converter-mode" "0.1.0" "A major emacs mode for editing soarer's converter config files" '((emacs "24.0")) :url "https://github.com/jasonm23/soarers-converter-mode")
