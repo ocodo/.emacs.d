6a0865df9687277be40b89f370e24a9f41e0c523
JasonM23 <jasonm23@gmail.com>
2016-07-05 22:59:39 -0400

    [Updating hackernews version: 20160627.1651][removing old version: 20160326.725]

--- elpa/hackernews-20160326.725/hackernews-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "hackernews" "20160326.725" "Access the hackernews aggregator from Emacs"
-  '((json "1.2")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/hackernews-20160627.1651/hackernews-pkg.el
@@ -0,0 +1,5 @@
+(define-package "hackernews" "20160627.1651" "Access the hackernews aggregator from Emacs"
+  '((json "1.2")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
