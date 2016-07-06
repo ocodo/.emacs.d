b2a8ce0ce0024d78fe686938599c5bc8520f449c
JasonM23 <jasonm23@gmail.com>
2016-07-05 22:43:23 -0400

    [Adding avy-flycheck version: 20160702.716]

---
+++ elpa/avy-flycheck-20160702.716/avy-flycheck-autoloads.el
@@ -0,0 +1,28 @@
+;;; avy-flycheck-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "avy-flycheck" "avy-flycheck.el" (22396 28723
+;;;;;;  115747 978000))
+;;; Generated autoloads from avy-flycheck.el
+
+(autoload 'avy-flycheck-goto-error "avy-flycheck" "\
+Jump to a flycheck syntax error.
+The window scope is determined by `avy-all-windows' (ARG negates it).
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'avy-flycheck-setup "avy-flycheck" "\
+Set up default keybindings.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; avy-flycheck-autoloads.el ends here
