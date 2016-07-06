a7fa46d09b41e37b3cfb3ca7d6c04d8b6f35c296
JasonM23 <jasonm23@gmail.com>
2016-07-05 22:59:34 -0400

    [Updating flycheck-clojure version: 20160704.1221][removing old version: 20160319.758]

--- elpa/flycheck-clojure-20160704.1221/flycheck-clojure-autoloads.el
+++ elpa/flycheck-clojure-20160704.1221/flycheck-clojure-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "flycheck-clojure" "flycheck-clojure.el" (22271
-;;;;;;  10588 250985 759000))
+;;;### (autoloads nil "flycheck-clojure" "flycheck-clojure.el" (22396
+;;;;;;  29100 42786 609000))
 ;;; Generated autoloads from flycheck-clojure.el
 
 (autoload 'flycheck-clojure-parse-cider-errors "flycheck-clojure" "\
