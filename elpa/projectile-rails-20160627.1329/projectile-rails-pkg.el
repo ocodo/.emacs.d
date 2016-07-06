49fcdb572a6d7a1d24cf7f153d7ca52393d937fb
JasonM23 <jasonm23@gmail.com>
2016-07-05 22:59:59 -0400

    [Updating projectile-rails version: 20160627.1329][removing old version: 20160519.39]

---
+++ elpa/projectile-rails-20160627.1329/projectile-rails-pkg.el
@@ -0,0 +1,1 @@
+(define-package "projectile-rails" "20160627.1329" "Minor mode for Rails projects based on projectile-mode" '((emacs "24.3") (projectile "0.12.0") (inflections "1.1") (inf-ruby "2.2.6") (f "0.13.0") (rake "0.3.2")) :url "https://github.com/asok/projectile-rails" :keywords '("rails" "projectile"))
