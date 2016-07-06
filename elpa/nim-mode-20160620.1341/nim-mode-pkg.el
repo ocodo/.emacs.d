a9641671ef2e499a71cd063bea05dcb475cc437b
JasonM23 <jasonm23@gmail.com>
2016-07-05 22:59:54 -0400

    [Updating nim-mode version: 20160620.1341][removing old version: 20160510.1955]

--- elpa/nim-mode-20160620.1341/nim-mode-pkg.el
+++ elpa/nim-mode-20160620.1341/nim-mode-pkg.el
@@ -1,9 +1,9 @@
-(define-package "nim-mode" "20160510.1955" "A major mode for the Nim programming language"
+(define-package "nim-mode" "20160620.1341" "A major mode for the Nim programming language"
   '((emacs "24.4")
     (epc "0.1.1")
     (let-alist "1.0.1")
     (commenter "0.5.1")
-    (flycheck "0.25.1")
+    (flycheck "28")
     (company "0.8.12"))
   :keywords
   '("nim" "languages"))
