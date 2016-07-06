cfe26a4554e0b9a4d855828c05cc3f4d0f9e74cc
JasonM23 <jasonm23@gmail.com>
2016-07-05 22:59:42 -0400

    [Updating helm-projectile version: 20160614.832][removing old version: 20160602.2111]

--- elpa/helm-projectile-20160602.2111/helm-projectile-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-projectile" "20160602.2111" "Helm integration for Projectile" '((helm "1.7.7") (projectile "0.13.0") (dash "1.5.0") (cl-lib "0.3")) :url "https://github.com/bbatsov/helm-projectile" :keywords '("project" "convenience"))
