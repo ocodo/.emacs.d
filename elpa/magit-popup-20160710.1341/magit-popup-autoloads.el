25d199a6b2fd81ee6556456251b57ba5ad6b0b85
JasonM23 <jasonm23@gmail.com>
2016-07-13 03:46:08 -0400

    [Updating magit-popup version: 20160710.1341][removing old version: 20160606.1041]

--- elpa/magit-popup-20160710.1341/magit-popup-autoloads.el
+++ elpa/magit-popup-20160710.1341/magit-popup-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
-;;;;;;  (22366 21555 835009 880000))
+;;;;;;  (22405 60843 445771 882000))
 
 ;;;***
 
--- elpa/magit-popup-20160710.1341/magit-popup-pkg.el
+++ elpa/magit-popup-20160710.1341/magit-popup-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit-popup" "20160606.1041" "Define prefix-infix-suffix command combos"
+(define-package "magit-popup" "20160710.1341" "Define prefix-infix-suffix command combos"
   '((emacs "24.4")
     (async "20150909.2257")
     (dash "20151021.113"))
