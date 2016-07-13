5f9b317042045199a94dce4103dde5b1a8a4db40
JasonM23 <jasonm23@gmail.com>
2016-07-13 03:46:12 -0400

    [Adding tern-context-coloring version: 20160709.837]

---
+++ elpa/tern-context-coloring-20160709.837/tern-context-coloring-autoloads.el
@@ -0,0 +1,22 @@
+;;; tern-context-coloring-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "tern-context-coloring" "tern-context-coloring.el"
+;;;;;;  (22405 60744 308721 591000))
+;;; Generated autoloads from tern-context-coloring.el
+
+(autoload 'tern-context-coloring-setup "tern-context-coloring" "\
+Add Tern support to `context-coloring-mode'.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; tern-context-coloring-autoloads.el ends here
