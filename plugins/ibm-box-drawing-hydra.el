af95e33c875a40cab818a32f7a0fa49cd6ff024b
jasonm23 <jasonm23@gmail.com>
2016-07-17 22:16:23 +0800

    Kill all the superfluous functions in ibm-box-drawing-hydra

--- plugins/ibm-box-drawing-hydra.el
+++ plugins/ibm-box-drawing-hydra.el
@@ -32,76 +32,21 @@
 
 ;;; Code:
 
-(defun ibm-box-top-left ()
-  "Insert box char top-left."
-  (interactive)
-  (insert "┌"))
-
-(defun ibm-box-top ()
-  "Insert box char top."
-  (interactive)
-  (insert "┬"))
-
-(defun ibm-box-top-right ()
-  "Insert box char top-right."
-  (interactive)
-  (insert "┐"))
-
-(defun ibm-box-left ()
-  "Insert box char left."
-  (interactive)
-  (insert "├"))
-
-(defun ibm-box-center ()
-  "Insert box char center."
-  (interactive)
-  (insert "┼"))
-
-(defun ibm-box-right ()
-  "Insert box char right."
-  (interactive)
-  (insert "┤"))
-
-(defun ibm-box-bottom-left ()
-  "Insert box char bottom-left."
-  (interactive)
-  (insert "└"))
-
-(defun ibm-box-bottom ()
-  "Insert box char bottom."
-  (interactive)
-  (insert "┴"))
-
-(defun ibm-box-bottom-right ()
-  "Insert box char bottom-right."
-  (interactive)
-  (insert "┘"))
-
-(defun ibm-box-vert ()
-  "Insert box char vertical."
-  (interactive)
-  (insert "│"))
-
-(defun ibm-box-horz ()
-  "Insert box char horizontal."
-  (interactive)
-  (insert "─"))
-
 (global-set-key (kbd "C-x d")
                 (defhydra hydra-draw-box (:color pink)
                   "Draw box with IBM single line box characters."
                   ("ESC" nil :color blue) ;; Esc to exit.
-                  ("q" ibm-box-top-left      "top left ┌")
-                  ("w" ibm-box-top           "top ┬")
-                  ("e" ibm-box-top-right     "top right ┐")
-                  ("a" ibm-box-left          "left ├")
-                  ("s" ibm-box-center        "center ┼")
-                  ("d" ibm-box-right         "right ┤")
-                  ("z" ibm-box-bottom-left   "bottom left └")
-                  ("x" ibm-box-bottom        "bottom ┴")
-                  ("c" ibm-box-bottom-right  "bottom right ┘")
-                  ("r" ibm-box-horz          "horizontal ─")
-                  ("v" ibm-box-vert          "vertical │")))
+                  ("q" (lambda () (interactive) (insert "┌")) "top left ┌")
+                  ("w" (lambda () (interactive) (insert "┬")) "top ┬")
+                  ("e" (lambda () (interactive) (insert "┐")) "top right ┐")
+                  ("a" (lambda () (interactive) (insert "├")) "left ├")
+                  ("s" (lambda () (interactive) (insert "┼")) "center ┼")
+                  ("d" (lambda () (interactive) (insert "┤")) "right ┤")
+                  ("z" (lambda () (interactive) (insert "└")) "bottom left └")
+                  ("x" (lambda () (interactive) (insert "┴")) "bottom ┴")
+                  ("c" (lambda () (interactive) (insert "┘")) "bottom right ┘")
+                  ("r" (lambda () (interactive) (insert "─")) "horizontal ─")
+                  ("v" (lambda () (interactive) (insert "│")) "vertical │")))
 
 (provide 'ibm-box-drawing-hydra)
 
