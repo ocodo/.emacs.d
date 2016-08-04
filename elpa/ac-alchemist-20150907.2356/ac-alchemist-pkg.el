55b468851f109db12298ad377df886a15c2e3ca7
jasonm23 <jasonm23@gmail.com>
2016-08-04 21:07:31 +0800

    [Adding ac-alchemist-20150907.2356]

---
+++ elpa/ac-alchemist-20150907.2356/ac-alchemist-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ac-alchemist" "20150907.2356" "auto-complete source for alchemist" '((auto-complete "1.5.0") (alchemist "1.5.0") (cl-lib "0.5")) :url "https://github.com/syohex/emacs-ac-alchemist")
