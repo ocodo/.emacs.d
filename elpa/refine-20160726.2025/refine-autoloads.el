1fc41427f2fefa16adbf787399448ccbd971e762
jasonm23 <jasonm23@gmail.com>
2016-08-04 21:08:18 +0800

    [Adding refine-20160726.2025]

---
+++ elpa/refine-20160726.2025/refine-autoloads.el
@@ -0,0 +1,21 @@
+;;; refine-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "refine" "refine.el" (22435 10444 0 0))
+;;; Generated autoloads from refine.el
+
+(autoload 'refine "refine" "\
+Interactively edit the value of a symbol (usually a list).
+
+\(fn SYMBOL)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; refine-autoloads.el ends here
