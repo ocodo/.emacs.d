c67ceb7b395fb70635c70db9d63c4ef36428346b
jasonm23 <jasonm23@gmail.com>
2016-08-10 11:37:58 +0800

    [Updating feature-mode version: 20160805.2015][removing old version: 20141121.1030]

---
+++ elpa/feature-mode-20160805.2015/feature-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; feature-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "feature-mode" "feature-mode.el" (0 0 0 0))
+;;; Generated autoloads from feature-mode.el
+
+(autoload 'feature-mode "feature-mode" "\
+Major mode for editing plain text stories
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.feature\\'" . feature-mode))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "feature-mode" '("feature-" "expand-home-shellism" "compute-given-when-then-offset" "given-when-then-wordlength" "build-keyword-matcher" "parse-gherkin-l10n" "try-find-next" "load-gherkin-i10n")))
+
+;;;***
+
+;;;### (autoloads nil nil ("feature-mode-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; feature-mode-autoloads.el ends here
---
+++ elpa/feature-mode-20160805.2015/feature-mode-pkg.el
@@ -0,0 +1,4 @@
+(define-package "feature-mode" "20160805.2015" "Major mode for editing Gherkin (i.e. Cucumber) user stories" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
