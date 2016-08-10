1b24ea1022086eb54c910615f93f589552e86318
jasonm23 <jasonm23@gmail.com>
2016-08-10 11:38:26 +0800

    [Updating vdiff version: 20160803.1035][removing old version: 20160712.1040]

---
+++ elpa/vdiff-20160803.1035/vdiff-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vdiff" "20160803.1035" "A diff tool similar to  vimdiff" '((emacs "24.4") (hydra "0.13.0")) :url "https://github.com/justbur/emacs-vdiff" :keywords '("diff"))
