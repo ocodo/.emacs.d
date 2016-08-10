ae8b7298473d650ad43f883ce9af718fbf082c0e
jasonm23 <jasonm23@gmail.com>
2016-08-10 11:37:52 +0800

    [Updating commify version: 20160724.2032][removing old version: 20160711.511]

--- elpa/commify-20160724.2032/commify.el
+++ elpa/commify-20160724.2032/commify.el
@@ -16,8 +16,8 @@
 ;; with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;; Author: Daniel E. Doherty <ded-commify@ddoherty.net>
-;; Version: 1.1.2
-;; Package-Version: 20160711.511
+;; Version: 1.2.1
+;; Package-Version: 20160724.2032
 ;; Package-Requires: ((s "1.9.0"))
 ;; Keywords: convenience, editing, numbers, grouping, commas
 ;; URL: https://github.com/ddoherty03/commify
@@ -74,7 +74,63 @@
   :type 'integer
   :group 'commify)
 
-;; Main code:
+;; Utility functions
+
+(defun commify--number-chars ()
+  "Characters that can appear in a number."
+  (concat commify-decimal-char commify-group-char "0-9eE+-"))
+
+(defun commify--number-re ()
+  "Regular expression of a valid number string.
+
+A valid number has a mandatory whole number part, which it
+captures as the second group.  The number may contain the
+commify-group-char in the whole number part and uses
+commify-decimal-char as the separator between the whole and
+fractional part of the number.  A leading sign, `+' or `-' is
+optional, as is a trailing exponent introduced by `e' or `E'.
+
+The matched sub-parts are:
+  1. the optional sign,
+  2. the whole number part,
+  3. the optional fractional part, including the decimal point, and
+  4. the optional exponent part."
+  (let ((sign "\\([-+]\\)?")
+        (whole (concat "\\([0-9" (regexp-quote commify-group-char) "]+\\)"))
+        (frac (concat "\\(" (regexp-quote commify-decimal-char) "[0-9]+\\)?"))
+        (exp "\\([eE][0-9]+\\)?"))
+    (concat sign whole frac exp)))
+
+(defun commify--exception-p ()
+  "Should the current buffer position be excluded from commify?"
+  (or (commify--on-date-p)
+      (commify--on-indentifier-p)
+      (commify--on-zero-filled-p)
+      (commify--on-hex-p)))
+
+(defun commify--on-date-p ()
+  "Is text to the right of the cursor part of a date?"
+  (or (looking-at-p "\\(?:19\\|20\\)[[:digit:]]\\{2\\}[-/]")
+      (save-excursion
+        (backward-char)
+        (looking-at-p "[-/]\\(?:19\\|20\\)[[:digit:]]\\{2\\}"))))
+
+(defun commify--on-indentifier-p ()
+  "Is text to the right of the cursor part of an identifier?"
+  (save-excursion
+    (backward-char)
+    (looking-at-p "\\s_")))
+
+(defun commify--on-zero-filled-p ()
+  "Does text to the right of the cursor start with zero?"
+  (looking-at-p "0"))
+
+
+(defun commify--on-hex-p ()
+  "Is text to the right of the cursor part of a hexadecimal number?"
+  (save-excursion
+    (backward-char 2)
+    (looking-at-p "0x")))
 
 (defun commify--commas (n  &optional group-char group-size)
   "For an integer string N, insert GROUP-CHAR between groups of GROUP-SIZE digits."
@@ -94,33 +150,53 @@
     ;; now chop off any trailing group-char and re-reverse the string.
     (s-reverse (s-chop-suffix group-char num))))
 
+(defun commify--uncommas (n &optional group-char)
+  "For an integer string N, remove all instances of GROUP-CHAR."
+  (unless group-char (setq group-char commify-group-char))
+  (s-replace-all `((,commify-group-char . "")) n))
+
+;; Commands
+
 ;;;###autoload
-(defun commify-toggle ()
+(defun commify-toggle-at-point ()
   "Toggle insertion or deletion of grouping characters in the number around point."
   (interactive)
   (save-excursion
     ;; find the beginning of the number the cursor is in or after.
-    (skip-chars-backward (concat commify-decimal-char commify-group-char "0-9e+-"))
-    ;; skip past any leading sign.
-    (when (looking-at "[-+]")
-      (skip-chars-forward "-+"))
-    ;; if there's a number to the right, proceed.
-    (when (looking-at "[0-9]")
-      ;; record the start of the number.
-      (let ((beg-num (point))
-            (num nil))
-        ;; go forward across any digits or group-char.
-        (skip-chars-forward (concat commify-group-char "0-9"))
-        ;; extract the number as a string.
-        (setq num (delete-and-extract-region beg-num (point)))
-        (if (s-contains? commify-group-char num)
-            ;; remove group-char if its in the string ...
-            (insert (s-replace-all `((,commify-group-char . "")) num))
-          ;; or add them if its not.
-          (insert (commify--commas num)))
-        (goto-char beg-num))))
+    (skip-chars-backward (commify--number-chars))
+    (when (looking-at (commify--number-re))
+      (unless (commify--exception-p)
+        (let ((num (match-string 2))
+              (num-beg (match-beginning 2))
+              (num-end (match-end 2)))
+          (delete-region num-beg num-end)
+          (if (s-contains? commify-group-char num)
+              (insert (commify--uncommas num))
+            (insert (commify--commas num)))))))
   ;; move cursor to the end of the number.
-  (skip-chars-forward (concat commify-decimal-char commify-group-char "0-9e+-")))
+  (skip-chars-forward (commify--number-chars)))
+
+;;;###autoload
+(defun commify-toggle-on-region (beg end)
+  "Toggle insertion or deletion of numeric grouping characters.
+Do so for all numbers in the region between BEG and END."
+  (interactive "r")
+  (let (deactivate-mark)
+    (save-excursion
+      (goto-char beg)
+      (while (search-forward-regexp (commify--number-re) (+ 30 end) t)
+        (commify-toggle-at-point))
+      (search-forward-regexp (commify--number-re) (+ 30 (point)) t))))
+
+;;;###autoload
+(defun commify-toggle ()
+  "Toggle commas at point or on the region from BEG to END."
+  (interactive)
+  (if (use-region-p)
+      (commify-toggle-on-region (region-beginning) (region-end))
+    (commify-toggle-at-point)))
+
 
 (provide 'commify)
 ;;; commify.el ends here
+
