4324ebf1cae9d2f9af11535d1cce1e86a16016cd
jasonm23 <jasonm23@gmail.com>
2016-08-10 11:38:29 +0800

    [Updating xterm-color version: 20160802.1752][removing old version: 20160707.1653]

--- elpa/xterm-color-20160707.1653/xterm-color-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; xterm-color-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("xterm-color.el") (22405 60763 322053
-;;;;;;  936000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; xterm-color-autoloads.el ends here
--- elpa/xterm-color-20160707.1653/xterm-color-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "xterm-color" "20160707.1653" "ANSI & XTERM 256 color support" 'nil)
