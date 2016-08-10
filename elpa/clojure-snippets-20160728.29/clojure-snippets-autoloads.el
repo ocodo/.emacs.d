7a32dffdc20496c780143a62e0e68c572e32b8b6
jasonm23 <jasonm23@gmail.com>
2016-08-10 11:37:51 +0800

    [Updating clojure-snippets version: 20160728.29][removing old version: 20160305.602]

--- elpa/clojure-snippets-20160728.29/clojure-snippets-autoloads.el
+++ elpa/clojure-snippets-20160728.29/clojure-snippets-autoloads.el
@@ -1,10 +1,10 @@
 ;;; clojure-snippets-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "clojure-snippets" "clojure-snippets.el" (22246
-;;;;;;  12477 945544 395000))
+;;;### (autoloads nil "clojure-snippets" "clojure-snippets.el" (0
+;;;;;;  0 0 0))
 ;;; Generated autoloads from clojure-snippets.el
 
 (autoload 'clojure-snippets-initialize "clojure-snippets" "\
@@ -16,8 +16,7 @@
 
 ;;;***
 
-;;;### (autoloads nil nil ("clojure-snippets-pkg.el") (22246 12477
-;;;;;;  957040 21000))
+;;;### (autoloads nil nil ("clojure-snippets-pkg.el") (0 0 0 0))
 
 ;;;***
 
