844eb31e244c5b61d9b13b8c98ad0993191cdd3b
jasonm23 <jasonm23@gmail.com>
2016-08-11 14:18:45 +0800

    Fix move-dup config. Add super up / down

--- _configs/use-move-dup.el
+++ _configs/use-move-dup.el
@@ -1,11 +1,15 @@
-;;; init-move-dup --- initialize move-dup
+;;; use-move-dup --- initialize move-dup
 ;;; Commentary:
 ;;; Code:
 (require 'use-package)
 
 (use-package move-dup
   :config ;; before use
-  (global-move-dup-mode t))
+  (progn (global-move-dup-mode t)
+         (bind-keys
+          :map move-dup-mode-map
+          ("s-<up>" . md/duplicate-up)
+          ("s-<down>" . md/duplicate-down))))
 
-(provide 'init-move-dup)
-;;; init-move-dup ends here
+(provide 'use-move-dup)
+;;; use-move-dup ends here
