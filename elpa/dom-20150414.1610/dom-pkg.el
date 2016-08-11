9a98f730d5a6656de7fbb16396864c4172bd3bde
jasonm23 <jasonm23@gmail.com>
2016-08-11 11:21:59 +0800

    [remove package dom-20150414]

--- elpa/dom-20150414.1610/dom-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dom" "20150414.1610" "DOM implementation and manipulation library" '((cl-lib "0.5")) :url "http://www.github.com/toroidal-code/dom.el/" :keywords '("xml" "dom"))
