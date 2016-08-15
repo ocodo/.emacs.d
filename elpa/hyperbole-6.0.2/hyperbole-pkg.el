b171a14720f9d8494e896a82d872f6a07eff58e5
jasonm23 <jasonm23@gmail.com>
2016-08-15 16:33:00 +0800

    [Adding hyperbole version: 6.0.2]

---
+++ elpa/hyperbole-6.0.2/hyperbole-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from hyperbole.el
+(define-package "hyperbole" "6.0.2" "GNU Hyperbole: The Everyday Hypertextual Information Manager" '((emacs "24.4")) :url "http://www.gnu.org/software/hyperbole" :keywords '("comm" "convenience" "files" "frames" "hypermedia" "languages" "mail" "matching" "mouse" "multimedia" "outlines" "tools" "wp"))
