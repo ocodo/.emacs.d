b5501fdb4a2f0d2e5c9f4485b608ed16eaf85496
jasonm23 <jasonm23@gmail.com>
2016-08-17 17:12:34 +0800

    fix up New yas from region

--- custom/handy-functions.el
+++ custom/handy-functions.el
@@ -906,12 +906,8 @@ Optionally check ALLBUFS."
 ;; ie. {test} becomes { test }
 ;;
 ;; NOTE: cursor must start ahead of the target expression
-
 (fset 'pad-inside-braces
    [?\C-s ?\{ return ?  ?\C-s ?\} return left ?  right])
-
-
-;;; WIP
 
 (defun make-yas-from-region (b e)
   "Make a parameterless yasnippet from the current region B E."
@@ -921,9 +917,10 @@ Optionally check ALLBUFS."
         ;; TODO make a new buffer with yas headers
         ;; ask for a name
         (let* ((name (read-from-minibuffer "Name: "))
-              (key (read-from-minibuffer "Key: "))
-              (snippet (buffer-substring b e))
-              (template (format "# -*- mode: snippet -*-
+               (key (read-from-minibuffer "Key: "))
+               (filename (format "%ssnippets/%s/%s" user-emacs-directory major-mode name))
+               (snippet (buffer-substring b e))
+               (template (format "# -*- mode: snippet -*-
 # name: %s
 # key: %s
 # --
@@ -932,12 +929,11 @@ Optionally check ALLBUFS."
                                 name
                                 key
                                 snippet)))
-          ;; body... make a temp buffer and save it to snippets (kill the buffer on save)
-
-          )
-
+          (with-temp-buffer
+            (insert template)
+            (write-file filename)))
         (yas-reload-all))
-    (error "An active region is needed to make a snippet")))
+        (error "An active region is needed to make a snippet")))
 
 ;; Key bindings
 
