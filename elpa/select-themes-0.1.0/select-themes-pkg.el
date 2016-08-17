b79b46b0f452bb090cc85ae9c5d7dcfde6384ad8
JasonM23 <jasonm23@gmail.com>
2016-08-17 11:07:04 -0400

    [Removing select-themes versions: 0.1.0,20160220.1706]

--- elpa/select-themes-0.1.0/select-themes-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "select-themes" "0.1.0" "Color theme selection with completeing-read" 'nil :url "https://github.com/jasonm23/emacs-select-themes")
--- elpa/select-themes-20160220.1706/select-themes-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; select-themes-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "select-themes" "select-themes.el" (22218 25973
-;;;;;;  639767 168000))
-;;; Generated autoloads from select-themes.el
-
-(autoload 'select-themes "select-themes" "\
-Interactively select a THEME, from the available custom themes.
-
-You can also select '*Emacs default*' to return to Emacs default theme.
-
-Note: multiple enabled themes cause Emacs to slow down, so we
-disable them before selecting the new theme.
-
-\(fn THEME)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; select-themes-autoloads.el ends here
