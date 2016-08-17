63720f7169f3570621d2145dea5262646c832e36
JasonM23 <jasonm23@gmail.com>
2016-08-17 11:06:53 -0400

    [Updating fountain-mode version: 20160814.608][removing old version: 20160809.735]

--- elpa/fountain-mode-20160814.608/fountain-mode-autoloads.el
+++ elpa/fountain-mode-20160814.608/fountain-mode-autoloads.el
@@ -3,7 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "fountain-mode" "fountain-mode.el" (0 0 0 0))
+;;;### (autoloads nil "fountain-mode" "fountain-mode.el" (22452 31752
+;;;;;;  704914 852000))
 ;;; Generated autoloads from fountain-mode.el
 
 (add-to-list 'auto-mode-alist '("\\.fountain\\'" . fountain-mode))
@@ -14,8 +15,6 @@
 Major mode for screenwriting in Fountain markup.
 
 \(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "fountain-mode" '("fountain-")))
 
 ;;;***
 
---
+++ elpa/fountain-mode-20160814.608/fountain-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fountain-mode" "20160814.608" "Major mode for screenwriting in Fountain markup" '((emacs "24.4")) :url "https://github.com/rnkn/fountain-mode" :keywords '("wp"))
