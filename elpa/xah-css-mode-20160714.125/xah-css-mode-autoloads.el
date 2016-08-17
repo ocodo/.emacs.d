a06ad7cd8e6b639cbe3a8555313c52a03f229992
JasonM23 <jasonm23@gmail.com>
2016-08-17 11:07:09 -0400

    [Removing xah-css-mode version: 20160714.125]

--- elpa/xah-css-mode-20160714.125/xah-css-mode-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; xah-css-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "xah-css-mode" "xah-css-mode.el" (0 0 0 0))
-;;; Generated autoloads from xah-css-mode.el
-
-(autoload 'xah-css-mode "xah-css-mode" "\
-A major mode for CSS.
-
-URL `http://ergoemacs.org/emacs/xah-css-mode.html'
-
-\\{xah-css-mode-map}
-
-\(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "xah-css-mode" '("xah-css-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; xah-css-mode-autoloads.el ends here
--- elpa/xah-css-mode-20160714.125/xah-css-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "xah-css-mode" "20160714.125" "Major mode for editing CSS code." '((emacs "24.3")) :url "http://ergoemacs.org/emacs/xah-css-mode.html" :keywords '("languages" "convenience" "css" "color"))
