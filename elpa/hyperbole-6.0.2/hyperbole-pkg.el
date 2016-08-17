8e5e011537976595b30820ce47917645dfcd1d17
JasonM23 <jasonm23@gmail.com>
2016-08-17 11:06:57 -0400

    [Removing hyperbole version: 6.0.2]

--- elpa/hyperbole-6.0.2/hyperbole-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from hyperbole.el
-(define-package "hyperbole" "6.0.2" "GNU Hyperbole: The Everyday Hypertextual Information Manager" '((emacs "24.4")) :url "http://www.gnu.org/software/hyperbole" :keywords '("comm" "convenience" "files" "frames" "hypermedia" "languages" "mail" "matching" "mouse" "multimedia" "outlines" "tools" "wp"))
