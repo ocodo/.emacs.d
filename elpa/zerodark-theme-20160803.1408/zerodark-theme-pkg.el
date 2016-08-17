88d7e060d3a4d73514d899b370c450694b8c6340
JasonM23 <jasonm23@gmail.com>
2016-08-17 11:07:10 -0400

    [Removing zerodark-theme version: 20160803.1408]

--- elpa/zerodark-theme-20160803.1408/zerodark-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zerodark-theme" "20160803.1408" "A dark, medium contrast theme for Emacs" 'nil :url "https://github.com/NicolasPetton/zerodark-theme" :keywords '("themes"))
