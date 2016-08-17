7abdc555e58e1402090e092bc61182333d771eb9
JasonM23 <jasonm23@gmail.com>
2016-08-17 11:07:09 -0400

    [Removing xah-fly-keys version: 20160809.1324]

--- elpa/xah-fly-keys-20160809.1324/xah-fly-keys-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; xah-fly-keys-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("xah-fly-keys.el") (22444 14733 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; xah-fly-keys-autoloads.el ends here
--- elpa/xah-fly-keys-20160809.1324/xah-fly-keys-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "xah-fly-keys" "20160809.1324" "A efficient modal keybinding set minor mode based on ergonomics." 'nil :url "http://ergoemacs.org/misc/ergoemacs_vi_mode.html" :keywords '("convenience" "emulations" "vim" "ergoemacs"))
