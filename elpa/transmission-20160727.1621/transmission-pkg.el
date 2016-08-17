4d36cf0c8b67af95b101352bee8a7f2765a46b3a
JasonM23 <jasonm23@gmail.com>
2016-08-17 11:07:07 -0400

    [Removing transmission version: 20160727.1621]

--- elpa/transmission-20160727.1621/transmission-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "transmission" "20160727.1621" "Interface to a Transmission session" '((emacs "24.4") (let-alist "1.0.3")) :keywords '("comm" "tools"))
