7fb70dc661a26a8315612b22af0efb7920e3ca51
Pair <pair@pivotal.io>
2016-08-19 10:52:36 +0800

    [Adding bind-key version: 20160227.48]

---
+++ elpa/bind-key-20160227.48/bind-key-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bind-key" "20160227.48" "A simple way to manage personal keybindings" 'nil :url "https://github.com/jwiegley/use-package" :keywords '("keys" "keybinding" "config" "dotemacs"))
