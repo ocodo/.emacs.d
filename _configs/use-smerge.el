5ca936469b16c769e81eb839783f57ced8546b50
JasonM23 <jasonm23@gmail.com>
2016-08-20 10:04:27 -0400

    improve use package init for smerge + add keep all for smerge hydra

--- _configs/use-smerge.el
+++ _configs/use-smerge.el
@@ -4,15 +4,17 @@
 (require 'use-package)
 
 (use-package smerge-mode
-  :init ;; before use
+  :functions smerge-next smerge-prev smerge-keep-all smerge-keep-mine smerge-keep-other
+  :config
   (progn
     (global-set-key (kbd "C-c s")
-                    (defhydra hydra-smerge (:body-pre (smerge-mode 1) :color red)
+                    (defhydra hydra-smerg (:body-pre (smerge-mode 1) :color red)
                       "Smerge mode"
-                      ("<down>" smerge-next "Next conflict")
-                      ("<up>" smerge-prev "Previous conflict")
-                      ("<H-left>" smerge-keep-mine "Keep mine")
-                      ("<H-right>" smerge-keep-other "Keep other")))))
+                      ("<down>" smerge-next        "Next conflict")
+                      ("<up>"   smerge-prev        "Previous conflict")
+                      ("M-a"    smerge-keep-all    "Keep all")
+                      ("M-m"    smerge-keep-mine   "Keep mine")
+                      ("M-o"    smerge-keep-other  "Keep other")))))
 
 (provide 'use-smerge)
 ;;; use-smerge ends here
