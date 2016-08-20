d32f6cfa14ebdc2902fa7148f377a1e861afa153
JasonM23 <jasonm23@gmail.com>
2016-08-20 10:05:02 -0400

    [Adding column-marker version: 20121128.43]

---
+++ elpa/column-marker-20121128.43/column-marker-autoloads.el
@@ -0,0 +1,20 @@
+;;; column-marker-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "column-marker" "column-marker.el" (22456 25289
+;;;;;;  666028 402000))
+;;; Generated autoloads from column-marker.el
+
+(autoload 'column-marker-1 "column-marker" "\
+Highlight a column." t)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; column-marker-autoloads.el ends here
---
+++ elpa/column-marker-20121128.43/column-marker-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "column-marker" "20121128.43" "Highlight certain character columns" 'nil :keywords '("tools" "convenience" "highlight"))
