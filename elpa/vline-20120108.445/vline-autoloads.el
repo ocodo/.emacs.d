6c505017675de29dc37ccfb704ea677aadba40fe
JasonM23 <jasonm23@gmail.com>
2016-08-20 10:05:03 -0400

    [Adding vline version: 20120108.445]

---
+++ elpa/vline-20120108.445/vline-autoloads.el
@@ -0,0 +1,43 @@
+;;; vline-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "vline" "vline.el" (22456 25301 982261 544000))
+;;; Generated autoloads from vline.el
+
+(autoload 'vline-mode "vline" "\
+Display vertical line mode.
+
+\(fn &optional ARG)" t nil)
+
+(defvar vline-global-mode nil "\
+Non-nil if Vline-Global mode is enabled.
+See the `vline-global-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `vline-global-mode'.")
+
+(custom-autoload 'vline-global-mode "vline" nil)
+
+(autoload 'vline-global-mode "vline" "\
+Toggle Vline mode in all buffers.
+With prefix ARG, enable Vline-Global mode if ARG is positive;
+otherwise, disable it.  If called from Lisp, enable the mode if
+ARG is omitted or nil.
+
+Vline mode is enabled in all buffers where
+`(lambda nil (unless (minibufferp) (vline-mode 1)))' would do it.
+See `vline-mode' for more information on Vline mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; vline-autoloads.el ends here
---
+++ elpa/vline-20120108.445/vline-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vline" "20120108.445" "show vertical line (column highlighting) mode." 'nil :url "http://www.emacswiki.org/cgi-bin/wiki/vline.el" :keywords '("faces" "editing" "emulating"))
