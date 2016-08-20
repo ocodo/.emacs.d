22d860444e50272ec8615b28b1b88166a7e8166e
jasonm23 <jasonm23@gmail.com>
2016-08-20 23:49:28 +0800

    [Adding crosshairs version: 20151231.1238]

---
+++ elpa/crosshairs-20151231.1238/crosshairs-autoloads.el
@@ -0,0 +1,88 @@
+;;; crosshairs-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "crosshairs" "crosshairs.el" (22455 52008 0
+;;;;;;  0))
+;;; Generated autoloads from crosshairs.el
+
+(let ((loads (get 'crosshairs 'custom-loads))) (if (member '"crosshairs" loads) nil (put 'crosshairs 'custom-loads (cons '"crosshairs" loads))))
+
+(defvar crosshairs-mode nil "\
+Non-nil if Crosshairs mode is enabled.
+See the `crosshairs-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `crosshairs-mode'.")
+
+(custom-autoload 'crosshairs-mode "crosshairs" nil)
+
+(autoload 'crosshairs-mode "crosshairs" "\
+Toggle highlighting the current line and column.
+With ARG, turn highlighting on if and only if ARG is positive.
+
+\(fn &optional ARG)" t nil)
+
+(defalias 'toggle-crosshairs-when-idle 'crosshairs-toggle-when-idle)
+
+(autoload 'crosshairs-toggle-when-idle "crosshairs" "\
+Toggle highlighting the current line and column when Emacs is idle.
+With prefix argument, turn on if ARG > 0; else turn off.
+You can use commands `col-highlight-set-interval' and
+`hl-line-when-idle-interval' to change the idle times.
+
+\(fn &optional ARG)" t nil)
+
+(defalias 'flash-crosshairs 'crosshairs-flash)
+
+(autoload 'crosshairs-flash "crosshairs" "\
+Highlight the current line and column temporarily.
+Highlight the line for `hl-line-flash-show-period' and the column for
+`column-show-period' seconds.  With prefix argument SECONDS, highlight
+both for SECONDS seconds.
+
+\(fn &optional SECONDS)" t nil)
+
+(autoload 'crosshairs "crosshairs" "\
+Highlight current position with crosshairs.
+With no prefix arg, highlighting turns off at the next command.
+With a prefix arg, highlighting stays on until you toggle it off using
+`crosshairs-mode'.
+
+\(fn &optional MODALP)" t nil)
+
+(autoload 'crosshairs-highlight "crosshairs" "\
+Echo current position and highlight it with crosshairs.
+If optional arg MODE is `line-only', then highlight only the line.
+If optional arg MODE is `col-only', then highlight only the column.
+ Interactively:
+  A non-negative prefix argument uses MODE `line-only'.
+  A negative prefix argument uses MODE `col-only'.
+
+Optional arg NOMSG non-nil means show no message.
+
+If the current buffer is not the same as the value of `orig-buff',
+then indicate the buffer, as well as the position.  Variable
+`orig-buff' is not bound here; if you want to take advantage of this
+feature in your code, then bind it.
+
+Return current position as a marker.
+
+\(fn &optional MODE NOMSG)" t nil)
+
+(autoload 'crosshairs-unhighlight "crosshairs" "\
+Turn off crosshairs highlighting of current position.
+Optional arg nil means do nothing if this event is a frame switch.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; crosshairs-autoloads.el ends here
