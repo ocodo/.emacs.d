87df3c0cd1f513a81460b30ad9cc298fb67c2809
jasonm23 <jasonm23@gmail.com>
2016-08-21 17:41:49 +0800

    Ensure that any makefile isn't "cleaned" (untabified)

--- _configs/use-buffer-clean.el
+++ _configs/use-buffer-clean.el
@@ -10,14 +10,12 @@
 ;; TODO: add a buffer local var via on load hook to flag a file which
 ;; is already tabbed, we should not untabify these.
 
-(defcustom suspend-whitespace-cleanup nil
-  "Suspend buffer cleanup if t")
+(defcustom suspend-whitespace-cleanup nil "Suspend buffer cleanup if t.")
 
 (defun safe-buffer-cleanup ()
   "Clean whitespace, kill tabs, set to UTF8."
   (unless suspend-whitespace-cleanup
-    (unless
-        (eq (with-current-buffer (current-buffer) major-mode) 'makefile-bsdmake-mode)
+    (unless (s-contains-p "makefile" (symbol-name (with-current-buffer (current-buffer) major-mode)))
       (untabify (point-min) (point-max)))
     (delete-trailing-whitespace)
     (set-buffer-file-coding-system 'utf-8)))
