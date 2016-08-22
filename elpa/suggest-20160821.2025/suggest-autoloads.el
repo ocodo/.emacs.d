5b7a8c48b3d4aaf6cb53da95c5e17fb8f147f41d
JasonM23 <jasonm23@gmail.com>
2016-08-22 11:09:14 -0400

    [Updating suggest version: 20160821.2025][removing old version: 20160814.1048]

--- elpa/suggest-20160821.2025/suggest-autoloads.el
+++ elpa/suggest-20160821.2025/suggest-autoloads.el
@@ -3,8 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "suggest" "suggest.el" (22452 31595 604400
-;;;;;;  720000))
+;;;### (autoloads nil "suggest" "suggest.el" (22459 4113 134346 743000))
 ;;; Generated autoloads from suggest.el
 
 (autoload 'suggest "suggest" "\
