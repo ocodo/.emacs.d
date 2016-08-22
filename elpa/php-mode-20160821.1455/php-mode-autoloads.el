7babccfedfa3c66051ca7101fa7871efe6ae546b
JasonM23 <jasonm23@gmail.com>
2016-08-22 11:09:12 -0400

    [Updating php-mode version: 20160821.1455][removing old version: 20160812.2247]

--- elpa/php-mode-20160821.1455/php-mode-autoloads.el
+++ elpa/php-mode-20160821.1455/php-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "php-mode" "php-mode.el" (22452 31620 140482
-;;;;;;  731000))
+;;;### (autoloads nil "php-mode" "php-mode.el" (22459 4137 590306
+;;;;;;  609000))
 ;;; Generated autoloads from php-mode.el
 
 (let ((loads (get 'php 'custom-loads))) (if (member '"php-mode" loads) nil (put 'php 'custom-loads (cons '"php-mode" loads))))
@@ -32,7 +32,7 @@ Major mode for editing PHP code.
 ;;;;;;  "php-exif.el" "php-ext.el" "php-filesystem.el" "php-gd.el"
 ;;;;;;  "php-math.el" "php-mode-pkg.el" "php-pcre.el" "php-regex.el"
 ;;;;;;  "php-simplexml.el" "php-strings.el" "php-var.el" "php-xmlparser.el"
-;;;;;;  "php-xmlreader.el") (22452 31620 164482 811000))
+;;;;;;  "php-xmlreader.el") (22459 4137 602306 589000))
 
 ;;;***
 
