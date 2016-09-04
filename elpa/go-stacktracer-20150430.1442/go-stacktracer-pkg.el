19e43e46dee27190ec88adf588f2185b5650b969
jasonm23 <jasonm23@gmail.com>
2016-09-05 06:48:20 +0800

    [Adding go-stacktracer version: 20150430.1442]

---
+++ elpa/go-stacktracer-20150430.1442/go-stacktracer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-stacktracer" "20150430.1442" "parse Go stack traces" 'nil :url "https://github.com/samertm/go-stacktracer.el" :keywords '("tools"))
