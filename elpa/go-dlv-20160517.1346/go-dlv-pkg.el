a2b7b25a86273ce7859100ad6f9853379b698b42
jasonm23 <jasonm23@gmail.com>
2016-09-05 06:48:18 +0800

    [Adding go-dlv version: 20160517.1346]

---
+++ elpa/go-dlv-20160517.1346/go-dlv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-dlv" "20160517.1346" "Go Delve - Debug Go programs interactively with the GUD." '((go-mode "1.3.1")) :url "https://github.com/benma/go-dlv.el/" :keywords '("go" "debug" "debugger" "delve" "interactive" "gud"))
