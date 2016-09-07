60e5700fa1495892698d8ff4f8d74165b3c1a733
JasonM23 <jasonm23@gmail.com>
2016-09-06 22:47:17 -0400

    [Updating js2-mode version: 20160904.1234][removing old version: 20160707.723]

--- elpa/js2-mode-20160904.1234/js2-mode-autoloads.el
+++ elpa/js2-mode-20160904.1234/js2-mode-autoloads.el
@@ -1,10 +1,10 @@
 ;;; js2-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "js2-imenu-extras" "js2-imenu-extras.el" (22405
-;;;;;;  60867 564501 155000))
+;;;### (autoloads nil "js2-imenu-extras" "js2-imenu-extras.el" (22479
+;;;;;;  30356 152555 162000))
 ;;; Generated autoloads from js2-imenu-extras.el
 
 (autoload 'js2-imenu-extras-setup "js2-imenu-extras" "\
@@ -19,8 +19,8 @@ Toggle Imenu support for frameworks and structural patterns.
 
 ;;;***
 
-;;;### (autoloads nil "js2-mode" "js2-mode.el" (22405 60867 572501
-;;;;;;  142000))
+;;;### (autoloads nil "js2-mode" "js2-mode.el" (22479 30356 156555
+;;;;;;  173000))
 ;;; Generated autoloads from js2-mode.el
 
 (autoload 'js2-highlight-unused-variables-mode "js2-mode" "\
@@ -56,7 +56,7 @@ variables (`sgml-basic-offset' et al) locally, like so:
 ;;;***
 
 ;;;### (autoloads nil nil ("js2-mode-pkg.el" "js2-old-indent.el")
-;;;;;;  (22405 60867 578635 996000))
+;;;;;;  (22479 30356 156555 173000))
 
 ;;;***
 
--- elpa/js2-mode-20160904.1234/js2-mode.el
+++ elpa/js2-mode-20160904.1234/js2-mode.el
@@ -6403,6 +6403,8 @@ its relevant fields and puts it into `js2-ti-tokens'."
         flags
         (continue t)
         (token (js2-new-token 0)))
+    (js2-record-text-property (1- js2-ts-cursor) js2-ts-cursor
+                              'syntax-table (string-to-syntax "\"/"))
     (setq js2-ts-string-buffer nil)
     (if (eq start-tt js2-ASSIGN_DIV)
         ;; mis-scanned /=
@@ -6429,6 +6431,8 @@ its relevant fields and puts it into `js2-ti-tokens'."
             (setq in-class nil)))
           (js2-add-to-string c))))
     (unless err
+      (js2-record-text-property (1- js2-ts-cursor) js2-ts-cursor
+                                'syntax-table (string-to-syntax "\"/"))
       (while continue
         (cond
          ((js2-match-char ?g)
@@ -10430,8 +10434,7 @@ array-literals, array comprehensions and regular expressions."
                                   :len (- end px-pos)
                                   :value (js2-current-token-string)
                                   :flags flags)
-          (js2-set-face px-pos end 'font-lock-string-face 'record)
-          (js2-record-text-property px-pos end 'syntax-table '(2)))))
+          (js2-set-face px-pos end 'font-lock-string-face 'record))))
      ((or (= tt js2-NULL)
           (= tt js2-THIS)
           (= tt js2-SUPER)
@@ -10743,7 +10746,8 @@ If ONLY-OF-P is non-nil, only the 'for (foo of bar)' form is allowed."
                                   :name name
                                   :extends extends
                                   :elems elems))
-    (apply #'js2-node-add-children pn (js2-class-node-elems pn))
+    (apply #'js2-node-add-children
+           pn name extends (js2-class-node-elems pn))
     pn))
 
 (defun js2-parse-object-literal ()
