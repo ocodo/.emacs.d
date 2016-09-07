f13d9558e482f4ad27407a02bf9592928a4bd2f5
JasonM23 <jasonm23@gmail.com>
2016-09-06 22:47:29 -0400

    [Updating sublimity version: 20160822.1856][removing old version: 20160629]

--- elpa/sublimity-20160822.1856/sublimity-autoloads.el
+++ elpa/sublimity-20160822.1856/sublimity-autoloads.el
@@ -1,15 +1,16 @@
 ;;; sublimity-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "sublimity" "sublimity.el" (22396 28862 188073
-;;;;;;  20000))
+;;;### (autoloads nil "sublimity" "sublimity.el" (22479 30222 964161
+;;;;;;  172000))
 ;;; Generated autoloads from sublimity.el
 
 (defvar sublimity-mode nil "\
 Non-nil if Sublimity mode is enabled.
-See the command `sublimity-mode' for a description of this minor mode.
+See the `sublimity-mode' command
+for a description of this minor mode.
 Setting this variable directly does not take effect;
 either customize it (see the info node `Easy Customization')
 or call the function `sublimity-mode'.")
@@ -24,8 +25,8 @@ smooth-scrolling and minimap, like sublime editor
 ;;;***
 
 ;;;### (autoloads nil nil ("sublimity-attractive.el" "sublimity-map.el"
-;;;;;;  "sublimity-pkg.el" "sublimity-scroll.el") (22396 28862 209473
-;;;;;;  153000))
+;;;;;;  "sublimity-pkg.el" "sublimity-scroll.el") (22479 30222 968161
+;;;;;;  185000))
 
 ;;;***
 
