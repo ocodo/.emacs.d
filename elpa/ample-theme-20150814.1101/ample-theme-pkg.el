81d42f6509d93d6e2561d99b3fb606eb8bd7b1dd
JasonM23 <jasonm23@gmail.com>
2016-09-06 22:46:57 -0400

    [Updating ample-theme version: 20160906.643][removing old version: 20150814.1101]

--- elpa/ample-theme-20150814.1101/ample-theme-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "ample-theme" "20150814.1101" "Calm Dark Theme for Emacs" 'nil :url "https://github.com/jordonbiondo/ample-theme" :keywords
-  '("theme" "dark"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/ample-theme-20160906.643/ample-theme-autoloads.el
+++ elpa/ample-theme-20160906.643/ample-theme-autoloads.el
@@ -1,10 +1,10 @@
 ;;; ample-theme-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ample-flat-theme" "ample-flat-theme.el" (21975
-;;;;;;  62560 475401 0))
+;;;### (autoloads nil "ample-flat-theme" "ample-flat-theme.el" (22479
+;;;;;;  32549 197949 905000))
 ;;; Generated autoloads from ample-flat-theme.el
 
 (when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -17,7 +17,7 @@ Apply the ample-flat-theme.
 ;;;***
 
 ;;;### (autoloads nil "ample-light-theme" "ample-light-theme.el"
-;;;;;;  (21975 62560 471401 0))
+;;;;;;  (22479 32549 173949 845000))
 ;;; Generated autoloads from ample-light-theme.el
 
 (when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -29,8 +29,8 @@ Apply the ample-light-theme.
 
 ;;;***
 
-;;;### (autoloads nil "ample-theme" "ample-theme.el" (21975 62560
-;;;;;;  467401 0))
+;;;### (autoloads nil "ample-theme" "ample-theme.el" (22479 32549
+;;;;;;  165949 825000))
 ;;; Generated autoloads from ample-theme.el
 
 (when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -42,8 +42,8 @@ Apply the ample-theme.
 
 ;;;***
 
-;;;### (autoloads nil nil ("ample-theme-pkg.el") (21975 62560 489416
-;;;;;;  524000))
+;;;### (autoloads nil nil ("ample-theme-pkg.el") (22479 32549 201949
+;;;;;;  915000))
 
 ;;;***
 
