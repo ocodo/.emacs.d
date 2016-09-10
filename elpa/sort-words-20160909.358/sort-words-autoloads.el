6bd415a02018788fc8ef46519168358eb85555e3
JasonM23 <jasonm23@gmail.com>
2016-09-09 23:17:12 -0400

    [Adding sort-words version: 20160909.358]

---
+++ elpa/sort-words-20160909.358/sort-words-autoloads.el
@@ -0,0 +1,24 @@
+;;; sort-words-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "sort-words" "sort-words.el" (22483 30716 752112
+;;;;;;  508000))
+;;; Generated autoloads from sort-words.el
+
+(autoload 'sort-words "sort-words" "\
+Sort words in region alphabetically.
+Then insert them replacing the existing region.
+START and END are boundries of the selected region.
+
+\(fn START END)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sort-words-autoloads.el ends here
---
+++ elpa/sort-words-20160909.358/sort-words-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sort-words" "20160909.358" "Sort words in a selected region" 'nil :url "http://github.org/dotemacs/sort-words.el" :keywords '("tools"))
