90577a531b15fb865761db41a80b71b26de5f806
JasonM23 <jasonm23@gmail.com>
2016-09-09 23:17:14 -0400

    [Adding white-theme version: 20160909.1400]

---
+++ elpa/white-theme-20160909.1400/white-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; white-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "white-theme" "white-theme.el" (22483 30715
+;;;;;;  164110 916000))
+;;; Generated autoloads from white-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; white-theme-autoloads.el ends here
