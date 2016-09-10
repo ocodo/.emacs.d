ed671e1ec8d29016a83bf51a239e5aad5ae9cb48
JasonM23 <jasonm23@gmail.com>
2016-09-09 23:17:06 -0400

    [Updating google-translate version: 20160909.1358][removing old version: 20160824.1707]

--- elpa/google-translate-20160824.1707/google-translate-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "google-translate" "20160824.1707" "Emacs interface to Google Translate." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/google-translate-20160909.1358/google-translate-pkg.el
@@ -0,0 +1,4 @@
+(define-package "google-translate" "20160909.1358" "Emacs interface to Google Translate." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
