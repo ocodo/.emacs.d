8993cdf1b44cd01e2fe36094aabf82414ee0ea48
jasonm23 <jasonm23@gmail.com>
2016-09-24 12:58:41 +0800

    Add embrace to the smartparens binding collection
    - (C-x r [c d a] | [change delete add] )

--- _configs/use-smartparens.el
+++ _configs/use-smartparens.el
@@ -1,4 +1,8 @@
 ;; use-smartparens
+;;; Code:
+
+(require 'smartparens)
+
 (smartparens-global-mode t)
 
 (unless (fboundp 'cua-replace-region)
@@ -43,12 +47,15 @@
 (sp-local-pair 'ruby-mode "|" "|" :wrap "C-c |")
 
 ;; Smartparens slurp / barf - use instead of paredit
-(global-set-key (kbd "C-x r <left>")        'sp-forward-slurp-sexp)
-(global-set-key (kbd "C-x r <right>")       'sp-forward-barf-sexp)
-(global-set-key (kbd "C-x a <left>")        'sp-backward-slurp-sexp)
-(global-set-key (kbd "C-x a <right>")       'sp-backward-barf-sexp)
-(global-set-key (kbd "C-x r r")             'sp-rewrap-sexp)
-(global-set-key (kbd "C-x r s")             'sp-split-sexp)
-(global-set-key (kbd "C-x r j")             'sp-join-sexp)
+(global-set-key (kbd "C-x r <left>") 'sp-forward-slurp-sexp)
+(global-set-key (kbd "C-x r <right>") 'sp-forward-barf-sexp)
+(global-set-key (kbd "C-x r <S-left>") 'sp-backward-slurp-sexp)
+(global-set-key (kbd "C-x r <S-right>") 'sp-backward-barf-sexp)
+(global-set-key (kbd "C-x r r") 'sp-rewrap-sexp)
+(global-set-key (kbd "C-x r s") 'sp-split-sexp)
+(global-set-key (kbd "C-x r j") 'sp-join-sexp)
+(global-set-key (kbd "C-x r d") 'embrace-delete)
+(global-set-key (kbd "C-x r a") 'embrace-add)
+(global-set-key (kbd "C-x r c") 'embrace-change)
 
 (provide 'use-smartparens)
