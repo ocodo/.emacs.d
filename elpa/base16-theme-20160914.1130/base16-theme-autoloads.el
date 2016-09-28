5911e48d7bc475218669fc92d8ace89cae1d215a
JasonM23 <jasonm23@gmail.com>
2016-09-28 04:23:35 -0400

    [Updating base16-theme version: 20160914.1130][removing old version: 20160907.1606]

--- elpa/base16-theme-20160914.1130/base16-theme-autoloads.el
+++ elpa/base16-theme-20160914.1130/base16-theme-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "base16-theme" "base16-theme.el" (22483 30704
-;;;;;;  336099 972000))
+;;;### (autoloads nil "base16-theme" "base16-theme.el" (22507 31611
+;;;;;;  289229 202000))
 ;;; Generated autoloads from base16-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -36,7 +36,7 @@
 ;;;;;;  "base16-summerfruit-dark-theme.el" "base16-summerfruit-light-theme.el"
 ;;;;;;  "base16-theme-pkg.el" "base16-tomorrow-night-theme.el" "base16-tomorrow-theme.el"
 ;;;;;;  "base16-twilight-theme.el" "base16-unikitty-dark-theme.el"
-;;;;;;  "base16-unikitty-light-theme.el") (22483 30704 452100 90000))
+;;;;;;  "base16-unikitty-light-theme.el") (22507 31611 381229 202000))
 
 ;;;***
 
