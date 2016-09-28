d765cb789246f8a56544640add2f19a40c98430e
JasonM23 <jasonm23@gmail.com>
2016-09-28 04:24:08 -0400

    [Updating smartparens version: 20160926.922][removing old version: 20160828.1136]

--- elpa/smartparens-20160926.922/smartparens-autoloads.el
+++ elpa/smartparens-20160926.922/smartparens-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "smartparens" "smartparens.el" (22479 30225
-;;;;;;  976170 841000))
+;;;### (autoloads nil "smartparens" "smartparens.el" (22507 31326
+;;;;;;  17226 247000))
 ;;; Generated autoloads from smartparens.el
 
 (autoload 'sp-cheat-sheet "smartparens" "\
@@ -182,10 +182,11 @@ Turn off `show-smartparens-mode'.
 ;;;***
 
 ;;;### (autoloads nil nil ("smartparens-clojure.el" "smartparens-config.el"
-;;;;;;  "smartparens-haskell.el" "smartparens-html.el" "smartparens-latex.el"
-;;;;;;  "smartparens-lua.el" "smartparens-pkg.el" "smartparens-python.el"
-;;;;;;  "smartparens-racket.el" "smartparens-ruby.el" "smartparens-rust.el"
-;;;;;;  "smartparens-scala.el") (22479 30225 996170 905000))
+;;;;;;  "smartparens-ess.el" "smartparens-haskell.el" "smartparens-html.el"
+;;;;;;  "smartparens-latex.el" "smartparens-lua.el" "smartparens-pkg.el"
+;;;;;;  "smartparens-python.el" "smartparens-racket.el" "smartparens-ruby.el"
+;;;;;;  "smartparens-rust.el" "smartparens-scala.el") (22507 31326
+;;;;;;  29226 248000))
 
 ;;;***
 
