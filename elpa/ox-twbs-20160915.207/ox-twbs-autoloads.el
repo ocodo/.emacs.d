5d1641613bc213df6c1a6145908dbe88254cc2df
JasonM23 <jasonm23@gmail.com>
2016-09-28 04:24:03 -0400

    [Updating ox-twbs version: 20160915.207][removing old version: 20160820.604]

--- elpa/ox-twbs-20160915.207/ox-twbs-autoloads.el
+++ elpa/ox-twbs-20160915.207/ox-twbs-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ox-twbs" "ox-twbs.el" (22459 4149 670287 56000))
+;;;### (autoloads nil "ox-twbs" "ox-twbs.el" (22507 31365 61226 987000))
 ;;; Generated autoloads from ox-twbs.el
 
 (put 'org-twbs-head-include-default-style 'safe-local-variable 'booleanp)
---
+++ elpa/ox-twbs-20160915.207/ox-twbs-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ox-twbs" "20160915.207" "Bootstrap compatible HTML Back-End for Org" 'nil :url "https://github.com/marsmining/ox-twbs" :keywords '("org" "html" "publish" "twitter" "bootstrap"))
