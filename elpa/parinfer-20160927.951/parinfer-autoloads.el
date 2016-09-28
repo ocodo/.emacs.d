37d939850cc9c79a3765c3d4cad7754ead88c3c5
JasonM23 <jasonm23@gmail.com>
2016-09-28 04:24:04 -0400

    [Adding parinfer version: 20160927.951]

---
+++ elpa/parinfer-20160927.951/parinfer-autoloads.el
@@ -0,0 +1,32 @@
+;;; parinfer-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "parinfer" "parinfer.el" (22507 31237 805224
+;;;;;;  84000))
+;;; Generated autoloads from parinfer.el
+
+(autoload 'parinfer-mode "parinfer" "\
+Parinfer mode.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'parinfer-region-mode "parinfer" "\
+Available when region is active.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("parinfer-pkg.el" "parinferlib.el") (22507
+;;;;;;  31237 809224 84000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; parinfer-autoloads.el ends here
---
+++ elpa/parinfer-20160927.951/parinfer-pkg.el
@@ -0,0 +1,9 @@
+(define-package "parinfer" "20160927.951" "Simpler Lisp editing"
+  '((paredit "24")
+    (aggressive-indent "1.8.1")
+    (cl-lib "0.5"))
+  :url "https://github.com/DogLooksGood/parinfer-mode" :keywords
+  '("parinfer"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
