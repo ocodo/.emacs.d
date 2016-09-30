2c2eaa89ca3b0d5031b26a2e5c4169b1d7f7a753
Jason Milkins <jasonm23@gmail.com>
2016-09-30 22:20:14 +0800

    Add config for move-text

---
+++ _activated/use-move-text.el
@@ -0,0 +1,1 @@
+../_configs/use-move-text.el
---
+++ _configs/use-move-text.el
@@ -0,0 +1,12 @@
+;;; use-move-text --- use move-text
+;;; Commentary:
+;;; Code:
+(require 'use-package)
+
+(use-package move-text
+  :config
+  (move-text-default-bindings))
+
+(provide 'use-move-text)
+
+;;; use-move-text ends here
