7470712345d3a7b6eba07ef77006ea4be7ad0469
jasonm23 <jasonm23@gmail.com>
2016-10-01 12:02:30 +0800

    Move duplicate-current-line-or-region to handy-functions

--- custom/handy-functions.el
+++ custom/handy-functions.el
@@ -842,6 +842,51 @@ If your're in the minibuffer it will use the other buffer file name."
             (message "No files to open")))
       (error "Not in a git repository"))))
 
+(defun duplicate-current-line-or-region-up (arg)
+  "Duplicates the current line or region up ARG times."
+  (interactive "p")
+  (duplicate-current-line-or-region arg t))
+
+;; Originally swiped from rejeep's emacs.d rejeep-defuns.el.
+(defun duplicate-current-line-or-region (arg &optional up)
+  "Duplicates the current line or region ARG times.
+
+If UP is non-nil, duplicate and move point to the top."
+  (interactive "p")
+  (let (beg
+        end
+        (origin (point))
+        (saved-region
+         (when (use-region-p) (list (region-beginning) (region-end)))))
+    (if (and (use-region-p) (> (point) (mark)))
+        (exchange-point-and-mark))
+    (setq beg (line-beginning-position))
+    (if (use-region-p)
+        (exchange-point-and-mark))
+    (setq end (line-end-position))
+    (let ((region (buffer-substring-no-properties beg end)))
+      (dotimes (i arg)
+        (goto-char end)
+        (newline)
+        (insert region)
+        (setq end (point)))
+      (if up
+          (goto-char origin)
+        (goto-char (+ origin (* (length region) arg) arg)))
+      (when saved-region
+        (if up
+            (progn (message "setting region (up)")
+                   (push-mark-command nil)
+                   (goto-char (second saved-region))
+                   (exchange-point-and-mark))
+          (progn (message "setting region")
+                 (push-mark-command nil)
+                 (goto-char (- (point) (length region)))))
+        (setq deactivate-mark nil)))))
+
+(global-set-key (kbd "<s-up>") 'duplicate-current-line-or-region-up)
+(global-set-key (kbd "<s-down>") 'duplicate-current-line-or-region)
+
 ;; Key bindings
 (global-set-key (kbd "C-c M-=")   'increase-default-font-height)
 (global-set-key (kbd "C-c M--")   'decrease-default-font-height)
--- custom/quick-funcs.el
+++ custom/quick-funcs.el
@@ -17,51 +17,6 @@
 
 (global-set-key (kbd "C-a") 'smart-beginning-of-line)
 
-(defun duplicate-current-line-or-region-up (arg)
-  "Duplicates the current line or region up ARG times."
-  (interactive "p")
-  (duplicate-current-line-or-region arg t))
-
-;; Originally swiped from rejeep's emacs.d rejeep-defuns.el.
-(defun duplicate-current-line-or-region (arg &optional up)
-  "Duplicates the current line or region ARG times.
-
-If UP is non-nil, duplicate and move point to the top."
-  (interactive "p")
-  (let (beg
-        end
-        (origin (point))
-        (saved-region
-         (when (use-region-p) (list (region-beginning) (region-end)))))
-    (if (and (use-region-p) (> (point) (mark)))
-        (exchange-point-and-mark))
-    (setq beg (line-beginning-position))
-    (if (use-region-p)
-        (exchange-point-and-mark))
-    (setq end (line-end-position))
-    (let ((region (buffer-substring-no-properties beg end)))
-      (dotimes (i arg)
-        (goto-char end)
-        (newline)
-        (insert region)
-        (setq end (point)))
-      (if up
-          (goto-char origin)
-        (goto-char (+ origin (* (length region) arg) arg)))
-      (when saved-region
-        (if up
-            (progn (message "setting region (up)")
-                   (push-mark-command nil)
-                   (goto-char (second saved-region))
-                   (exchange-point-and-mark))
-          (progn (message "setting region")
-                 (push-mark-command nil)
-                 (goto-char (- (point) (length region)))))
-        (setq deactivate-mark nil)))))
-
-(global-set-key (kbd "<s-up>") 'duplicate-current-line-or-region-up)
-(global-set-key (kbd "<s-down>") 'duplicate-current-line-or-region)
-
 (provide 'quick-funcs)
 
 ;;; quick-funcs.el ends here
