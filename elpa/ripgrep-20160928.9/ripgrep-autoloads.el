1d3e813bfe8b6b458d8ebb3708eb45c8fc2a2827
JasonM23 <jasonm23@gmail.com>
2016-10-02 20:42:43 -0400

    [Adding ripgrep version: 20160928.9]

---
+++ elpa/ripgrep-20160928.9/ripgrep-autoloads.el
@@ -0,0 +1,23 @@
+;;; ripgrep-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "ripgrep" "ripgrep.el" (22513 42759 151401
+;;;;;;  261000))
+;;; Generated autoloads from ripgrep.el
+
+(autoload 'ripgrep-regexp "ripgrep" "\
+Run a ripgrep search with `REGEXP' rooted at `DIRECTORY'.
+`ARGS' provides Ripgrep command line arguments.
+
+\(fn REGEXP DIRECTORY &optional ARGS)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ripgrep-autoloads.el ends here
---
+++ elpa/ripgrep-20160928.9/ripgrep-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ripgrep" "20160928.9" "Front-end for ripgrep, a command line search tool" 'nil :url "https://github.com/nlamirault/ripgrep.el" :keywords '("ripgrep" "ack" "pt" "ag" "sift" "grep" "search"))
