cafde5dc4fdcd3d0f4d7d1312438d230375e22ef
JasonM23 <jasonm23@gmail.com>
2016-10-07 23:21:38 -0400

    [Adding easy-kill version: 20151030.2229]

---
+++ elpa/easy-kill-20151030.2229/easy-kill-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "easy-kill" "20151030.2229" "kill & mark things easily" '((emacs "24") (cl-lib "0.5")) :url "https://github.com/leoliu/easy-kill" :keywords '("killing" "convenience"))
