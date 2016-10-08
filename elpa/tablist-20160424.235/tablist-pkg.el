269907836205873ee99938bdcf01d8bb2d3b985a
JasonM23 <jasonm23@gmail.com>
2016-10-07 23:22:05 -0400

    [Adding tablist version: 20160424.235]

---
+++ elpa/tablist-20160424.235/tablist-pkg.el
@@ -0,0 +1,7 @@
+(define-package "tablist" "20160424.235" "Extended tabulated-list-mode"
+  '((emacs "24.3"))
+  :keywords
+  '("extensions" "lisp"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
