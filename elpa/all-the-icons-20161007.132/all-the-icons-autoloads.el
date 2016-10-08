e21e45804eb60ccb6e4e4369fdfdfcde50c19c1c
JasonM23 <jasonm23@gmail.com>
2016-10-07 23:21:28 -0400

    [Updating all-the-icons version: 20161007.132][removing old version: 20160921.935]

--- elpa/all-the-icons-20161007.132/all-the-icons-autoloads.el
+++ elpa/all-the-icons-20161007.132/all-the-icons-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("all-the-icons-faces.el" "all-the-icons-pkg.el"
-;;;;;;  "all-the-icons.el") (22507 31633 777229 231000))
+;;;;;;  "all-the-icons.el") (22519 51861 904545 200000))
 
 ;;;***
 
--- elpa/all-the-icons-20161007.132/all-the-icons-pkg.el
+++ elpa/all-the-icons-20161007.132/all-the-icons-pkg.el
@@ -1,6 +1,7 @@
-(define-package "all-the-icons" "20160921.935" "A library for inserting Developer icons"
+(define-package "all-the-icons" "20161007.132" "A library for inserting Developer icons"
   '((dash "2.12.0")
-    (emacs "24.3"))
+    (emacs "24.3")
+    (font-lock+ "0"))
   :url "https://github.com/domtronn/all-the-icons.el" :keywords
   '("convenient" "lisp"))
 ;; Local Variables:
