398c012ee9c86552f78cfb1cf0f741b2b65fa115
JasonM23 <jasonm23@gmail.com>
2016-10-07 23:21:34 -0400

    [Updating counsel version: 20161005.850][removing old version: 20160926.812]

--- elpa/counsel-20161005.850/counsel-autoloads.el
+++ elpa/counsel-20161005.850/counsel-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "counsel" "counsel.el" (22507 31590 205229
-;;;;;;  153000))
+;;;### (autoloads nil "counsel" "counsel.el" (22519 51821 256487
+;;;;;;  721000))
 ;;; Generated autoloads from counsel.el
 
 (autoload 'counsel-el "counsel" "\
--- elpa/counsel-20161005.850/counsel.el
+++ elpa/counsel-20161005.850/counsel.el
@@ -4,7 +4,7 @@
 
 ;; Author: Oleh Krehel <ohwoeowho@gmail.com>
 ;; URL: https://github.com/abo-abo/swiper
-;; Package-Version: 20160926.812
+;; Package-Version: 20161005.850
 ;; Version: 0.8.0
 ;; Package-Requires: ((emacs "24.3") (swiper "0.8.0"))
 ;; Keywords: completion, matching
@@ -1361,11 +1361,13 @@ When INITIAL-INPUT is non-nil, use it in the minibuffer during completion."
           (file-name-as-directory (file-name-nondirectory dir-file-name)))))
 
 (defun counsel-at-git-issue-p ()
-  "Whe point is at an issue in a Git-versioned file, return the issue string."
+  "When point is at an issue in a Git-versioned file, return the issue string."
   (and (looking-at "#[0-9]+")
        (or
         (eq (vc-backend (buffer-file-name)) 'Git)
-        (memq major-mode '(magit-commit-mode)))
+        (or
+         (memq major-mode '(magit-commit-mode))
+         (bound-and-true-p magit-commit-mode)))
        (match-string-no-properties 0)))
 
 (defun counsel-github-url-p ()
@@ -1410,8 +1412,12 @@ When INITIAL-INPUT is non-nil, use it in the minibuffer during completion."
   (ivy-read "Recentf: " recentf-list
             :action (lambda (f)
                       (with-ivy-window
-                        (find-file f)))
+                       (find-file f)))
             :caller 'counsel-recentf))
+(ivy-set-actions
+ 'counsel-recentf
+ '(("j" find-file-other-window "other-window")
+   ("x" counsel-find-file-extern "open externally")))
 
 ;;** `counsel-locate'
 (defcustom counsel-locate-cmd (cond ((eq system-type 'darwin)
@@ -1699,7 +1705,8 @@ the command."
                   (setq ivy--old-re
                         (ivy--regex string)))))
       (counsel--async-command
-       (format counsel-grep-base-command regex counsel--git-grep-dir))
+       (format counsel-grep-base-command regex
+               (shell-quote-argument counsel--git-grep-dir)))
       nil)))
 
 (defun counsel-grep-action (x)
@@ -2299,7 +2306,7 @@ And insert it into the minibuffer. Useful during
                      (lookup-key keymap (kbd (nth 0 x)))))
                   heads)))
     (ivy-read "head: " head-names
-              :action #'call-interactively)
+              :action (lambda (x) (call-interactively (cdr x))))
     (hydra-keyboard-quit)))
 ;;** `counsel-semantic'
 (declare-function semantic-tag-start "tag")
