0a6a23712fb0bc6f24546c195aacea5ec915e2c8
JasonM23 <jasonm23@gmail.com>
2016-10-07 23:21:44 -0400

    [Adding font-lock+ version: 20151231.1319]

---
+++ elpa/font-lock+-20151231.1319/font-lock+-autoloads.el
@@ -0,0 +1,15 @@
+;;; font-lock+-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("font-lock+.el") (22519 51859 644542 3000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; font-lock+-autoloads.el ends here
---
+++ elpa/font-lock+-20151231.1319/font-lock+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "font-lock+" "20151231.1319" "Enhancements to standard library `font-lock.el'." 'nil :url "http://www.emacswiki.org/font-lock+.el" :keywords '("languages" "faces" "highlighting"))
