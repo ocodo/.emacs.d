2415db3f1a23a6a534fd1a22378979fa2897acd8
JasonM23 <jasonm23@gmail.com>
2016-10-07 23:21:36 -0400

    [Adding dired-ranger version: 20160924.335]

---
+++ elpa/dired-ranger-20160924.335/dired-ranger-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-ranger" "20160924.335" "Implementation of useful ranger features for dired" '((dash "2.7.0") (dired-hacks-utils "0.0.1")) :keywords '("files"))
