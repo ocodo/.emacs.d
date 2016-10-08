d83497854deb2a91e9aa10b06109ef090be7ad7f
JasonM23 <jasonm23@gmail.com>
2016-10-07 23:21:49 -0400

    [Updating js2-refactor version: 20161003.1225][removing old version: 20160920.159]

--- elpa/js2-refactor-20161003.1225/js2-refactor-autoloads.el
+++ elpa/js2-refactor-20161003.1225/js2-refactor-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "js2-refactor" "js2-refactor.el" (22507 31460
-;;;;;;  693228 304000))
+;;;### (autoloads nil "js2-refactor" "js2-refactor.el" (22519 51739
+;;;;;;  932372 895000))
 ;;; Generated autoloads from js2-refactor.el
 
 (autoload 'js2-refactor-mode "js2-refactor" "\
@@ -24,8 +24,8 @@ Add js2r keybindings using the modifier MODIFIER.
 
 ;;;***
 
-;;;### (autoloads nil "js2r-vars" "js2r-vars.el" (22507 31460 681228
-;;;;;;  304000))
+;;;### (autoloads nil "js2r-vars" "js2r-vars.el" (22519 51739 908372
+;;;;;;  861000))
 ;;; Generated autoloads from js2r-vars.el
 
 (autoload 'js2r-extract-var "js2r-vars" "\
@@ -38,7 +38,7 @@ Add js2r keybindings using the modifier MODIFIER.
 ;;;### (autoloads nil nil ("js2-refactor-pkg.el" "js2r-conditionals.el"
 ;;;;;;  "js2r-conveniences.el" "js2r-formatting.el" "js2r-functions.el"
 ;;;;;;  "js2r-helpers.el" "js2r-iife.el" "js2r-paredit.el" "js2r-wrapping.el")
-;;;;;;  (22507 31460 693228 304000))
+;;;;;;  (22519 51739 920372 878000))
 
 ;;;***
 
