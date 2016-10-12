7cdedaacf4b8905cb35d6b5aa253972df290fd9d
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:24 -0400

    [Removing swift-mode version: 20161002.506]

--- elpa/swift-mode-20161002.506/swift-mode-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "swift-mode" "20161002.506" "Major-mode for Apple's Swift programming language."
-  '((emacs "24.4"))
-  :url "https://github.com/swift-emacs/swift-mode" :keywords
-  '("languages" "swift"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
