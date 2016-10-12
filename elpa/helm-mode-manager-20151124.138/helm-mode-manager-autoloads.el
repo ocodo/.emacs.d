e06ae5088e3c76ee905d615ba2bd1aeb35e89395
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:17 -0400

    [Adding helm-mode-manager version: 20151124.138]

---
+++ elpa/helm-mode-manager-20151124.138/helm-mode-manager-autoloads.el
@@ -0,0 +1,34 @@
+;;; helm-mode-manager-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "helm-mode-manager" "helm-mode-manager.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from helm-mode-manager.el
+
+(autoload 'helm-enable-minor-mode "helm-mode-manager" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-disable-minor-mode "helm-mode-manager" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-switch-major-mode "helm-mode-manager" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-mode-manager" '("helm-mode-manager-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-mode-manager-autoloads.el ends here
