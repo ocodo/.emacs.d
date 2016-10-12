5d51dff09d086eaa6851e599726f662e4cfeb359
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:23 -0400

    [Adding signal version: 20160816.738]

---
+++ elpa/signal-20160816.738/signal-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "signal" "20160816.738" "Advanced hook" '((emacs "24") (cl-lib "0.5")) :url "https://github.com/mola-T/signal" :keywords '("internal" "lisp" "processes" "tools"))
