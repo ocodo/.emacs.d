480d48fd95762e2c9440dbd2d4d6557528ed0d14
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:25 -0400

    [Removing vdirel version: 20151215.2255]

--- elpa/vdirel-20151215.2255/vdirel-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "vdirel" "20151215.2255" "Manipulate vdir (i.e., vCard) repositories" '((emacs "24.4") (org-vcard "0.1.0") (helm "1.7.0") (seq "1.11")) :keywords '("vdirsyncer" "vdir" "vcard" "carddav" "contact" "addressbook" "helm"))
