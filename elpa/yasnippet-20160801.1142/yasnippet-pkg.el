db05cd261878e39c0df0feef292512121ebdd6e6
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:26 -0400

    [Removing yasnippet version: 20160801.1142]

--- elpa/yasnippet-20160801.1142/yasnippet-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "yasnippet" "20160801.1142" "Yet another snippet extension for Emacs."
-  '((cl-lib "0.5"))
-  :url "http://github.com/capitaomorte/yasnippet" :keywords
-  '("convenience" "emulation"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
