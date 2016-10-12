9621c96072a5f1a39f2f7bdde38c9dc2971156fb
Jason Milkins <jasonm23@gmail.com>
2016-10-12 21:41:17 +0800

    Configure Dirsubtree

---
+++ _configs/use-dired-subtree.el
@@ -0,0 +1,5 @@
+(use-package dired-subtree :ensure t
+  :after dired
+  :config
+  (bind-key "<tab>" #'dired-subtree-toggle dired-mode-map)
+  (bind-key "<backtab>" #'dired-subtree-cycle dired-mode-map))
