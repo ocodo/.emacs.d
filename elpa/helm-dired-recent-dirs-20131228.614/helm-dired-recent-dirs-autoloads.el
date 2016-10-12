1bcf8a349e261e3d2ef5ae4642da07973ffd906a
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:15 -0400

    [Adding helm-dired-recent-dirs version: 20131228.614]

---
+++ elpa/helm-dired-recent-dirs-20131228.614/helm-dired-recent-dirs-autoloads.el
@@ -0,0 +1,24 @@
+;;; helm-dired-recent-dirs-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "helm-dired-recent-dirs" "helm-dired-recent-dirs.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from helm-dired-recent-dirs.el
+
+(autoload 'helm-dired-recent-dirs-view "helm-dired-recent-dirs" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-dired-recent-dirs" '("helm-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-dired-recent-dirs-autoloads.el ends here
---
+++ elpa/helm-dired-recent-dirs-20131228.614/helm-dired-recent-dirs-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-dired-recent-dirs" "20131228.614" "Show recent dirs with helm.el support." '((helm "1.0")) :keywords '("helm" "dired" "zsh"))
