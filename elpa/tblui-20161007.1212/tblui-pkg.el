9f9b40f725469c4a5c581be5d27788c937e4d73f
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:04:55 -0400

    [Adding tblui version: 20161007.1212]

---
+++ elpa/tblui-20161007.1212/tblui-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tblui" "20161007.1212" "Define tabulated list UI easily" '((dash "2.12.1") (magit-popup "2.6.0") (tablist "0.70") (cl-lib "0.5")) :url "https://github.com/Yuki-Inoue/tblui.el")
