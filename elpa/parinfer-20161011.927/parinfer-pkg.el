213fd9c0bb5638c432b0dd5508999d90c84d2cc1
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:04:53 -0400

    [Adding parinfer version: 20161011.927]

---
+++ elpa/parinfer-20161011.927/parinfer-pkg.el
@@ -0,0 +1,8 @@
+(define-package "parinfer" "20161011.927" "Simpler Lisp editing"
+  '((dash "2.13.0")
+    (cl-lib "0.5"))
+  :url "https://github.com/DogLooksGood/parinfer-mode" :keywords
+  '("parinfer"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
