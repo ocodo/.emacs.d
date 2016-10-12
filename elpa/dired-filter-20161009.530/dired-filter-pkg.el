f5eaed76380dc2a7c706480ff25a44c9adda8af5
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:10 -0400

    [Adding dired-filter version: 20161009.530]

---
+++ elpa/dired-filter-20161009.530/dired-filter-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-filter" "20161009.530" "Ibuffer-like filtering for dired" '((dash "2.10.0") (dired-hacks-utils "0.0.1") (f "0.17.0") (cl-lib "0.3")) :keywords '("files"))
