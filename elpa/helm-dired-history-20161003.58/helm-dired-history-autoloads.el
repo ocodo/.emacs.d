226ee4390870d14423cceb2770d03d65d1d6cc64
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:15 -0400

    [Adding helm-dired-history version: 20161003.58]

---
+++ elpa/helm-dired-history-20161003.58/helm-dired-history-autoloads.el
@@ -0,0 +1,24 @@
+;;; helm-dired-history-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "helm-dired-history" "helm-dired-history.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from helm-dired-history.el
+
+(autoload 'helm-dired-history-view "helm-dired-history" "\
+call `helm' to show dired history.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-dired-history" '("helm-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-dired-history-autoloads.el ends here
---
+++ elpa/helm-dired-history-20161003.58/helm-dired-history-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-dired-history" "20161003.58" "Show dired history with helm.el support." '((helm "1.9.8") (cl-lib "0.5")) :url "https://github.com/jixiuf/helm-dired-history" :keywords '("helm" "dired history"))
