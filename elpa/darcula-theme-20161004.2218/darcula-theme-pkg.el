0a2c3a1fb3bde373c93a9f0cd4425818c04f84fc
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:09 -0400

    [Adding darcula-theme version: 20161004.2218]

---
+++ elpa/darcula-theme-20161004.2218/darcula-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "darcula-theme" "20161004.2218" "Inspired by IntelliJ's Darcula theme" 'nil :url "https://github.com/fommil/darcula-theme-emacs" :keywords '("faces"))
