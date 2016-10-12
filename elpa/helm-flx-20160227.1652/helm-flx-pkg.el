1359b0bf1a686e2db98203d3054acc4e8d4bdff0
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:15 -0400

    [Adding helm-flx version: 20160227.1652]

---
+++ elpa/helm-flx-20160227.1652/helm-flx-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-flx" "20160227.1652" "Sort helm candidates by flx score" '((emacs "24.4") (helm "1.7.9") (flx "0.5")) :url "https://github.com/PythonNut/helm-flx" :keywords '("convenience" "helm" "fuzzy" "flx"))
