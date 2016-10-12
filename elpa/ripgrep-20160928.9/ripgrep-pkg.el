c97a822ac3e9c6c8ec5fb32eb35c718322576a51
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:22 -0400

    [Removing ripgrep version: 20160928.9]

--- elpa/ripgrep-20160928.9/ripgrep-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ripgrep" "20160928.9" "Front-end for ripgrep, a command line search tool" 'nil :url "https://github.com/nlamirault/ripgrep.el" :keywords '("ripgrep" "ack" "pt" "ag" "sift" "grep" "search"))
