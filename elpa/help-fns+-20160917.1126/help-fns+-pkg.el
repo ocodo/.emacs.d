fa902980153ca3ca0c699131b04c01db61e0ddd0
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:18 -0400

    [Adding help-fns+ version: 20160917.1126]

---
+++ elpa/help-fns+-20160917.1126/help-fns+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "help-fns+" "20160917.1126" "Extensions to `help-fns.el'." 'nil :url "http://www.emacswiki.org/help-fns+.el" :keywords '("help" "faces" "characters" "packages" "description"))
