478a563a2946041b1116aea1cb0d31153533bcb3
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:22 -0400

    [Removing parinfer version: 20161007.342]

--- elpa/parinfer-20161007.342/parinfer-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "parinfer" "20161007.342" "Simpler Lisp editing"
-  '((dash "2.13.0")
-    (aggressive-indent "1.8.1")
-    (cl-lib "0.5"))
-  :url "https://github.com/DogLooksGood/parinfer-mode" :keywords
-  '("parinfer"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
