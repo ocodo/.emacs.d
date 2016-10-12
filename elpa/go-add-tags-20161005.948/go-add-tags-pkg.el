77540be3162e36d6b06d997d9af3b4bef2253c28
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:12 -0400

    [Adding go-add-tags version: 20161005.948]

---
+++ elpa/go-add-tags-20161005.948/go-add-tags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-add-tags" "20161005.948" "Add field tags for struct fields" '((emacs "24") (s "1.11.0") (cl-lib "0.5")) :url "https://github.com/syohex/emacs-go-add-tags")
