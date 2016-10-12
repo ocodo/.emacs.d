d9a2608e4bed0f27421544f7327536bc2b3b7f4c
jasonm23 <jasonm23@gmail.com>
2016-10-12 10:21:05 +0800

    [Removing helm-package version: 20160917.2232]

--- elpa/helm-package-20160917.2232/helm-package-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; helm-package-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "helm-package" "helm-package.el" (22507 31495
-;;;;;;  293228 630000))
-;;; Generated autoloads from helm-package.el
-
-(autoload 'helm-package "helm-package" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-package-autoloads.el ends here
--- elpa/helm-package-20160917.2232/helm-package-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-package" "20160917.2232" "Listing ELPA packages with helm interface" '((helm "2.0") (emacs "24.4")) :url "https://github.com/syohex/emacs-helm-package")
