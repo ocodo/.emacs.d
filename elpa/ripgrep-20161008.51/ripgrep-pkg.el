2abf9d980c5180114dc3035504c3726601b8a2a8
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:04:54 -0400

    [Adding ripgrep version: 20161008.51]

---
+++ elpa/ripgrep-20161008.51/ripgrep-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ripgrep" "20161008.51" "Front-end for ripgrep, a command line search tool" 'nil :url "https://github.com/nlamirault/ripgrep.el" :keywords '("ripgrep" "ack" "pt" "ag" "sift" "grep" "search"))
