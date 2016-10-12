1da9f0ed584e7aea2295ccde4b14d0ee4dce9d1c
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:24 -0400

    [Removing tblui version: 20161007.819]

--- elpa/tblui-20161007.819/tblui-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "tblui" "20161007.819" "Define tabulated list UI easily" '((dash "2.12.1") (magit-popup "2.6.0") (tablist "0.70") (cl-lib "0.5")) :url "https://github.com/Yuki-Inoue/tblui.el")
