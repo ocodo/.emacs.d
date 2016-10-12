ae671551a7110c0f31726ac34075bde07a74c88a
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:20:17 -0400

    [Adding helm-lobsters version: 20150213.746]

---
+++ elpa/helm-lobsters-20150213.746/helm-lobsters-autoloads.el
@@ -0,0 +1,23 @@
+;;; helm-lobsters-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "helm-lobsters" "helm-lobsters.el" (0 0 0 0))
+;;; Generated autoloads from helm-lobsters.el
+
+(autoload 'helm-lobsters "helm-lobsters" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-lobsters" '("helm-lobsters-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-lobsters-autoloads.el ends here
