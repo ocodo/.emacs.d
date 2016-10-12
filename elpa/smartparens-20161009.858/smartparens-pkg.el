fd5701f71309faaad7b5e74358f044952c16161d
JasonM23 <jasonm23@gmail.com>
2016-10-11 20:04:54 -0400

    [Adding smartparens version: 20161009.858]

---
+++ elpa/smartparens-20161009.858/smartparens-pkg.el
@@ -0,0 +1,6 @@
+(define-package "smartparens" "20161009.858" "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
+  '((dash "2.12.1")
+    (cl-lib "0.3")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
