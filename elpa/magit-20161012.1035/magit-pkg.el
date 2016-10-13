8fde51b6197645871a7b6a5b5b2fd9220ea8a9ed
jasonm23 <jasonm23@gmail.com>
2016-10-13 23:00:24 +0800

    [Updating magit version: 20161012.1035][removing old versions: 20161011.1700,20161011.744]

---
+++ elpa/magit-20161012.1035/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "20161012.1035" "A Git porcelain inside Emacs"
+  '((emacs "24.4")
+    (async "20160711.223")
+    (dash "20160820.501")
+    (with-editor "20160929.734")
+    (git-commit "20160929.801")
+    (magit-popup "20160821.1338"))
+  :url "https://github.com/magit/magit" :keywords
+  '("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
