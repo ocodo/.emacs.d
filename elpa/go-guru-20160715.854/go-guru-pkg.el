837de70af6a11fe68415bf172d92e80929ed44e7
jasonm23 <jasonm23@gmail.com>
2016-10-13 23:00:22 +0800

    [Updating go-guru version: 20161012.721][removing old version: 20160715.854]

--- elpa/go-guru-20160715.854/go-guru-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-guru" "20160715.854" "Integration of the Go 'guru' analysis tool into Emacs." '((go-mode "1.3.1") (cl-lib "0.5")) :keywords '("tools"))
