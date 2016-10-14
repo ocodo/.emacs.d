4cdfdbc71cd0cd664c3ca75a00ee66173013e1c6
jasonm23 <jasonm23@gmail.com>
2016-10-14 13:48:13 +0800

    [Updating go-mode version: 20161013.1055][removing old version: 20160913.944]

--- elpa/go-mode-20161013.1055/go-mode-autoloads.el
+++ elpa/go-mode-20161013.1055/go-mode-autoloads.el
@@ -3,8 +3,104 @@
 ;;; Code:
 
 
-;;;### (autoloads nil "go-mode" "go-mode.el" (22507 31544 161228
-;;;;;;  965000))
+;;;### (autoloads nil "go-guru" "go-guru.el" (22528 24123 0 0))
+;;; Generated autoloads from go-guru.el
+
+(autoload 'go-guru-set-scope "go-guru" "\
+Set the scope for the Go guru, prompting the user to edit the previous scope.
+
+The scope restricts analysis to the specified packages.
+Its value is a comma-separated list of patterns of these forms:
+  golang.org/x/tools/cmd/guru     # a single package
+  golang.org/x/tools/...          # all packages beneath dir
+  ...                             # the entire workspace.
+
+A pattern preceded by '-' is negative, so the scope
+  encoding/...,-encoding/xml
+matches all encoding packages except encoding/xml.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-callees "go-guru" "\
+Show possible callees of the function call at the current point.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-callers "go-guru" "\
+Show the set of callers of the function containing the current point.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-callstack "go-guru" "\
+Show an arbitrary path from a root of the call graph to the
+function containing the current point.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-definition "go-guru" "\
+Jump to the definition of the selected identifier.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-describe "go-guru" "\
+Describe the selected syntax, its kind, type and methods.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-pointsto "go-guru" "\
+Show what the selected expression points to.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-implements "go-guru" "\
+Describe the 'implements' relation for types in the package
+containing the current point.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-freevars "go-guru" "\
+Enumerate the free variables of the current selection.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-peers "go-guru" "\
+Enumerate the set of possible corresponding sends/receives for
+this channel receive/send operation.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-referrers "go-guru" "\
+Enumerate all references to the object denoted by the selected
+identifier.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-whicherrs "go-guru" "\
+Show globals, constants and types to which the selected
+expression (of type 'error') may refer.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-unhighlight-identifiers "go-guru" "\
+Remove highlights from previously highlighted identifier.
+
+\(fn)" nil nil)
+
+(autoload 'go-guru-hl-identifier "go-guru" "\
+Highlight all instances of the identifier under point. Removes
+highlights from previously highlighted identifier.
+
+\(fn)" t nil)
+
+(autoload 'go-guru-hl-identifier-mode "go-guru" "\
+Highlight instances of the identifier at point after a short
+timeout.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "go-mode" "go-mode.el" (22528 24123 0 0))
 ;;; Generated autoloads from go-mode.el
 
 (autoload 'go-mode "go-mode" "\
@@ -96,8 +192,7 @@ Tries to look for a URL at point.
 
 ;;;***
 
-;;;### (autoloads nil nil ("go-mode-pkg.el") (22507 31544 157228
-;;;;;;  965000))
+;;;### (autoloads nil nil ("go-mode-pkg.el") (22528 24123 0 0))
 
 ;;;***
 
