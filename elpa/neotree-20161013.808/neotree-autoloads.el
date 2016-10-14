e6f90af555779ec8da8b6afc80e3dd158916b814
jasonm23 <jasonm23@gmail.com>
2016-10-14 08:15:15 +0800

    [Updating neotree version: 20161013.808][removing old version: 20161012.757]

--- elpa/neotree-20161013.808/neotree-autoloads.el
+++ elpa/neotree-20161013.808/neotree-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "neotree" "neotree.el" (22527 37146 0 0))
+;;;### (autoloads nil "neotree" "neotree.el" (22527 49138 0 0))
 ;;; Generated autoloads from neotree.el
 
 (autoload 'neotree-find "neotree" "\
@@ -48,7 +48,7 @@ Show the NeoTree window.")
 
 ;;;***
 
-;;;### (autoloads nil nil ("neotree-pkg.el") (22527 37146 0 0))
+;;;### (autoloads nil nil ("neotree-pkg.el") (22527 49138 0 0))
 
 ;;;***
 
