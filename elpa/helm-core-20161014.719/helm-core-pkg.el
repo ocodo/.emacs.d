57196c6c59c671efe49b1b50fc3508c9d3e791ed
Jason Milkins <jason@example.com>
2016-10-16 20:08:38 +0800

    [Updating helm-core version: 20161014.719][removing old version: 20161012.1153]

--- elpa/helm-core-20161014.719/helm-core-pkg.el
+++ elpa/helm-core-20161014.719/helm-core-pkg.el
@@ -1,4 +1,4 @@
-(define-package "helm-core" "20161012.1153" "Development files for Helm"
+(define-package "helm-core" "20161014.719" "Development files for Helm"
   '((emacs "24.4")
     (async "1.9"))
   :url "https://emacs-helm.github.io/helm/")
