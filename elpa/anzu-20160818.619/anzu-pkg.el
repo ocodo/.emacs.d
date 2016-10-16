a8467774401446829b7cb5c1723e6ab9500bd205
Jason Milkins <jason@example.com>
2016-10-16 20:20:45 +0800

    [Updating anzu version: 20161015.808][removing old version: 20160818.619]

--- elpa/anzu-20160818.619/anzu-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "anzu" "20160818.619" "Show number of matches in mode-line while searching" '((cl-lib "0.5") (emacs "24")) :url "https://github.com/syohex/emacs-anzu")
