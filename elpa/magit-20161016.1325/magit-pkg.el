423ffc7729b7db290d27dadd9e9ebe0f2b206466
Jasonm23 <jasonm23@gmail.com>
2016-10-17 12:20:03 +0800

    [Updating magit version: 20161016.1325][removing old version: 20161015.430]

--- elpa/magit-20161016.1325/magit-pkg.el
+++ elpa/magit-20161016.1325/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20161015.430" "A Git porcelain inside Emacs"
+(define-package "magit" "20161016.1325" "A Git porcelain inside Emacs"
   '((emacs "24.4")
     (async "20160711.223")
     (dash "20160820.501")
