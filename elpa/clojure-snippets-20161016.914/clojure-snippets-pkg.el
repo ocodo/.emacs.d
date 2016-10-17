53999bd1a9c7f37a2006a434deb66d8a5e62c3db
Jasonm23 <jasonm23@gmail.com>
2016-10-17 12:20:01 +0800

    [Updating clojure-snippets version: 20161016.914][removing old version: 20161010.451]

--- elpa/clojure-snippets-20161016.914/clojure-snippets-pkg.el
+++ elpa/clojure-snippets-20161016.914/clojure-snippets-pkg.el
@@ -1,4 +1,4 @@
-(define-package "clojure-snippets" "20161010.451" "Yasnippets for clojure"
+(define-package "clojure-snippets" "20161016.914" "Yasnippets for clojure"
   '((yasnippet "0.10.0")))
 ;; Local Variables:
 ;; no-byte-compile: t
