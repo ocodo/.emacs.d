19969a00ceb74b8f53257176b95a5d575a60c246
Jasonm23 <jasonm23@gmail.com>
2016-10-18 12:32:21 +0800

    [Adding http-post-simple version: 20131010.2058]

---
+++ elpa/http-post-simple-20131010.2058/http-post-simple-autoloads.el
@@ -0,0 +1,16 @@
+;;; http-post-simple-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("http-post-simple.el") (22533 41768 0
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; http-post-simple-autoloads.el ends here
---
+++ elpa/http-post-simple-20131010.2058/http-post-simple-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "http-post-simple" "20131010.2058" "HTTP POST requests using the url library" 'nil :keywords '("comm" "data" "processes" "hypermedia"))
