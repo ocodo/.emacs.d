89c4753ee60d034e5a9e2fd6d89eabd529dc5dd5
Jasonm23 <jasonm23@gmail.com>
2016-10-18 12:32:24 +0800

    [Adding stream version: 2.2.3]

---
+++ elpa/stream-2.2.3/stream-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "stream" "2.2.3" "Implementation of streams" '((emacs "25")) :url "http://elpa.gnu.org/packages/stream.html" :keywords '("stream" "laziness" "sequences"))
