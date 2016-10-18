d9584654f296da0b350bbf5622981915cde6f5d4
Jasonm23 <jasonm23@gmail.com>
2016-10-18 12:32:21 +0800

    [Adding http version: 20160701.2025]

---
+++ elpa/http-20160701.2025/http-autoloads.el
@@ -0,0 +1,49 @@
+;;; http-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "http" "http.el" (22533 41769 0 0))
+;;; Generated autoloads from http.el
+
+(defvar-local http-hostname nil "\
+Default hostname used when url is an endpoint.")
+
+(put 'http-hostname 'safe-local-variable #'stringp)
+
+(autoload 'http-curl-command "http" "\
+Kill current http request as curl command.
+
+\(fn)" t nil)
+
+(autoload 'http-process "http" "\
+Process a http request.
+
+If SYNC is non-nil executes the request synchronously.
+
+\(fn &optional SYNC)" t nil)
+
+(autoload 'http-mode "http" "\
+Major mode for HTTP client.
+
+\\{http-mode-map}
+
+\(fn)" t nil)
+
+(autoload 'http-response-mode "http" "\
+Major mode for HTTP responses from `http-mode'
+
+\\{http-response-mode-map}
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.http\\'" . http-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; http-autoloads.el ends here
---
+++ elpa/http-20160701.2025/http-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "http" "20160701.2025" "Yet another HTTP client" '((emacs "24.4") (request "0.2.0")) :url "https://github.com/emacs-pe/http.el" :keywords '("convenience"))
