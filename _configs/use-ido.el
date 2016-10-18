0667fcd3fef6ca8ce5002efe27a9c519c66ba0e9
Jasonm23 <jasonm23@gmail.com>
2016-10-18 10:51:04 +0800

    Activate google this keybindings and invoke ido-everywhere

--- _configs/use-ido.el
+++ _configs/use-ido.el
@@ -15,16 +15,17 @@
     (use-package ido-vertical-mode)
     (ido-mode t)
     (ido-vertical-mode t)
+    (ido-everywhere 1)
     (ido-ubiquitous-mode t)
     (flx-ido-mode t)
     (setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right)
     (setq ido-create-new-buffer 'always)
     (setq ido-enable-flex-matching t)
-    (setq ido-everywhere t)
     (setq ido-auto-merge-work-directories-length -1))
 
   :bind
   (("M-x" . smex)
+   ("M-s-x" . execute-extended-command)
    ("M-X" . smex-major-mode-commands)))
 
 (provide 'use-ido)
