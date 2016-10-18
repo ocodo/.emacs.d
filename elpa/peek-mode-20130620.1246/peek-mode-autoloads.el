14f3b94bb9a7ae32f43cfc9f4c27f6811ef23414
Jasonm23 <jasonm23@gmail.com>
2016-10-18 12:32:24 +0800

    [Adding peek-mode version: 20130620.1246]

---
+++ elpa/peek-mode-20130620.1246/peek-mode-autoloads.el
@@ -0,0 +1,25 @@
+;;; peek-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "peek-mode" "peek-mode.el" (22533 41767 0 0))
+;;; Generated autoloads from peek-mode.el
+
+(autoload 'peek-mode "peek-mode" "\
+Serves the buffer live over HTTP.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("peek-mode-pkg.el") (22533 41767 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; peek-mode-autoloads.el ends here
---
+++ elpa/peek-mode-20130620.1246/peek-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "peek-mode" "20130620.1246" "Serve buffers live over HTTP with elnode backend"
+  '((elnode "0.9.8.1"))
+  :url "https://github.com/erikriverson/peek-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
