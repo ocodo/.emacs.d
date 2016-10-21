4ac1fe1a6fb0779c88ded1a819c1b21fc806c4e5
Jasonm23 <jasonm23@gmail.com>
2016-10-21 23:20:53 +0800

    [Updating merlin version: 20161017.205][removing old version: 20160830.242]

--- elpa/merlin-20161017.205/merlin-autoloads.el
+++ elpa/merlin-20161017.205/merlin-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "merlin" "merlin.el" (22527 38937 0 0))
+;;;### (autoloads nil "merlin" "merlin.el" (22538 11465 0 0))
 ;;; Generated autoloads from merlin.el
 
 (autoload 'merlin-mode "merlin" "\
@@ -17,8 +17,8 @@ Short cuts:
 
 ;;;***
 
-;;;### (autoloads nil "merlin-company" "merlin-company.el" (22527
-;;;;;;  38937 0 0))
+;;;### (autoloads nil "merlin-company" "merlin-company.el" (22538
+;;;;;;  11465 0 0))
 ;;; Generated autoloads from merlin-company.el
 
 (autoload 'merlin-company-backend "merlin-company" "\
@@ -29,7 +29,7 @@ Short cuts:
 ;;;***
 
 ;;;### (autoloads nil nil ("merlin-ac.el" "merlin-cap.el" "merlin-compat.el"
-;;;;;;  "merlin-iedit.el" "merlin-pkg.el") (22527 38937 0 0))
+;;;;;;  "merlin-iedit.el" "merlin-pkg.el") (22538 11465 0 0))
 
 ;;;***
 
---
+++ elpa/merlin-20161017.205/merlin-pkg.el
@@ -0,0 +1,5 @@
+(define-package "merlin" "20161017.205" "Mode for Merlin, an assistant for OCaml." 'nil :url "https://github.com/the-lambda-church/merlin" :keywords
+  '("ocaml" "languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
