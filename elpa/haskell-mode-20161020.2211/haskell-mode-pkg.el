800d7898aa0d247784ca8f97cc4cc1d2068a8064
Jasonm23 <jasonm23@gmail.com>
2016-10-21 23:20:50 +0800

    [Updating haskell-mode version: 20161020.2211][removing old version: 20161010.118]

---
+++ elpa/haskell-mode-20161020.2211/haskell-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "haskell-mode" "20161020.2211" "A Haskell editing mode"
+  '((emacs "24.3"))
+  :keywords
+  '("haskell" "cabal" "ghc" "repl")
+  :url "https://github.com/haskell/haskell-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
