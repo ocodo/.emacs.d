2fd33ce02baad42854500ae6bf76a5d0799f5a33
Jasonm23 <jasonm23@gmail.com>
2016-10-21 23:20:47 +0800

    [Updating flycheck version: 20161018.1343][removing old version: 20161017.604]

--- elpa/flycheck-20161018.1343/flycheck-pkg.el
+++ elpa/flycheck-20161018.1343/flycheck-pkg.el
@@ -1,4 +1,4 @@
-(define-package "flycheck" "20161017.604" "On-the-fly syntax checking"
+(define-package "flycheck" "20161018.1343" "On-the-fly syntax checking"
   '((dash "2.12.1")
     (pkg-info "0.4")
     (let-alist "1.0.4")
