0ea82c8e3115ad2a02df73090bd3c1d1857c9d9b
Jasonm23 <jasonm23@gmail.com>
2016-10-23 11:09:11 +0800

    [Updating ag version: 20161021.2133][removing old version: 20160731.1323]

--- elpa/ag-20160731.1323/ag-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ag" "20160731.1323" "A front-end for ag ('the silver searcher'), the C ack replacement." '((dash "2.8.0") (s "1.9.0") (cl-lib "0.5")))
