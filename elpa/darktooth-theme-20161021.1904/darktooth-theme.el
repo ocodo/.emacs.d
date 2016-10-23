c0e34d8e7b6090e94021aa786975be56a3fed387
Jasonm23 <jasonm23@gmail.com>
2016-10-23 11:09:12 +0800

    [Updating darktooth-theme version: 20161021.1904][removing old version: 20161014.8]

--- elpa/darktooth-theme-20161021.1904/darktooth-theme.el
+++ elpa/darktooth-theme-20161021.1904/darktooth-theme.el
@@ -4,8 +4,8 @@
 
 ;; Authors: Jason Milkins <jasonm23@gmail.com>
 ;; URL: http://github.com/emacsfodder/emacs-theme-darktooth
-;; Package-Version: 20161014.8
-;; Version: 0.1.57
+;; Package-Version: 20161021.1904
+;; Version: 0.1.58
 
 ;;; Commentary:
 ;;  A color theme for Emacs, from the darkness... it watches
@@ -656,10 +656,10 @@
    `(term-default-bg-color                     ((t (:background ,darktooth-dark0))))
 
    ;; MODE SUPPORT: Elfeed
-   `(elfeed-search-date-face                    ((t (:foreground ,darktooth-dark1))))
-   `(elfeed-search-feed-face                    ((t (:foreground ,darktooth-dark1))))
-   `(elfeed-search-tag-face                     ((t (:foreground ,darktooth-dark0))))
-   `(elfeed-search-title-face                   ((t (:foreground ,darktooth-dark0)))))
+   `(elfeed-search-date-face                    ((t (:foreground ,darktooth-light1))))
+   `(elfeed-search-feed-face                    ((t (:foreground ,darktooth-light1))))
+   `(elfeed-search-tag-face                     ((t (:foreground ,darktooth-light0))))
+   `(elfeed-search-title-face                   ((t (:foreground ,darktooth-light0)))))
 
   (custom-theme-set-variables
    'darktooth
