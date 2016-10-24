255edd6476310a6cda889cd08cbbff9540964891
Jasonm23 <jasonm23@gmail.com>
2016-10-24 11:56:11 +0800

    [Updating websocket version: 20161022.2054][removing old version: 20160720.2051]

--- elpa/websocket-20160720.2051/websocket-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "websocket" "20160720.2051" "Emacs WebSocket client and server" 'nil :keywords '("communication" "websocket" "server"))
---
+++ elpa/websocket-20161022.2054/websocket-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "websocket" "20161022.2054" "Emacs WebSocket client and server" 'nil :keywords '("communication" "websocket" "server"))
