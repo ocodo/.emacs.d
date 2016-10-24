e6d9b869580481f372e15e0b403d82f8d58b1f7e
Jasonm23 <jasonm23@gmail.com>
2016-10-24 11:56:11 +0800

    [Updating yasnippet version: 20161022.646][removing old version: 20160924.2001]

--- elpa/yasnippet-20161022.646/yasnippet-autoloads.el
+++ elpa/yasnippet-20161022.646/yasnippet-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "yasnippet" "yasnippet.el" (0 0 0 0))
+;;;### (autoloads nil "yasnippet" "yasnippet.el" (22540 33721 0 0))
 ;;; Generated autoloads from yasnippet.el
 
 (autoload 'yas-minor-mode "yasnippet" "\
@@ -49,11 +49,9 @@ A mode for editing yasnippets
 
 \(fn)" t nil)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "yasnippet" '("yas-" "help-snippet-def" "snippet-mode-map" "last-buffer-undo-list")))
-
 ;;;***
 
-;;;### (autoloads nil nil ("yasnippet-pkg.el") (0 0 0 0))
+;;;### (autoloads nil nil ("yasnippet-pkg.el") (22540 33720 0 0))
 
 ;;;***
 
