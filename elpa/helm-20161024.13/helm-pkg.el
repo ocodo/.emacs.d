3981c996ae1adc175a2aad4b46fd5f071acba3da
Jasonm23 <jasonm23@gmail.com>
2016-10-25 16:13:26 +0800

    [Updating helm version: 20161024.13][removing old version: 20161022.2217]

--- elpa/helm-20161024.13/helm-pkg.el
+++ elpa/helm-20161024.13/helm-pkg.el
@@ -1,8 +1,8 @@
-(define-package "helm" "20161022.2217" "Helm is an Emacs incremental and narrowing framework"
+(define-package "helm" "20161024.13" "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.4")
     (async "1.9")
     (popup "0.5.3")
-    (helm-core "2.3.0"))
+    (helm-core "2.3.1"))
   :url "https://emacs-helm.github.io/helm/")
 ;; Local Variables:
 ;; no-byte-compile: t
