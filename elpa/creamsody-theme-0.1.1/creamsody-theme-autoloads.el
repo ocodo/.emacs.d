7e53441f56add4148185981f83b8604cf5ae8bbe
jasonm23 <jasonm23@gmail.com>
2016-10-25 10:12:41 +0800

    [Adding creamsody-theme version: 0.1.1]

---
+++ elpa/creamsody-theme-0.1.1/creamsody-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; creamsody-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "creamsody-theme" "creamsody-theme.el" (22542
+;;;;;;  48513 0 0))
+;;; Generated autoloads from creamsody-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; creamsody-theme-autoloads.el ends here
---
+++ elpa/creamsody-theme-0.1.1/creamsody-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "creamsody-theme" "0.1.1" "Straight from the soda fountain." 'nil :url "http://github.com/emacsfodder/emacs-theme-creamsody")
