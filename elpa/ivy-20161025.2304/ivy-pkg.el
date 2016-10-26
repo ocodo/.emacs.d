dff2a3228a7dd42dabece60427906e13234b9076
jasonm23 <jasonm23@gmail.com>
2016-10-26 15:17:40 +0800

    [Updating ivy version: 20161025.2304][removing old version: 20161024.2346]

--- elpa/ivy-20161025.2304/ivy-pkg.el
+++ elpa/ivy-20161025.2304/ivy-pkg.el
@@ -1,4 +1,4 @@
-(define-package "ivy" "20161024.2346" "Incremental Vertical completYon"
+(define-package "ivy" "20161025.2304" "Incremental Vertical completYon"
   '((emacs "24.1"))
   :url "https://github.com/abo-abo/swiper" :keywords
   '("matching"))
