b2c9dd2f6a71753f30939ad87796e0b8188b3812
jasonm23 <jasonm23@gmail.com>
2016-10-26 15:18:21 +0800

    Improvements to p-out-p

--- plugins/packages-outdated-packages.el
+++ plugins/packages-outdated-packages.el
@@ -37,19 +37,36 @@
 
 ;;; Code:
 (require 'epl)
+(require 'bind-key)
+
+(defun packages-outdated-packages-make-plist (pack)
+  "Destructure PACK package metadata into a list."
+  (pcase-let* ((`[cl-struct-epl-package ,package ,package-desc] pack)
+               (`[cl-struct-package-desc ,name ,version-list ,summary ,_ ,_ ,_ ,path ,extras ,_] package-desc)
+               (version (format "%s.%s" (car version-list) (car (cdr version-list)))))
+    (list :pack pack
+          :name (symbol-name package)
+          :version version
+          :summary summary
+          :version-list version-list
+          :path path
+          :extras extras
+          :package-desc package-desc)))
+
+(defun packages-outdated-packages-list-vector (pack)
+  "Build a vector for the tabulated list from PACK."
+  (let* ((package-plist (packages-outdated-packages-make-plist pack))
+         (name (plist-get package-plist :name))
+         (version (plist-get package-plist :version))
+         (summary (plist-get package-plist :summary)))
+    (list pack (vector
+                (or name "")
+                (or version "")
+                (or summary "")))))
 
 (defun packages-outdated-packages-get-list-entries ()
   "Get the list of outdated packages."
-  (mapcar (lambda (pack)
-            (pcase-let* ((`[cl-struct-epl-package ,package ,package-desc] pack)
-                         (`[cl-struct-package-desc ,name ,version-list ,summary ,_ ,_ ,_ ,path ,extras ,_] package-desc)
-                         (version (format "%s.%s" (car version-list) (car (cdr version-list)))))
-              (list pack
-                    (vector
-                     (or (symbol-name package) "")
-                     (or version "")
-                     (or summary "")))))
-          (epl-outdated-packages)))
+  (mapcar 'packages-outdated-packages-list-vector (epl-outdated-packages)))
 
 (defvar packages-outdated-packages-table-layout
   "Packages outdated packages table layout.")
@@ -59,20 +76,48 @@
   (epl-upgrade packages)
   (revert-buffer))
 
+(defun packages-outdated-packages-refresh ()
+  "Refresh the outdated package list."
+  (interactive)
+  (package-refresh-contents)
+  (revert-buffer))
+
+(defun packages-outdated-packages-visit-url (&optional pkg)
+  "Visit the url of the package PKG."
+  (interactive)
+  (let* ((package (tabulated-list-get-id))
+         (pkg-plist (packages-outdated-packages-make-plist package))
+         (name (plist-get pkg-plist :name))
+         (extras (plist-get pkg-plist :extras))
+         (url (alist-get :url extras)))
+    (if url
+        (browse-url url)
+      (message "There's no URL for %s" (or name "this package")))))
+
+(defun packages-outdated-packages-open-package (&optional pkg)
+  "Show info for the package PKG."
+  (interactive)
+  (let* ((package (tabulated-list-get-id))
+         (pkg-plist (packages-outdated-packages-make-plist package))
+         (path (plist-get pkg-plist :path)))
+    (find-file path)))
+
 (tblui-define
  packages-outdated-packages
  packages-outdated-packages-get-list-entries
  [("Package" 18 t)
   ("Version" 15 t)
   ("Summary" 40 nil)]
- ((:key "x"
-   :name packages-outdated-packages-upgrade-popup
-   :funcs((?y "Confirm upgrade" packages-outdated-packages-upgrade-marked-packages)))))
+ ((:key "?"
+        :name packages-outdated-packages-info-popup
+        :funcs ((?v "Visit package URL" packages-outdated-packages-visit-url)
+                (?o "Open package" packages-outdated-packages-open-package))
+        )
+  (:key "x"
+        :name packages-outdated-packages-upgrade-popup
+        :funcs ((?y "Confirm upgrade" packages-outdated-packages-upgrade-marked-packages)))))
 
-(define-key packages-outdated-packages-mode-map (kbd "g") (lambda ()
-                                  (interactive)
-                                  (package-refresh-contents)
-                                  (revert-buffer)))
+(bind-key "g" 'packages-outdated-packages-refresh packages-outdated-packages-mode-map)
 
 (defun packages-outdated-packages ()
   "Something."
