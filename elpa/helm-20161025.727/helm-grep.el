a61912531b8b0f2e56e85aefd0481d21003526f2
jasonm23 <jasonm23@gmail.com>
2016-10-26 12:03:47 +0800

    [Updating helm version: 20161025.727][removing old version: 20161024.13]

--- elpa/helm-20161025.727/helm-grep.el
+++ elpa/helm-20161025.727/helm-grep.el
@@ -503,10 +503,10 @@ It is intended to use as a let-bound variable, DON'T set this globaly.")
                (helm-process-deferred-sentinel-hook
                 process event (helm-default-directory)))
              (cond ((and noresult
-                         ;; [FIXME] This is a workaround for zgrep
+                         ;; This is a workaround for zgrep
                          ;; that exit with code 1
                          ;; after a certain amount of results.
-                         (not (with-helm-buffer helm-grep-use-zgrep)))
+                         (with-helm-buffer (helm-empty-buffer-p)))
                     (with-helm-buffer
                       (insert (concat "* Exit with code 1, no result found,"
                                       " command line was:\n\n "
@@ -525,7 +525,12 @@ It is intended to use as a let-bound variable, DON'T set this globaly.")
                                               (helm-grep-command t)
                                             (helm-grep-command)))))
                                       'face 'helm-grep-finish))))))
-                   ((string= event "finished\n")
+                   ((or (string= event "finished\n")
+                        (and noresult
+                             ;; This is a workaround for zgrep
+                             ;; that exit with code 1
+                             ;; after a certain amount of results.
+                             (with-helm-buffer (not (helm-empty-buffer-p)))))
                     (with-helm-window
                       (setq mode-line-format
                             '(" " mode-line-buffer-identification " "
@@ -982,7 +987,7 @@ It is used actually to specify 'zgrep' or 'git'.
 When BACKEND 'zgrep' is used don't prompt for a choice
 in recurse, and ignore EXTS, search being made recursively on files matching
 `helm-zgrep-file-extension-regexp' only."
-  (when (and (helm-grep-use-ack-p)
+  (when (and (string-match-p "\\`ack" (helm-grep-command recurse backend))
              helm-ff-default-directory
              (file-remote-p helm-ff-default-directory))
     (error "Error: Remote operation not supported with ack-grep."))
@@ -1009,7 +1014,14 @@ in recurse, and ignore EXTS, search being made recursively on files matching
                      ;; ignore types and do not prompt for choice.
                      (string-match "%e" helm-grep-default-command)
                      (helm-grep-read-ack-type)))
-         (src-name (capitalize (helm-grep-command recurse backend))))
+         (src-name (capitalize (helm-grep-command recurse backend)))
+         (com (cond ((eq backend 'zgrep) helm-default-zgrep-command)
+                    ((eq backend 'git) helm-grep-git-grep-command)
+                    (recurse helm-grep-default-recurse-command)
+                    ;; When resuming, the local value of
+                    ;; `helm-grep-default-command' is used, only git-grep
+                    ;; should need this.
+                    (t helm-grep-default-command))))
     ;; When called as action from an other source e.g *-find-files
     ;; we have to kill action buffer.
     (when (get-buffer helm-action-buffer)
@@ -1027,19 +1039,12 @@ in recurse, and ignore EXTS, search being made recursively on files matching
      'helm-grep-include-files (or include-files types)
      'helm-grep-in-recurse recurse
      'helm-grep-use-zgrep (eq backend 'zgrep)
-     'helm-grep-default-command
-     (cond ((eq backend 'zgrep) helm-default-zgrep-command)
-           ((eq backend 'git) helm-grep-git-grep-command)
-           (recurse helm-grep-default-recurse-command)
-           ;; When resuming, the local value of
-           ;; `helm-grep-default-command' is used, only git-grep
-           ;; should need this.
-           (t helm-grep-default-command))
+     'helm-grep-default-command com
      'default-directory helm-ff-default-directory) ;; [1]
     ;; Setup the source.
     (set source (helm-make-source src-name 'helm-grep-class
                   :backend backend
-                  :pcre (if recurse ack-rec-p (helm-grep-use-ack-p))))
+                  :pcre (string-match-p "\\`ack" com)))
     (helm
      :sources source
      :buffer (format "*helm %s*" (helm-grep-command recurse backend))
