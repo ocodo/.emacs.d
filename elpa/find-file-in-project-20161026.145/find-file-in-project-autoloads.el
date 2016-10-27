bf5f5bbc12a97b286e95b30cbb4f19c29800cbff
jasonm23 <jasonm23@gmail.com>
2016-10-27 11:27:17 +0800

    [Updating find-file-in-project version: 20161026.145][removing old version: 20161025.516]

--- elpa/find-file-in-project-20161026.145/find-file-in-project-autoloads.el
+++ elpa/find-file-in-project-20161026.145/find-file-in-project-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "find-file-in-project" "find-file-in-project.el"
-;;;;;;  (22544 10660 0 0))
+;;;;;;  (22545 29258 0 0))
 ;;; Generated autoloads from find-file-in-project.el
 
 (autoload 'ffip-diff-backend-git-show-commit "find-file-in-project" "\
--- elpa/find-file-in-project-20161026.145/find-file-in-project.el
+++ elpa/find-file-in-project-20161026.145/find-file-in-project.el
@@ -4,7 +4,7 @@
 ;;   Phil Hagelberg, Doug Alcorn, Will Farrington, Chen Bin
 ;;
 ;; Version: 5.2.5
-;; Package-Version: 20161025.516
+;; Package-Version: 20161026.145
 ;; Author: Phil Hagelberg, Doug Alcorn, and Will Farrington
 ;; Maintainer: Chen Bin <chenbin.sh@gmail.com>
 ;; URL: https://github.com/technomancy/find-file-in-project
@@ -711,7 +711,8 @@ You can set `ffip-find-relative-path-callback' to format the string before copyi
   (let* ((keyword (if (region-active-p)
                       (buffer-substring-no-properties (region-beginning) (region-end))
                     (read-string "Enter keyword (or press ENTER):")))
-         (project-files (ffip-project-search keyword find-directory)))
+         (project-files (ffip-project-search keyword find-directory))
+         root)
     (if (> (length project-files) 0)
         (progn
           (setq root (file-name-nondirectory
