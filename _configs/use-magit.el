837dd97bfc5c47666213f5aa8760589863ddea52
jasonm23 <jasonm23@gmail.com>
2016-10-28 11:25:32 +0800

    Add workspace and .emacs.d as magit-repository-directories

--- _configs/use-magit.el
+++ _configs/use-magit.el
@@ -1,6 +1,7 @@
 ;; Magit config
 
 (require 'magit-extras)
+(setq magit-repository-directories '(("~/.emacs.d" . 0) ("~/workspace/" . 1)))
 
 (put 'magit-clean 'disabled nil)
 (define-key magit-mode-map (kbd "C-c C") 'magit-clean)
