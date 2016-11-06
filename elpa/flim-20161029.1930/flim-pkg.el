734d031a9ab6807b4ab014a4463f6582aeba6f46
JasonM23 <jasonm23@gmail.com>
2016-11-06 17:31:52 -0500

    [Updating flim version: 20161029.1930][removing old version: 20160831.633]

---
+++ elpa/flim-20161029.1930/flim-pkg.el
@@ -0,0 +1,5 @@
+(define-package "flim" "20161029.1930" "A library to provide basic features about message representation or encoding."
+  '((apel "10.8")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
