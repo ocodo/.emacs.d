2f98dd51bb1f54b6045ddb480de4c8308e188edf
JasonM23 <jasonm23@gmail.com>
2016-11-06 17:32:09 -0500

    [Updating telephone-line version: 20161105.840][removing old version: 20160302.1515]

---
+++ elpa/telephone-line-20161105.840/telephone-line-autoloads.el
@@ -0,0 +1,62 @@
+;;; telephone-line-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "telephone-line" "telephone-line.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from telephone-line.el
+
+(defvar telephone-line-mode nil "\
+Non-nil if Telephone-Line mode is enabled.
+See the `telephone-line-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `telephone-line-mode'.")
+
+(custom-autoload 'telephone-line-mode "telephone-line" nil)
+
+(autoload 'telephone-line-mode "telephone-line" "\
+Toggle telephone-line on or off.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "telephone-line" '("telephone-line-")))
+
+;;;***
+
+;;;### (autoloads nil "telephone-line-segments" "telephone-line-segments.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from telephone-line-segments.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "telephone-line-segments" '("telephone-line-process-segment")))
+
+;;;***
+
+;;;### (autoloads nil "telephone-line-separators" "telephone-line-separators.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from telephone-line-separators.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "telephone-line-separators" '("telephone-line-")))
+
+;;;***
+
+;;;### (autoloads nil "telephone-line-utils" "telephone-line-utils.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from telephone-line-utils.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "telephone-line-utils" '("telephone-line-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("telephone-line-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; telephone-line-autoloads.el ends here
