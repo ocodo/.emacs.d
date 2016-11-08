314006ea58929057f9000ab6794e6ff24759c6cd
Jason Milkins <jasonm23@gmail.com>
2016-11-08 21:47:13 +0800

    [Updating web-mode version: 20161106.132][removing old version: 20161105.553]

--- elpa/web-mode-20161106.132/web-mode-autoloads.el
+++ elpa/web-mode-20161106.132/web-mode-autoloads.el
@@ -3,15 +3,13 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (0 0 0 0))
+;;;### (autoloads nil "web-mode" "web-mode.el" (22561 51260 0 0))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\
 Major mode for editing web templates.
 
 \(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "web-mode" '("web-mode-")))
 
 ;;;***
 
