aa07216b52bc71f7a7227c3cea29967472f039cb
JasonM23 <jasonm23@gmail.com>
2016-11-20 00:27:35 -0500

    [Updating merlin version: 20161103.821][removing old version: 20161017.205]

--- elpa/merlin-20161017.205/merlin-autoloads.el
+++
@@ -1,41 +0,0 @@
-;;; merlin-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "merlin" "merlin.el" (22538 11465 0 0))
-;;; Generated autoloads from merlin.el
-
-(autoload 'merlin-mode "merlin" "\
-Minor mode for interacting with a merlin process.
-Runs a merlin process in the background and perform queries on it.
-
-Short cuts:
-\\{merlin-mode-map}
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "merlin-company" "merlin-company.el" (22538
-;;;;;;  11465 0 0))
-;;; Generated autoloads from merlin-company.el
-
-(autoload 'merlin-company-backend "merlin-company" "\
-
-
-\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("merlin-ac.el" "merlin-cap.el" "merlin-compat.el"
-;;;;;;  "merlin-iedit.el" "merlin-pkg.el") (22538 11465 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; merlin-autoloads.el ends here
