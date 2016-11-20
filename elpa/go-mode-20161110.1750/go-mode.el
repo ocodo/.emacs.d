bab9c190d6bc8b5a754d91e1c8a46f376ea4b088
JasonM23 <jasonm23@gmail.com>
2016-11-20 00:27:28 -0500

    [Updating go-mode version: 20161110.1750][removing old version: 20161022.1435]

--- elpa/go-mode-20161110.1750/go-mode.el
+++ elpa/go-mode-20161110.1750/go-mode.el
@@ -6,7 +6,7 @@
 
 ;; Author: The go-mode Authors
 ;; Version: 1.4.0
-;; Package-Version: 20161022.1435
+;; Package-Version: 20161110.1750
 ;; Keywords: languages go
 ;; URL: https://github.com/dominikh/go-mode.el
 ;;
@@ -956,6 +956,12 @@ Function result is a unparenthesized type or a parameter list."
          (go--match-parameter-list end))
         (t nil)))
 
+(defun go--reset-dangling-cache-before-change (&optional unused-beg unused-end)
+  "Reset `go-dangling-cache'.
+
+This is intended to be called from `before-change-functions'."
+  (setq go-dangling-cache (make-hash-table :test 'eql)))
+
 ;;;###autoload
 (define-derived-mode go-mode prog-mode "Go"
   "Major mode for editing Go source text.
@@ -1049,7 +1055,7 @@ with goflymake \(see URL `https://github.com/dougm/goflymake'), gocode
   (set (make-local-variable 'compilation-error-screen-columns) nil)
 
   (set (make-local-variable 'go-dangling-cache) (make-hash-table :test 'eql))
-  (add-hook 'before-change-functions (lambda (x y) (setq go-dangling-cache (make-hash-table :test 'eql))) t t)
+  (add-hook 'before-change-functions #'go--reset-dangling-cache-before-change t t)
 
   ;; ff-find-other-file
   (setq ff-other-file-alist 'go-other-file-alist)
