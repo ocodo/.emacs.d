b2aaf3f29431c20bc9c8d78fbec27679e0a2adfe
JasonM23 <jasonm23@gmail.com>
2016-11-20 00:27:27 -0500

    [Updating git-commit version: 20161108.1335][removing old version: 20161011.1738]

--- elpa/git-commit-20161011.1738/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20161011.1738" "Edit Git commit messages" '((emacs "24.4") (dash "20160820.501") (with-editor "20160929.734")) :url "https://github.com/magit/magit" :keywords '("git" "tools" "vc"))
--- elpa/git-commit-20161108.1335/git-commit.el
+++ elpa/git-commit-20161108.1335/git-commit.el
@@ -12,7 +12,7 @@
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 
 ;; Package-Requires: ((emacs "24.4") (dash "20160820.501") (with-editor "20160929.734"))
-;; Package-Version: 20161011.1738
+;; Package-Version: 20161108.1335
 ;; Keywords: git tools vc
 ;; Homepage: https://github.com/magit/magit
 
@@ -223,11 +223,21 @@ to consider doing so."
   :safe 'numberp
   :type 'number)
 
-(defcustom git-commit-fill-column 72
-  "Automatically wrap commit message lines beyond this column."
+(defcustom git-commit-fill-column nil
+  "Override `fill-column' in commit message buffers.
+
+If this is non-nil, then it should be an integer.  If that is the
+case and the buffer-local value of `fill-column' is not already
+set by the time `git-commit-turn-on-auto-fill' is called as a
+member of `git-commit-setup-hook', then that function sets the
+buffer-local value of `fill-column' to the value of this option.
+
+This option exists mostly for historic reasons.  If you are not
+already using it, then you probably shouldn't start doing so."
   :group 'git-commit
   :safe 'numberp
-  :type 'number)
+  :type '(choice (const :tag "use regular fill-column")
+                 number))
 
 (defcustom git-commit-known-pseudo-headers
   '("Signed-off-by" "Acked-by" "Cc"
@@ -392,7 +402,11 @@ to consider doing so."
     (when (file-accessible-directory-p (file-name-directory it))
       (find-alternate-file it)))
   (when git-commit-major-mode
-    (funcall git-commit-major-mode))
+    (let ((auto-mode-alist (list (cons (concat "\\`"
+                                               (regexp-quote buffer-file-name)
+                                               "\\'")
+                                       git-commit-major-mode))))
+      (normal-mode)))
   (setq with-editor-show-usage nil)
   (with-editor-mode 1)
   (add-hook 'with-editor-finish-query-functions
@@ -452,8 +466,12 @@ Don't use it directly, instead enable `global-git-commit-mode'."
 
 (defun git-commit-turn-on-auto-fill ()
   "Unconditionally turn on Auto Fill mode.
-And set `fill-column' to `git-commit-fill-column'."
-  (setq fill-column git-commit-fill-column)
+If `git-commit-fill-column' is non-nil, and `fill-column'
+doesn't already have a buffer-local value, then set that
+to `git-commit-fill-column'."
+  (when (and (numberp git-commit-fill-column)
+             (not (local-variable-p 'fill-column)))
+    (setq fill-column git-commit-fill-column))
   (turn-on-auto-fill))
 
 (defun git-commit-turn-on-flyspell ()
