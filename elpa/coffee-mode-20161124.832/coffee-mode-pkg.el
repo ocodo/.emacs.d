78dbc38763b668029337610eb15c0e066e3a4420
JasonM23 <jasonm23@gmail.com>
2016-11-27 08:13:53 -0500

    [Updating coffee-mode version: 20161124.832][removing old version: 20160808.1712]

---
+++ elpa/coffee-mode-20161124.832/coffee-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "coffee-mode" "20161124.832" "Major mode for CoffeeScript code" '((emacs "24.3")) :url "http://github.com/defunkt/coffee-mode" :keywords '("coffeescript" "major" "mode"))
