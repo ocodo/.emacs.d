1d4bea5eda9a8bd7e6b7aa3147c57db3884f9121
JasonM23 <jasonm23@gmail.com>
2016-11-27 08:14:01 -0500

    [Updating gh version: 20161119.2004][removing old version: 20161118.2109]

--- elpa/gh-20161119.2004/gh-pkg.el
+++ elpa/gh-20161119.2004/gh-pkg.el
@@ -1,4 +1,4 @@
-(define-package "gh" "20161118.2109" "A GitHub library for Emacs"
+(define-package "gh" "20161119.2004" "A GitHub library for Emacs"
   '((emacs "24.3")
     (s "1.9.0")
     (dash "2.9.0")
