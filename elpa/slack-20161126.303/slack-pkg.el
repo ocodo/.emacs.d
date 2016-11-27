7acfd450eeaed5afb0ad3d176aa4a534cb2f9e37
JasonM23 <jasonm23@gmail.com>
2016-11-27 08:14:21 -0500

    [Updating slack version: 20161126.303][removing old version: 20161113.1832]

--- elpa/slack-20161126.303/slack-pkg.el
+++ elpa/slack-20161126.303/slack-pkg.el
@@ -1,4 +1,4 @@
-(define-package "slack" "20161113.1832" "Slack client for Emacs"
+(define-package "slack" "20161126.303" "Slack client for Emacs"
   '((websocket "1.5")
     (request "0.2.0")
     (oauth2 "0.10")
