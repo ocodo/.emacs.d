784b54ab59a08547ef3764198d3c69f19ade6865
JasonM23 <jasonm23@gmail.com>
2016-11-27 08:13:53 -0500

    [Updating company version: 20161126.1629][removing old version: 20161113.1747]

--- elpa/company-20161126.1629/company-pkg.el
+++ elpa/company-20161126.1629/company-pkg.el
@@ -1,4 +1,4 @@
-(define-package "company" "20161113.1747" "Modular text completion framework"
+(define-package "company" "20161126.1629" "Modular text completion framework"
   '((emacs "24.3"))
   :url "http://company-mode.github.io/" :keywords
   '("abbrev" "convenience" "matching"))
