89a6ba3cd09b7b1f66c2cbd2c17c6c25b565f2d2
JasonM23 <jasonm23@gmail.com>
2016-11-27 08:14:26 -0500

    [Updating which-key version: 20161125.600][removing old version: 20161106.950]

---
+++ elpa/which-key-20161125.600/which-key-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "which-key" "20161125.600" "Display available keybindings in popup" '((emacs "24.3")) :url "https://github.com/justbur/emacs-which-key")
