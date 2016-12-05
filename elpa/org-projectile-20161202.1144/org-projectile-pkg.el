7e7011cc46b683a61a4d6c0a3e0d98fc2a622f3e
JasonM23 <jasonm23@gmail.com>
2016-12-05 01:52:32 -0500

    [Updating org-projectile version: 20161202.1144][removing old version: 20161124.1110]

--- elpa/org-projectile-20161202.1144/org-projectile-pkg.el
+++ elpa/org-projectile-20161202.1144/org-projectile-pkg.el
@@ -1,8 +1,7 @@
-(define-package "org-projectile" "20161124.1110" "Repository todo management for org-mode"
+(define-package "org-projectile" "20161202.1144" "Repository todo management for org-mode"
   '((projectile "0.11.0")
     (dash "2.10.0")
-    (emacs "24")
-    (pcache "0.4.1"))
+    (emacs "24"))
   :url "https://github.com/IvanMalison/org-projectile" :keywords
   '("org" "projectile" "todo"))
 ;; Local Variables:
