c054cc7659e676b0a765cd236c3133e50a645f85
JasonM23 <jasonm23@gmail.com>
2016-12-05 01:52:19 -0500

    [Adding electric-spacing version: 20161127.720]

---
+++ elpa/electric-spacing-20161127.720/electric-spacing-autoloads.el
@@ -0,0 +1,11 @@
+;;; electric-spacing-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; electric-spacing-autoloads.el ends here
