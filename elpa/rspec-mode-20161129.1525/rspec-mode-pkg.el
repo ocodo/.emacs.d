26f11313d8b84fe53dad08ddf5727f62ec4cad5c
JasonM23 <jasonm23@gmail.com>
2016-12-05 01:52:34 -0500

    [Updating rspec-mode version: 20161129.1525][removing old version: 20160715.509]

--- elpa/rspec-mode-20161129.1525/rspec-mode-pkg.el
+++ elpa/rspec-mode-20161129.1525/rspec-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "rspec-mode" "20160715.509" "Enhance ruby-mode for RSpec"
+(define-package "rspec-mode" "20161129.1525" "Enhance ruby-mode for RSpec"
   '((ruby-mode "1.0")
     (cl-lib "0.4"))
   :url "http://github.com/pezra/rspec-mode" :keywords
