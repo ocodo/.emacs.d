ea1371a40c129cbae8fe73f72d838756f500131b
jasonm23 <jasonm23@gmail.com>
2016-12-18 13:07:26 +0800

    [Updating persp-mode version: 20161214.243][removing old version: 20161119.505]

--- elpa/persp-mode-20161214.243/persp-mode.el
+++ elpa/persp-mode-20161214.243/persp-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Constantin Kulikov (Bad_ptr) <zxnotdead@gmail.com>
 ;; Version: 2.9.5
-;; Package-Version: 20161119.505
+;; Package-Version: 20161214.243
 ;; Package-Requires: ()
 ;; Keywords: perspectives, session, workspace, persistence, windows, buffers, convenience
 ;; URL: https://github.com/Bad-ptr/persp-mode.el
@@ -1680,13 +1680,13 @@ and then killed.\nWhat do you really want to do? "
 (defun persp-after-change-major-mode-h ()
   (let ((buf (current-buffer)))
     (persp-find-and-set-persps-for-buffer buf)
-    (when persp-add-buffer-on-after-change-major-mode
-      (unless (persp-buffer-filtered-out-p
-               buf persp-add-buffer-on-after-change-major-mode-filter-functions)
-        (case persp-add-buffer-on-after-change-major-mode
-          ('nil nil)
-          (free (and (persp-buffer-free-p buf) (persp-add-buffer buf)))
-          (t (persp-add-buffer buf)))))))
+    (when (and (case persp-add-buffer-on-after-change-major-mode
+                 ('nil nil)
+                 (free (persp-buffer-free-p buf))
+                 (t t))
+               (not (persp-buffer-filtered-out-p
+                     buf persp-add-buffer-on-after-change-major-mode-filter-functions)))
+      (persp-add-buffer buf (get-current-persp) nil nil))))
 
 (defun persp-server-switch ()
   (condition-case-unless-debug err
