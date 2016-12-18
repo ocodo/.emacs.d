1a48c9d1b21aa172a7867e920c75b9ee15390fd5
jasonm23 <jasonm23@gmail.com>
2016-12-18 13:07:30 +0800

    [Updating wanderlust version: 20161212.1531][removing old version: 20161114.1527]

--- elpa/wanderlust-20161114.1527/wanderlust-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "wanderlust" "20161114.1527" "Yet Another Message Interface on Emacsen"
-  '((semi "1.14.7")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/wanderlust-20161212.1531/wanderlust-pkg.el
@@ -0,0 +1,5 @@
+(define-package "wanderlust" "20161212.1531" "Yet Another Message Interface on Emacsen"
+  '((semi "1.14.7")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
