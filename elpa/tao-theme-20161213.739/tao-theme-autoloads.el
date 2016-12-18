d534d903f99317604e15abbc9293610d54b0e44b
jasonm23 <jasonm23@gmail.com>
2016-12-18 13:07:29 +0800

    [Updating tao-theme version: 20161213.739][removing old version: 20160915.2124]

--- elpa/tao-theme-20161213.739/tao-theme-autoloads.el
+++ elpa/tao-theme-20161213.739/tao-theme-autoloads.el
@@ -3,24 +3,24 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "tao-yang-theme" "tao-yang-theme.el" (22507
-;;;;;;  31315 65226 21000))
+;;;### (autoloads nil "tao-yang-theme" "tao-yang-theme.el" (22613
+;;;;;;  12755 473672 167000))
 ;;; Generated autoloads from tao-yang-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
 
 ;;;***
 
-;;;### (autoloads nil "tao-yin-theme" "tao-yin-theme.el" (22507 31315
-;;;;;;  77226 21000))
+;;;### (autoloads nil "tao-yin-theme" "tao-yin-theme.el" (22613 12755
+;;;;;;  497672 864000))
 ;;; Generated autoloads from tao-yin-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
 
 ;;;***
 
-;;;### (autoloads nil nil ("tao-theme-pkg.el" "tao-yinyang-common.el")
-;;;;;;  (22507 31315 73226 21000))
+;;;### (autoloads nil nil ("tao-theme-pkg.el" "tao-theme.el") (22613
+;;;;;;  12755 505673 97000))
 
 ;;;***
 
