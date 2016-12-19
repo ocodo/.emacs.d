500ab657d63ba8f72fdca38488c124b7a46d66f6
JasonM23 <jasonm23@gmail.com>
2016-12-18 20:37:49 -0500

    [Updating darktooth-theme version: 0.2.3][removing old version: 20161210.2038]

--- elpa/darktooth-theme-0.2.3/darktooth-theme-autoloads.el
+++ elpa/darktooth-theme-0.2.3/darktooth-theme-autoloads.el
@@ -3,11 +3,13 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "darktooth-theme" "darktooth-theme.el" (22613
-;;;;;;  6667 369002 604000))
+;;;### (autoloads nil "darktooth-theme" "darktooth-theme.el" (0 0
+;;;;;;  0 0))
 ;;; Generated autoloads from darktooth-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "darktooth-theme" '("darktooth")))
 
 ;;;***
 
