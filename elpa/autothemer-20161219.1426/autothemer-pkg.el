bc2cee7e63b8c50335c8eeba8143a044da4b9275
jasonm23 <jasonm23@gmail.com>
2016-12-20 22:37:23 +0800

    [Adding autothemer version: 20161219.1426]

---
+++ elpa/autothemer-20161219.1426/autothemer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "autothemer" "20161219.1426" "Conveniently define themes." '((dash "2.10.0") (emacs "24") (cl-lib "0.5")) :url "https://github.com/sebastiansturm/autothemer")
