4b705d8c9e8f00c4a6248b7258889037f9e2d05b
jasonm23 <jasonm23@gmail.com>
2016-12-20 22:37:26 +0800

    [Updating inkpot-theme version: 20161218.2159][removing old version: 20161217.8]

--- elpa/inkpot-theme-20161217.8/inkpot-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "inkpot-theme" "20161217.8" "port of vim's inkpot theme" 'nil :url "http://github.com/ideasman42/emacs24-inkpot.git" :keywords '("color" "theme"))
---
+++ elpa/inkpot-theme-20161218.2159/inkpot-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "inkpot-theme" "20161218.2159" "port of vim's inkpot theme" 'nil :url "https://github.com/ideasman42/emacs-inkpot-theme" :keywords '("color" "theme"))
