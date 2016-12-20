4f36ac35f940116219c0462f7583974cf67e9826
jasonm23 <jasonm23@gmail.com>
2016-12-20 22:37:23 +0800

    [Updating all-the-icons version: 20161219.329][removing old version: 20161102.415]

--- elpa/all-the-icons-20161219.329/all-the-icons-pkg.el
+++ elpa/all-the-icons-20161219.329/all-the-icons-pkg.el
@@ -1,4 +1,4 @@
-(define-package "all-the-icons" "20161102.415" "A library for inserting Developer icons"
+(define-package "all-the-icons" "20161219.329" "A library for inserting Developer icons"
   '((dash "2.12.0")
     (emacs "24.3")
     (font-lock+ "0"))
