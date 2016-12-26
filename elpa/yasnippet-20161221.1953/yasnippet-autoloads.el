30a620d1915ee6bac5dbe546a276e214f6aa3d98
jasonm23 <jasonm23@gmail.com>
2016-12-26 12:08:35 +0800

    [Updating yasnippet version: 20161221.1953][removing old version: 20161211.1918]

--- elpa/yasnippet-20161221.1953/yasnippet-autoloads.el
+++ elpa/yasnippet-20161221.1953/yasnippet-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "yasnippet" "yasnippet.el" (22613 12705 576220
-;;;;;;  927000))
+;;;### (autoloads nil "yasnippet" "yasnippet.el" (22624 31660 192058
+;;;;;;  61000))
 ;;; Generated autoloads from yasnippet.el
 
 (autoload 'yas-minor-mode "yasnippet" "\
@@ -52,8 +52,8 @@ A mode for editing yasnippets
 
 ;;;***
 
-;;;### (autoloads nil nil ("yasnippet-pkg.el") (22613 12695 711933
-;;;;;;  850000))
+;;;### (autoloads nil nil ("yasnippet-pkg.el") (22624 31650 612082
+;;;;;;  142000))
 
 ;;;***
 
--- elpa/yasnippet-20161221.1953/yasnippet-pkg.el
+++ elpa/yasnippet-20161221.1953/yasnippet-pkg.el
@@ -1,4 +1,4 @@
-(define-package "yasnippet" "20161211.1918" "Yet another snippet extension for Emacs."
+(define-package "yasnippet" "20161221.1953" "Yet another snippet extension for Emacs."
   '((cl-lib "0.5"))
   :url "http://github.com/joaotavora/yasnippet" :keywords
   '("convenience" "emulation"))
