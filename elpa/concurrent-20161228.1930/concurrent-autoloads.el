6b4a2e78491fe29ad4ac9da790d722d8b293f2ec
Jasonm23 <jasonm23@gmail.com>
2016-12-30 08:27:30 +0700

    [Updating concurrent version: 20161228.1930][removing old version: 20160109.2046]

--- elpa/concurrent-20161228.1930/concurrent-autoloads.el
+++ elpa/concurrent-20161228.1930/concurrent-autoloads.el
@@ -1,9 +1,9 @@
 ;;; concurrent-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("concurrent.el") (22168 61987 418582 149000))
+;;;### (autoloads nil nil ("concurrent.el") (22628 53713 0 0))
 
 ;;;***
 
