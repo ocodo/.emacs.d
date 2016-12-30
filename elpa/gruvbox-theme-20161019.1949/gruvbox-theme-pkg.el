10bc4621444e6b840cba067704588374df6b245f
Jasonm23 <jasonm23@gmail.com>
2016-12-30 08:27:33 +0700

    [Updating gruvbox-theme version: 20161228.1532][removing old version: 20161019.1949]

--- elpa/gruvbox-theme-20161019.1949/gruvbox-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gruvbox-theme" "20161019.1949" "A retro-groove colour theme for Emacs" 'nil :url "http://github.com/Greduan/emacs-theme-gruvbox")
--- elpa/gruvbox-theme-20161228.1532/gruvbox-theme-autoloads.el
+++ elpa/gruvbox-theme-20161228.1532/gruvbox-theme-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "gruvbox-theme" "gruvbox-theme.el" (22538 11515
+;;;### (autoloads nil "gruvbox-theme" "gruvbox-theme.el" (22628 53733
 ;;;;;;  0 0))
 ;;; Generated autoloads from gruvbox-theme.el
 
