bd1eef48d9bc41f51f45d9c59b817cd3f8936ff3
Jasonm23 <jasonm23@gmail.com>
2016-12-30 08:27:31 +0700

    [Updating deferred version: 20161228.1930][removing old version: 20160109.2046]

--- elpa/deferred-20161228.1930/deferred-autoloads.el
+++ elpa/deferred-20161228.1930/deferred-autoloads.el
@@ -1,9 +1,9 @@
 ;;; deferred-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("deferred.el") (22168 61982 532764 516000))
+;;;### (autoloads nil nil ("deferred.el") (22628 53718 0 0))
 
 ;;;***
 
