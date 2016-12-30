9c885559f67d71a70b3bc1364c925c851ce33c8e
Jasonm23 <jasonm23@gmail.com>
2016-12-30 08:27:38 +0700

    [Updating tao-theme version: 20161228.743][removing old version: 20161213.739]

--- elpa/tao-theme-20161213.739/tao-theme-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "tao-theme" "20161213.739" "Light & dark themes with greyscale palettes generated from the golden mean" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/tao-theme-20161228.743/tao-theme-pkg.el
@@ -0,0 +1,4 @@
+(define-package "tao-theme" "20161228.743" "Light & dark themes with greyscale palettes generated from the golden mean" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
