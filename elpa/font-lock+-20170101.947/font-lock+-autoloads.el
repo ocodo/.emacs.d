afd9c974573824817bcbfcf7e3dd6fd953991f40
JasonM23 <jasonm23@gmail.com>
2017-01-03 05:57:31 -0500

    [Updating font-lock+ version: 20170101.947][removing old version: 20151231.1319]

--- elpa/font-lock+-20170101.947/font-lock+-autoloads.el
+++ elpa/font-lock+-20170101.947/font-lock+-autoloads.el
@@ -3,7 +3,10 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("font-lock+.el") (22519 51859 644542 3000))
+;;;### (autoloads nil "font-lock+" "font-lock+.el" (0 0 0 0))
+;;; Generated autoloads from font-lock+.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "font-lock+" '("font-lock-" "put-text-property-unless-ignore")))
 
 ;;;***
 
