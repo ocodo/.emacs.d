d4fa1a3c9e4a3a80c3d65da075e6061bb2c649aa
JasonM23 <jasonm23@gmail.com>
2017-01-03 05:57:35 -0500

    [Updating help-mode+ version: 20170101.1004][removing old version: 20151231.1331]

--- elpa/help-mode+-20151231.1331/help-mode+-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "help-mode+" "20151231.1331" "Extensions to `help-mode.el'" 'nil :url "http://www.emacswiki.org/help-mode+.el" :keywords '("help"))
---
+++ elpa/help-mode+-20170101.1004/help-mode+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "help-mode+" "20170101.1004" "Extensions to `help-mode.el'" 'nil :url "http://www.emacswiki.org/help-mode+.el" :keywords '("help"))
