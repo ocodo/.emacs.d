0f02c965651b0ea09331d36ebb450c7131a1c76b
JasonM23 <jasonm23@gmail.com>
2017-01-03 05:57:36 -0500

    [Updating hide-comnt version: 20170101.1008][removing old version: 20161227.1008]

--- elpa/hide-comnt-20161227.1008/hide-comnt-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "hide-comnt" "20161227.1008" "Hide/show comments in code." 'nil :url "http://www.emacswiki.org/hide-comnt.el" :keywords '("comment" "hide" "show"))
