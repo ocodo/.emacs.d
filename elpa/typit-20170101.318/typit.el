dfb79792284bdbfb938e6932ebac9f4b4c687e65
JasonM23 <jasonm23@gmail.com>
2017-01-03 05:57:43 -0500

    [Updating typit version: 20170101.318][removing old version: 20161125.1205]

--- elpa/typit-20170101.318/typit.el
+++ elpa/typit-20170101.318/typit.el
@@ -1,6 +1,6 @@
 ;;; typit.el --- Typing game similar to tests on 10 fast fingers -*- lexical-binding: t; -*-
 ;;
-;; Copyright © 2016 Mark Karpov <markkarpov@openmailbox.org>
+;; Copyright © 2016–2017 Mark Karpov <markkarpov@openmailbox.org>
 ;;
 ;; Author: Mark Karpov <markkarpov@openmailbox.org>
 ;; URL: https://github.com/mrkkrp/typit
