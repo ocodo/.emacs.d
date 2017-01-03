5004696b9003eac21b2caca9267a442c15920963
JasonM23 <jasonm23@gmail.com>
2017-01-03 05:57:41 -0500

    [Updating showkey version: 20170101.1132][removing old version: 20161027.653]

--- elpa/showkey-20161027.653/showkey-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "showkey" "20161027.653" "Show keys as you use them." 'nil :url "http://www.emacswiki.org/showkey.el" :keywords '("help" "keys" "mouse"))
