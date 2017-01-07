e8aaf5b9aca1ca6825b3d2e4f2b6171d9f1e082e
JasonM23 <jasonm23@gmail.com>
2017-01-07 04:55:13 -0500

    [Updating creamsody-theme version: 20170105.2029][removing old version: 20161231.2153]

---
+++ elpa/creamsody-theme-20170105.2029/creamsody-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "creamsody-theme" "20170105.2029" "Straight from the soda fountain." '((autothemer "0.2")) :url "http://github.com/emacsfodder/emacs-theme-creamsody")
--- elpa/creamsody-theme-20170105.2029/creamsody-theme.el
+++ elpa/creamsody-theme-20170105.2029/creamsody-theme.el
@@ -4,8 +4,8 @@
 
 ;; Authors: Jason Milkins <jasonm23@gmail.com>
 ;; URL: http://github.com/emacsfodder/emacs-theme-creamsody
-;; Package-Version: 20161231.2153
-;; Version: 0.3.4
+;; Package-Version: 20170105.2029
+;; Version: 0.3.6
 ;; Package-Requires: ((autothemer "0.2"))
 
 ;;; Commentary:
@@ -359,12 +359,12 @@
   (powerline-inactive2                       (:background creamsody-background2 :inherit 'mode-line-inactive))
 
   ;; MODE SUPPORT: spaceline)
-  (spaceline-evil-normal                     (:background creamsody-bright_blue :foreground creamsody-dark0))
-  (spaceline-evil-insert                     (:background creamsody-bright_yellow :foreground creamsody-dark0))
-  (spaceline-evil-visual                     (:background creamsody-bright_purple :foreground creamsody-dark0))
-  (spaceline-evil-motion                     (:background creamsody-bright_green :foreground creamsody-dark0))
-  (spaceline-evil-replace                    (:background creamsody-bright_orange :foreground creamsody-dark0))
-  (spaceline-evil-emacs                      (:background creamsody-bright_red :foreground creamsody-dark0))
+  (spaceline-evil-normal                     (:background creamsody-bright_blue :foreground creamsody-background0))
+  (spaceline-evil-insert                     (:background creamsody-bright_yellow :foreground creamsody-background0))
+  (spaceline-evil-visual                     (:background creamsody-bright_purple :foreground creamsody-background0))
+  (spaceline-evil-motion                     (:background creamsody-bright_green :foreground creamsody-background0))
+  (spaceline-evil-replace                    (:background creamsody-bright_orange :foreground creamsody-background0))
+  (spaceline-evil-emacs                      (:background creamsody-bright_red :foreground creamsody-background0))
 
   ;; MODE SUPPORT: smart-mode-line)
   (sml/modes                                 (:foreground creamsody-foreground0_hard :weight 'bold :bold t))
