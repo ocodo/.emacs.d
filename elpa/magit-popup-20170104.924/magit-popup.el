198a2713a3e4d5bc4f7b07f724f2755fd2411f2e
JasonM23 <jasonm23@gmail.com>
2017-01-07 04:55:20 -0500

    [Updating magit-popup version: 20170104.924][removing old version: 20161227.1257]

--- elpa/magit-popup-20170104.924/magit-popup.el
+++ elpa/magit-popup-20170104.924/magit-popup.el
@@ -1,6 +1,6 @@
 ;;; magit-popup.el --- Define prefix-infix-suffix command combos  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2010-2016  The Magit Project Contributors
+;; Copyright (C) 2010-2017  The Magit Project Contributors
 ;;
 ;; You should have received a copy of the AUTHORS.md file which
 ;; lists all contributors.  If not, see http://magit.vc/authors.
@@ -12,7 +12,7 @@
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 
-;; Package-Requires: ((emacs "24.4") (async "20160711.223") (dash "20160820.501"))
+;; Package-Requires: ((emacs "24.4") (async "20161103.1036") (dash "20161121.55"))
 ;; Keywords: bindings
 ;; Homepage: https://github.com/magit/magit
 
