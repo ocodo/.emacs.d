234eb4177ea8f36afb26c72225ad4c4682bbcdbf
jasonm23 <jasonm23@gmail.com>
2017-01-17 18:48:19 +0800

    [Updating helm-projectile version: 20170113.209][removing old version: 20161213.2311]

--- elpa/helm-projectile-20170113.209/helm-projectile-autoloads.el
+++ elpa/helm-projectile-20170113.209/helm-projectile-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "helm-projectile" "helm-projectile.el" (22613
-;;;;;;  12864 548839 805000))
+;;;### (autoloads nil "helm-projectile" "helm-projectile.el" (22653
+;;;;;;  62729 558278 973000))
 ;;; Generated autoloads from helm-projectile.el
 
 (defvar helm-projectile-fuzzy-match t "\
