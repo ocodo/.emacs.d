830b082403f236ba0b658130d6f7839b8c6ebacc
jasonm23 <jasonm23@gmail.com>
2017-01-17 18:48:20 +0800

    [Updating ivy version: 20170109.626][removing old version: 20170105.711]

--- elpa/ivy-20170109.626/ivy-autoloads.el
+++ elpa/ivy-20170109.626/ivy-autoloads.el
@@ -3,14 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "colir" "colir.el" (0 0 0 0))
-;;; Generated autoloads from colir.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "colir" '("colir-")))
-
-;;;***
-
-;;;### (autoloads nil "ivy" "ivy.el" (0 0 0 0))
+;;;### (autoloads nil "ivy" "ivy.el" (22653 62712 609921 182000))
 ;;; Generated autoloads from ivy.el
 
 (autoload 'ivy-resume "ivy" "\
@@ -120,18 +113,10 @@ Switch to another buffer in another window.
 
 \(fn)" t nil)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ivy" '("ivy-" "with-ivy-window")))
-
 ;;;***
 
-;;;### (autoloads nil "ivy-overlay" "ivy-overlay.el" (0 0 0 0))
-;;; Generated autoloads from ivy-overlay.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ivy-overlay" '("ivy-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("ivy-pkg.el") (0 0 0 0))
+;;;### (autoloads nil nil ("colir.el" "ivy-overlay.el" "ivy-pkg.el")
+;;;;;;  (22653 62712 585920 677000))
 
 ;;;***
 
