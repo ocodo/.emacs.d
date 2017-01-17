6a5d5b5ae1d3e2063c3cc5504bbfba6b2955a570
jasonm23 <jasonm23@gmail.com>
2017-01-17 18:48:18 +0800

    [Updating helm-gtags version: 20170115.2129][removing old version: 20160917.2238]

---
+++ elpa/helm-gtags-20170115.2129/helm-gtags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-gtags" "20170115.2129" "GNU GLOBAL helm interface" '((emacs "24.4") (helm "2.0")) :url "https://github.com/syohex/emacs-helm-gtags")
