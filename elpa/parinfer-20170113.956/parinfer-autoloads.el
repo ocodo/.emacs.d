cc7bc35f8452c87f37ff4dff233872b964490acc
JasonM23 <jasonm23@gmail.com>
2017-01-28 00:57:13 -0500

    [Updating parinfer version: 20170126.2111][removing old version: 20170113.956]

--- elpa/parinfer-20170113.956/parinfer-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; parinfer-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "parinfer" "parinfer.el" (22653 62682 285288
-;;;;;;  940000))
-;;; Generated autoloads from parinfer.el
-
-(autoload 'parinfer-mode "parinfer" "\
-Parinfer mode.
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'parinfer-region-mode "parinfer" "\
-Available when region is active.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("parinfer-ext.el" "parinfer-pkg.el" "parinferlib.el")
-;;;;;;  (22653 62682 297289 189000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; parinfer-autoloads.el ends here
--- elpa/parinfer-20170126.2111/parinfer-pkg.el
+++ elpa/parinfer-20170126.2111/parinfer-pkg.el
@@ -1,4 +1,4 @@
-(define-package "parinfer" "20170113.956" "Simpler Lisp editing"
+(define-package "parinfer" "20170126.2111" "Simpler Lisp editing"
   '((dash "2.13.0")
     (cl-lib "0.5"))
   :url "https://github.com/DogLooksGood/parinfer-mode" :keywords
