6ec95224f6508da3fa6b285a28d0f23ee2d4b8b5
JasonM23 <jasonm23@gmail.com>
2017-01-28 00:57:20 -0500

    [Updating spacemacs-theme version: 20170127.457][removing old version: 20170106.539]

--- elpa/spacemacs-theme-20170127.457/spacemacs-theme-pkg.el
+++ elpa/spacemacs-theme-20170127.457/spacemacs-theme-pkg.el
@@ -1,4 +1,4 @@
-(define-package "spacemacs-theme" "20170106.539" "Color theme with a dark and light versions" 'nil :keywords
+(define-package "spacemacs-theme" "20170127.457" "Color theme with a dark and light versions" 'nil :keywords
   '("color" "theme")
   :url "https://github.com/nashamri/spacemacs-theme")
 ;; Local Variables:
