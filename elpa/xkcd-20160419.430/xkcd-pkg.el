f5b2d1c1c6f6cd837572157d1f4e68d206379d0b
Jasonm23 <jasonm23@gmail.com>
2017-01-30 21:08:53 +0800

    [Adding xkcd version: 20160419.430]

---
+++ elpa/xkcd-20160419.430/xkcd-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "xkcd" "20160419.430" "View xkcd from Emacs" '((json "1.3")) :url "https://github.com/vibhavp/emacs-xkcd" :keywords '("xkcd" "webcomic"))
