e99e62c5e0ac0936b9879727d79200054bd0e11a
Jasonm23 <jasonm23@gmail.com>
2017-01-30 21:08:48 +0800

    [Adding bar-cursor version: 20160923.920]

---
+++ elpa/bar-cursor-20160923.920/bar-cursor-autoloads.el
@@ -0,0 +1,35 @@
+;;; bar-cursor-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "bar-cursor" "bar-cursor.el" (22670 43788 0
+;;;;;;  0))
+;;; Generated autoloads from bar-cursor.el
+
+(defvar bar-cursor-mode nil "\
+Non-nil if Bar-Cursor mode is enabled.
+See the `bar-cursor-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `bar-cursor-mode'.")
+
+(custom-autoload 'bar-cursor-mode "bar-cursor" nil)
+
+(autoload 'bar-cursor-mode "bar-cursor" "\
+Toggle use of 'bar-cursor-mode'.
+
+This global minor mode changes cursor to a bar cursor in insert
+mode, and a block cursor in overwrite mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; bar-cursor-autoloads.el ends here
---
+++ elpa/bar-cursor-20160923.920/bar-cursor-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bar-cursor" "20160923.920" "package used to switch block cursor to a bar" 'nil :url "https://github.com/ajsquared/bar-cursor" :keywords '("files"))
