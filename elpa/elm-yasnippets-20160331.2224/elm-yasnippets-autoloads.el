b3b8fbd8414c75707c9f33429a269372b743fbaa
Jasonm23 <jasonm23@gmail.com>
2017-01-30 21:08:49 +0800

    [Adding elm-yasnippets version: 20160331.2224]

---
+++ elpa/elm-yasnippets-20160331.2224/elm-yasnippets-autoloads.el
@@ -0,0 +1,29 @@
+;;; elm-yasnippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "elm-yasnippets" "elm-yasnippets.el" (22670
+;;;;;;  854 0 0))
+;;; Generated autoloads from elm-yasnippets.el
+
+(autoload 'elm-snippets-initialize "elm-yasnippets" "\
+
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(elm-snippets-initialize))
+
+;;;***
+
+;;;### (autoloads nil nil ("elm-yasnippets-pkg.el") (22670 854 0
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; elm-yasnippets-autoloads.el ends here
---
+++ elpa/elm-yasnippets-20160331.2224/elm-yasnippets-pkg.el
@@ -0,0 +1,7 @@
+(define-package "elm-yasnippets" "20160331.2224" "Yasnippets for Elm"
+  '((yasnippet "0.8.0"))
+  :keywords
+  '("snippets"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
