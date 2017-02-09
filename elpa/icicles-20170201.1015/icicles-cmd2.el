28bf9b9ba934cbe73ad2a0201427a66e3bcfc57e
JasonM23 <jasonm23@gmail.com>
2017-02-09 08:52:35 -0500

    [Updating icicles version: 20170201.1015][removing old version: 20170126.851]

--- elpa/icicles-20170201.1015/icicles-cmd2.el
+++ elpa/icicles-20170201.1015/icicles-cmd2.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 1996-2017, Drew Adams, all rights reserved.
 ;; Created: Thu May 21 13:31:43 2009 (-0700)
-;; Last-Updated: Tue Jan 17 09:09:56 2017 (-0800)
+;; Last-Updated: Wed Feb  1 10:09:49 2017 (-0800)
 ;;           By: dradams
-;;     Update #: 7417
+;;     Update #: 7434
 ;; URL: http://www.emacswiki.org/icicles-cmd2.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -114,11 +114,12 @@
 ;;    (+)`icicle-Info-menu', (+)`icicle-Info-menu-cmd',
 ;;    `icicle-Info-virtual-book' (Emacs 22+),
 ;;    (+)`icicle-insert-thesaurus-entry', (+)`icicle-load-library'
-;;    (Emacs 21+), (+)`icicle-map', `icicle-next-font-lock-keywords'
-;;    (Emacs 22+), `icicle-next-font-lock-keywords-repeat' (Emacs
-;;    22+), `icicle-next-visible-thing',
-;;    `icicle-non-whitespace-string-p', (+)`icicle-object-action',
-;;    (+)`icicle-occur', (+)`icicle-occur-dired-marked',
+;;    (Emacs 21+), (+)`icicle-man' (Emacs 23+), (+)`icicle-map',
+;;    `icicle-next-font-lock-keywords' (Emacs 22+),
+;;    `icicle-next-font-lock-keywords-repeat' (Emacs 22+),
+;;    `icicle-next-visible-thing', `icicle-non-whitespace-string-p',
+;;    (+)`icicle-object-action', (+)`icicle-occur',
+;;    (+)`icicle-occur-dired-marked',
 ;;    (+)`icicle-occur-dired-marked-recursive',
 ;;    (+)`icicle-pick-color-by-name', (+)`icicle-plist',
 ;;    `icicle-previous-visible-thing', `icicle-read-color',
@@ -169,7 +170,8 @@
 ;;    (+)`icicle-show-only-faces', (+)`icicle-synonyms',
 ;;    (+)`icicle-tag-a-file', (+)`icicle-tags-search',
 ;;    (+)`icicle-untag-a-file', (+)`icicle-vardoc',
-;;    (+)`icicle-where-is', (+)`synonyms', (+)`what-which-how'.
+;;    (+)`icicle-where-is', (+)`icicle-woman' (Emacs 22+),
+;;    (+)`synonyms', (+)`what-which-how'.
 ;;
 ;;  Non-interactive functions defined here:
 ;;
@@ -453,6 +455,10 @@
 (defvar icicle-track-pt)                ; In `icicle-insert-thesaurus-entry'
 (defvar imenu-after-jump-hook)          ; In `imenu.el' (Emacs 22+)
 (defvar replace-count)                  ; In `replace.el'
+(defvar woman-expanded-directory-path)  ; In `woman.el'
+(defvar woman-manpath)                  ; In `woman.el'
+(defvar woman-path)                     ; In `woman.el'
+(defvar woman-topic-all-completions)    ; In `woman.el'
 (defvar zz-izones)                      ; In `zones.el'
 (defvar zz-lighter-narrowing-part)      ; In `zones.el'
 
@@ -8469,6 +8475,38 @@ candidates to packages of different kinds."
           (when (fboundp 'Info-make-manuals-xref) ; In `help-fns+.el', for Emacs 23.2+.
             (Info-make-manuals-xref (concat (symbol-name package) " package")
                                     nil nil (not (called-interactively-p 'interactive)))))))))
+
+(when (> emacs-major-version 22)        ; `Man-completion-table'
+
+  (icicle-define-command icicle-man
+    "Multi-command version of `man'."
+    man "Manual entry: " 'Man-completion-table nil nil nil 'Man-topic-history (Man-default-man-entry) nil nil
+    (require 'man))
+
+  )
+
+(when (> emacs-major-version 21)
+
+  (icicle-define-command icicle-woman
+    "Multi-command version of `woman'."
+    woman "Manual entry: " woman-topic-all-completions nil 1 nil 'woman-topic-history
+    (let* ((word-at-point  (current-word))
+           (default        (and word-at-point
+                                (test-completion word-at-point woman-topic-all-completions)
+                                word-at-point)))
+      default)
+    nil nil
+    (when (require 'woman nil t)
+      (unless (or (and woman-expanded-directory-path  woman-topic-all-completions)
+                  (woman-read-directory-cache))
+        (message "Building list of manual directory expansions...")
+        (setq woman-expanded-directory-path  (woman-expand-directory-path woman-manpath woman-path))
+        (message "Building completion list of all manual topics...")
+        (setq woman-topic-all-completions  (woman-topic-all-completions woman-expanded-directory-path))
+        (message "Building completion list of all manual topics...done")
+        (woman-write-directory-cache))))
+
+  )
 
 (defun icicle-buffer-narrowing (&optional variable not-buf-local-p set-var-p)
   "Choose a narrowing (buffer restriction) and apply it.
--- elpa/icicles-20170201.1015/icicles-pkg.el
+++ elpa/icicles-20170201.1015/icicles-pkg.el
@@ -1,4 +1,4 @@
-(define-package "icicles" "20170126.851" "Minibuffer input completion and cycling." 'nil :url "http://www.emacswiki.org/icicles.el" :keywords
+(define-package "icicles" "20170201.1015" "Minibuffer input completion and cycling." 'nil :url "http://www.emacswiki.org/icicles.el" :keywords
   '("extensions" "help" "abbrev" "local" "minibuffer" "projects" "keys" "apropos" "completion" "matching" "regexp" "command"))
 ;; Local Variables:
 ;; no-byte-compile: t
