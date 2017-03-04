b475addfcc607ce6831446b0e77a97272311edb0
JasonM23 <jasonm23@gmail.com>
2017-03-03 19:43:12 -0500

    [Updating header2 version: 20170223.729][removing old version: 20170101.954]

--- elpa/header2-20170101.954/header2-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "header2" "20170101.954" "Support for creation and update of file headers." 'nil :url "http://www.emacswiki.org/header2.el" :keywords '("tools" "docs" "maint" "abbrev" "local"))
---
+++ elpa/header2-20170223.729/header2-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "header2" "20170223.729" "Support for creation and update of file headers." 'nil :url "https://www.emacswiki.org/emacs/download/header2.el" :keywords '("tools" "docs" "maint" "abbrev" "local"))
