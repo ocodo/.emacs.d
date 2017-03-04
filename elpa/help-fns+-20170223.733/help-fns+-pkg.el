bf40ad17cbf8b054af6d442b7461ba3ac7d07466
JasonM23 <jasonm23@gmail.com>
2017-03-03 19:43:17 -0500

    [Updating help-fns+ version: 20170223.733][removing old version: 20170101.959]

---
+++ elpa/help-fns+-20170223.733/help-fns+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "help-fns+" "20170223.733" "Extensions to `help-fns.el'." 'nil :url "https://www.emacswiki.org/emacs/download/help-fns%2b.el" :keywords '("help" "faces" "characters" "packages" "description"))
