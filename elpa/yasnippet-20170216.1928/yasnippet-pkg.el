3c0c1486bce13cc864594afa7b80a3ef79f66891
JasonM23 <jasonm23@gmail.com>
2017-03-03 19:43:49 -0500

    [Updating yasnippet version: 20170216.1928][removing old version: 20170203.626]

--- elpa/yasnippet-20170216.1928/yasnippet-pkg.el
+++ elpa/yasnippet-20170216.1928/yasnippet-pkg.el
@@ -1,4 +1,4 @@
-(define-package "yasnippet" "20170203.626" "Yet another snippet extension for Emacs."
+(define-package "yasnippet" "20170216.1928" "Yet another snippet extension for Emacs."
   '((cl-lib "0.5"))
   :url "http://github.com/joaotavora/yasnippet" :keywords
   '("convenience" "emulation"))
