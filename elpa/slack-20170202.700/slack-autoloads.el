1e87cdacc14dc2647fe6f19d5926c9002be32bbe
JasonM23 <jasonm23@gmail.com>
2017-03-03 19:43:40 -0500

    [Updating slack version: 20170228.602][removing old version: 20170202.700]

--- elpa/slack-20170202.700/slack-autoloads.el
+++
@@ -1,222 +0,0 @@
-;;; slack-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "slack" "slack.el" (0 0 0 0))
-;;; Generated autoloads from slack.el
-
-(autoload 'slack-start "slack" "\
-
-
-\(fn &optional TEAM)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-bot-message" "slack-bot-message.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from slack-bot-message.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-bot-message" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-buffer" "slack-buffer.el" (0 0 0 0))
-;;; Generated autoloads from slack-buffer.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-buffer" '("slack-" "lui-prompt-string")))
-
-;;;***
-
-;;;### (autoloads nil "slack-channel" "slack-channel.el" (0 0 0 0))
-;;; Generated autoloads from slack-channel.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-channel" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-file" "slack-file.el" (0 0 0 0))
-;;; Generated autoloads from slack-file.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-file" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-group" "slack-group.el" (0 0 0 0))
-;;; Generated autoloads from slack-group.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-group" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-im" "slack-im.el" (0 0 0 0))
-;;; Generated autoloads from slack-im.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-im" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-message" "slack-message.el" (0 0 0 0))
-;;; Generated autoloads from slack-message.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-message" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-message-delete" "slack-message-delete.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from slack-message-delete.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-message-delete" '("slack-" "_slack-message-delete")))
-
-;;;***
-
-;;;### (autoloads nil "slack-message-editor" "slack-message-editor.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from slack-message-editor.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-message-editor" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-message-formatter" "slack-message-formatter.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from slack-message-formatter.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-message-formatter" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-message-notification" "slack-message-notification.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from slack-message-notification.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-message-notification" '("slack-message-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-message-reaction" "slack-message-reaction.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from slack-message-reaction.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-message-reaction" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-message-sender" "slack-message-sender.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from slack-message-sender.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-message-sender" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-reaction" "slack-reaction.el" (0 0 0
-;;;;;;  0))
-;;; Generated autoloads from slack-reaction.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-reaction" '("slack-reaction")))
-
-;;;***
-
-;;;### (autoloads nil "slack-reminder" "slack-reminder.el" (0 0 0
-;;;;;;  0))
-;;; Generated autoloads from slack-reminder.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-reminder" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-reply" "slack-reply.el" (0 0 0 0))
-;;; Generated autoloads from slack-reply.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-reply" '("slack-message-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-request" "slack-request.el" (0 0 0 0))
-;;; Generated autoloads from slack-request.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-request" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-room" "slack-room.el" (0 0 0 0))
-;;; Generated autoloads from slack-room.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-room" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-search" "slack-search.el" (0 0 0 0))
-;;; Generated autoloads from slack-search.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-search" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-team" "slack-team.el" (0 0 0 0))
-;;; Generated autoloads from slack-team.el
-
-(autoload 'slack-register-team "slack-team" "\
-PLIST must contain :name :client-id :client-secret with value.
-setting :token will reduce your configuration step.
-you will notified when receive message with channel included in subscribed-chennels.
-if :default is t and `slack-prefer-current-team' is t, skip selecting team when channels listed.
-you can change current-team with `slack-change-current-team'
-
-\(fn &rest PLIST)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-team" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-thread" "slack-thread.el" (0 0 0 0))
-;;; Generated autoloads from slack-thread.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-thread" '("slack-" "all-threads-url" "lui-prompt-string")))
-
-;;;***
-
-;;;### (autoloads nil "slack-user" "slack-user.el" (0 0 0 0))
-;;; Generated autoloads from slack-user.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-user" '("slack-user-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-user-message" "slack-user-message.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from slack-user-message.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-user-message" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil "slack-util" "slack-util.el" (0 0 0 0))
-;;; Generated autoloads from slack-util.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-util" '("slack-" "company-slack-backend")))
-
-;;;***
-
-;;;### (autoloads nil "slack-websocket" "slack-websocket.el" (0 0
-;;;;;;  0 0))
-;;; Generated autoloads from slack-websocket.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slack-websocket" '("slack-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("slack-pkg.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; slack-autoloads.el ends here
---
+++ elpa/slack-20170228.602/slack-message-formatter.el
@@ -0,0 +1,265 @@
+;;; slack-message-formatter.el --- format message text  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  yuya.minami
+
+;; Author: yuya.minami <yuya.minami@yuyaminami-no-MacBook-Pro.local>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'eieio)
+(require 'slack-user)
+(require 'slack-room)
+
+(defface slack-message-output-text
+  '((t (:weight normal :height 0.9)))
+  "Face used to text message."
+  :group 'slack)
+
+(defface slack-message-output-header
+  '((t (:foreground "#FFA000"
+                    :weight bold
+                    :height 1.0
+                    :underline t)))
+  "Face used to text message."
+  :group 'slack)
+
+(defface slack-message-output-reaction
+  '((t (:overline t)))
+  "Face used to reactions."
+  :group 'slack)
+
+(defface slack-message-deleted-face
+  '((t (:strike-through t)))
+  "Face used to deleted message."
+  :group 'slack)
+
+(defface slack-attachment-header
+  '((t (:weight bold)))
+  "Face used to shared message header."
+  :group 'slack)
+
+(defface slack-attachment-footer
+  '((t (:height 0.8)))
+  "Face used to shared message footer."
+  :group 'slack)
+
+(defface slack-attachment-pad
+  '((t (:weight ultra-bold)))
+  "Face used to shared message pad."
+  :group 'slack)
+
+(defface slack-attachment-field-title
+  '((t (:weight bold :height 1.0)))
+  "Face used to attachment field title."
+  :group 'slack)
+
+(defun slack-message-put-header-property (header)
+  (if header
+      (propertize header 'face 'slack-message-output-header)))
+
+(defun slack-message-put-text-property (text)
+  (if text
+      (propertize text 'face 'slack-message-output-text)))
+
+(defun slack-message-put-reactions-property (text)
+  (if text
+      (propertize text 'face 'slack-message-output-reaction)))
+
+(defun slack-message-put-hard (text)
+  (if text
+      (propertize text 'hard t)))
+
+(defun slack-message-put-deleted-property (text)
+  (if text
+      (propertize text 'face 'slack-message-deleted-face)))
+
+(defmethod slack-message-propertize ((m slack-message) text)
+  text)
+
+(defun slack-message-time-to-string (ts)
+  (when ts
+    (when (stringp ts)
+      (setf ts (string-to-number ts)))
+    (format-time-string "%Y-%m-%d %H:%M:%S"
+                        (seconds-to-time ts))))
+
+(defun slack-message-reactions-to-string (reactions)
+  (if reactions
+      (concat "\n" (mapconcat #'slack-reaction-to-string reactions " "))))
+
+(defmethod slack-message-header ((m slack-message) team)
+  (slack-message-sender-name m team))
+
+(defun slack-format-message (header body attachment-body reactions thread)
+  (let ((messages (list header body attachment-body thread reactions)))
+    (concat (mapconcat #'identity
+                       (cl-remove-if #'(lambda (e) (< (length e) 1)) messages)
+                       "\n")
+            "\n")))
+
+(defmethod slack-message-to-string ((m slack-message) team)
+  (let ((text (if (slot-boundp m 'text)
+                  (oref m text))))
+    (let* ((header (slack-message-put-header-property
+                    (slack-message-header m team)))
+           (row-body (slack-message-body m team))
+           (attachment-body (slack-message-attachment-body m team))
+           (body (if (oref m deleted-at)
+                     (slack-message-put-deleted-property row-body)
+                   (slack-message-put-text-property row-body)))
+           (reactions-str
+            (slack-message-put-reactions-property
+             (slack-message-reactions-to-string
+              (slack-message-get-reactions m))))
+           (thread (slack-thread-to-string m team)))
+      (slack-message-propertize
+       m (slack-format-message header body attachment-body reactions-str thread)))))
+
+(defmethod slack-message-body ((m slack-message) team)
+  (with-slots (text) m
+    (slack-message-unescape-string text team)))
+
+(defmethod slack-message-body ((_m slack-reply-broadcast-message) _team)
+  "Replied to a thread")
+
+(defmethod slack-message-attachment-body ((m slack-message) team)
+  (with-slots (attachments) m
+    (let ((body (mapconcat #'slack-attachment-to-string attachments "\n\t-\n")))
+      (if (< 0 (length body))
+          (slack-message-unescape-string body team)))))
+
+(defmethod slack-message-to-alert ((m slack-message) team)
+  (with-slots (text) m
+    (slack-message-unescape-string text team)))
+
+(defun slack-message-unescape-string (text team)
+  (when text
+    (let* ((and-unescpaed
+            (replace-regexp-in-string "&amp;" "&" text))
+           (lt-unescaped
+            (replace-regexp-in-string "&lt;" "<" and-unescpaed))
+           (gt-unescaped
+            (replace-regexp-in-string "&gt;" ">" lt-unescaped)))
+      (slack-message-unescape-command
+       (slack-message-unescape-user-id
+        (slack-message-unescape-channel gt-unescaped)
+        team)))))
+
+(defun slack-message-unescape-user-id (text team)
+  (let ((user-regexp "<@\\(U.*?\\)>"))
+    (cl-labels ((unescape-user-id
+                 (text)
+                 (concat "@" (or
+                              (slack-message-replace-user-name text)
+                              (slack-user-name (match-string 1 text) team)
+                              (match-string 1 text)))))
+      (replace-regexp-in-string user-regexp
+                                #'unescape-user-id
+                                text t))))
+
+(defun slack-message-replace-user-name (text)
+  (let ((user-name-regexp "<@U.*?|\\(.*?\\)>"))
+    (cl-labels ((replace-user-id-with-name (text)
+                                           (match-string 1 text)))
+      (if (string-match-p user-name-regexp text)
+          (replace-regexp-in-string user-name-regexp
+                                    #'replace-user-id-with-name
+                                    text)))))
+
+(defun slack-message-unescape-command (text)
+  (let ((command-regexp "<!\\(.*?\\)>"))
+    (cl-labels ((unescape-command
+                 (text)
+                 (concat "@" (match-string 1 text))))
+      (replace-regexp-in-string command-regexp
+                                #'unescape-command
+                                text))))
+
+(defun slack-message-unescape-channel (text)
+  (let ((channel-regexp "<#\\(C.*?\\)|\\(.*?\\)>"))
+    (cl-labels ((unescape-channel
+                 (text)
+                 (concat "#" (or (match-string 2 text)
+                                 (slack-room-find
+                                  (match-string 1 text))
+                                 (match-string 1 text)))))
+      (replace-regexp-in-string channel-regexp
+                                #'unescape-channel
+                                text t))))
+
+(defmethod slack-attachment-header ((attachment slack-attachment))
+  (with-slots (title title-link author-name author-subname) attachment
+    (concat (or (and title title-link (slack-linkfy title title-link))
+                title
+                "")
+            (or author-name author-subname ""))))
+
+(defmethod slack-attachment-to-string ((attachment slack-attachment))
+  (with-slots
+      (fallback text ts color from-url footer fields pretext) attachment
+    (let* ((pad-raw (propertize "|" 'face 'slack-attachment-pad))
+           (pad (or (and color (propertize pad-raw 'face (list :foreground (concat "#" color))))
+                    pad-raw))
+           (header-raw (slack-attachment-header attachment))
+           (header (and (not (slack-string-blankp header-raw))
+                        (format "%s\t%s" pad
+                                (propertize header-raw
+                                            'face 'slack-attachment-header))))
+           (pretext (and pretext (format "%s\t%s" pad pretext)))
+           (body (and text (format "%s\t%s" pad (mapconcat #'identity
+                                                           (split-string text "\n")
+                                                           (format "\n\t%s\t" pad)))))
+           (fields (if fields (mapconcat #'(lambda (field)
+                                             (slack-attachment-field-to-string field
+                                                                               (format "\t%s" pad)))
+                                         fields
+                                         (format "\n\t%s\n" pad))))
+           (footer (if (and footer ts)
+                       (format "%s\t%s"
+                               pad
+                               (propertize
+                                (format "%s|%s" footer
+                                        (slack-message-time-to-string ts))
+                                'face 'slack-attachment-footer)))))
+      (if (and (slack-string-blankp header)
+               (slack-string-blankp pretext)
+               (slack-string-blankp body)
+               (slack-string-blankp fields)
+               (slack-string-blankp footer))
+          fallback
+        (format "%s%s%s%s%s"
+                (or (and header (format "\t%s\n" header)) "")
+                (or (and pretext (format "\t%s\n" pretext)) "")
+                (or (and body (format "\t%s" body)) "")
+                (or (and fields fields) "")
+                (or (and footer (format "\n\t%s" footer)) ""))
+        ))))
+
+(defmethod slack-attachment-field-to-string ((field slack-attachment-field) &optional pad)
+  (unless pad (setq pad ""))
+  (let ((title (propertize (or (oref field title) "") 'face 'slack-attachment-field-title))
+        (value (mapconcat #'(lambda (e) (format "\t%s" e))
+                          (split-string (or (oref field value) "") "\n")
+                          (format "\n%s\t" pad))))
+    (format "%s\t%s\n%s\t%s" pad title pad value)))
+
+(provide 'slack-message-formatter)
+;;; slack-message-formatter.el ends here
