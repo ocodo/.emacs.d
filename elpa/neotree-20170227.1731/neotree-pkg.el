161d4e0fa473ed26927dfe93a06eeefe4a57f55a
JasonM23 <jasonm23@gmail.com>
2017-03-03 19:43:32 -0500

    [Updating neotree version: 20170227.1731][removing old version: 20170206.804]

--- elpa/neotree-20170227.1731/neotree-pkg.el
+++ elpa/neotree-20170227.1731/neotree-pkg.el
@@ -1,4 +1,4 @@
-(define-package "neotree" "20170206.804" "A tree plugin like NerdTree for Vim"
+(define-package "neotree" "20170227.1731" "A tree plugin like NerdTree for Vim"
   '((cl-lib "0.5"))
   :url "https://github.com/jaypei/emacs-neotree")
 ;; Local Variables:
