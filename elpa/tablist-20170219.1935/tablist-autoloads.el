f741bdcdd52880973ab8b2d902979ce570057d3d
JasonM23 <jasonm23@gmail.com>
2017-03-03 19:43:42 -0500

    [Updating tablist version: 20170219.1935][removing old version: 20160424.235]

--- elpa/tablist-20170219.1935/tablist-autoloads.el
+++ elpa/tablist-20170219.1935/tablist-autoloads.el
@@ -3,8 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "tablist" "tablist.el" (22519 50649 138885
-;;;;;;  318000))
+;;;### (autoloads nil "tablist" "tablist.el" (0 0 0 0))
 ;;; Generated autoloads from tablist.el
 
 (autoload 'tablist-minor-mode "tablist" "\
@@ -21,10 +20,19 @@ the mode if ARG is omitted or nil, and toggle it if ARG is `toggle'.
 
 \(fn)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tablist" '("tablist-")))
+
 ;;;***
 
-;;;### (autoloads nil nil ("tablist-filter.el" "tablist-pkg.el")
-;;;;;;  (22519 50649 134885 313000))
+;;;### (autoloads nil "tablist-filter" "tablist-filter.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from tablist-filter.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tablist-filter" '("tablist-filter-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("tablist-pkg.el") (0 0 0 0))
 
 ;;;***
 
--- elpa/tablist-20170219.1935/tablist-pkg.el
+++ elpa/tablist-20170219.1935/tablist-pkg.el
@@ -1,4 +1,4 @@
-(define-package "tablist" "20160424.235" "Extended tabulated-list-mode"
+(define-package "tablist" "20170219.1935" "Extended tabulated-list-mode"
   '((emacs "24.3"))
   :keywords
   '("extensions" "lisp"))
