ada5a9e044092257aa3f122eaaa82e442b807ec9
JasonM23 <jasonm23@gmail.com>
2017-03-03 19:43:11 -0500

    [Updating haskell-mode version: 20170228.730][removing old version: 20170209.41]

--- elpa/haskell-mode-20170228.730/haskell-mode-pkg.el
+++ elpa/haskell-mode-20170228.730/haskell-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "haskell-mode" "20170209.41" "A Haskell editing mode"
+(define-package "haskell-mode" "20170228.730" "A Haskell editing mode"
   '((emacs "24.3"))
   :keywords
   '("haskell" "cabal" "ghc" "repl")
