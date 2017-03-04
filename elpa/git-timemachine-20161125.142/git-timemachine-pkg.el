aac73ef8521c5ab1b0de4d8c86a4753d395c5c6a
JasonM23 <jasonm23@gmail.com>
2017-03-03 19:43:07 -0500

    [Updating git-timemachine version: 20170226.24][removing old version: 20161125.142]

--- elpa/git-timemachine-20161125.142/git-timemachine-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-timemachine" "20161125.142" "Walk through git revisions of a file" '((emacs "24.3")) :url "https://github.com/pidu/git-timemachine" :keywords '("git"))
