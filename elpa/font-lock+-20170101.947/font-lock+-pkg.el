9de01bc2609f5e2ad372de40a8c0295315315286
JasonM23 <jasonm23@gmail.com>
2017-03-03 19:43:06 -0500

    [Updating font-lock+ version: 20170222.1755][removing old version: 20170101.947]

--- elpa/font-lock+-20170101.947/font-lock+-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "font-lock+" "20170101.947" "Enhancements to standard library `font-lock.el'." 'nil :url "http://www.emacswiki.org/font-lock+.el" :keywords '("languages" "faces" "highlighting"))
---
+++ elpa/font-lock+-20170222.1755/font-lock+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "font-lock+" "20170222.1755" "Enhancements to standard library `font-lock.el'." 'nil :url "https://www.emacswiki.org/emacs/download/font-lock%2b.el" :keywords '("languages" "faces" "highlighting"))
