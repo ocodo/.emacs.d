793d5ff344a46280536f9b14b302fd81609874cc
JasonM23 <jasonm23@gmail.com>
2017-03-03 19:42:49 -0500

    [Updating anaconda-mode version: 20170214.556][removing old version: 20161121.1137]

--- elpa/anaconda-mode-20170214.556/anaconda-mode.el
+++ elpa/anaconda-mode-20170214.556/anaconda-mode.el
@@ -4,8 +4,8 @@
 
 ;; Author: Artem Malyshev <proofit404@gmail.com>
 ;; URL: https://github.com/proofit404/anaconda-mode
-;; Package-Version: 20161121.1137
-;; Version: 0.1.6
+;; Package-Version: 20170214.556
+;; Version: 0.1.7
 ;; Package-Requires: ((emacs "24") (pythonic "0.1.0") (dash "2.6.0") (s "1.9") (f "0.16.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -94,7 +94,7 @@
 
 ;;; Server.
 
-(defvar anaconda-mode-server-version "0.1.6"
+(defvar anaconda-mode-server-version "0.1.7"
   "Server version needed to run anaconda-mode.")
 
 (defvar anaconda-mode-server-command "
