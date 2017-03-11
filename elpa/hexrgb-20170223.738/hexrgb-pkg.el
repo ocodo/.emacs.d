3662a64ecf7f5ef09eaf4592cf2e47bea8d034f0
JasonM23 <jasonm23@gmail.com>
2017-03-10 19:42:36 -0500

    [Updating hexrgb version: 20170304.1213][removing old version: 20170223.738]

--- elpa/hexrgb-20170223.738/hexrgb-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "hexrgb" "20170223.738" "Functions to manipulate colors, including RGB hex strings." 'nil :url "https://www.emacswiki.org/emacs/download/hexrgb.el" :keywords '("number" "hex" "rgb" "color" "background" "frames" "display"))
---
+++ elpa/hexrgb-20170304.1213/hexrgb-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "hexrgb" "20170304.1213" "Functions to manipulate colors, including RGB hex strings." 'nil :url "https://www.emacswiki.org/emacs/download/hexrgb.el" :keywords '("number" "hex" "rgb" "color" "background" "frames" "display"))
