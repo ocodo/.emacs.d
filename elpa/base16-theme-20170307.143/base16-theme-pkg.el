641d27d8148aae0eb5370fabaddb305e9788b886
JasonM23 <jasonm23@gmail.com>
2017-03-10 19:42:23 -0500

    [Updating base16-theme version: 20170307.143][removing old version: 20170227.1310]

---
+++ elpa/base16-theme-20170307.143/base16-theme-pkg.el
@@ -0,0 +1,4 @@
+(define-package "base16-theme" "20170307.143" "Collection of themes built on combinations of 16 base colors" 'nil :url "https://github.com/belak/base16-emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
