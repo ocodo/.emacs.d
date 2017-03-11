17fce28e0378fd9b1cc4657e65d8b2c8861c46ae
JasonM23 <jasonm23@gmail.com>
2017-03-10 19:42:46 -0500

    [Updating tide version: 20170309.205][removing old version: 20170303.1211]

--- elpa/tide-20170309.205/tide-pkg.el
+++ elpa/tide-20170309.205/tide-pkg.el
@@ -1,4 +1,4 @@
-(define-package "tide" "20170303.1211" "Typescript Interactive Development Environment"
+(define-package "tide" "20170309.205" "Typescript Interactive Development Environment"
   '((dash "2.10.0")
     (flycheck "27")
     (typescript-mode "0.1")
