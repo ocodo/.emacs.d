dc3db8b1a813966ca362aa975211ce2514831bf6
JasonM23 <jasonm23@gmail.com>
2017-04-07 00:26:21 -0400

    [Updating kotlin-mode version: 20170403.826][removing old version: 20160720.1339]

--- elpa/kotlin-mode-20160720.1339/kotlin-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "kotlin-mode" "20160720.1339" "Major mode for kotlin" '((emacs "24.3")) :keywords '("languages"))
---
+++ elpa/kotlin-mode-20170403.826/kotlin-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kotlin-mode" "20170403.826" "Major mode for kotlin" '((emacs "24.3")) :keywords '("languages"))
