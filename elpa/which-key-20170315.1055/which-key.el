c5029323b40dc3dd7f87c399404301d366289461
JasonM23 <jasonm23@gmail.com>
2017-04-07 00:26:42 -0400

    [Updating which-key version: 20170315.1055][removing old version: 20170209.729]

--- elpa/which-key-20170315.1055/which-key.el
+++ elpa/which-key-20170315.1055/which-key.el
@@ -4,7 +4,7 @@
 
 ;; Author: Justin Burkett <justin@burkett.cc>
 ;; URL: https://github.com/justbur/emacs-which-key
-;; Package-Version: 20170209.729
+;; Package-Version: 20170315.1055
 ;; Version: 2.0
 ;; Keywords:
 ;; Package-Requires: ((emacs "24.3"))
@@ -1460,7 +1460,10 @@ alists. Returns a list (key separator description)."
       (let* ((key (car key-binding))
              (orig-desc (cdr key-binding))
              (group (which-key--group-p orig-desc))
-             (keys (concat (which-key--current-key-string) " " key))
+             ;; At top-level prefix is nil
+             (keys (if which-key--current-prefix
+                       (concat (which-key--current-key-string) " " key)
+                     key))
              (local (eq (which-key--safe-lookup-key local-map (kbd keys))
                         (intern orig-desc)))
              (hl-face (which-key--highlight-face orig-desc))
