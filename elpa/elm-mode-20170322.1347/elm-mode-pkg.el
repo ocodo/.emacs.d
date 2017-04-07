feff714f35b5315d805623239301ca97e4e9fbe7
JasonM23 <jasonm23@gmail.com>
2017-04-07 00:26:05 -0400

    [Updating elm-mode version: 20170322.1347][removing old version: 20161210.49]

--- elpa/elm-mode-20170322.1347/elm-mode-pkg.el
+++ elpa/elm-mode-20170322.1347/elm-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "elm-mode" "20161210.49" "Major mode for Elm"
+(define-package "elm-mode" "20170322.1347" "Major mode for Elm"
   '((f "0.17")
     (let-alist "1.0.4")
     (s "1.7.0")
