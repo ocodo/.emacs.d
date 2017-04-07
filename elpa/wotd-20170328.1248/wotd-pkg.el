9ab2f3fc70438163aa4895bff6306b1fc28cb0cc
JasonM23 <jasonm23@gmail.com>
2017-04-06 23:42:49 -0400

    [Adding wotd version: 20170328.1248]

---
+++ elpa/wotd-20170328.1248/wotd-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "wotd" "20170328.1248" "Fetch word-of-the-day from multiple online sources" '((emacs "24.4") (org "8.2.10")) :keywords '("extensions"))
