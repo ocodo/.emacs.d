3b9a286fa3a7d44bebe37a1ce6f64f7546a92131
JasonM23 <jasonm23@gmail.com>
2017-04-07 00:26:19 -0400

    [Updating js2-mode version: 20170330.639][removing old version: 20170221.1636]

--- elpa/js2-mode-20170330.639/js2-mode-pkg.el
+++ elpa/js2-mode-20170330.639/js2-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "js2-mode" "20170221.1636" "Improved JavaScript editing mode"
+(define-package "js2-mode" "20170330.639" "Improved JavaScript editing mode"
   '((emacs "24.1")
     (cl-lib "0.5"))
   :url "https://github.com/mooz/js2-mode/" :keywords
--- elpa/js2-mode-20170330.639/js2-mode.el
+++ elpa/js2-mode-20170330.639/js2-mode.el
@@ -7490,9 +7490,12 @@ returns nil.  Otherwise returns the string name/value of the node."
 Returns nil if the node doesn't contribute."
   (copy-sequence
    (or (js2-prop-node-name node)
-       (if (and (js2-function-node-p node)
-                (js2-function-node-name node))
-           (js2-name-node-name (js2-function-node-name node))))))
+       (cond
+        ((and (js2-function-node-p node)
+              (js2-function-node-name node))
+         (js2-name-node-name (js2-function-node-name node)))
+        ((js2-computed-prop-name-node-p node)
+         "[computed]")))))
 
 (defun js2-record-imenu-entry (fn-node qname pos)
   "Add an entry to `js2-imenu-recorder'.
@@ -8307,7 +8310,9 @@ represented by FN-NODE at POS."
                  (js2-report-error "msg.param.after.rest" nil
                                    (js2-node-pos param) (js2-node-len param)))
                while
-               (js2-match-token js2-COMMA))
+               (and (js2-match-token js2-COMMA)
+                    (or (< js2-language-version 200)
+                        (not (= js2-RP (js2-peek-token))))))
       (when (and (not paren-free-arrow)
                  (js2-must-match js2-RP "msg.no.paren.after.parms"))
         (setf (js2-function-node-rp fn-node) (- (js2-current-token-beg) pos)))
