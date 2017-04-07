8ea8ceabd0f324a0a238939e047a85a40cac6e71
JasonM23 <jasonm23@gmail.com>
2017-04-06 23:42:48 -0400

    [Adding migemo version: 20160924.741]

---
+++ elpa/migemo-20160924.741/migemo-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "migemo" "20160924.741" "Japanese incremental search through dynamic pattern expansion" '((cl-lib "0.5")) :url "https://github.com/emacs-jp/migemo")
