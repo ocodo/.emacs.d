3959ab76fb3eb80e791783e64ba17abf96c9eefd
JasonM23 <jasonm23@gmail.com>
2017-04-21 08:09:23 -0400

    [Updating emacsql version: 20170410.1218][removing old version: 20170110.1853]

--- elpa/emacsql-20170410.1218/emacsql-pkg.el
+++ elpa/emacsql-20170410.1218/emacsql-pkg.el
@@ -1,5 +1,6 @@
-(define-package "emacsql" "20170110.1853" "high-level SQL database front-end"
+(define-package "emacsql" "20170410.1218" "high-level SQL database front-end"
   '((emacs "24.3")
+    (cl-generic "0.3")
     (cl-lib "0.3")
     (finalize "1.0.0"))
   :url "https://github.com/skeeto/emacsql")
