38cf825df380ba22a3c2ef8efcb0451d447299ce
JasonM23 <jasonm23@gmail.com>
2017-04-21 08:09:24 -0400

    [Updating evil-lion version: 20170420.1028][removing old version: 20170328.21]

---
+++ elpa/evil-lion-20170420.1028/evil-lion-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-lion" "20170420.1028" "Evil align operator, port of vim-lion" '((emacs "24") (evil "1.0.0")) :url "http://github.com/edkolev/evil-lion" :keywords '("emulations" "evil" "vim"))
