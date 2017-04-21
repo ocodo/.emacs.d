2102e934c090f1478ec779721b41bb9ab1300595
JasonM23 <jasonm23@gmail.com>
2017-04-21 08:09:36 -0400

    [Updating rust-mode version: 20170411.2043][removing old version: 20170405.707]

---
+++ elpa/rust-mode-20170411.2043/rust-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rust-mode" "20170411.2043" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
