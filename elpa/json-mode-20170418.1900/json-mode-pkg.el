4553fb1844747e9d132304f57459037ae3b03c9c
JasonM23 <jasonm23@gmail.com>
2017-04-21 08:09:31 -0400

    [Updating json-mode version: 20170418.1900][removing old version: 20170402.1808]

---
+++ elpa/json-mode-20170418.1900/json-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "json-mode" "20170418.1900" "Major mode for editing JSON files." '((json-reformat "0.0.5") (json-snatcher "1.0.0")) :url "https://github.com/joshwnj/json-mode")
