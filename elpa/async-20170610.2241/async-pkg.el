d35afb706e948829cb6cbcc0f426aac4c5da65ac
Jasonm23 <jasonm23@gmail.com>
2017-07-03 09:33:42 +0800

    [Updating async version: 20170610.2241][removing old version: 20170219.942]

--- elpa/async-20170610.2241/async-pkg.el
+++ elpa/async-20170610.2241/async-pkg.el
@@ -1,4 +1,4 @@
-(define-package "async" "20170219.942" "Asynchronous processing in Emacs" 'nil :keywords
+(define-package "async" "20170610.2241" "Asynchronous processing in Emacs" 'nil :keywords
   '("async")
   :url "https://github.com/jwiegley/emacs-async")
 ;; Local Variables:
