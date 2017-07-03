8f94c3d4d30f99db36f00c4d8d22e23aa84375c6
Jasonm23 <jasonm23@gmail.com>
2017-07-03 09:34:09 +0800

    [Updating general version: 20170528.336][removing old version: 20170328.954]

--- elpa/general-20170528.336/general-pkg.el
+++ elpa/general-20170528.336/general-pkg.el
@@ -1,4 +1,4 @@
-(define-package "general" "20170328.954" "Convenience wrappers for keybindings."
+(define-package "general" "20170528.336" "Convenience wrappers for keybindings."
   '((cl-lib "0.5"))
   :url "https://github.com/noctuid/general.el" :keywords
   '("vim" "evil" "leader" "keybindings" "keys"))
