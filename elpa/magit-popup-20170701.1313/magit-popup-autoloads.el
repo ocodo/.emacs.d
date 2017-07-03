371ac277272de3246bd52a458a66ce17b4d2f415
Jasonm23 <jasonm23@gmail.com>
2017-07-03 09:34:32 +0800

    [Updating magit-popup version: 20170701.1313][removing old version: 20170420.544]

--- elpa/magit-popup-20170701.1313/magit-popup-autoloads.el
+++ elpa/magit-popup-20170701.1313/magit-popup-autoloads.el
@@ -3,14 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "magit-popup" "magit-popup.el" (0 0 0 0))
-;;; Generated autoloads from magit-popup.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "magit-popup" '("magit-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("magit-popup-pkg.el") (0 0 0 0))
+;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
+;;;;;;  (22873 40425 0 0))
 
 ;;;***
 
