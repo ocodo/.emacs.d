929191d8f8e365584345e80ce0bf24a5a75d7ad4
Jasonm23 <jasonm23@gmail.com>
2017-07-03 09:34:07 +0800

    [Updating flycheck-ycmd version: 20170614.1434][removing old version: 20170327.1418]

---
+++ elpa/flycheck-ycmd-20170614.1434/flycheck-ycmd-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-ycmd" "20170614.1434" "flycheck integration for ycmd" '((emacs "24") (dash "2.13.0") (flycheck "0.22") (ycmd "1.2") (let-alist "1.0.5")) :commit "d042a673b4d717c3ca9d641f120bfe16c994c740" :url "https://github.com/abingham/emacs-ycmd")
