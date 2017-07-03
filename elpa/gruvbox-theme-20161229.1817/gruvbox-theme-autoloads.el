cf27fb64f7805cba8d4486a7c9635380be726c67
Jasonm23 <jasonm23@gmail.com>
2017-07-03 09:34:14 +0800

    [Updating gruvbox-theme version: 20170630.213][removing old version: 20161229.1817]

--- elpa/gruvbox-theme-20161229.1817/gruvbox-theme-autoloads.el
+++
@@ -1,20 +0,0 @@
-;;; gruvbox-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "gruvbox-theme" "gruvbox-theme.el" (0 0 0 0))
-;;; Generated autoloads from gruvbox-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "gruvbox-theme" '("gruvbox-contrast")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; gruvbox-theme-autoloads.el ends here
