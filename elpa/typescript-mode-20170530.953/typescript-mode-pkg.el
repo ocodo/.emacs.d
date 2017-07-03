8c0897aa6413de0393e52c3a49990b3fdb57d9a5
Jasonm23 <jasonm23@gmail.com>
2017-07-03 09:34:52 +0800

    [Updating typescript-mode version: 20170530.953][removing old version: 20170324.1301]

---
+++ elpa/typescript-mode-20170530.953/typescript-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "typescript-mode" "20170530.953" "Major mode for editing typescript" 'nil :commit "9ad892fb2274decb637d12d81a8640d2c6cf11d3" :url "http://github.com/ananthakumaran/typescript.el" :keywords '("typescript" "languages"))
