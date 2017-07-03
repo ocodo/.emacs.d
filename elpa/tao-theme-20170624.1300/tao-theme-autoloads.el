719cbcd6f1891f1c96ac6f43071834a162633bfa
Jasonm23 <jasonm23@gmail.com>
2017-07-03 09:34:49 +0800

    [Updating tao-theme version: 20170624.1300][removing old version: 20170419.1308]

--- elpa/tao-theme-20170624.1300/tao-theme-autoloads.el
+++ elpa/tao-theme-20170624.1300/tao-theme-autoloads.el
@@ -3,33 +3,24 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "tao-theme" "tao-theme.el" (0 0 0 0))
-;;; Generated autoloads from tao-theme.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tao-theme" '("tao-")))
-
-;;;***
-
-;;;### (autoloads nil "tao-yang-theme" "tao-yang-theme.el" (0 0 0
-;;;;;;  0))
+;;;### (autoloads nil "tao-yang-theme" "tao-yang-theme.el" (22873
+;;;;;;  40526 0 0))
 ;;; Generated autoloads from tao-yang-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tao-yang-theme" '("tao-yang")))
-
 ;;;***
 
-;;;### (autoloads nil "tao-yin-theme" "tao-yin-theme.el" (0 0 0 0))
+;;;### (autoloads nil "tao-yin-theme" "tao-yin-theme.el" (22873 40526
+;;;;;;  0 0))
 ;;; Generated autoloads from tao-yin-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tao-yin-theme" '("tao-yin")))
-
 ;;;***
 
-;;;### (autoloads nil nil ("tao-theme-pkg.el") (0 0 0 0))
+;;;### (autoloads nil nil ("tao-theme-pkg.el" "tao-theme.el") (22873
+;;;;;;  40526 0 0))
 
 ;;;***
 
