8dc238242348396351b31974dede91c310798a2b
Jasonm23 <jasonm23@gmail.com>
2017-07-03 09:34:30 +0800

    [Updating kubernetes version: 20170523.1517][removing old version: 20170421.120]

---
+++ elpa/kubernetes-20170523.1517/kubernetes-pkg.el
@@ -0,0 +1,7 @@
+(define-package "kubernetes" "20170523.1517" "Magit-like porcelain for Kubernetes."
+  '((emacs "25.1")
+    (dash "2.12.0")
+    (magit "2.8.0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
