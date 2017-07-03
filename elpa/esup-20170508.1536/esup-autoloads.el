90f05a5598621e050f5ab33e0bc211503cbfa6b0
Jasonm23 <jasonm23@gmail.com>
2017-07-03 09:34:01 +0800

    [Updating esup version: 20170508.1536][removing old version: 20170125.43]

--- elpa/esup-20170508.1536/esup-autoloads.el
+++ elpa/esup-20170508.1536/esup-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "esup" "esup.el" (0 0 0 0))
+;;;### (autoloads nil "esup" "esup.el" (22873 40273 0 0))
 ;;; Generated autoloads from esup.el
 
 (autoload 'esup "esup" "\
@@ -12,18 +12,10 @@ If INIT-FILE is non-nil, profile that instead of USER-INIT-FILE.
 
 \(fn &optional INIT-FILE)" t nil)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "esup" '("esup-" "render")))
-
 ;;;***
 
-;;;### (autoloads nil "esup-child" "esup-child.el" (0 0 0 0))
-;;; Generated autoloads from esup-child.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "esup-child" '("esup-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("esup-pkg.el") (0 0 0 0))
+;;;### (autoloads nil nil ("esup-child.el" "esup-pkg.el") (22873
+;;;;;;  40273 0 0))
 
 ;;;***
 
--- elpa/esup-20170508.1536/esup-pkg.el
+++ elpa/esup-20170508.1536/esup-pkg.el
@@ -1,4 +1,4 @@
-(define-package "esup" "20170125.43" "the Emacs StartUp Profiler (ESUP)"
+(define-package "esup" "20170508.1536" "the Emacs StartUp Profiler (ESUP)"
   '((cl-lib "0.5")
     (emacs "24"))
   :url "http://github.com/jschaf/esup" :keywords
