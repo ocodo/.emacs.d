52ce181e88bb748d5dec861f71c052a3760b79c0
Jasonm23 <jasonm23@gmail.com>
2017-07-03 09:34:39 +0800

    [Updating parinfer version: 20170612.1048][removing old version: 20170315.2109]

--- elpa/parinfer-20170315.2109/parinfer-autoloads.el
+++
@@ -1,46 +0,0 @@
-;;; parinfer-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "parinfer" "parinfer.el" (0 0 0 0))
-;;; Generated autoloads from parinfer.el
-
-(autoload 'parinfer-mode "parinfer" "\
-Parinfer mode.
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'parinfer-region-mode "parinfer" "\
-Available when region is active.
-
-\(fn &optional ARG)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "parinfer" '("parinfer-")))
-
-;;;***
-
-;;;### (autoloads nil "parinfer-ext" "parinfer-ext.el" (0 0 0 0))
-;;; Generated autoloads from parinfer-ext.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "parinfer-ext" '("parinfer-")))
-
-;;;***
-
-;;;### (autoloads nil "parinferlib" "parinferlib.el" (0 0 0 0))
-;;; Generated autoloads from parinferlib.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "parinferlib" '("parinferlib-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("parinfer-pkg.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; parinfer-autoloads.el ends here
--- elpa/parinfer-20170612.1048/parinfer-pkg.el
+++ elpa/parinfer-20170612.1048/parinfer-pkg.el
@@ -1,4 +1,4 @@
-(define-package "parinfer" "20170315.2109" "Simpler Lisp editing"
+(define-package "parinfer" "20170612.1048" "Simpler Lisp editing"
   '((dash "2.13.0")
     (cl-lib "0.5"))
   :url "https://github.com/DogLooksGood/parinfer-mode" :keywords
