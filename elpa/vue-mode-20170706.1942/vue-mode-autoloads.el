919e6c8fac18545f9ebca9646c30211488e920d5
Jason Milkins <jasonm23@gmail.com>
2017-07-08 20:38:46 +0800

    [Updating vue-mode version: 20170706.1942][removing old version: 20170621.652]

--- elpa/vue-mode-20170706.1942/vue-mode-autoloads.el
+++ elpa/vue-mode-20170706.1942/vue-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "vue-mode" "vue-mode.el" (22873 40554 0 0))
+;;;### (autoloads nil "vue-mode" "vue-mode.el" (22880 30281 0 0))
 ;;; Generated autoloads from vue-mode.el
 
 (autoload 'vue-mode "vue-mode" "\
--- elpa/vue-mode-20170706.1942/vue-mode.el
+++ elpa/vue-mode-20170706.1942/vue-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: codefalling <code.falling@gmail.com>
 ;; Keywords: languages
-;; Package-Version: 20170621.652
+;; Package-Version: 20170706.1942
 
 ;; Version: 0.2
 ;; Package-Requires: ((mmm-mode "0.5.4") (vue-html-mode "0.1") (ssass-mode "0.1"))
@@ -54,7 +54,7 @@
     (:type style :name css :mode css-mode)
     (:type style :name stylus :mode stylus-mode)
     (:type style :name less :mode less-css-mode)
-    (:type style :name scss :mode scss-mode)
+    (:type style :name scss :mode css-mode)
     (:type style :name sass :mode ssass-mode))
   "A list of vue component languages, their type, and their corresponding major modes"
   :type '(list (plist :type 'symbol :name 'symbol :mode 'function))
