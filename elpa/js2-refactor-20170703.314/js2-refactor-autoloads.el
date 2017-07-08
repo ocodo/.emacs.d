4b3d42e35d44147c75e3f2594b060fc49bdf3fbd
Jason Milkins <jasonm23@gmail.com>
2017-07-08 20:38:35 +0800

    [Updating js2-refactor version: 20170703.314][removing old version: 20170525.544]

--- elpa/js2-refactor-20170703.314/js2-refactor-autoloads.el
+++ elpa/js2-refactor-20170703.314/js2-refactor-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "js2-refactor" "js2-refactor.el" (22873 40393
+;;;### (autoloads nil "js2-refactor" "js2-refactor.el" (22880 30407
 ;;;;;;  0 0))
 ;;; Generated autoloads from js2-refactor.el
 
@@ -24,7 +24,7 @@ Add js2r keybindings using the modifier MODIFIER.
 
 ;;;***
 
-;;;### (autoloads nil "js2r-vars" "js2r-vars.el" (22873 40393 0 0))
+;;;### (autoloads nil "js2r-vars" "js2r-vars.el" (22880 30407 0 0))
 ;;; Generated autoloads from js2r-vars.el
 
 (autoload 'js2r-extract-var "js2r-vars" "\
@@ -37,7 +37,7 @@ Add js2r keybindings using the modifier MODIFIER.
 ;;;### (autoloads nil nil ("js2-refactor-pkg.el" "js2r-conditionals.el"
 ;;;;;;  "js2r-conveniences.el" "js2r-formatting.el" "js2r-functions.el"
 ;;;;;;  "js2r-helpers.el" "js2r-iife.el" "js2r-paredit.el" "js2r-wrapping.el")
-;;;;;;  (22873 40393 0 0))
+;;;;;;  (22880 30407 0 0))
 
 ;;;***
 
--- elpa/js2-refactor-20170703.314/js2-refactor-pkg.el
+++ elpa/js2-refactor-20170703.314/js2-refactor-pkg.el
@@ -1,4 +1,4 @@
-(define-package "js2-refactor" "20170525.544" "A JavaScript refactoring library for emacs."
+(define-package "js2-refactor" "20170703.314" "A JavaScript refactoring library for emacs."
   '((js2-mode "20101228")
     (s "1.9.0")
     (multiple-cursors "1.0.0")
