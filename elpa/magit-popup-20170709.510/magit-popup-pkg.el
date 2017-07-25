d34656a503b973541d85c1722662d1756afd8bb5
JasonM23 <jasonm23@gmail.com>
2017-07-25 10:23:23 -0400

    [Updating magit-popup version: 20170709.510][removing old version: 20170701.1313]

--- elpa/magit-popup-20170709.510/magit-popup-pkg.el
+++ elpa/magit-popup-20170709.510/magit-popup-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit-popup" "20170701.1313" "Define prefix-infix-suffix command combos"
+(define-package "magit-popup" "20170709.510" "Define prefix-infix-suffix command combos"
   '((emacs "24.4")
     (async "20170219.942")
     (dash "20170207.2056"))
