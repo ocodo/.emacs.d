1803b5d3c9cf5375939fec83b8e71935155e7a2d
JasonM23 <jasonm23@gmail.com>
2017-07-25 10:23:15 -0400

    [Updating helm-ls-git version: 20170716.109][removing old version: 20170601.2200]

--- elpa/helm-ls-git-20170601.2200/helm-ls-git-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-ls-git" "20170601.2200" "list git files." '((helm "1.7.8")) :commit "1253c7615da055a95548b4f92a8a503fe249884e")
---
+++ elpa/helm-ls-git-20170716.109/helm-ls-git-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-ls-git" "20170716.109" "list git files." '((helm "1.7.8")) :commit "47981a8d80e1d464a7cddde5a391b35626929f00")
