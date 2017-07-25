fdc7fdb264316c24a8102a3d888e3004c160ec53
JasonM23 <jasonm23@gmail.com>
2017-07-25 10:23:26 -0400

    [Updating mustang-theme version: 20170719.246][removing old version: 20141017.1623]

--- elpa/mustang-theme-20141017.1623/mustang-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mustang-theme" "20141017.1623" "port of vim's mustang theme" 'nil :url "http://github.com/mswift42/mustang-theme")
--- elpa/mustang-theme-20170719.246/mustang-theme-autoloads.el
+++ elpa/mustang-theme-20170719.246/mustang-theme-autoloads.el
@@ -1,10 +1,9 @@
 ;;; mustang-theme-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "mustang-theme" "mustang-theme.el" (21783 34331
-;;;;;;  247401 0))
+;;;### (autoloads nil "mustang-theme" "mustang-theme.el" (0 0 0 0))
 ;;; Generated autoloads from mustang-theme.el
 
 (when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
