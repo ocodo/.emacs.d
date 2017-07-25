ecb4bf0f8e56fc2fb5008d788b7ad6511bf1de41
JasonM23 <jasonm23@gmail.com>
2017-07-25 10:23:01 -0400

    [Updating evil version: 20170712.2350][removing old version: 20170706.1233]

--- elpa/evil-20170706.1233/evil-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; evil-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "evil-core" "evil-core.el" (22880 30494 0 0))
-;;; Generated autoloads from evil-core.el
- (autoload 'evil-mode "evil")
-
-;;;***
-
-;;;### (autoloads nil nil ("evil-command-window.el" "evil-commands.el"
-;;;;;;  "evil-common.el" "evil-digraphs.el" "evil-ex.el" "evil-integration.el"
-;;;;;;  "evil-jumps.el" "evil-macros.el" "evil-maps.el" "evil-pkg.el"
-;;;;;;  "evil-repeat.el" "evil-search.el" "evil-states.el" "evil-test-helpers.el"
-;;;;;;  "evil-types.el" "evil-vars.el" "evil.el") (22880 30494 0
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; evil-autoloads.el ends here
