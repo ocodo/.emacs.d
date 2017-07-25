9e9f5603d49bfaaad74bce3a0e77302e7010b7e9
JasonM23 <jasonm23@gmail.com>
2017-07-25 10:23:29 -0400

    [Updating php-mode version: 20170722.1205][removing old version: 20170621.2242]

--- elpa/php-mode-20170722.1205/php-mode-pkg.el
+++ elpa/php-mode-20170722.1205/php-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "php-mode" "20170621.2242" "Major mode for editing PHP code"
+(define-package "php-mode" "20170722.1205" "Major mode for editing PHP code"
   '((emacs "24")
     (cl-lib "0.5"))
   :url "https://github.com/ejmr/php-mode")
