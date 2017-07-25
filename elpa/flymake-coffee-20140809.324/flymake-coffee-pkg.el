e8c5b6f75c3541f4f77fe3aec7054965accf5815
JasonM23 <jasonm23@gmail.com>
2017-07-25 10:23:05 -0400

    [Updating flymake-coffee version: 20170722.1846][removing old version: 20140809.324]

--- elpa/flymake-coffee-20140809.324/flymake-coffee-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flymake-coffee" "20140809.324" "A flymake handler for coffee script" '((flymake-easy "0.1")))
---
+++ elpa/flymake-coffee-20170722.1846/flymake-coffee-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-coffee" "20170722.1846" "A flymake handler for coffee script" '((flymake-easy "0.1")) :commit "dee295acf30820ed15fe0de17137d50bc27fc80c" :url "https://github.com/purcell/flymake-coffee")
