68c5a66d5c499fb2758ebbd20839560c19f95fab
JasonM23 <jasonm23@gmail.com>
2017-07-25 10:22:56 -0400

    [Updating diff-hl version: 20170709.2000][removing old version: 20170617.1540]

--- elpa/diff-hl-20170709.2000/diff-hl-autoloads.el
+++ elpa/diff-hl-20170709.2000/diff-hl-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "diff-hl" "diff-hl.el" (22873 40239 0 0))
+;;;### (autoloads nil "diff-hl" "diff-hl.el" (0 0 0 0))
 ;;; Generated autoloads from diff-hl.el
 
 (autoload 'diff-hl-mode "diff-hl" "\
@@ -38,10 +38,11 @@ See `diff-hl-mode' for more information on Diff-Hl mode.
 
 \(fn &optional ARG)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "diff-hl" '("diff-hl-")))
+
 ;;;***
 
-;;;### (autoloads nil "diff-hl-amend" "diff-hl-amend.el" (22873 40239
-;;;;;;  0 0))
+;;;### (autoloads nil "diff-hl-amend" "diff-hl-amend.el" (0 0 0 0))
 ;;; Generated autoloads from diff-hl-amend.el
 
 (autoload 'diff-hl-amend-mode "diff-hl-amend" "\
@@ -74,10 +75,11 @@ See `diff-hl-amend-mode' for more information on Diff-Hl-Amend mode.
 
 \(fn &optional ARG)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "diff-hl-amend" '("turn-on-diff-hl-amend-mode" "diff-hl-amend-setup")))
+
 ;;;***
 
-;;;### (autoloads nil "diff-hl-dired" "diff-hl-dired.el" (22873 40239
-;;;;;;  0 0))
+;;;### (autoloads nil "diff-hl-dired" "diff-hl-dired.el" (0 0 0 0))
 ;;; Generated autoloads from diff-hl-dired.el
 
 (autoload 'diff-hl-dired-mode "diff-hl-dired" "\
@@ -90,10 +92,12 @@ Toggle VC diff highlighting on the side of a Dired window.
 
 \(fn)" nil nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "diff-hl-dired" '("diff-hl-dired-")))
+
 ;;;***
 
-;;;### (autoloads nil "diff-hl-flydiff" "diff-hl-flydiff.el" (22873
-;;;;;;  40239 0 0))
+;;;### (autoloads nil "diff-hl-flydiff" "diff-hl-flydiff.el" (0 0
+;;;;;;  0 0))
 ;;; Generated autoloads from diff-hl-flydiff.el
 
 (defvar diff-hl-flydiff-mode nil "\
@@ -112,10 +116,12 @@ This is a global minor mode.  It alters how `diff-hl-mode' works.
 
 \(fn &optional ARG)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "diff-hl-flydiff" '("diff-hl-flydiff")))
+
 ;;;***
 
-;;;### (autoloads nil "diff-hl-margin" "diff-hl-margin.el" (22873
-;;;;;;  40239 0 0))
+;;;### (autoloads nil "diff-hl-margin" "diff-hl-margin.el" (0 0 0
+;;;;;;  0))
 ;;; Generated autoloads from diff-hl-margin.el
 
 (defvar diff-hl-margin-mode nil "\
@@ -133,9 +139,11 @@ Toggle displaying `diff-hl-mode' highlights on the margin.
 
 \(fn &optional ARG)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "diff-hl-margin" '("diff-hl-")))
+
 ;;;***
 
-;;;### (autoloads nil nil ("diff-hl-pkg.el") (22873 40239 0 0))
+;;;### (autoloads nil nil ("diff-hl-pkg.el") (0 0 0 0))
 
 ;;;***
 
