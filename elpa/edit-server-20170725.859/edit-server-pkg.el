6e452d298a7c93d95f712a70a3213d25f647425f
JasonM23 <jasonm23@gmail.com>
2017-08-06 04:32:19 -0400

    [Updating edit-server version: 20170725.859][removing old version: 20170704.819]

---
+++ elpa/edit-server-20170725.859/edit-server-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "edit-server" "20170725.859" "server that responds to edit requests from Chrome" 'nil :commit "462c57be72b3a8652f705bde0d3b7cd2f79644fa" :url "https://github.com/stsquad/emacs_chrome")
--- elpa/edit-server-20170725.859/edit-server.el
+++ elpa/edit-server-20170725.859/edit-server.el
@@ -6,7 +6,7 @@
 ;; Author: Alex Bennée <alex@bennee.com>
 ;; Maintainer: Alex Bennée <alex@bennee.com>
 ;; Version: 1.14
-;; Package-Version: 20170704.819
+;; Package-Version: 20170725.859
 ;; Homepage: https://github.com/stsquad/emacs_chrome
 
 ;; This file is not part of GNU Emacs.
