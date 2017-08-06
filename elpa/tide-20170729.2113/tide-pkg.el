d8a149db5fcd523faebe2e2babcd8999902774b4
JasonM23 <jasonm23@gmail.com>
2017-08-06 04:33:03 -0400

    [Updating tide version: 20170729.2113][removing old version: 20170720.2101]

---
+++ elpa/tide-20170729.2113/tide-pkg.el
@@ -0,0 +1,10 @@
+(define-package "tide" "20170729.2113" "Typescript Interactive Development Environment"
+  '((dash "2.10.0")
+    (flycheck "27")
+    (typescript-mode "0.1")
+    (cl-lib "0.5"))
+  :url "http://github.com/ananthakumaran/tide" :keywords
+  '("typescript"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
