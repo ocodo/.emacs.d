f2dc5e03310db8d2c36d89438188cedc2164c80d
JasonM23 <jasonm23@gmail.com>
2017-08-06 04:32:52 -0400

    [Updating org-projectile version: 20170803.634][removing old version: 20170520.2310]

--- elpa/org-projectile-20170520.2310/org-projectile-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "org-projectile" "20170520.2310" "Repository todo management for org-mode"
-  '((projectile "0.11.0")
-    (dash "2.10.0")
-    (emacs "24"))
-  :url "https://github.com/IvanMalison/org-projectile" :keywords
-  '("org" "projectile" "todo"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/org-projectile-20170803.634/org-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-projectile" "20170803.634" "Repository todo management for org-mode" '((projectile "0.11.0") (dash "2.10.0") (emacs "24") (s "1.9.0") (org-category-capture "0.0.0")) :commit "c39d28d925916ee7e08a879d5d9dd2287b3b9184" :url "https://github.com/IvanMalison/org-projectile" :keywords '("org-mode" "projectile" "todo" "tools" "outlines"))
