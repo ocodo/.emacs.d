184695a2d29c35d65a0441dd014f4eed6c1b76da
JasonM23 <jasonm23@gmail.com>
2017-08-06 04:32:09 -0400

    [Updating atom-one-dark-theme version: 20170803.916][removing old version: 20170117.1905]

---
+++ elpa/atom-one-dark-theme-20170803.916/atom-one-dark-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "atom-one-dark-theme" "20170803.916" "Atom One Dark color theme" 'nil :commit "6ad96e25f8b46942380afbc33e3659a5e9fa09b1" :url "https://github.com/jonathanchu/atom-one-dark-theme")
