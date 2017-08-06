3629e7731b71b4e5f111af3ad737142e2a38d255
JasonM23 <jasonm23@gmail.com>
2017-08-06 04:32:07 -0400

    [Updating alchemist version: 20170803.414][removing old version: 20170614.218]

--- elpa/alchemist-20170803.414/alchemist-pkg.el
+++ elpa/alchemist-20170803.414/alchemist-pkg.el
@@ -1,4 +1,4 @@
-(define-package "alchemist" "20170614.218" "Elixir tooling integration into Emacs"
+(define-package "alchemist" "20170803.414" "Elixir tooling integration into Emacs"
   '((elixir-mode "2.2.5")
     (dash "2.11.0")
     (emacs "24.4")
