de28925d4127e7446cc0c03b5db34f96b23b37fd
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:10 +0000

    [Adding el-mock version: 20170824.1254]

---
+++ elpa/el-mock-20170824.1254/el-mock-autoloads.el
@@ -0,0 +1,15 @@
+;;; el-mock-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("el-mock.el") (22955 53584 205739 974000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; el-mock-autoloads.el ends here
