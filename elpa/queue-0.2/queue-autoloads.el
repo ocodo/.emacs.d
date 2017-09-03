8905f104daa523104a35b6133f399b9a12393c74
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:40 +0000

    [Adding queue version: 0.2]

---
+++ elpa/queue-0.2/queue-autoloads.el
@@ -0,0 +1,19 @@
+;;; queue-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "queue" "queue.el" (22955 53173 405283 974000))
+;;; Generated autoloads from queue.el
+
+(defalias 'make-queue 'queue-create "\
+Create an empty queue data structure.")
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; queue-autoloads.el ends here
