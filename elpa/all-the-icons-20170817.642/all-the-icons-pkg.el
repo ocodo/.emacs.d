c853e2216fa70d8f7f9a2c45c7b712cd8ed5babf
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:04 +0000

    [Adding all-the-icons version: 20170817.642]

---
+++ elpa/all-the-icons-20170817.642/all-the-icons-pkg.el
@@ -0,0 +1,9 @@
+(define-package "all-the-icons" "20170817.642" "A library for inserting Developer icons"
+  '((emacs "24.3")
+    (font-lock+ "0")
+    (memoize "1.0.1"))
+  :url "https://github.com/domtronn/all-the-icons.el" :keywords
+  '("convenient" "lisp"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
