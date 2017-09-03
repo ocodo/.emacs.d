27cda1057c033297268ac7a32b606f385abe9d0b
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:09 +0000

    [Adding easy-hugo version: 20170819.347]

---
+++ elpa/easy-hugo-20170819.347/easy-hugo-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "easy-hugo" "20170819.347" "Write blogs made with hugo by markdown or org-mode" '((emacs "24.4")) :commit "108cc2347507ff8c92b10a817de0850e9eb99ab2" :url "https://github.com/masasam/emacs-easy-hugo")
