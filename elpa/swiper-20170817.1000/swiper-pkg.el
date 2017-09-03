3ee6ed7c6369f4a5bec2c498948d1ce6caa235b5
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:46 +0000

    [Adding swiper version: 20170817.1000]

---
+++ elpa/swiper-20170817.1000/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20170817.1000" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "03222ede416dd8b8ee28a93d8720bd85b7c1612f" :url "https://github.com/abo-abo/swiper" :keywords '("matching"))
