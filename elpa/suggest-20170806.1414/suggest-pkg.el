f0e37fad4b43522f18dffb26ba77e590ca9f81fb
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:45 +0000

    [Adding suggest version: 20170806.1414]

---
+++ elpa/suggest-20170806.1414/suggest-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "suggest" "20170806.1414" "suggest elisp functions that give the output requested" '((emacs "24.4") (loop "1.3") (dash "2.13.0") (s "1.11.0") (f "0.18.2")) :commit "6ae090cab9bc16650c1788e83ccd2fe95a959de2" :url "https://github.com/Wilfred/suggest.el" :keywords '("convenience"))
