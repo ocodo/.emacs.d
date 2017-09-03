51f6869743bca999e590bb9732afc14af09df2b2
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:33 +0000

    [Adding mbo70s-theme version: 20170808.615]

---
+++ elpa/mbo70s-theme-20170808.615/mbo70s-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mbo70s-theme" "20170808.615" "70s style palette, with similarities to mbo theme" '((emacs "24.0")) :commit "bed3db8965708ed4e9482b224a9b084765c052f2" :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
