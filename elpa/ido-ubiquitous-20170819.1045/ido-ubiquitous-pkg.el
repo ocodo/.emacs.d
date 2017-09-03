844cb69849ccceb90621474cf5385df1c6b7372b
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:27 +0000

    [Adding ido-ubiquitous version: 20170819.1045]

---
+++ elpa/ido-ubiquitous-20170819.1045/ido-ubiquitous-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ido-ubiquitous" "20170819.1045" "Use ido (nearly) everywhere." '((emacs "24.1") (ido-completing-read+ "4.6") (cl-lib "0.5")) :commit "02f21215ce5ef2234c0e58dac9ec181cfc0900c1" :url "https://github.com/DarwinAwardWinner/ido-ubiquitous" :keywords '("convenience" "completion" "ido"))
