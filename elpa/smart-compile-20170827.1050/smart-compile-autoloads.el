d554a6f9c5e8a94d3943e2499a250a53638f07a7
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:43 +0000

    [Adding smart-compile version: 20170827.1050]

---
+++ elpa/smart-compile-20170827.1050/smart-compile-autoloads.el
@@ -0,0 +1,24 @@
+;;; smart-compile-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "smart-compile" "smart-compile.el" (22955 53146
+;;;;;;  429236 294000))
+;;; Generated autoloads from smart-compile.el
+
+(autoload 'smart-compile "smart-compile" "\
+An interface to `compile'.
+It calls `compile' or other compile function,
+which is defined in `smart-compile-alist'.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; smart-compile-autoloads.el ends here
---
+++ elpa/smart-compile-20170827.1050/smart-compile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smart-compile" "20170827.1050" "an interface to `compile'" 'nil :commit "64258b424e471c749a3399d32aa35b39dfb05222" :keywords '("tools" "unix"))
