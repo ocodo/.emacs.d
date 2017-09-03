b7ddb7e2f23b0b53f5167dca0584beb9f00cdea4
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:12 +0000

    [Adding esxml version: 20170831.916]

---
+++ elpa/esxml-20170831.916/esxml-autoloads.el
@@ -0,0 +1,15 @@
+;;; esxml-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("esxml.el") (22955 53561 353724 759000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; esxml-autoloads.el ends here
---
+++ elpa/esxml-20170831.916/esxml-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "esxml" "20170831.916" "Library for working with xml via esxml and sxml" 'nil :commit "dc05dfcd6cf8f90cbaf9b6e7fc7f4c25720c38b7" :keywords '("tools" "lisp" "comm"))
