0c900a80e7a513e239da287dcc3e101c7eb0d6cd
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:17 +0000

    [Adding flatland-black-theme version: 20170808.612]

---
+++ elpa/flatland-black-theme-20170808.612/flatland-black-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flatland-black-theme" "20170808.612" "an Emacs 24 theme based on Flatland Black (tmTheme)" '((emacs "24.0")) :commit "348c5d5fe615e6ea13cadc17f046e506e789ce07" :url "https://github.com/emacsfodder/flatland-black-theme")
