51f20d84cd0470fa21cf296326fe2b45bdfba39f
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:07 +0000

    [Adding counsel-projectile version: 20170816.200]

---
+++ elpa/counsel-projectile-20170816.200/counsel-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-projectile" "20170816.200" "Ivy integration for Projectile" '((counsel "0.8.0") (projectile "0.14.0")) :commit "363c875df6ea658e543df59c11497f79bfa36b54" :url "https://github.com/ericdanan/counsel-projectile" :keywords '("project" "convenience"))
