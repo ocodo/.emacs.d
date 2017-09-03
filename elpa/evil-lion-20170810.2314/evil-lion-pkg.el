b4119331a0a85d96dbaa45b774a411f20a01b18a
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:14 +0000

    [Adding evil-lion version: 20170810.2314]

---
+++ elpa/evil-lion-20170810.2314/evil-lion-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-lion" "20170810.2314" "Evil align operator, port of vim-lion" '((emacs "24.3") (evil "1.0.0")) :commit "aaa3874ad54c31b4322ac5bbc63e331498b11d61" :url "http://github.com/edkolev/evil-lion" :keywords '("emulations" "evil" "vim"))
