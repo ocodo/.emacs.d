32826b7d135a6ba7456dc46192584a169c8cf57b
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:36 +0000

    [Adding olivetti version: 20170823.1754]

---
+++ elpa/olivetti-20170823.1754/olivetti-autoloads.el
@@ -0,0 +1,33 @@
+;;; olivetti-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "olivetti" "olivetti.el" (22955 53266 249427
+;;;;;;  872000))
+;;; Generated autoloads from olivetti.el
+
+(autoload 'turn-on-olivetti-mode "olivetti" "\
+Turn on `olivetti-mode' unconditionally.
+
+\(fn)" t nil)
+
+(autoload 'olivetti-mode "olivetti" "\
+Olivetti provides a nice writing environment.
+
+Window margins are set to relative widths to accomodate a text
+body width set with `olivetti-body-width'.
+
+When `olivetti-hide-mode-line' is non-nil, the mode line is also
+hidden.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; olivetti-autoloads.el ends here
