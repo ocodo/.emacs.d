9796f25105e5508c3f4b34d0f01a04eaa30f7814
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:44 +0000

    [Adding ssass-mode version: 20170817.1216]

---
+++ elpa/ssass-mode-20170817.1216/ssass-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ssass-mode" "20170817.1216" "Edit Sass without a Turing Machine" '((emacs "24.3")) :commit "d17d2de381ffc96e62e77435fb7b387bf59aceec" :url "http://github.com/AdamNiederer/ssass-mode" :keywords '("languages" "sass"))
