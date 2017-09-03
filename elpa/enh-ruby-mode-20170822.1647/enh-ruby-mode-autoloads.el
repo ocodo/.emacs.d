8fffa71dee0455c942567c442603a48667cb6a48
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:12 +0000

    [Adding enh-ruby-mode version: 20170822.1647]

---
+++ elpa/enh-ruby-mode-20170822.1647/enh-ruby-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; enh-ruby-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "enh-ruby-mode" "enh-ruby-mode.el" (22955 53564
+;;;;;;  117726 647000))
+;;; Generated autoloads from enh-ruby-mode.el
+
+(autoload 'enh-ruby-mode "enh-ruby-mode" "\
+Enhanced Major mode for editing Ruby code.
+
+\\{enh-ruby-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("enh-ruby-mode-pkg.el") (22955 53564 113726
+;;;;;;  644000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; enh-ruby-mode-autoloads.el ends here
---
+++ elpa/enh-ruby-mode-20170822.1647/enh-ruby-mode-pkg.el
@@ -0,0 +1,7 @@
+(define-package "enh-ruby-mode" "20170822.1647" "Major mode for editing Ruby files"
+  '((emacs "24"))
+  :url "http://github.com/zenspider/Enhanced-Ruby-Mode" :keywords
+  '("languages elisp" "ruby"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
