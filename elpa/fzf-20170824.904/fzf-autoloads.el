f9d8f234125a260ae08f99a484490f2de7558ffb
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:18 +0000

    [Adding fzf version: 20170824.904]

---
+++ elpa/fzf-20170824.904/fzf-autoloads.el
@@ -0,0 +1,31 @@
+;;; fzf-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "fzf" "fzf.el" (22955 53520 249695 27000))
+;;; Generated autoloads from fzf.el
+
+(autoload 'fzf "fzf" "\
+Starts a fzf session.
+
+\(fn)" t nil)
+
+(autoload 'fzf-directory "fzf" "\
+Starts a fzf session at the specified directory.
+
+\(fn)" t nil)
+
+(autoload 'fzf-git "fzf" "\
+Starts a fzf session at the root of the current git.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; fzf-autoloads.el ends here
---
+++ elpa/fzf-20170824.904/fzf-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fzf" "20170824.904" "A front-end for fzf." '((emacs "24.4")) :commit "9b1744b8cf8817912164fc08d01b3d9e541d6479" :url "https://github.com/bling/fzf.el" :keywords '("fzf" "fuzzy" "search"))
