788c826866db2a1a57f0aab2108fd96370dec395
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:42 +0000

    [Adding slime-theme version: 20170808.622]

---
+++ elpa/slime-theme-20170808.622/slime-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "slime-theme" "20170808.622" "an Emacs 24 theme based on Slime (tmTheme)" '((emacs "24.0")) :commit "8e5880ac69e0b6a079103001cc3a90bdb688998f" :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
