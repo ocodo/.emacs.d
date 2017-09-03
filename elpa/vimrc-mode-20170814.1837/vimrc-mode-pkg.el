338d5dacfa838fbb3d1088acc409018cb88a0041
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:49 +0000

    [Adding vimrc-mode version: 20170814.1837]

---
+++ elpa/vimrc-mode-20170814.1837/vimrc-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vimrc-mode" "20170814.1837" "Major mode for vimrc files" 'nil :commit "ba8140fba6e03a35b123acbd62fc8c6f0a03bf4a" :url "https://github.com/mcandre/vimrc-mode" :keywords '("languages" "vim"))
