1915dc632b19deaab08867d242a13fce79e6beaf
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:22 +0000

    [Adding harvest version: 20170822.1046]

---
+++ elpa/harvest-20170822.1046/harvest-autoloads.el
@@ -0,0 +1,27 @@
+;;; harvest-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "harvest" "harvest.el" (22955 53493 457673
+;;;;;;  917000))
+;;; Generated autoloads from harvest.el
+
+(autoload 'harvest "harvest" "\
+Start the main Harvest hydra.
+
+\(fn)" t nil)
+
+(autoload 'harvest-clock-out "harvest" "\
+Clock out of any active timer.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; harvest-autoloads.el ends here
