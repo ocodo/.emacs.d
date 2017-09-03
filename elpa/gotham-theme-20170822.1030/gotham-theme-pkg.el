2a64a8c302107d7510c44326b7b7280f9b3ddcc3
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:20 +0000

    [Adding gotham-theme version: 20170822.1030]

---
+++ elpa/gotham-theme-20170822.1030/gotham-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gotham-theme" "20170822.1030" "A very dark Emacs color theme." 'nil :commit "417d61978d139cb5d089c5365fc8d3166d76d3ac" :url "https://github.com/wasamasa/gotham-theme")
