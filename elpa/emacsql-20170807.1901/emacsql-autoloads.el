f08775772d727a0830326b064484ad65eb355670
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:12 +0000

    [Adding emacsql version: 20170807.1901]

---
+++ elpa/emacsql-20170807.1901/emacsql-autoloads.el
@@ -0,0 +1,28 @@
+;;; emacsql-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "emacsql" "emacsql.el" (22955 53566 809728
+;;;;;;  460000))
+;;; Generated autoloads from emacsql.el
+
+(autoload 'emacsql-show-last-sql "emacsql" "\
+Display the compiled SQL of the s-expression SQL expression before point.
+A prefix argument causes the SQL to be printed into the current buffer.
+
+\(fn &optional PREFIX)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("emacsql-compiler.el" "emacsql-pkg.el")
+;;;;;;  (22955 53566 817728 466000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; emacsql-autoloads.el ends here
---
+++ elpa/emacsql-20170807.1901/emacsql-pkg.el
@@ -0,0 +1,9 @@
+(define-package "emacsql" "20170807.1901" "high-level SQL database front-end"
+  '((emacs "24.3")
+    (cl-generic "0.3")
+    (cl-lib "0.3")
+    (finalize "1.0.0"))
+  :url "https://github.com/skeeto/emacsql")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
