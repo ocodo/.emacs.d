ee873740f9b64460b9154a66dede523cf009ed07
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:11 +0000

    [Adding elfeed version: 20170902.1318]

---
+++ elpa/elfeed-20170902.1318/elfeed-autoloads.el
@@ -0,0 +1,70 @@
+;;; elfeed-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "elfeed" "elfeed.el" (22955 53576 917735 219000))
+;;; Generated autoloads from elfeed.el
+
+(autoload 'elfeed-update "elfeed" "\
+Update all the feeds in `elfeed-feeds'.
+
+\(fn)" t nil)
+
+(autoload 'elfeed "elfeed" "\
+Enter elfeed.
+
+\(fn)" t nil)
+
+(autoload 'elfeed-load-opml "elfeed" "\
+Load feeds from an OPML file into `elfeed-feeds'.
+When called interactively, the changes to `elfeed-feeds' are
+saved to your customization file.
+
+\(fn FILE)" t nil)
+
+(autoload 'elfeed-export-opml "elfeed" "\
+Export the current feed listing to OPML-formatted FILE.
+
+\(fn FILE)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "elfeed-link" "elfeed-link.el" (22955 53576
+;;;;;;  917735 219000))
+;;; Generated autoloads from elfeed-link.el
+
+(eval-after-load 'org '(require 'elfeed-link))
+
+;;;***
+
+;;;### (autoloads nil "elfeed-search" "elfeed-search.el" (22955 53576
+;;;;;;  925735 225000))
+;;; Generated autoloads from elfeed-search.el
+
+(autoload 'elfeed-search-bookmark-handler "elfeed-search" "\
+Jump to an elfeed-search bookmarked location.
+
+\(fn RECORD)" nil nil)
+
+(autoload 'elfeed-search-desktop-restore "elfeed-search" "\
+Restore the state of an elfeed-search buffer on desktop restore.
+
+\(fn FILE-NAME BUFFER-NAME SEARCH-FILTER)" nil nil)
+
+(add-to-list 'desktop-buffer-mode-handlers '(elfeed-search-mode . elfeed-search-desktop-restore))
+
+;;;***
+
+;;;### (autoloads nil nil ("elfeed-csv.el" "elfeed-curl.el" "elfeed-db.el"
+;;;;;;  "elfeed-lib.el" "elfeed-log.el" "elfeed-pkg.el" "elfeed-show.el"
+;;;;;;  "xml-query.el") (22955 53576 929735 228000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; elfeed-autoloads.el ends here
---
+++ elpa/elfeed-20170902.1318/elfeed-pkg.el
@@ -0,0 +1,5 @@
+(define-package "elfeed" "20170902.1318" "an Emacs Atom/RSS feed reader"
+  '((emacs "24.3")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
