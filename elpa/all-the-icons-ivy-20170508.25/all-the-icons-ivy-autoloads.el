d484eb694ea688d8b656858d401c09cd69d8ff41
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:05 +0000

    [Adding all-the-icons-ivy version: 20170508.25]

---
+++ elpa/all-the-icons-ivy-20170508.25/all-the-icons-ivy-autoloads.el
@@ -0,0 +1,22 @@
+;;; all-the-icons-ivy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "all-the-icons-ivy" "all-the-icons-ivy.el"
+;;;;;;  (22955 52901 904650 319000))
+;;; Generated autoloads from all-the-icons-ivy.el
+
+(autoload 'all-the-icons-ivy-setup "all-the-icons-ivy" "\
+Set ivy's display transformers to show relevant icons next to the candidates.
+
+\(fn)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; all-the-icons-ivy-autoloads.el ends here
