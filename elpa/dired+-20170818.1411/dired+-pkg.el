59876bc867185ea09dec8542b6911045f4ecff21
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:09 +0000

    [Adding dired+ version: 20170818.1411]

---
+++ elpa/dired+-20170818.1411/dired+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired+" "20170818.1411" "Extensions to Dired." 'nil :url "https://www.emacswiki.org/emacs/download/dired%2b.el" :keywords '("unix" "mouse" "directories" "diredp" "dired"))
