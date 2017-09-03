a497b33a876862ddc1e182b136ab709294bd4e51
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:25 +0000

    [Adding highlight-numbers version: 20170815.1412]

---
+++ elpa/highlight-numbers-20170815.1412/highlight-numbers-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "highlight-numbers" "20170815.1412" "Highlight numbers in source code" '((emacs "24") (parent-mode "2.0")) :commit "444614973a4af7c9ad2431bdaa2005d95db41ee7" :url "https://github.com/Fanael/highlight-numbers")
