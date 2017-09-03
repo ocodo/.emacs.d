916dadcf2433cf1df8c654516a0b6ef41f23061d
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:43 +0000

    [Adding sml-modeline version: 20170614.1411]

---
+++ elpa/sml-modeline-20170614.1411/sml-modeline-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sml-modeline" "20170614.1411" "Show position in a scrollbar like way in mode-line" 'nil :commit "d2f9f70174c4cf68c67eb3bb8088235735e34d9a" :url "http://bazaar.launchpad.net/~nxhtml/nxhtml/main/annotate/head%3A/util/sml-modeline.el")
