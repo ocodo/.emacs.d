8bff882d1678ce5400400a178c55d80a1b1d0517
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:19 +0000

    [Adding gotest version: 20170823.141]

---
+++ elpa/gotest-20170823.141/gotest-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gotest" "20170823.141" "Launch GO unit tests" '((emacs "24.3") (s "1.11.0") (f "0.19.0") (go-mode "1.5.0")) :commit "8a5ef7363f83edb3b77c5e23876f13dd8c23b2b9" :url "https://github.com/nlamirault/gotest.el" :keywords '("languages" "go" "tests"))
