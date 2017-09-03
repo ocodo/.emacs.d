68bb5d6605a728d39801842c230843cc31b544b3
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:39 +0000

    [Adding point-stack version: 20170808.958]

---
+++ elpa/point-stack-20170808.958/point-stack-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "point-stack" "20170808.958" "Back and forward navigation through buffer locations" 'nil :commit "76e17311e3a810314c7d31ac46dc55450ff30fa2")
