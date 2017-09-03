17b28d73e1b013112b599358eb7644652208383b
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:38 +0000

    [Adding org-projectile version: 20170819.1047]

---
+++ elpa/org-projectile-20170819.1047/org-projectile-autoloads.el
@@ -0,0 +1,50 @@
+;;; org-projectile-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "org-projectile" "org-projectile.el" (22955
+;;;;;;  53189 817311 622000))
+;;; Generated autoloads from org-projectile.el
+
+(autoload 'org-projectile-goto-location-for-project "org-projectile" "\
+Goto the location at which TODOs for PROJECT are stored.
+
+\(fn PROJECT)" t nil)
+
+(autoload 'org-projectile-single-file "org-projectile" "\
+Set `org-projectile-strategy' so that captures occur in a single file.
+
+\(fn)" t nil)
+
+(autoload 'org-projectile-per-project "org-projectile" "\
+Set `org-projectile-strategy' so that captures occur within each project.
+
+\(fn)" t nil)
+
+(autoload 'org-projectile-project-todo-completing-read "org-projectile" "\
+Select a project using a `projectile-completing-read' and record a TODO.
+
+If CAPTURE-TEMPLATE is provided use it as the capture template
+for the TODO. ADDITIONAL-OPTIONS will be supplied as though they
+were part of the capture template definition.
+
+\(fn &optional CAPTURE-TEMPLATE &rest ADDITIONAL-OPTIONS)" t nil)
+
+(autoload 'org-projectile-capture-for-current-project "org-projectile" "\
+Capture a TODO for the current active projectile project.
+
+If CAPTURE-TEMPLATE is provided use it as the capture template
+for the TODO. ADDITIONAL-OPTIONS will be supplied as though they
+were part of the capture template definition.
+
+\(fn &optional CAPTURE-TEMPLATE &rest ADDITIONAL-OPTIONS)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; org-projectile-autoloads.el ends here
---
+++ elpa/org-projectile-20170819.1047/org-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-projectile" "20170819.1047" "Repository todo management for org-mode" '((projectile "0.11.0") (dash "2.10.0") (emacs "24") (s "1.9.0") (org-category-capture "0.0.0")) :commit "48f621b595e748c5e03431f237facf258ffc9443" :url "https://github.com/IvanMalison/org-projectile" :keywords '("org-mode" "projectile" "todo" "tools" "outlines"))
