e7082ce557ceb0543f8324df4ebec65fa9393d98
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:07 +0000

    [Adding csv version: 20161113.710]

---
+++ elpa/csv-20161113.710/csv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "csv" "20161113.710" "Functions for reading and parsing CSV files." 'nil :commit "aa1dfa1263565d5fac3879c21d8ddf5f8915e411" :keywords '("extensions" "data" "csv"))
