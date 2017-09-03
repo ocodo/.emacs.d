3089ffde26a89d33a29b35f375863f437b8a5cbd
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:04 +0000

    [Adding all-the-icons-dired version: 20170418.1431]

---
+++ elpa/all-the-icons-dired-20170418.1431/all-the-icons-dired-autoloads.el
@@ -0,0 +1,22 @@
+;;; all-the-icons-dired-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "all-the-icons-dired" "all-the-icons-dired.el"
+;;;;;;  (22955 52904 504658 281000))
+;;; Generated autoloads from all-the-icons-dired.el
+
+(autoload 'all-the-icons-dired-mode "all-the-icons-dired" "\
+Display all-the-icons icon for each files in a dired buffer.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; all-the-icons-dired-autoloads.el ends here
