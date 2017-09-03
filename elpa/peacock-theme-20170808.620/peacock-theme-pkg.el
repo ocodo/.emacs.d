aaabc7cd1c61823c2079fb79d69776bf228045f6
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:38 +0000

    [Adding peacock-theme version: 20170808.620]

---
+++ elpa/peacock-theme-20170808.620/peacock-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "peacock-theme" "20170808.620" "an Emacs 24 theme based on Peacock (tmTheme)" '((emacs "24.0")) :commit "9e46fbfb562b6e26c6e3d6d618b044b3694da4c8" :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
