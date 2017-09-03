b57be7cf2e42eceb33b50c44c28424026bf7ae19
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:44 +0000

    [Adding splitter version: 20170809.1508]

---
+++ elpa/splitter-20170809.1508/splitter-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "splitter" "20170809.1508" "Manage window splits" 'nil :commit "6bdb51e9a346907d60a9625f6180bddd06be6674" :url "https://github.com/chumpage/chumpy-windows" :keywords '("frames" "convenience"))
