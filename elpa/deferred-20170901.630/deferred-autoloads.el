b50b28ea9edbc3c5c1ea0a1c5305772e3f532cab
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:08 +0000

    [Adding deferred version: 20170901.630]

---
+++ elpa/deferred-20170901.630/deferred-autoloads.el
@@ -0,0 +1,15 @@
+;;; deferred-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("deferred.el") (22955 53596 753747 949000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; deferred-autoloads.el ends here
