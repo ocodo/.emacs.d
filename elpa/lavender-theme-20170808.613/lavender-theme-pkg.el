fe407dfbd8986814474fda7d91c2ecf7fe72cca7
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:31 +0000

    [Adding lavender-theme version: 20170808.613]

---
+++ elpa/lavender-theme-20170808.613/lavender-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lavender-theme" "20170808.613" "an Emacs 24 theme based on Lavender (tmTheme)" '((emacs "24.0")) :commit "ef5e959b95d7fb8152137bc186c4c24e986c1e3c" :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
