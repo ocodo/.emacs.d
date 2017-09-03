c93f7c16d052b3d01af71b377827e9e75b11fe3d
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:45 +0000

    [Adding sublimity version: 20170820.827]

---
+++ elpa/sublimity-20170820.827/sublimity-pkg.el
@@ -0,0 +1,6 @@
+(define-package "sublimity" "20170820.827" "smooth-scrolling, minimap and distraction-free mode"
+  '((cl-lib "0.3"))
+  :url "https://github.com/zk-phi/sublimity")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
