7a51db61fd73333068f8ca02ead911fbacdffa13
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:08 +0000

    [Adding dark-krystal-theme version: 20170808.600]

---
+++ elpa/dark-krystal-theme-20170808.600/dark-krystal-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dark-krystal-theme" "20170808.600" "an Emacs 24 theme based on Dark Krystal (tmTheme)" '((emacs "24.0")) :commit "79084b99665dc9ffb0ec62cc092349a5ecebebbc" :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
