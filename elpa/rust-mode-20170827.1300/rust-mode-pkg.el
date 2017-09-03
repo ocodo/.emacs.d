2decf48e79a4b9fecd18e20ebffba06c83e0dad1
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:41 +0000

    [Adding rust-mode version: 20170827.1300]

---
+++ elpa/rust-mode-20170827.1300/rust-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rust-mode" "20170827.1300" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "c8280aea1d02ae84bff2a6f02f8dc7701309c8b6" :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
