2348e042dbb05fb499ac257589f042cd1660e2a7
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:26 +0000

    [Adding http version: 20170822.2149]

---
+++ elpa/http-20170822.2149/http-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "http" "20170822.2149" "Yet another HTTP client" '((emacs "24.4") (request "0.2.0") (edit-indirect "0.1.4")) :commit "f7844d9f84cb5e89655934ab0f23f9e4b82402cb" :url "https://github.com/emacs-pe/http.el" :keywords '("convenience"))
