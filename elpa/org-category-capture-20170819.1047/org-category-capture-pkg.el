15a52ad4f039a6238b8c2967c63818253cc74caf
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:37 +0000

    [Adding org-category-capture version: 20170819.1047]

---
+++ elpa/org-category-capture-20170819.1047/org-category-capture-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-category-capture" "20170819.1047" "Contextualy capture of org-mode TODOs." '((org "9.0.0") (emacs "24")) :commit "48f621b595e748c5e03431f237facf258ffc9443" :url "https://github.com/IvanMalison/org-projectile" :keywords '("org-mode" "todo" "tools" "outlines"))
