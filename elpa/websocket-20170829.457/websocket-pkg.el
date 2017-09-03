38f5e678980ab4df02395656fa087d28fabf79ed
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:51 +0000

    [Adding websocket version: 20170829.457]

---
+++ elpa/websocket-20170829.457/websocket-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "websocket" "20170829.457" "Emacs WebSocket client and server" '((cl-lib "0.5")) :commit "33d0406901f7306b326928190b6ad94247d01a5f" :keywords '("communication" "websocket" "server"))
