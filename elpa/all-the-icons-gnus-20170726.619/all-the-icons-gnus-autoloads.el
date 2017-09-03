86232b97484090c28b23e91914bc873cbb6ebe97
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:04 +0000

    [Adding all-the-icons-gnus version: 20170726.619]

---
+++ elpa/all-the-icons-gnus-20170726.619/all-the-icons-gnus-autoloads.el
@@ -0,0 +1,22 @@
+;;; all-the-icons-gnus-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "all-the-icons-gnus" "all-the-icons-gnus.el"
+;;;;;;  (22955 52932 460741 252000))
+;;; Generated autoloads from all-the-icons-gnus.el
+
+(autoload 'all-the-icons-gnus-setup "all-the-icons-gnus" "\
+Add icons for Gnus.
+
+\(fn)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; all-the-icons-gnus-autoloads.el ends here
