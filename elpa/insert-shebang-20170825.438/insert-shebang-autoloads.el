a0e8f9884397f2b7a37ce5f214a04e1cdeef1aa2
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:28 +0000

    [Adding insert-shebang version: 20170825.438]

---
+++ elpa/insert-shebang-20170825.438/insert-shebang-autoloads.el
@@ -0,0 +1,25 @@
+;;; insert-shebang-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "insert-shebang" "insert-shebang.el" (22955
+;;;;;;  53390 601578 659000))
+;;; Generated autoloads from insert-shebang.el
+
+(autoload 'insert-shebang "insert-shebang" "\
+Insert shebang line automatically.
+Calls function `insert-shebang-get-extension-and-insert'.  With argument as
+`buffer-name'.
+
+\(fn)" t nil)
+(add-hook 'find-file-hook 'insert-shebang)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; insert-shebang-autoloads.el ends here
---
+++ elpa/insert-shebang-20170825.438/insert-shebang-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "insert-shebang" "20170825.438" "Insert shebang line automatically." 'nil :commit "adfa473f07443b231914d277c20a3419b30399b6" :url "http://github.com/psachin/insert-shebang" :keywords '("shebang" "tool" "convenience"))
