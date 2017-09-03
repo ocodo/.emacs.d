215c07d7e3f1813c2ab17a8e476a3d98a7588828
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:21 +0000

    [Adding hackernews version: 20170829.1044]

---
+++ elpa/hackernews-20170829.1044/hackernews-autoloads.el
@@ -0,0 +1,27 @@
+;;; hackernews-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "hackernews" "hackernews.el" (22955 53495 705675
+;;;;;;  743000))
+;;; Generated autoloads from hackernews.el
+
+(autoload 'hackernews "hackernews" "\
+Read Hacker News.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("hackernews-pkg.el") (22955 53495 709675
+;;;;;;  746000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; hackernews-autoloads.el ends here
---
+++ elpa/hackernews-20170829.1044/hackernews-pkg.el
@@ -0,0 +1,5 @@
+(define-package "hackernews" "20170829.1044" "Access the hackernews aggregator from Emacs"
+  '((json "1.2")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
