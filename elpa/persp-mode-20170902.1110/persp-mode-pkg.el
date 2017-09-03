a275150104621dc3ffbcc80ef261bb77394b6180
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:39 +0000

    [Adding persp-mode version: 20170902.1110]

---
+++ elpa/persp-mode-20170902.1110/persp-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "persp-mode" "20170902.1110" "windows/buffers sets shared among frames + save/load." 'nil :commit "298420817486ab7dcbe6f0d7b3f272814f0670ee" :url "https://github.com/Bad-ptr/persp-mode.el" :keywords '("perspectives" "session" "workspace" "persistence" "windows" "buffers" "convenience"))
