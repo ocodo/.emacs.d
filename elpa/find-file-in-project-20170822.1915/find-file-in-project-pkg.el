3ca16f8035f5a170a6762b6a12eff026f90b521a
Jasonm23 <jasonm23@gmail.com>
2017-09-03 10:26:16 +0000

    [Adding find-file-in-project version: 20170822.1915]

---
+++ elpa/find-file-in-project-20170822.1915/find-file-in-project-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "find-file-in-project" "20170822.1915" "Find file/directory and review Diff/Patch/Commit efficiently everywhere" '((ivy "0.7.0") (emacs "24.3")) :commit "cb61762e50e85894febbbc82eeafa85d67707218" :url "https://github.com/technomancy/find-file-in-project" :keywords '("project" "convenience"))
