04d68ee13a13c90135083d95cdf220b6b7f1762f
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:42 +0000

    [Adding icicles version: 20170917.942]

---
+++ elpa/icicles-20170917.942/icicles-pkg.el
@@ -0,0 +1,5 @@
+(define-package "icicles" "20170917.942" "Minibuffer input completion and cycling." 'nil :url "https://www.emacswiki.org/emacs/download/icicles.el" :keywords
+  '("extensions" "help" "abbrev" "local" "minibuffer" "projects" "keys" "apropos" "completion" "matching" "regexp" "command"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
