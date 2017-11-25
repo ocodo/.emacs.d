d6a52df33b7e9208d9ab4ef84bbb36c56574b2cb
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:21 +0000

    [Adding buffer-watcher version: 20170913.139]

---
+++ elpa/buffer-watcher-20170913.139/buffer-watcher-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "buffer-watcher" "20170913.139" "Easily run shell scripts per filetype/directory when a buffer is saved" '((f "0.16.2") (cl-lib "0.5")) :commit "b32c67c8a5d724257d759f4c903d0dedc32246ef")
