2c2c3d37ed9f63039ddf0c491ec2387b8285d741
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:26 +0000

    [Adding sicp version: 20171028.1523]

---
+++ elpa/sicp-20171028.1523/sicp-autoloads.el
@@ -0,0 +1,16 @@
+;;; sicp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("sicp-pkg.el" "sicp.el") (23064 60643
+;;;;;;  824506 730000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sicp-autoloads.el ends here
---
+++ elpa/sicp-20171028.1523/sicp-pkg.el
@@ -0,0 +1,4 @@
+(define-package "sicp" "20171028.1523" "Structure and Interpretation of Computer Programs in info format" 'nil :url "https://mitpress.mit.edu/sicp")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
