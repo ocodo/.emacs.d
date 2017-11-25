e018810914fc9be6e98e3a5f58314ec875d72177
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:15 +0000

    [Adding git-commit version: 20171123.752]

---
+++ elpa/git-commit-20171123.752/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20171123.752" "Edit Git commit messages" '((emacs "24.4") (dash "20170810") (with-editor "20170817")) :commit "976d023381c7268fe8c7ac18232928ddf35e598e" :url "https://github.com/magit/magit" :keywords '("git" "tools" "vc"))
