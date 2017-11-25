59298604bd39e337bb6b0a5ae500cf8148403c5d
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:22 +0000

    [Adding crystal-mode version: 20171023.212]

---
+++ elpa/crystal-mode-20171023.212/crystal-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "crystal-mode" "20171023.212" "Major mode for editing Crystal files" '((emacs "24.3")) :commit "2931006427a0979cd02e8e0af1e422636402ea9c" :url "https://github.com/crystal-lang-tools/emacs-crystal-mode" :keywords '("languages" "crystal"))
