d697fdf75e83f5742b24e3b991de91b3651e911d
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:23 +0000

    [Adding diredfl version: 20171014.1402]

---
+++ elpa/diredfl-20171014.1402/diredfl-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "diredfl" "20171014.1402" "Extra font lock rules for a more colourful dired" '((emacs "24")) :commit "45e9c46f58c655694758d4986b9195c8bb40ddf3" :url "https://github.com/purcell/diredfl" :keywords '("faces"))
