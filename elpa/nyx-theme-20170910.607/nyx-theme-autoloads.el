3fd6b0cf47f9766d46a63b130a6afe0ad12f4dfc
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:25 +0000

    [Adding nyx-theme version: 20170910.607]

---
+++ elpa/nyx-theme-20170910.607/nyx-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; nyx-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "nyx-theme" "nyx-theme.el" (23064 60652 824420
+;;;;;;  692000))
+;;; Generated autoloads from nyx-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; nyx-theme-autoloads.el ends here
---
+++ elpa/nyx-theme-20170910.607/nyx-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "nyx-theme" "20170910.607" "Dark theme" '((emacs "24")) :commit "afe2b8c3b5421b4c292d182dcf77079b278e93d8" :url "https://github.com/GuidoSchmidt/emacs-nyx-theme" :keywords '("themes" "dark-theme"))
