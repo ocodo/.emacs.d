473a6c1abbdead936c0e70579c24c3a66100b5b6
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:31 +0000

    [Adding use-package version: 20171121.1430]

---
+++ elpa/use-package-20171121.1430/use-package-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "use-package" "20171121.1430" "A use-package declaration for simplifying your .emacs" '((bind-key "1.0") (diminish "0.44")) :commit "98f9e120aaac902f611807002ed58215b2620bfd" :url "https://github.com/jwiegley/use-package" :keywords '("dotemacs" "startup" "speed" "config" "package"))
