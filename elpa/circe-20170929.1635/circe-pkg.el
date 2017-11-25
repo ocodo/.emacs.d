660cb1b3182ac12cee4bb1073d64a400b6c0ea85
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:33:44 +0000

    [Updating circe version: 20170929.1635][removing old version: 20170728.638]

--- elpa/circe-20170929.1635/circe-pkg.el
+++ elpa/circe-20170929.1635/circe-pkg.el
@@ -1,4 +1,4 @@
-(define-package "circe" "20170728.638" "Client for IRC in Emacs"
+(define-package "circe" "20170929.1635" "Client for IRC in Emacs"
   '((cl-lib "0.5"))
   :url "https://github.com/jorgenschaefer/circe")
 ;; Local Variables:
