04946bff659e08767b4e52ef04b0c779622de37c
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:13 +0000

    [Adding gist version: 20171119.1737]

---
+++ elpa/gist-20171119.1737/gist-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gist" "20171119.1737" "Emacs integration for gist.github.com" '((emacs "24.1") (gh "0.10.0")) :commit "952048407e0bcd19bbe7e3a591ecbc0586a04abc" :url "https://github.com/defunkt/gist.el" :keywords '("tools"))
