ed3c15cce0b4ae8ce35fb9d1aa100bd3763dbcb3
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:33 +0000

    [Adding vue-mode version: 20171029.1905]

---
+++ elpa/vue-mode-20171029.1905/vue-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vue-mode" "20171029.1905" "Major mode for vue component based on mmm-mode" '((mmm-mode "0.5.4") (vue-html-mode "0.1") (ssass-mode "0.1") (edit-indirect "0.1.4")) :commit "a34f8a14fe6a680bb18cfa4519f7984d8071a4e3" :keywords '("languages"))
