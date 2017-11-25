ab238233d769ed5691cfcf6f1758385b7f898823
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:32 +0000

    [Adding vue-html-mode version: 20170928.1057]

---
+++ elpa/vue-html-mode-20170928.1057/vue-html-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vue-html-mode" "20170928.1057" "Major mode for editing Vue.js templates" 'nil :commit "9218c61ff1ab2bdc6a6711d21ad760198d3511a8" :url "http://github.com/AdamNiederer/vue-html-mode" :keywords '("languages" "vue" "template"))
