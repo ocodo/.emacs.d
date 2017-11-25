991dabd1081af9a137ba9c8b4ed9e676495fc252
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:57 +0000

    [Adding merlin version: 20170917.430]

---
+++ elpa/merlin-20170917.430/merlin-pkg.el
@@ -0,0 +1,5 @@
+(define-package "merlin" "20170917.430" "Mode for Merlin, an assistant for OCaml." 'nil :url "https://github.com/ocaml/merlin" :keywords
+  '("ocaml" "languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
