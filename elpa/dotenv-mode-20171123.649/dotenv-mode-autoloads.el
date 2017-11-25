60b5485a083cc0cc73fbf44b6e18c1defdcbfdf9
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:23 +0000

    [Adding dotenv-mode version: 20171123.649]

---
+++ elpa/dotenv-mode-20171123.649/dotenv-mode-autoloads.el
@@ -0,0 +1,24 @@
+;;; dotenv-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "dotenv-mode" "dotenv-mode.el" (23064 60665
+;;;;;;  220304 252000))
+;;; Generated autoloads from dotenv-mode.el
+
+(autoload 'dotenv-mode "dotenv-mode" "\
+Major mode for `.env' files.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.env\\'" . dotenv-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dotenv-mode-autoloads.el ends here
