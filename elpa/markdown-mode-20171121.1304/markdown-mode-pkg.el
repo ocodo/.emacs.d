e44cd38a45b507cc80916568f85005b9f95351ee
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:56 +0000

    [Adding markdown-mode version: 20171121.1304]

---
+++ elpa/markdown-mode-20171121.1304/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20171121.1304" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "74bcce2ee11c2623091c6d0f8c48d078a98d4a01" :url "https://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
