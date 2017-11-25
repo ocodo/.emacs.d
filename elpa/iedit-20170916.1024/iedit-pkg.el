a547e592169537b941387bc7e501192904c13e22
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:45 +0000

    [Adding iedit version: 20170916.1024]

---
+++ elpa/iedit-20170916.1024/iedit-pkg.el
@@ -0,0 +1,5 @@
+(define-package "iedit" "20170916.1024" "Edit multiple regions in the same way simultaneously." 'nil :url "http://www.emacswiki.org/emacs/Iedit" :keywords
+  '("occurrence" "region" "simultaneous" "refactoring"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
