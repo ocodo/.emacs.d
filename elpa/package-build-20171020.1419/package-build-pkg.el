0737d69a080cbe5c390ee2caabd106556d8c617a
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:07 +0000

    [Adding package-build version: 20171020.1419]

---
+++ elpa/package-build-20171020.1419/package-build-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "package-build" "20171020.1419" "Tools for assembling a package archive" '((cl-lib "0.5")) :commit "e4d174870ea40275a8bc35996416326cf28051f9" :keywords '("tools"))
