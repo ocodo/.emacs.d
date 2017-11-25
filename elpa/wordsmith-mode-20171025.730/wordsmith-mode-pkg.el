6bf209836e0be95516264506853cc033de6071e9
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:37 +0000

    [Adding wordsmith-mode version: 20171025.730]

---
+++ elpa/wordsmith-mode-20171025.730/wordsmith-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "wordsmith-mode" "20171025.730" "Syntax analysis and NLP text-processing in Emacs (OSX-only)" 'nil :commit "589a97412138145bea70e0450eeddeb7f138d538")
