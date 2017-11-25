691180d118b69ff56fc4702c44ad78cfdfcb2b60
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:27 +0000

    [Adding xcode-project version: 20170922.612]

---
+++ elpa/xcode-project-20170922.612/xcode-project-autoloads.el
@@ -0,0 +1,16 @@
+;;; xcode-project-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("xcode-parser.el" "xcode-project-pkg.el"
+;;;;;;  "xcode-project.el") (23064 60634 976592 511000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; xcode-project-autoloads.el ends here
---
+++ elpa/xcode-project-20170922.612/xcode-project-pkg.el
@@ -0,0 +1,7 @@
+(define-package "xcode-project" "20170922.612" "A package for reading Xcode project files."
+  '((emacs "25"))
+  :url "https://github.com/nhojb/xcode-project.git" :keywords
+  '("languages" "tools"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
