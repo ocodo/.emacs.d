e3c0bdf5a8d8102c4f523f109418e57d393d1080
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:34 +0000

    [Adding helm-dash version: 20171120.446]

---
+++ elpa/helm-dash-20171120.446/helm-dash-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-dash" "20171120.446" "Offline documentation browser for +150 APIs using Dash docsets." '((helm "1.9.2") (cl-lib "0.5")) :commit "13dcc176556a7dd0970790e96dbbf9105dc395de" :url "http://github.com/areina/helm-dash" :keywords '("docs"))
