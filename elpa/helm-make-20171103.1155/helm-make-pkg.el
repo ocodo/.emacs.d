125b74efe59541095fba41fb79d96de733477cfb
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:36 +0000

    [Adding helm-make version: 20171103.1155]

---
+++ elpa/helm-make-20171103.1155/helm-make-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-make" "20171103.1155" "Select a Makefile target with helm" '((helm "1.5.3") (projectile "0.11.0")) :commit "feae8df22bc4b20705ea08ac9adfc2b43bb348d0" :url "https://github.com/abo-abo/helm-make" :keywords '("makefile"))
