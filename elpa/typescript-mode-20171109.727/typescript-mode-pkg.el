4827b57f2f214ccb8bb9011093258f062cc86b8d
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:30 +0000

    [Adding typescript-mode version: 20171109.727]

---
+++ elpa/typescript-mode-20171109.727/typescript-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "typescript-mode" "20171109.727" "Major mode for editing typescript" 'nil :commit "5b0487aae890e7e9f7105a679deecc50428e912d" :url "http://github.com/ananthakumaran/typescript.el" :keywords '("typescript" "languages"))
