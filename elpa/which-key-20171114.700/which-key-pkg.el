2f2433fc4f7b293b6794c84ac5a5ed735fbef158
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:36 +0000

    [Adding which-key version: 20171114.700]

---
+++ elpa/which-key-20171114.700/which-key-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "which-key" "20171114.700" "Display available keybindings in popup" '((emacs "24.4")) :commit "6e8df961f4aecbbb59655ea804c62f09eb13b271" :url "https://github.com/justbur/emacs-which-key")
