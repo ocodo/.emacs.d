1055d340a1a980b44c2daf1e05b0aaaf239271a1
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:17 +0000

    [Adding go-autocomplete version: 20170907]

---
+++ elpa/go-autocomplete-20170907/go-autocomplete-autoloads.el
@@ -0,0 +1,16 @@
+;;; go-autocomplete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("go-autocomplete.el") (23064 61472 939841
+;;;;;;  213000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; go-autocomplete-autoloads.el ends here
---
+++ elpa/go-autocomplete-20170907/go-autocomplete-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-autocomplete" "20170907" "auto-complete-mode backend for go-mode" '((auto-complete "1.4.0")) :commit "c7fddb39ecbc9ebd1ebe7d2a3af473ed0fffffa1" :keywords '("languages"))
