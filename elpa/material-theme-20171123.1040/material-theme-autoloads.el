cc17890f1e20436cdd9c5cdd5399ac517bc5d23d
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:57 +0000

    [Adding material-theme version: 20171123.1040]

---
+++ elpa/material-theme-20171123.1040/material-theme-autoloads.el
@@ -0,0 +1,32 @@
+;;; material-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "material-light-theme" "material-light-theme.el"
+;;;;;;  (23064 61224 616765 782000))
+;;; Generated autoloads from material-light-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "material-theme" "material-theme.el" (23064
+;;;;;;  61224 616765 782000))
+;;; Generated autoloads from material-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("material-theme-pkg.el") (23064 61224
+;;;;;;  620765 765000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; material-theme-autoloads.el ends here
---
+++ elpa/material-theme-20171123.1040/material-theme-pkg.el
@@ -0,0 +1,7 @@
+(define-package "material-theme" "20171123.1040" "A Theme based on the colors of the Google Material Design"
+  '((emacs "24.1"))
+  :url "http://github.com/cpaulik/emacs-material-theme" :keywords
+  '("themes"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
