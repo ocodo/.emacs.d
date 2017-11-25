44a999e7ee1e29cbd629ba9ff3eaa0940d7eed66
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:09 +0000

    [Adding flycheck-nimsuggest version: 20171027.1508]

---
+++ elpa/flycheck-nimsuggest-20171027.1508/flycheck-nimsuggest-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-nimsuggest" "20171027.1508" "flycheck backend for Nim using nimsuggest" '((flycheck "0.23") (emacs "24.3")) :commit "dc9a5de1cb3ee05db5794d824610959a1f603bc9" :url "https://github.com/yuutayamada/flycheck-nimsuggest")
