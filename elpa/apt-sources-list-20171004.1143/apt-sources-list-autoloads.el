40f100551825fe72c802f05a0275726867957b99
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:18 +0000

    [Adding apt-sources-list version: 20171004.1143]

---
+++ elpa/apt-sources-list-20171004.1143/apt-sources-list-autoloads.el
@@ -0,0 +1,40 @@
+;;; apt-sources-list-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "apt-sources-list" "apt-sources-list.el" (23064
+;;;;;;  60688 724089 654000))
+;;; Generated autoloads from apt-sources-list.el
+
+(autoload 'apt-sources-list-mode "apt-sources-list" "\
+Major mode for editing APT’s “.list” files.
+
+The “/etc/apt/sources.list” file and other files in
+“/etc/apt/sources.list.d” tell APT, found on Debian-based systems
+and others, where to find packages for installation.
+
+This format specifies a package source with a single line, e.g.:
+
+    deb http://deb.debian.org/debian stable main contrib
+
+For more information about the format you can read the manual
+pages “apt(8)” and “sources.list(5)”, also on the web at URL
+‘https://manpages.debian.org/stable/apt/sources.list.5.en.html’
+and URL ‘https://manpages.debian.org/stable/apt/apt.8.en.html’.
+
+\\{apt-sources-list-mode-map}
+
+The above editing commands will raise errors if the current line
+is not a correctly-formatted APT source.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; apt-sources-list-autoloads.el ends here
---
+++ elpa/apt-sources-list-20171004.1143/apt-sources-list-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "apt-sources-list" "20171004.1143" "Mode for editing APT source.list files" '((emacs "24.4")) :commit "842c3896f660295e4c16938aa1fd195e5d377460" :url "https://git.korewanetadesu.com/apt-sources-list.git")
