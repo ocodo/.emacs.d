c76058228c3519589459eb4644e12e5684b36108
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:06 +0000

    [Adding org-vcard version: 20170929.410]

---
+++ elpa/org-vcard-20170929.410/org-vcard-pkg.el
@@ -0,0 +1,5 @@
+(define-package "org-vcard" "20170929.410" "org-mode support for vCard export and import." 'nil :url "https://github.com/flexibeast/org-vcard" :keywords
+  '("outlines" "org" "vcard"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
