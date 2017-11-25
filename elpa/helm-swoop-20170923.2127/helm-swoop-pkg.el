7f6658a6598afa564f9c3e1e94b125357edad8d1
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:37 +0000

    [Adding helm-swoop version: 20170923.2127]

---
+++ elpa/helm-swoop-20170923.2127/helm-swoop-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-swoop" "20170923.2127" "Efficiently hopping squeezed lines powered by helm interface" '((helm "1.0") (emacs "24.3")) :commit "66a951700e0ef0621cc705791bdd6699488ab121" :url "https://github.com/ShingoFukuyama/helm-swoop" :keywords '("helm" "swoop" "inner" "buffer" "search"))
