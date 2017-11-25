902c518c9fcf5ff8161f6b608256ab66c0c90671
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:31 +0000

    [Adding typing version: 20171115.2331]

---
+++ elpa/typing-20171115.2331/typing-autoloads.el
@@ -0,0 +1,26 @@
+;;; typing-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "typing" "typing.el" (23064 60983 253962 631000))
+;;; Generated autoloads from typing.el
+
+(autoload 'typing-of-emacs "typing" "\
+Play the game The Typing Of Emacs.
+The game builds a list of words from the current buffer.
+In the buffer *The Typing Of Emacs* you will be asked to
+type the words.  Should you take more than a certain
+number of seconds to do the typing, you loose.  As you
+continue playing the words will get longer and longer.
+
+\(fn &optional ZOMBIE)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; typing-autoloads.el ends here
---
+++ elpa/typing-20171115.2331/typing-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "typing" "20171115.2331" "The Typing Of Emacs" 'nil :commit "1ada06484695b8959f4a7c41cacf7f78c2aad998" :url "http://www.emacswiki.org/emacs/TypingOfEmacs" :keywords '("games"))
