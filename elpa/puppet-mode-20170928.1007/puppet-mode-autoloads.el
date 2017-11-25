09ef6b46eac82ab60fb96fa71b33502ee372e66d
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:12 +0000

    [Adding puppet-mode version: 20170928.1007]

---
+++ elpa/puppet-mode-20170928.1007/puppet-mode-autoloads.el
@@ -0,0 +1,24 @@
+;;; puppet-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "puppet-mode" "puppet-mode.el" (23064 61081
+;;;;;;  505430 300000))
+;;; Generated autoloads from puppet-mode.el
+
+(autoload 'puppet-mode "puppet-mode" "\
+
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; puppet-mode-autoloads.el ends here
---
+++ elpa/puppet-mode-20170928.1007/puppet-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "puppet-mode" "20170928.1007" "Major mode for Puppet manifests" '((emacs "24.1") (pkg-info "0.4")) :commit "e04f041386ebfe29fc67c3407e85b577b820df4f" :url "https://github.com/voxpupuli/puppet-mode" :keywords '("languages"))
