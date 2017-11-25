185566e64bb73dbf7d69da4736ceb55f15ff56cc
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:18 +0000

    [Adding adafruit-wisdom version: 20171115.1228]

---
+++ elpa/adafruit-wisdom-20171115.1228/adafruit-wisdom-autoloads.el
@@ -0,0 +1,24 @@
+;;; adafruit-wisdom-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "adafruit-wisdom" "adafruit-wisdom.el" (23064
+;;;;;;  60690 820070 911000))
+;;; Generated autoloads from adafruit-wisdom.el
+
+(autoload 'adafruit-wisdom "adafruit-wisdom" "\
+Display one of Adafruit's quotes in the minibuffer.
+If INSERT is non-nil the joke will be inserted into the current
+buffer rather than shown in the minibuffer.
+
+\(fn &optional INSERT)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; adafruit-wisdom-autoloads.el ends here
---
+++ elpa/adafruit-wisdom-20171115.1228/adafruit-wisdom-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "adafruit-wisdom" "20171115.1228" "Get/display adafruit.com quotes" '((emacs "24")) :commit "7492e13a5c9e6dabe2cb98072888cdad7694ef64" :url "https://github.com/gonewest818/adafruit-wisdom.el" :keywords '("games"))
