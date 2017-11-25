0e813de489c3fd08a3ff33567d6b553ff89672d3
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:48 +0000

    [Adding inkpot-theme version: 20171106.640]

---
+++ elpa/inkpot-theme-20171106.640/inkpot-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; inkpot-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "inkpot-theme" "inkpot-theme.el" (23064 61320
+;;;;;;  228380 633000))
+;;; Generated autoloads from inkpot-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; inkpot-theme-autoloads.el ends here
---
+++ elpa/inkpot-theme-20171106.640/inkpot-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "inkpot-theme" "20171106.640" "port of vim's inkpot theme" 'nil :commit "046d0bb361500e14c718b00930d31c73890929cb" :url "https://github.com/ideasman42/emacs-inkpot-theme" :keywords '("color" "theme"))
