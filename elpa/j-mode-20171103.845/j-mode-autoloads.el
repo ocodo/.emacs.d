296fd2d61d600ca8929e14654992fafad1ea49c6
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:49 +0000

    [Adding j-mode version: 20171103.845]

---
+++ elpa/j-mode-20171103.845/j-mode-autoloads.el
@@ -0,0 +1,55 @@
+;;; j-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "j-console" "j-console.el" (23064 61313 324407
+;;;;;;  95000))
+;;; Generated autoloads from j-console.el
+
+(autoload 'j-console "j-console" "\
+Ensures a running j-console-cmd session and switches focus to
+the containing buffer
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "j-help" "j-help.el" (23064 61313 316407 126000))
+;;; Generated autoloads from j-help.el
+
+(autoload 'j-help-lookup-symbol "j-help" "\
+Lookup symbol in dictionary
+
+\(fn SYMBOL)" t nil)
+
+(autoload 'j-help-lookup-symbol-at-point "j-help" "\
+Determine the symbol nearest to POINT and look it up in the dictionary
+
+\(fn POINT)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "j-mode" "j-mode.el" (23064 61313 320407 111000))
+;;; Generated autoloads from j-mode.el
+
+(autoload 'j-mode "j-mode" "\
+Major mode for editing J
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.ij[rstp]$" . j-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("j-font-lock.el" "j-mode-pkg.el") (23064
+;;;;;;  61313 324407 95000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; j-mode-autoloads.el ends here
---
+++ elpa/j-mode-20171103.845/j-mode-pkg.el
@@ -0,0 +1,5 @@
+(define-package "j-mode" "20171103.845" "Major mode for editing J programs" 'nil :url "http://github.com/zellio/j-mode" :keywords
+  '("j" "languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
