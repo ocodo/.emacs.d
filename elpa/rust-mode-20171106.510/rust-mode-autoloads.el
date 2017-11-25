415843f60942217f351d52ab31b519c77a126d1d
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:18 +0000

    [Adding rust-mode version: 20171106.510]

---
+++ elpa/rust-mode-20171106.510/rust-mode-autoloads.el
@@ -0,0 +1,26 @@
+;;; rust-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "rust-mode" "rust-mode.el" (23064 61059 5546
+;;;;;;  135000))
+;;; Generated autoloads from rust-mode.el
+
+(autoload 'rust-mode "rust-mode" "\
+Major mode for Rust code.
+
+\\{rust-mode-map}
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; rust-mode-autoloads.el ends here
