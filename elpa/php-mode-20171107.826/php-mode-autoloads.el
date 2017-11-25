4f7c169e8e1fa1add6c9b863a4153bd979aa8c67
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:09 +0000

    [Adding php-mode version: 20171107.826]

---
+++ elpa/php-mode-20171107.826/php-mode-autoloads.el
@@ -0,0 +1,56 @@
+;;; php-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "php-mode" "php-mode.el" (23064 61094 433365
+;;;;;;  271000))
+;;; Generated autoloads from php-mode.el
+
+(let ((loads (get 'php 'custom-loads))) (if (member '"php-mode" loads) nil (put 'php 'custom-loads (cons '"php-mode" loads))))
+
+(defvar php-extra-constants 'nil "\
+A list of additional strings to treat as PHP constants.")
+
+(custom-autoload 'php-extra-constants "php-mode" nil)
+
+(if (version< emacs-version "24.4") (dolist (i '("php" "php5" "php7")) (add-to-list 'interpreter-mode-alist (cons i 'php-mode))) (add-to-list 'interpreter-mode-alist (cons "php\\(?:-?[3457]\\(?:\\.[0-9]+\\)*\\)?" 'php-mode)))
+
+(let ((loads (get 'php-faces 'custom-loads))) (if (member '"php-mode" loads) nil (put 'php-faces 'custom-loads (cons '"php-mode" loads))))
+
+(autoload 'php-mode "php-mode" "\
+Major mode for editing PHP code.
+
+\\{php-mode-map}
+
+\(fn)" t nil)
+
+(autoload 'php-current-class "php-mode" "\
+Insert current class name if cursor in class context.
+
+\(fn)" t nil)
+
+(autoload 'php-current-namespace "php-mode" "\
+Insert current namespace if cursor in namespace context.
+
+\(fn)" t nil)
+
+(dolist (pattern '("\\.php[s345t]?\\'" "/\\.php_cs\\(\\.dist\\)?\\'" "\\.phtml\\'" "/Amkfile\\'" "\\.amk\\'")) (add-to-list 'auto-mode-alist `(,pattern . php-mode) t))
+
+;;;***
+
+;;;### (autoloads nil nil ("php-array.el" "php-classobj.el" "php-control-structures.el"
+;;;;;;  "php-crack.el" "php-dio.el" "php-dom.el" "php-exceptions.el"
+;;;;;;  "php-exif.el" "php-ext.el" "php-filesystem.el" "php-gd.el"
+;;;;;;  "php-math.el" "php-mode-pkg.el" "php-pcre.el" "php-regex.el"
+;;;;;;  "php-simplexml.el" "php-strings.el" "php-var.el" "php-xmlparser.el"
+;;;;;;  "php-xmlreader.el") (23064 61094 445365 211000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; php-mode-autoloads.el ends here
