e8a055fb766ff263b7eb8b6cd08ee955e8fbdd61
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:29 +0000

    [Adding tuareg version: 20171119.306]

---
+++ elpa/tuareg-20171119.306/tuareg-pkg.el
@@ -0,0 +1,7 @@
+(define-package "tuareg" "20171119.306" "OCaml mode for Emacs."
+  '((caml "3.12.0.1"))
+  :url "https://github.com/ocaml/tuareg" :keywords
+  '("ocaml" "languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
