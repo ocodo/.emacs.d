c2ce9f2a713361031af1d7b6939def39d7894aae
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:41 +0000

    [Adding ibuffer-projectile version: 20171103.2004]

---
+++ elpa/ibuffer-projectile-20171103.2004/ibuffer-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ibuffer-projectile" "20171103.2004" "Group ibuffer's list by projectile root" '((projectile "0.11.0")) :commit "6f03040a93d116b73f9fd3d6b52102b57bfaf597" :url "http://github.com/purcell/ibuffer-projectile" :keywords '("themes"))
