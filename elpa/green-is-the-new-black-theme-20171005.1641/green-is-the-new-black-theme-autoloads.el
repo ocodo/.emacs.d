3dc630bdc6a8b8f753ee110ac09eb4a3037d382a
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:26 +0000

    [Adding green-is-the-new-black-theme version: 20171005.1641]

---
+++ elpa/green-is-the-new-black-theme-20171005.1641/green-is-the-new-black-theme-autoloads.el
@@ -0,0 +1,16 @@
+;;; green-is-the-new-black-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("green-is-the-new-black-theme.el") (23064
+;;;;;;  61456 999893 792000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; green-is-the-new-black-theme-autoloads.el ends here
---
+++ elpa/green-is-the-new-black-theme-20171005.1641/green-is-the-new-black-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "green-is-the-new-black-theme" "20171005.1641" "A cool and minimalist green blackened theme" 'nil :commit "b9cdecd04691311c30c94ad4ec81124e63b9b122" :url "https://github.com/fredcamps/green-is-the-new-black-emacs" :keywords '("faces" "themes"))
