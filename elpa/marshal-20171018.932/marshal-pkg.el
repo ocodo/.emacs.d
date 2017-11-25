eb321c242ddc7b5b1b7eab05ade3afdf5973810a
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:56 +0000

    [Adding marshal version: 20171018.932]

---
+++ elpa/marshal-20171018.932/marshal-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "marshal" "20171018.932" "eieio extension for automatic (un)marshalling" '((eieio "1.4") (json "1.3") (ht "2.1")) :commit "406ea2f60c7e4aeafd384b8d6696d726b3d8f611" :url "https://github.com/sigma/marshal.el" :keywords '("eieio"))
