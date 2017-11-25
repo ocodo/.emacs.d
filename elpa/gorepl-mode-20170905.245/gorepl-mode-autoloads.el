765a1a189bcb72b844b16c29d6b21322c5914e2a
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:20 +0000

    [Adding gorepl-mode version: 20170905.245]

---
+++ elpa/gorepl-mode-20170905.245/gorepl-mode-autoloads.el
@@ -0,0 +1,22 @@
+;;; gorepl-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "gorepl-mode" "gorepl-mode.el" (23064 61465
+;;;;;;  299866 307000))
+;;; Generated autoloads from gorepl-mode.el
+
+(autoload 'gorepl-mode "gorepl-mode" "\
+A minor mode for run a go repl on top of gore
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; gorepl-mode-autoloads.el ends here
