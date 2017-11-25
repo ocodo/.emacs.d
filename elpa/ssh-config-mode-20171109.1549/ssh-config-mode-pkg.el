c762d1283ac968a267b1ff696ccaf99ddb7da87b
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:23 +0000

    [Adding ssh-config-mode version: 20171109.1549]

---
+++ elpa/ssh-config-mode-20171109.1549/ssh-config-mode-pkg.el
@@ -0,0 +1,5 @@
+(define-package "ssh-config-mode" "20171109.1549" "Mode for fontification of ~/.ssh/config" 'nil :url "https://github.com/jhgorrell/ssh-config-mode-el" :keywords
+  '("ssh" "config" "emacs"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
