9161f18727f1fc7830723378b23c1c64b508b655
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:38 +0000

    [Adding writegood-mode version: 20170925.647]

---
+++ elpa/writegood-mode-20170925.647/writegood-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "writegood-mode" "20170925.647" "Polish up poor writing on the fly" 'nil :commit "416453af5a44da52646052fa2ee3a63acfd7bf8a" :url "http://github.com/bnbeckwith/writegood-mode" :keywords '("writing" "weasel-words" "grammar"))
