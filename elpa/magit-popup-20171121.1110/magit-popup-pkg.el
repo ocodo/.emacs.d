22674915d64f899d86df960cd7b8a95e6091d4a2
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:55 +0000

    [Adding magit-popup version: 20171121.1110]

---
+++ elpa/magit-popup-20171121.1110/magit-popup-pkg.el
@@ -0,0 +1,9 @@
+(define-package "magit-popup" "20171121.1110" "Define prefix-infix-suffix command combos"
+  '((emacs "24.4")
+    (async "1.9.2")
+    (dash "2.13.0"))
+  :url "https://github.com/magit/magit-popup" :keywords
+  '("bindings"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
