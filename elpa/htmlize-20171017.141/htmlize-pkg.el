5af5da5266fb136f414b550649957161256f88f6
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:40 +0000

    [Adding htmlize version: 20171017.141]

---
+++ elpa/htmlize-20171017.141/htmlize-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "htmlize" "20171017.141" "Convert buffer text and decorations to HTML." 'nil :commit "a0b55cba65a225df8d9e17a3ac448ce9e8ec962d" :keywords '("hypermedia" "extensions"))
