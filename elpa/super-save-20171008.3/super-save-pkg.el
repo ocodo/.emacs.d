a255913345d45ad9cae9057df60634670da8bf53
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:25 +0000

    [Adding super-save version: 20171008.3]

---
+++ elpa/super-save-20171008.3/super-save-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "super-save" "20171008.3" "Auto-save buffers, based on your activity." '((emacs "24.4")) :commit "1c8fbd5e18277e4af0ada2678a854b1c9072db38" :url "https://github.com/bbatsov/super-save" :keywords '("convenience"))
