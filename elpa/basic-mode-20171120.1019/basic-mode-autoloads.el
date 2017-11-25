fe78a54711d3a5eb832ade382c7bafa7fe40b0e3
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:20 +0000

    [Adding basic-mode version: 20171120.1019]

---
+++ elpa/basic-mode-20171120.1019/basic-mode-autoloads.el
@@ -0,0 +1,31 @@
+;;; basic-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "basic-mode" "basic-mode.el" (23064 60680 220166
+;;;;;;  373000))
+;;; Generated autoloads from basic-mode.el
+
+(autoload 'basic-mode "basic-mode" "\
+Major mode for editing BASIC code.
+Commands:
+TAB indents for BASIC code. RET will insert a new line starting
+with a fresh line number if line numbers are turned on.
+
+To turn on line numbers, customize variables `basic-auto-number'
+and `basic-line-number-cols'.
+
+\\{basic-mode-map}
+
+\(fn)" t nil)
+ (add-to-list 'auto-mode-alist '("\\.bas\\'" . basic-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; basic-mode-autoloads.el ends here
