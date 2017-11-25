46b18d1d33afd6dd0943bebf40782f7bc75feb25
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:53 +0000

    [Adding kubernetes version: 20171122.1819]

---
+++ elpa/kubernetes-20171122.1819/kubernetes-pkg.el
@@ -0,0 +1,7 @@
+(define-package "kubernetes" "20171122.1819" "Magit-like porcelain for Kubernetes."
+  '((emacs "25.1")
+    (dash "2.12.0")
+    (magit "2.8.0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
