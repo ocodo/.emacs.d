9e15043d4b73f46eef1af66660b97c3e6c6ae3aa
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:21 +0000

    [Adding smart-compile version: 20171104.2333]

---
+++ elpa/smart-compile-20171104.2333/smart-compile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smart-compile" "20171104.2333" "an interface to `compile'" 'nil :commit "2a0c9b33bd97461d100e24df0103d4e5526a30d6" :keywords '("tools" "unix"))
