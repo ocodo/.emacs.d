c5d78ef83887f976b0a033dabb0f08b4e315386a
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:46 +0000

    [Adding imenu-list version: 20171120.1255]

---
+++ elpa/imenu-list-20171120.1255/imenu-list-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "imenu-list" "20171120.1255" "Show imenu entries in a separate buffer" '((cl-lib "0.5")) :commit "62de9b897f0f6168f57cc64532dbcaa1bfcbe47e" :url "https://github.com/bmag/imenu-list")
