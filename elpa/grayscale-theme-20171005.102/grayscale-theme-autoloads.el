abb3c7a223b400ae69207347601c25b99a9b4515
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:24 +0000

    [Adding grayscale-theme version: 20171005.102]

---
+++ elpa/grayscale-theme-20171005.102/grayscale-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; grayscale-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "grayscale-theme" "grayscale-theme.el" (23064
+;;;;;;  60663 320321 939000))
+;;; Generated autoloads from grayscale-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; grayscale-theme-autoloads.el ends here
