dbf093ee12e47986f8da8fc2ec7c2a12e52b7081
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:24 +0000

    [Adding goto-chg version: 20170917.1200]

---
+++ elpa/goto-chg-20170917.1200/goto-chg-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "goto-chg" "20170917.1200" "goto last change" 'nil :commit "81fca94d2592cf32bab451c39a9cd2d9b03fbd6b" :keywords '("convenience" "matching"))
