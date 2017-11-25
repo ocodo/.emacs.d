6e5cf53e000586eefbcb411ea35ed9173913d3a6
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:28 +0000

    [Adding groovy-mode version: 20171024.221]

---
+++ elpa/groovy-mode-20171024.221/groovy-mode-pkg.el
@@ -0,0 +1,7 @@
+(define-package "groovy-mode" "20171024.221" "Major mode for Groovy source files"
+  '((s "1.12.0"))
+  :keywords
+  '("languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
