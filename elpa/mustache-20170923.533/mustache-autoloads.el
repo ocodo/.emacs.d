0030521a6229e0c3aa74f3fd51e8d355f7784952
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:00 +0000

    [Adding mustache version: 20170923.533]

---
+++ elpa/mustache-20170923.533/mustache-autoloads.el
@@ -0,0 +1,17 @@
+;;; mustache-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("mustache-lex.el" "mustache-parse.el"
+;;;;;;  "mustache-pkg.el" "mustache-render.el" "mustache.el") (23064
+;;;;;;  61201 928863 548000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; mustache-autoloads.el ends here
