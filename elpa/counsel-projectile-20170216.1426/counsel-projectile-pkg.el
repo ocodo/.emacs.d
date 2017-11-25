a3316b5a6f145745da4328261977c7f43e4c6fab
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:33:51 +0000

    [Updating counsel-projectile version: 20171107.1456][removing old versions: 20170216.1426,20170816.200]

--- elpa/counsel-projectile-20170216.1426/counsel-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel-projectile" "20170216.1426" "Ivy integration for Projectile" '((counsel "0.8.0") (projectile "0.14.0")) :url "https://github.com/ericdanan/counsel-projectile" :keywords '("project" "convenience"))
--- elpa/counsel-projectile-20171107.1456/counsel-projectile-autoloads.el
+++ elpa/counsel-projectile-20171107.1456/counsel-projectile-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "counsel-projectile" "counsel-projectile.el"
-;;;;;;  (0 0 0 0))
+;;;;;;  (23064 61823 894844 232000))
 ;;; Generated autoloads from counsel-projectile.el
 
 (autoload 'counsel-projectile-find-file "counsel-projectile" "\
@@ -28,12 +28,20 @@ Switch to a project buffer.
 \(fn)" t nil)
 
 (autoload 'counsel-projectile-ag "counsel-projectile" "\
-Ivy version of `projectile-ag'.
+Run an ag search in the project.
+
+OPTIONS, if non-nil, is a string containing additional options to
+be passed to ag. It is read from the minibuffer if the function
+is called with a prefix argument.
 
 \(fn &optional OPTIONS)" t nil)
 
 (autoload 'counsel-projectile-rg "counsel-projectile" "\
-Ivy version of `projectile-rg'.
+Run an rg search in the project.
+
+OPTIONS, if non-nil, is a string containing additional options to
+be passed to rg. It is read from the minibuffer if the function
+is called with a prefix argument.
 
 \(fn &optional OPTIONS)" t nil)
 
@@ -41,11 +49,9 @@ Ivy version of `projectile-rg'.
 Switch to a project we have visited before.
 
 Invokes the command referenced by
-`projectile-switch-project-action' on switch.  With a prefix ARG
-invokes `projectile-commander' instead of
-`projectile-switch-project-action.'
+`projectile-switch-project-action' on switch.
 
-\(fn &optional ARG)" t nil)
+\(fn)" t nil)
 
 (autoload 'counsel-projectile "counsel-projectile" "\
 Use projectile with Ivy instead of ido.
@@ -65,8 +71,6 @@ Turn on counsel-projectile key bindings.
 Turn off counsel-projectile key bindings.
 
 \(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "counsel-projectile" '("counsel-projectile-")))
 
 ;;;***
 
