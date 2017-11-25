81146dc00267dbef1c8aefa5280f53bb399fd2b4
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:35 +0000

    [Adding helm-lastpass version: 20170914.142]

---
+++ elpa/helm-lastpass-20170914.142/helm-lastpass-autoloads.el
@@ -0,0 +1,22 @@
+;;; helm-lastpass-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "helm-lastpass" "helm-lastpass.el" (23064 61387
+;;;;;;  212133 745000))
+;;; Generated autoloads from helm-lastpass.el
+
+(autoload 'helm-lastpass "helm-lastpass" "\
+Helm interface of LastPass.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-lastpass-autoloads.el ends here
