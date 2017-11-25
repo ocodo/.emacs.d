7d1649aad995aed36a824e49d104a258255e2702
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:34:12 +0000

    [Updating esup version: 20170923.1328][removing old version: 20170729.1742]

--- elpa/esup-20170923.1328/esup-pkg.el
+++ elpa/esup-20170923.1328/esup-pkg.el
@@ -1,4 +1,4 @@
-(define-package "esup" "20170729.1742" "the Emacs StartUp Profiler (ESUP)"
+(define-package "esup" "20170923.1328" "the Emacs StartUp Profiler (ESUP)"
   '((cl-lib "0.5")
     (emacs "24"))
   :url "http://github.com/jschaf/esup" :keywords
