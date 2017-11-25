09dd24e70a423561472fa7741f994eca9851158d
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:43 +0000

    [Adding ido-completing-read+ version: 20170820.3]

---
+++ elpa/ido-completing-read+-20170820.3/ido-completing-read+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ido-completing-read+" "20170820.3" "A completing-read-function using ido" '((emacs "24.4") (cl-lib "0.5") (s "0.1") (memoize "1.1")) :commit "e2ea358725f03ae623ae03ed90715efb92a61030" :url "https://github.com/DarwinAwardWinner/ido-completing-read-plus" :keywords '("ido" "completion" "convenience"))
