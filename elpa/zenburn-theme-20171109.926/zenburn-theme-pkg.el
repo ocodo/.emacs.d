3506f652c75f5beb780c6540a858ec5693e63650
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:11:28 +0000

    [Adding zenburn-theme version: 20171109.926]

---
+++ elpa/zenburn-theme-20171109.926/zenburn-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zenburn-theme" "20171109.926" "A low contrast color theme for Emacs." 'nil :commit "613f4c69513c7ffe7cb00a6c477cd5e8e6378d40" :url "http://github.com/bbatsov/zenburn-emacs")
