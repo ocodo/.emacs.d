5811be41d615377eab11f5b66728515924d840c4
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:24:39 +0000

    [Adding ht version: 20171112.1405]

---
+++ elpa/ht-20171112.1405/ht-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ht" "20171112.1405" "The missing hash table library for Emacs" '((dash "2.12.0")) :commit "ce65ce5d535cc86698ad6012f60fb56d7d80d8db" :keywords '("hash table" "hash map" "hash"))
