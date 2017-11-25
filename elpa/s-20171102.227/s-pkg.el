79f4ecec7e148336e5167669e433c74def6d42be
jasonm23 <jasonm23@gmail.com>
2017-11-25 04:25:19 +0000

    [Adding s version: 20171102.227]

---
+++ elpa/s-20171102.227/s-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "s" "20171102.227" "The long lost Emacs string manipulation library." 'nil :commit "5e9a6857d42015c67681616aa3519f599f97b8d8" :keywords '("strings"))
