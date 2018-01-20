03427bf950bbb04eed8dfd19ea524bc5be3e3e97
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:56 +0000

    [Removing mustache version: 20131117.2207]

--- elpa/mustache-20131117.2207/mustache-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; mustache-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("mustache-lex.el" "mustache-parse.el"
-;;;;;;  "mustache-pkg.el" "mustache-render.el" "mustache.el") (21133
-;;;;;;  56774 823988 0))
-
-;;;***
-
-(provide 'mustache-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; mustache-autoloads.el ends here
--- elpa/mustache-20131117.2207/mustache-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "mustache" "20131117.2207" "a mustache templating library in emacs lisp"
-  '((ht "0.9")
-    (s "1.3.0")
-    (dash "1.2.0")))
