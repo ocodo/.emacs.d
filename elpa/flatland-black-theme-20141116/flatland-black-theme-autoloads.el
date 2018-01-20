12fcac990e163f8e126e6d5a0874a7bb13e1a482
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:14 +0000

    [Removing flatland-black-theme version: 20141116]

--- elpa/flatland-black-theme-20141116/flatland-black-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; flatland-black-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "flatland-black-theme" "flatland-black-theme.el"
-;;;;;;  (21608 19133 0 0))
-;;; Generated autoloads from flatland-black-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flatland-black-theme-autoloads.el ends here
--- elpa/flatland-black-theme-20141116/flatland-black-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flatland-black-theme" "20141116" "an Emacs 24 theme based on Flatland Black (tmTheme)" '((emacs "24.0")) :url "https://github.com/emacsfodder/flatland-black-theme")
