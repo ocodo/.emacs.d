28e93c491daea41b16af7f8adbc5eec0f1fc928c
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:53 +0000

    [Removing mbo70s-theme version: 20141122.642]

--- elpa/mbo70s-theme-20141122.642/mbo70s-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mbo70s-theme" "20141122.642" "70s style palette, with similarities to mbo theme" '((emacs "24.0")) :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
