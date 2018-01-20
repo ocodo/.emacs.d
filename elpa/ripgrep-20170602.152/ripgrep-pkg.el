20875319ccb65de5f0486f218453bd39e870191e
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:10 +0000

    [Removing ripgrep version: 20170602.152]

--- elpa/ripgrep-20170602.152/ripgrep-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ripgrep" "20170602.152" "Front-end for ripgrep, a command line search tool" 'nil :commit "5af6a0b2ee8a639cf857724ce4328f1f0955c99e" :url "https://github.com/nlamirault/ripgrep.el" :keywords '("ripgrep" "ack" "pt" "ag" "sift" "grep" "search"))
