d41f8b5bd34cb3be527603bc73eef214f019046c
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:14 +0000

    [Removing smart-compile versions: 20161118.403,20170827.1050]

--- elpa/smart-compile-20161118.403/smart-compile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smart-compile" "20161118.403" "an interface to `compile'" 'nil :keywords '("tools" "unix"))
--- elpa/smart-compile-20170827.1050/smart-compile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smart-compile" "20170827.1050" "an interface to `compile'" 'nil :commit "64258b424e471c749a3399d32aa35b39dfb05222" :keywords '("tools" "unix"))
