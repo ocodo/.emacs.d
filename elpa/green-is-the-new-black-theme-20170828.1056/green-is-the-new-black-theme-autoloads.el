f13fb23065ae9b56ca5b9adf69879b2b257e7f34
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:26 +0000

    [Removing green-is-the-new-black-theme version: 20170828.1056]

--- elpa/green-is-the-new-black-theme-20170828.1056/green-is-the-new-black-theme-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; green-is-the-new-black-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("green-is-the-new-black-theme.el") (22955
-;;;;;;  52825 504395 886000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; green-is-the-new-black-theme-autoloads.el ends here
