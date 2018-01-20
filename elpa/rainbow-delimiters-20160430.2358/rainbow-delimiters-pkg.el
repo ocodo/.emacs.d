6cbcfdba3019e433de2fdbf9dec1bf3b8a634643
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:08 +0000

    [Removing rainbow-delimiters version: 20160430.2358]

--- elpa/rainbow-delimiters-20160430.2358/rainbow-delimiters-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rainbow-delimiters" "20160430.2358" "Highlight brackets according to their depth" 'nil :url "https://github.com/Fanael/rainbow-delimiters" :keywords '("faces" "convenience" "lisp" "tools"))
