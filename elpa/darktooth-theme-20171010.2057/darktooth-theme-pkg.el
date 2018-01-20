c3a8ae98a8905cf5b2c83fc7c44db7e617594d43
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:03:57 +0000

    [Updating darktooth-theme version: 20171206.639][removing old version: 20171010.2057]

--- elpa/darktooth-theme-20171010.2057/darktooth-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "darktooth-theme" "20171010.2057" "From the darkness... it watches" '((autothemer "0.2")) :commit "e7c13abeeb18f50658482c7df32701ae4ec375a0" :url "http://github.com/emacsfodder/emacs-theme-darktooth")
