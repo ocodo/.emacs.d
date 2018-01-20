7b9d70bc6373493bff41a0db381bc765baf1d84a
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:48 +0000

    [Removing lavender-theme version: 20141116]

--- elpa/lavender-theme-20141116/lavender-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; lavender-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "lavender-theme" "lavender-theme.el" (21608
-;;;;;;  19135 0 0))
-;;; Generated autoloads from lavender-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; lavender-theme-autoloads.el ends here
--- elpa/lavender-theme-20141116/lavender-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "lavender-theme" "20141116" "an Emacs 24 theme based on Lavender (tmTheme)" '((emacs "24.0")) :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
