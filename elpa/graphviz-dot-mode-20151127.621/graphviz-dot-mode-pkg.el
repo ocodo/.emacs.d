99f248cdc04af3684ca4e6631965ea1678cae722
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:25 +0000

    [Removing graphviz-dot-mode version: 20151127.621]

--- elpa/graphviz-dot-mode-20151127.621/graphviz-dot-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "graphviz-dot-mode" "20151127.621" "Mode for the dot-language used by graphviz (att)." 'nil :url "http://ppareit.github.com/graphviz-dot-mode/" :keywords '("mode" "dot" "dot-language" "dotlanguage" "graphviz" "graphs" "att"))
