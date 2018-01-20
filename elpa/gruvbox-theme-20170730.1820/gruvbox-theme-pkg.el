aab711971d1af1959dfa3671ac31c16d3cab7053
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:27 +0000

    [Removing gruvbox-theme versions: 20170730.1820,20170828.144]

--- elpa/gruvbox-theme-20170730.1820/gruvbox-theme-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "gruvbox-theme" "20170730.1820" "A retro-groove colour theme for Emacs"
-  '((autothemer "0.2"))
-  :url "http://github.com/Greduan/emacs-theme-gruvbox")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
