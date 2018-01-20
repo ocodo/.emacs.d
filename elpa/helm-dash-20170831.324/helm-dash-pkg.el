36d4bb3f5e54cf219af9b82efe1aa3b8aea0bba7
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:31 +0000

    [Removing helm-dash versions: 20170531.135,20170831.324]

--- elpa/helm-dash-20170831.324/helm-dash-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-dash" "20170831.324" "Offline documentation browser for +150 APIs using Dash docsets." '((helm "1.9.2") (cl-lib "0.5")) :commit "a4e8da47d3a82b316d2b3668c95215746c6c375b" :url "http://github.com/areina/helm-dash" :keywords '("docs"))
