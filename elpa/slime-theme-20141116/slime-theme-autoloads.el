a48951928b69cf2c11c80cfb70e516e41a6e3cfe
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:14 +0000

    [Removing slime-theme version: 20141116]

--- elpa/slime-theme-20141116/slime-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; slime-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "slime-theme" "slime-theme.el" (21608 19143
-;;;;;;  0 0))
-;;; Generated autoloads from slime-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; slime-theme-autoloads.el ends here
--- elpa/slime-theme-20141116/slime-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "slime-theme" "20141116" "an Emacs 24 theme based on Slime (tmTheme)" '((emacs "24.0")) :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
