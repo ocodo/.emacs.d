0b002b687cf36ddbafbac089ad251c37f78c3235
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:04 +0000

    [Removing el-mock version: 20170501.1123]

--- elpa/el-mock-20170501.1123/el-mock-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; el-mock-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("el-mock.el") (22873 40254 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; el-mock-autoloads.el ends here
--- elpa/el-mock-20170501.1123/el-mock-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "el-mock" "20170501.1123" "Tiny Mock and Stub framework in Emacs Lisp" 'nil :commit "e3017494230cb90c5f542acc07c539be1497123b" :url "http://github.com/rejeep/el-mock.el" :keywords '("lisp" "testing" "unittest"))
