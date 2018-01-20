1102cd7ad7869fb360be802f39dbdb68961445ef
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:32 +0000

    [Updating helm-lastpass version: 20180114.937][removing old versions: 20170808.511,20170914.142]

--- elpa/helm-lastpass-20170914.142/helm-lastpass-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-lastpass" "20170914.142" "Helm interface of LastPass" '((emacs "24.1") (helm-core "2.8.1") (csv "2.1")) :commit "ae5d1252d60450082a5c26af3ad2be43994201ec" :url "https://github.com/xuchunyang/helm-lastpass" :keywords '("lastpass"))
---
+++ elpa/helm-lastpass-20180114.937/helm-lastpass.el
@@ -0,0 +1,185 @@
+;;; helm-lastpass.el --- Helm interface of LastPass  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017, 2018  Xu Chunyang
+
+;; Author: Xu Chunyang <mail@xuchunyang.me>
+;; Homepage: https://github.com/xuchunyang/helm-lastpass
+;; Package-Requires: ((emacs "25.1") (helm "2.0") (csv "2.1"))
+;; Package-Version: 20180114.937
+;; Version: 0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Helm interface of LastPass.
+
+;;; Code:
+
+(require 'csv)
+(require 'helm)
+(require 'auth-source)
+(require 'cl-lib)
+(require 'subr-x)
+
+(defgroup helm-lastpass nil
+  "Helm interface of LastPass."
+  :group 'tools)
+
+(defcustom helm-lastpass-cli "lpass"
+  "The program name of the LastPass command line tool."
+  :type 'string
+  :group 'helm-lastpass)
+
+(defcustom helm-lastpass-actions
+  '(("Copy password" . helm-lastpass-copy-password)
+    ("Copy username" . helm-lastpass-copy-username)
+    ("Copy URL"      . helm-lastpass-copy-url)
+    ("Browse URL"    . helm-lastpass-browse-url))
+  "Actions for `helm-lastpass'."
+  :group 'helm-lastpass
+  :type '(alist :key-type string :value-type function))
+
+(defun helm-lastpass-copy-password (al)
+  (let ((password (alist-get 'password al)))
+    (if password
+        (progn
+          (kill-new password)
+          (message "Copied: %s" password)))
+    (user-error "No password for this entry")))
+
+(defun helm-lastpass-copy-username (al)
+  (let ((username (alist-get 'username al)))
+    (if username
+        (progn
+          (kill-new username)
+          (message "Copied: %s" username))
+      (user-error "No username for this entry"))))
+
+(defun helm-lastpass-copy-url (al)
+  (let ((url (alist-get 'url al)))
+    (if url
+        (progn
+          (kill-new url)
+          (message "Copied: %s" url))
+      (user-error "No URL for this entry"))))
+
+(defun helm-lastpass-browse-url (al)
+  (let ((url (alist-get 'url al)))
+    (if url
+        (browse-url url)
+      (user-error "No URL for this entry"))))
+
+(defun helm-lastpass-cli ()
+  (or (executable-find helm-lastpass-cli)
+      (error "Error: `lpass' is not found, please install it first")))
+
+(defun helm-lastpass-logged-in-p ()
+  (zerop (call-process (helm-lastpass-cli) nil nil nil "status")))
+
+(defun helm-lastpass-login (&optional email password)
+  (let ((plist (car (auth-source-search :max 1 :host "lastpass.com"))))
+    (cl-flet ((value (k) (let ((v (plist-get plist k)))
+                           (if (functionp v) (funcall v) v))))
+      (setq email (value :user)
+            password (value :secret))))
+  (let* ((email
+          (or email (read-string "Email: " user-mail-address)))
+         (password
+          (or password (read-passwd "Password: ")))
+         (command
+          ;; XXX Is there any better solution?
+          (format "echo -n '%s' | LPASS_DISABLE_PINENTRY=1 %s login --color=never %s"
+                  password
+                  (shell-quote-argument (helm-lastpass-cli))
+                  email)))
+    (with-temp-buffer
+      (message "helm-lastpass: Logging as %s..." email)
+      (if (zerop (call-process-shell-command command nil t nil))
+          (message "helm-lastpass: Logging as %s...done" email)
+        (error "%s" (buffer-string))))))
+
+(defun helm-lastpass-export (&optional sync)
+  "Run lpass export subcommand, SYNC is for --sync.
+Return a list of alist which contain all account information."
+  (let* ((sync (pcase sync
+                 (`nil   "--sync=auto")
+                 (`auto "--sync=auto")
+                 (`now  "--sync=now")
+                 (`no   "--sync=no")
+                 (_     (error "Invalid argument '%s'" sync))))
+         (fields (concat
+                  "--fields="
+                  (mapconcat #'identity
+                             '("id"
+                               "url"
+                               "username"
+                               "password"
+                               "extra"
+                               "name"
+                               "fav"
+                               "id"
+                               "grouping"
+                               "group"
+                               "fullname"
+                               "last_touch"
+                               "last_modified_gmt"
+                               "attachpresent")
+                             ",")))
+         (args (list "export" "--color=never" sync fields)))
+    (with-temp-buffer
+      (message "helm-lastpass: Retrieving data...")
+      (if (zerop (apply #'call-process (helm-lastpass-cli) nil t nil args))
+          (progn
+            (message "helm-lastpass: Retrieving data...done")
+            (let ((list-of-alist (csv-parse-buffer t)))
+              (mapcar
+               (lambda (alist)
+                 (mapcar
+                  (pcase-lambda (`(,field . ,value))
+                    ;; Strip 
+                    (when (and (string-suffix-p "" field)
+                               (string-suffix-p "" value))
+                      (setq field (substring field 0 -1)
+                            value (substring value 0 -1)))
+                    (when (string= "" value)
+                      (setq value nil))
+                    (cons (intern field) value))
+                  alist))
+               list-of-alist)))
+        (error "%s" (buffer-string))))))
+
+(defun helm-lastpass-candidates ()
+  (mapcar
+   (lambda (alist)
+     (cons (alist-get 'fullname alist) alist))
+   (helm-lastpass-export)))
+
+(defvar helm-lastpass-source
+  (helm-build-sync-source "LastPass"
+    :candidates #'helm-lastpass-candidates
+    :action helm-lastpass-actions)
+  "Source for `helm-lastpass'.")
+
+;;;###autoload
+(defun helm-lastpass ()
+  "Helm interface of LastPass."
+  (interactive)
+  (unless (helm-lastpass-logged-in-p)
+    (helm-lastpass-login))
+  (helm :sources helm-lastpass-source
+        :buffer "*helm LastPass*"))
+
+(provide 'helm-lastpass)
+;;; helm-lastpass.el ends here
