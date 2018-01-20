4eb56731811e259aa86461c533947f6408cde170
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:09 +0000

    [Removing evil-lion version: 20170801.2232]

--- elpa/evil-lion-20170801.2232/evil-lion-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "evil-lion" "20170801.2232" "Evil align operator, port of vim-lion" '((emacs "24.3") (evil "1.0.0")) :commit "5ac2cf2f2e22475038dffbba65ddcbf3d5933067" :url "http://github.com/edkolev/evil-lion" :keywords '("emulations" "evil" "vim"))
