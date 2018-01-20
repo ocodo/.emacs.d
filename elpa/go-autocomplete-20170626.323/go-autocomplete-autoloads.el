c5f686a8bdfcaa17505f00d0f042ed66470fd6aa
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:21 +0000

    [Removing go-autocomplete version: 20170626.323]

--- elpa/go-autocomplete-20170626.323/go-autocomplete-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; go-autocomplete-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("go-autocomplete.el") (22873 40314 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; go-autocomplete-autoloads.el ends here
--- elpa/go-autocomplete-20170626.323/go-autocomplete-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-autocomplete" "20170626.323" "auto-complete-mode backend for go-mode" '((auto-complete "1.4.0")) :commit "beae6bdcc6fc300059038961b7a3e977e0fb7c61" :keywords '("languages"))
