326dcd712f460b74a3cc1340f21b7a466ce76783
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:10 +0000

    [Removing robots-txt-mode version: 20160820.202]

--- elpa/robots-txt-mode-20160820.202/robots-txt-mode-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; robots-txt-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "robots-txt-mode" "robots-txt-mode.el" (22459
-;;;;;;  4130 338318 431000))
-;;; Generated autoloads from robots-txt-mode.el
-
-(autoload 'robots-txt-mode "robots-txt-mode" "\
-Major mode for editing `robots.txt'
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("/robots\\.txt\\'" . robots-txt-mode))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; robots-txt-mode-autoloads.el ends here
--- elpa/robots-txt-mode-20160820.202/robots-txt-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "robots-txt-mode" "20160820.202" "Major mode for editing robots.txt" 'nil :keywords '("web"))
