9d57caef3b0de07a90c626752a45611d76ed5e3a
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:05 +0000

    [Removing plain-theme version: 20170625.1100]

--- elpa/plain-theme-20170625.1100/plain-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "plain-theme" "20170625.1100" "Plain theme without syntax highlighting" 'nil :commit "7c376f5bf9d653bf12e414176284736cbdd19108" :url "https://github.com/yegortimoshenko/plain-theme")
