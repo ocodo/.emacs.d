cca96c9e8a6f837377b6705f09cc16b7a67a131f
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:15 +0000

    [Removing sml-modeline version: 20120110.1240]

--- elpa/sml-modeline-20120110.1240/sml-modeline-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "sml-modeline" "20120110.1240" "Show position in a scrollbar like way in mode-line" 'nil :url "http://bazaar.launchpad.net/~nxhtml/nxhtml/main/annotate/head%3A/util/sml-modeline.el")
