c626ae95269b4f3f8bbd66a2bc83bbf97534afc5
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:28 +0000

    [Removing harvest version: 20160811.1810]

--- elpa/harvest-20160811.1810/harvest-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "harvest" "20160811.1810" "Harvest integration" '((swiper "0.7.0") (hydra "0.13.0") (s "1.11.0")) :url "https://github.com/kostajh/harvest.el" :keywords '("harvest"))
