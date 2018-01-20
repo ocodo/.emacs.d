cdd4ace032a28d0a3891adbc9dbea3f47f972afc
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:31 +0000

    [Removing zone-nyan version: 20161014.154]

--- elpa/zone-nyan-20161014.154/zone-nyan-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zone-nyan" "20161014.154" "Zone out with nyan cat" '((esxml "0.3.1")) :url "https://github.com/wasamasa/zone-nyan" :keywords '("zone"))
