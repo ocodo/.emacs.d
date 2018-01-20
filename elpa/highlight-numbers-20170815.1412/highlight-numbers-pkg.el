4bd59a92bc9bed41a9bb9d3e0d091246a891d075
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:35 +0000

    [Removing highlight-numbers versions: 20160717.1228,20170815.1412]

--- elpa/highlight-numbers-20170815.1412/highlight-numbers-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "highlight-numbers" "20170815.1412" "Highlight numbers in source code" '((emacs "24") (parent-mode "2.0")) :commit "444614973a4af7c9ad2431bdaa2005d95db41ee7" :url "https://github.com/Fanael/highlight-numbers")
