9f266819323d4c880817762755f869ef92b6effc
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:41 +0000

    [Removing inflections version: 20161126.1727]

--- elpa/inflections-20161126.1727/inflections-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "inflections" "20161126.1727" "convert english words between singular and plural" '((cl-lib "0.5") (emacs "24")) :url "https://github.com/eschulte/jump.el" :keywords '("languages" "tools" "wp"))
