3ba218ee1185b13a4cc6f7c4c1284da7fe3758c2
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:21 +0000

    [Updating tao-theme version: 20171221.1001][removing old version: 20170718.2306]

---
+++ elpa/tao-theme-20171221.1001/tao-theme-pkg.el
@@ -0,0 +1,4 @@
+(define-package "tao-theme" "20171221.1001" "This package provides two parametrized uncoloured color themes for Emacs: tao-yin and tao-yang." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
