aba349acd012ed1a08c13bfcebfa38d808def625
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:21 +0000

    [Removing tern version: 20161222.850]

--- elpa/tern-20161222.850/tern-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; tern-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "tern" "tern.el" (22624 31673 748041 775000))
-;;; Generated autoloads from tern.el
-
-(autoload 'tern-use-server "tern" "\
-
-
-\(fn PORT SERVER)" t nil)
-
-(autoload 'tern-mode "tern" "\
-Minor mode binding to the Tern JavaScript analyzer
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; tern-autoloads.el ends here
--- elpa/tern-20161222.850/tern-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "tern" "20161222.850" "Tern-powered JavaScript integration" '((json "1.2") (cl-lib "0.5") (emacs "24")) :url "http://ternjs.net/")
