ce956d90f6f6c108c679a21e27806b1b7cb28c0a
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:47 +0000

    [Removing kotlin-mode version: 20170522.1524]

--- elpa/kotlin-mode-20170522.1524/kotlin-mode-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; kotlin-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "kotlin-mode" "kotlin-mode.el" (22873 40399
-;;;;;;  0 0))
-;;; Generated autoloads from kotlin-mode.el
-
-(autoload 'kotlin-mode "kotlin-mode" "\
-Major mode for editing Kotlin.
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.kt\\'" . kotlin-mode) t)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; kotlin-mode-autoloads.el ends here
--- elpa/kotlin-mode-20170522.1524/kotlin-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "kotlin-mode" "20170522.1524" "Major mode for kotlin" '((emacs "24.3")) :commit "e5ee4c4bd25a61e0f5067ca8939d1a3185909471" :keywords '("languages"))
