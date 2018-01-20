cb0e590a065c899184d1f69fe824c9f70e9fc0ed
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:53 +0000

    [Removing mediawiki version: 20170418.1035]

--- elpa/mediawiki-20170418.1035/mediawiki-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mediawiki" "20170418.1035" "mediawiki frontend" 'nil :url "https://github.com/hexmode/mediawiki-el" :keywords '("mediawiki" "wikipedia" "network" "wiki"))
