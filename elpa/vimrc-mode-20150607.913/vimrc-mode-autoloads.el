f6efc496c706e1fb479d9a606d03737d7467031e
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:25 +0000

    [Removing vimrc-mode version: 20150607.913]

--- elpa/vimrc-mode-20150607.913/vimrc-mode-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; vimrc-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "vimrc-mode" "vimrc-mode.el" (21879 1171 115401
-;;;;;;  0))
-;;; Generated autoloads from vimrc-mode.el
- (add-to-list 'auto-mode-alist '("\\.vim\\'" . vimrc-mode))
- (add-to-list 'auto-mode-alist '("[._]?g?vimrc\\'" . vimrc-mode))
- (add-to-list 'auto-mode-alist '("\\.exrc\\'" . vimrc-mode))
-
-(autoload 'vimrc-mode "vimrc-mode" "\
-Major mode for editing `vimrc', `xxx.vim' and `.exrc' configuration files.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; vimrc-mode-autoloads.el ends here
--- elpa/vimrc-mode-20150607.913/vimrc-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "vimrc-mode" "20150607.913" "Major mode for vimrc files" 'nil :url "https://github.com/mcandre/vimrc-mode" :keywords '("languages" "vim"))
