f819d70fc2fbd72d76cd72cf07844bccd9759499
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:25 +0000

    [Updating use-package version: 20180108.1754][removing old versions: 20170710.1234,20170812.2256,20171121.1430]

--- elpa/use-package-20170812.2256/use-package-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "use-package" "20170812.2256" "A use-package declaration for simplifying your .emacs" '((bind-key "1.0") (diminish "0.44")) :commit "360df30683a711c443f87e495ba14cdd125a505d" :url "https://github.com/jwiegley/use-package" :keywords '("dotemacs" "startup" "speed" "config" "package"))
---
+++ elpa/use-package-20180108.1754/use-package-pkg.el
@@ -0,0 +1,8 @@
+(define-package "use-package" "20180108.1754" "A configuration macro for simplifying your .emacs"
+  '((emacs "24.3")
+    (bind-key "2.4"))
+  :url "https://github.com/jwiegley/use-package" :keywords
+  '("dotemacs" "startup" "speed" "config" "package"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
