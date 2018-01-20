a714621f504e79c37a7c6b9a262d54396891ad19
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:19 +0000

    [Removing super-save version: 20160426.729]

--- elpa/super-save-20160426.729/super-save-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "super-save" "20160426.729" "Auto-save buffers, based on your activity." '((emacs "24.4")) :url "https://github.com/bbatsov/super-save" :keywords '("convenience"))
