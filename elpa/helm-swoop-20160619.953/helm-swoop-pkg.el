b4eccb4ed15a66e638e6fe26db3c6d1d8e539f14
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:34 +0000

    [Removing helm-swoop version: 20160619.953]

--- elpa/helm-swoop-20160619.953/helm-swoop-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-swoop" "20160619.953" "Efficiently hopping squeezed lines powered by helm interface" '((helm "1.0") (emacs "24.3")) :url "https://github.com/ShingoFukuyama/helm-swoop" :keywords '("helm" "swoop" "inner" "buffer" "search"))
