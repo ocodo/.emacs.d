a8e975e91761c1955ef2a5ce2756e1e1aca5a090
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:17 +0000

    [Removing splitter version: 20130705.50]

--- elpa/splitter-20130705.50/splitter-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; splitter-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("splitter-pkg.el" "splitter.el") (20956
-;;;;;;  1409 671033 0))
-
-;;;***
-
-(provide 'splitter-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; splitter-autoloads.el ends here
