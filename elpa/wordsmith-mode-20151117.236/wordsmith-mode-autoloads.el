f7a53727769f8ee508aa905eea555dde08f33023
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:30 +0000

    [Removing wordsmith-mode version: 20151117.236]

--- elpa/wordsmith-mode-20151117.236/wordsmith-mode-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; wordsmith-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "wordsmith-mode" "wordsmith-mode.el" (22095
-;;;;;;  53437 989777 403000))
-;;; Generated autoloads from wordsmith-mode.el
-
-(autoload 'wordsmith-mode "wordsmith-mode" "\
-Toggle wordsmith mode.
-Interactively with no argument, this command toggles the mode.
-to show buffer size and position in mode-line.  You can customize
-this minor mode, see option `wordsmith-mode'.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; wordsmith-mode-autoloads.el ends here
--- elpa/wordsmith-mode-20151117.236/wordsmith-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "wordsmith-mode" "20151117.236" "Syntax analysis and NLP text-processing in Emacs (OSX-only)" 'nil)
