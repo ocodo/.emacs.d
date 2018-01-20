3cb1157a8d1847b7830006a224c84a5c11325aee
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:03:48 +0000

    [Updating caml version: 20171209.1232][removing old version: 20171120.824]

--- elpa/caml-20171120.824/caml-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "caml" "20171120.824" "OCaml code editing commands for Emacs" 'nil :url "https://github.com/ocaml/ocaml/" :keywords
-  '("ocaml"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/caml-20171209.1232/caml-autoloads.el
+++ elpa/caml-20171209.1232/caml-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "camldebug" "camldebug.el" (23064 61889 458683
-;;;;;;  737000))
+;;;### (autoloads nil "camldebug" "camldebug.el" (23138 48893 798554
+;;;;;;  938000))
 ;;; Generated autoloads from camldebug.el
 
 (defvar camldebug-command-name "ocamldebug" "\
@@ -23,7 +23,7 @@ the camldebug commands `cd DIR' and `directory'.
 ;;;### (autoloads nil nil ("caml-compat.el" "caml-emacs.el" "caml-font-old.el"
 ;;;;;;  "caml-font.el" "caml-help.el" "caml-hilit.el" "caml-pkg.el"
 ;;;;;;  "caml-types.el" "caml-xemacs.el" "caml.el" "inf-caml.el")
-;;;;;;  (23064 61889 474683 699000))
+;;;;;;  (23138 48893 806554 946000))
 
 ;;;***
 
