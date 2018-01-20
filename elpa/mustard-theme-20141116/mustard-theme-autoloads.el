b38e8f1dd60fc678e8d204a2645bc643a32b3a81
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:57 +0000

    [Removing mustard-theme version: 20141116]

--- elpa/mustard-theme-20141116/mustard-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; mustard-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "mustard-theme" "mustard-theme.el" (21608 19139
-;;;;;;  0 0))
-;;; Generated autoloads from mustard-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; mustard-theme-autoloads.el ends here
--- elpa/mustard-theme-20141116/mustard-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "mustard-theme" "20141116" "an Emacs 24 theme based on Mustard (tmTheme)" '((emacs "24.0")) :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
