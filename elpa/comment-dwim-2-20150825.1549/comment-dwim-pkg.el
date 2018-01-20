d8a27a47caa898a91266d94bd0ee6de91be405f7
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:03:52 +0000

    [Removing comment-dwim-2 version: 20150825.1549]

--- elpa/comment-dwim-2-20150825.1549/comment-dwim-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "comment-dwim-2" "20150825.1549" "An all-in-one comment command to rule them all" 'nil :stars '(#("37" 0 2 (font-lock-face paradox-star-face))) :url "https://github.com/remyferre/comment-dwim-2" :keywords '("convenience"))
