d74de24aef6970865f9de1f4d143279a1b780d1c
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:42 +0000

    [Removing insert-shebang version: 20160626.409]

--- elpa/insert-shebang-20160626.409/insert-shebang-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "insert-shebang" "20160626.409" "Insert shebang line automatically." 'nil :url "http://github.com/psachin/insert-shebang" :keywords '("shebang" "tool" "convenience"))
