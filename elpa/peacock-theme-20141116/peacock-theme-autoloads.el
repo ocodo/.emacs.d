e5c839013c52cd79dae96963745913cc8a381b5f
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:02 +0000

    [Removing peacock-theme version: 20141116]

--- elpa/peacock-theme-20141116/peacock-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; peacock-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "peacock-theme" "peacock-theme.el" (21608 19141
-;;;;;;  0 0))
-;;; Generated autoloads from peacock-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; peacock-theme-autoloads.el ends here
--- elpa/peacock-theme-20141116/peacock-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "peacock-theme" "20141116" "an Emacs 24 theme based on Peacock (tmTheme)" '((emacs "24.0")) :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
