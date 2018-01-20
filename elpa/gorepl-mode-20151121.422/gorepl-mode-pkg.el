8a2d46422301a6f16683706327b87a3eb3ecccbb
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:24 +0000

    [Removing gorepl-mode version: 20151121.422]

--- elpa/gorepl-mode-20151121.422/gorepl-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gorepl-mode" "20151121.422" "Go REPL Interactive Development in top of Gore" '((emacs "24")) :url "http://www.github.com/manute/gorepl-mode" :keywords '("languages" "go" "golang" "gorepl"))
