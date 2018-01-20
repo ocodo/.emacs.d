578d454c713ddc4faf111e46ba43f12aefaa430b
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:20 +0000

    [Adding ghub version: 20180117.1249]

---
+++ elpa/ghub-20180117.1249/ghub-pkg.el
@@ -0,0 +1,8 @@
+(define-package "ghub" "20180117.1249" "minuscule client library for the Github API"
+  '((emacs "24.4")
+    (let-alist "1.0.5"))
+  :url "https://github.com/magit/ghub" :keywords
+  '("tools"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
