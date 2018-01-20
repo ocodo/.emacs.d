5be521284ca2e635c3221574ef24c4b169d863ab
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:23 +0000

    [Removing google-this version: 20160710.1720]

--- elpa/google-this-20160710.1720/google-this-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "google-this" "20160710.1720" "A set of functions and bindings to google under point." '((emacs "24.1")) :url "http://github.com/Malabarba/emacs-google-this" :keywords '("convenience" "hypermedia"))
