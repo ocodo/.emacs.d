bf9bc8c673c1b297dc4885c9e80068e5d947693c
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:39 +0000

    [Removing ido-ubiquitous versions: 20170801.46,20170819.1045]

--- elpa/ido-ubiquitous-20170801.46/ido-ubiquitous-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; ido-ubiquitous-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "ido-ubiquitous" "ido-ubiquitous.el" (0 0 0
-;;;;;;  0))
-;;; Generated autoloads from ido-ubiquitous.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ido-ubiquitous" '("ido-ubiquitous-version")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ido-ubiquitous-autoloads.el ends here
--- elpa/ido-ubiquitous-20170819.1045/ido-ubiquitous-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; ido-ubiquitous-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("ido-ubiquitous.el") (22955 53398 849587
-;;;;;;  183000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ido-ubiquitous-autoloads.el ends here
