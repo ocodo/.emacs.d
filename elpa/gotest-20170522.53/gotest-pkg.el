82a385f6d7924e6f6f0a73b330ab9aba947e74ea
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:24 +0000

    [Removing gotest version: 20170522.53]

--- elpa/gotest-20170522.53/gotest-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gotest" "20170522.53" "Launch GO unit tests" '((emacs "24.3") (s "1.11.0") (f "0.19.0") (go-mode "1.4.0")) :commit "c15bdcb78a46167e7a3fbaf5f71cbeddf2f13b78" :url "https://github.com/nlamirault/gotest.el" :keywords '("languages" "go" "tests"))
