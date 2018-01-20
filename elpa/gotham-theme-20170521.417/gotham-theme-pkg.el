94b4604ccaf315a9a20ea3163fed272570ecea3e
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:24 +0000

    [Removing gotham-theme versions: 20170521.417,20170822.1030]

--- elpa/gotham-theme-20170521.417/gotham-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gotham-theme" "20170521.417" "A very dark Emacs color theme." 'nil :commit "df428b477eb84f2ccd791c4310587cfd72644692" :url "https://github.com/wasamasa/gotham-theme")
--- elpa/gotham-theme-20170822.1030/gotham-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; gotham-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "gotham-theme" "gotham-theme.el" (22955 53508
-;;;;;;  9685 557000))
-;;; Generated autoloads from gotham-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; gotham-theme-autoloads.el ends here
