1c04017a22272820a2f15a1babac54fe54824a93
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:04:33 +0000

    [Removing helm-make version: 20170430.1053]

--- elpa/helm-make-20170430.1053/helm-make-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-make" "20170430.1053" "Select a Makefile target with helm" '((helm "1.5.3") (projectile "0.11.0")) :commit "8bb531fa77fe1259cccdc2fa7629d65989684f16" :url "https://github.com/abo-abo/helm-make" :keywords '("makefile"))
