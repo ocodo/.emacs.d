731903a0462f75fc55fc0619a236df9c64169d3a
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:03:55 +0000

    [Updating counsel-projectile version: 20180105.632][removing old version: 20171107.1456]

--- elpa/counsel-projectile-20171107.1456/counsel-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel-projectile" "20171107.1456" "Ivy integration for Projectile" '((counsel "0.8.0") (projectile "0.14.0")) :commit "5ec2118cbb3a6c047638ce305e50b1cd11ef3c03" :url "https://github.com/ericdanan/counsel-projectile" :keywords '("project" "convenience"))
---
+++ elpa/counsel-projectile-20180105.632/counsel-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-projectile" "20180105.632" "Ivy integration for Projectile" '((counsel "0.10.0") (projectile "0.14.0")) :commit "4d78ae8c90e8ebb903b8e70442989a69e46ff069" :url "https://github.com/ericdanan/counsel-projectile" :keywords '("project" "convenience"))
