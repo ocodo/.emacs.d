8dd4226c70c5a707561a443a9d7057d575364687
jasonm23 <jasonm23@gmail.com>
2018-01-20 04:05:30 +0000

    [Removing writegood-mode version: 20160802.1405]

--- elpa/writegood-mode-20160802.1405/writegood-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "writegood-mode" "20160802.1405" "Polish up poor writing on the fly" 'nil :url "http://github.com/bnbeckwith/writegood-mode" :keywords '("writing" "weasel-words" "grammar"))
