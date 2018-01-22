9208511be3d149b668788e19c2fa2c212f34951a
jasonm23 <jasonm23@gmail.com>
2018-01-22 14:28:56 +0800

    Fix ansi-term config

--- _configs/use-term.el
+++ _configs/use-term.el
@@ -1,13 +1,13 @@
-;;; use-aani-term --- initialize ansi-term
+;;; use-term --- initialize ansi-term
 ;;; Commentary:
 ;;; Code:
 (require 'use-package)
 
-(use-package ansi-term
+(use-package term
   :init
   (progn
     (bind-key
      "C-x y" 'term-paste)))
 
-(provide 'use-aani-term)
+(provide 'use-term)
 ;;; use-aani-term ends here
