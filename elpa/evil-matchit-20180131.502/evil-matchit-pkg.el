7739571e09190d6dd1d4357ce82cb6e9f47d0886
jasonm23 <jasonm23@gmail.com>
2018-02-10 02:57:55 +0000

    [Updating evil-matchit version: 20180131.502][removing old version: 20171127.245]

--- elpa/evil-matchit-20180131.502/evil-matchit-pkg.el
+++ elpa/evil-matchit-20180131.502/evil-matchit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "evil-matchit" "20171127.245" "Vim matchit ported to Evil"
+(define-package "evil-matchit" "20180131.502" "Vim matchit ported to Evil"
   '((evil "1.0.7"))
   :url "http://github.com/redguardtoo/evil-matchit" :keywords
   '("matchit" "vim" "evil"))
