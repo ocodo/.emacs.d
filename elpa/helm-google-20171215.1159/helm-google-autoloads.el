005927769ba90a4db03a1f685906d2c764473338
jasonm23 <jasonm23@gmail.com>
2018-02-10 02:58:00 +0000

    [Updating helm-google version: 20180208.120][removing old version: 20171215.1159]

--- elpa/helm-google-20171215.1159/helm-google-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; helm-google-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "helm-google" "helm-google.el" (23138 48444
-;;;;;;  614013 727000))
-;;; Generated autoloads from helm-google.el
-
-(autoload 'helm-google "helm-google" "\
-Preconfigured `helm' : Google search.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-google-autoloads.el ends here
---
+++ elpa/helm-google-20180208.120/helm-google-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-google" "20180208.120" "Emacs Helm Interface for quick Google searches" '((helm "0")) :commit "740d10c2945c8b757eebe42c8f415e3010a05640" :url "https://github.com/steckerhalter/helm-google" :keywords '("helm" "google" "search" "browse" "searx"))
