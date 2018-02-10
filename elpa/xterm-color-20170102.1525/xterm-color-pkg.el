862ee6f9ae91f5c578043aeb82542dd21a029035
jasonm23 <jasonm23@gmail.com>
2018-02-10 02:58:15 +0000

    [Updating xterm-color version: 20180202.1518][removing old version: 20170102.1525]

--- elpa/xterm-color-20170102.1525/xterm-color-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "xterm-color" "20170102.1525" "ANSI & XTERM 256 color support" '((cl-lib "0.5")) :url "https://github.com/atomontage/xterm-color" :keywords '("faces"))
---
+++ elpa/xterm-color-20180202.1518/xterm-color-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "xterm-color" "20180202.1518" "ANSI & XTERM 256 color support" '((cl-lib "0.5")) :commit "42374a98f1039e105cad9f16ce585dffc96a3f1c" :url "https://github.com/atomontage/xterm-color" :keywords '("faces"))
