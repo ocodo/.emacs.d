4a1a69db5c5bd311f52030da84ff6d2fa2fc1d56
Jasonm23 <jasonm23@gmail.com>
2018-05-06 16:39:29 +0800

    [Updating package-build version: 20180414.938][removing old version: 20180205.1521]

--- elpa/package-build-20180414.938/package-build-autoloads.el
+++ elpa/package-build-20180414.938/package-build-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "package-build" "package-build.el" (23166 23236
-;;;;;;  184611 306000))
+;;;### (autoloads nil "package-build" "package-build.el" (23278 48104
+;;;;;;  0 0))
 ;;; Generated autoloads from package-build.el
 
 (autoload 'package-build-archive "package-build" "\
@@ -13,23 +13,11 @@ Build a package archive for the package named NAME.
 \(fn NAME)" t nil)
 
 (autoload 'package-build-package "package-build" "\
-Create version VERSION of the package named NAME.
+Create version VERSION of the package specified by RCP.
+Return the archive entry for the package and store the package
+in `package-build-archive-dir'.
 
-The information in FILE-SPECS is used to gather files from
-SOURCE-DIR.
-
-The resulting package will be stored as a .el or .tar file in
-TARGET-DIR, depending on whether there are multiple files.
-
-Argument FILE-SPECS is a list of specs for source files, which
-should be relative to SOURCE-DIR.  The specs can be wildcards,
-and optionally specify different target paths.  They extended
-syntax is currently only documented in the MELPA README.  You can
-simply pass `package-build-default-files-spec' in most cases.
-
-Returns the archive entry for the package.
-
-\(fn NAME VERSION FILE-SPECS SOURCE-DIR TARGET-DIR)" nil nil)
+\(fn RCP VERSION)" nil nil)
 
 (autoload 'package-build-all "package-build" "\
 Build all packages in the `package-build-recipe-alist'.
@@ -39,7 +27,7 @@ Build all packages in the `package-build-recipe-alist'.
 ;;;***
 
 ;;;### (autoloads nil "package-recipe-mode" "package-recipe-mode.el"
-;;;;;;  (23166 23236 188611 301000))
+;;;;;;  (23278 48104 0 0))
 ;;; Generated autoloads from package-recipe-mode.el
 
 (autoload 'package-build-create-recipe "package-recipe-mode" "\
@@ -54,8 +42,8 @@ Build archive for the recipe defined in the current buffer.
 
 ;;;***
 
-;;;### (autoloads nil nil ("package-build-badges.el" "package-build-pkg.el")
-;;;;;;  (23166 23236 184611 306000))
+;;;### (autoloads nil nil ("package-build-badges.el" "package-build-pkg.el"
+;;;;;;  "package-recipe.el") (23278 48104 0 0))
 
 ;;;***
 
--- elpa/package-build-20180414.938/package-build-pkg.el
+++ elpa/package-build-20180414.938/package-build-pkg.el
@@ -1,4 +1,4 @@
-(define-package "package-build" "20180205.1521" "Tools for assembling a package archive"
+(define-package "package-build" "20180414.938" "Tools for assembling a package archive"
   '((cl-lib "0.5"))
   :keywords
   '("tools"))
