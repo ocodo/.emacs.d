451819d6cffac2a517fc4fdf2527cc94449ce02b
Jasonm23 <jasonm23@gmail.com>
2018-05-06 16:39:24 +0800

    [Updating magit version: 20180505.1620][removing old version: 20180208.1958]

---
+++ elpa/magit-20180505.1620/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "20180505.1620" "A Git porcelain inside Emacs."
+  '((emacs "24.4")
+    (async "20170823")
+    (dash "20170810")
+    (ghub "20180328")
+    (git-commit "20180329")
+    (let-alist "1.0.5")
+    (magit-popup "20180329")
+    (with-editor "20180318")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
