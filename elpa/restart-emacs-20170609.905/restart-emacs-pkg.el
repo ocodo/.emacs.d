0fc6404ed7b8d20a16f2aca481d67fd2b190bfdf
Jasonm23 <jasonm23@gmail.com>
2018-05-06 16:39:37 +0800

    [Adding restart-emacs version: 20170609.905]

---
+++ elpa/restart-emacs-20170609.905/restart-emacs-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "restart-emacs" "20170609.905" "Restart emacs from within emacs" 'nil :commit "98aa4efd2caeaf0f42ca385c24fa14a843b271b7" :keywords '("convenience") :url "https://github.com/iqbalansari/restart-emacs")
