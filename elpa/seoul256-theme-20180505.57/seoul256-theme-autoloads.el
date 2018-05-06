c67688654ee8e98c6082301b2a48fa83a95eeca1
Jasonm23 <jasonm23@gmail.com>
2018-05-06 16:39:40 +0800

    [Updating seoul256-theme version: 20180505.57][removing old version: 20171213.1209]

--- elpa/seoul256-theme-20180505.57/seoul256-theme-autoloads.el
+++ elpa/seoul256-theme-20180505.57/seoul256-theme-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "seoul256-theme" "seoul256-theme.el" (23138
-;;;;;;  47878 860800 716000))
+;;;### (autoloads nil "seoul256-theme" "seoul256-theme.el" (23278
+;;;;;;  48064 0 0))
 ;;; Generated autoloads from seoul256-theme.el
 
 (when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
