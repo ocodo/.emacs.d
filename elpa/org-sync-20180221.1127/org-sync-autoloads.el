7955acf6f24d4bcf684da26b73af64500efef1be
Jasonm23 <jasonm23@gmail.com>
2018-05-06 16:39:28 +0800

    [Updating org-sync version: 20180221.1127][removing old version: 20150817.754]

--- elpa/org-sync-20180221.1127/org-sync-autoloads.el
+++ elpa/org-sync-20180221.1127/org-sync-autoloads.el
@@ -1,11 +1,11 @@
 ;;; org-sync-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("org-sync-bb.el" "org-sync-github.el"
-;;;;;;  "org-sync-pkg.el" "org-sync-redmine.el" "org-sync-rtm.el"
-;;;;;;  "org-sync.el") (21975 62253 772341 511000))
+;;;;;;  "org-sync-gitlab.el" "org-sync-pkg.el" "org-sync-redmine.el"
+;;;;;;  "org-sync-rtm.el" "org-sync.el") (23278 48111 0 0))
 
 ;;;***
 
---
+++ elpa/org-sync-20180221.1127/org-sync-pkg.el
@@ -0,0 +1,10 @@
+(define-package "org-sync" "20180221.1127" "Synchronize Org documents with External Issue Trackers"
+  '((cl-lib "0.5")
+    (org "8.2")
+    (emacs "24"))
+  :keywords
+  '("org" "synchronization" "issue tracking" "github" "redmine")
+  :url "https://github.com/arbox/org-sync")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
