1823ea82ce961d1429c374699060051dddadfb34
Jasonm23 <jasonm23@gmail.com>
2018-05-06 20:59:25 +0800

    Add fullscreen style to Magit

--- _configs/use-magit.el
+++ _configs/use-magit.el
@@ -1,9 +1,18 @@
-;; Magit config
+;;; use-magit --- Configuration for Magit
+;;
+;;; Commentary:
+;;  Settings for Magit
+;;
+;;; Code:
 
-;; (require 'magit-extras)
+(require 'magit)
+
+(setq magit-commit-arguments '("--verbose"))
 (setq magit-repository-directories (quote (("~/.emacs.d" . 0) ("~/workspace/" . 1))))
+(setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
 
 (put 'magit-clean 'disabled nil)
 (define-key magit-mode-map (kbd "C-c C") 'magit-clean)
 
 (provide 'use-magit)
+;;; use-magit.el ends here
