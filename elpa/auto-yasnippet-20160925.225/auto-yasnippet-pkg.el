5ae3014fc547cd4512857fd13068089be600106f
Jasonm23 <jasonm23@gmail.com>
2018-05-06 16:38:19 +0800

    [Updating auto-yasnippet version: 20180503.1208][removing old version: 20160925.225]

--- elpa/auto-yasnippet-20160925.225/auto-yasnippet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "auto-yasnippet" "20160925.225" "Quickly create disposable yasnippets" '((yasnippet "0.8.0")) :url "https://github.com/abo-abo/auto-yasnippet")
--- elpa/auto-yasnippet-20180503.1208/auto-yasnippet-autoloads.el
+++ elpa/auto-yasnippet-20180503.1208/auto-yasnippet-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "auto-yasnippet" "auto-yasnippet.el" (22507
-;;;;;;  31625 361229 224000))
+;;;### (autoloads nil "auto-yasnippet" "auto-yasnippet.el" (23278
+;;;;;;  48465 0 0))
 ;;; Generated autoloads from auto-yasnippet.el
 
 (autoload 'aya-create-one-line "auto-yasnippet" "\
