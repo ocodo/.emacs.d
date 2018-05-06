3c27d54031813a2bdfda3656ddb795b8811da91a
Jasonm23 <jasonm23@gmail.com>
2018-05-06 16:38:28 +0800

    [Updating common-lisp-snippets version: 20180226.723][removing old version: 20170918.356]

--- elpa/common-lisp-snippets-20170918.356/common-lisp-snippets-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "common-lisp-snippets" "20170918.356" "Yasnippets for Common Lisp"
-  '((yasnippet "0.8.0"))
-  :url "https://github.com/mrkkrp/common-lisp-snippets" :keywords
-  '("snippets"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/common-lisp-snippets-20180226.723/common-lisp-snippets-autoloads.el
+++ elpa/common-lisp-snippets-20180226.723/common-lisp-snippets-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "common-lisp-snippets" "common-lisp-snippets.el"
-;;;;;;  (23064 61844 122794 48000))
+;;;;;;  (23278 48411 0 0))
 ;;; Generated autoloads from common-lisp-snippets.el
 
 (autoload 'common-lisp-snippets-initialize "common-lisp-snippets" "\
@@ -16,8 +16,8 @@ Initialize Common Lisp snippets, so Yasnippet can see them.
 
 ;;;***
 
-;;;### (autoloads nil nil ("common-lisp-snippets-pkg.el") (23064
-;;;;;;  61844 122794 48000))
+;;;### (autoloads nil nil ("common-lisp-snippets-pkg.el") (23278
+;;;;;;  48411 0 0))
 
 ;;;***
 
