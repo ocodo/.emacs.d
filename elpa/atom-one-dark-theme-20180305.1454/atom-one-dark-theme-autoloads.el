6090d17befe36826d12606b0f0228ebbc82cea87
Jasonm23 <jasonm23@gmail.com>
2018-05-06 16:38:19 +0800

    [Updating atom-one-dark-theme version: 20180305.1454][removing old version: 20180201.1853]

--- elpa/atom-one-dark-theme-20180305.1454/atom-one-dark-theme-autoloads.el
+++ elpa/atom-one-dark-theme-20180305.1454/atom-one-dark-theme-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "atom-one-dark-theme" "atom-one-dark-theme.el"
-;;;;;;  (23166 22942 956964 28000))
+;;;;;;  (23278 48466 0 0))
 ;;; Generated autoloads from atom-one-dark-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
---
+++ elpa/atom-one-dark-theme-20180305.1454/atom-one-dark-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "atom-one-dark-theme" "20180305.1454" "Atom One Dark color theme" 'nil :commit "7b73ab795286fe121d23026dcc94d6a0a211e4c4" :url "https://github.com/jonathanchu/atom-one-dark-theme")
