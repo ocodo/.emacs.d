13f552de48d0b215d26975b12a178d88197836af
Jasonm23 <jasonm23@gmail.com>
2018-05-06 16:39:30 +0800

    [Updating paradox version: 20180216.334][removing old version: 20161117.502]

---
+++ elpa/paradox-20180216.334/paradox-pkg.el
@@ -0,0 +1,12 @@
+(define-package "paradox" "20180216.334" "A modern Packages Menu. Colored, with package ratings, and customizable."
+  '((emacs "24.4")
+    (seq "1.7")
+    (let-alist "1.0.3")
+    (spinner "1.7.3")
+    (hydra "0.13.2"))
+  :keywords
+  '("package" "packages")
+  :url "https://github.com/Malabarba/paradox")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
