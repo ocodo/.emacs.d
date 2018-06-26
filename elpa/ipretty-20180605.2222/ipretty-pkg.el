821d92f47e8a057c6d2196e85bb998354236bb17
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:05 +0000

    [Adding ipretty version: 20180605.2222]

---
+++ elpa/ipretty-20180605.2222/ipretty-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ipretty" "20180605.2222" "Interactive Emacs Lisp pretty-printing" 'nil :commit "042f5cc4e6f81d59115e8335c582bb5c571c2585" :keywords '("pretty-print" "elisp" "buffer") :url "https://framagit.org/steckerhalter/ipretty")
