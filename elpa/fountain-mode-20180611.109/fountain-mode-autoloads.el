0ced581187b42b5399c42687311c3f30042f73e8
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:57 +0000

    [Adding fountain-mode version: 20180611.109]

---
+++ elpa/fountain-mode-20180611.109/fountain-mode-autoloads.el
@@ -0,0 +1,24 @@
+;;; fountain-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "fountain-mode" "fountain-mode.el" (23344 45014
+;;;;;;  131763 869000))
+;;; Generated autoloads from fountain-mode.el
+
+(add-to-list 'auto-mode-alist '("\\.fountain\\'" . fountain-mode))
+
+(autoload 'fountain-mode "fountain-mode" "\
+Major mode for screenwriting in Fountain markup.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; fountain-mode-autoloads.el ends here
---
+++ elpa/fountain-mode-20180611.109/fountain-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fountain-mode" "20180611.109" "Major mode for screenwriting in Fountain markup" '((emacs "24.5")) :commit "b4c39f0a8965c3cdde4600730704fa9ac12eaabd" :keywords '("wp" "text") :url "https://github.com/rnkn/fountain-mode")
