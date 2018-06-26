bcb504fd92ef3acbd0629ece47b528f9f2e66104
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:05 +0000

    [Adding inf-ruby version: 20180521.648]

---
+++ elpa/inf-ruby-20180521.648/inf-ruby-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "inf-ruby" "20180521.648" "Run a Ruby process in a buffer" 'nil :commit "49d59a7897f594e3be74ecbddae83719f9a6c0f0" :keywords '("languages" "ruby") :url "http://github.com/nonsequitur/inf-ruby")
