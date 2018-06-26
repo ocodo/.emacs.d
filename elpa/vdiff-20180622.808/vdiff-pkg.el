c07ea263c561447d7c48719b09095fd961f49899
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:20 +0000

    [Adding vdiff version: 20180622.808]

---
+++ elpa/vdiff-20180622.808/vdiff-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vdiff" "20180622.808" "A diff tool similar to  vimdiff" '((emacs "24.4") (hydra "0.13.0")) :commit "348d1a78d217603daebaf2db75f6802cdd435138" :keywords '("diff") :url "https://github.com/justbur/emacs-vdiff")
