dd1841a3a6341acfa726a1781a7dcc6420737d67
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:12 +0000

    [Adding perspective version: 20180614.1533]

---
+++ elpa/perspective-20180614.1533/perspective-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "perspective" "20180614.1533" "switch between named \"perspectives\" of the editor" '((cl-lib "0.5")) :commit "ca2a3b51a8411853b9adef2a9fe38011f84b91d9" :keywords '("workspace" "convenience" "frames") :url "http://github.com/nex3/perspective-el")
