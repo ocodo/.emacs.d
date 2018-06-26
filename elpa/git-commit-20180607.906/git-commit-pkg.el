e4506d9118f349c1affac77a36a9d9e8776b83d9
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:59 +0000

    [Adding git-commit version: 20180607.906]

---
+++ elpa/git-commit-20180607.906/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20180607.906" "Edit Git commit messages" '((emacs "25.1") (dash "20180413") (with-editor "20180414")) :commit "c98ea64ac1e64e72c55614ef482a1fc4b095fb17" :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")
