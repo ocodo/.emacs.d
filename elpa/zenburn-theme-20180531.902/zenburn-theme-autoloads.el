a31443d20056034b2c45229645cb4fb100fb2213
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:24 +0000

    [Adding zenburn-theme version: 20180531.902]

---
+++ elpa/zenburn-theme-20180531.902/zenburn-theme-autoloads.el
@@ -0,0 +1,27 @@
+;;; zenburn-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (23344 44633
+;;;;;;  46830 750000))
+;;; Generated autoloads from zenburn-theme.el
+
+(defvar zenburn-override-colors-alist 'nil "\
+Place to override default theme colors.
+
+You can override a subset of the theme's default colors by
+defining them in this alist.")
+
+(custom-autoload 'zenburn-override-colors-alist "zenburn-theme" t)
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; zenburn-theme-autoloads.el ends here
