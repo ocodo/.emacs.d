9e709d6ebf5502e99123d1a9b9d7ed39afc6de7b
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:19 +0000

    [Adding twittering-mode version: 20180507.721]

---
+++ elpa/twittering-mode-20180507.721/twittering-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "twittering-mode" "20180507.721" "Major mode for Twitter" 'nil :commit "6d10d1765a7b4de4c723395c8a2200a1649beeb0" :keywords '("twitter" "web") :url "http://twmode.sf.net/")
