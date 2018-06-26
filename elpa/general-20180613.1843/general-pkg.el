b80f2d44d7294a60769e94305d1283edda298e43
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:58 +0000

    [Adding general version: 20180613.1843]

---
+++ elpa/general-20180613.1843/general-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "general" "20180613.1843" "Convenience wrappers for keybindings." '((emacs "24.4") (cl-lib "0.5")) :commit "38590b2b40d9ae71a047914f938416a7bd01e370" :keywords '("vim" "evil" "leader" "keybindings" "keys") :url "https://github.com/noctuid/general.el")
