599a14ea14d7d570266bc9e45660d3b60b04cb41
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:03 +0000

    [Adding highlight version: 20180604.2335]

---
+++ elpa/highlight-20180604.2335/highlight-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "highlight" "20180604.2335" "Highlighting commands." 'nil :commit "6f92253690dde26d9bfd21546fdf68ef2fdd486b" :keywords '("faces" "help" "local") :url "https://www.emacswiki.org/emacs/download/highlight.el")
