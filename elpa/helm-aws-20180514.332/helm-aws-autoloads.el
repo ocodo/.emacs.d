7136a9d92c279a2e69f0a262f8a966ccb5979a98
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:01 +0000

    [Adding helm-aws version: 20180514.332]

---
+++ elpa/helm-aws-20180514.332/helm-aws-autoloads.el
@@ -0,0 +1,22 @@
+;;; helm-aws-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "helm-aws" "helm-aws.el" (23344 44956 926420
+;;;;;;  455000))
+;;; Generated autoloads from helm-aws.el
+
+(autoload 'helm-aws "helm-aws" "\
+Show helm with a table of aws information.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-aws-autoloads.el ends here
