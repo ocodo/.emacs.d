c817415f6a69c27638e66e2389630c1728c47c81
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:49 +0000

    [Adding circadian version: 20180516.1509]

---
+++ elpa/circadian-20180516.1509/circadian-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "circadian" "20180516.1509" "Theme-switching based on daytime" '((emacs "24.4")) :commit "1be87f4e096d8cc28e3ba66b403cfdc4ddb7a461" :keywords '("themes") :url "https://github.com/GuidoSchmidt/circadian")
