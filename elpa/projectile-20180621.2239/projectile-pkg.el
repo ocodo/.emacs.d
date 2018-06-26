a26626b4b4b66de004add2f5e90cf22975f97058
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:13 +0000

    [Adding projectile version: 20180621.2239]

---
+++ elpa/projectile-20180621.2239/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "projectile" "20180621.2239" "Manage and navigate projects in Emacs easily" '((emacs "24.3") (pkg-info "0.4")) :commit "98de987369119bd0db94c562267c0b1847a9c737" :keywords '("project" "convenience") :url "https://github.com/bbatsov/projectile")
