0dba16ef28730c0e9ff7d614081de68fda3dbf9f
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:59 +0000

    [Adding git-timemachine version: 20180607.120]

---
+++ elpa/git-timemachine-20180607.120/git-timemachine-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-timemachine" "20180607.120" "Walk through git revisions of a file" '((emacs "24.3")) :commit "90a980578249c102da3e904fccdc9a2a5a0e7bcc" :keywords '("git") :url "https://github.com/pidu/git-timemachine")
