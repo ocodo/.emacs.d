7bdc5bb0d2fdef4eb0f22e33b268f3a45a9a51ae
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:54 +0000

    [Adding emacsql version: 20180507.1308]

---
+++ elpa/emacsql-20180507.1308/emacsql-pkg.el
@@ -0,0 +1,6 @@
+(define-package "emacsql" "20180507.1308" "high-level SQL database front-end"
+  '((emacs "25.1"))
+  :url "https://github.com/skeeto/emacsql")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
