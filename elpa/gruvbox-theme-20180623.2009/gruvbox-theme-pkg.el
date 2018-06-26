4969f8adbdff22010640514a37f4e26c78719feb
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:00 +0000

    [Adding gruvbox-theme version: 20180623.2009]

---
+++ elpa/gruvbox-theme-20180623.2009/gruvbox-theme-pkg.el
@@ -0,0 +1,6 @@
+(define-package "gruvbox-theme" "20180623.2009" "A retro-groove colour theme for Emacs"
+  '((autothemer "0.2"))
+  :url "http://github.com/greduan/emacs-theme-gruvbox")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
