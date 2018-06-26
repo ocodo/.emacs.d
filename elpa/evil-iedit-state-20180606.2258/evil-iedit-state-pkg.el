abab0855565cf432a116576052e465e86270044c
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:56 +0000

    [Adding evil-iedit-state version: 20180606.2258]

---
+++ elpa/evil-iedit-state-20180606.2258/evil-iedit-state-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-iedit-state" "20180606.2258" "Evil states to interface iedit mode." '((evil "1.0.9") (iedit "0.97")) :commit "f75cff4ecbd5beaa9ca64a6c157c4105f078daec" :keywords '("convenience" "editing" "evil" "iedit" "mnemonic") :url "https://github.com/syl20bnr/evil-iedit-state")
