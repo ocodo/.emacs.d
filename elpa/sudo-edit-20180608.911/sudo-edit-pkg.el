b259b2a4367b22124bfeda1de2f32589d884070c
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:17 +0000

    [Adding sudo-edit version: 20180608.911]

---
+++ elpa/sudo-edit-20180608.911/sudo-edit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sudo-edit" "20180608.911" "Open files as another user" '((emacs "24") (cl-lib "0.5")) :commit "185edd69729b2adc8dccd015266636dae2df83d3" :keywords '("convenience") :url "https://github.com/nflath/sudo-edit")
