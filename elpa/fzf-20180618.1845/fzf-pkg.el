318546b13b1967e5dd75ec11e8809e240fd2629e
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:58 +0000

    [Adding fzf version: 20180618.1845]

---
+++ elpa/fzf-20180618.1845/fzf-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fzf" "20180618.1845" "A front-end for fzf." '((emacs "24.4")) :commit "521d18933cb586337c4e34281bdc71ac07202c98" :keywords '("fzf" "fuzzy" "search") :url "https://github.com/bling/fzf.el")
