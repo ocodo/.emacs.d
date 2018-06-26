d0150e0132f6e0c4c6f3ac2c6b90ac7b0afad7b4
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:11 +0000

    [Adding ox-hugo version: 20180620.1102]

---
+++ elpa/ox-hugo-20180620.1102/ox-hugo-autoloads.el
@@ -0,0 +1,229 @@
+;;; ox-hugo-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "ox-blackfriday" "ox-blackfriday.el" (23344
+;;;;;;  44823 547291 113000))
+;;; Generated autoloads from ox-blackfriday.el
+
+(autoload 'org-blackfriday-export-as-markdown "ox-blackfriday" "\
+Export current buffer to a Github Flavored Markdown buffer.
+
+If narrowing is active in the current buffer, only export its
+narrowed part.
+
+If a region is active, export that region.
+
+A non-nil optional argument ASYNC means the process should happen
+asynchronously.  The resulting buffer should be accessible
+through the `org-export-stack' interface.
+
+When optional argument SUBTREEP is non-nil, export the sub-tree
+at point, extracting information from the headline properties
+first.
+
+When optional argument VISIBLE-ONLY is non-nil, don't export
+contents of hidden elements.
+
+Export is done in a buffer named \"*Org BLACKFRIDAY Export*\", which will
+be displayed when `org-export-show-temporary-export-buffer' is
+non-nil.
+
+\(fn &optional ASYNC SUBTREEP VISIBLE-ONLY)" t nil)
+
+(autoload 'org-blackfriday-convert-region-to-md "ox-blackfriday" "\
+Convert text in the current region to Blackfriday Markdown.
+The text is assumed to be in Org mode format.
+
+This can be used in any buffer.  For example, you can write an
+itemized list in Org mode syntax in a Markdown buffer and use
+this command to convert it.
+
+\(fn)" t nil)
+
+(autoload 'org-blackfriday-export-to-markdown "ox-blackfriday" "\
+Export current buffer to a Github Flavored Markdown file.
+
+If narrowing is active in the current buffer, only export its
+narrowed part.
+
+If a region is active, export that region.
+
+A non-nil optional argument ASYNC means the process should happen
+asynchronously.  The resulting file should be accessible through
+the `org-export-stack' interface.
+
+When optional argument SUBTREEP is non-nil, export the sub-tree
+at point, extracting information from the headline properties
+first.
+
+When optional argument VISIBLE-ONLY is non-nil, don't export
+contents of hidden elements.
+
+Return output file's name.
+
+\(fn &optional ASYNC SUBTREEP VISIBLE-ONLY)" t nil)
+
+(autoload 'org-blackfriday-publish-to-blackfriday "ox-blackfriday" "\
+Publish an Org file to Blackfriday Markdown file.
+
+PLIST is the property list for the given project.  FILENAME is
+the filename of the Org file to be published.  PUB-DIR is the
+publishing directory.
+
+Return output file name.
+
+\(fn PLIST FILENAME PUB-DIR)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil "ox-hugo" "ox-hugo.el" (23344 44823 551291
+;;;;;;  207000))
+;;; Generated autoloads from ox-hugo.el
+
+(autoload 'org-hugo-slug "ox-hugo" "\
+Return a slug string for STR.
+STR is in Markdown format, most likely a Markdown heading.  The
+returned slug string has the following specification:
+
+- Should contain only lower case alphabet, number and hyphen
+  characters.
+- Remove *any* HTML tag like \"<code>..</code>\", \"<span
+  class=..>..</span>\", etc from STR if present.
+- URLs if present in STR should be removed.
+- Replace \".\" in STR with \"and\", and \"&\" with \"and\".
+- Parentheses should be replaced with double-hyphens ( \"foo (bar)
+  baz\" becomes \"foo--bar--baz\").
+- One or more consecutive spaces should be replaced with a single
+  hyphen.
+- Maximum number of consecutive hyphens allowed is two.
+- No hyphens should be present at the leading or trailing end of the
+  returned string .
+
+\(fn STR)" nil nil)
+
+(autoload 'org-hugo-export-as-md "ox-hugo" "\
+Export current buffer to a Hugo-compatible Markdown buffer.
+
+If narrowing is active in the current buffer, only export its
+narrowed part.
+
+If a region is active, export that region.
+
+A non-nil optional argument ASYNC means the process should happen
+asynchronously.  The resulting buffer should be accessible
+through the `org-export-stack' interface.
+
+When optional argument SUBTREEP is non-nil, export the sub-tree
+at point, extracting information from the headline properties
+first.
+
+When optional argument VISIBLE-ONLY is non-nil, don't export
+contents of hidden elements.
+
+Export is done in a buffer named \"*Org Hugo Export*\", which
+will be displayed when `org-export-show-temporary-export-buffer'
+is non-nil.
+
+Return the buffer the export happened to.
+
+\(fn &optional ASYNC SUBTREEP VISIBLE-ONLY)" t nil)
+
+(autoload 'org-hugo-export-to-md "ox-hugo" "\
+Export current buffer to a Hugo-compatible Markdown file.
+
+If narrowing is active in the current buffer, only export its
+narrowed part.
+
+If a region is active, export that region.
+
+A non-nil optional argument ASYNC means the process should happen
+asynchronously.  The resulting file should be accessible through
+the `org-export-stack' interface.
+
+When optional argument SUBTREEP is non-nil, export the sub-tree
+at point, extracting information from the headline properties
+first.
+
+When optional argument VISIBLE-ONLY is non-nil, don't export
+contents of hidden elements.
+
+Return output file's name.
+
+\(fn &optional ASYNC SUBTREEP VISIBLE-ONLY)" t nil)
+
+(autoload 'org-hugo-export-wim-to-md "ox-hugo" "\
+Export the current subtree/all subtrees/current file to a Hugo post.
+
+This is an Export \"What I Mean\" function:
+
+- If the current subtree has the \"EXPORT_FILE_NAME\" property, export
+  that subtree.
+- If the current subtree doesn't have that property, but one of its
+  parent subtrees has, then export from that subtree's scope.
+- If none of the subtrees have that property (or if there are no Org
+  subtrees at all), but the Org #+title keyword is present,
+  export the whole Org file as a post with that title (calls
+  `org-hugo-export-to-md' with its SUBTREEP argument set to nil).
+
+- If ALL-SUBTREES is non-nil, export all valid Hugo post subtrees
+  (that have the \"EXPORT_FILE_NAME\" property) in the current file
+  to multiple Markdown posts.
+- If ALL-SUBTREES is non-nil, and again if none of the subtrees have
+  that property (or if there are no Org subtrees), but the Org #+title
+  keyword is present, export the whole Org file.
+
+- If the file neither has valid Hugo post subtrees, nor has the
+  #+title present, throw a user error.  If NOERROR is non-nil, use
+  `message' to display the error message instead of signaling a user
+  error.
+
+A non-nil optional argument ASYNC means the process should happen
+asynchronously.  The resulting file should be accessible through
+the `org-export-stack' interface.
+
+When optional argument VISIBLE-ONLY is non-nil, don't export
+contents of hidden elements.
+
+If ALL-SUBTREES is nil, return output file's name.
+If ALL-SUBTREES is non-nil, and valid subtrees are found, return
+a list of output files.
+If ALL-SUBTREES is non-nil, and valid subtrees are not found,
+return the output file's name (exported using file-based
+approach).
+
+\(fn &optional ALL-SUBTREES ASYNC VISIBLE-ONLY NOERROR)" t nil)
+
+(autoload 'org-hugo-export-wim-to-md-after-save "ox-hugo" "\
+Fn for `after-save-hook' to run `org-hugo-export-wim-to-md'.
+
+The export is also skipped if `org-hugo-allow-export-after-save'
+is nil.  This variable is intended to be toggled dynamically in
+`org-capture-before-finalize-hook' and
+`org-capture-after-finalize-hook' hooks.  See the ‘Auto-export on
+Saving’ section in this package's documentation for an example.
+
+\(fn)" nil nil)
+
+(autoload 'org-hugo-debug-info "ox-hugo" "\
+Get Emacs, Org and Hugo version and ox-hugo customization info.
+The information is converted to Markdown format and copied to the
+kill ring.  The same information is displayed in the Messages
+buffer and returned as a string in Org format.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ox-hugo-pkg.el") (23344 44823 551291
+;;;;;;  207000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ox-hugo-autoloads.el ends here
