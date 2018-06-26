8b7a4e2e148e2f522dbf4e0455585b5882cd8e49
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:57 +0000

    [Adding flymake-json version: 20180511.211]

---
+++ elpa/flymake-json-20180511.211/flymake-json-autoloads.el
@@ -0,0 +1,27 @@
+;;; flymake-json-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "flymake-json" "flymake-json.el" (23344 45016
+;;;;;;  227813 106000))
+;;; Generated autoloads from flymake-json.el
+
+(autoload 'flymake-json-load "flymake-json" "\
+Configure flymake mode to check the current buffer's javascript syntax.
+
+\(fn)" t nil)
+
+(autoload 'flymake-json-maybe-load "flymake-json" "\
+Call `flymake-json-load' if this file appears to be json.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flymake-json-autoloads.el ends here
