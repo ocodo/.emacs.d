f5b8cfa1204091050ea2c28257d341bfc5bbfd2e
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:55 +0000

    [Adding emojify version: 20180611.838]

---
+++ elpa/emojify-20180611.838/emojify-pkg.el
@@ -0,0 +1,10 @@
+(define-package "emojify" "20180611.838" "Display emojis in Emacs"
+  '((seq "1.11")
+    (ht "2.0")
+    (emacs "24.3"))
+  :keywords
+  '("multimedia" "convenience")
+  :url "https://github.com/iqbalansari/emacs-emojify")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
