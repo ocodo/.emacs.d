c93262b9fc714ca1dde469ff7be20a8616add7da
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:49 +0000

    [Adding clojure-mode version: 20180608.507]

---
+++ elpa/clojure-mode-20180608.507/clojure-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clojure-mode" "20180608.507" "Major mode for Clojure code" '((emacs "24.4")) :commit "610d1f6d723f43d5f47b1e65438fe38176a730a6" :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
