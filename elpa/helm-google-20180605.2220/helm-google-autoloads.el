d4f9e20de8380de9968efc1ddcd68dc331fde808
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:02 +0000

    [Adding helm-google version: 20180605.2220]

---
+++ elpa/helm-google-20180605.2220/helm-google-autoloads.el
@@ -0,0 +1,32 @@
+;;; helm-google-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "helm-google" "helm-google.el" (23344 44949
+;;;;;;  966257 50000))
+;;; Generated autoloads from helm-google.el
+
+(autoload 'helm-google "helm-google" "\
+Web search interface for Emacs.
+
+\(fn &optional ENGINE SEARCH-TERM)" t nil)
+
+(autoload 'helm-google-searx "helm-google" "\
+Explicitly use Searx for the web search.
+
+\(fn &optional SEARCH-TERM)" t nil)
+
+(autoload 'helm-google-google "helm-google" "\
+Explicitly use Google for the web search.
+
+\(fn &optional SEARCH-TERM)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-google-autoloads.el ends here
