6df69237fa1f426fdb4905b9e6f8f2a9820131d8
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:08 +0000

    [Adding mc-extras version: 20180519.2139]

---
+++ elpa/mc-extras-20180519.2139/mc-extras-pkg.el
@@ -0,0 +1,8 @@
+(define-package "mc-extras" "20180519.2139" "Extra functions for multiple-cursors mode."
+  '((multiple-cursors "1.2.1"))
+  :keywords
+  '("editing" "cursors")
+  :url "https://github.com/knu/mc-extras.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
