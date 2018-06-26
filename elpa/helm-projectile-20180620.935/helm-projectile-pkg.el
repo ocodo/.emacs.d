f7fed25e0a9c8e8b7a3e439479315b25eaf91e43
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:03 +0000

    [Adding helm-projectile version: 20180620.935]

---
+++ elpa/helm-projectile-20180620.935/helm-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-projectile" "20180620.935" "Helm integration for Projectile" '((helm "1.9.9") (projectile "0.14.0") (cl-lib "0.3")) :commit "8aae5d87fa9937777cfa67b7012f5ca33c81893f" :keywords '("project" "convenience") :url "https://github.com/bbatsov/helm-projectile")
