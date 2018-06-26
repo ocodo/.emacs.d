e6b0f403a227926f72e4a69ed4276d98a9fdbb2e
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:00 +0000

    [Adding gruber-darker-theme version: 20180529.12]

---
+++ elpa/gruber-darker-theme-20180529.12/gruber-darker-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; gruber-darker-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "gruber-darker-theme" "gruber-darker-theme.el"
+;;;;;;  (23344 44994 327298 700000))
+;;; Generated autoloads from gruber-darker-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; gruber-darker-theme-autoloads.el ends here
