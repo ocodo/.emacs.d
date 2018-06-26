1d54dea0549784117edcd45204f509ff6a7a2965
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:09 +0000

    [Adding memoize version: 20180614.1230]

---
+++ elpa/memoize-20180614.1230/memoize-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "memoize" "20180614.1230" "Memoization functions" 'nil :commit "9a561268ffb550b257a08710489a95cd087998b6" :url "https://github.com/skeeto/emacs-memoize")
