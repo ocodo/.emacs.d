156f5188bdea7336a783bb3874fa9dadfc5e10ba
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:51 +0000

    [Adding darktooth-theme version: 20180612.1814]

---
+++ elpa/darktooth-theme-20180612.1814/darktooth-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "darktooth-theme" "20180612.1814" "From the darkness... it watches" '((autothemer "0.2")) :commit "98f909497710ff948c996c810ca1bddf1f8b91d8" :url "http://github.com/emacsfodder/emacs-theme-darktooth")
