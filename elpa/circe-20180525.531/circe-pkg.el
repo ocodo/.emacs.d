8dc19486d308e5946a94c0542551a0cd452e79c8
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:49 +0000

    [Adding circe version: 20180525.531]

---
+++ elpa/circe-20180525.531/circe-pkg.el
@@ -0,0 +1,6 @@
+(define-package "circe" "20180525.531" "Client for IRC in Emacs"
+  '((cl-lib "0.5"))
+  :url "https://github.com/jorgenschaefer/circe")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
