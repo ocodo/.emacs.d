963cd5e9214d7276c4ee1220e59196fc937dac97
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:22 +0000

    [Adding window-purpose version: 20180531.1336]

---
+++ elpa/window-purpose-20180531.1336/window-purpose-pkg.el
@@ -0,0 +1,11 @@
+(define-package "window-purpose" "20180531.1336" "Purpose-based window management for Emacs"
+  '((emacs "24")
+    (cl-lib "0.5")
+    (let-alist "1.0.3")
+    (imenu-list "0.1"))
+  :keywords
+  '("frames")
+  :url "https://github.com/bmag/emacs-purpose")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
