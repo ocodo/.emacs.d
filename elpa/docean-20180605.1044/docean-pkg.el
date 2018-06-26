2cba95a99ab1566ecf4dde77c01e963d65e427fc
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:42:53 +0000

    [Adding docean version: 20180605.1044]

---
+++ elpa/docean-20180605.1044/docean-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "docean" "20180605.1044" "Interact with DigitalOcean from Emacs." '((emacs "24") (cl-lib "0.5") (request "0.2.0")) :commit "bbe2298fd21f7876fc2d5c52a69b931ff59df979" :keywords '("convenience") :url "https://github.com/emacs-pe/docean.el")
