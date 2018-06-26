96fca7124cd1be4e27f7d0cf4ae8286c676368c1
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:18 +0000

    [Adding tide version: 20180608.239]

---
+++ elpa/tide-20180608.239/tide-pkg.el
@@ -0,0 +1,12 @@
+(define-package "tide" "20180608.239" "Typescript Interactive Development Environment"
+  '((dash "2.10.0")
+    (s "1.11.0")
+    (flycheck "27")
+    (typescript-mode "0.1")
+    (cl-lib "0.5"))
+  :keywords
+  '("typescript")
+  :url "http://github.com/ananthakumaran/tide")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
