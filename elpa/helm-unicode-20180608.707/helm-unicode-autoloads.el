a0a7d9ae2e5c04a2c4eb5008fe918451cb178593
jasonm23 <jasonm23@gmail.com>
2018-06-26 00:43:03 +0000

    [Adding helm-unicode version: 20180608.707]

---
+++ elpa/helm-unicode-20180608.707/helm-unicode-autoloads.el
@@ -0,0 +1,24 @@
+;;; helm-unicode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "helm-unicode" "helm-unicode.el" (23344 44940
+;;;;;;  526035 434000))
+;;; Generated autoloads from helm-unicode.el
+
+(autoload 'helm-unicode "helm-unicode" "\
+Precofigured `helm' for looking up unicode characters by name.
+
+With prefix ARG, reinitialize the cache.
+
+\(fn ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-unicode-autoloads.el ends here
