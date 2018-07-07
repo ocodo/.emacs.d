97d33890eedf9d87ae4676d652371e154ce2bac6
jasonm23 <jasonm23@gmail.com>
2018-07-07 03:54:57 +0000

    [Adding template-overlays version: 20180706.432]

---
+++ elpa/template-overlays-20180706.432/template-overlays-autoloads.el
@@ -0,0 +1,22 @@
+;;; template-overlays-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "template-overlays" "template-overlays.el"
+;;;;;;  (23360 14563 154713 135000))
+;;; Generated autoloads from template-overlays.el
+
+(autoload 'template-overlays-mode "template-overlays" "\
+Template overlays minor mode
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; template-overlays-autoloads.el ends here
---
+++ elpa/template-overlays-20180706.432/template-overlays-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "template-overlays" "20180706.432" "Display template regions using overlays" '((emacs "24.4") (ov "1.0.6")))
