fadddba5d84901163020614f162a6f5a59981a4b
jasonm23 <jasonm23@gmail.com>
2018-07-07 03:54:48 +0000

    [Adding counsel-codesearch version: 20180527.728]

---
+++ elpa/counsel-codesearch-20180527.728/counsel-codesearch-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-codesearch" "20180527.728" "Counsel interface for codesearch.el" '((codesearch "1") (counsel "0.10.0") (emacs "24") (ivy "0.10.0")))
