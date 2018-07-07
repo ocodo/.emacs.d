931985d7072805842fb9d849f976744645f81390
jasonm23 <jasonm23@gmail.com>
2018-07-07 03:54:49 +0000

    [Adding deadgrep version: 20180705.602]

---
+++ elpa/deadgrep-20180705.602/deadgrep-autoloads.el
@@ -0,0 +1,22 @@
+;;; deadgrep-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "deadgrep" "deadgrep.el" (23360 14653 376860
+;;;;;;  690000))
+;;; Generated autoloads from deadgrep.el
+
+(autoload 'deadgrep "deadgrep" "\
+Start a ripgrep search for SEARCH-TERM.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; deadgrep-autoloads.el ends here
