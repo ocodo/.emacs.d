8f8a4bdeb4935273e0ea35d35f6ca483fa47a518
jasonm23 <jasonm23@gmail.com>
2018-07-07 03:54:48 +0000

    [Adding counsel-notmuch version: 20171223.145]

---
+++ elpa/counsel-notmuch-20171223.145/counsel-notmuch-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-notmuch" "20171223.145" "Search emails in Notmuch asynchronously with Ivy" '((emacs "24") (ivy "0.10.0") (notmuch "0.21") (s "1.12.0")))
