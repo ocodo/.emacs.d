58dbf2489f8bd86fd9924ae400905fdc5a45fe94
jasonm23 <jasonm23@gmail.com>
2018-07-07 03:54:51 +0000

    [Adding elog version: 20160724.1555]

---
+++ elpa/elog-20160724.1555/elog-autoloads.el
@@ -0,0 +1,15 @@
+;;; elog-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("elog.el") (23360 14615 395955 958000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; elog-autoloads.el ends here
---
+++ elpa/elog-20160724.1555/elog-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elog" "20160724.1555" "logging library extended from logito" '((eieio "1.3")))
