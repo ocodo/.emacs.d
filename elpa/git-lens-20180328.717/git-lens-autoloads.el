0c9709318dbcab39950b05007617d27b563945b6
jasonm23 <jasonm23@gmail.com>
2018-07-07 03:54:52 +0000

    [Adding git-lens version: 20180328.717]

---
+++ elpa/git-lens-20180328.717/git-lens-autoloads.el
@@ -0,0 +1,22 @@
+;;; git-lens-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "git-lens" "git-lens.el" (23360 14611 251857
+;;;;;;  298000))
+;;; Generated autoloads from git-lens.el
+
+(autoload 'git-lens "git-lens" "\
+Start git lens.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; git-lens-autoloads.el ends here
---
+++ elpa/git-lens-20180328.717/git-lens-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-lens" "20180328.717" "Show new, deleted or modified files in branch" '((emacs "24.4")))
