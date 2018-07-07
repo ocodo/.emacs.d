02ee98098316fb16f0a1857be1076ff166afdb91
jasonm23 <jasonm23@gmail.com>
2018-07-07 03:54:54 +0000

    [Adding intero version: 20180703.18]

---
+++ elpa/intero-20180703.18/intero-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "intero" "20180703.18" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")))
