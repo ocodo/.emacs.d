45c5445f5eb4f5120810a2a7048c54613788517d
jasonm23 <jasonm23@gmail.com>
2018-07-07 03:54:54 +0000

    [Adding ivy-prescient version: 20180702.1140]

---
+++ elpa/ivy-prescient-20180702.1140/ivy-prescient-autoloads.el
@@ -0,0 +1,32 @@
+;;; ivy-prescient-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "ivy-prescient" "ivy-prescient.el" (23360 14592
+;;;;;;  919421 0))
+;;; Generated autoloads from ivy-prescient.el
+
+(defvar ivy-prescient-mode nil "\
+Non-nil if Ivy-Prescient mode is enabled.
+See the `ivy-prescient-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `ivy-prescient-mode'.")
+
+(custom-autoload 'ivy-prescient-mode "ivy-prescient" nil)
+
+(autoload 'ivy-prescient-mode "ivy-prescient" "\
+Minor mode to use prescient.el in Ivy menus.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ivy-prescient-autoloads.el ends here
---
+++ elpa/ivy-prescient-20180702.1140/ivy-prescient-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ivy-prescient" "20180702.1140" "prescient.el + Ivy" '((emacs "25.1") (prescient "2.0") (ivy "0.10.0")))
