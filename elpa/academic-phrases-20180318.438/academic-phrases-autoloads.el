83b53d1bd1367fcca26cf24150f05557ace0ce9f
jasonm23 <jasonm23@gmail.com>
2018-07-07 03:54:47 +0000

    [Adding academic-phrases version: 20180318.438]

---
+++ elpa/academic-phrases-20180318.438/academic-phrases-autoloads.el
@@ -0,0 +1,27 @@
+;;; academic-phrases-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "academic-phrases" "academic-phrases.el" (23360
+;;;;;;  14669 449243 831000))
+;;; Generated autoloads from academic-phrases.el
+
+(autoload 'academic-phrases "academic-phrases" "\
+Insert a phrase from a list of academic phrases by topic.
+
+\(fn)" t nil)
+
+(autoload 'academic-phrases-by-section "academic-phrases" "\
+Insert a phrase from a list of academic phrases by the paper section.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; academic-phrases-autoloads.el ends here
---
+++ elpa/academic-phrases-20180318.438/academic-phrases-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "academic-phrases" "20180318.438" "Bypass that mental block when writing your papers." '((dash "2.12.0") (s "1.12.0") (ht "2.0") (emacs "24")))
