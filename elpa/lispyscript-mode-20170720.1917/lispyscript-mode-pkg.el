c2b5b6eee9746556162b509262215b6d147165c4
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:49 +0000

    [Adding lispyscript-mode version: 20170720.1917]

---
+++ elpa/lispyscript-mode-20170720.1917/lispyscript-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lispyscript-mode" "20170720.1917" "Major mode for LispyScript code." 'nil :commit "def632e3335b0c481fbcf5a17f18b0a8c58dd12f" :keywords '("lisp" "languages") :authors '(("Kris Jenkins" . "krisajenkins@gmail.com")) :maintainer '("Kris Jenkins" . "krisajenkins@gmail.com") :url "https://github.com/krisajenkins/lispyscript-mode")
