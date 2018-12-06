73005866f69f7bc662af2ab56816e3c2fafae3e5
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:04 +0000

    [Adding colonoscopy-theme version: 20170808.1309]

---
+++ elpa/colonoscopy-theme-20170808.1309/colonoscopy-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "colonoscopy-theme" "20170808.1309" "an Emacs 24 theme based on Colonoscopy (tmTheme)" '((emacs "24.0")) :commit "64bbb322b13dae91ce9f1e3581f836f94f800ead" :authors '(("Jason Milkins")) :maintainer '("Jason Milkins") :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
