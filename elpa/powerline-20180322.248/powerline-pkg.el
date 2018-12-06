8b9ed41ee250dbf7dd65f7025a622630f692af55
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:07:47 +0000

    [Adding powerline version: 20180322.248]

---
+++ elpa/powerline-20180322.248/powerline-pkg.el
@@ -0,0 +1,12 @@
+(define-package "powerline" "20180322.248" "Rewrite of Powerline"
+  '((cl-lib "0.2"))
+  :keywords
+  '("mode-line")
+  :authors
+  '(("Donald Ephraim Curtis" . "dcurtis@milkbox.net"))
+  :maintainer
+  '("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+  :url "http://github.com/milkypostman/powerline/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
