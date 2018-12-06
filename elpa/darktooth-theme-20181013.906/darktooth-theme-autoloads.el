9e48d8ab616f4f10411f489514aa658270189b22
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:25 +0000

    [Adding darktooth-theme version: 20181013.906]

---
+++ elpa/darktooth-theme-20181013.906/darktooth-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; darktooth-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "darktooth-theme" "darktooth-theme.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from darktooth-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "darktooth-theme" '("darktooth-modeline")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; darktooth-theme-autoloads.el ends here
