f05d7e0d2dc858f786b10990965911bda6dadd96
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:32 +0000

    [Adding flycheck-jest version: 20180411.328]

---
+++ elpa/flycheck-jest-20180411.328/flycheck-jest-autoloads.el
@@ -0,0 +1,27 @@
+;;; flycheck-jest-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flycheck-jest" "flycheck-jest.el" (0 0 0 0))
+;;; Generated autoloads from flycheck-jest.el
+
+(autoload 'flycheck-jest-setup "flycheck-jest" "\
+Setup Flycheck for Jest.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flycheck-jest" '("flycheck-jest-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flycheck-jest-autoloads.el ends here
---
+++ elpa/flycheck-jest-20180411.328/flycheck-jest-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-jest" "20180411.328" "Flycheck extension for Jest." '((emacs "25.1") (flycheck "0.25")) :commit "08f27c5ed97c83c445f99fab58f0b6c826f14449" :keywords '("languages" "jest") :maintainer '("James Nguyen" . "james@jojojames.com") :url "https://github.com/jojojames/flycheck-jest")
