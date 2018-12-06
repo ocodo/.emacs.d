44905b7d475009762467dca4fe770f5ac476f6c5
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:34 +0000

    [Adding scad-preview version: 20160206.1336]

---
+++ elpa/scad-preview-20160206.1336/scad-preview-autoloads.el
@@ -0,0 +1,27 @@
+;;; scad-preview-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "scad-preview" "scad-preview.el" (0 0 0 0))
+;;; Generated autoloads from scad-preview.el
+
+(autoload 'scad-preview-mode "scad-preview" "\
+Preview SCAD models in real-time within Emacs.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "scad-preview" '("scad-preview-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; scad-preview-autoloads.el ends here
---
+++ elpa/scad-preview-20160206.1336/scad-preview-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "scad-preview" "20160206.1336" "Preview SCAD models in real-time within Emacs" '((scad-mode "91.0")) :commit "fee011589671cc8f1296cb6aa81553e5bb699819" :authors '(("zk_phi")) :maintainer '("zk_phi") :url "http://hins11.yu-yake.com/")
