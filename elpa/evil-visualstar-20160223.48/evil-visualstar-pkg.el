f06d1a35234de427549b245a785098fec8d9e7ef
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:16 +0000

    [Adding evil-visualstar version: 20160223.48]

---
+++ elpa/evil-visualstar-20160223.48/evil-visualstar-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-visualstar" "20160223.48" "Starts a * or # search from the visual selection" '((evil "0")) :commit "06c053d8f7381f91c53311b1234872ca96ced752" :keywords '("evil" "vim" "visualstar") :authors '(("Bailey Ling")) :maintainer '("Bailey Ling") :url "https://github.com/bling/evil-visualstar")
