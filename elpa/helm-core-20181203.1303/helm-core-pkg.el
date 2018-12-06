591d2ee7c4087e56ce814ac8ec94e5c2adf82a0e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:35 +0000

    [Adding helm-core version: 20181203.1303]

---
+++ elpa/helm-core-20181203.1303/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20181203.1303" "Development files for Helm"
+  '((emacs "24.4")
+    (async "1.9.3"))
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
