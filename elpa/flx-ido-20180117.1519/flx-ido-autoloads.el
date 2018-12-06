f71f507ed237cb298300bb162a92e2df9f90b5b0
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:28 +0000

    [Adding flx-ido version: 20180117.1519]

---
+++ elpa/flx-ido-20180117.1519/flx-ido-autoloads.el
@@ -0,0 +1,34 @@
+;;; flx-ido-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flx-ido" "flx-ido.el" (0 0 0 0))
+;;; Generated autoloads from flx-ido.el
+
+(defvar flx-ido-mode nil "\
+Non-nil if Flx-Ido mode is enabled.
+See the `flx-ido-mode' command
+for a description of this minor mode.")
+
+(custom-autoload 'flx-ido-mode "flx-ido" nil)
+
+(autoload 'flx-ido-mode "flx-ido" "\
+Toggle flx ido mode
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flx-ido" '("flx-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flx-ido-autoloads.el ends here
---
+++ elpa/flx-ido-20180117.1519/flx-ido-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flx-ido" "20180117.1519" "flx integration for ido" '((flx "0.1") (cl-lib "0.3")) :commit "46040d0b096a0340d91235561f27a959a61d0fef" :authors '(("Le Wang")) :maintainer '("Le Wang") :url "https://github.com/lewang/flx")
