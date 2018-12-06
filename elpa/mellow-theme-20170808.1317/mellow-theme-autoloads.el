8891d76b530fece00e45935939cf964a2876ea87
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:06 +0000

    [Adding mellow-theme version: 20170808.1317]

---
+++ elpa/mellow-theme-20170808.1317/mellow-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; mellow-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "mellow-theme" "mellow-theme.el" (0 0 0 0))
+;;; Generated autoloads from mellow-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "mellow-theme" '("mellow")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; mellow-theme-autoloads.el ends here
---
+++ elpa/mellow-theme-20170808.1317/mellow-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mellow-theme" "20170808.1317" "an Emacs 24 theme based on Mellow (tmTheme)" '((emacs "24.0")) :commit "2bdf18f05f5212b6f269d9a94afe2cf201766891" :authors '(("Jason Milkins")) :maintainer '("Jason Milkins") :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
