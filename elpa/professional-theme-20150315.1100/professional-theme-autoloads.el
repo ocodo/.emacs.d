1613676a044b33c6932bd4abc01b7824e2f8171f
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:07:49 +0000

    [Adding professional-theme version: 20150315.1100]

---
+++ elpa/professional-theme-20150315.1100/professional-theme-autoloads.el
@@ -0,0 +1,30 @@
+;;; professional-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "professional-theme" "professional-theme.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from professional-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(autoload 'professional-theme "professional-theme" "\
+Apply the professional-theme.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "professional-theme" '("professional")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; professional-theme-autoloads.el ends here
---
+++ elpa/professional-theme-20150315.1100/professional-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "professional-theme" "20150315.1100" "Emacs port of Vim's professional theme" 'nil :commit "0927d1474049a193f9f366bde5eb1887b9ba20ed" :keywords '("theme" "light" "professional") :authors '(("Juanjo Alvarez" . "juanjo@juanjoalvarez.net")) :maintainer '("Juanjo Alvarez" . "juanjo@juanjoalvarez.net") :url "https://github.com/juanjux/emacs-professional-theme")
