7c98b9179d4b458fe9a197015f8011ec1be32f36
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:56 +0000

    [Adding cbm version: 20171116.1240]

---
+++ elpa/cbm-20171116.1240/cbm-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cbm" "20171116.1240" "Switch to similar buffers." '((cl-lib "0.5")) :commit "5b41c936ba9f6d170309a85ffebc9939c1050b31" :keywords '("buffers") :authors '(("Lukas Fürmetz" . "fuermetz@mailbox.org")) :maintainer '("Lukas Fürmetz" . "fuermetz@mailbox.org") :url "http://github.com/akermu/cbm.el")
