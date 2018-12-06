8eb8355d3bc480513d5c437b87ef59161473995b
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:46 +0000

    [Adding helm-j-cheatsheet version: 20170217.829]

---
+++ elpa/helm-j-cheatsheet-20170217.829/helm-j-cheatsheet-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-j-cheatsheet" "20170217.829" "Quick J reference for Emacs" '((helm "1.5.3")) :commit "6c47e7162b9ba2de4b41221d01180146973d860b" :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/helm-j-cheatsheet")
