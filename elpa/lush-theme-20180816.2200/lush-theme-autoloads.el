a827fc9484d051e54e15fb3e01ff083cde89165b
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:55 +0000

    [Adding lush-theme version: 20180816.2200]

---
+++ elpa/lush-theme-20180816.2200/lush-theme-autoloads.el
@@ -0,0 +1,29 @@
+;;; lush-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "lush-theme" "lush-theme.el" (0 0 0 0))
+;;; Generated autoloads from lush-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(autoload 'lush-theme "lush-theme" "\
+Load lush-theme.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "lush-theme" '("lush")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lush-theme-autoloads.el ends here
---
+++ elpa/lush-theme-20180816.2200/lush-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lush-theme" "20180816.2200" "A dark theme with lush colors" '((emacs "24")) :commit "7cfc993709d712f75c51b505078608c9e1c11466" :keywords '("theme" "dark" "strong colors") :authors '(("Andre Richter" . "andre.o.richter@gmail.com")) :maintainer '("Andre Richter" . "andre.o.richter@gmail.com") :url "https://github.com/andre-richter/emacs-lush-theme")
