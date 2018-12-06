65461f926ee837e94b47041d4ea2461a7110b53b
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:04 +0000

    [Adding mbo70s-theme version: 20170808.1315]

---
+++ elpa/mbo70s-theme-20170808.1315/mbo70s-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; mbo70s-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "mbo70s-theme" "mbo70s-theme.el" (0 0 0 0))
+;;; Generated autoloads from mbo70s-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "mbo70s-theme" '("mbo70s")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; mbo70s-theme-autoloads.el ends here
---
+++ elpa/mbo70s-theme-20170808.1315/mbo70s-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mbo70s-theme" "20170808.1315" "70s style palette, with similarities to mbo theme" '((emacs "24.0")) :commit "bed3db8965708ed4e9482b224a9b084765c052f2" :authors '(("Jason Milkins")) :maintainer '("Jason Milkins") :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
