574e10ae701777efab945ffc08527a263fa39226
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:30 +0000

    [Adding helm-ack version: 20141030.1226]

---
+++ elpa/helm-ack-20141030.1226/helm-ack-autoloads.el
@@ -0,0 +1,32 @@
+;;; helm-ack-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-ack" "helm-ack.el" (0 0 0 0))
+;;; Generated autoloads from helm-ack.el
+
+(autoload 'helm-ack-pop-stack "helm-ack" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-ack "helm-ack" "\
+
+
+\(fn &optional DIR)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-ack" '("helm-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-ack-autoloads.el ends here
---
+++ elpa/helm-ack-20141030.1226/helm-ack-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-ack" "20141030.1226" "Ack command with helm interface" '((helm "1.0") (cl-lib "0.5")) :commit "889bc225318d14c6e3be80e73b1d9d6fb30e48c3" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-helm-ack")
