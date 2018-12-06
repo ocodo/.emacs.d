4b9b69dc6736560fbec2caa30caf21d58ee32dd2
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:07 +0000

    [Adding go-impl version: 20170125.1552]

---
+++ elpa/go-impl-20170125.1552/go-impl-autoloads.el
@@ -0,0 +1,27 @@
+;;; go-impl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "go-impl" "go-impl.el" (0 0 0 0))
+;;; Generated autoloads from go-impl.el
+
+(autoload 'go-impl "go-impl" "\
+
+
+\(fn RECEIVER INTERFACE)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "go-impl" '("go-impl-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-impl-autoloads.el ends here
---
+++ elpa/go-impl-20170125.1552/go-impl-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-impl" "20170125.1552" "impl integration for go-mode" '((emacs "24.3") (go-mode "1.3.0")) :commit "69f0d0ef05771487e15abec500cd06befd171abf" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-go-impl")
