49d78f0d589d8c5a8dd602d12cc1a12492d6500f
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:36 +0000

    [Adding dired-rsync version: 20180906.1002]

---
+++ elpa/dired-rsync-20180906.1002/dired-rsync-autoloads.el
@@ -0,0 +1,34 @@
+;;; dired-rsync-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "dired-rsync" "dired-rsync.el" (0 0 0 0))
+;;; Generated autoloads from dired-rsync.el
+
+(autoload 'dired-rsync "dired-rsync" "\
+Asynchronously copy files in dired to `DEST' using rsync.
+
+`DEST' can be a relative filename and will be processed by
+`expand-file-name' before being passed to the rsync command.
+
+This function runs the copy asynchronously so Emacs won't block whilst
+the copy is running.  It also handles both source and destinations on
+ssh/scp tramp connections.
+
+\(fn DEST)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "dired-rsync" '("dired-rsync-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dired-rsync-autoloads.el ends here
---
+++ elpa/dired-rsync-20180906.1002/dired-rsync-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-rsync" "20180906.1002" "Allow rsync from dired buffers" '((s "1.12.0") (dash "2.0.0") (emacs "24")) :commit "e112bf22c913d1491bb2149250be866ceb1806ff" :authors '(("Alex Bennée" . "alex@bennee.com")) :maintainer '("Alex Bennée" . "alex@bennee.com") :url "https://github.com/stsquad/dired-rsync")
