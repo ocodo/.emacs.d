d84ca00545fa5d466bd47bbca213ef58128fc3d6
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:32 +0000

    [Adding helm-aws version: 20180514.1032]

---
+++ elpa/helm-aws-20180514.1032/helm-aws-autoloads.el
@@ -0,0 +1,27 @@
+;;; helm-aws-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-aws" "helm-aws.el" (0 0 0 0))
+;;; Generated autoloads from helm-aws.el
+
+(autoload 'helm-aws "helm-aws" "\
+Show helm with a table of aws information.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-aws" '("aws-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-aws-autoloads.el ends here
---
+++ elpa/helm-aws-20180514.1032/helm-aws-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-aws" "20180514.1032" "Manage AWS EC2 server instances directly from Emacs" '((helm "1.5.3") (cl-lib "0.5") (s "1.9.0")) :commit "b36c744b3f00f458635a91d1f5158fccbb5baef6" :authors '(("istib")) :maintainer '("istib") :url "https://github.com/istib/helm-aws")
