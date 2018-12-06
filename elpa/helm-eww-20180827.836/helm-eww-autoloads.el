1a7ad0f4ed38329808dc68c34ffa82afe74b07f2
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:39 +0000

    [Adding helm-eww version: 20180827.836]

---
+++ elpa/helm-eww-20180827.836/helm-eww-autoloads.el
@@ -0,0 +1,42 @@
+;;; helm-eww-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-eww" "helm-eww.el" (0 0 0 0))
+;;; Generated autoloads from helm-eww.el
+
+(autoload 'helm-eww-buffers "helm-eww" "\
+Preconfigured `helm' to list EWW buffers.
+
+\(fn)" t nil)
+
+(autoload 'helm-eww-bookmarks "helm-eww" "\
+Preconfigured `helm' to list EWW bookmarks.
+
+\(fn)" t nil)
+
+(autoload 'helm-eww-history "helm-eww" "\
+Preconfigured `helm' to list EWW history.
+
+\(fn)" t nil)
+
+(autoload 'helm-eww "helm-eww" "\
+Preconfigured `helm' to list all EWW sources.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-eww" '("helm-eww-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-eww-autoloads.el ends here
