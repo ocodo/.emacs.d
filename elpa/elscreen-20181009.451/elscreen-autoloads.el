c3fbeda86ac48552f0f18878a4b5c9c425ecec92
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:55 +0000

    [Adding elscreen version: 20181009.451]

---
+++ elpa/elscreen-20181009.451/elscreen-autoloads.el
@@ -0,0 +1,113 @@
+;;; elscreen-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "elscreen" "elscreen.el" (0 0 0 0))
+;;; Generated autoloads from elscreen.el
+
+(autoload 'elscreen-start "elscreen" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen" '("elscreen-" "lookup-key")))
+
+;;;***
+
+;;;### (autoloads nil "elscreen-buffer-list" "elscreen-buffer-list.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from elscreen-buffer-list.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen-buffer-list" '("toggle-elscreen-buffer-list" "elscreen-" "reorder-buffer-list" "buffer-dead-p")))
+
+;;;***
+
+;;;### (autoloads nil "elscreen-color-theme" "elscreen-color-theme.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from elscreen-color-theme.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen-color-theme" '("elscreen-color-theme-")))
+
+;;;***
+
+;;;### (autoloads nil "elscreen-dired" "elscreen-dired.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from elscreen-dired.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen-dired" '("elscreen-dired-version")))
+
+;;;***
+
+;;;### (autoloads nil "elscreen-dnd" "elscreen-dnd.el" (0 0 0 0))
+;;; Generated autoloads from elscreen-dnd.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen-dnd" '("elscreen-dnd-")))
+
+;;;***
+
+;;;### (autoloads nil "elscreen-gf" "elscreen-gf.el" (0 0 0 0))
+;;; Generated autoloads from elscreen-gf.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen-gf" '("elscreen-gf-")))
+
+;;;***
+
+;;;### (autoloads nil "elscreen-goby" "elscreen-goby.el" (0 0 0 0))
+;;; Generated autoloads from elscreen-goby.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen-goby" '("elscreen-goby-version")))
+
+;;;***
+
+;;;### (autoloads nil "elscreen-howm" "elscreen-howm.el" (0 0 0 0))
+;;; Generated autoloads from elscreen-howm.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen-howm" '("howm-save-and-kill-buffer/screen" "elscreen-howm-")))
+
+;;;***
+
+;;;### (autoloads nil "elscreen-server" "elscreen-server.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from elscreen-server.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen-server" '("elscreen-server-")))
+
+;;;***
+
+;;;### (autoloads nil "elscreen-speedbar" "elscreen-speedbar.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from elscreen-speedbar.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen-speedbar" '("elscreen-speedbar-")))
+
+;;;***
+
+;;;### (autoloads nil "elscreen-w3m" "elscreen-w3m.el" (0 0 0 0))
+;;; Generated autoloads from elscreen-w3m.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen-w3m" '("elscreen-w3m-")))
+
+;;;***
+
+;;;### (autoloads nil "elscreen-wl" "elscreen-wl.el" (0 0 0 0))
+;;; Generated autoloads from elscreen-wl.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elscreen-wl" '("elscreen-wl-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("elscreen-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; elscreen-autoloads.el ends here
---
+++ elpa/elscreen-20181009.451/elscreen-pkg.el
@@ -0,0 +1,12 @@
+(define-package "elscreen" "20181009.451" "Emacs window session manager"
+  '((emacs "24"))
+  :keywords
+  '("window" "convenience")
+  :authors
+  '(("Naoto Morishima" . "naoto@morishima.net"))
+  :maintainer
+  '("Akinori MUSHA" . "knu@iDaemons.org")
+  :url "https://github.com/knu/elscreen")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
