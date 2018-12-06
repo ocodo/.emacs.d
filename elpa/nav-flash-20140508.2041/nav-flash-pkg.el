08b3237e2385506ec7f5f1bae9054c91da2ba826
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:16 +0000

    [Adding nav-flash version: 20140508.2041]

---
+++ elpa/nav-flash-20140508.2041/nav-flash-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "nav-flash" "20140508.2041" "Briefly highlight the current line" 'nil :commit "53f5bc59e3f32c1192d15637d3979732dacb2c35" :keywords '("extensions" "navigation" "interface") :authors '(("Roland Walker" . "walker@pobox.com")) :maintainer '("Roland Walker" . "walker@pobox.com") :url "http://github.com/rolandwalker/nav-flash")
