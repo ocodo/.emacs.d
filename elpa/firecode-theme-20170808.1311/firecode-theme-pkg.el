fbba1539f96e901f028ab31bd4c5a42513844f19
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:24 +0000

    [Adding firecode-theme version: 20170808.1311]

---
+++ elpa/firecode-theme-20170808.1311/firecode-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "firecode-theme" "20170808.1311" "an Emacs 24 theme based on FireCode (tmTheme)" '((emacs "24.0")) :commit "8b7b03ecdd41e70dab145b98906017e1392eaef4" :authors '(("Jason Milkins")) :maintainer '("Jason Milkins") :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
