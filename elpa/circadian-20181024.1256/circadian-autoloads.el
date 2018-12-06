7726558e0e6655b8316bffd525a1578268826e11
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:58 +0000

    [Adding circadian version: 20181024.1256]

---
+++ elpa/circadian-20181024.1256/circadian-autoloads.el
@@ -0,0 +1,27 @@
+;;; circadian-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "circadian" "circadian.el" (0 0 0 0))
+;;; Generated autoloads from circadian.el
+
+(autoload 'circadian-setup "circadian" "\
+Setup circadian based on `circadian-themes'.
+
+\(fn)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "circadian" '("circadian-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; circadian-autoloads.el ends here
---
+++ elpa/circadian-20181024.1256/circadian-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "circadian" "20181024.1256" "Theme-switching based on daytime" '((emacs "24.4")) :commit "414127acad8e2e0092ca60918e6a7cb89da6e28a" :keywords '("themes") :authors '(("Guido Schmidt")) :maintainer '("Guido Schmidt" . "git@guidoschmidt.cc") :url "https://github.com/GuidoSchmidt/circadian")
