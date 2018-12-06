25d078cb155429ec3e675bdaad88be7a288a68f2
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:26 +0000

    [Adding flatui-theme version: 20160619.127]

---
+++ elpa/flatui-theme-20160619.127/flatui-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; flatui-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flatui-theme" "flatui-theme.el" (0 0 0 0))
+;;; Generated autoloads from flatui-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flatui-theme" '("flatui")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flatui-theme-autoloads.el ends here
---
+++ elpa/flatui-theme-20160619.127/flatui-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flatui-theme" "20160619.127" "A color theme for Emacs based on flatuicolors.com" 'nil :commit "9c15db5526c15c8dba55023f5698372b19c2a780" :authors '(("John Louis Del Rosario" . "john2x@gmail.com")) :maintainer '("John Louis Del Rosario" . "john2x@gmail.com") :url "https://github.com/john2x/flatui-theme.el")
