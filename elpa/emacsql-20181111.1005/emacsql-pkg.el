8b4ea94b9c5383738abd403e1f0a280ed83c921a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:55 +0000

    [Adding emacsql version: 20181111.1005]

---
+++ elpa/emacsql-20181111.1005/emacsql-pkg.el
@@ -0,0 +1,10 @@
+(define-package "emacsql" "20181111.1005" "high-level SQL database front-end"
+  '((emacs "25.1"))
+  :authors
+  '(("Christopher Wellons" . "wellons@nullprogram.com"))
+  :maintainer
+  '("Christopher Wellons" . "wellons@nullprogram.com")
+  :url "https://github.com/skeeto/emacsql")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
