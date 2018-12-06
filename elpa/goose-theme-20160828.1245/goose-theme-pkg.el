950524c7447b9040eba263772736ad4a666a8b56
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:15 +0000

    [Adding goose-theme version: 20160828.1245]

---
+++ elpa/goose-theme-20160828.1245/goose-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "goose-theme" "20160828.1245" "A gray color theme" '((emacs "24.1")) :commit "acd017b50ab25a75fd1331eb3de66467e2042e9c" :authors '(("Stephen Whipple" . "shw@wicdmedia.org")) :maintainer '("Stephen Whipple" . "shw@wicdmedia.org") :url "https://github.com/thwg/goose-theme")
