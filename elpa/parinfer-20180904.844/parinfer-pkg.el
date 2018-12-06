aab0fbda8f94c460f109defb8dd81906f7ac9b33
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:33 +0000

    [Adding parinfer version: 20180904.844]

---
+++ elpa/parinfer-20180904.844/parinfer-pkg.el
@@ -0,0 +1,13 @@
+(define-package "parinfer" "20180904.844" "Simpler Lisp editing"
+  '((dash "2.13.0")
+    (cl-lib "0.5"))
+  :keywords
+  '("parinfer")
+  :authors
+  '(("Shi Tianshu"))
+  :maintainer
+  '("Shi Tianshu")
+  :url "https://github.com/DogLooksGood/parinfer-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
