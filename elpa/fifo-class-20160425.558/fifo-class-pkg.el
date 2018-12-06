354815038c2ded403f2b9bde076bf64cc9544849
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:20 +0000

    [Adding fifo-class version: 20160425.558]

---
+++ elpa/fifo-class-20160425.558/fifo-class-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fifo-class" "20160425.558" "First in first out abstract class" 'nil :commit "8fe4cf690727f4ac7b67f29c55f845df023c3f21" :keywords '("lisp") :authors '(("Mola-T" . "Mola@molamola.xyz")) :maintainer '("Mola-T" . "Mola@molamola.xyz") :url "https://github.com/mola-T/fifo-class")
