18c333bcb3dd20402e0e4b57e23caab4970833e4
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:25 +0000

    [Updating angular-mode version: 20151201.2127][removing old version: 20151201.1327]

--- elpa/angular-mode-20151201.1327/angular-mode-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "angular-mode" "20151201.1327" "Major mode for Angular.js" 'nil :url "https://github.com/omouse/angularjs-mode" :keywords
-  '("languages" "javascript"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/angular-mode-20151201.2127/angular-mode-autoloads.el
+++ elpa/angular-mode-20151201.2127/angular-mode-autoloads.el
@@ -1,10 +1,13 @@
 ;;; angular-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
 ;;;### (autoloads nil "angular-html-mode" "angular-html-mode.el"
-;;;;;;  (22955 52948 512786 707000))
+;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from angular-html-mode.el
 
 (autoload 'angular-html-mode "angular-html-mode" "\
@@ -13,10 +16,11 @@ Major HTML mode for AngularJS.
 
 \(fn)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "angular-html-mode" '("angular-html-font-lock-keywords")))
+
 ;;;***
 
-;;;### (autoloads nil "angular-mode" "angular-mode.el" (22955 52948
-;;;;;;  540786 786000))
+;;;### (autoloads nil "angular-mode" "angular-mode.el" (0 0 0 0))
 ;;; Generated autoloads from angular-mode.el
 
 (autoload 'angular-mode "angular-mode" "\
@@ -25,10 +29,11 @@ Major mode for AngularJS.
 
 \(fn)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "angular-mode" '("angular-")))
+
 ;;;***
 
-;;;### (autoloads nil nil ("angular-mode-pkg.el") (22955 52948 540786
-;;;;;;  786000))
+;;;### (autoloads nil nil ("angular-mode-pkg.el") (0 0 0 0))
 
 ;;;***
 
@@ -36,5 +41,6 @@ Major mode for AngularJS.
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; angular-mode-autoloads.el ends here
