0589b2cb75c7e5150774ee9677e3c5a28720de76
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:57 +0000

    [Adding hide-lines version: 20151127.1840]

---
+++ elpa/hide-lines-20151127.1840/hide-lines-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "hide-lines" "20151127.1840" "Commands for hiding lines based on a regexp" 'nil :commit "331122bf19361130351cfe55968c2a7820329eb3" :keywords '("convenience") :authors '(("Mark Hulme-Jones <ture at plig cucumber dot net>")) :maintainer '("Joe Bloggs" . "vapniks@yahoo.com") :url "https://github.com/vapniks/hide-lines")
