8acb1cd9ff2434aa0ed328c5a4b4ef58e17d86d7
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:28 +0000

    [Adding jekyll-modes version: 20141117.1314]

---
+++ elpa/jekyll-modes-20141117.1314/jekyll-modes-autoloads.el
@@ -0,0 +1,29 @@
+;;; jekyll-modes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "jekyll-modes" "jekyll-modes.el" (0 0 0 0))
+;;; Generated autoloads from jekyll-modes.el
+
+(autoload 'liquid-tags-mode "jekyll-modes" "\
+Major mode for Liquid Tags
+
+\(fn)" t nil)
+ (autoload 'jekyll-markdown-mode "jekyll-modes")
+ (autoload 'jekyll-html-mode "jekyll-modes")
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "jekyll-modes" '("jekyll" "liquid-tags-font-lock-keywords")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; jekyll-modes-autoloads.el ends here
---
+++ elpa/jekyll-modes-20141117.1314/jekyll-modes-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "jekyll-modes" "20141117.1314" "Major modes (markdown and HTML) for authoring Jekyll content" '((polymode "0.2")) :commit "7cb10b50fd2883e3f7b10fdfd98f19f2f0b2381c" :keywords '("docs") :authors '(("Fredrik Appelberg" . "fredrik@milgrim.local")) :maintainer '("Fredrik Appelberg" . "fredrik@milgrim.local") :url "https://github.com/fred-o/jekyll-modes")
