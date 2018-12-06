34ac660f4448444b3dac1624e7976b5b1c7b1bf1
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:37 +0000

    [Adding zoutline version: 20180314.1759]

---
+++ elpa/zoutline-20180314.1759/zoutline-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zoutline" "20180314.1759" "Simple outline library." 'nil :commit "b3ee0f0e0b916838c2d2c249beba74ffdb8d5699" :keywords '("outline") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/zoutline")
