141f74f3e2184f65291fb55fce28349b3e0d7d85
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:11 +0000

    [Adding splitjoin version: 20150505.1432]

---
+++ elpa/splitjoin-20150505.1432/splitjoin-autoloads.el
@@ -0,0 +1,27 @@
+;;; splitjoin-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "splitjoin" "splitjoin.el" (0 0 0 0))
+;;; Generated autoloads from splitjoin.el
+
+(autoload 'splitjoin "splitjoin" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "splitjoin" '("splitjoin--")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; splitjoin-autoloads.el ends here
---
+++ elpa/splitjoin-20150505.1432/splitjoin-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "splitjoin" "20150505.1432" "Transition between multiline and single-line code" '((cl-lib "0.5")) :commit "e2945ee269e6e90f0243d6f2a33e067bb0a2873c" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-splitjoin")
