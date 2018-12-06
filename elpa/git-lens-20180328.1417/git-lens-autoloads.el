62eeba7dec572e1c34256d37ff78bc6b8d5889f2
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:55 +0000

    [Adding git-lens version: 20180328.1417]

---
+++ elpa/git-lens-20180328.1417/git-lens-autoloads.el
@@ -0,0 +1,27 @@
+;;; git-lens-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "git-lens" "git-lens.el" (0 0 0 0))
+;;; Generated autoloads from git-lens.el
+
+(autoload 'git-lens "git-lens" "\
+Start git lens.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "git-lens" '("git-lens-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; git-lens-autoloads.el ends here
