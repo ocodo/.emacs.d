53174617f8fe0538242e7d95adb9cea5b4224f83
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:55 +0000

    [Adding git-gutter-fringe+ version: 20140729.1103]

---
+++ elpa/git-gutter-fringe+-20140729.1103/git-gutter-fringe+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-gutter-fringe+" "20140729.1103" "Fringe version of git-gutter+.el" '((git-gutter+ "0.1") (fringe-helper "1.0.1")) :commit "7a2f49d2455a3a872e90e5f7dd4e6b27f1d96cfc" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/nonsequitur/git-gutter-fringe-plus")
