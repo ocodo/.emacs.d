af35621c04188e24017926c76f6749ec428a4967
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:29 +0000

    [Adding ruby-interpolation version: 20131112.1652]

---
+++ elpa/ruby-interpolation-20131112.1652/ruby-interpolation-autoloads.el
@@ -0,0 +1,28 @@
+;;; ruby-interpolation-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ruby-interpolation" "ruby-interpolation.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ruby-interpolation.el
+
+(autoload 'ruby-interpolation-mode "ruby-interpolation" "\
+Automatic insertion of ruby string interpolation.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ruby-interpolation" '("ruby-interpolation-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ruby-interpolation-autoloads.el ends here
---
+++ elpa/ruby-interpolation-20131112.1652/ruby-interpolation-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ruby-interpolation" "20131112.1652" "Ruby string interpolation helpers" 'nil :commit "1978e337601222cedf00e117bf4b5cac15d1f203" :authors '(("Arthur Leonard Andersen" . "leoc.git@gmail.com")) :maintainer '("Arthur Leonard Andersen" . "leoc.git@gmail.com") :url "http://github.com/leoc/ruby-interpolation.el")
