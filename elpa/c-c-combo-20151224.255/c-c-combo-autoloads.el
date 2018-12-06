94d83284152bbd9f7e0bfec81aac170673df8194
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:54 +0000

    [Adding c-c-combo version: 20151224.255]

---
+++ elpa/c-c-combo-20151224.255/c-c-combo-autoloads.el
@@ -0,0 +1,41 @@
+;;; c-c-combo-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "c-c-combo" "c-c-combo.el" (0 0 0 0))
+;;; Generated autoloads from c-c-combo.el
+
+(defvar c-c-combo-mode nil "\
+Non-nil if C-C-Combo mode is enabled.
+See the `c-c-combo-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `c-c-combo-mode'.")
+
+(custom-autoload 'c-c-combo-mode "c-c-combo" nil)
+
+(autoload 'c-c-combo-mode "c-c-combo" "\
+c-c-combo
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "c-c-combo" '("c-c-combo-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("c-c-combo-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; c-c-combo-autoloads.el ends here
---
+++ elpa/c-c-combo-20151224.255/c-c-combo-pkg.el
@@ -0,0 +1,8 @@
+(define-package "c-c-combo" "20151224.255" "Make stuff happen when you reach a target wpm" 'nil :authors
+  '(("Diego Berrocal" . "cestdiego@gmail.com"))
+  :maintainer
+  '("Diego Berrocal" . "cestdiego@gmail.com")
+  :url "https://www.github.com/CestDiego/c-c-combo.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
