33bb7a7e041bc18efa8e134320a17d816c3102fb
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:13 +0000

    [Adding mustache version: 20170923.1233]

---
+++ elpa/mustache-20170923.1233/mustache-autoloads.el
@@ -0,0 +1,49 @@
+;;; mustache-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "mustache" "mustache.el" (0 0 0 0))
+;;; Generated autoloads from mustache.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "mustache" '("mustache-")))
+
+;;;***
+
+;;;### (autoloads nil "mustache-lex" "mustache-lex.el" (0 0 0 0))
+;;; Generated autoloads from mustache-lex.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "mustache-lex" '("mst--")))
+
+;;;***
+
+;;;### (autoloads nil "mustache-parse" "mustache-parse.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from mustache-parse.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "mustache-parse" '("mst--")))
+
+;;;***
+
+;;;### (autoloads nil "mustache-render" "mustache-render.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from mustache-render.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "mustache-render" '("mst--")))
+
+;;;***
+
+;;;### (autoloads nil nil ("mustache-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; mustache-autoloads.el ends here
---
+++ elpa/mustache-20170923.1233/mustache-pkg.el
@@ -0,0 +1,7 @@
+(define-package "mustache" "20170923.1233" "a mustache templating library in emacs lisp"
+  '((ht "0.9")
+    (s "1.3.0")
+    (dash "1.2.0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
