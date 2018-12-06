104c27f6449931a79bc6cff8bb6ff5accd199b16
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:19 +0000

    [Adding fancy-battery version: 20150101.1204]

---
+++ elpa/fancy-battery-20150101.1204/fancy-battery-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fancy-battery" "20150101.1204" "Fancy battery display" '((emacs "24.1")) :commit "9b88ae77a01aa3edc529840338bcb2db7f445822" :keywords '("convenience" "tools" "hardware") :authors '(("Sebastian Wiesner" . "swiesner@lunaryorn.com")) :maintainer '("Sebastian Wiesner" . "swiesner@lunaryorn.com") :url "https://github.com/lunaryorn/fancy-battery.el")
