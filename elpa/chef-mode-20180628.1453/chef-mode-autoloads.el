f02c2dd726a886234f81bed8b40f827be2581043
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:57 +0000

    [Adding chef-mode version: 20180628.1453]

---
+++ elpa/chef-mode-20180628.1453/chef-mode-autoloads.el
@@ -0,0 +1,22 @@
+;;; chef-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "chef-mode" "chef-mode.el" (0 0 0 0))
+;;; Generated autoloads from chef-mode.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "chef-mode" '("chef" "knife" "find-chef-root" "global-chef-mode" "turn-on-chef-mode")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; chef-mode-autoloads.el ends here
---
+++ elpa/chef-mode-20180628.1453/chef-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "chef-mode" "20180628.1453" "minor mode for editing an opscode chef repository" 'nil :commit "048d691cb63981ae235763d4a6ced4af5c729924" :keywords '("chef" "knife") :authors '(("Maciej Pasternacki" . "maciej@pasternacki.net")) :maintainer '("Maciej Pasternacki" . "maciej@pasternacki.net"))
