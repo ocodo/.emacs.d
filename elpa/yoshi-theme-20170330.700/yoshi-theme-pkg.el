2ec98f8a1f2ebfd338aaa7aaa799720925245d8a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:32 +0000

    [Adding yoshi-theme version: 20170330.700]

---
+++ elpa/yoshi-theme-20170330.700/yoshi-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yoshi-theme" "20170330.700" "Theme named after my cat" 'nil :commit "eab4fb67e8fec47c25c4c1393ac1211f043dbd44" :keywords '("faces") :authors '(("Tom Willemse" . "tom@ryuslash.org")) :maintainer '("Tom Willemse" . "tom@ryuslash.org") :url "http://projects.ryuslash.org/yoshi-theme/")
