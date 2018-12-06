e8797276aa5a47278354c54e9ce29dab86268306
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:21 +0000

    [Adding csv version: 20161113.1510]

---
+++ elpa/csv-20161113.1510/csv-autoloads.el
@@ -0,0 +1,22 @@
+;;; csv-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "csv" "csv.el" (0 0 0 0))
+;;; Generated autoloads from csv.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "csv" '("csv-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; csv-autoloads.el ends here
---
+++ elpa/csv-20161113.1510/csv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "csv" "20161113.1510" "Functions for reading and parsing CSV files." 'nil :commit "aa1dfa1263565d5fac3879c21d8ddf5f8915e411" :keywords '("extensions" "data" "csv") :authors '(("Ulf Jasper" . "ulf.jasper@web.de")) :maintainer '("Ulf Jasper" . "ulf.jasper@web.de"))
