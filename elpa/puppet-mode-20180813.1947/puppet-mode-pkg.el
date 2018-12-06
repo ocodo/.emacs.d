948bb569c351531fd0c9dcea92564609e492693f
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:14 +0000

    [Adding puppet-mode version: 20180813.1947]

---
+++ elpa/puppet-mode-20180813.1947/puppet-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "puppet-mode" "20180813.1947" "Major mode for Puppet manifests" '((emacs "24.1") (pkg-info "0.4")) :commit "7dee1b5a5debac6e56f9107492a413b6c0edb94d" :keywords '("languages") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com") ("Sebastian Wiesner" . "swiesner@lunaryorn.com") ("Russ Allbery" . "rra@stanford.edu")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/voxpupuli/puppet-mode")
