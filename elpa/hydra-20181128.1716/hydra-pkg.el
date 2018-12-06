4484446ab8aac9da389895bdbf2b9b4afa244995
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:09 +0000

    [Adding hydra version: 20181128.1716]

---
+++ elpa/hydra-20181128.1716/hydra-pkg.el
@@ -0,0 +1,12 @@
+(define-package "hydra" "20181128.1716" "Make bindings that stick around."
+  '((cl-lib "0.5"))
+  :keywords
+  '("bindings")
+  :authors
+  '(("Oleh Krehel" . "ohwoeowho@gmail.com"))
+  :maintainer
+  '("Oleh Krehel" . "ohwoeowho@gmail.com")
+  :url "https://github.com/abo-abo/hydra")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
