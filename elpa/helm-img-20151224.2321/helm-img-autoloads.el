e2cf53ec7cbee43102367e86e789946878fafd09
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:44 +0000

    [Adding helm-img version: 20151224.2321]

---
+++ elpa/helm-img-20151224.2321/helm-img-autoloads.el
@@ -0,0 +1,50 @@
+;;; helm-img-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-img" "helm-img.el" (0 0 0 0))
+;;; Generated autoloads from helm-img.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-img" '("helm-img-")))
+
+;;;***
+
+;;;### (autoloads nil "helm-img-bing" "helm-img-bing.el" (0 0 0 0))
+;;; Generated autoloads from helm-img-bing.el
+
+(autoload 'helm-img-bing "helm-img-bing" "\
+
+
+\(fn QUERY)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-img-bing" '("helm-img-bing-")))
+
+;;;***
+
+;;;### (autoloads nil "helm-img-dir" "helm-img-dir.el" (0 0 0 0))
+;;; Generated autoloads from helm-img-dir.el
+
+(autoload 'helm-img-dir "helm-img-dir" "\
+
+
+\(fn DIR)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-img-dir" '("helm-img-dir-make-candidates")))
+
+;;;***
+
+;;;### (autoloads nil nil ("helm-img-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-img-autoloads.el ends here
---
+++ elpa/helm-img-20151224.2321/helm-img-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm-img" "20151224.2321" "Utilities for making image sources for helm."
+  '((helm "1.7.7")
+    (cl-lib "0.5"))
+  :keywords
+  '("convenience")
+  :authors
+  '(("Sho Matsumoto <l3msh0_at_gmail.com>"))
+  :maintainer
+  '("l3msh0")
+  :url "https://github.com/l3msh0/helm-img")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
