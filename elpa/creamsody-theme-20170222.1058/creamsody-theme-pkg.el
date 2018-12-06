b7ee54e2dc47e370c3e2fd5e82fb83fdb98fe9f0
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:19 +0000

    [Adding creamsody-theme version: 20170222.1058]

---
+++ elpa/creamsody-theme-20170222.1058/creamsody-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "creamsody-theme" "20170222.1058" "Straight from the soda fountain." '((autothemer "0.2")) :commit "32fa3f4e461da92700523b1b20e7b28974c19a26" :url "http://github.com/emacsfodder/emacs-theme-creamsody")
