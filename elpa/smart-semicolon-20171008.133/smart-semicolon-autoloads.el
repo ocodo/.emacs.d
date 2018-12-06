ce72db829bec3ad219a974a1ed43b5fc98e1d5b7
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:04 +0000

    [Adding smart-semicolon version: 20171008.133]

---
+++ elpa/smart-semicolon-20171008.133/smart-semicolon-autoloads.el
@@ -0,0 +1,28 @@
+;;; smart-semicolon-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "smart-semicolon" "smart-semicolon.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from smart-semicolon.el
+
+(autoload 'smart-semicolon-mode "smart-semicolon" "\
+Minor mode to insert semicolon smartly.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "smart-semicolon" '("smart-semicolon-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smart-semicolon-autoloads.el ends here
---
+++ elpa/smart-semicolon-20171008.133/smart-semicolon-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smart-semicolon" "20171008.133" "Insert semicolon smartly" '((emacs "25")) :commit "bcea2aa37befa40abf8b24a2d2314904e6df43b3" :authors '(("Iku Iwasa" . "iku.iwasa@gmail.com")) :maintainer '("Iku Iwasa" . "iku.iwasa@gmail.com") :url "https://github.com/iquiw/smart-semicolon")
