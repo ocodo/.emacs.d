6d1de4b3661358c52a1c0b263aff62c9b90a1f0b
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:11 +0000

    [Adding evil-paredit version: 20150413.2048]

---
+++ elpa/evil-paredit-20150413.2048/evil-paredit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-paredit" "20150413.2048" "Paredit support for evil keybindings" '((evil "1.0.9") (paredit "25beta")) :commit "e058fbdcf9dbf7ad6cc77f0172d7517ef233d55f" :keywords '("paredit" "evil") :authors '(("Roman Gonzalez" . "romanandreg@gmail.com")) :maintainer '("Roman Gonzalez" . "romanandreg@gmail.com") :url "https://github.com/roman/evil-paredit")
