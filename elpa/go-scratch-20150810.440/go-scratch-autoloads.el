69672012c34fa82e3bdf4f4f1265ff4c660ed1fc
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:10 +0000

    [Adding go-scratch version: 20150810.440]

---
+++ elpa/go-scratch-20150810.440/go-scratch-autoloads.el
@@ -0,0 +1,27 @@
+;;; go-scratch-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "go-scratch" "go-scratch.el" (0 0 0 0))
+;;; Generated autoloads from go-scratch.el
+
+(autoload 'go-scratch "go-scratch" "\
+Go to the Go scratch buffer.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "go-scratch" '("go-scratch-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-scratch-autoloads.el ends here
---
+++ elpa/go-scratch-20150810.440/go-scratch-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-scratch" "20150810.440" "*scratch* buffer for Go" '((go-mode "1.3.1") (emacs "24")) :commit "3f68cbcce04f59eb8e83af109164731ec0454be0" :keywords '("languages" "go") :authors '(("Emanuel Evans" . "mail@emanuel.industries")) :maintainer '("Emanuel Evans" . "mail@emanuel.industries"))
