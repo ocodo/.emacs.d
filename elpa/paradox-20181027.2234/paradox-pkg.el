62de6d348a57e1406b6e51e01e25de8b691856e8
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:31 +0000

    [Adding paradox version: 20181027.2234]

---
+++ elpa/paradox-20181027.2234/paradox-pkg.el
@@ -0,0 +1,16 @@
+(define-package "paradox" "20181027.2234" "A modern Packages Menu. Colored, with package ratings, and customizable."
+  '((emacs "24.4")
+    (seq "1.7")
+    (let-alist "1.0.3")
+    (spinner "1.7.3")
+    (hydra "0.13.2"))
+  :keywords
+  '("package" "packages")
+  :authors
+  '(("Artur Malabarba" . "emacs@endlessparentheses.com"))
+  :maintainer
+  '("Artur Malabarba" . "emacs@endlessparentheses.com")
+  :url "https://github.com/Malabarba/paradox")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
