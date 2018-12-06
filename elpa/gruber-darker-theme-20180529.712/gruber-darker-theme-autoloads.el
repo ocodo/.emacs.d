6f6c3a7d5b2f810ffa4543f957c004bd0c8a64ad
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:23 +0000

    [Adding gruber-darker-theme version: 20180529.712]

---
+++ elpa/gruber-darker-theme-20180529.712/gruber-darker-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; gruber-darker-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "gruber-darker-theme" "gruber-darker-theme.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from gruber-darker-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "gruber-darker-theme" '("gruber-darker")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; gruber-darker-theme-autoloads.el ends here
---
+++ elpa/gruber-darker-theme-20180529.712/gruber-darker-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gruber-darker-theme" "20180529.712" "Gruber Darker color theme for Emacs 24." 'nil :commit "c7687ec0511941db1371dcd70b31061d74aa5668" :authors '(("Alexey Kutepov" . "reximkut@gmail.com")) :maintainer '("Alexey Kutepov" . "reximkut@gmail.com") :url "http://github.com/rexim/gruber-darker-theme")
