4189fee3d5e9af2913465603fc95884946d4df4a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:06 +0000

    [Adding colormaps version: 20171008.2224]

---
+++ elpa/colormaps-20171008.2224/colormaps-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "colormaps" "20171008.2224" "Hex colormaps" '((emacs "25")) :commit "19fbb64a6288d505b9cf45c9b5a3eed0bfb135e2" :keywords '("tools") :authors '(("Abhinav Tushar" . "lepisma@fastmail.com")) :maintainer '("Abhinav Tushar" . "lepisma@fastmail.com") :url "https://github.com/lepisma/colormaps.el")
