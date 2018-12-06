545fe996b107e84a00e56ae0fd18d55abbd17e14
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:42 +0000

    [Adding kotlin-mode version: 20181122.422]

---
+++ elpa/kotlin-mode-20181122.422/kotlin-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kotlin-mode" "20181122.422" "Major mode for kotlin" '((emacs "24.3")) :commit "002dd1497ce7fb7266729cb2c9284cc945988411" :keywords '("languages") :authors '(("Shodai Yokoyama" . "quantumcars@gmail.com")) :maintainer '("Shodai Yokoyama" . "quantumcars@gmail.com"))
