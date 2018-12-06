1795d5704cf2598ffd0be8443790da46977cb3bf
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:36 +0000

    [Adding flymake-easy version: 20140818.755]

---
+++ elpa/flymake-easy-20140818.755/flymake-easy-autoloads.el
@@ -0,0 +1,22 @@
+;;; flymake-easy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-easy" "flymake-easy.el" (0 0 0 0))
+;;; Generated autoloads from flymake-easy.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-easy" '("flymake-easy-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-easy-autoloads.el ends here
---
+++ elpa/flymake-easy-20140818.755/flymake-easy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-easy" "20140818.755" "Helpers for easily building flymake checkers" 'nil :commit "de41ea49503f71f997e5c359a2ad08df696c0147" :keywords '("convenience" "internal") :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/flymake-easy")
