06af68cd46e35c5d8469c861eb5b044ed850b9a3
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:19 +0000

    [Adding grails version: 20160417.636]

---
+++ elpa/grails-20160417.636/grails-autoloads.el
@@ -0,0 +1,32 @@
+;;; grails-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "grails" "grails.el" (0 0 0 0))
+;;; Generated autoloads from grails.el
+
+(autoload 'grails "grails" "\
+Grails minor mode.
+     With no argument, this command toggles the mode.
+     Non-null prefix argument turns on the mode.
+     Null prefix argument turns off the mode.
+     When Grails minor mode is enabled you have some
+     shortcut to fast navigate a Grails project.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "grails" '("grails-" "util-string-from-file")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; grails-autoloads.el ends here
