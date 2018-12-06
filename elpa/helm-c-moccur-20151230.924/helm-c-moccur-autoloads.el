402578efe21caf9e196e5b51232051849995da87
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:33 +0000

    [Adding helm-c-moccur version: 20151230.924]

---
+++ elpa/helm-c-moccur-20151230.924/helm-c-moccur-autoloads.el
@@ -0,0 +1,22 @@
+;;; helm-c-moccur-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-c-moccur" "helm-c-moccur.el" (0 0 0 0))
+;;; Generated autoloads from helm-c-moccur.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-c-moccur" '("helm-c-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-c-moccur-autoloads.el ends here
---
+++ elpa/helm-c-moccur-20151230.924/helm-c-moccur-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-c-moccur" "20151230.924" "helm source for color-moccur.el" '((helm "20120811") (color-moccur "2.71")) :commit "b0a906f85fa352db091f88b91a9c510de607dfe9" :keywords '("convenience" "emulation") :authors '(("Kenji.I (Kenji Imakado)" . "ken.imakaado@gmail.com")) :maintainer '("Kenji.I (Kenji Imakado)" . "ken.imakaado@gmail.com"))
