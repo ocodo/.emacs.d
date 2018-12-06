70389eb1e011c56b301d747733bdf810ed7d8771
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:42 +0000

    [Adding flymake-sass version: 20170723.146]

---
+++ elpa/flymake-sass-20170723.146/flymake-sass-autoloads.el
@@ -0,0 +1,27 @@
+;;; flymake-sass-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-sass" "flymake-sass.el" (0 0 0 0))
+;;; Generated autoloads from flymake-sass.el
+
+(autoload 'flymake-sass-load "flymake-sass" "\
+Configure flymake mode to check the current buffer's sass syntax.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-sass" '("flymake-sass-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-sass-autoloads.el ends here
---
+++ elpa/flymake-sass-20170723.146/flymake-sass-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-sass" "20170723.146" "Flymake handler for sass and scss files" '((flymake-easy "0.1")) :commit "2de28148e92deb93bff3d55fe14e7c67ac476056" :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/flymake-sass")
