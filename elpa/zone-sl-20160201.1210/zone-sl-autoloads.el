5755e181af5b9c0c63fbe3166f7f6752a34ad3db
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:36 +0000

    [Adding zone-sl version: 20160201.1210]

---
+++ elpa/zone-sl-20160201.1210/zone-sl-autoloads.el
@@ -0,0 +1,32 @@
+;;; zone-sl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "zone-sl" "zone-sl.el" (0 0 0 0))
+;;; Generated autoloads from zone-sl.el
+
+(autoload 'zone-pgm-sl "zone-sl" "\
+Zone out with steam locomotive.
+
+\(fn)" nil nil)
+
+(autoload 'zone-sl "zone-sl" "\
+Zone out with steame locomotive.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "zone-sl" '("zone-sl-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zone-sl-autoloads.el ends here
---
+++ elpa/zone-sl-20160201.1210/zone-sl-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zone-sl" "20160201.1210" "Zone out with steam locomotives." '((emacs "24.3")) :commit "7ec22e3661c6348382f9fc39a9d0063dbd2352ff" :keywords '("games") :authors '(("KAWABATA, Taichi <kawabata.taichi_at_gmail.com>")) :maintainer '("KAWABATA, Taichi <kawabata.taichi_at_gmail.com>") :url "https://github.com/kawabata/zone-sl")
