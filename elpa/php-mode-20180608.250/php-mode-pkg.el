646601b40226f74568842bafc154ba40108e005b
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:07:33 +0000

    Remove stray PHP mode

--- elpa/php-mode-20180608.250/php-mode-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "php-mode" "20180608.250" "Major mode for editing PHP code"
-  '((emacs "24.3")
-    (cl-lib "0.5"))
-  :keywords
-  '("languages" "php")
-  :url "https://github.com/ejmr/php-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
