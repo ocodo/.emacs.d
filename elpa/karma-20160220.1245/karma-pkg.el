42c1e1e47363fddbe87fdc089306a7e0df29eca8
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:36 +0000

    [Adding karma version: 20160220.1245]

---
+++ elpa/karma-20160220.1245/karma-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "karma" "20160220.1245" "Karma Test Runner Emacs Integration" '((pkg-info "0.4") (emacs "24")) :commit "31d3e7708246183d7ed0686be92bf23140af348c" :keywords '("language" "javascript" "js" "karma" "testing") :authors '(("Samuel Tonini")) :maintainer '("Samuel Tonini") :url "http://github.com/tonini/karma.el")
