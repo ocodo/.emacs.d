dd78796e3e97c8c29e1fcb0a38e7d28f063b03c5
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:33 +0000

    [Adding helm-c-yasnippet version: 20170128.1542]

---
+++ elpa/helm-c-yasnippet-20170128.1542/helm-c-yasnippet-autoloads.el
@@ -0,0 +1,38 @@
+;;; helm-c-yasnippet-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-c-yasnippet" "helm-c-yasnippet.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from helm-c-yasnippet.el
+
+(autoload 'helm-yas-complete "helm-c-yasnippet" "\
+List of yasnippet snippets using `helm' interface.
+
+\(fn)" t nil)
+
+(autoload 'helm-yas-visit-snippet-file "helm-c-yasnippet" "\
+List of yasnippet snippet files
+
+\(fn)" t nil)
+
+(autoload 'helm-yas-create-snippet-on-region "helm-c-yasnippet" "\
+Create a snippet from region.
+
+\(fn &optional START END FILE-NAME)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-c-yasnippet" '("helm-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-c-yasnippet-autoloads.el ends here
