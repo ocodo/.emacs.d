4eaf08ae26bf0c8fb30724a50bfeb68b3228f0b8
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:17 +0000

    [Adding exec-path-from-shell version: 20180324.204]

---
+++ elpa/exec-path-from-shell-20180324.204/exec-path-from-shell-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "exec-path-from-shell" "20180324.204" "Get environment variables such as $PATH from the shell" 'nil :commit "9bc0f4a762f16d488376fb52409c58239a86d75d" :keywords '("unix" "environment") :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/exec-path-from-shell")
