c51169f67412d059a5062a12deb7c48c96d5a135
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:53 +0000

    [Adding elm-yasnippets version: 20160401.524]

---
+++ elpa/elm-yasnippets-20160401.524/elm-yasnippets-autoloads.el
@@ -0,0 +1,32 @@
+;;; elm-yasnippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "elm-yasnippets" "elm-yasnippets.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from elm-yasnippets.el
+
+(autoload 'elm-snippets-initialize "elm-yasnippets" "\
+
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(elm-snippets-initialize))
+
+;;;***
+
+;;;### (autoloads nil nil ("elm-yasnippets-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; elm-yasnippets-autoloads.el ends here
---
+++ elpa/elm-yasnippets-20160401.524/elm-yasnippets-pkg.el
@@ -0,0 +1,11 @@
+(define-package "elm-yasnippets" "20160401.524" "Yasnippets for Elm"
+  '((yasnippet "0.8.0"))
+  :keywords
+  '("snippets")
+  :authors
+  '(("Austin Bingham" . "austin.bingham@gmail.com"))
+  :maintainer
+  '("Austin Bingham" . "austin.bingham@gmail.com"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
