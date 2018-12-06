7a1064682f753f4822e5c60eb36f7c42fffe4fba
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:17 +0000

    [Adding stylus-mode version: 20150313.1512]

---
+++ elpa/stylus-mode-20150313.1512/stylus-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "stylus-mode" "20150313.1512" "Major mode for editing .jade files" '((sws-mode "0")) :commit "4dbde92542fc7ad61df38776980905a4721d642e" :authors '(("Brian M. Carlson and other contributors")) :maintainer '("Brian M. Carlson and other contributors") :url "https://github.com/brianc/jade-mode")
