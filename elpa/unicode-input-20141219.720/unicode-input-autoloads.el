7f47ec053fe12af13492c1dfaa09a8ab28cdc721
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:06 +0000

    [Adding unicode-input version: 20141219.720]

---
+++ elpa/unicode-input-20141219.720/unicode-input-autoloads.el
@@ -0,0 +1,37 @@
+;;; unicode-input-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "unicode-input" "unicode-input.el" (0 0 0 0))
+;;; Generated autoloads from unicode-input.el
+
+(autoload 'unicode-input-mode "unicode-input" "\
+For unicode characters input. 
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'unicode-input-mode-enable "unicode-input" "\
+
+
+\(fn)" nil nil)
+
+(autoload 'unicode-input-mode-disable "unicode-input" "\
+
+
+\(fn)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "unicode-input" '("*unicode-input-" "global-abbrev-table" "unicode-input-prefix")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; unicode-input-autoloads.el ends here
