3d23e7274ae7e346557815b700eaebfbe110b212
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:36 +0000

    [Adding dired-subtree version: 20180922.1615]

---
+++ elpa/dired-subtree-20180922.1615/dired-subtree-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-subtree" "20180922.1615" "Insert subdirectories in a tree-like fashion" '((dash "2.5.0") (dired-hacks-utils "0.0.1")) :commit "c7184dc8f6ed3f2330db8d8f21f01c4b08056dd3" :keywords '("files") :authors '(("Matúš Goljer" . "matus.goljer@gmail.com")) :maintainer '("Matúš Goljer" . "matus.goljer@gmail.com"))
