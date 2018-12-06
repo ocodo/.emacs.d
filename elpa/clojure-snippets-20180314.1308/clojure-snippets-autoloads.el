01df557de2c66db11337858e6950fdeca84d17d1
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:01 +0000

    [Adding clojure-snippets version: 20180314.1308]

---
+++ elpa/clojure-snippets-20180314.1308/clojure-snippets-autoloads.el
@@ -0,0 +1,32 @@
+;;; clojure-snippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "clojure-snippets" "clojure-snippets.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from clojure-snippets.el
+
+(autoload 'clojure-snippets-initialize "clojure-snippets" "\
+
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(clojure-snippets-initialize))
+
+;;;***
+
+;;;### (autoloads nil nil ("clojure-snippets-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; clojure-snippets-autoloads.el ends here
---
+++ elpa/clojure-snippets-20180314.1308/clojure-snippets-pkg.el
@@ -0,0 +1,5 @@
+(define-package "clojure-snippets" "20180314.1308" "Yasnippets for clojure"
+  '((yasnippet "0.10.0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
