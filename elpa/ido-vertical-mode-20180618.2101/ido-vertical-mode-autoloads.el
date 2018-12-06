418568ee963972750e9eb37f2990c3a5027ed95f
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:15 +0000

    [Adding ido-vertical-mode version: 20180618.2101]

---
+++ elpa/ido-vertical-mode-20180618.2101/ido-vertical-mode-autoloads.el
@@ -0,0 +1,38 @@
+;;; ido-vertical-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ido-vertical-mode" "ido-vertical-mode.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ido-vertical-mode.el
+
+(defvar ido-vertical-mode nil "\
+Non-nil if Ido-Vertical mode is enabled.
+See the `ido-vertical-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `ido-vertical-mode'.")
+
+(custom-autoload 'ido-vertical-mode "ido-vertical-mode" nil)
+
+(autoload 'ido-vertical-mode "ido-vertical-mode" "\
+Makes ido-mode display vertically.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ido-vertical-mode" '("ido-vertical-" "turn-o")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ido-vertical-mode-autoloads.el ends here
---
+++ elpa/ido-vertical-mode-20180618.2101/ido-vertical-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ido-vertical-mode" "20180618.2101" "Makes ido-mode display vertically." 'nil :commit "16c4c1a112796ee0bcf401ea39d3e2643a89feaf" :keywords '("convenience") :authors '(("Steven Degutis")) :maintainer '("Christopher Reichert" . "creichert07@gmail.com") :url "https://github.com/creichert/ido-vertical-mode.el")
