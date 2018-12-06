b00e78a11fadd244fa7ed43fdfc323415467e9c9
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:31 +0000

    [Adding yaxception version: 20150105.1452]

---
+++ elpa/yaxception-20150105.1452/yaxception-autoloads.el
@@ -0,0 +1,22 @@
+;;; yaxception-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "yaxception" "yaxception.el" (0 0 0 0))
+;;; Generated autoloads from yaxception.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "yaxception" '("yaxception")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; yaxception-autoloads.el ends here
---
+++ elpa/yaxception-20150105.1452/yaxception-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yaxception" "20150105.1452" "Provide framework about exception like Java for Elisp" 'nil :commit "4e94cf3e0b9b5631b0e90eb4b7de597ee7185875" :keywords '("exception" "error" "signal") :authors '(("Hiroaki Otsu" . "ootsuhiroaki@gmail.com")) :maintainer '("Hiroaki Otsu" . "ootsuhiroaki@gmail.com") :url "https://github.com/aki2o/yaxception")
