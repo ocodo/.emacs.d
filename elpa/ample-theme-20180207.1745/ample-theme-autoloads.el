f7dd9f8fe3aa75c9398ccf01fd4810d85fbdaf62
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:21 +0000

    [Updating ample-theme version: 20180207.1745][removing old version: 20180207.945]

--- elpa/ample-theme-20180207.1745/ample-theme-autoloads.el
+++ elpa/ample-theme-20180207.1745/ample-theme-autoloads.el
@@ -1,10 +1,13 @@
 ;;; ample-theme-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "ample-flat-theme" "ample-flat-theme.el" (23166
-;;;;;;  22939 968 908000))
+;;;### (autoloads nil "ample-flat-theme" "ample-flat-theme.el" (0
+;;;;;;  0 0 0))
 ;;; Generated autoloads from ample-flat-theme.el
 
 (when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -14,10 +17,12 @@ Apply the ample-flat-theme.
 
 \(fn)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ample-flat-theme" '("ample-flat")))
+
 ;;;***
 
 ;;;### (autoloads nil "ample-light-theme" "ample-light-theme.el"
-;;;;;;  (23166 22939 968 908000))
+;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from ample-light-theme.el
 
 (when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -27,10 +32,11 @@ Apply the ample-light-theme.
 
 \(fn)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ample-light-theme" '("ample-light")))
+
 ;;;***
 
-;;;### (autoloads nil "ample-theme" "ample-theme.el" (23166 22938
-;;;;;;  996968 913000))
+;;;### (autoloads nil "ample-theme" "ample-theme.el" (0 0 0 0))
 ;;; Generated autoloads from ample-theme.el
 
 (when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -40,10 +46,11 @@ Apply the ample-theme.
 
 \(fn)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ample-theme" '("ample")))
+
 ;;;***
 
-;;;### (autoloads nil nil ("ample-theme-pkg.el") (23166 22939 968
-;;;;;;  908000))
+;;;### (autoloads nil nil ("ample-theme-pkg.el") (0 0 0 0))
 
 ;;;***
 
@@ -51,5 +58,6 @@ Apply the ample-theme.
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; ample-theme-autoloads.el ends here
---
+++ elpa/ample-theme-20180207.1745/ample-theme-pkg.el
@@ -0,0 +1,10 @@
+(define-package "ample-theme" "20180207.1745" "Calm Dark Theme for Emacs" 'nil :keywords
+  '("theme" "dark")
+  :authors
+  '(("Jordon Biondo" . "jordonbiondo@gmail.com"))
+  :maintainer
+  '("Jordon Biondo" . "jordonbiondo@gmail.com")
+  :url "https://github.com/jordonbiondo/ample-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
