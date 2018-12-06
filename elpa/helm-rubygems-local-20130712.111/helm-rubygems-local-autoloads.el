52ad4c37616ad07c44befe7aea3d45575b385915
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:51 +0000

    [Adding helm-rubygems-local version: 20130712.111]

---
+++ elpa/helm-rubygems-local-20130712.111/helm-rubygems-local-autoloads.el
@@ -0,0 +1,28 @@
+;;; helm-rubygems-local-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-rubygems-local" "helm-rubygems-local.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from helm-rubygems-local.el
+
+(autoload 'helm-rubygems-local "helm-rubygems-local" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-rubygems-local" '("helm-c-source-rubygems-local")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-rubygems-local-autoloads.el ends here
---
+++ elpa/helm-rubygems-local-20130712.111/helm-rubygems-local-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-rubygems-local" "20130712.111" "Installed local rubygems find-file for helm" '((helm "1.5.3")) :commit "289cb33d41c703af9791d6da46b55f070013c2e3" :authors '(("hadashiA" . "dev@hadashikick.jp")) :maintainer '("hadashiA" . "dev@hadashikick.jp") :url "https://github.com/f-kubotar/helm-rubygems-local")
