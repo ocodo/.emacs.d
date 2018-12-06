60031dec48f7b8fb4f4402799a45923a96542aa1
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:22 +0000

    [Adding finalize version: 20170418.1945]

---
+++ elpa/finalize-20170418.1945/finalize-pkg.el
@@ -0,0 +1,8 @@
+(define-package "finalize" "20170418.1945" "finalizers for Emacs Lisp"
+  '((emacs "24.1")
+    (cl-generic "0.3")
+    (cl-lib "0.3")
+    (eieio "1.4")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
