ad9c9e134499319ae59dd423cf7a2061ab5de94c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:24 +0000

    [Adding darkburn-theme version: 20170423.1652]

---
+++ elpa/darkburn-theme-20170423.1652/darkburn-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; darkburn-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "darkburn-theme" "darkburn-theme.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from darkburn-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "darkburn-theme" '("darkburn")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; darkburn-theme-autoloads.el ends here
---
+++ elpa/darkburn-theme-20170423.1652/darkburn-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "darkburn-theme" "20170423.1652" "A not-so-low contrast color theme for Emacs." 'nil :commit "0af794ff7fac19778ac8a7efb92455c6f6c2158f" :authors '(("Jonas Gorauskas" . "jgorauskas@gmail.com")) :maintainer '("Jonas Gorauskas" . "jgorauskas@gmail.com") :url "http://github.com/gorauskas/darkburn-theme")
