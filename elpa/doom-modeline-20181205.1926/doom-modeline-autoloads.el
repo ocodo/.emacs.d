47ad611d520840d00d0351a2b462ada775aa2e87
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:41 +0000

    [Adding doom-modeline version: 20181205.1926]

---
+++ elpa/doom-modeline-20181205.1926/doom-modeline-autoloads.el
@@ -0,0 +1,47 @@
+;;; doom-modeline-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "doom-modeline" "doom-modeline.el" (0 0 0 0))
+;;; Generated autoloads from doom-modeline.el
+
+(autoload 'doom-modeline-init "doom-modeline" "\
+Initialize doom mode-line.
+
+\(fn)" nil nil)
+
+(autoload 'doom-modeline-set-minimal-modeline "doom-modeline" "\
+Set sepcial mode-line.
+
+\(fn)" nil nil)
+
+(autoload 'doom-modeline-set-special-modeline "doom-modeline" "\
+Set sepcial mode-line.
+
+\(fn)" nil nil)
+
+(autoload 'doom-modeline-set-media-modeline "doom-modeline" "\
+Set media mode-line.
+
+\(fn)" nil nil)
+
+(autoload 'doom-modeline-set-project-modeline "doom-modeline" "\
+Set project mode-line.
+
+\(fn)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "doom-modeline" '("doom-modeline")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; doom-modeline-autoloads.el ends here
---
+++ elpa/doom-modeline-20181205.1926/doom-modeline-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "doom-modeline" "20181205.1926" "A minimal and modern modeline" '((emacs "25.1") (all-the-icons "1.0.0") (shrink-path "0.2.0") (eldoc-eval "0.1") (dash "2.11.0")) :commit "f73eba86fadf31c12e5ce8a5cb52d34a71202942" :keywords '("faces" "mode-line") :authors '(("Vincent Zhang" . "seagle0128@gmail.com")) :maintainer '("Vincent Zhang" . "seagle0128@gmail.com") :url "https://github.com/seagle0128/doom-modeline")
