06a027f5a107d55f3bc49d757ff11e011d14bb72
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:15 +0000

    [Adding counsel version: 20181129.1747]

---
+++ elpa/counsel-20181129.1747/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20181129.1747" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "86635fb8268c07b9fb534357e8e0940c23edeef7" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
