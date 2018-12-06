4cb9745b0053c1cff80515f2f92212237fc83f54
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:16 +0000

    [Adding idomenu version: 20141123.2120]

---
+++ elpa/idomenu-20141123.2120/idomenu-autoloads.el
@@ -0,0 +1,27 @@
+;;; idomenu-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "idomenu" "idomenu.el" (0 0 0 0))
+;;; Generated autoloads from idomenu.el
+
+(autoload 'idomenu "idomenu" "\
+Switch to a buffer-local tag from Imenu via Ido.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "idomenu" '("idomenu--")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; idomenu-autoloads.el ends here
---
+++ elpa/idomenu-20141123.2120/idomenu-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "idomenu" "20141123.2120" "imenu tag selection a la ido" 'nil :commit "4b0152d606360c70204fb4c27f68de79ca885386" :authors '(("Georg Brandl" . "georg@python.org")) :maintainer '("Georg Brandl" . "georg@python.org"))
