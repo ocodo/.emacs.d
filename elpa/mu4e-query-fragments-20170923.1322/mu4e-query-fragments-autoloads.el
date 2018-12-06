9675325fa8dfe22891104512ee1a9ac7f9ca0820
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:10 +0000

    [Adding mu4e-query-fragments version: 20170923.1322]

---
+++ elpa/mu4e-query-fragments-20170923.1322/mu4e-query-fragments-autoloads.el
@@ -0,0 +1,51 @@
+;;; mu4e-query-fragments-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "mu4e-query-fragments" "mu4e-query-fragments.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from mu4e-query-fragments.el
+
+(defvar mu4e-query-fragments-list nil "\
+Define query fragments available in `mu4e' searches and bookmarks.
+List of (FRAGMENT . EXPANSION), where FRAGMENT is the string to be
+substituted and EXPANSION is the query string to be expanded.
+
+FRAGMENT should be an unique text symbol that doesn't conflict with the
+regular mu4e/xapian search syntax or previous fragments. EXPANSION is
+expanded as (EXPANSION), composing properly with boolean operators and
+can contain fragments in itself.
+
+Example:
+
+\(setq mu4e-query-fragments-list
+   '((\"%junk\" . \"maildir:/Junk OR subject:SPAM\")
+     (\"%hidden\" . \"flag:trashed OR %junk\")))")
+
+(custom-autoload 'mu4e-query-fragments-list "mu4e-query-fragments" t)
+
+(defvar mu4e-query-fragments-append nil "\
+Query fragment appended to new searches by `mu4e-query-fragments-search'.")
+
+(custom-autoload 'mu4e-query-fragments-append "mu4e-query-fragments" t)
+
+(autoload 'mu4e-query-fragments-expand "mu4e-query-fragments" "\
+Expand fragments defined in `mu4e-query-fragments-list' in QUERY.
+
+\(fn QUERY)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "mu4e-query-fragments" '("mu4e-query-fragments-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; mu4e-query-fragments-autoloads.el ends here
---
+++ elpa/mu4e-query-fragments-20170923.1322/mu4e-query-fragments-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mu4e-query-fragments" "20170923.1322" "mu4e query fragments extension" '((emacs "24.4")) :commit "34ddad4e6785f575333efcc66153d892daa1c884" :keywords '("mu4e" "mail" "convenience") :authors '(("Yuri D'Elia" . "wavexx@thregr.org")) :maintainer '("Yuri D'Elia" . "wavexx@thregr.org") :url "https://github.com/wavexx/mu4e-query-fragments.el")
