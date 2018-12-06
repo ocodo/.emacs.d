a5fa6bd0d39de2d14ea327a590ee81429ad3b396
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:43 +0000

    [Adding flyparens version: 20140723.1846]

---
+++ elpa/flyparens-20140723.1846/flyparens-autoloads.el
@@ -0,0 +1,39 @@
+;;; flyparens-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flyparens" "flyparens.el" (0 0 0 0))
+;;; Generated autoloads from flyparens.el
+
+(autoload 'flyparens-mode "flyparens" "\
+ FlyParens
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flyparens" '("flyparens-")))
+
+;;;***
+
+;;;### (autoloads nil "flyparens-config-example-1" "flyparens-config-example-1.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from flyparens-config-example-1.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flyparens-config-example-1" '("my-flyparens-function")))
+
+;;;***
+
+;;;### (autoloads nil nil ("flyparens-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flyparens-autoloads.el ends here
---
+++ elpa/flyparens-20140723.1846/flyparens-pkg.el
@@ -0,0 +1,9 @@
+(define-package "flyparens" "20140723.1846" "Check for unbalanced parens on the fly" 'nil :keywords
+  '("faces" "convenience" "lisp" "matching" "parentheses" "parens")
+  :authors
+  '(("Jisang Yoo"))
+  :maintainer
+  '("Jisang Yoo"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
