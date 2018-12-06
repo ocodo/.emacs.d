e6b3ca6a3196eb80e4568561284600fd82aa4a61
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:34 +0000

    [Adding zenity-color-picker version: 20160302.1154]

---
+++ elpa/zenity-color-picker-20160302.1154/zenity-color-picker-autoloads.el
@@ -0,0 +1,38 @@
+;;; zenity-color-picker-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "zenity-color-picker" "zenity-color-picker.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from zenity-color-picker.el
+
+(autoload 'zenity-cp-adjust-color-at-point "zenity-color-picker" "\
+Adjust color at point
+
+\(fn)" t nil)
+
+(autoload 'zenity-cp-insert-color-at-point "zenity-color-picker" "\
+Insert color at point
+
+\(fn)" t nil)
+
+(autoload 'zenity-cp-color-at-point-dwim "zenity-color-picker" "\
+Adjust or insert color at point
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "zenity-color-picker" '("zenity-cp-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zenity-color-picker-autoloads.el ends here
---
+++ elpa/zenity-color-picker-20160302.1154/zenity-color-picker-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zenity-color-picker" "20160302.1154" "Insert and adjust colors using Zenity" '((emacs "24.4")) :commit "4f4f46676a461ebc881487fb70c8c181e323db5e" :keywords '("colors") :authors '(("Samuel Laurén" . "samuel.lauren@iki.fi")) :maintainer '("Samuel Laurén" . "samuel.lauren@iki.fi") :url "https://bitbucket.org/Soft/zenity-color-picker.el")
