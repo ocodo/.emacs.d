a74224b4a85ed2d2d1667e036b4a0e0363da658c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:12 +0000

    [Adding evil-smartparens version: 20171210.1513]

---
+++ elpa/evil-smartparens-20171210.1513/evil-smartparens-autoloads.el
@@ -0,0 +1,28 @@
+;;; evil-smartparens-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "evil-smartparens" "evil-smartparens.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from evil-smartparens.el
+
+(autoload 'evil-smartparens-mode "evil-smartparens" "\
+Toggle evil-smartparens.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-smartparens" '("evil-s")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; evil-smartparens-autoloads.el ends here
---
+++ elpa/evil-smartparens-20171210.1513/evil-smartparens-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-smartparens" "20171210.1513" "Evil support for smartparens" '((evil "1.0") (emacs "24.4") (smartparens "1.10.1")) :commit "026d4a3cfce415a4dfae1457f871b385386e61d3" :keywords '("evil" "smartparens") :authors '(("Lars Andersen" . "expez@expez.com")) :maintainer '("Lars Andersen" . "expez@expez.com") :url "https://www.github.com/expez/evil-smartparens")
