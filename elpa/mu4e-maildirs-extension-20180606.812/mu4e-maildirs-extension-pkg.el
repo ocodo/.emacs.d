59002c2ac35daed54c831d9cd6ccba2bf1c764e3
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:10 +0000

    [Adding mu4e-maildirs-extension version: 20180606.812]

---
+++ elpa/mu4e-maildirs-extension-20180606.812/mu4e-maildirs-extension-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mu4e-maildirs-extension" "20180606.812" "Show mu4e maildirs summary in mu4e-main-view" '((dash "0.0.0")) :commit "3ef4c48516be66e73d24fe764aadbcfc126b7964" :authors '(("Andreu Gil Pàmies" . "agpchil@gmail.com")) :maintainer '("Andreu Gil Pàmies" . "agpchil@gmail.com") :url "http://github.com/agpchil/mu4e-maildirs-extension")
