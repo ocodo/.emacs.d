6cf9eac2d16cbee540cff938d429fa435ef6f135
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:27 +0000

    [Adding tern version: 20181108.722]

---
+++ elpa/tern-20181108.722/tern-autoloads.el
@@ -0,0 +1,32 @@
+;;; tern-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "tern" "tern.el" (0 0 0 0))
+;;; Generated autoloads from tern.el
+
+(autoload 'tern-use-server "tern" "\
+
+
+\(fn PORT SERVER)" t nil)
+
+(autoload 'tern-mode "tern" "\
+Minor mode binding to the Tern JavaScript analyzer
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tern" '("tern-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; tern-autoloads.el ends here
---
+++ elpa/tern-20181108.722/tern-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tern" "20181108.722" "Tern-powered JavaScript integration" '((json "1.2") (cl-lib "0.5") (emacs "24")) :commit "e94e20be8510040744fc803c9fda6bbb9625c6d2" :authors '(("Marijn Haverbeke")) :maintainer '("Marijn Haverbeke") :url "http://ternjs.net/")
