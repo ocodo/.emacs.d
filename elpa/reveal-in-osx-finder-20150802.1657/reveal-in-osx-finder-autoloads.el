05b73dbd34eae3a56c9b414b73631e4ab1660150
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:23 +0000

    [Adding reveal-in-osx-finder version: 20150802.1657]

---
+++ elpa/reveal-in-osx-finder-20150802.1657/reveal-in-osx-finder-autoloads.el
@@ -0,0 +1,29 @@
+;;; reveal-in-osx-finder-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "reveal-in-osx-finder" "reveal-in-osx-finder.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from reveal-in-osx-finder.el
+
+(autoload 'reveal-in-osx-finder "reveal-in-osx-finder" "\
+Reveal the file associated with the current buffer in the OS X Finder.
+In a dired buffer, it will open the current directory.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "reveal-in-osx-finder" '("reveal-in-osx-finder-as")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; reveal-in-osx-finder-autoloads.el ends here
---
+++ elpa/reveal-in-osx-finder-20150802.1657/reveal-in-osx-finder-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "reveal-in-osx-finder" "20150802.1657" "Reveal file associated with buffer in OS X Finder" 'nil :commit "5710e5936e47139a610ec9a06899f72e77ddc7bc" :keywords '("os x" "finder") :authors '(("Kazuki YOSHIDA")) :maintainer '("Kazuki YOSHIDA") :url "https://github.com/kaz-yos/reveal-in-osx-finder")
