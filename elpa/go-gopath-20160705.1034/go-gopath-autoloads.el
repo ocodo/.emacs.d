ec0a6cb21479cafd2fac9b0890ec27c9ff15ccfa
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:06 +0000

    [Adding go-gopath version: 20160705.1034]

---
+++ elpa/go-gopath-20160705.1034/go-gopath-autoloads.el
@@ -0,0 +1,27 @@
+;;; go-gopath-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "go-gopath" "go-gopath.el" (0 0 0 0))
+;;; Generated autoloads from go-gopath.el
+
+(autoload 'go-gopath-set-gopath "go-gopath" "\
+
+
+\(fn GOPATH)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "go-gopath" '("go-gopath-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-gopath-autoloads.el ends here
---
+++ elpa/go-gopath-20160705.1034/go-gopath-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-gopath" "20160705.1034" "Will guess GOPATH using gb and projectile." '((cl-lib "0.5")) :commit "5172fc53f21edbf9347d5ee7d1d745da1ec88a15" :authors '(("Andrew Kirilenko" . "andrew.kirilenko.main@gmail.com")) :maintainer '("Andrew Kirilenko" . "andrew.kirilenko.main@gmail.com") :url "http://github.com/iced/go-gopath/")
