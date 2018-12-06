69e7c2960cb6c835a486115d752e6c16273e05f2
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:01 +0000

    [Adding highlight-numbers version: 20181013.1744]

---
+++ elpa/highlight-numbers-20181013.1744/highlight-numbers-autoloads.el
@@ -0,0 +1,34 @@
+;;; highlight-numbers-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "highlight-numbers" "highlight-numbers.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from highlight-numbers.el
+
+(autoload 'highlight-numbers-mode "highlight-numbers" "\
+Minor mode for highlighting numeric literals in source code.
+
+Toggle Highlight Numbers mode on or off.
+
+With a prefix argument ARG, enable Highlight Numbers mode if ARG is
+positive, and disable it otherwise. If called from Lisp, enable
+the mode if ARG is omitted or nil, and toggle it if ARG is `toggle'.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "highlight-numbers" '("highlight-numbers-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; highlight-numbers-autoloads.el ends here
---
+++ elpa/highlight-numbers-20181013.1744/highlight-numbers-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "highlight-numbers" "20181013.1744" "Highlight numbers in source code" '((emacs "24") (parent-mode "2.0")) :commit "8b4744c7f46c72b1d3d599d4fb75ef8183dee307" :authors '(("Fanael Linithien" . "fanael4@gmail.com")) :maintainer '("Fanael Linithien" . "fanael4@gmail.com") :url "https://github.com/Fanael/highlight-numbers")
