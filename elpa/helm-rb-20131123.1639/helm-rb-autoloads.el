0016e8f89af577a0e7b6c5f808f71d5935bd0d96
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:50 +0000

    [Adding helm-rb version: 20131123.1639]

---
+++ elpa/helm-rb-20131123.1639/helm-rb-autoloads.el
@@ -0,0 +1,33 @@
+;;; helm-rb-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-rb" "helm-rb.el" (0 0 0 0))
+;;; Generated autoloads from helm-rb.el
+
+(autoload 'helm-rb "helm-rb" "\
+Search Ruby's method by using helm and ag.
+If you want to change methods searching program, you can specify
+the program's path to `helm-rb-get-methods-program' variable.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-rb" '("helm-rb-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("helm-rb-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-rb-autoloads.el ends here
---
+++ elpa/helm-rb-20131123.1639/helm-rb-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm-rb" "20131123.1639" "Search Ruby's method by ag and display helm"
+  '((helm "1.0")
+    (helm-ag-r "20131123"))
+  :keywords
+  '("searching" "ruby")
+  :authors
+  '(("Yuta Yamada <cokesboy\"at\"gmail.com>"))
+  :maintainer
+  '("Yuta Yamada <cokesboy\"at\"gmail.com>")
+  :url "https://github.com/yuutayamada/helm-rb")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
