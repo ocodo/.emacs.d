3238386ac487e6725cd2b25fbd1243367b56adf3
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:59 +0000

    [Adding tuareg version: 20181204.2147]

---
+++ elpa/tuareg-20181204.2147/tuareg-pkg.el
@@ -0,0 +1,17 @@
+(define-package "tuareg" "20181204.2147" "OCaml mode for Emacs."
+  '((caml "3.12.0.1"))
+  :keywords
+  '("ocaml" "languages")
+  :authors
+  '(("Albert Cohen" . "Albert.Cohen@inria.fr")
+    ("Sam Steingold" . "sds@gnu.org")
+    ("Christophe Troestler" . "Christophe.Troestler@umons.ac.be")
+    ("Till Varoquaux" . "till@pps.jussieu.fr")
+    ("Sean McLaughlin" . "seanmcl@gmail.com")
+    ("Stefan Monnier" . "monnier@iro.umontreal.ca"))
+  :maintainer
+  '("Albert Cohen" . "Albert.Cohen@inria.fr")
+  :url "https://github.com/ocaml/tuareg")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
