15d430cfb2eaccb05917a62206600f495834fe7b
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:13 +0000

    [Adding waher-theme version: 20141115.1230]

---
+++ elpa/waher-theme-20141115.1230/waher-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "waher-theme" "20141115.1230" "Emacs 24 theme based on waher for st2 by dduckster" '((emacs "24.1")) :commit "60d31519fcfd8e797723d47961b255ae2f2e2c0a" :authors '(("Jasonm23" . "jasonm23@gmail.com")) :maintainer '("Jasonm23" . "jasonm23@gmail.com") :url "https://github.com/jasonm23/emacs-waher-theme")
