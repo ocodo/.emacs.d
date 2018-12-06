f2407cbc826ff10ea884ad55e7caa524a93e4015
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:07 +0000

    [Adding http-twiddle version: 20160801.1911]

---
+++ elpa/http-twiddle-20160801.1911/http-twiddle-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "http-twiddle" "20160801.1911" "send & twiddle & resend HTTP requests" 'nil :commit "4d0c73b7dcbde8b483d4f3a75c49c74d2fe3ca45" :keywords '("http" "rest" "soap") :authors '(("Luke Gorrie" . "luke@synap.se")) :maintainer '("Hasan Veldstra" . "h@vidiowiki.com") :url "https://github.com/hassy/http-twiddle/blob/master/http-twiddle.el")
