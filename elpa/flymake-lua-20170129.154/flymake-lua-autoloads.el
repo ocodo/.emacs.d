e687ae4c33a5f26227eada823e6b39aef70ee615
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:40 +0000

    [Adding flymake-lua version: 20170129.154]

---
+++ elpa/flymake-lua-20170129.154/flymake-lua-autoloads.el
@@ -0,0 +1,27 @@
+;;; flymake-lua-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-lua" "flymake-lua.el" (0 0 0 0))
+;;; Generated autoloads from flymake-lua.el
+
+(autoload 'flymake-lua-load "flymake-lua" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-lua" '("flymake-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-lua-autoloads.el ends here
---
+++ elpa/flymake-lua-20170129.154/flymake-lua-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-lua" "20170129.154" "Flymake for Lua" 'nil :commit "84589f20066921a5b79cf3a1f914a223a2552d2a" :keywords '("lua") :authors '((nil . "Sébastien Roccaserra (format \"<%s%s@%s.%s>\" \"s\" \"roccaserra\" \"yahoo\" \"com\")")) :maintainer '(nil . "Sébastien Roccaserra (format \"<%s%s@%s.%s>\" \"s\" \"roccaserra\" \"yahoo\" \"com\")"))
