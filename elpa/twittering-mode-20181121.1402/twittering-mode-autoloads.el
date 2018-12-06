ccd66b1a8b693058aed2201102a38adcd779815d
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:00 +0000

    [Adding twittering-mode version: 20181121.1402]

---
+++ elpa/twittering-mode-20181121.1402/twittering-mode-autoloads.el
@@ -0,0 +1,28 @@
+;;; twittering-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "twittering-mode" "twittering-mode.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from twittering-mode.el
+
+(autoload 'twit "twittering-mode" "\
+Start twittering-mode.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "twittering-mode" '("twittering-" "debug-print" "case-string" "list-push" "assocref")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; twittering-mode-autoloads.el ends here
---
+++ elpa/twittering-mode-20181121.1402/twittering-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "twittering-mode" "20181121.1402" "Major mode for Twitter" 'nil :commit "114891e8fdb4f06b1326a6cf795e49c205cf9e29" :keywords '("twitter" "web") :authors '(("Tadashi MATSUO" . "tad@mymail.twin.ne.jp") ("Y. Hayamizu" . "y.hayamizu@gmail.com") ("Tsuyoshi CHO" . "Tsuyoshi.CHO+develop@Gmail.com") ("Alberto Garcia" . "agarcia@igalia.com") ("Xavier Maillard" . "xavier@maillard.im")) :maintainer '("Tadashi MATSUO" . "tad@mymail.twin.ne.jp") :url "http://twmode.sf.net/")
