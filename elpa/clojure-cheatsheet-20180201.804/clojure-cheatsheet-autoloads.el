cca5789916960a5f380d556d82bdf0ac0263f954
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:00 +0000

    [Adding clojure-cheatsheet version: 20180201.804]

---
+++ elpa/clojure-cheatsheet-20180201.804/clojure-cheatsheet-autoloads.el
@@ -0,0 +1,28 @@
+;;; clojure-cheatsheet-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "clojure-cheatsheet" "clojure-cheatsheet.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from clojure-cheatsheet.el
+
+(autoload 'clojure-cheatsheet "clojure-cheatsheet" "\
+Use helm to show a Clojure cheatsheet.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "clojure-cheatsheet" '("helm-source-clojure-cheatsheet" "clojure-cheatsheet")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; clojure-cheatsheet-autoloads.el ends here
---
+++ elpa/clojure-cheatsheet-20180201.804/clojure-cheatsheet-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clojure-cheatsheet" "20180201.804" "The Clojure Cheatsheet for Emacs" '((helm "1.7.7") (cider "0.9.0")) :commit "85c382317a56bbdfac03ae95999c28fc0cde65d7" :keywords '("clojure" "cider" "cheatsheet" "helm") :authors '(("Kris Jenkins" . "krisajenkins@gmail.com")) :maintainer '("Kris Jenkins" . "krisajenkins@gmail.com") :url "https://github.com/clojure-emacs/clojure-cheatsheet")
