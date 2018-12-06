48fc9862243d28432becfbe0ed073d6e389559bf
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:27 +0000

    [Adding db version: 20140421.2111]

---
+++ elpa/db-20140421.2111/db-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "db" "20140421.2111" "A database for EmacsLisp" '((kv "0.0.11")) :commit "b3a423fb8e72f9013009cbe033d654df2ce31438" :keywords '("data" "lisp") :authors '(("Nic Ferrier" . "nferrier@ferrier.me.uk")) :maintainer '("Nic Ferrier" . "nferrier@ferrier.me.uk"))
