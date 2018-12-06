53328dbefe24f048faecdd64d71ce479942a9eb7
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:23 +0000

    [Adding dakrone-theme version: 20170801.1933]

---
+++ elpa/dakrone-theme-20170801.1933/dakrone-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; dakrone-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "dakrone-theme" "dakrone-theme.el" (0 0 0 0))
+;;; Generated autoloads from dakrone-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "dakrone-theme" '("dakrone")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dakrone-theme-autoloads.el ends here
---
+++ elpa/dakrone-theme-20170801.1933/dakrone-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dakrone-theme" "20170801.1933" "dakrone's custom dark theme" 'nil :commit "232ad1be5f3572dcbdf528f1655109aa355a6937" :keywords '("color" "themes") :authors '(("Lee Hinman <lee _AT_ writequit.org>")) :maintainer '("Lee Hinman <lee _AT_ writequit.org>") :url "https://github.com/dakrone/dakrone-theme")
