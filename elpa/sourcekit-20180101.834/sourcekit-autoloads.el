3e3fc27566bf0e7952ff87d078f7d53b9a94aafe
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:08 +0000

    [Adding sourcekit version: 20180101.834]

---
+++ elpa/sourcekit-20180101.834/sourcekit-autoloads.el
@@ -0,0 +1,22 @@
+;;; sourcekit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "sourcekit" "sourcekit.el" (0 0 0 0))
+;;; Generated autoloads from sourcekit.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sourcekit" '("sourcekit-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sourcekit-autoloads.el ends here
---
+++ elpa/sourcekit-20180101.834/sourcekit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sourcekit" "20180101.834" "Library to interact with sourcekittendaemon" '((emacs "24.3") (dash "2.12.1") (dash-functional "1.2.0") (request "0.2.0")) :commit "abf9bc5a0102eb666d3aa6d6bf22f6efcc852781" :keywords '("tools" "processes") :authors '(("Nathan Kot" . "nk@nathankot.com")) :maintainer '("Nathan Kot" . "nk@nathankot.com") :url "https://github.com/nathankot/company-sourcekit")
