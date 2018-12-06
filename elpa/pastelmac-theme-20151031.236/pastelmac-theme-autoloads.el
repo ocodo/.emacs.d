5007266077819cfb6a920c025d97c2303a2562ed
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:34 +0000

    [Adding pastelmac-theme version: 20151031.236]

---
+++ elpa/pastelmac-theme-20151031.236/pastelmac-theme-autoloads.el
@@ -0,0 +1,30 @@
+;;; pastelmac-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "pastelmac-theme" "pastelmac-theme.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from pastelmac-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(autoload 'pastelmac-theme "pastelmac-theme" "\
+Load the pastelmac-theme.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "pastelmac-theme" '("pastelmac")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; pastelmac-theme-autoloads.el ends here
---
+++ elpa/pastelmac-theme-20151031.236/pastelmac-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pastelmac-theme" "20151031.236" "a soothing theme with a pastel color palette" '((emacs "24.1")) :commit "bead21741e3f46f6506e8aef4469d4240a819389" :keywords '("themes") :authors '(("Brian Mastenbrook" . "brian@mastenbrook.net")) :maintainer '("Brian Mastenbrook" . "brian@mastenbrook.net") :url "https://github.com/bmastenbrook/pastelmac-theme-el")
