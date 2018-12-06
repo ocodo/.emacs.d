ce15f0da795f36e6a63a2bd42b5137619c25bb0f
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:51 +0000

    [Adding elixir-mode version: 20180711.1245]

---
+++ elpa/elixir-mode-20180711.1245/elixir-mode-pkg.el
@@ -0,0 +1,9 @@
+(define-package "elixir-mode" "20180711.1245" "Major mode for editing Elixir files"
+  '((emacs "24")
+    (pkg-info "0.4"))
+  :keywords
+  '("languages" "elixir")
+  :url "https://github.com/elixir-lang/emacs-elixir")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
