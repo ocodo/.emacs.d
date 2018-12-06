af738d1b2d94af405df78bdfeef68e9fe7628cac
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:17 +0000

    [Adding counsel-osx-app version: 20160821.809]

---
+++ elpa/counsel-osx-app-20160821.809/counsel-osx-app-autoloads.el
@@ -0,0 +1,28 @@
+;;; counsel-osx-app-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "counsel-osx-app" "counsel-osx-app.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from counsel-osx-app.el
+
+(autoload 'counsel-osx-app "counsel-osx-app" "\
+Launch an application via ivy interface.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "counsel-osx-app" '("counsel-osx-app-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; counsel-osx-app-autoloads.el ends here
---
+++ elpa/counsel-osx-app-20160821.809/counsel-osx-app-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-osx-app" "20160821.809" "launch osx applications via ivy interface" '((ivy "0.8.0") (emacs "24.3")) :commit "b1c54cbc033c4939966910d85ce035503079e108" :authors '(("Boris Buliga" . "d12frosted@gmail.com")) :maintainer '("Boris Buliga" . "d12frosted@gmail.com") :url "https://github.com/d12frosted/counsel-osx-app")
