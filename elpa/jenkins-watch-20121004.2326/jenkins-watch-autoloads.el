8df97b2cb71f0e6f3582259b5030c86705f69c14
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:29 +0000

    [Adding jenkins-watch version: 20121004.2326]

---
+++ elpa/jenkins-watch-20121004.2326/jenkins-watch-autoloads.el
@@ -0,0 +1,22 @@
+;;; jenkins-watch-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "jenkins-watch" "jenkins-watch.el" (0 0 0 0))
+;;; Generated autoloads from jenkins-watch.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "jenkins-watch" '("jenkins-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; jenkins-watch-autoloads.el ends here
---
+++ elpa/jenkins-watch-20121004.2326/jenkins-watch-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "jenkins-watch" "20121004.2326" "Watch continuous integration build status" 'nil :commit "37b84dfbd98240a57ff798e1ff8bc7dba2913577" :authors '(("Andrew Taylor" . "ataylor@redtoad.ca")) :maintainer '("Andrew Taylor" . "ataylor@redtoad.ca") :url "https://github.com/ataylor284/jenkins-watch")
