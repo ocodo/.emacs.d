62ec2450ee633c24065585168af09342c3d9047c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:00 +0000

    [Adding highlight-leading-spaces version: 20151216.1222]

---
+++ elpa/highlight-leading-spaces-20151216.1222/highlight-leading-spaces-autoloads.el
@@ -0,0 +1,28 @@
+;;; highlight-leading-spaces-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "highlight-leading-spaces" "highlight-leading-spaces.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from highlight-leading-spaces.el
+
+(autoload 'highlight-leading-spaces-mode "highlight-leading-spaces" "\
+Highlight leading spaces.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "highlight-leading-spaces" '("highlight-leading-spaces-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; highlight-leading-spaces-autoloads.el ends here
---
+++ elpa/highlight-leading-spaces-20151216.1222/highlight-leading-spaces-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "highlight-leading-spaces" "20151216.1222" "Highlight leading spaces" '((emacs "24.4")) :commit "840db19d863dd97993fd9f893f5be501627b6354" :authors '(("Thomas Winant" . "dewinant@gmail.com")) :maintainer '("Thomas Winant" . "dewinant@gmail.com") :url "https://github.com/mrBliss/highlight-leading-spaces")
