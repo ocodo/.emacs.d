3a1c715664871259ce8d656d967c8ff289269917
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:21 +0000

    [Adding sws-mode version: 20150317.1945]

---
+++ elpa/sws-mode-20150317.1945/sws-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; sws-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "sws-mode" "sws-mode.el" (0 0 0 0))
+;;; Generated autoloads from sws-mode.el
+
+(autoload 'sws-mode "sws-mode" "\
+Major mode for editing significant whitespace files
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sws-mode" '("sws-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sws-mode-autoloads.el ends here
---
+++ elpa/sws-mode-20150317.1945/sws-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sws-mode" "20150317.1945" "(S)ignificant (W)hite(S)pace mode" 'nil :commit "4dbde92542fc7ad61df38776980905a4721d642e" :authors '(("Brian M. Carlson and other contributors")) :maintainer '("Brian M. Carlson and other contributors") :url "https://github.com/brianc/jade-mode")
