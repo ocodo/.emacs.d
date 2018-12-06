4fd13ff73d850e8faceec05484cd333d49d78e43
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:53 +0000

    [Adding elmine version: 20170511.720]

---
+++ elpa/elmine-20170511.720/elmine-autoloads.el
@@ -0,0 +1,22 @@
+;;; elmine-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "elmine" "elmine.el" (0 0 0 0))
+;;; Generated autoloads from elmine.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmine" '("elmine/" "plist-merge")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; elmine-autoloads.el ends here
