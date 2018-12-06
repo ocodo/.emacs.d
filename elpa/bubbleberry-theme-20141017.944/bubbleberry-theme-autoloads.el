a867ccaf06aff2225d00352ad2b41bb5cfe629bf
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:50 +0000

    [Adding bubbleberry-theme version: 20141017.944]

---
+++ elpa/bubbleberry-theme-20141017.944/bubbleberry-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; bubbleberry-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "bubbleberry-theme" "bubbleberry-theme.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from bubbleberry-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "bubbleberry-theme" '("bubbleberry")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; bubbleberry-theme-autoloads.el ends here
---
+++ elpa/bubbleberry-theme-20141017.944/bubbleberry-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bubbleberry-theme" "20141017.944" "A theme based on LightTable for Emacs24" '((emacs "24.1")) :commit "22e9adf4586414024e4592972022ec297321b320" :authors '(("Jason Milkins" . "jasonm23@gmail.com") ("Gaurav Giri github.com/grvgr")) :maintainer '("Jason Milkins" . "jasonm23@gmail.com") :url "https://github.com/jasonm23/emacs-bubbleberry-theme")
