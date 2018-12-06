6816f93a45a9da41111ac8019ea5b80d86acdd73
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:13 +0000

    [Adding mustache-mode version: 20141024.1432]

---
+++ elpa/mustache-mode-20141024.1432/mustache-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mustache-mode" "20141024.1432" "A major mode for editing Mustache files." 'nil :commit "bf9897eb287ca47ced65d7d4e07ea61ea0aec39f" :authors '(("Tony Gentilcore") ("Chris Wanstrath") ("Daniel Hackney")) :maintainer '("Tony Gentilcore"))
