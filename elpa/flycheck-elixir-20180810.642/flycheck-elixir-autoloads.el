50c515cb1037e8d5be3e34defff0f521c1efa592
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:31 +0000

    [Adding flycheck-elixir version: 20180810.642]

---
+++ elpa/flycheck-elixir-20180810.642/flycheck-elixir-autoloads.el
@@ -0,0 +1,23 @@
+;;; flycheck-elixir-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flycheck-elixir" "flycheck-elixir.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from flycheck-elixir.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flycheck-elixir" '("elixirc-params" "find-project-root")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flycheck-elixir-autoloads.el ends here
---
+++ elpa/flycheck-elixir-20180810.642/flycheck-elixir-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-elixir" "20180810.642" "Support Elixir in flycheck" '((flycheck "0.25")) :commit "11998d7e3e63a33453e934d25b3673f7c558e579" :authors '(("Lorenzo Bolla" . "lbolla@gmail.com")) :maintainer '("Lorenzo Bolla" . "lbolla@gmail.com"))
