a0b17fbff68bc60fa54e37be933a1a5ed0bf5b78
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:31 +0000

    [Adding jq-mode version: 20181103.1258]

---
+++ elpa/jq-mode-20181103.1258/jq-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "jq-mode" "20181103.1258" "Edit jq scripts."
+  '((emacs "25.1"))
+  :authors
+  '(("Bjarte Johansen <Bjarte dot Johansen at gmail dot com>"))
+  :maintainer
+  '("Bjarte Johansen <Bjarte dot Johansen at gmail dot com>")
+  :url "https://github.com/ljos/jq-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
