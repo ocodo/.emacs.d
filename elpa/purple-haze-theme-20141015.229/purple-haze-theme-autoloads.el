b715fbbeca9d17d01b40ec9a0592c7a624134c6c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:15 +0000

    [Adding purple-haze-theme version: 20141015.229]

---
+++ elpa/purple-haze-theme-20141015.229/purple-haze-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; purple-haze-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "purple-haze-theme" "purple-haze-theme.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from purple-haze-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "purple-haze-theme" '("purple-haze")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; purple-haze-theme-autoloads.el ends here
