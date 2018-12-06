9253c1edd09d62b35bdf5aa231e529780c0885a8
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:20 +0000

    [Adding request-deferred version: 20181129.317]

---
+++ elpa/request-deferred-20181129.317/request-deferred-autoloads.el
@@ -0,0 +1,23 @@
+;;; request-deferred-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "request-deferred" "request-deferred.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from request-deferred.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "request-deferred" '("request-deferred")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; request-deferred-autoloads.el ends here
---
+++ elpa/request-deferred-20181129.317/request-deferred-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "request-deferred" "20181129.317" "Wrap request.el by deferred" '((deferred "0.3.1") (request "0.2.0")) :commit "b12f54af8ab6656aa8c1143c5ccc7675c5bb02cd" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
