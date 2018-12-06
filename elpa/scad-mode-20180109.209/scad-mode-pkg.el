2529c8474b8d77ba5b2172098581d6974ca44b97
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:33 +0000

    [Adding scad-mode version: 20180109.209]

---
+++ elpa/scad-mode-20180109.209/scad-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "scad-mode" "20180109.209" "A major mode for editing OpenSCAD code" 'nil :commit "d26c1b7f3a08e7cc10007bc8a620560cc45564c4" :keywords '("languages") :authors '(("Len Trigg, Łukasz Stelmach")) :maintainer '("Len Trigg" . "lenbok@gmail.com") :url "https://raw.github.com/openscad/openscad/master/contrib/scad-mode.el")
