6d6caa1c7708a91464cbc3a30611402a9bfa7a53
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:56 +0000

    [Adding emamux version: 20170227.337]

---
+++ elpa/emamux-20170227.337/emamux-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "emamux" "20170227.337" "Interact with tmux" '((emacs "24.3")) :commit "39f57786b2cdd3844888df42d71c7bd251f07158" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-emamux")
