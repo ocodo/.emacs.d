a03fb7e085684ee9154474d895fa2b9b8d980746
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:38 +0000

    [Adding helm-dired-recent-dirs version: 20131228.1414]

---
+++ elpa/helm-dired-recent-dirs-20131228.1414/helm-dired-recent-dirs-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-dired-recent-dirs" "20131228.1414" "Show recent dirs with helm.el support." '((helm "1.0")) :commit "3bcd125b44f5a707588ae3868777d91192351523" :keywords '("helm" "dired" "zsh") :authors '(("Akisute" . "akisute3@gmail.com")) :maintainer '("Akisute" . "akisute3@gmail.com"))
