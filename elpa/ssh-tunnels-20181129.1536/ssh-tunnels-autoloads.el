16ad9e5a1c06dc476fab5695cbbcf7a0fd3f5d9e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:14 +0000

    [Adding ssh-tunnels version: 20181129.1536]

---
+++ elpa/ssh-tunnels-20181129.1536/ssh-tunnels-autoloads.el
@@ -0,0 +1,39 @@
+;;; ssh-tunnels-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-ssh-tunnels" "helm-ssh-tunnels.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from helm-ssh-tunnels.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-ssh-tunnels" '("helm-ssh-tunnels")))
+
+;;;***
+
+;;;### (autoloads nil "ssh-tunnels" "ssh-tunnels.el" (0 0 0 0))
+;;; Generated autoloads from ssh-tunnels.el
+
+(autoload 'ssh-tunnels "ssh-tunnels" "\
+View and manipulate SSH tunnels.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ssh-tunnels" '("auto-ssh-tunnels-mode" "open-network-stream@run-ssh-tunnel" "ssh-tunnels-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("ssh-tunnels-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ssh-tunnels-autoloads.el ends here
