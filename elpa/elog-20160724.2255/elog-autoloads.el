8b6f31d4783f72953702d003dd660b24b7fc5edf
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:54 +0000

    [Adding elog version: 20160724.2255]

---
+++ elpa/elog-20160724.2255/elog-autoloads.el
@@ -0,0 +1,22 @@
+;;; elog-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "elog" "elog.el" (0 0 0 0))
+;;; Generated autoloads from elog.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elog" '("elog-" "initialize-instance")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; elog-autoloads.el ends here
---
+++ elpa/elog-20160724.2255/elog-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elog" "20160724.2255" "logging library extended from logito" '((eieio "1.3")) :commit "a67237d9813c7591614d95e2ef31cc5e5ed3f31b" :keywords '("lisp" "tool" "log") :authors '(("DarkSun" . "lujun9972@gmail.com")) :maintainer '("DarkSun" . "lujun9972@gmail.com"))
