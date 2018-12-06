4e91add5a8abf1be34a146bdba907afe3813e160
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:39 +0000

    [Adding flymake-jslint version: 20170723.146]

---
+++ elpa/flymake-jslint-20170723.146/flymake-jslint-autoloads.el
@@ -0,0 +1,43 @@
+;;; flymake-jslint-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-jslint" "flymake-jslint.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from flymake-jslint.el
+
+(defvar flymake-jslint-detect-trailing-comma t "\
+Whether or not to report warnings about trailing commas.")
+
+(custom-autoload 'flymake-jslint-detect-trailing-comma "flymake-jslint" t)
+
+(defvar flymake-jslint-command (if (executable-find "jsl") "jsl" "jslint") "\
+Name (and optionally full path) of jslint executable.")
+
+(custom-autoload 'flymake-jslint-command "flymake-jslint" t)
+
+(defvar flymake-jslint-args (unless (string-equal "jsl" flymake-jslint-command) (mapcar 'symbol-name '(--white --undef --nomen --regexp --plusplus --bitwise --newcap --sloppy --vars --eqeq))) "\
+Command-line args for jslint executable.")
+
+(custom-autoload 'flymake-jslint-args "flymake-jslint" t)
+
+(autoload 'flymake-jslint-load "flymake-jslint" "\
+Configure flymake mode to check the current buffer's javascript syntax.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-jslint" '("flymake-jslint-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-jslint-autoloads.el ends here
---
+++ elpa/flymake-jslint-20170723.146/flymake-jslint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-jslint" "20170723.146" "A flymake handler for javascript using jslint" '((flymake-easy "0.1")) :commit "8edb82be605542b0ef62d38d818adcdde335eecb" :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/flymake-jslint")
