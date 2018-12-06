6399e9f4147ad8413c69e79c1541c58bbddbfcf0
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:27 +0000

    [Adding jasminejs-mode version: 20150527.5]

---
+++ elpa/jasminejs-mode-20150527.5/jasminejs-mode-autoloads.el
@@ -0,0 +1,32 @@
+;;; jasminejs-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "jasminejs-mode" "jasminejs-mode.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from jasminejs-mode.el
+
+(autoload 'jasminejs-mode "jasminejs-mode" "\
+To better edit your files
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "jasminejs-mode" '("jasminejs-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("jasminejs-mode-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; jasminejs-mode-autoloads.el ends here
---
+++ elpa/jasminejs-mode-20150527.5/jasminejs-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "jasminejs-mode" "20150527.5" "A minor mode for manipulating jasmine test files" 'nil :keywords
+  '("javascript" "jasmine")
+  :authors
+  '(("Eric Stolten" . "stoltene2@gmail.com"))
+  :maintainer
+  '("Eric Stolten" . "stoltene2@gmail.com")
+  :url "https://github.com/stoltene2/jasminejs-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
