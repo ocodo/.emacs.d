d443b4a12fed47daa4a6ecd91b510760cdf23170
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:16 +0000

    [Adding ido-yes-or-no version: 20161108.2351]

---
+++ elpa/ido-yes-or-no-20161108.2351/ido-yes-or-no-autoloads.el
@@ -0,0 +1,34 @@
+;;; ido-yes-or-no-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ido-yes-or-no" "ido-yes-or-no.el" (0 0 0 0))
+;;; Generated autoloads from ido-yes-or-no.el
+
+(defvar ido-yes-or-no-mode nil "\
+Non-nil if Ido-Yes-Or-No mode is enabled.
+See the `ido-yes-or-no-mode' command
+for a description of this minor mode.")
+
+(custom-autoload 'ido-yes-or-no-mode "ido-yes-or-no" nil)
+
+(autoload 'ido-yes-or-no-mode "ido-yes-or-no" "\
+Use ido for `yes-or-no-p'.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ido-yes-or-no" '("ido-yes-or-no-p")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ido-yes-or-no-autoloads.el ends here
---
+++ elpa/ido-yes-or-no-20161108.2351/ido-yes-or-no-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ido-yes-or-no" "20161108.2351" "Use Ido to answer yes-or-no questions" '((ido-completing-read+ "0")) :commit "c55383b1fce5879e87e7ca6809fc60534508e182" :keywords '("convenience" "completion" "ido") :authors '(("Ryan C. Thompson")) :maintainer '("Ryan C. Thompson") :url "https://github.com/DarwinAwardWinner/ido-yes-or-no")
