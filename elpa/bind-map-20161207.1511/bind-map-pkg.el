2de7a022f8334522d171abbae09b2d7cbca6218b
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:47 +0000

    [Adding bind-map version: 20161207.1511]

---
+++ elpa/bind-map-20161207.1511/bind-map-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bind-map" "20161207.1511" "Bind personal keymaps in multiple locations" '((emacs "24.3")) :commit "bf4181e3a41463684adfffc6c5c305b30480e30f" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-bind-map")
