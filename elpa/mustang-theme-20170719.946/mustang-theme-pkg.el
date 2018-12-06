354f4108a3f4763331b4d378901bb33c87e02806
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:14 +0000

    [Adding mustang-theme version: 20170719.946]

---
+++ elpa/mustang-theme-20170719.946/mustang-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mustang-theme" "20170719.946" "port of vim's mustang theme" 'nil :commit "dda6d04803f1c9b196b620ef564e7768fee15de2" :authors '(("martin haesler")) :maintainer '("martin haesler") :url "http://github.com/mswift42/mustang-theme")
