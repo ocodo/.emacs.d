d7fdf7161cb49f25f15959b7d576954a4e26b5d2
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:46 +0000

    [Adding helm-lastpass version: 20180722.806]

---
+++ elpa/helm-lastpass-20180722.806/helm-lastpass-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-lastpass" "20180722.806" "Helm interface of LastPass" '((emacs "25.1") (helm "2.0") (csv "2.1")) :commit "82e1ffb6ae77d9d9e29c398eb013cd20ce963f77" :authors '(("Xu Chunyang" . "mail@xuchunyang.me")) :maintainer '("Xu Chunyang" . "mail@xuchunyang.me") :url "https://github.com/xuchunyang/helm-lastpass")
