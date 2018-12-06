09e3fcf0be08945308c9ff3e15bcf4ef05f4d9a8
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:08 +0000

    [Adding evil-lisp-state version: 20160404.248]

---
+++ elpa/evil-lisp-state-20160404.248/evil-lisp-state-autoloads.el
@@ -0,0 +1,23 @@
+;;; evil-lisp-state-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "evil-lisp-state" "evil-lisp-state.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from evil-lisp-state.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-lisp-state" '("lisp-state-" "evil-lisp-state")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; evil-lisp-state-autoloads.el ends here
