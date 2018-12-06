30f7a5721cb08239dc6ff461ca35fd95d4896476
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:47 +0000

    [Adding el-x version: 20140111.2201]

---
+++ elpa/el-x-20140111.2201/el-x-autoloads.el
@@ -0,0 +1,33 @@
+;;; el-x-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "dflet" "dflet.el" (0 0 0 0))
+;;; Generated autoloads from dflet.el
+
+(autoload 'dflet "dflet")
+
+(autoload 'adflet "dflet" "\
+Anaphoric version of `dflet'. Binds `this-fn' to the original
+definition of the function.
+
+\(fn BINDINGS &rest BODY)" nil t)
+
+;;;***
+
+;;;### (autoloads nil nil ("el-x-pkg.el" "el-x.el" "subr-compat.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; el-x-autoloads.el ends here
---
+++ elpa/el-x-20140111.2201/el-x-pkg.el
@@ -0,0 +1,9 @@
+(define-package "el-x" "20140111.2201" "main entry point for el-x package" 'nil :keywords
+  '("lisp")
+  :authors
+  '(("Yann Hodique" . "yann.hodique@gmail.com"))
+  :maintainer
+  '("Yann Hodique" . "yann.hodique@gmail.com"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
