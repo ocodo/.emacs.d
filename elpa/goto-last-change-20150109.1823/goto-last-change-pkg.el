5529bf79a0c69c953aef0fb54bfb508d982b5327
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:18 +0000

    [Adding goto-last-change version: 20150109.1823]

---
+++ elpa/goto-last-change-20150109.1823/goto-last-change-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "goto-last-change" "20150109.1823" "Move point through buffer-undo-list positions" 'nil :commit "58b0928bc255b47aad318cd183a5dce8f62199cc" :keywords '("convenience") :authors '(("Kevin Rodgers" . "ihs_4664@yahoo.com")) :maintainer '("Kevin Rodgers" . "ihs_4664@yahoo.com") :url "https://github.com/camdez/goto-last-change.el")
