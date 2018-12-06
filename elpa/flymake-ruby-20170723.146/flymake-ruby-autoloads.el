efb3e7ea9a833a554381cb4f7d9c75064a4d4224
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:41 +0000

    [Adding flymake-ruby version: 20170723.146]

---
+++ elpa/flymake-ruby-20170723.146/flymake-ruby-autoloads.el
@@ -0,0 +1,27 @@
+;;; flymake-ruby-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-ruby" "flymake-ruby.el" (0 0 0 0))
+;;; Generated autoloads from flymake-ruby.el
+
+(autoload 'flymake-ruby-load "flymake-ruby" "\
+Configure flymake mode to check the current buffer's ruby syntax.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-ruby" '("flymake-ruby-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-ruby-autoloads.el ends here
