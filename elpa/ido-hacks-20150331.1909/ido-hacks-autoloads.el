1214f54e3a6139a405871ca0cd61b3b24f1a0567
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:13 +0000

    [Adding ido-hacks version: 20150331.1909]

---
+++ elpa/ido-hacks-20150331.1909/ido-hacks-autoloads.el
@@ -0,0 +1,22 @@
+;;; ido-hacks-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ido-hacks" "ido-hacks.el" (0 0 0 0))
+;;; Generated autoloads from ido-hacks.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ido-hacks" '("ido-" "string-prefixp")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ido-hacks-autoloads.el ends here
---
+++ elpa/ido-hacks-20150331.1909/ido-hacks-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ido-hacks" "20150331.1909" "Put more IDO in your IDO" 'nil :commit "b7e7514a0e011e4d767d1f5755c5eae9d85f83dc" :keywords '("convenience") :authors '(("Andreas Politz")) :maintainer '("Scott Jaderholm" . "jaderholm@gmail.com"))
