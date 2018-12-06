8fce7d6e2def8e1691c0e5fcd4ec89498ff782f1
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:03 +0000

    [Adding markup-faces version: 20141110.817]

---
+++ elpa/markup-faces-20141110.817/markup-faces-autoloads.el
@@ -0,0 +1,22 @@
+;;; markup-faces-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "markup-faces" "markup-faces.el" (0 0 0 0))
+;;; Generated autoloads from markup-faces.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "markup-faces" '("markup-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; markup-faces-autoloads.el ends here
---
+++ elpa/markup-faces-20141110.817/markup-faces-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markup-faces" "20141110.817" "collection of faces for markup language modes" 'nil :commit "98a807ed82473eb41c6a201ed7ef816d6bcd67b0" :keywords '("wp" "faces") :authors '(("Florian Kaufmann" . "sensorflo@gmail.com")) :maintainer '("Florian Kaufmann" . "sensorflo@gmail.com") :url "https://github.com/sensorflo/markup-faces")
