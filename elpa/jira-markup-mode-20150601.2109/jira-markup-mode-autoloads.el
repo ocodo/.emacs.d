d94b1a7436f71cfd9e27797c5728ab8e33e7c27d
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:30 +0000

    [Adding jira-markup-mode version: 20150601.2109]

---
+++ elpa/jira-markup-mode-20150601.2109/jira-markup-mode-autoloads.el
@@ -0,0 +1,28 @@
+;;; jira-markup-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "jira-markup-mode" "jira-markup-mode.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from jira-markup-mode.el
+
+(autoload 'jira-markup-mode "jira-markup-mode" "\
+Major mode for editing Jira-Markup files.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "jira-markup-mode" '("jira-markup-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; jira-markup-mode-autoloads.el ends here
---
+++ elpa/jira-markup-mode-20150601.2109/jira-markup-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "jira-markup-mode" "20150601.2109" "Emacs Major mode for JIRA-markup-formatted text files" 'nil :commit "4fc534c47df26a2f402bf835ebe2ed89474a4062" :keywords '("jira" "markup") :authors '(("Matthias Nuessler" . "m.nuessler@web.de>")) :maintainer '("Matthias Nuessler" . "m.nuessler@web.de>") :url "https://github.com/mnuessler/jira-markup-mode")
