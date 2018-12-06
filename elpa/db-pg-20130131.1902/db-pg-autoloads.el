ad382458b4d673eaab317aa0a0eee0c8f0836107
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:28 +0000

    [Adding db-pg version: 20130131.1902]

---
+++ elpa/db-pg-20130131.1902/db-pg-autoloads.el
@@ -0,0 +1,22 @@
+;;; db-pg-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "db-pg" "db-pg.el" (0 0 0 0))
+;;; Generated autoloads from db-pg.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "db-pg" '("db-pg")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; db-pg-autoloads.el ends here
---
+++ elpa/db-pg-20130131.1902/db-pg-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "db-pg" "20130131.1902" "A PostgreSQL adapter for emacs-db" '((pg "0.12") (db "0.0.6")) :commit "7d5ab86b74b05fe003b3b434d4835f37f3f3eded" :keywords '("data" "comm" "database" "postgresql") :authors '(("Nic Ferrier" . "nic@ferrier.me.uk")) :maintainer '("Nic Ferrier" . "nic@ferrier.me.uk"))
