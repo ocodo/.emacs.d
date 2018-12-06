4c61f4a5461ec0e87f1ade47b1577828ced11c83
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:36 +0000

    [Adding seoul256-theme version: 20180505.757]

---
+++ elpa/seoul256-theme-20180505.757/seoul256-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "seoul256-theme" "20180505.757" "Low-contrast color scheme based on Seoul Colors." '((emacs "24.3")) :commit "d28a9de73a5ffb1a1c9492db75a5c1efe5e9815f" :keywords '("theme") :authors '(("Anand Iyer" . "anand.ucb@gmail.com")) :maintainer '("Anand Iyer" . "anand.ucb@gmail.com") :url "http://github.com/anandpiyer/seoul256-emacs")
