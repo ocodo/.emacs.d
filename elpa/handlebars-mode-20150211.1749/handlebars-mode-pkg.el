345840af858f732e193299cbfdbd45d10e746b48
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:26 +0000

    [Adding handlebars-mode version: 20150211.1749]

---
+++ elpa/handlebars-mode-20150211.1749/handlebars-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "handlebars-mode" "20150211.1749" "A major mode for editing Handlebars files." 'nil :commit "81f6b73fea8f397807781a1b51568397af21a6ef" :authors '(("Tony Gentilcore") ("Chris Wanstrath") ("Daniel Hackney") ("Daniel Evans")) :maintainer '("Tony Gentilcore"))
