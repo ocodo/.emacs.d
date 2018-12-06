23bae1f5baf0c09efa8f3376045a0ba64ef1f22e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:33 +0000

    [Adding parent-mode version: 20150824.2300]

---
+++ elpa/parent-mode-20150824.2300/parent-mode-autoloads.el
@@ -0,0 +1,22 @@
+;;; parent-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "parent-mode" "parent-mode.el" (0 0 0 0))
+;;; Generated autoloads from parent-mode.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "parent-mode" '("parent-mode-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; parent-mode-autoloads.el ends here
