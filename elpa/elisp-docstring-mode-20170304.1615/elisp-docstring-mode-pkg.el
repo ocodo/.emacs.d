00f04f29bd985b91c44905c7655576d8b226ce09
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:50 +0000

    [Adding elisp-docstring-mode version: 20170304.1615]

---
+++ elpa/elisp-docstring-mode-20170304.1615/elisp-docstring-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elisp-docstring-mode" "20170304.1615" "Major mode for editing elisp docstrings." 'nil :commit "f512e509dd690f65133e55563ebbfd2dede5034f" :keywords '("languages") :authors '(("Matúš Goljer" . "matus.goljer@gmail.com")) :maintainer '("Matúš Goljer" . "matus.goljer@gmail.com"))
