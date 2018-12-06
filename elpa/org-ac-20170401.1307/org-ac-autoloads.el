7007ac7b09cca10810279d9d0b6072f59ef9fab4
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:25 +0000

    [Adding org-ac version: 20170401.1307]

---
+++ elpa/org-ac-20170401.1307/org-ac-autoloads.el
@@ -0,0 +1,32 @@
+;;; org-ac-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "org-ac" "org-ac.el" (0 0 0 0))
+;;; Generated autoloads from org-ac.el
+
+(autoload 'org-ac/setup-current-buffer "org-ac" "\
+Do setup for using org-ac in current buffer.
+
+\(fn)" t nil)
+
+(autoload 'org-ac/config-default "org-ac" "\
+Do setting recommemded configuration.
+
+\(fn)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "org-ac" '("org-ac" "ac-source-org-ac-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; org-ac-autoloads.el ends here
---
+++ elpa/org-ac-20170401.1307/org-ac-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-ac" "20170401.1307" "Some auto-complete sources for org-mode" '((auto-complete-pcmp "0.0.1") (log4e "0.2.0") (yaxception "0.1")) :commit "41e3ef8e4039619d0370c23c66730b3b2e9e32ed" :keywords '("org" "completion") :authors '(("Hiroaki Otsu" . "ootsuhiroaki@gmail.com")) :maintainer '("Hiroaki Otsu" . "ootsuhiroaki@gmail.com") :url "https://github.com/aki2o/org-ac")
