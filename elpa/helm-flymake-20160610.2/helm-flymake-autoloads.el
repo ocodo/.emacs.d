95442b604acbe05b46e5ac52d71bbe7b92054399
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:41 +0000

    [Adding helm-flymake version: 20160610.2]

---
+++ elpa/helm-flymake-20160610.2/helm-flymake-autoloads.el
@@ -0,0 +1,27 @@
+;;; helm-flymake-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-flymake" "helm-flymake.el" (0 0 0 0))
+;;; Generated autoloads from helm-flymake.el
+
+(autoload 'helm-flymake "helm-flymake" "\
+helm interface for flymake.
+
+\(fn ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-flymake" '("helm-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-flymake-autoloads.el ends here
---
+++ elpa/helm-flymake-20160610.2/helm-flymake-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-flymake" "20160610.2" "helm interface for flymake" '((helm "1.0")) :commit "72cf18a1a1f843db9bb5d58301739ea9ccb1655b" :authors '(("Akira Tamamori" . "tamamori5917@gmail.com")) :maintainer '("Akira Tamamori" . "tamamori5917@gmail.com") :url "https://github.com/tam17aki")
