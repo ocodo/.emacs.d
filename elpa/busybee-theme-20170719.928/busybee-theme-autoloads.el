171d96a73a888d49f81b7c5b17498405bbc47aa4
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:52 +0000

    [Adding busybee-theme version: 20170719.928]

---
+++ elpa/busybee-theme-20170719.928/busybee-theme-autoloads.el
@@ -0,0 +1,22 @@
+;;; busybee-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "busybee-theme" "busybee-theme.el" (0 0 0 0))
+;;; Generated autoloads from busybee-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; busybee-theme-autoloads.el ends here
