ad6affbb31c72cb78c5b6115c4ca9e6318d036ba
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:06 +0000

    [Adding evil-god-state version: 20141117.255]

---
+++ elpa/evil-god-state-20141117.255/evil-god-state-autoloads.el
@@ -0,0 +1,28 @@
+;;; evil-god-state-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "evil-god-state" "evil-god-state.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from evil-god-state.el
+
+(autoload 'evil-execute-in-god-state "evil-god-state" "\
+Execute the next command in God state.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-god-state" '("evil-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; evil-god-state-autoloads.el ends here
