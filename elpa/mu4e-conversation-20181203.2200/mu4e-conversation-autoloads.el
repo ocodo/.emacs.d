14e3d0dc552069036e60db3b87960bb4d7d6d988
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:09 +0000

    [Adding mu4e-conversation version: 20181203.2200]

---
+++ elpa/mu4e-conversation-20181203.2200/mu4e-conversation-autoloads.el
@@ -0,0 +1,28 @@
+;;; mu4e-conversation-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "mu4e-conversation" "mu4e-conversation.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from mu4e-conversation.el
+
+(autoload 'mu4e-conversation "mu4e-conversation" "\
+
+
+\(fn &optional MSG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "mu4e-conversation" '("global-mu4e-conversation-mode" "mu4e-conversation-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; mu4e-conversation-autoloads.el ends here
