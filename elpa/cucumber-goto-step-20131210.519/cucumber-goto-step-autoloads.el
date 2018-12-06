836cf023dd2175a3c4a21ebc11ca3dbf83d1e9d1
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:22 +0000

    [Adding cucumber-goto-step version: 20131210.519]

---
+++ elpa/cucumber-goto-step-20131210.519/cucumber-goto-step-autoloads.el
@@ -0,0 +1,28 @@
+;;; cucumber-goto-step-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "cucumber-goto-step" "cucumber-goto-step.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from cucumber-goto-step.el
+
+(autoload 'jump-to-cucumber-step "cucumber-goto-step" "\
+Jumps to a step definition.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "cucumber-goto-step" '("cgs-" "*cgs-default-root-markers*")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; cucumber-goto-step-autoloads.el ends here
---
+++ elpa/cucumber-goto-step-20131210.519/cucumber-goto-step-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cucumber-goto-step" "20131210.519" "Jump to cucumber step definition" '((pcre2el "1.5")) :commit "f2713ffb26ebe1b757d1f2ea80e900b55e5895aa" :authors '(("Glen Stampoultzis" . "gstamp@gmail.com")) :maintainer '("Glen Stampoultzis" . "gstamp@gmail.com") :url "http://orthogonal.me")
