9dcf931100887fb0c163a1ad2b2a453ad6ef0624
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:41 +0000

    [Adding helm-git version: 20120630.2103]

---
+++ elpa/helm-git-20120630.2103/helm-git-autoloads.el
@@ -0,0 +1,22 @@
+;;; helm-git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-git" "helm-git.el" (0 0 0 0))
+;;; Generated autoloads from helm-git.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-git" '("helm-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-git-autoloads.el ends here
---
+++ elpa/helm-git-20120630.2103/helm-git-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-git" "20120630.2103" "Helm extension for Git." 'nil :commit "cb96a52b5aecadd3c27aba7749d14e43ab128d55" :keywords '("helm" "git") :authors '(("Marian Schubert" . "marian.schubert@gmail.com")) :maintainer '("Marian Schubert" . "marian.schubert@gmail.com") :url "https://github.com/maio/helm-git")
