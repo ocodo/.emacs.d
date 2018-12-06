c54cad32167e1e727f37b40ca852e8a1f632e123
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:16 +0000

    [Adding rails-log-mode version: 20140408.425]

---
+++ elpa/rails-log-mode-20140408.425/rails-log-mode-autoloads.el
@@ -0,0 +1,38 @@
+;;; rails-log-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "rails-log-mode" "rails-log-mode.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from rails-log-mode.el
+
+(autoload 'rails-log-show-development "rails-log-mode" "\
+
+
+\(fn)" t nil)
+
+(autoload 'rails-log-show-test "rails-log-mode" "\
+
+
+\(fn)" t nil)
+
+(autoload 'rails-log-show-production "rails-log-mode" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "rails-log-mode" '("rails-log-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; rails-log-mode-autoloads.el ends here
---
+++ elpa/rails-log-mode-20140408.425/rails-log-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rails-log-mode" "20140408.425" "Major mode for viewing Rails log files" 'nil :commit "ff440003ad7d47cb0ac3300f2a632f4cfd36a446" :keywords '("rails" "log") :authors '(("Anantha kumaran" . "ananthakumaran@gmail.com")) :maintainer '("Anantha kumaran" . "ananthakumaran@gmail.com"))
