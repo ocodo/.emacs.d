46438379691cee2366f651966274a4f7c844a645
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:23 +0000

    [Adding wordsmith-mode version: 20171025.1430]

---
+++ elpa/wordsmith-mode-20171025.1430/wordsmith-mode-autoloads.el
@@ -0,0 +1,31 @@
+;;; wordsmith-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "wordsmith-mode" "wordsmith-mode.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from wordsmith-mode.el
+
+(autoload 'wordsmith-mode "wordsmith-mode" "\
+Toggle wordsmith mode.
+Interactively with no argument, this command toggles the mode.
+to show buffer size and position in mode-line.  You can customize
+this minor mode, see option `wordsmith-mode'. 
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wordsmith-mode" '("wordsmith-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; wordsmith-mode-autoloads.el ends here
---
+++ elpa/wordsmith-mode-20171025.1430/wordsmith-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "wordsmith-mode" "20171025.1430" "Syntax analysis and NLP text-processing in Emacs (OSX-only)" 'nil :commit "589a97412138145bea70e0450eeddeb7f138d538" :authors '(("istib" . "istib@thebati.net")) :maintainer '("istib" . "istib@thebati.net"))
