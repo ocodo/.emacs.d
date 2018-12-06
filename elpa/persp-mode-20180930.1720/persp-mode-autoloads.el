ac92194572fa46961320fa78b8ede0750ae5b455
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:38 +0000

    [Adding persp-mode version: 20180930.1720]

---
+++ elpa/persp-mode-20180930.1720/persp-mode-autoloads.el
@@ -0,0 +1,55 @@
+;;; persp-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "persp-mode" "persp-mode.el" (0 0 0 0))
+;;; Generated autoloads from persp-mode.el
+
+(autoload 'persp-def-auto-persp "persp-mode" "\
+
+
+\(fn NAME &rest KEYARGS &key BUFFER-NAME FILE-NAME MODE MODE-NAME MINOR-MODE MINOR-MODE-NAME PREDICATE HOOKS DYN-ENV GET-NAME GET-BUFFER GET-PERSP SWITCH PARAMETERS NOAUTO WEAK USER-DATA ON-MATCH AFTER-MATCH DONT-PICK-UP-BUFFERS DELETE)" nil nil)
+
+(define-obsolete-function-alias 'def-auto-persp 'persp-def-auto-persp "persp-mode 2.9.6")
+
+(autoload 'persp-def-buffer-save/load "persp-mode" "\
+
+
+\(fn &rest KEYARGS &key BUFFER-NAME FILE-NAME MODE MODE-NAME MINOR-MODE MINOR-MODE-NAME PREDICATE TAG-SYMBOL SAVE-VARS SAVE-FUNCTION LOAD-FUNCTION AFTER-LOAD-FUNCTION MODE-RESTORE-FUNCTION APPEND)" nil nil)
+
+(define-obsolete-function-alias 'def-persp-buffer-save/load 'persp-def-buffer-save/load "persp-mode 2.9.6")
+
+(defvar persp-mode nil "\
+Non-nil if Persp mode is enabled.
+See the `persp-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `persp-mode'.")
+
+(custom-autoload 'persp-mode "persp-mode" nil)
+
+(autoload 'persp-mode "persp-mode" "\
+Toggle the persp-mode.
+When active, keeps track of multiple 'perspectives',
+named collections of buffers and window configurations.
+Here is a keymap of this minor mode:
+\\{persp-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "persp-mode" '("persp" "ido-toggle-persp-filter" "find-other-frame-with-persp" "safe-persp-" "set-" "get-" "clear-window-persp" "with-persp-" "window-persp-set-p" "delete-persp-parameter" "modify-persp-parameters" "*persp-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; persp-mode-autoloads.el ends here
---
+++ elpa/persp-mode-20180930.1720/persp-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "persp-mode" "20180930.1720" "windows/buffers sets shared among frames + save/load." 'nil :commit "689f63e7370cd9424d84b9f7b2eb3d1955443313" :keywords '("perspectives" "session" "workspace" "persistence" "windows" "buffers" "convenience") :authors '(("Constantin Kulikov (Bad_ptr)" . "zxnotdead@gmail.com")) :maintainer '("Constantin Kulikov (Bad_ptr)" . "zxnotdead@gmail.com") :url "https://github.com/Bad-ptr/persp-mode.el")
