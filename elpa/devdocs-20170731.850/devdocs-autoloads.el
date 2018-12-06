6bf08063d28c21029f5705f4d735c4315007cd11
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:30 +0000

    [Adding devdocs version: 20170731.850]

---
+++ elpa/devdocs-20170731.850/devdocs-autoloads.el
@@ -0,0 +1,28 @@
+;;; devdocs-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "devdocs" "devdocs.el" (0 0 0 0))
+;;; Generated autoloads from devdocs.el
+
+(autoload 'devdocs-search "devdocs" "\
+Launch Devdocs search.
+CONFIRM goes with asking for confirmation.
+
+\(fn &optional CONFIRM)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "devdocs" '("devdocs-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; devdocs-autoloads.el ends here
---
+++ elpa/devdocs-20170731.850/devdocs-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "devdocs" "20170731.850" "Launch DevDocs search" 'nil :commit "a2d51e824f0cc48a9dd611cc740bc8b86143e611" :authors '(("Chunyang Xu" . "xuchunyang.me@gmail.com")) :maintainer '("Chunyang Xu" . "xuchunyang.me@gmail.com") :url "https://github.com/xuchunyang/DevDocs.el")
