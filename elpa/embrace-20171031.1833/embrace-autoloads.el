120ce305a0f46b8d9faa10f6b663d0e4ab34ff9a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:56 +0000

    [Adding embrace version: 20171031.1833]

---
+++ elpa/embrace-20171031.1833/embrace-autoloads.el
@@ -0,0 +1,62 @@
+;;; embrace-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "embrace" "embrace.el" (0 0 0 0))
+;;; Generated autoloads from embrace.el
+
+(autoload 'embrace-delete "embrace" "\
+
+
+\(fn)" t nil)
+
+(autoload 'embrace-change "embrace" "\
+
+
+\(fn)" t nil)
+
+(autoload 'embrace-add "embrace" "\
+
+
+\(fn)" t nil)
+
+(autoload 'embrace-commander "embrace" "\
+
+
+\(fn)" t nil)
+
+(autoload 'embrace-LaTeX-mode-hook "embrace" "\
+
+
+\(fn)" nil nil)
+
+(autoload 'embrace-org-mode-hook "embrace" "\
+
+
+\(fn)" nil nil)
+
+(autoload 'embrace-ruby-mode-hook "embrace" "\
+
+
+\(fn)" nil nil)
+
+(autoload 'embrace-emacs-lisp-mode-hook "embrace" "\
+
+
+\(fn)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "embrace" '("embrace-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; embrace-autoloads.el ends here
---
+++ elpa/embrace-20171031.1833/embrace-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "embrace" "20171031.1833" "Add/Change/Delete pairs based on `expand-region'" '((cl-lib "0.5") (expand-region "0.10.0")) :commit "dd5da196e5bcc5e6d87e1937eca0c21da4334ef2" :keywords '("extensions") :authors '(("Junpeng Qiu" . "qjpchmail@gmail.com")) :maintainer '("Junpeng Qiu" . "qjpchmail@gmail.com"))
