4fb111da382121f6d81f7998f08535b0df6b65c6
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:29 +0000

    [Adding hc-zenburn-theme version: 20150928.1633]

---
+++ elpa/hc-zenburn-theme-20150928.1633/hc-zenburn-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; hc-zenburn-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "hc-zenburn-theme" "hc-zenburn-theme.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from hc-zenburn-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "hc-zenburn-theme" '("hc-zenburn")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; hc-zenburn-theme-autoloads.el ends here
---
+++ elpa/hc-zenburn-theme-20150928.1633/hc-zenburn-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "hc-zenburn-theme" "20150928.1633" "An higher contrast version of the Zenburn theme." 'nil :commit "fd0024a5191cdce204d91c8f1db99ba31640f6e9" :authors '(("Nantas Nardelli" . "nantas.nardelli@gmail.com")) :maintainer '("Nantas Nardelli" . "nantas.nardelli@gmail.com") :url "https:github.com/edran/hc-zenburn-emacs")
