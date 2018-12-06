3062f487f4aafeb898580a7ffcb6c58f999b32c5
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:14 +0000

    [Adding wanderlust version: 20181117.717]

---
+++ elpa/wanderlust-20181117.717/wanderlust-autoloads.el
@@ -0,0 +1,575 @@
+;;; wanderlust-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "acap" "acap.el" (0 0 0 0))
+;;; Generated autoloads from acap.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "acap" '("acap-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo" "elmo.el" (0 0 0 0))
+;;; Generated autoloads from elmo.el
+
+(autoload 'elmo-make-folder "elmo" "\
+Make an ELMO folder structure specified by NAME.
+If optional argument NON-PERSISTENT is non-nil, the folder msgdb is not saved.
+If optional argument MIME-CHARSET is specified, it is used for
+encode and decode a multibyte string.
+
+\(fn NAME &optional NON-PERSISTENT MIME-CHARSET)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-access" "elmo-access.el" (0 0 0 0))
+;;; Generated autoloads from elmo-access.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-access" '("elmo-access-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-archive" "elmo-archive.el" (0 0 0 0))
+;;; Generated autoloads from elmo-archive.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-archive" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-cache" "elmo-cache.el" (0 0 0 0))
+;;; Generated autoloads from elmo-cache.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-cache" '("elmo-cache-folder-list-message-locations")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-date" "elmo-date.el" (0 0 0 0))
+;;; Generated autoloads from elmo-date.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-date" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-dop" "elmo-dop.el" (0 0 0 0))
+;;; Generated autoloads from elmo-dop.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-dop" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-file" "elmo-file.el" (0 0 0 0))
+;;; Generated autoloads from elmo-file.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-file" '("elmo-file-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-filter" "elmo-filter.el" (0 0 0 0))
+;;; Generated autoloads from elmo-filter.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-filter" '("elmo-f")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-flag" "elmo-flag.el" (0 0 0 0))
+;;; Generated autoloads from elmo-flag.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-flag" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-imap4" "elmo-imap4.el" (0 0 0 0))
+;;; Generated autoloads from elmo-imap4.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-imap4" '("elmo-" "with-elmo-imap4-session-process-buffer")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-internal" "elmo-internal.el" (0 0 0 0))
+;;; Generated autoloads from elmo-internal.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-internal" '("elmo-internal-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-localdir" "elmo-localdir.el" (0 0 0 0))
+;;; Generated autoloads from elmo-localdir.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-localdir" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-localnews" "elmo-localnews.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from elmo-localnews.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-localnews" '("elmo-localnews-folder-path")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-maildir" "elmo-maildir.el" (0 0 0 0))
+;;; Generated autoloads from elmo-maildir.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-maildir" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-map" "elmo-map.el" (0 0 0 0))
+;;; Generated autoloads from elmo-map.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-map" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-mime" "elmo-mime.el" (0 0 0 0))
+;;; Generated autoloads from elmo-mime.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-mime" '("elmo-m")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-msgdb" "elmo-msgdb.el" (0 0 0 0))
+;;; Generated autoloads from elmo-msgdb.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-msgdb" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-multi" "elmo-multi.el" (0 0 0 0))
+;;; Generated autoloads from elmo-multi.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-multi" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-net" "elmo-net.el" (0 0 0 0))
+;;; Generated autoloads from elmo-net.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-net" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-nntp" "elmo-nntp.el" (0 0 0 0))
+;;; Generated autoloads from elmo-nntp.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-nntp" '("elmo-nntp-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-null" "elmo-null.el" (0 0 0 0))
+;;; Generated autoloads from elmo-null.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-null" '("elmo-folder-append-messages-*-null")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-passwd" "elmo-passwd.el" (0 0 0 0))
+;;; Generated autoloads from elmo-passwd.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-passwd" '("elmo-passwd-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-pipe" "elmo-pipe.el" (0 0 0 0))
+;;; Generated autoloads from elmo-pipe.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-pipe" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-pop3" "elmo-pop3.el" (0 0 0 0))
+;;; Generated autoloads from elmo-pop3.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-pop3" '("elmo-pop3-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-rss" "elmo-rss.el" (0 0 0 0))
+;;; Generated autoloads from elmo-rss.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-rss" '("elmo-rss-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-search" "elmo-search.el" (0 0 0 0))
+;;; Generated autoloads from elmo-search.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-search" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-sendlog" "elmo-sendlog.el" (0 0 0 0))
+;;; Generated autoloads from elmo-sendlog.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-sendlog" '("elmo-sendlog-f")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-shimbun" "elmo-shimbun.el" (0 0 0 0))
+;;; Generated autoloads from elmo-shimbun.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-shimbun" '("elmo-shimbun-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-signal" "elmo-signal.el" (0 0 0 0))
+;;; Generated autoloads from elmo-signal.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-signal" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-spam" "elmo-spam.el" (0 0 0 0))
+;;; Generated autoloads from elmo-spam.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-spam" '("elmo-spam-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-split" "elmo-split.el" (0 0 0 0))
+;;; Generated autoloads from elmo-split.el
+
+(autoload 'elmo-split "elmo-split" "\
+Split messages in the `elmo-split-folder' according to `elmo-split-rule'.
+If prefix argument ARG is specified, do a reharsal (no harm).
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-split" '("elmo-split-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-util" "elmo-util.el" (0 0 0 0))
+;;; Generated autoloads from elmo-util.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-util" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-vars" "elmo-vars.el" (0 0 0 0))
+;;; Generated autoloads from elmo-vars.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-vars" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elmo-version" "elmo-version.el" (0 0 0 0))
+;;; Generated autoloads from elmo-version.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elmo-version" '("elmo-")))
+
+;;;***
+
+;;;### (autoloads nil "elsp-bogofilter" "elsp-bogofilter.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from elsp-bogofilter.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elsp-bogofilter" '("elsp-bogofilter-register-buffer" "elmo-spam-bogofilter-")))
+
+;;;***
+
+;;;### (autoloads nil "elsp-bsfilter" "elsp-bsfilter.el" (0 0 0 0))
+;;; Generated autoloads from elsp-bsfilter.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elsp-bsfilter" '("elsp-bsfilter-" "elmo-spam-bsfilter-")))
+
+;;;***
+
+;;;### (autoloads nil "elsp-sa" "elsp-sa.el" (0 0 0 0))
+;;; Generated autoloads from elsp-sa.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elsp-sa" '("elmo-spam")))
+
+;;;***
+
+;;;### (autoloads nil "elsp-spamfilter" "elsp-spamfilter.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from elsp-spamfilter.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elsp-spamfilter" '("elmo-spam-spamfilter-corpus-filename" "elsp-spamfilter-")))
+
+;;;***
+
+;;;### (autoloads nil "elsp-spamoracle" "elsp-spamoracle.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from elsp-spamoracle.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elsp-spamoracle" '("elmo-spam-spamoracle-")))
+
+;;;***
+
+;;;### (autoloads nil "mmimap" "mmimap.el" (0 0 0 0))
+;;; Generated autoloads from mmimap.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "mmimap" '("mmimap-" "mime-imap-entity-header-string")))
+
+;;;***
+
+;;;### (autoloads nil "modb" "modb.el" (0 0 0 0))
+;;; Generated autoloads from modb.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "modb" '("elmo-msgdb-match-condition-primitive")))
+
+;;;***
+
+;;;### (autoloads nil "modb-entity" "modb-entity.el" (0 0 0 0))
+;;; Generated autoloads from modb-entity.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "modb-entity" '("modb-" "elmo-m")))
+
+;;;***
+
+;;;### (autoloads nil "modb-legacy" "modb-legacy.el" (0 0 0 0))
+;;; Generated autoloads from modb-legacy.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "modb-legacy" '("elmo-msgdb-" "modb-legacy-")))
+
+;;;***
+
+;;;### (autoloads nil "modb-standard" "modb-standard.el" (0 0 0 0))
+;;; Generated autoloads from modb-standard.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "modb-standard" '("modb-standard-")))
+
+;;;***
+
+;;;### (autoloads nil "pldap" "pldap.el" (0 0 0 0))
+;;; Generated autoloads from pldap.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "pldap" '("ldap-static-if")))
+
+;;;***
+
+;;;### (autoloads nil "slp" "slp.el" (0 0 0 0))
+;;; Generated autoloads from slp.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slp" '("slp-")))
+
+;;;***
+
+;;;### (autoloads nil "wl" "wl.el" (0 0 0 0))
+;;; Generated autoloads from wl.el
+
+(autoload 'wl "wl" "\
+Start Wanderlust -- Yet Another Message Interface On Emacsen.
+If ARG (prefix argument) is specified, folder checkings are skipped.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'wl-other-frame "wl" "\
+Pop up a frame to read messages via Wanderlust.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-acap" "wl-acap.el" (0 0 0 0))
+;;; Generated autoloads from wl-acap.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-acap" '("wl-acap-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-action" "wl-action.el" (0 0 0 0))
+;;; Generated autoloads from wl-action.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-action" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-address" "wl-address.el" (0 0 0 0))
+;;; Generated autoloads from wl-address.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-address" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-addrmgr" "wl-addrmgr.el" (0 0 0 0))
+;;; Generated autoloads from wl-addrmgr.el
+
+(autoload 'wl-addrmgr "wl-addrmgr" "\
+Start an Address manager.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-addrmgr" '("wl-addrmgr-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-batch" "wl-batch.el" (0 0 0 0))
+;;; Generated autoloads from wl-batch.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-batch" '("wl-batch-prefetch")))
+
+;;;***
+
+;;;### (autoloads nil "wl-demo" "wl-demo.el" (0 0 0 0))
+;;; Generated autoloads from wl-demo.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-demo" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-draft" "wl-draft.el" (0 0 0 0))
+;;; Generated autoloads from wl-draft.el
+
+(autoload 'wl-draft "wl-draft" "\
+Write and send mail/news message with Wanderlust.
+
+\(fn &optional HEADER-ALIST CONTENT-TYPE CONTENT-TRANSFER-ENCODING BODY EDIT-AGAIN PARENT-FOLDER PARENT-NUMBER)" t nil)
+
+(autoload 'wl-user-agent-compose "wl-draft" "\
+Support the `compose-mail' interface for wl.
+Only support for TO, SUBJECT, and OTHER-HEADERS has been implemented.
+Support for CONTINUE, YANK-ACTION, SEND-ACTIONS and RETURN-ACTION has not
+been implemented yet.  Partial support for SWITCH-FUNCTION now supported.
+
+\(fn &optional TO SUBJECT OTHER-HEADERS CONTINUE SWITCH-FUNCTION YANK-ACTION SEND-ACTIONS RETURN-ACTION)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-draft" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-e21" "wl-e21.el" (0 0 0 0))
+;;; Generated autoloads from wl-e21.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-e21" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-expire" "wl-expire.el" (0 0 0 0))
+;;; Generated autoloads from wl-expire.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-expire" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-fldmgr" "wl-fldmgr.el" (0 0 0 0))
+;;; Generated autoloads from wl-fldmgr.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-fldmgr" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-folder" "wl-folder.el" (0 0 0 0))
+;;; Generated autoloads from wl-folder.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-folder" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-highlight" "wl-highlight.el" (0 0 0 0))
+;;; Generated autoloads from wl-highlight.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-highlight" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-message" "wl-message.el" (0 0 0 0))
+;;; Generated autoloads from wl-message.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-message" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-mime" "wl-mime.el" (0 0 0 0))
+;;; Generated autoloads from wl-mime.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-mime" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-qs" "wl-qs.el" (0 0 0 0))
+;;; Generated autoloads from wl-qs.el
+
+(autoload 'wl-quicksearch-goto-search-folder-wrapper "wl-qs" "\
+Call `wl-quicksearch-goto-search-folder' on a folder built from `wl-quicksearch-folder'.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-qs" '("wl-quicksearch-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-refile" "wl-refile.el" (0 0 0 0))
+;;; Generated autoloads from wl-refile.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-refile" '("wl-refile-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-score" "wl-score.el" (0 0 0 0))
+;;; Generated autoloads from wl-score.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-score" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-spam" "wl-spam.el" (0 0 0 0))
+;;; Generated autoloads from wl-spam.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-spam" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-summary" "wl-summary.el" (0 0 0 0))
+;;; Generated autoloads from wl-summary.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-summary" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-template" "wl-template.el" (0 0 0 0))
+;;; Generated autoloads from wl-template.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-template" '("wl-template")))
+
+;;;***
+
+;;;### (autoloads nil "wl-thread" "wl-thread.el" (0 0 0 0))
+;;; Generated autoloads from wl-thread.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-thread" '("wl-thread-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-util" "wl-util.el" (0 0 0 0))
+;;; Generated autoloads from wl-util.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-util" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-vars" "wl-vars.el" (0 0 0 0))
+;;; Generated autoloads from wl-vars.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-vars" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil "wl-version" "wl-version.el" (0 0 0 0))
+;;; Generated autoloads from wl-version.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wl-version" '("wl-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("wanderlust-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; wanderlust-autoloads.el ends here
---
+++ elpa/wanderlust-20181117.717/wanderlust-pkg.el
@@ -0,0 +1,5 @@
+(define-package "wanderlust" "20181117.717" "Yet Another Message Interface on Emacsen"
+  '((semi "1.14.7")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
