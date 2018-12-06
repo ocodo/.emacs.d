1e926c2efe432db5ddb2430438978d7ff3574edc
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:47 +0000

    [Adding lines-at-once version: 20180422.247]

---
+++ elpa/lines-at-once-20180422.247/lines-at-once-autoloads.el
@@ -0,0 +1,27 @@
+;;; lines-at-once-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "lines-at-once" "lines-at-once.el" (0 0 0 0))
+;;; Generated autoloads from lines-at-once.el
+
+(autoload 'lines-at-once-insert "lines-at-once" "\
+Expand the line at point to multiple lines.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "lines-at-once" '("lines-at-once-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lines-at-once-autoloads.el ends here
---
+++ elpa/lines-at-once-20180422.247/lines-at-once-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lines-at-once" "20180422.247" "Insert and edit multiple lines at once" '((emacs "25")) :commit "a018ba90549384d52ec58c2685fd14a0f65252be" :keywords '("abbrev" "tools") :authors '(("Jiahao Li" . "jiahaowork@gmail.com")) :maintainer '("Jiahao Li" . "jiahaowork@gmail.com") :url "https://github.com/jiahaowork/lines-at-once.el")
