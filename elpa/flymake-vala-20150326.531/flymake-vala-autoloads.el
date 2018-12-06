17cdcde519e85ba358544a7f77913419ac0c0e04
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:43 +0000

    [Adding flymake-vala version: 20150326.531]

---
+++ elpa/flymake-vala-20150326.531/flymake-vala-autoloads.el
@@ -0,0 +1,27 @@
+;;; flymake-vala-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-vala" "flymake-vala.el" (0 0 0 0))
+;;; Generated autoloads from flymake-vala.el
+
+(autoload 'flymake-vala-load "flymake-vala" "\
+Configure flymake mode to check the current buffer's vala syntax.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-vala" '("flymake-vala-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-vala-autoloads.el ends here
---
+++ elpa/flymake-vala-20150326.531/flymake-vala-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-vala" "20150326.531" "A flymake handler for vala-mode files" '((flymake-easy "0.1")) :commit "c3674f461fc84fb0300cd3a562fb903a59782745" :keywords '("convenience" "vala") :authors '(("Daniel Lawrence" . "dannyla@linux.com")) :maintainer '("Daniel Lawrence" . "dannyla@linux.com") :url "https://github.com/daniellawrence/flymake-vala")
