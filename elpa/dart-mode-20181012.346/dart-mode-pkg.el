29c81f67a0c30edf61604f6ce9309b08257a1de4
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:25 +0000

    [Adding dart-mode version: 20181012.346]

---
+++ elpa/dart-mode-20181012.346/dart-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dart-mode" "20181012.346" "Major mode for editing Dart files" '((emacs "24.5") (cl-lib "0.5") (dash "2.10.0") (flycheck "0.23") (s "1.10")) :commit "c30eaa9a8ef21a51a965544f67a454e172c9689f" :keywords '("language") :authors '(("Natalie Weizenbaum")) :maintainer '("Natalie Weizenbaum") :url "https://github.com/nex3/dart-mode")
