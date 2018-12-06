4292677fa6ce6f58751ea9d3a1439af5f67cea50
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:29 +0000

    [Adding yabin version: 20140206.351]

---
+++ elpa/yabin-20140206.351/yabin-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yabin" "20140206.351" "Yet Another Bignum package (A thin wrapper of calc.el)." 'nil :commit "db8c404507560ef9147fcce2b94cd706fbfa03b5" :keywords '("data") :authors '(("Daisuke Kobayashi" . "d5884jp@gmail.com")) :maintainer '("Daisuke Kobayashi" . "d5884jp@gmail.com"))
