26973270d9f1b28dc2fc8e92963b34c09dca4277
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:11 +0000

    [Adding company-flx version: 20180103.518]

---
+++ elpa/company-flx-20180103.518/company-flx-autoloads.el
@@ -0,0 +1,37 @@
+;;; company-flx-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "company-flx" "company-flx.el" (0 0 0 0))
+;;; Generated autoloads from company-flx.el
+
+(defvar company-flx-mode nil "\
+Non-nil if Company-Flx mode is enabled.
+See the `company-flx-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `company-flx-mode'.")
+
+(custom-autoload 'company-flx-mode "company-flx" nil)
+
+(autoload 'company-flx-mode "company-flx" "\
+company-flx minor mode
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "company-flx" '("company-flx-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; company-flx-autoloads.el ends here
---
+++ elpa/company-flx-20180103.518/company-flx-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-flx" "20180103.518" "flx based fuzzy matching for company" '((emacs "24") (company "0.8.12") (flx "0.5")) :commit "16ca0d2f84e8e768bf2db8c5cfe421230a00bded" :keywords '("convenience" "company" "fuzzy" "flx") :authors '(("PythonNut" . "pythonnut@pythonnut.com")) :maintainer '("PythonNut" . "pythonnut@pythonnut.com") :url "https://github.com/PythonNut/company-flx")
