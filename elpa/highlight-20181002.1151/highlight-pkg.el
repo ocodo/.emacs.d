28d0fa85a316b271636180d0863af5dd9055fc31
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:58 +0000

    [Adding highlight version: 20181002.1151]

---
+++ elpa/highlight-20181002.1151/highlight-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "highlight" "20181002.1151" "Highlighting commands." 'nil :commit "ea733e17884aeae19172407e20559fc693fdd3a7" :keywords '("faces" "help" "local") :authors '(("Drew Adams")) :maintainer '(nil . "Drew Adams (concat \"drew.adams\" \"@\" \"oracle\" \".com\")") :url "https://www.emacswiki.org/emacs/download/highlight.el")
