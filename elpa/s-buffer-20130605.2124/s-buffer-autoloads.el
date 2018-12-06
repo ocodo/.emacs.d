ec52e5c3bcc5d2315b4713e8f53b0ced0e4d620f
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:32 +0000

    [Adding s-buffer version: 20130605.2124]

---
+++ elpa/s-buffer-20130605.2124/s-buffer-autoloads.el
@@ -0,0 +1,22 @@
+;;; s-buffer-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "s-buffer" "s-buffer.el" (0 0 0 0))
+;;; Generated autoloads from s-buffer.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "s-buffer" '("s-buffer-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; s-buffer-autoloads.el ends here
---
+++ elpa/s-buffer-20130605.2124/s-buffer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "s-buffer" "20130605.2124" "s operations for buffers" '((s "1.6.0") (noflet "0.0.3")) :commit "f95d234282377f00a2c3a9846681080cb95bb1df" :keywords '("lisp") :authors '(("Nic Ferrier" . "nferrier@ferrier.me.uk")) :maintainer '("Nic Ferrier" . "nferrier@ferrier.me.uk") :url "http://github.com/nicferrier/emacs-s-buffer")
