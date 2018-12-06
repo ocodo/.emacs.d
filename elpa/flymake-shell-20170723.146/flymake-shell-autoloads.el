5d51553a95d86525a3f2a5e0efe6e4ec5e8eec7f
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:42 +0000

    [Adding flymake-shell version: 20170723.146]

---
+++ elpa/flymake-shell-20170723.146/flymake-shell-autoloads.el
@@ -0,0 +1,27 @@
+;;; flymake-shell-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-shell" "flymake-shell.el" (0 0 0 0))
+;;; Generated autoloads from flymake-shell.el
+
+(autoload 'flymake-shell-load "flymake-shell" "\
+Configure flymake mode to check the current buffer's shell-script syntax.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-shell" '("flymake-shell-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-shell-autoloads.el ends here
---
+++ elpa/flymake-shell-20170723.146/flymake-shell-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-shell" "20170723.146" "A flymake syntax-checker for shell scripts" '((flymake-easy "0.1")) :commit "a16cf453056b9849cc7c912bb127fb0b08fc6dab" :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/flymake-shell")
