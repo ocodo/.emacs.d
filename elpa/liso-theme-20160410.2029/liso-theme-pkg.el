c2fdf25f09dc4c71f1a758b550c0721cb1929db7
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:48 +0000

    [Adding liso-theme version: 20160410.2029]

---
+++ elpa/liso-theme-20160410.2029/liso-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "liso-theme" "20160410.2029" "Eclectic Dark Theme for GNU Emacs" 'nil :commit "844688245eb860d23043455e165ee24503454c81" :keywords '("theme" "themes") :authors '(("Vlad Piersec" . "vlad.piersec@gmail.com")) :maintainer '("Vlad Piersec" . "vlad.piersec@gmail.com") :url "https://github.com/caisah/liso-theme")
