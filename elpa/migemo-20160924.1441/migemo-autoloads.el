4202129c1a82c834494b62b1d51fb45d07d92230
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:08 +0000

    [Adding migemo version: 20160924.1441]

---
+++ elpa/migemo-20160924.1441/migemo-autoloads.el
@@ -0,0 +1,22 @@
+;;; migemo-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "migemo" "migemo.el" (0 0 0 0))
+;;; Generated autoloads from migemo.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "migemo" '("migemo-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; migemo-autoloads.el ends here
