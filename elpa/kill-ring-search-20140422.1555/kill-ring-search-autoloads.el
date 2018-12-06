d8424d6c50e8db3888feb45e1a71a4538c606145
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:39 +0000

    [Adding kill-ring-search version: 20140422.1555]

---
+++ elpa/kill-ring-search-20140422.1555/kill-ring-search-autoloads.el
@@ -0,0 +1,28 @@
+;;; kill-ring-search-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "kill-ring-search" "kill-ring-search.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from kill-ring-search.el
+
+(autoload 'kill-ring-search "kill-ring-search" "\
+Search the kill ring in the minibuffer.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "kill-ring-search" '("kill-ring-" "get-next-match")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; kill-ring-search-autoloads.el ends here
---
+++ elpa/kill-ring-search-20140422.1555/kill-ring-search-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kill-ring-search" "20140422.1555" "incremental search for the kill ring" 'nil :commit "23535b4a01a1cb1574604e36c49614e84e85c883" :keywords '("convenience" "matching") :authors '(("Nikolaj Schumacher <bugs * nschum de>")) :maintainer '("Nikolaj Schumacher <bugs * nschum de>") :url "http://nschum.de/src/emacs/kill-ring-search/")
