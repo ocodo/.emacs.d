a725f242f5f81b489574d0e6e1ecb37612a88f2e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:51 +0000

    [Adding literate-coffee-mode version: 20170211.1515]

---
+++ elpa/literate-coffee-mode-20170211.1515/literate-coffee-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "literate-coffee-mode" "20170211.1515" "major-mode for Literate CoffeeScript" '((coffee-mode "0.5.0")) :commit "55ce0305495f4a38c8063c4bd63deb1e1252373d" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-literate-coffee-mode")
