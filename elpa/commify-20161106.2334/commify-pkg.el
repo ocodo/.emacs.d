70120bca68936c160a179cdcac45a5d0bfa7f3e5
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:09 +0000

    [Adding commify version: 20161106.2334]

---
+++ elpa/commify-20161106.2334/commify-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "commify" "20161106.2334" "Toggle grouping commas in numbers" '((s "1.9.0")) :commit "78732c2fa6c1a10288b7436d7c561ec9ebdd41be" :keywords '("convenience" "editing" "numbers" "grouping" "commas") :authors '(("Daniel E. Doherty" . "ded-commify@ddoherty.net")) :maintainer '("Daniel E. Doherty" . "ded-commify@ddoherty.net") :url "https://github.com/ddoherty03/commify")
