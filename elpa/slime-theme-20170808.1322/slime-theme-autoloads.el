7d85fa67198258f3d0554b4a3684e589e8480868
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:02 +0000

    [Adding slime-theme version: 20170808.1322]

---
+++ elpa/slime-theme-20170808.1322/slime-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; slime-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "slime-theme" "slime-theme.el" (0 0 0 0))
+;;; Generated autoloads from slime-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slime-theme" '("slime")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; slime-theme-autoloads.el ends here
---
+++ elpa/slime-theme-20170808.1322/slime-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "slime-theme" "20170808.1322" "an Emacs 24 theme based on Slime (tmTheme)" '((emacs "24.0")) :commit "8e5880ac69e0b6a079103001cc3a90bdb688998f" :authors '(("Jason Milkins")) :maintainer '("Jason Milkins") :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
