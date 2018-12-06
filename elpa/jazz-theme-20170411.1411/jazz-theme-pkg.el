31aa9646cb466fa43cd04310295d2c01787e6233
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:28 +0000

    [Adding jazz-theme version: 20170411.1411]

---
+++ elpa/jazz-theme-20170411.1411/jazz-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "jazz-theme" "20170411.1411" "A warm color theme for Emacs 24+." 'nil :commit "b1cb78a97cc4050f19d88a89e455c3e52d98240e" :authors '(("Roman Parykin" . "donderom@ymail.com")) :maintainer '("Roman Parykin" . "donderom@ymail.com") :url "https://github.com/donderom/jazz-theme")
