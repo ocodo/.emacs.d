53363ab70f962d0cd3a233a5d83f96008398b727
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:46 +0000

    [Adding fold-this version: 20180828.1336]

---
+++ elpa/fold-this-20180828.1336/fold-this-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fold-this" "20180828.1336" "Just fold this region please" 'nil :commit "59ec711ee5f4decf197b8168e333b691b852c827" :keywords '("convenience") :authors '(("Magnar Sveen" . "magnars@gmail.com")) :maintainer '("Magnar Sveen" . "magnars@gmail.com"))
