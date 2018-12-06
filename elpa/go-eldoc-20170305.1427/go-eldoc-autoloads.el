04edf085092911ff78bdee4c32a63488f0f8a5eb
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:05 +0000

    [Adding go-eldoc version: 20170305.1427]

---
+++ elpa/go-eldoc-20170305.1427/go-eldoc-autoloads.el
@@ -0,0 +1,27 @@
+;;; go-eldoc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "go-eldoc" "go-eldoc.el" (0 0 0 0))
+;;; Generated autoloads from go-eldoc.el
+
+(autoload 'go-eldoc-setup "go-eldoc" "\
+Set up eldoc function and enable eldoc-mode.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "go-eldoc" '("go-eldoc-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-eldoc-autoloads.el ends here
---
+++ elpa/go-eldoc-20170305.1427/go-eldoc-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-eldoc" "20170305.1427" "eldoc for go-mode" '((emacs "24.3") (go-mode "1.0.0")) :commit "cbbd2ea1e94a36004432a9ac61414cb5a95a39bd" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-go-eldoc")
