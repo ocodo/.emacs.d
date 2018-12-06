268624926e5a77cfa234ea892681b722d32d394f
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:02 +0000

    [Adding markdown-toc version: 20170711.1949]

---
+++ elpa/markdown-toc-20170711.1949/markdown-toc-pkg.el
@@ -0,0 +1,7 @@
+(define-package "markdown-toc" "20170711.1949" "A simple TOC generator for markdown file"
+  '((s "1.9.0")
+    (dash "2.11.0")
+    (markdown-mode "2.1")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
