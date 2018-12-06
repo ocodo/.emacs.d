034a8c9e95f326467244f26c2b2c9215a824564a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:39 +0000

    [Adding helm-emmet version: 20160713.1231]

---
+++ elpa/helm-emmet-20160713.1231/helm-emmet-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-emmet" "20160713.1231" "helm sources for emmet-mode's snippets" '((helm "1.0") (emmet-mode "1.0.2")) :commit "f0364e736b10cf44232053a78de04133a88185ae" :keywords '("convenience" "helm" "emmet") :authors '(("Yasuyuki Oka" . "yasuyk@gmail.com")) :maintainer '("Yasuyuki Oka" . "yasuyk@gmail.com") :url "https://github.com/yasuyk/helm-emmet")
