5bf63fb868d6ccfe4814733b26eb129f227a7780
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:03 +0000

    [Adding ubuntu-theme version: 20150805.1506]

---
+++ elpa/ubuntu-theme-20150805.1506/ubuntu-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ubuntu-theme" "20150805.1506" "A theme inspired by the default terminal colors in Ubuntu" 'nil :commit "88b0eefc75d4cbcde103057e1c5968d4c3052f69" :authors '(("Francesc Rocher" . "francesc.rocher@gmail.com")) :maintainer '("Francesc Rocher" . "francesc.rocher@gmail.com") :url "http://github.com/rocher/ubuntu-theme")
