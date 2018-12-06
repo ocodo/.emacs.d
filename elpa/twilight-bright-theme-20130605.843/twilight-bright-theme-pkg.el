0c35c44a6739dadc1ab9eb81d507bdb4e610a78e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:00 +0000

    [Adding twilight-bright-theme version: 20130605.843]

---
+++ elpa/twilight-bright-theme-20130605.843/twilight-bright-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "twilight-bright-theme" "20130605.843" "A Emacs 24 faces port of the TextMate theme" 'nil :commit "322157cb2f3bf7920ecd209dafc31bc1c7959f49" :keywords '("themes") :authors '(("Jim Myhrberg" . "contact@jimeh.me")) :maintainer '("Jim Myhrberg" . "contact@jimeh.me") :url "https://github.com/jimeh/twilight-bright-theme.el")
