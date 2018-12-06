f76a9a5ef2036a885a40b09493230a4011ec08c8
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:26 +0000

    [Adding rotate version: 20160909.836]

---
+++ elpa/rotate-20160909.836/rotate-autoloads.el
@@ -0,0 +1,57 @@
+;;; rotate-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "rotate" "rotate.el" (0 0 0 0))
+;;; Generated autoloads from rotate.el
+
+(autoload 'rotate-layout "rotate" "\
+
+
+\(fn)" t nil)
+
+(autoload 'rotate-window "rotate" "\
+
+
+\(fn)" t nil)
+
+(autoload 'rotate:even-horizontal "rotate" "\
+
+
+\(fn)" t nil)
+
+(autoload 'rotate:even-vertical "rotate" "\
+
+
+\(fn)" t nil)
+
+(autoload 'rotate:main-horizontal "rotate" "\
+
+
+\(fn)" t nil)
+
+(autoload 'rotate:main-vertical "rotate" "\
+
+
+\(fn)" t nil)
+
+(autoload 'rotate:tiled "rotate" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "rotate" '("rotate")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; rotate-autoloads.el ends here
---
+++ elpa/rotate-20160909.836/rotate-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rotate" "20160909.836" "Rotate the layout of emacs" 'nil :commit "091b5ac4fc310773253efb317e3dbe8e46959ba6" :keywords '("window" "layout") :authors '(("daichi.hirata <hirata.daichi at gmail.com>")) :maintainer '("daichi.hirata <hirata.daichi at gmail.com>") :url "https://github.com/daichirata/emacs-rotate")
