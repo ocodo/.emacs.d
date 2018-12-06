b7970e18b982129913935102d501d0c05d29cb70
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:07 +0000

    [Adding soothe-theme version: 20141027.1441]

---
+++ elpa/soothe-theme-20141027.1441/soothe-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; soothe-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "soothe-theme" "soothe-theme.el" (0 0 0 0))
+;;; Generated autoloads from soothe-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "soothe-theme" '("soothe")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; soothe-theme-autoloads.el ends here
---
+++ elpa/soothe-theme-20141027.1441/soothe-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "soothe-theme" "20141027.1441" "a dark colorful theme for Emacs24." '((emacs "24.1")) :commit "0786fe70c6c1b4ddcfb932fdc6862b9611cfc09b" :authors '(("Jason Milkins" . "jasonm23@gmail.com")) :maintainer '("Jason Milkins" . "jasonm23@gmail.com") :url "https://github.com/jasonm23/emacs-soothe-theme")
