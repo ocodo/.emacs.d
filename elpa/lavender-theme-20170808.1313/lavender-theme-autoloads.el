d66486b02edeef521feb072b96be22f9f2b48c04
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:44 +0000

    [Adding lavender-theme version: 20170808.1313]

---
+++ elpa/lavender-theme-20170808.1313/lavender-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; lavender-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "lavender-theme" "lavender-theme.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from lavender-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "lavender-theme" '("lavender")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lavender-theme-autoloads.el ends here
