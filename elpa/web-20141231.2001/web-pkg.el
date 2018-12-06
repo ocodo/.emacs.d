44902fefc5c84a405485880cca03b60f28cadc60
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:15 +0000

    [Adding web version: 20141231.2001]

---
+++ elpa/web-20141231.2001/web-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "web" "20141231.2001" "useful HTTP client" '((dash "2.9.0") (s "1.5.0")) :commit "483188dac4bc6b409b985c9dae45f3324a425efd" :keywords '("lisp" "http" "hypermedia") :authors '(("Nic Ferrier" . "nferrier@ferrier.me.uk")) :maintainer '("Nic Ferrier" . "nferrier@ferrier.me.uk") :url "http://github.com/nicferrier/emacs-web")
