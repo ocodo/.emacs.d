e49bec0b9c4d82710ee3411c8d1c1586f0b2fdda
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:09 +0000

    [Adding spacegray-theme version: 20150719.1931]

---
+++ elpa/spacegray-theme-20150719.1931/spacegray-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; spacegray-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "spacegray-theme" "spacegray-theme.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from spacegray-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "spacegray-theme" '("spacegray")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; spacegray-theme-autoloads.el ends here
---
+++ elpa/spacegray-theme-20150719.1931/spacegray-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "spacegray-theme" "20150719.1931" "A Hyperminimal UI Theme" '((emacs "24.1")) :commit "7f70ee36297e5ccf9bc90b1f81472024f5a7a749" :keywords '("themes") :authors '(("Bruce Williams" . "brwcodes@gmail.com")) :maintainer '("Bruce Williams" . "brwcodes@gmail.com") :url "http://github.com/bruce/emacs-spacegray-theme")
