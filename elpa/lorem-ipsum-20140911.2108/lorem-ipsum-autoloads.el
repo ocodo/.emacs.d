f017aaa92e92086d446ef669a4d8f64d4d247b6e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:54 +0000

    [Adding lorem-ipsum version: 20140911.2108]

---
+++ elpa/lorem-ipsum-20140911.2108/lorem-ipsum-autoloads.el
@@ -0,0 +1,51 @@
+;;; lorem-ipsum-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "lorem-ipsum" "lorem-ipsum.el" (0 0 0 0))
+;;; Generated autoloads from lorem-ipsum.el
+
+(autoload 'lorem-ipsum-use-default-bindings "lorem-ipsum" "\
+Use the default keybindings of C-c l [spl].
+
+\(fn)" t nil)
+
+(autoload 'lorem-ipsum-insert-paragraphs "lorem-ipsum" "\
+Insert lorem ipsum paragraphs into buffer.
+If NUM is non-nil, insert NUM paragraphs.
+
+\(fn &optional NUM)" t nil)
+
+(defalias 'Lorem-ipsum-insert-paragraphs 'lorem-ipsum-insert-paragraphs)
+
+(autoload 'lorem-ipsum-insert-sentences "lorem-ipsum" "\
+Insert lorem ipsum sentences into buffer.
+If NUM is non-nil, insert NUM sentences.
+
+\(fn &optional NUM)" t nil)
+
+(defalias 'Lorem-ipsum-insert-sentences 'lorem-ipsum-insert-sentences)
+
+(autoload 'lorem-ipsum-insert-list "lorem-ipsum" "\
+Insert lorem ipsum list items into buffer.
+If NUM is non-nil, insert NUM list items.
+
+\(fn &optional NUM)" t nil)
+
+(defalias 'Lorem-ipsum-insert-list 'lorem-ipsum-insert-list)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "lorem-ipsum" '("lorem-ipsum-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lorem-ipsum-autoloads.el ends here
---
+++ elpa/lorem-ipsum-20140911.2108/lorem-ipsum-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lorem-ipsum" "20140911.2108" "Insert dummy pseudo Latin text." 'nil :commit "4b39f6fed455d67f635b3837cf5668bf74d0f6cd" :keywords '("tools" "language" "convenience") :authors '(("Jean-Philippe Theberge" . "jphil21@sourceforge.net")) :maintainer '("Joe Schafer" . "joe@jschaf.com"))
