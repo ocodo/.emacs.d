0db02936f94248311d6219ffde92bc2dacf277b9
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:42 +0000

    [Adding flymake-rust version: 20170729.2139]

---
+++ elpa/flymake-rust-20170729.2139/flymake-rust-autoloads.el
@@ -0,0 +1,22 @@
+;;; flymake-rust-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-rust" "flymake-rust.el" (0 0 0 0))
+;;; Generated autoloads from flymake-rust.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-rust" '("flymake-rust-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-rust-autoloads.el ends here
---
+++ elpa/flymake-rust-20170729.2139/flymake-rust-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-rust" "20170729.2139" "A flymake handler for rust-mode files" '((flymake-easy "0.1")) :commit "2f42d1f2dad73ec9de460eda6176e3ab25c446f0" :authors '(("Joao Oliveira" . "joaoxsouls@gmail.com")) :maintainer '("Joao Oliveira" . "joaoxsouls@gmail.com") :url "https://github.com/joaoxsouls/flymake-rust")
