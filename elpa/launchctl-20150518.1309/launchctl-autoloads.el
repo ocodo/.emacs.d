6cb25e6126ec30c662cbbbfdc5f7aa4621f5b62c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:44 +0000

    [Adding launchctl version: 20150518.1309]

---
+++ elpa/launchctl-20150518.1309/launchctl-autoloads.el
@@ -0,0 +1,27 @@
+;;; launchctl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "launchctl" "launchctl.el" (0 0 0 0))
+;;; Generated autoloads from launchctl.el
+
+(autoload 'launchctl "launchctl" "\
+Launchctl - major mode for managing Launchd services on Mac OS X.  This is the entry point.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "launchctl" '("launchctl-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; launchctl-autoloads.el ends here
