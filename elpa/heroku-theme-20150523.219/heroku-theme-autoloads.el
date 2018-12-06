df34b39d39adc6985666f4eadb3e65adb81137d6
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:56 +0000

    [Adding heroku-theme version: 20150523.219]

---
+++ elpa/heroku-theme-20150523.219/heroku-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; heroku-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "heroku-theme" "heroku-theme.el" (0 0 0 0))
+;;; Generated autoloads from heroku-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "heroku-theme" '("heroku")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; heroku-theme-autoloads.el ends here
---
+++ elpa/heroku-theme-20150523.219/heroku-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "heroku-theme" "20150523.219" "Heroku color theme" 'nil :commit "8083643fe92ec3a1c3eb82f1b8dc2236c9c9691d" :authors '(("Jonathan Chu" . "me@jonathanchu.is")) :maintainer '("Jonathan Chu" . "me@jonathanchu.is") :url "https://github.com/jonathanchu/color-theme-heroku")
