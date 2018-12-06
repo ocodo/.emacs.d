32711edf35fe3cd4484283f98d8dd8615d485f51
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:37 +0000

    [Adding direx version: 20170422.1327]

---
+++ elpa/direx-20170422.1327/direx-autoloads.el
@@ -0,0 +1,53 @@
+;;; direx-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "direx" "direx.el" (0 0 0 0))
+;;; Generated autoloads from direx.el
+
+(autoload 'direx:jump-to-directory "direx" "\
+
+
+\(fn)" t nil)
+
+(autoload 'direx:jump-to-directory-other-window "direx" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "direx" '("direx:")))
+
+;;;***
+
+;;;### (autoloads nil "direx-project" "direx-project.el" (0 0 0 0))
+;;; Generated autoloads from direx-project.el
+
+(autoload 'direx-project:jump-to-project-root "direx-project" "\
+
+
+\(fn)" t nil)
+
+(autoload 'direx-project:jump-to-project-root-other-window "direx-project" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "direx-project" '("direx-project:")))
+
+;;;***
+
+;;;### (autoloads nil nil ("direx-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; direx-autoloads.el ends here
---
+++ elpa/direx-20170422.1327/direx-pkg.el
@@ -0,0 +1,9 @@
+(define-package "direx" "20170422.1327" "Simple Directory Explorer" 'nil :keywords
+  '("convenience")
+  :authors
+  '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com"))
+  :maintainer
+  '("Tomohiro Matsuyama" . "m2ym.pub@gmail.com"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
