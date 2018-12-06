b31e322de3a4fc4130c4c61be90eff777257ed74
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:26 +0000

    [Adding flatland-theme version: 20171113.1521]

---
+++ elpa/flatland-theme-20171113.1521/flatland-theme-autoloads.el
@@ -0,0 +1,27 @@
+;;; flatland-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flatland-theme" "flatland-theme.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from flatland-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(add-to-list 'safe-local-eval-forms '(when (require 'rainbow-mode nil t) (rainbow-mode 1)))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flatland-theme" '("flatland")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flatland-theme-autoloads.el ends here
