2c04cfdfc344cf0213945caa47d7138ec43cf499
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:02 +0000

    [Adding esup version: 20180727.342]

---
+++ elpa/esup-20180727.342/esup-pkg.el
@@ -0,0 +1,13 @@
+(define-package "esup" "20180727.342" "the Emacs StartUp Profiler (ESUP)"
+  '((cl-lib "0.5")
+    (emacs "24"))
+  :keywords
+  '("convenience" "processes")
+  :authors
+  '(("Joe Schafer" . "joe@jschaf.com"))
+  :maintainer
+  '("Joe Schafer" . "joe@jschaf.com")
+  :url "http://github.com/jschaf/esup")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
