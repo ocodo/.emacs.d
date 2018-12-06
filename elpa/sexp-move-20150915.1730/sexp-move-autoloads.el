3b616e4c0741f5510c3008e69792e40944a7870c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:38 +0000

    [Adding sexp-move version: 20150915.1730]

---
+++ elpa/sexp-move-20150915.1730/sexp-move-autoloads.el
@@ -0,0 +1,22 @@
+;;; sexp-move-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "sexp-move" "sexp-move.el" (0 0 0 0))
+;;; Generated autoloads from sexp-move.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sexp-move" '("sexp-move-" "*sexp-move-read-table*")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sexp-move-autoloads.el ends here
---
+++ elpa/sexp-move-20150915.1730/sexp-move-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sexp-move" "20150915.1730" "Improved S-Expression Movement" 'nil :commit "117f7a91ab7c25e438413753e916570122011ce7" :keywords '("sexp") :authors '(("Philip Woods" . "elzairthesorcerer@gmail.com")) :maintainer '("Philip Woods" . "elzairthesorcerer@gmail.com") :url "https://gitlab.com/elzair/sexp-move")
