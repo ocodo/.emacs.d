bbfbbe01c598db83049b68ae3afe3731d68133a3
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:31 +0000

    [Adding timp version: 20160618.803]

---
+++ elpa/timp-20160618.803/timp-autoloads.el
@@ -0,0 +1,47 @@
+;;; timp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "timp" "timp.el" (0 0 0 0))
+;;; Generated autoloads from timp.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "timp" '("timp" "initialize-instance")))
+
+;;;***
+
+;;;### (autoloads nil "timp-packet" "timp-packet.el" (0 0 0 0))
+;;; Generated autoloads from timp-packet.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "timp-packet" '("timp-" "initialize-instance")))
+
+;;;***
+
+;;;### (autoloads nil "timp-server" "timp-server.el" (0 0 0 0))
+;;; Generated autoloads from timp-server.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "timp-server" '("timp-s" "with-timp-server-inhibited-message")))
+
+;;;***
+
+;;;### (autoloads nil "timp-socket" "timp-socket.el" (0 0 0 0))
+;;; Generated autoloads from timp-socket.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "timp-socket" '("timp-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("timp-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; timp-autoloads.el ends here
---
+++ elpa/timp-20160618.803/timp-pkg.el
@@ -0,0 +1,15 @@
+(define-package "timp" "20160618.803" "Multithreading library"
+  '((emacs "24.4")
+    (cl-lib "0.5")
+    (fifo-class "1.0")
+    (signal "1.0"))
+  :keywords
+  '("internal" "lisp" "processes" "tools")
+  :authors
+  '(("Mola-T" . "Mola@molamola.xyz"))
+  :maintainer
+  '("Mola-T" . "Mola@molamola.xyz")
+  :url "https://github.com/mola-T/timp")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
