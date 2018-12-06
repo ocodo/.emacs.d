8f4c28790e691343375e64dd3c050e839d831411
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:05 +0000

    [Adding homebrew-mode version: 20160615.1320]

---
+++ elpa/homebrew-mode-20160615.1320/homebrew-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "homebrew-mode" "20160615.1320" "minor mode for editing Homebrew formulae" '((emacs "24.4") (inf-ruby "2.4.0") (dash "1.2.0")) :commit "d422307aee2f897d1a92e3b959c3214bc54cbe38" :keywords '("homebrew" "brew" "ruby") :authors '(("Alex Dunn" . "dunn.alex@gmail.com")) :maintainer '("Alex Dunn" . "dunn.alex@gmail.com") :url "https://github.com/dunn/homebrew-mode")
