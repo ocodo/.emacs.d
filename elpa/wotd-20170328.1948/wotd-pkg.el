12cd7b516b4da79ed7a4b593aa649f3bad6e9395
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:24 +0000

    [Adding wotd version: 20170328.1948]

---
+++ elpa/wotd-20170328.1948/wotd-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "wotd" "20170328.1948" "Fetch word-of-the-day from multiple online sources" '((emacs "24.4") (org "8.2.10")) :commit "d2937a3d91e014f8028a1f33d21c18cc0b065a64" :keywords '("extensions") :authors '(("Junpeng Qiu" . "qjpchmail@gmail.com")) :maintainer '("Junpeng Qiu" . "qjpchmail@gmail.com"))
