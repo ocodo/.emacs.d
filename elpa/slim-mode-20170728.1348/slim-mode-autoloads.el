b3a1ea1d5921fc0324fe78f1aa7f5d7981a8ea5d
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:01 +0000

    [Adding slim-mode version: 20170728.1348]

---
+++ elpa/slim-mode-20170728.1348/slim-mode-autoloads.el
@@ -0,0 +1,31 @@
+;;; slim-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "slim-mode" "slim-mode.el" (0 0 0 0))
+;;; Generated autoloads from slim-mode.el
+
+(autoload 'slim-mode "slim-mode" "\
+Major mode for editing Slim files.
+
+\\{slim-mode-map}
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.slim\\'" . slim-mode))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slim-mode" '("slim-" "html-tags")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; slim-mode-autoloads.el ends here
---
+++ elpa/slim-mode-20170728.1348/slim-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "slim-mode" "20170728.1348" "Major mode for editing Slim files" 'nil :commit "3636d18ab1c8b316eea71c4732eb44743e2ded87" :keywords '("markup" "language") :authors '(("Nathan Weizenbaum")) :maintainer '("Nathan Weizenbaum") :url "http://github.com/slim-template/emacs-slim")
