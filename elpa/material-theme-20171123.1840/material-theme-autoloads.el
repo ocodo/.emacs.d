2675af7e4e88612f67628ecbf9e82caa886fdfbe
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:04 +0000

    [Adding material-theme version: 20171123.1840]

---
+++ elpa/material-theme-20171123.1840/material-theme-autoloads.el
@@ -0,0 +1,39 @@
+;;; material-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "material-light-theme" "material-light-theme.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from material-light-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "material-light-theme" '("material-light")))
+
+;;;***
+
+;;;### (autoloads nil "material-theme" "material-theme.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from material-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "material-theme" '("material")))
+
+;;;***
+
+;;;### (autoloads nil nil ("material-theme-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; material-theme-autoloads.el ends here
---
+++ elpa/material-theme-20171123.1840/material-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "material-theme" "20171123.1840" "A Theme based on the colors of the Google Material Design"
+  '((emacs "24.1"))
+  :keywords
+  '("themes")
+  :authors
+  '(("Christoph Paulik" . "cpaulik@gmail.com"))
+  :maintainer
+  '("Christoph Paulik" . "cpaulik@gmail.com")
+  :url "http://github.com/cpaulik/emacs-material-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
