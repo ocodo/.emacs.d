27ff0fc24cba8735369b5654eaa925e1fb433565
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:23 +0000

    [Adding workgroups version: 20110726.1641]

---
+++ elpa/workgroups-20110726.1641/workgroups-autoloads.el
@@ -0,0 +1,22 @@
+;;; workgroups-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "workgroups" "workgroups.el" (0 0 0 0))
+;;; Generated autoloads from workgroups.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "workgroups" '("w-set-frame-size-and-scale-wtree" "wg-" "workgroups-mode")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; workgroups-autoloads.el ends here
---
+++ elpa/workgroups-20110726.1641/workgroups-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "workgroups" "20110726.1641" "workgroups for windows (for Emacs)" 'nil :commit "9572b3492ee09054dc329f64ed846c962b395e39" :keywords '("session" "management" "window-configuration" "persistence") :authors '(("tlh" . "thunkout@gmail.com")) :maintainer '("tlh" . "thunkout@gmail.com"))
