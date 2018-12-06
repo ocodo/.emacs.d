6a90ae39ff996b7184c32a3d3cef582d7ab5fbfb
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:11 +0000

    [Adding evil-rsi version: 20160221.2104]

---
+++ elpa/evil-rsi-20160221.2104/evil-rsi-autoloads.el
@@ -0,0 +1,35 @@
+;;; evil-rsi-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "evil-rsi" "evil-rsi.el" (0 0 0 0))
+;;; Generated autoloads from evil-rsi.el
+
+(defvar evil-rsi-mode nil "\
+Non-nil if Evil-Rsi mode is enabled.
+See the `evil-rsi-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `evil-rsi-mode'.")
+
+(custom-autoload 'evil-rsi-mode "evil-rsi" nil)
+
+(autoload 'evil-rsi-mode "evil-rsi" "\
+Rsi mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; evil-rsi-autoloads.el ends here
---
+++ elpa/evil-rsi-20160221.2104/evil-rsi-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-rsi" "20160221.2104" "Use emacs motion keys in evil, inspired by vim-rsi" '((evil "1.0.0")) :commit "65ae60866be494e4622fe383e23975e04d2a42a3" :keywords '("evil" "rsi" "evil-rsi") :authors '(("Quang Linh LE" . "linktohack@gmail.com")) :maintainer '("Quang Linh LE" . "linktohack@gmail.com") :url "http://github.com/linktohack/evil-rsi")
