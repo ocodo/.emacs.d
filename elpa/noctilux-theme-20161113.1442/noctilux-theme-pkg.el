85ed0e5ad721f59e2232b4fa182d3d372671946c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:19 +0000

    [Adding noctilux-theme version: 20161113.1442]

---
+++ elpa/noctilux-theme-20161113.1442/noctilux-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "noctilux-theme" "20161113.1442" "Dark theme inspired by LightTable" '((emacs "24")) :commit "a3265a1be7f4d73f44acce6d968ca6f7add1f2ca" :authors '(("Simon Manning" . "simon@ecksdee.org")) :maintainer '("Simon Manning" . "simon@ecksdee.org") :url "https://github.com/sjrmanning/noctilux-theme")
