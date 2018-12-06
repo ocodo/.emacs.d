830490cc088de620ef287366a87ef2712b6ddd0a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:26 +0000

    [Adding xbm-life version: 20160103.1017]

---
+++ elpa/xbm-life-20160103.1017/xbm-life-autoloads.el
@@ -0,0 +1,29 @@
+;;; xbm-life-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "xbm-life" "xbm-life.el" (0 0 0 0))
+;;; Generated autoloads from xbm-life.el
+
+(autoload 'xbm-life "xbm-life" "\
+Launch a XBM demo of Conway's Game of Life.
+Use ARG as prefix argument to create a buffer with a different
+name.
+
+\(fn ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "xbm-life" '("xbm-life-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; xbm-life-autoloads.el ends here
