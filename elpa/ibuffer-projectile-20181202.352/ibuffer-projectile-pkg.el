b4365f3158161772def05561299ec9823e038715
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:10 +0000

    [Adding ibuffer-projectile version: 20181202.352]

---
+++ elpa/ibuffer-projectile-20181202.352/ibuffer-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ibuffer-projectile" "20181202.352" "Group ibuffer's list by projectile root" '((projectile "0.11.0") (emacs "24")) :commit "76496214144687cee0b5139be2e61b1e400cac87" :keywords '("convenience") :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "http://github.com/purcell/ibuffer-projectile")
