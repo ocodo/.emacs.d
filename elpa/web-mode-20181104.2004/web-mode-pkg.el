9670fcd513817e7b58043a88cc92fd4ab529bf19
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:16 +0000

    [Adding web-mode version: 20181104.2004]

---
+++ elpa/web-mode-20181104.2004/web-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "web-mode" "20181104.2004" "major mode for editing web templates" '((emacs "23.1")) :commit "29ced993bb1a435bd82d3e7395bed13b99e87de4" :keywords '("languages") :authors '(("François-Xavier Bois <fxbois AT Google Mail Service>")) :maintainer '("François-Xavier Bois") :url "http://web-mode.org")
