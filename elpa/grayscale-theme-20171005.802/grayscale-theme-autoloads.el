e8df1406fce5a6625344763b7c263196785c2304
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:21 +0000

    [Adding grayscale-theme version: 20171005.802]

---
+++ elpa/grayscale-theme-20171005.802/grayscale-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; grayscale-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "grayscale-theme" "grayscale-theme.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from grayscale-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "grayscale-theme" '("grayscale-theme-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; grayscale-theme-autoloads.el ends here
---
+++ elpa/grayscale-theme-20171005.802/grayscale-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "grayscale-theme" "20171005.802" "A simple grayscale theme" 'nil :commit "53ad50e10e68f2f076ebfc96e10ecef7a932d38d" :keywords '("lisp") :authors '(("Kaleb Elwert" . "belak@coded.io")) :maintainer '("Kaleb Elwert" . "belak@coded.io") :url "https://github.com/belak/emacs-grayscale-theme")
