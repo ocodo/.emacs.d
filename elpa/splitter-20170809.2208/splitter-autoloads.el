1ebce909f7968d252e1b1817e54cb3125a08d17d
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:11 +0000

    [Adding splitter version: 20170809.2208]

---
+++ elpa/splitter-20170809.2208/splitter-autoloads.el
@@ -0,0 +1,22 @@
+;;; splitter-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "splitter" "splitter.el" (0 0 0 0))
+;;; Generated autoloads from splitter.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "splitter" '("spl-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; splitter-autoloads.el ends here
---
+++ elpa/splitter-20170809.2208/splitter-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "splitter" "20170809.2208" "Manage window splits" 'nil :commit "6bdb51e9a346907d60a9625f6180bddd06be6674" :keywords '("frames" "convenience") :authors '(("Steven Thomas")) :maintainer '("Steven Thomas") :url "https://github.com/chumpage/chumpy-windows")
