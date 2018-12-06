913b6d86fc7e703cc20e64920cc39d2b60c0e3a6
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:21 +0000

    [Adding windata version: 20090830.1040]

---
+++ elpa/windata-20090830.1040/windata-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "windata" "20090830.1040" "convert window configuration to list" 'nil :commit "a723fc446ceaec23d5f29ecc8245d94c99d91625" :keywords '("convenience" "frames") :authors '((nil . "wenbinye@gmail.com")) :maintainer '(nil . "wenbinye@gmail.com"))
