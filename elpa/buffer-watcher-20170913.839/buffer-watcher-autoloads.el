6af01dbb667cfdc3add7f317f3cb91c4be40eaa6
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:50 +0000

    [Adding buffer-watcher version: 20170913.839]

---
+++ elpa/buffer-watcher-20170913.839/buffer-watcher-autoloads.el
@@ -0,0 +1,23 @@
+;;; buffer-watcher-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "buffer-watcher" "buffer-watcher.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from buffer-watcher.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "buffer-watcher" '("buffer-watcher-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; buffer-watcher-autoloads.el ends here
