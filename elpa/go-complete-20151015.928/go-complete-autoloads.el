c505606713dcc709c9eeb5f423750ff5083b2249
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:04 +0000

    [Adding go-complete version: 20151015.928]

---
+++ elpa/go-complete-20151015.928/go-complete-autoloads.el
@@ -0,0 +1,27 @@
+;;; go-complete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "go-complete" "go-complete.el" (0 0 0 0))
+;;; Generated autoloads from go-complete.el
+
+(autoload 'go-complete-at-point "go-complete" "\
+Complete go expression at point.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "go-complete" '("go-complete-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-complete-autoloads.el ends here
---
+++ elpa/go-complete-20151015.928/go-complete-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-complete" "20151015.928" "Native code completion for Go" '((go-mode "0") (cl-lib "0.5")) :commit "e39efc356f6e19f17db3f3d2c81f28d38b31a55e" :keywords '("go" "golang" "completion") :authors '(("Vibhav Pant" . "vibhavp@gmail.com")) :maintainer '("Vibhav Pant" . "vibhavp@gmail.com") :url "https://github.com/vibhavp/go-complete")
