66c396c857ed9a6a434e55f5c3e22a0ca1744821
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:16 +0000

    [Adding counsel-codesearch version: 20180925.803]

---
+++ elpa/counsel-codesearch-20180925.803/counsel-codesearch-autoloads.el
@@ -0,0 +1,30 @@
+;;; counsel-codesearch-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "counsel-codesearch" "counsel-codesearch.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from counsel-codesearch.el
+
+(autoload 'counsel-codesearch "counsel-codesearch" "\
+Call the \"csearch\" shell command.
+
+INITIAL-INPUT can be given as the initial minibuffer input.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "counsel-codesearch" '("counsel-codesearch--")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; counsel-codesearch-autoloads.el ends here
---
+++ elpa/counsel-codesearch-20180925.803/counsel-codesearch-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-codesearch" "20180925.803" "Counsel interface for codesearch.el" '((codesearch "1") (counsel "0.10.0") (emacs "24") (ivy "0.10.0")) :commit "b7989fad3e06f301c31d5e896c42b6cc549a0e0c" :keywords '("tools") :authors '(("Austin Bingham" . "austin.bingham@gmail.com")) :maintainer '("Austin Bingham" . "austin.bingham@gmail.com") :url "https://github.com/abingham/emacs-counsel-codesearch")
