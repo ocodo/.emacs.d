f3404c6a24286e2062be93ab948e2169e64bb812
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:47 +0000

    [Adding helm-ls-git version: 20180711.923]

---
+++ elpa/helm-ls-git-20180711.923/helm-ls-git-autoloads.el
@@ -0,0 +1,31 @@
+;;; helm-ls-git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-ls-git" "helm-ls-git.el" (0 0 0 0))
+;;; Generated autoloads from helm-ls-git.el
+
+(eieio-defclass-autoload 'helm-ls-git-source '(helm-source-in-buffer) "helm-ls-git" nil)
+
+(eieio-defclass-autoload 'helm-ls-git-status-source '(helm-source-in-buffer) "helm-ls-git" nil)
+
+(autoload 'helm-ls-git-ls "helm-ls-git" "\
+
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-ls-git" '("helm-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-ls-git-autoloads.el ends here
---
+++ elpa/helm-ls-git-20180711.923/helm-ls-git-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-ls-git" "20180711.923" "list git files." '((helm "1.7.8")) :commit "e38cee59c42fbf16b6de239a537d27af58473394")
