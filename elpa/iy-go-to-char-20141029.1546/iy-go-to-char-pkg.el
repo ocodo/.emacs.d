06e61a08034803b8dde54a6211cf6d8e80c392db
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:26 +0000

    [Adding iy-go-to-char version: 20141029.1546]

---
+++ elpa/iy-go-to-char-20141029.1546/iy-go-to-char-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "iy-go-to-char" "20141029.1546" "Go to next CHAR which is similar to \"f\" and \"t\" in vim" 'nil :commit "04ab4f5f3a241cbbc9b8c178a22b412a62f632f9" :keywords '("navigation" "search") :authors '(("Ian Yang <doit dot ian (at) gmail dot com>")) :maintainer '("Ian Yang <doit dot ian (at) gmail dot com>") :url "https://github.com/doitian/iy-go-to-char")
