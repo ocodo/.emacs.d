b232a74a935bb795bbed7f3c0402de56c1d836c8
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:56 +0000

    [Adding cheatsheet version: 20170126.2150]

---
+++ elpa/cheatsheet-20170126.2150/cheatsheet-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cheatsheet" "20170126.2150" "create your own cheatsheet" '((emacs "24") (cl-lib "0.5")) :commit "e4f8e0110167ea16a17a74517d1f10cb7ff805b8" :keywords '("convenience" "usability") :authors '(("Shirin Nikita" . "shirin.nikita@gmail.com")) :maintainer '("Shirin Nikita" . "shirin.nikita@gmail.com") :url "http://github.com/darksmile/cheatsheet/")
