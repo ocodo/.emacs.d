06b3cc5e4f109d624dbe9358c7c6ed95fc767d94
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:48 +0000

    [Adding linum-relative version: 20180124.1047]

---
+++ elpa/linum-relative-20180124.1047/linum-relative-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "linum-relative" "20180124.1047" "display relative line number in emacs." 'nil :commit "c74a6981b688a5e1e6b8e0809363963ff558ce4d" :keywords '("converience") :authors '(("coldnew" . "coldnew.tw@gmail.com")) :maintainer '("coldnew" . "coldnew.tw@gmail.com") :url "http://github.com/coldnew/linum-relative")
