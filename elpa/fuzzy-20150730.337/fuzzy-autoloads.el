875cefce80333c7c08f81d89ab7d431ec2346c5e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:50 +0000

    [Adding fuzzy version: 20150730.337]

---
+++ elpa/fuzzy-20150730.337/fuzzy-autoloads.el
@@ -0,0 +1,22 @@
+;;; fuzzy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "fuzzy" "fuzzy.el" (0 0 0 0))
+;;; Generated autoloads from fuzzy.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "fuzzy" '("fuzzy-" "turn-o")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; fuzzy-autoloads.el ends here
---
+++ elpa/fuzzy-20150730.337/fuzzy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fuzzy" "20150730.337" "Fuzzy Matching" 'nil :commit "534d723ad2e06322ff8d9bd0ba4863d243f698e7" :keywords '("convenience") :authors '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com")) :maintainer '("Tomohiro Matsuyama" . "m2ym.pub@gmail.com"))
