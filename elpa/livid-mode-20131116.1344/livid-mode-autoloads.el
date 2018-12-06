577c150758f71a133097d1eb90d1a06badacdd56
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:52 +0000

    [Adding livid-mode version: 20131116.1344]

---
+++ elpa/livid-mode-20131116.1344/livid-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; livid-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "livid-mode" "livid-mode.el" (0 0 0 0))
+;;; Generated autoloads from livid-mode.el
+
+(autoload 'livid-mode "livid-mode" "\
+Minor mode for automatic evaluation of a JavaScript buffer on every change
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "livid-mode" '("livid-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; livid-mode-autoloads.el ends here
---
+++ elpa/livid-mode-20131116.1344/livid-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "livid-mode" "20131116.1344" "Live browser eval of JavaScript every time a buffer changes" '((skewer-mode "1.5.3") (s "1.8.0")) :commit "dfe5212fa64738bc4138bfebf349fbc8bc237c26" :authors '(("Murphy McMahon")) :maintainer '("Murphy McMahon") :url "https://github.com/pandeiro/livid-mode")
