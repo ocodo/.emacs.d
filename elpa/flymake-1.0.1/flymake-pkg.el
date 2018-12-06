4fa51b7229e333a2fd103594fd4565fff6391721
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:35 +0000

    [Adding flymake version: 1.0.1]

---
+++ elpa/flymake-1.0.1/flymake-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake" "1.0.1" "A universal on-the-fly syntax checker" '((emacs "26.1")) :url "http://elpa.gnu.org/packages/flymake.html" :keywords '("c" "languages" "tools"))
