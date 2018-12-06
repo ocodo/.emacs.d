2750b74203e7d362dd27e94a152f41d1e5855a1a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:18 +0000

    [Adding noccur version: 20150514.2120]

---
+++ elpa/noccur-20150514.2120/noccur-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "noccur" "20150514.2120" "Run multi-occur on project/dired files" 'nil :commit "6cc02ce07178a61ae38a849f80472c01969272bc" :keywords '("convenience") :authors '(("Nicolas Petton" . "petton.nicolas@gmail.com")) :maintainer '("Nicolas Petton" . "petton.nicolas@gmail.com"))
