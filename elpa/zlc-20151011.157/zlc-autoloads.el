459448e7283dff871a6a39f1002107fa4d638b57
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:35 +0000

    [Adding zlc version: 20151011.157]

---
+++ elpa/zlc-20151011.157/zlc-autoloads.el
@@ -0,0 +1,28 @@
+;;; zlc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "zlc" "zlc.el" (0 0 0 0))
+;;; Generated autoloads from zlc.el
+
+(autoload 'zlc-mode "zlc" "\
+Toggle zlc (zsh like completion) on or off.
+With ARG, turn zlc on if arg is positive, off otherwise.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "zlc" '("zlc-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zlc-autoloads.el ends here
---
+++ elpa/zlc-20151011.157/zlc-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zlc" "20151011.157" "Provides zsh like completion system to Emacs" 'nil :commit "4dd2ba267ecdeac845a7cbb3147294ee7daa25f4" :keywords '("matching" "convenience") :authors '(("mooz" . "stillpedant@gmail.com")) :maintainer '("mooz" . "stillpedant@gmail.com"))
