b9413ba8a42f482fafedebb25fddfa3c2c206cc7
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:07:49 +0000

    [Adding project-explorer version: 20150504.14]

---
+++ elpa/project-explorer-20150504.14/project-explorer-autoloads.el
@@ -0,0 +1,38 @@
+;;; project-explorer-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "project-explorer" "project-explorer.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from project-explorer.el
+
+(autoload 'project-explorer-helm "project-explorer" "\
+Browse the project using helm.
+
+\(fn)" t nil)
+
+(autoload 'project-explorer-open "project-explorer" "\
+Show or create the project explorer for the current project.
+
+\(fn)" t nil)
+
+(autoload 'project-explorer-toggle "project-explorer" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "project-explorer" '("project-explorer-mode" "pe/")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; project-explorer-autoloads.el ends here
---
+++ elpa/project-explorer-20150504.14/project-explorer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "project-explorer" "20150504.14" "A project explorer sidebar" '((cl-lib "0.3") (es-lib "0.3") (es-windows "0.1") (emacs "24")) :commit "589a09008706f5f4ef91393dc4306eede0d15ca9" :authors '(("sabof")) :maintainer '("sabof") :url "https://github.com/sabof/project-explorer")
