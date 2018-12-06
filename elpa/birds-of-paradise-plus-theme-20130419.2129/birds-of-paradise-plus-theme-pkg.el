bb7b6c3b0182c214e4785ad37fed6c1be5ea63be
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:47 +0000

    [Adding birds-of-paradise-plus-theme version: 20130419.2129]

---
+++ elpa/birds-of-paradise-plus-theme-20130419.2129/birds-of-paradise-plus-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "birds-of-paradise-plus-theme" "20130419.2129" "A brown/orange light-on-dark theme for Emacs 24 (deftheme)." 'nil :commit "bb9f9d4ef7f7872a388ec4eee1253069adcadb6f" :keywords '("themes") :authors '(("Jim Myhrberg" . "contact@jimeh.me")) :maintainer '("Jim Myhrberg" . "contact@jimeh.me") :url "https://github.com/jimeh/birds-of-paradise-plus-theme.el")
