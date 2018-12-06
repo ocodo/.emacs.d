620cc14fa78c30255e529967f532849f14296da3
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:31 +0000

    [Adding flycheck-cython version: 20170724.958]

---
+++ elpa/flycheck-cython-20170724.958/flycheck-cython-autoloads.el
@@ -0,0 +1,23 @@
+;;; flycheck-cython-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flycheck-cython" "flycheck-cython.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from flycheck-cython.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flycheck-cython" '("flycheck-cython-map-source-file")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flycheck-cython-autoloads.el ends here
---
+++ elpa/flycheck-cython-20170724.958/flycheck-cython-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-cython" "20170724.958" "Support Cython in flycheck" '((flycheck "0.25")) :commit "ecc4454d35ab5317ab66a04406f36f0c1dbc0b76" :authors '(("Lorenzo Bolla" . "lbolla@gmail.com")) :maintainer '("Lorenzo Bolla" . "lbolla@gmail.com"))
