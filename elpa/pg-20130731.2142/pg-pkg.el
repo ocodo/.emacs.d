06e5fb230f637dbe6dac7bde4d8dd9fdcb9bc44b
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:40 +0000

    [Adding pg version: 20130731.2142]

---
+++ elpa/pg-20130731.2142/pg-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pg" "20130731.2142" "Emacs Lisp interface to the PostgreSQL RDBMS" 'nil :commit "4f6516ec3946d95dcef49abb6703cc89ecb5183d" :keywords '("data" "comm" "database" "postgresql") :authors '(("Eric Marsden" . "emarsden@laas.fr")) :maintainer '("Helmut Eller" . "heller@common-lisp.net"))
