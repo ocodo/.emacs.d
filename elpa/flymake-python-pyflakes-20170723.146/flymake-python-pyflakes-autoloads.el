a1b65c2ecdbec04f27dcce5592963fefe94ff4b9
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:41 +0000

    [Adding flymake-python-pyflakes version: 20170723.146]

---
+++ elpa/flymake-python-pyflakes-20170723.146/flymake-python-pyflakes-autoloads.el
@@ -0,0 +1,28 @@
+;;; flymake-python-pyflakes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-python-pyflakes" "flymake-python-pyflakes.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from flymake-python-pyflakes.el
+
+(autoload 'flymake-python-pyflakes-load "flymake-python-pyflakes" "\
+Configure flymake mode to check the current buffer's python syntax using pyflakes.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-python-pyflakes" '("flymake-python-pyflakes-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-python-pyflakes-autoloads.el ends here
