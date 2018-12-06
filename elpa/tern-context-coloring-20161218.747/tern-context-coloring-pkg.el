ac8086198d79c374b66747eee5f380aa817d3c74
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:28 +0000

    [Adding tern-context-coloring version: 20161218.747]

---
+++ elpa/tern-context-coloring-20161218.747/tern-context-coloring-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tern-context-coloring" "20161218.747" "Use Tern for context coloring" '((emacs "24.3") (context-coloring "8.1.0") (tern "0.0.1")) :commit "3a8e979d6cc83aabcb3dda3f5f31a6422532efba" :keywords '("convenience" "faces" "tools") :authors '(("Jackson Ray Hamilton" . "jackson@jacksonrayhamilton.com")) :maintainer '("Jackson Ray Hamilton" . "jackson@jacksonrayhamilton.com") :url "https://github.com/jacksonrayhamilton/tern-context-coloring")
