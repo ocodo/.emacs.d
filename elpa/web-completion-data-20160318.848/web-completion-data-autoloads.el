daa79a42ed6eadceff7dcf3056d4b53929c1c744
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:16 +0000

    [Adding web-completion-data version: 20160318.848]

---
+++ elpa/web-completion-data-20160318.848/web-completion-data-autoloads.el
@@ -0,0 +1,27 @@
+;;; web-completion-data-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "web-completion-data" "web-completion-data.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from web-completion-data.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "web-completion-data" '("web-completion-data-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("web-completion-data-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; web-completion-data-autoloads.el ends here
---
+++ elpa/web-completion-data-20160318.848/web-completion-data-pkg.el
@@ -0,0 +1,10 @@
+(define-package "web-completion-data" "20160318.848" "Shared completion data for ac-html and company-web" 'nil :keywords
+  '("html" "auto-complete" "company")
+  :authors
+  '(("Olexandr Sydorchuk" . "olexandr.syd@gmail.com"))
+  :maintainer
+  '("Olexandr Sydorchuk" . "olexandr.syd@gmail.com")
+  :url "https://github.com/osv/web-completion-data")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
