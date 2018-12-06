0afc3f17d7ff7e3512c6b00c5dd99383bfc1dfc9
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:47 +0000

    [Adding format-all version: 20181108.921]

---
+++ elpa/format-all-20181108.921/format-all-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "format-all" "20181108.921" "Auto-format C, C++, JS, Python, Ruby and 30 other languages" '((emacs "24") (cl-lib "0.5")) :commit "76054ed7236f18737cca4566fe8be7257f58cd81" :keywords '("languages" "util") :authors '(("Lassi Kortela" . "lassi@lassi.io")) :maintainer '("Lassi Kortela" . "lassi@lassi.io") :url "https://github.com/lassik/emacs-format-all-the-code")
