75417c19eabd2582cc90d0dcc57038b173753a38
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:02 +0000

    [Adding hippie-exp-ext version: 20160502.2326]

---
+++ elpa/hippie-exp-ext-20160502.2326/hippie-exp-ext-autoloads.el
@@ -0,0 +1,23 @@
+;;; hippie-exp-ext-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "hippie-exp-ext" "hippie-exp-ext.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from hippie-exp-ext.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "hippie-exp-ext" '("he-" "hippie-expand-" "try-expand-dabbrev-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; hippie-exp-ext-autoloads.el ends here
---
+++ elpa/hippie-exp-ext-20160502.2326/hippie-exp-ext-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "hippie-exp-ext" "20160502.2326" "Extension of hippie-expand" 'nil :commit "4eda13f90da51ab217d024701f4c30f91ffcb90e" :keywords '("abbrev" "convenience" "completions" "hippie-expand") :authors '(("rubikitch" . "rubikitch@ruby-lang.org")) :maintainer '("rubikitch" . "rubikitch@ruby-lang.org") :url "http://www.emacswiki.org/emacs/download/hippie-exp-ext.el")
