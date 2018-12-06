cbbe382c281cbddd0a2c671b009170906ac9932e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:24 +0000

    [Adding tabula-rasa version: 20141216.547]

---
+++ elpa/tabula-rasa-20141216.547/tabula-rasa-autoloads.el
@@ -0,0 +1,22 @@
+;;; tabula-rasa-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "tabula-rasa" "tabula-rasa.el" (0 0 0 0))
+;;; Generated autoloads from tabula-rasa.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tabula-rasa" '("tabula-rasa-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; tabula-rasa-autoloads.el ends here
---
+++ elpa/tabula-rasa-20141216.547/tabula-rasa-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tabula-rasa" "20141216.547" "Distraction free writing mode" '((emacs "24.4")) :commit "e85fff9de18dc31bc6a7aca726e34a95cc5459f5" :keywords '("distraction free" "writing") :authors '(("Ido Magal" . "misc@satans.church")) :maintainer '("Ido Magal" . "misc@satans.church") :url "https://github.com/idomagal/Tabula-Rasa/blob/master/tabula-rasa.el")
