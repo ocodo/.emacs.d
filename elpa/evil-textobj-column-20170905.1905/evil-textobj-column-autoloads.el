49901f87bd8a0d6f5b4a4da456baae5e14476ece
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:15 +0000

    [Adding evil-textobj-column version: 20170905.1905]

---
+++ elpa/evil-textobj-column-20170905.1905/evil-textobj-column-autoloads.el
@@ -0,0 +1,25 @@
+;;; evil-textobj-column-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "evil-textobj-column" "evil-textobj-column.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from evil-textobj-column.el
+ (autoload 'evil-textobj-column-word "evil-textobj-column" nil t)
+ (autoload 'evil-textobj-column-WORD "evil-textobj-column" nil t)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-textobj-column" '("evil-textobj-column-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; evil-textobj-column-autoloads.el ends here
