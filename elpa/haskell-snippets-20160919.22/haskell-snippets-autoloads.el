a72eda570948fe3d2fb38c9aa7562c4e6af81e2a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:28 +0000

    [Adding haskell-snippets version: 20160919.22]

---
+++ elpa/haskell-snippets-20160919.22/haskell-snippets-autoloads.el
@@ -0,0 +1,32 @@
+;;; haskell-snippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "haskell-snippets" "haskell-snippets.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from haskell-snippets.el
+
+(autoload 'haskell-snippets-initialize "haskell-snippets" "\
+
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(haskell-snippets-initialize))
+
+;;;***
+
+;;;### (autoloads nil nil ("haskell-snippets-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; haskell-snippets-autoloads.el ends here
---
+++ elpa/haskell-snippets-20160919.22/haskell-snippets-pkg.el
@@ -0,0 +1,13 @@
+(define-package "haskell-snippets" "20160919.22" "Yasnippets for Haskell"
+  '((cl-lib "0.5")
+    (yasnippet "0.8.0"))
+  :keywords
+  '("snippets" "haskell")
+  :authors
+  '(("Luke Hoersten" . "luke@hoersten.org"))
+  :maintainer
+  '("Luke Hoersten" . "luke@hoersten.org")
+  :url "https://github.com/haskell/haskell-snippets")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
