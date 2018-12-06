8968ef0daf98a94f52f594b4dd972c1035f7de83
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:19 +0000

    [Adding fancy-narrow version: 20171031.16]

---
+++ elpa/fancy-narrow-20171031.16/fancy-narrow-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fancy-narrow" "20171031.16" "narrow-to-region with more eye candy." 'nil :commit "9f4a587f6a5a387271fb665e13f59d41fd42504c" :keywords '("faces" "convenience") :authors '(("Artur Malabarba" . "bruce.connor.am@gmail.com")) :maintainer '("Artur Malabarba" . "bruce.connor.am@gmail.com") :url "http://github.com/Bruce-Connor/fancy-narrow")
