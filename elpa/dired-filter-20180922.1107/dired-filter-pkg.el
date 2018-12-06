746a4be2a9975cbf80d1795c1f008afa71c5d556
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:34 +0000

    [Adding dired-filter version: 20180922.1107]

---
+++ elpa/dired-filter-20180922.1107/dired-filter-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-filter" "20180922.1107" "Ibuffer-like filtering for dired" '((dash "2.10.0") (dired-hacks-utils "0.0.1") (f "0.17.0") (cl-lib "0.3")) :commit "c7184dc8f6ed3f2330db8d8f21f01c4b08056dd3" :keywords '("files") :authors '(("Matúš Goljer" . "matus.goljer@gmail.com")) :maintainer '("Matúš Goljer" . "matus.goljer@gmail.com"))
