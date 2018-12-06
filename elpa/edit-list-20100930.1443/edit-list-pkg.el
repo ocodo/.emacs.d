93c5696082f7879847aaafa20848e6ff5900f14c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:44 +0000

    [Adding edit-list version: 20100930.1443]

---
+++ elpa/edit-list-20100930.1443/edit-list-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "edit-list" "20100930.1443" "edit a single list" 'nil :commit "f460d3f9e208a4e606fe6ded307f1b011916ca71" :authors '(("Michael Olson" . "mwolson@gnu.org")) :maintainer '("Michael Olson" . "mwolson@gnu.org") :url "http://mwolson.org/static/dist/elisp/edit-list.el")
