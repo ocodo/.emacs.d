148d4dbd1b0a5d67cb179f508556d5e74160cf56
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:09 +0000

    [Adding vdirel version: 20181001.1135]

---
+++ elpa/vdirel-20181001.1135/vdirel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vdirel" "20181001.1135" "Manipulate vdir (i.e., vCard) repositories" '((emacs "24.4") (org-vcard "0.1.0") (helm "1.7.0") (seq "1.11")) :commit "dfe5f9478405b8292f85fc911db92e81b627626a" :authors '(("Damien Cassou" . "damien@cassou.me")) :maintainer '("Damien Cassou" . "damien@cassou.me") :url "https://github.com/DamienCassou/vdirel")
