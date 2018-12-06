b1a8641f2b441a3d0ad944dd5caacbdd6a63dcbe
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:19 +0000

    [Adding refine version: 20180315.2228]

---
+++ elpa/refine-20180315.2228/refine-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "refine" "20180315.2228" "interactive value editing" '((emacs "24.3") (s "1.11.0") (dash "2.12.0") (list-utils "0.4.4") (loop "1.2")) :commit "0a99439a0b4ed6f79b9a240ea1270140a9e328bc" :keywords '("convenience") :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk"))
