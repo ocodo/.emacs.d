873ad70fafb8203c1eb5975ec1fca3801d9018b4
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:49 +0000

    [Adding helm-perldoc version: 20160918.556]

---
+++ elpa/helm-perldoc-20160918.556/helm-perldoc-autoloads.el
@@ -0,0 +1,51 @@
+;;; helm-perldoc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-perldoc" "helm-perldoc.el" (0 0 0 0))
+;;; Generated autoloads from helm-perldoc.el
+
+(autoload 'helm-perldoc:carton-setup "helm-perldoc" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-perldoc:clear-carton-path "helm-perldoc" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-perldoc:setup "helm-perldoc" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-perldoc:history "helm-perldoc" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-perldoc "helm-perldoc" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-perldoc" '("helm-perldoc:" "with-perl5lib")))
+
+;;;***
+
+;;;### (autoloads nil nil ("helm-perldoc-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-perldoc-autoloads.el ends here
---
+++ elpa/helm-perldoc-20160918.556/helm-perldoc-pkg.el
@@ -0,0 +1,12 @@
+(define-package "helm-perldoc" "20160918.556" "perldoc with helm interface"
+  '((helm-core "2.0")
+    (deferred "0.3.1")
+    (emacs "24.4"))
+  :authors
+  '(("Syohei YOSHIDA" . "syohex@gmail.com"))
+  :maintainer
+  '("Syohei YOSHIDA" . "syohex@gmail.com")
+  :url "https://github.com/syohex/emacs-helm-perldoc")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
