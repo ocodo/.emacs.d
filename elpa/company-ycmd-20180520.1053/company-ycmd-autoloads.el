f593af3271785e7013656e773ed329aac7e1e013
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:14 +0000

    [Adding company-ycmd version: 20180520.1053]

---
+++ elpa/company-ycmd-20180520.1053/company-ycmd-autoloads.el
@@ -0,0 +1,27 @@
+;;; company-ycmd-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "company-ycmd" "company-ycmd.el" (0 0 0 0))
+;;; Generated autoloads from company-ycmd.el
+
+(autoload 'company-ycmd-setup "company-ycmd" "\
+Add company-ycmd to the front of company-backends.
+
+\(fn)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "company-ycmd" '("company-ycmd")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; company-ycmd-autoloads.el ends here
---
+++ elpa/company-ycmd-20180520.1053/company-ycmd-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-ycmd" "20180520.1053" "company-mode backend for ycmd" '((ycmd "1.3") (company "0.9.3") (deferred "0.5.1") (s "1.11.0") (dash "2.13.0") (let-alist "1.0.5") (f "0.19.0")) :commit "ef87d020d3314efbac2e8925c115d0ac5c128c2a" :url "https://github.com/abingham/emacs-ycmd")
