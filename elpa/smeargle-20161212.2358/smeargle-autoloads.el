1569db27586f213be623001eacefa94917545e7b
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:05 +0000

    [Adding smeargle version: 20161212.2358]

---
+++ elpa/smeargle-20161212.2358/smeargle-autoloads.el
@@ -0,0 +1,37 @@
+;;; smeargle-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "smeargle" "smeargle.el" (0 0 0 0))
+;;; Generated autoloads from smeargle.el
+
+(autoload 'smeargle-clear "smeargle" "\
+Clear smeargle overlays in current buffer.
+
+\(fn)" t nil)
+
+(autoload 'smeargle "smeargle" "\
+Highlight regions by last updated time.
+
+\(fn &optional UPDATE-TYPE)" t nil)
+
+(autoload 'smeargle-commits "smeargle" "\
+Highlight regions by age of commits.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "smeargle" '("sme")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smeargle-autoloads.el ends here
---
+++ elpa/smeargle-20161212.2358/smeargle-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smeargle" "20161212.2358" "Highlighting region by last updated time" '((emacs "24.3")) :commit "0665b1ff5109731898bc4a0ca6d939933b804777" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-smeargle")
