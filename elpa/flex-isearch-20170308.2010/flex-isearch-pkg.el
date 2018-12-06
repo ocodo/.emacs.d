469eacfb730c35f3c362e03f54a88285ec7e9cb2
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:27 +0000

    [Adding flex-isearch version: 20170308.2010]

---
+++ elpa/flex-isearch-20170308.2010/flex-isearch-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flex-isearch" "20170308.2010" "Flex matching (like ido) in isearch." 'nil :keywords '("convenience" "search") :authors '(("Jonathan Kotta" . "jpkotta@gmail.com")) :maintainer '("Jonathan Kotta" . "jpkotta@gmail.com") :url "https://bitbucket.org/jpkotta/flex-isearch")
