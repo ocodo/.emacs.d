92b21d6d95581ec9496caad3a88c9090a88a27b2
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:09 +0000

    [Adding evil-nerd-commenter version: 20180722.2325]

---
+++ elpa/evil-nerd-commenter-20180722.2325/evil-nerd-commenter-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil-nerd-commenter" "20180722.2325" "Comment/uncomment lines efficiently. Like Nerd Commenter in Vim"
+  '((emacs "24.4"))
+  :keywords
+  '("commenter" "vim" "line" "evil")
+  :authors
+  '(("Chen Bin" . "chenbin.sh@gmail.com"))
+  :maintainer
+  '("Chen Bin" . "chenbin.sh@gmail.com")
+  :url "http://github.com/redguardtoo/evil-nerd-commenter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
