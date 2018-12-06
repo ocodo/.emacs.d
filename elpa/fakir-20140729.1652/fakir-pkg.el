fb0d6cec0fe120914368ae7c1e2a1f1640b9f914
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:19 +0000

    [Adding fakir version: 20140729.1652]

---
+++ elpa/fakir-20140729.1652/fakir-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fakir" "20140729.1652" "fakeing bits of Emacs" '((noflet "0.0.8") (dash "1.3.2") (kv "0.0.19")) :commit "1fca406ad7de80fece6319ff75d4230b648534b0" :keywords '("lisp" "tools") :authors '(("Nic Ferrier" . "nferrier@ferrier.me.uk")) :maintainer '("Nic Ferrier" . "nferrier@ferrier.me.uk") :url "http://github.com/nicferrier/emacs-fakir")
