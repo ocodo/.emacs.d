f6e869b8562cc66415258b96b9f594d95c23d351
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:40 +0000

    [Adding helm-flx version: 20180103.516]

---
+++ elpa/helm-flx-20180103.516/helm-flx-autoloads.el
@@ -0,0 +1,37 @@
+;;; helm-flx-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-flx" "helm-flx.el" (0 0 0 0))
+;;; Generated autoloads from helm-flx.el
+
+(defvar helm-flx-mode nil "\
+Non-nil if Helm-Flx mode is enabled.
+See the `helm-flx-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `helm-flx-mode'.")
+
+(custom-autoload 'helm-flx-mode "helm-flx" nil)
+
+(autoload 'helm-flx-mode "helm-flx" "\
+helm-flx minor mode
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-flx" '("helm-flx-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-flx-autoloads.el ends here
---
+++ elpa/helm-flx-20180103.516/helm-flx-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-flx" "20180103.516" "Sort helm candidates by flx score" '((emacs "24.4") (helm "1.7.9") (flx "0.5")) :commit "6640fac5cb16bee73c95b8ed1248a4e5e113690e" :keywords '("convenience" "helm" "fuzzy" "flx") :authors '(("PythonNut" . "pythonnut@pythonnut.com")) :maintainer '("PythonNut" . "pythonnut@pythonnut.com") :url "https://github.com/PythonNut/helm-flx")
