97d02aeb42dd4eefc442ecbd4870bb3680cf1bb1
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:48 +0000

    [Adding borland-blue-theme version: 20160117.1321]

---
+++ elpa/borland-blue-theme-20160117.1321/borland-blue-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "borland-blue-theme" "20160117.1321" "Blue/yellow theme based on old DOS Borland/Turbo C IDE" '((emacs "24.1")) :commit "db74eefebbc89d3c62575f8f50b319e87b4a3470" :keywords '("themes") :authors '(("Alexey Veretennikov <alexey dot veretennikov at gmail dot com>")) :maintainer '("Alexey Veretennikov <alexey dot veretennikov at gmail dot com>") :url "http://github.com/fourier/borland-blue-theme")
