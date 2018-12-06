1145aa1c345a13a2d0a2d0177d7135254f149aff
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:45 +0000

    [Adding less-css-mode version: 20161001.453]

---
+++ elpa/less-css-mode-20161001.453/less-css-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "less-css-mode" "20161001.453" "Major mode for editing LESS CSS files (lesscss.org)" 'nil :commit "c7fa3d56d83206b28657f2e56439dc62280a2bf2" :keywords '("less" "css" "mode") :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/less-css-mode")
