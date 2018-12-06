3d5c02f60495333951b6737fa59a08086c021458
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:06 +0000

    [Adding mediawiki version: 20170813.555]

---
+++ elpa/mediawiki-20170813.555/mediawiki-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mediawiki" "20170813.555" "mediawiki frontend" 'nil :commit "8473e12d1839f5287a4227586bf117dad820f867" :keywords '("mediawiki" "wikipedia" "network" "wiki") :authors '(("Mark A. Hershberger" . "mah@everybody.org")) :maintainer '("Mark A. Hershberger" . "mah@everybody.org") :url "https://github.com/hexmode/mediawiki-el")
