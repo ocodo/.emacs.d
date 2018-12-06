eb97ff980741274ff3ba4ae293bed15e0402f6fe
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:39 +0000

    [Adding flymake-jshint version: 20140319.2200]

---
+++ elpa/flymake-jshint-20140319.2200/flymake-jshint-autoloads.el
@@ -0,0 +1,28 @@
+;;; flymake-jshint-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-jshint" "flymake-jshint.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from flymake-jshint.el
+
+(autoload 'flymake-jshint-load "flymake-jshint" "\
+Configure flymake mode to check the current buffer's JavaScript syntax.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-jshint" '("flymake-jshint-" "jshint-configuration-path")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-jshint-autoloads.el ends here
---
+++ elpa/flymake-jshint-20140319.2200/flymake-jshint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-jshint" "20140319.2200" "making flymake work with JSHint" '((flymake-easy "0.8")) :commit "79dd554c227883c487db38ac111306c8d5382c95" :keywords '("flymake" "jshint" "javascript") :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk"))
