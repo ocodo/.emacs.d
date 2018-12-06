91292cf6daf69fc8e390aef33b5558788599f2b3
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:41 +0000

    [Adding know-your-http-well version: 20160208.2304]

---
+++ elpa/know-your-http-well-20160208.2304/know-your-http-well-autoloads.el
@@ -0,0 +1,82 @@
+;;; know-your-http-well-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "http-headers" "http-headers.el" (0 0 0 0))
+;;; Generated autoloads from http-headers.el
+
+(autoload 'http-header "http-headers" "\
+Display the meaning of an HTTP header
+
+\(fn HEADER)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "http-headers" '("http-headers")))
+
+;;;***
+
+;;;### (autoloads nil "http-methods" "http-methods.el" (0 0 0 0))
+;;; Generated autoloads from http-methods.el
+
+(autoload 'http-method "http-methods" "\
+Display the meaning of an HTTP method
+
+\(fn METHOD)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "http-methods" '("http-methods")))
+
+;;;***
+
+;;;### (autoloads nil "http-relations" "http-relations.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from http-relations.el
+
+(autoload 'http-relation "http-relations" "\
+Display the meaning of an HTTP relation
+
+\(fn RELATION)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "http-relations" '("http-relations")))
+
+;;;***
+
+;;;### (autoloads nil "http-status-codes" "http-status-codes.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from http-status-codes.el
+
+(autoload 'http-status-code "http-status-codes" "\
+Display the meaning of an HTTP status code or phrase
+
+\(fn STATUS)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "http-status-codes" '("http-status")))
+
+;;;***
+
+;;;### (autoloads nil "media-types" "media-types.el" (0 0 0 0))
+;;; Generated autoloads from media-types.el
+
+(autoload 'media-type "media-types" "\
+Display the template of a media-type
+
+\(fn MEDIA-TYPE)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "media-types" '("media-types")))
+
+;;;***
+
+;;;### (autoloads nil nil ("know-your-http-well-pkg.el" "know-your-http-well.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; know-your-http-well-autoloads.el ends here
---
+++ elpa/know-your-http-well-20160208.2304/know-your-http-well-pkg.el
@@ -0,0 +1,4 @@
+(define-package "know-your-http-well" "20160208.2304" "Look up the meaning of HTTP headers, methods, relations, status codes" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
