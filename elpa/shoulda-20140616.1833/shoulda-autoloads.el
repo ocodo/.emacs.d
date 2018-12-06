05c92f328ad98b39972cdb38663f2cd67d641776
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:39 +0000

    [Adding shoulda version: 20140616.1833]

---
+++ elpa/shoulda-20140616.1833/shoulda-autoloads.el
@@ -0,0 +1,32 @@
+;;; shoulda-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "shoulda" "shoulda.el" (0 0 0 0))
+;;; Generated autoloads from shoulda.el
+
+(autoload 'shoulda-run-should-at-point "shoulda" "\
+Run Shoulda should test at point.
+
+\(fn)" t nil)
+
+(autoload 'shoulda-run-context-at-point "shoulda" "\
+Run Shoulda context test at point.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "shoulda" '("shoulda-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; shoulda-autoloads.el ends here
---
+++ elpa/shoulda-20140616.1833/shoulda-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "shoulda" "20140616.1833" "Shoulda test support for ruby" '((cl-lib "0.5")) :commit "fbe8eb8efc6cfcca1713283a290882cfcdc8725e" :keywords '("ruby" "tests" "shoulda") :authors '(("Marcwebbie" . "marcwebbie@gmail.com")) :maintainer '("Marcwebbie" . "marcwebbie@gmail.com"))
