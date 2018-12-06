076634b79d58ab51f6d03a7fde2ac5b24379f1ee
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:51 +0000

    [Adding literal-string version: 20170301.1530]

---
+++ elpa/literal-string-20170301.1530/literal-string-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "literal-string" "20170301.1530" "edit string literals in a dedicated buffer" '((markdown-mode "2.0") (emacs "25")) :commit "2ca4fc08b8e19e6183b1f1db747bb0a4aa4f98eb" :keywords '("lisp" "tools" "docs") :authors '(("Joost Diepenmaat" . "joost@zeekat.nl")) :maintainer '("Joost Diepenmaat" . "joost@zeekat.nl") :url "https://github.com/joodie/literal-string-mode/")
