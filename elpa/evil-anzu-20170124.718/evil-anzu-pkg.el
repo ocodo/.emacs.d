987bc8214e4662610b5d4a51e4b78824ad9af1fa
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:04 +0000

    [Adding evil-anzu version: 20170124.718]

---
+++ elpa/evil-anzu-20170124.718/evil-anzu-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-anzu" "20170124.718" "anzu for evil-mode" '((evil "1.0.0") (anzu "0.46")) :commit "9bca6ca14d865e7e005bc02a28a09b4ae74facc9" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com") ("Fredrik Bergroth" . "fbergroth@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-evil-anzu")
