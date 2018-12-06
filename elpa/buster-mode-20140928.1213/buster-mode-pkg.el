7bdfea70ba0adb6147df70aa685f075c5cd5d7a4
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:51 +0000

    [Adding buster-mode version: 20140928.1213]

---
+++ elpa/buster-mode-20140928.1213/buster-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "buster-mode" "20140928.1213" "Minor mode to speed up development when writing tests with Buster.js" 'nil :commit "de6958ef8369400922618b8d1e99abfa91b97ac5" :keywords '("buster" "testing" "javascript"))
