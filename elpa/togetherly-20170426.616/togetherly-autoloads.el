73f89bd5e6043db09967e0da91eb3335bf621ff3
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:32 +0000

    [Adding togetherly version: 20170426.616]

---
+++ elpa/togetherly-20170426.616/togetherly-autoloads.el
@@ -0,0 +1,22 @@
+;;; togetherly-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "togetherly" "togetherly.el" (0 0 0 0))
+;;; Generated autoloads from togetherly.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "togetherly" '("togetherly-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; togetherly-autoloads.el ends here
---
+++ elpa/togetherly-20170426.616/togetherly-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "togetherly" "20170426.616" "allow multiple clients to edit a single buffer online" '((cl-lib "0.3")) :commit "a6491bd5dd84f2aded0cd112ff06ae76ff78dfeb" :authors '(("zk_phi")) :maintainer '("zk_phi") :url "http://hins11.yu-yake.com/")
