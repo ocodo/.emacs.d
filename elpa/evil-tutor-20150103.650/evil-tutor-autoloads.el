e4402e56bdd6ffd81e0a9f0bb0d83589b69591f4
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:15 +0000

    [Adding evil-tutor version: 20150103.650]

---
+++ elpa/evil-tutor-20150103.650/evil-tutor-autoloads.el
@@ -0,0 +1,33 @@
+;;; evil-tutor-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "evil-tutor" "evil-tutor.el" (0 0 0 0))
+;;; Generated autoloads from evil-tutor.el
+
+(autoload 'evil-tutor-start "evil-tutor" "\
+Start a evil-tutor session.
+
+\(fn)" t nil)
+
+(defalias 'evil-tutor-resume 'evil-tutor-start)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-tutor" '("evil-tutor-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("evil-tutor-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; evil-tutor-autoloads.el ends here
---
+++ elpa/evil-tutor-20150103.650/evil-tutor-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil-tutor" "20150103.650" "Vimtutor adapted to Evil and wrapped in a major-mode"
+  '((evil "1.0.9"))
+  :keywords
+  '("convenience" "editing" "evil")
+  :authors
+  '(("Sylvain Benner" . "sylvain.benner@gmail.com"))
+  :maintainer
+  '("Sylvain Benner" . "sylvain.benner@gmail.com")
+  :url "https://github.com/syl20bnr/evil-tutor")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
