fedd903a64eb4c86494d39d4847c47125eff9d07
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:43 +0000

    [Adding kurecolor version: 20180401.1221]

---
+++ elpa/kurecolor-20180401.1221/kurecolor-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kurecolor" "20180401.1221" "color editing goodies for Emacs" '((emacs "24.1") (s "1.0")) :commit "a27153f6a01f38226920772dc4917b73166da5e6" :authors '(("Jason Milkins" . "jasonm23@gmail.com")) :maintainer '("Jason Milkins" . "jasonm23@gmail.com"))
