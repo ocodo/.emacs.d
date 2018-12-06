a1db9e34cc2e5e1764351d1689c5552588264250
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:43 +0000

    [Adding kv version: 20140108.1534]

---
+++ elpa/kv-20140108.1534/kv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kv" "20140108.1534" "key/value data structure functions" 'nil :commit "721148475bce38a70e0b678ba8aa923652e8900e" :keywords '("lisp") :authors '(("Nic Ferrier" . "nferrier@ferrier.me.uk")) :maintainer '("Nic Ferrier" . "nferrier@ferrier.me.uk"))
