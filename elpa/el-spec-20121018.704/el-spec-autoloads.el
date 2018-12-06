531cfca5a90dd09ea02d324cda554e0b674245e0
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:46 +0000

    [Adding el-spec version: 20121018.704]

---
+++ elpa/el-spec-20121018.704/el-spec-autoloads.el
@@ -0,0 +1,22 @@
+;;; el-spec-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "el-spec" "el-spec.el" (0 0 0 0))
+;;; Generated autoloads from el-spec.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "el-spec" '("el-s" "ert-" "describe")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; el-spec-autoloads.el ends here
---
+++ elpa/el-spec-20121018.704/el-spec-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "el-spec" "20121018.704" "ruby's rspec like syntax test frame work" 'nil :commit "1dbc465401d4aea5560318c4f13ff30920a0718d" :keywords '("test") :authors '(("Yuuki Arisawa" . "yuuki.ari@gmail.com")) :maintainer '("Yuuki Arisawa" . "yuuki.ari@gmail.com") :url "https://github.com/uk-ar/el-spec")
