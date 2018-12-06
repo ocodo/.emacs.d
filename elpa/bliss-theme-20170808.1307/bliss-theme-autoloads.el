5043dfdfe512454a379a0038339550c8e9f459f6
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:48 +0000

    [Adding bliss-theme version: 20170808.1307]

---
+++ elpa/bliss-theme-20170808.1307/bliss-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; bliss-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "bliss-theme" "bliss-theme.el" (0 0 0 0))
+;;; Generated autoloads from bliss-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "bliss-theme" '("bliss")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; bliss-theme-autoloads.el ends here
---
+++ elpa/bliss-theme-20170808.1307/bliss-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bliss-theme" "20170808.1307" "an Emacs 24 theme based on Bliss (tmTheme)" '((emacs "24.0")) :commit "c3cf6d8a666ab26909b7da158f9e94df71a5fbbf" :authors '(("Jason Milkins")) :maintainer '("Jason Milkins") :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
