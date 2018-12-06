3dfb3c1dc52b1e7c9df3d428bc40d38b6d720fb4
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:54 +0000

    [Adding helm-unicode version: 20180608.1407]

---
+++ elpa/helm-unicode-20180608.1407/helm-unicode-autoloads.el
@@ -0,0 +1,29 @@
+;;; helm-unicode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-unicode" "helm-unicode.el" (0 0 0 0))
+;;; Generated autoloads from helm-unicode.el
+
+(autoload 'helm-unicode "helm-unicode" "\
+Precofigured `helm' for looking up unicode characters by name.
+
+With prefix ARG, reinitialize the cache.
+
+\(fn ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-unicode" '("helm-unicode-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-unicode-autoloads.el ends here
---
+++ elpa/helm-unicode-20180608.1407/helm-unicode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-unicode" "20180608.1407" "Helm command for unicode characters." '((helm "1.9.8") (emacs "24.4")) :commit "fbeb0c5e741a6f462520884b744d43a9acbe1d34")
