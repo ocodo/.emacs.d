8f3237c1f27da3e37dfd38f7a8e015119e15b3e1
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:34 +0000

    [Adding dired-hacks-utils version: 20160527.2136]

---
+++ elpa/dired-hacks-utils-20160527.2136/dired-hacks-utils-autoloads.el
@@ -0,0 +1,23 @@
+;;; dired-hacks-utils-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "dired-hacks-utils" "dired-hacks-utils.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from dired-hacks-utils.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "dired-hacks-utils" '("dired-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dired-hacks-utils-autoloads.el ends here
