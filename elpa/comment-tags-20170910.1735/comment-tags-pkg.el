7dab91a5d848c3f6ee36028e401252292f8e810c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:08 +0000

    [Adding comment-tags version: 20170910.1735]

---
+++ elpa/comment-tags-20170910.1735/comment-tags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "comment-tags" "20170910.1735" "Highlight & navigate comment tags like 'TODO'." '((emacs "24.5")) :commit "7d914097f0a03484af71e621db533737fc692f58" :keywords '("convenience" "comments" "tags") :authors '(("Vincent Dumas" . "vincekd@gmail.com")) :maintainer '("Vincent Dumas" . "vincekd@gmail.com") :url "https://github.com/vincekd/comment-tags")
