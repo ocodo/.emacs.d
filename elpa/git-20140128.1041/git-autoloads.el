60cbcc61de0f6a0180e60ff569d3b6fb20d39f17
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:53 +0000

    [Adding git version: 20140128.1041]

---
+++ elpa/git-20140128.1041/git-autoloads.el
@@ -0,0 +1,22 @@
+;;; git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "git" "git.el" (0 0 0 0))
+;;; Generated autoloads from git.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "git" '("git-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; git-autoloads.el ends here
---
+++ elpa/git-20140128.1041/git-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git" "20140128.1041" "An Elisp API for programmatically using Git" '((s "1.7.0") (dash "2.2.0") (f "0.10.0")) :commit "a3396a7027a7d986598c6a2d6d5599bac918f3da" :keywords '("git") :authors '(("Johan Andersson" . "johan.rejeep@gmail.com")) :maintainer '("Johan Andersson" . "johan.rejeep@gmail.com") :url "http://github.com/rejeep/git.el")
