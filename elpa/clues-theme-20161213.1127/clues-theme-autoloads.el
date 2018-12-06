ab04d134bacec37b45080f90217ef786a47fdbe9
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:02 +0000

    [Adding clues-theme version: 20161213.1127]

---
+++ elpa/clues-theme-20161213.1127/clues-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; clues-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "clues-theme" "clues-theme.el" (0 0 0 0))
+;;; Generated autoloads from clues-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "clues-theme" '("clues")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; clues-theme-autoloads.el ends here
---
+++ elpa/clues-theme-20161213.1127/clues-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clues-theme" "20161213.1127" "an Emacs 24 theme which may well be fully awesome..." '((emacs "24.0")) :commit "abd61f2b7f3e98de58ca26e6d1230e70c6406cc7" :authors '(("Jason Milkins" . "jasonm23@gmail.com")) :maintainer '("Jason Milkins" . "jasonm23@gmail.com") :url "https://github.com/emacsfodder/emacs-clues-theme")
