4ddbb0d415129ff63327bc7dfcd238574fbf107c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:40 +0000

    [Adding helm-flycheck version: 20160710.829]

---
+++ elpa/helm-flycheck-20160710.829/helm-flycheck-autoloads.el
@@ -0,0 +1,27 @@
+;;; helm-flycheck-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-flycheck" "helm-flycheck.el" (0 0 0 0))
+;;; Generated autoloads from helm-flycheck.el
+
+(autoload 'helm-flycheck "helm-flycheck" "\
+Show flycheck errors with `helm'.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-flycheck" '("helm-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-flycheck-autoloads.el ends here
---
+++ elpa/helm-flycheck-20160710.829/helm-flycheck-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-flycheck" "20160710.829" "Show flycheck errors with helm" '((dash "2.12.1") (flycheck "28") (helm-core "1.9.8")) :commit "3cf7d3bb194acacc6395f88360588013d92675d6" :keywords '("helm" "flycheck") :authors '(("Yasuyuki Oka" . "yasuyk@gmail.com")) :maintainer '("Yasuyuki Oka" . "yasuyk@gmail.com") :url "https://github.com/yasuyk/helm-flycheck")
