18257536a16d8e07dd21baa11607ff5687685eea
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:25 +0000

    [Adding flatland-black-theme version: 20170808.1312]

---
+++ elpa/flatland-black-theme-20170808.1312/flatland-black-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; flatland-black-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flatland-black-theme" "flatland-black-theme.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from flatland-black-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flatland-black-theme" '("flatland-black")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flatland-black-theme-autoloads.el ends here
