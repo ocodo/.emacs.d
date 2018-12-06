e4b2e607cb95ff27c7a096b003b6df6a8cf2ee21
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:38 +0000

    [Adding keymap-utils version: 20180318.2237]

---
+++ elpa/keymap-utils-20180318.2237/keymap-utils-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "keymap-utils" "20180318.2237" "keymap utilities" '((cl-lib "0.3")) :commit "1ad766dbc111ec78b1a292da97b9bd4856cd2ff7" :keywords '("convenience" "extensions") :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/tarsius/keymap-utils")
