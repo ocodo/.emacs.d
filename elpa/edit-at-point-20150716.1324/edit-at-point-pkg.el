10cac9ce0557e4022f4e37a312fda947d3f5dcac
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:43 +0000

    [Adding edit-at-point version: 20150716.1324]

---
+++ elpa/edit-at-point-20150716.1324/edit-at-point-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "edit-at-point" "20150716.1324" "edit(copy,cut..) current things(word,symbol..) under cursor" 'nil :commit "3b800c11685102e1eab62ec71c5fc1589ebb81a7" :authors '((nil . "<e.enoson@gmail.com>")) :maintainer '(nil . "<e.enoson@gmail.com>") :url "http://github.com/enoson/edit-at-point.el")
