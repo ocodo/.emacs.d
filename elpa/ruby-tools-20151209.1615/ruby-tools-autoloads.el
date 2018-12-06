a304ee9fdf00154f8e5f4f2bd131f0119aeb04af
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:30 +0000

    [Adding ruby-tools version: 20151209.1615]

---
+++ elpa/ruby-tools-20151209.1615/ruby-tools-autoloads.el
@@ -0,0 +1,31 @@
+;;; ruby-tools-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ruby-tools" "ruby-tools.el" (0 0 0 0))
+;;; Generated autoloads from ruby-tools.el
+
+(autoload 'ruby-tools-mode "ruby-tools" "\
+Collection of handy functions for ruby-mode.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ruby-tools" '("ruby-tools-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("ruby-tools-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ruby-tools-autoloads.el ends here
---
+++ elpa/ruby-tools-20151209.1615/ruby-tools-pkg.el
@@ -0,0 +1,4 @@
+(define-package "ruby-tools" "20151209.1615" "Collection of handy functions for ruby-mode." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
