b328287150fbe88c37918490b560c48b27b023f8
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:50 +0000

    [Adding litable version: 20160922.1559]

---
+++ elpa/litable-20160922.1559/litable-autoloads.el
@@ -0,0 +1,27 @@
+;;; litable-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "litable" "litable.el" (0 0 0 0))
+;;; Generated autoloads from litable.el
+
+(autoload 'litable-mode "litable" "\
+Toggle litable-mode
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "litable" '("litable-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; litable-autoloads.el ends here
---
+++ elpa/litable-20160922.1559/litable-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "litable" "20160922.1559" "dynamic evaluation replacement with emacs" '((dash "2.6.0")) :commit "90a2dca14a6da9b24fe332a65cff899ab4a90810" :keywords '("lisp") :authors '(("Matus Goljer" . "matus.goljer@gmail.com")) :maintainer '("Matus Goljer" . "matus.goljer@gmail.com"))
