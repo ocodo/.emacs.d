f43cad2cb63c32b594205eaaf6a096794d35f968
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:07 +0000

    [Adding smyx-theme version: 20141127.828]

---
+++ elpa/smyx-theme-20141127.828/smyx-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smyx-theme" "20141127.828" "smyx Color Theme" 'nil :commit "6263f6b401bbabaed388c8efcfc0be2e58c51401" :keywords '("color" "theme" "smyx") :authors '(("Uriel G Maldonado" . "uriel781@gmail.com")) :maintainer '("Uriel G Maldonado" . "uriel781@gmail.com"))
