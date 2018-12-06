06e9c3cb34f549001cba4d819d5f24c3bdc9d54b
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:45 +0000

    [Adding helm-ispell version: 20151231.853]

---
+++ elpa/helm-ispell-20151231.853/helm-ispell-autoloads.el
@@ -0,0 +1,27 @@
+;;; helm-ispell-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-ispell" "helm-ispell.el" (0 0 0 0))
+;;; Generated autoloads from helm-ispell.el
+
+(autoload 'helm-ispell "helm-ispell" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-ispell" '("helm-ispell-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-ispell-autoloads.el ends here
