2c7f20683bd40f4fd455637b297d079f46161539
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:34 +0000

    [Adding parse-csv version: 20160512.1723]

---
+++ elpa/parse-csv-20160512.1723/parse-csv-autoloads.el
@@ -0,0 +1,22 @@
+;;; parse-csv-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "parse-csv" "parse-csv.el" (0 0 0 0))
+;;; Generated autoloads from parse-csv.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "parse-csv" '("parse-csv-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; parse-csv-autoloads.el ends here
---
+++ elpa/parse-csv-20160512.1723/parse-csv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "parse-csv" "20160512.1723" "Parse strings with CSV fields into s-expressions" 'nil :commit "96bef1ffbc89ea12d13311c9fa239c5c3e864890" :keywords '("csv") :authors '(("Edward Marco Baringer (Common Lisp)") ("Matt Curtis" . "matt.r.curtis@gmail.com")) :maintainer '("Matt Curtis" . "matt.r.curtis@gmail.com") :url "https://github.com/mrc/el-csv")
