15ccaf65c9bf82aaed1106fd49cee75e583087ba
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:51 +0000

    [Adding helm-robe version: 20151209.355]

---
+++ elpa/helm-robe-20151209.355/helm-robe-autoloads.el
@@ -0,0 +1,25 @@
+;;; helm-robe-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-robe" "helm-robe.el" (0 0 0 0))
+;;; Generated autoloads from helm-robe.el
+
+(autoload 'helm-robe-completing-read "helm-robe" "\
+
+
+\(fn PROMPT CHOICES &optional PREDICATE REQUIRE-MATCH &rest ARGS)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-robe-autoloads.el ends here
---
+++ elpa/helm-robe-20151209.355/helm-robe-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-robe" "20151209.355" "completing read function for robe" '((helm "1.7.7")) :commit "6e69543b4ee76c5f8f3f2510c76e6d9aed17a370" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-helm-robe")
