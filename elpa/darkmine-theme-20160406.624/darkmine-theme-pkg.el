6017009c04e070deae32f6f0dcc3e0ad23f4387c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:24 +0000

    [Adding darkmine-theme version: 20160406.624]

---
+++ elpa/darkmine-theme-20160406.624/darkmine-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "darkmine-theme" "20160406.624" "Yet another emacs dark color theme." 'nil :commit "7f7e82ca03bcad52911fa41fb3e204e32d6ee63e" :authors '(("Pierre Lecocq" . "pierre.lecocq@gmail.com")) :maintainer '("Pierre Lecocq" . "pierre.lecocq@gmail.com") :url "https://github.com/pierre-lecocq/darkmine-theme")
