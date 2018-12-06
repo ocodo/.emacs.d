8e306ed00194b34094b16a9470cb3e9186e8e3d2
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:17 +0000

    [Adding counsel-notmuch version: 20181203.935]

---
+++ elpa/counsel-notmuch-20181203.935/counsel-notmuch-autoloads.el
@@ -0,0 +1,28 @@
+;;; counsel-notmuch-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "counsel-notmuch" "counsel-notmuch.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from counsel-notmuch.el
+
+(autoload 'counsel-notmuch "counsel-notmuch" "\
+Search for your email in notmuch with INITIAL-INPUT.
+
+\(fn &optional INITIAL-INPUT)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "counsel-notmuch" '("counsel-notmuch-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; counsel-notmuch-autoloads.el ends here
---
+++ elpa/counsel-notmuch-20181203.935/counsel-notmuch-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-notmuch" "20181203.935" "Search emails in Notmuch asynchronously with Ivy" '((emacs "24") (ivy "0.10.0") (notmuch "0.21") (s "1.12.0")) :commit "a4a1562935e4180c42524c51609d1283e9be0688" :keywords '("mail") :authors '(("Alexander Fu Xi" . "fuxialexander@gmail.com")) :maintainer '("Alexander Fu Xi" . "fuxialexander@gmail.com") :url "https://github.com/fuxialexander/counsel-notmuch")
