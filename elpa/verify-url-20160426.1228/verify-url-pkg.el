883bb6371e8628e65ae85393802011a20c63ede2
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:10 +0000

    [Adding verify-url version: 20160426.1228]

---
+++ elpa/verify-url-20160426.1228/verify-url-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "verify-url" "20160426.1228" "find out invalid urls in the buffer or region" '((cl-lib "0.5")) :commit "d6f3623cda8cd526a2d198619b137059cb1ba1ab" :keywords '("convenience" "usability" "url") :authors '(("DarkSun" . "lujun9972@gmail.com")) :maintainer '("DarkSun" . "lujun9972@gmail.com") :url "https://github.com/lujun9972/verify-url")
