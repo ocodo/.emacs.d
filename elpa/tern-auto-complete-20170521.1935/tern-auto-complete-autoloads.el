1fe144a4d511f2c44451684cc7a1f1f28d382ada
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:28 +0000

    [Adding tern-auto-complete version: 20170521.1935]

---
+++ elpa/tern-auto-complete-20170521.1935/tern-auto-complete-autoloads.el
@@ -0,0 +1,28 @@
+;;; tern-auto-complete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "tern-auto-complete" "tern-auto-complete.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from tern-auto-complete.el
+
+(autoload 'tern-ac-setup "tern-auto-complete" "\
+Setup auto-complete for tern-mode.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tern-auto-complete" '("tern-ac-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; tern-auto-complete-autoloads.el ends here
---
+++ elpa/tern-auto-complete-20170521.1935/tern-auto-complete-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tern-auto-complete" "20170521.1935" "Tern Completion by auto-complete.el" '((tern "0.0.1") (auto-complete "1.4") (cl-lib "0.5") (emacs "24")) :commit "e94e20be8510040744fc803c9fda6bbb9625c6d2" :authors '(("<m.sakurai at kiwanami.net>")) :maintainer '("<m.sakurai at kiwanami.net>"))
