4750d917373e7ae59ed5a3764e25a2e3b63f332e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:27 +0000

    [Adding j-mode version: 20171224.1856]

---
+++ elpa/j-mode-20171224.1856/j-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "j-mode" "20171224.1856" "Major mode for editing J programs" 'nil :keywords
+  '("j" "languages")
+  :url "http://github.com/zellio/j-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
