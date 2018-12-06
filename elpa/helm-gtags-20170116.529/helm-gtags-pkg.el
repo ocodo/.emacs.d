692800f1aceb3d1ea0f3b7c5e83f4c4694903b6e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:44 +0000

    [Adding helm-gtags version: 20170116.529]

---
+++ elpa/helm-gtags-20170116.529/helm-gtags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-gtags" "20170116.529" "GNU GLOBAL helm interface" '((emacs "24.4") (helm "2.0")) :commit "108e93d0d099ebb7b98847388f368311cf177033" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-helm-gtags")
