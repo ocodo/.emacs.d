322c7edf0f7d08793301987b4aabae14322b2344
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:08 +0000

    [Adding httpcode version: 20121002.345]

---
+++ elpa/httpcode-20121002.345/httpcode-autoloads.el
@@ -0,0 +1,27 @@
+;;; httpcode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "httpcode" "httpcode.el" (0 0 0 0))
+;;; Generated autoloads from httpcode.el
+
+(autoload 'hc "httpcode" "\
+Display the meaning of an HTTP status code
+
+\(fn CODE)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "httpcode" '("http-codes")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; httpcode-autoloads.el ends here
---
+++ elpa/httpcode-20121002.345/httpcode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "httpcode" "20121002.345" "explains the meaning of an HTTP status code" 'nil :commit "a45e735082b09477cd704a99294d336cdbeb12ba" :authors '(("Ruslan Spivak" . "ruslan.spivak@gmail.com")) :maintainer '("Ruslan Spivak" . "ruslan.spivak@gmail.com") :url "http://github.com/rspivak/httpcode.el")
