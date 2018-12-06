2bd7108642857a5d011bfa77c84fbb5e7cea4e81
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:04 +0000

    [Adding go-direx version: 20150316.143]

---
+++ elpa/go-direx-20150316.143/go-direx-autoloads.el
@@ -0,0 +1,32 @@
+;;; go-direx-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "go-direx" "go-direx.el" (0 0 0 0))
+;;; Generated autoloads from go-direx.el
+
+(autoload 'go-direx-pop-to-buffer "go-direx" "\
+
+
+\(fn)" t nil)
+
+(autoload 'go-direx-switch-to-buffer "go-direx" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "go-direx" '("go-direx-" "direx:")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-direx-autoloads.el ends here
---
+++ elpa/go-direx-20150316.143/go-direx-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-direx" "20150316.143" "Tree style source code viewer for Go language" '((direx "1.0.0") (cl-lib "0.5")) :commit "8f2206469328ee932c7f1892f5e1fb02dec98432" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-go-direx")
