11ee76f10a729e80952be1b1b776b68a83f8f62e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:52 +0000

    [Adding helm-smex version: 20171004.2008]

---
+++ elpa/helm-smex-20171004.2008/helm-smex-autoloads.el
@@ -0,0 +1,32 @@
+;;; helm-smex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-smex" "helm-smex.el" (0 0 0 0))
+;;; Generated autoloads from helm-smex.el
+
+(autoload 'helm-smex "helm-smex" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-smex-major-mode-commands "helm-smex" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-smex" '("helm-smex-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-smex-autoloads.el ends here
