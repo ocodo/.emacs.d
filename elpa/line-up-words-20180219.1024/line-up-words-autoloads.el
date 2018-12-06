1a17b76789255ab0be87e24fe9a0e08a0510931e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:47 +0000

    [Adding line-up-words version: 20180219.1024]

---
+++ elpa/line-up-words-20180219.1024/line-up-words-autoloads.el
@@ -0,0 +1,27 @@
+;;; line-up-words-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "line-up-words" "line-up-words.el" (0 0 0 0))
+;;; Generated autoloads from line-up-words.el
+
+(autoload 'line-up-words "line-up-words" "\
+Try to align words in the region in an intelligent way.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "line-up-words" '("line-up-words-command")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; line-up-words-autoloads.el ends here
---
+++ elpa/line-up-words-20180219.1024/line-up-words-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "line-up-words" "20180219.1024" "Align words in an intelligent way" 'nil :commit "b6713d19dbc507a30e39f892f698a511f1cf3d1f" :url "https://github.com/janestreet/line-up-words")
