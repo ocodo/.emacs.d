1d1f6fa25d51fc44b3842d98db21afd7d63c26f8
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:48 +0000

    [Adding helm-make version: 20181107.2126]

---
+++ elpa/helm-make-20181107.2126/helm-make-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-make" "20181107.2126" "Select a Makefile target with helm" '((helm "1.5.3") (projectile "0.11.0")) :commit "f86c3973af760df6d7d0ecc61c3d9d14e1d93e2b" :keywords '("makefile") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/helm-make")
