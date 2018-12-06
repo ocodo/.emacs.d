1c1420c0ad1f5ff24c0653f3243c1a14f70dfc36
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:17 +0000

    [Adding gotham-theme version: 20171013.1916]

---
+++ elpa/gotham-theme-20171013.1916/gotham-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; gotham-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "gotham-theme" "gotham-theme.el" (0 0 0 0))
+;;; Generated autoloads from gotham-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "gotham-theme" '("gotham")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; gotham-theme-autoloads.el ends here
---
+++ elpa/gotham-theme-20171013.1916/gotham-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gotham-theme" "20171013.1916" "A very dark Emacs color theme." 'nil :commit "5e97554d1f9639698faedb0660e63694be33bd84" :authors '(("Vasilij Schneidermann" . "v.schneidermann@gmail.com")) :maintainer '("Vasilij Schneidermann" . "v.schneidermann@gmail.com") :url "https://github.com/wasamasa/gotham-theme")
