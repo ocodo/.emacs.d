465b6c3d7b46a645d326dbdc5288840a86f6c2dd
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:38 +0000

    [Adding shift-number version: 20170301.1459]

---
+++ elpa/shift-number-20170301.1459/shift-number-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "shift-number" "20170301.1459" "Increase/decrease the number at point" 'nil :commit "cd099a5582fc996b800ac7607f6c38a004ce9740" :keywords '("convenience") :authors '(("Alex Kost" . "alezost@gmail.com")) :maintainer '("Alex Kost" . "alezost@gmail.com") :url "https://github.com/alezost/shift-number.el")
