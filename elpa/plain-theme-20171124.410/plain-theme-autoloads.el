14f821d6523da137c2f39fe416a6ae9ae86e2741
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:07:44 +0000

    [Adding plain-theme version: 20171124.410]

---
+++ elpa/plain-theme-20171124.410/plain-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; plain-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "plain-theme" "plain-theme.el" (0 0 0 0))
+;;; Generated autoloads from plain-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "plain-theme" '("plain")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; plain-theme-autoloads.el ends here
---
+++ elpa/plain-theme-20171124.410/plain-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "plain-theme" "20171124.410" "Plain theme without syntax highlighting" '((emacs "24")) :commit "a3d5389a44326314da21f147bad2ede60e2cf986")
