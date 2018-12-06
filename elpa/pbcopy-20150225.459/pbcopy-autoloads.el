4cfcce1250d0d3b62234ba593684cec969724f60
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:35 +0000

    [Adding pbcopy version: 20150225.459]

---
+++ elpa/pbcopy-20150225.459/pbcopy-autoloads.el
@@ -0,0 +1,32 @@
+;;; pbcopy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "pbcopy" "pbcopy.el" (0 0 0 0))
+;;; Generated autoloads from pbcopy.el
+
+(autoload 'turn-on-pbcopy "pbcopy" "\
+
+
+\(fn)" t nil)
+
+(autoload 'turn-off-pbcopy "pbcopy" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "pbcopy" '("pbpaste-program" "pbcopy-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; pbcopy-autoloads.el ends here
---
+++ elpa/pbcopy-20150225.459/pbcopy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pbcopy" "20150225.459" "Emacs Interface to pbcopy" 'nil :commit "338f7245746b5de1bb96c5cc2b32bfd9b5d83272" :keywords '("mac" "osx" "pbcopy") :authors '(("Daniel Nelson")) :maintainer '("Daniel Nelson") :url "https://github.com/jkp/pbcopy.el")
