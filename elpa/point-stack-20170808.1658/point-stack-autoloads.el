34a4d13fafbd49f089c2dbf36a328ea8849175a6
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:07:44 +0000

    [Adding point-stack version: 20170808.1658]

---
+++ elpa/point-stack-20170808.1658/point-stack-autoloads.el
@@ -0,0 +1,44 @@
+;;; point-stack-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "point-stack" "point-stack.el" (0 0 0 0))
+;;; Generated autoloads from point-stack.el
+
+(autoload 'point-stack-push "point-stack" "\
+Push current buffer, point, and window scroll position onto the stack.
+
+\(fn)" t nil)
+
+(autoload 'point-stack-pop "point-stack" "\
+Push current location on forward stack, move to previous location.
+
+\(fn)" t nil)
+
+(autoload 'point-stack-forward-stack-pop "point-stack" "\
+Push current location on stack, pop and move to location from forward stack.
+
+\(fn)" t nil)
+
+(autoload 'point-stack-setup-advices "point-stack" "\
+Advise navigation functions to call `point-stack-push' before
+any navigation is made. This way, it can be used as a replacement
+for the global mark ring.
+
+\(fn)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "point-stack" '("point-stack-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; point-stack-autoloads.el ends here
---
+++ elpa/point-stack-20170808.1658/point-stack-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "point-stack" "20170808.1658" "Back and forward navigation through buffer locations" 'nil :commit "76e17311e3a810314c7d31ac46dc55450ff30fa2" :authors '(("Matt Harrison" . "matthewharrison@gmail.com") ("Dmitry Gutov" . "dgutov@yandex.ru")) :maintainer '("Matt Harrison" . "matthewharrison@gmail.com"))
