dddb1b08cb6808cc1d25964ee35b508b7b6522d5
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:23 +0000

    [Adding darcula-theme version: 20171227.1845]

---
+++ elpa/darcula-theme-20171227.1845/darcula-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; darcula-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "darcula-theme" "darcula-theme.el" (0 0 0 0))
+;;; Generated autoloads from darcula-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "darcula-theme" '("darcula")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; darcula-theme-autoloads.el ends here
---
+++ elpa/darcula-theme-20171227.1845/darcula-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "darcula-theme" "20171227.1845" "Inspired by IntelliJ's Darcula theme" 'nil :commit "d9b82b58ded9014985be6658f4ab17e26ed9e93e" :keywords '("faces") :authors '(("Sam Halliday" . "Sam.Halliday@gmail.com")) :maintainer '("Sam Halliday" . "Sam.Halliday@gmail.com") :url "https://gitlab.com/fommil/emacs-darcula-theme")
