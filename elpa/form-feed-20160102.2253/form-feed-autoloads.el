1815a134dfa9767798ebdd024eca796ed88aff63
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:47 +0000

    [Adding form-feed version: 20160102.2253]

---
+++ elpa/form-feed-20160102.2253/form-feed-autoloads.el
@@ -0,0 +1,31 @@
+;;; form-feed-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "form-feed" "form-feed.el" (0 0 0 0))
+;;; Generated autoloads from form-feed.el
+
+(autoload 'form-feed-mode "form-feed" "\
+Toggle form-feed-mode.
+
+This minor mode displays page delimiters which usually appear as ^L
+glyphs on a single line as horizontal lines spanning the entire
+window.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "form-feed" '("form-feed-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; form-feed-autoloads.el ends here
---
+++ elpa/form-feed-20160102.2253/form-feed-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "form-feed" "20160102.2253" "Display ^L glyphs as horizontal lines" 'nil :commit "799ca3e72b20a59a755a094b8cead57f654f3170" :keywords '("faces") :authors '(("Vasilij Schneidermann" . "v.schneidermann@gmail.com")) :maintainer '("Vasilij Schneidermann" . "v.schneidermann@gmail.com") :url "https://github.com/wasamasa/form-feed")
