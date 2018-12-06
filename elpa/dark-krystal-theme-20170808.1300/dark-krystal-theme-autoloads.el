e060ba3f18d07b5f00e3eb1f6af79bd998cda7b9
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:24 +0000

    [Adding dark-krystal-theme version: 20170808.1300]

---
+++ elpa/dark-krystal-theme-20170808.1300/dark-krystal-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; dark-krystal-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "dark-krystal-theme" "dark-krystal-theme.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from dark-krystal-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "dark-krystal-theme" '("dark-krystal")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dark-krystal-theme-autoloads.el ends here
---
+++ elpa/dark-krystal-theme-20170808.1300/dark-krystal-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dark-krystal-theme" "20170808.1300" "an Emacs 24 theme based on Dark Krystal (tmTheme)" '((emacs "24.0")) :commit "79084b99665dc9ffb0ec62cc092349a5ecebebbc" :authors '(("Jason Milkins")) :maintainer '("Jason Milkins") :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
