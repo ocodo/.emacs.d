549d41f684122872c80adce151beec9a42611bb4
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:20 +0000

    [Adding white-theme version: 20160917.1743]

---
+++ elpa/white-theme-20160917.1743/white-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "white-theme" "20160917.1743" "Minimalistic light color theme inspired by basic-theme" '((emacs "24")) :commit "e9e6d5b9d43da6eb15e86f5fbc8b1ba83abe8c78" :keywords '("color" "theme" "minimal" "basic" "simple" "white") :authors '(("Anler Hernandez Peral" . "inbox@anler.me")) :maintainer '("Anler Hernandez Peral" . "inbox@anler.me") :url "http://github.com/anler/white-theme.el")
