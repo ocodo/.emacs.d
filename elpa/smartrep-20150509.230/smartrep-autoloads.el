810101b89f71ddfae8efdbdab25fa576e36c5eeb
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:05 +0000

    [Adding smartrep version: 20150509.230]

---
+++ elpa/smartrep-20150509.230/smartrep-autoloads.el
@@ -0,0 +1,22 @@
+;;; smartrep-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "smartrep" "smartrep.el" (0 0 0 0))
+;;; Generated autoloads from smartrep.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "smartrep" '("smartrep-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smartrep-autoloads.el ends here
---
+++ elpa/smartrep-20150509.230/smartrep-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smartrep" "20150509.230" "Support sequential operation which omitted prefix keys." 'nil :commit "f0ff5a6d7b8603603598ae3045c98b011e58d86e" :keywords '("convenience") :authors '(("myuhe <yuhei.maeda_at_gmail.com>")) :maintainer '("myuhe") :url "https://github.com/myuhe/smartrep.el")
