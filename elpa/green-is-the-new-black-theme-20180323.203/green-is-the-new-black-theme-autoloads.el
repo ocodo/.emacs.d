a56728e90a2b066727382b1ef57725129bfcf038
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:21 +0000

    [Adding green-is-the-new-black-theme version: 20180323.203]

---
+++ elpa/green-is-the-new-black-theme-20180323.203/green-is-the-new-black-theme-autoloads.el
@@ -0,0 +1,23 @@
+;;; green-is-the-new-black-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "green-is-the-new-black-theme" "green-is-the-new-black-theme.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from green-is-the-new-black-theme.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "green-is-the-new-black-theme" '("green-is-the-new-black")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; green-is-the-new-black-theme-autoloads.el ends here
