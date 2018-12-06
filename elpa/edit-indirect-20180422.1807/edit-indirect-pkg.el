08d031c4d6acb1250da3d4cdbfba5a8d77dae24a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:44 +0000

    [Adding edit-indirect version: 20180422.1807]

---
+++ elpa/edit-indirect-20180422.1807/edit-indirect-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "edit-indirect" "20180422.1807" "Edit regions in separate buffers" '((emacs "24.3")) :commit "de645d8144e8a08f039a9c88185121ec81d957ef" :authors '(("Fanael Linithien" . "fanael4@gmail.com")) :maintainer '("Fanael Linithien" . "fanael4@gmail.com") :url "https://github.com/Fanael/edit-indirect")
