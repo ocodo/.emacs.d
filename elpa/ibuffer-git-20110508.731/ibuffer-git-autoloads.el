81068b7ae2691d1ac4252388797f4d5380762375
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:09 +0000

    [Adding ibuffer-git version: 20110508.731]

---
+++ elpa/ibuffer-git-20110508.731/ibuffer-git-autoloads.el
@@ -0,0 +1,22 @@
+;;; ibuffer-git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ibuffer-git" "ibuffer-git.el" (0 0 0 0))
+;;; Generated autoloads from ibuffer-git.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ibuffer-git" '("git-status" "ibuffer-git-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ibuffer-git-autoloads.el ends here
---
+++ elpa/ibuffer-git-20110508.731/ibuffer-git-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ibuffer-git" "20110508.731" "show git status in ibuffer column" 'nil :commit "d326319c05ddb8280885b31f9094040c1b365876" :keywords '("convenience") :authors '(("Jonathan Rockway" . "jon@jrock.us")) :maintainer '("Jonathan Rockway" . "jon@jrock.us"))
