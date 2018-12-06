1ef07f118fac3623a7d70c99acfc20eee7c9637e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:32 +0000

    [Adding helm-bundle-show version: 20151221.1230]

---
+++ elpa/helm-bundle-show-20151221.1230/helm-bundle-show-autoloads.el
@@ -0,0 +1,28 @@
+;;; helm-bundle-show-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-bundle-show" "helm-bundle-show.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from helm-bundle-show.el
+
+(autoload 'helm-bundle-show "helm-bundle-show" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-bundle-show" '("helm-bundle-show-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-bundle-show-autoloads.el ends here
---
+++ elpa/helm-bundle-show-20151221.1230/helm-bundle-show-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-bundle-show" "20151221.1230" "bundle show with helm interface" '((helm "1.8.0")) :commit "850fecb36f609f1dfd5d20ca0170c9a6b7f90ab9" :authors '(("Takashi Masuda" . "masutaka.net@gmail.com")) :maintainer '("Takashi Masuda" . "masutaka.net@gmail.com") :url "https://github.com/masutaka/emacs-helm-bundle-show")
