4001e9cf8e3fdab5b4e7c90123425dac40dab199
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:38 +0000

    [Adding helm-dired-history version: 20170524.1046]

---
+++ elpa/helm-dired-history-20170524.1046/helm-dired-history-autoloads.el
@@ -0,0 +1,23 @@
+;;; helm-dired-history-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-dired-history" "helm-dired-history.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from helm-dired-history.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-dired-history" '("helm-dired-history-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-dired-history-autoloads.el ends here
---
+++ elpa/helm-dired-history-20170524.1046/helm-dired-history-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-dired-history" "20170524.1046" "Show dired history with helm.el support." '((helm "1.9.8") (cl-lib "0.5")) :commit "281523f9fc46cf00fafd670ba5cd16552a607212" :keywords '("helm" "dired history") :authors '(("Joseph(纪秀峰)" . "jixiuf@gmail.com")) :maintainer '("Joseph(纪秀峰)" . "jixiuf@gmail.com") :url "https://github.com/jixiuf/helm-dired-history")
