2451dede44188d5eac1942fb6758e3f2ce72a98c
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:20 +0000

    [Adding request version: 20181129.1138]

---
+++ elpa/request-20181129.1138/request-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "request" "20181129.1138" "Compatible layer for URL request in Emacs" '((emacs "24.4")) :commit "b12f54af8ab6656aa8c1143c5ccc7675c5bb02cd" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
