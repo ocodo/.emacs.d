ed2c79073f493e42c2a906ad650feb94054eee71
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:06 +0000

    [Adding evil-exchange version: 20170511.259]

---
+++ elpa/evil-exchange-20170511.259/evil-exchange-autoloads.el
@@ -0,0 +1,40 @@
+;;; evil-exchange-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "evil-exchange" "evil-exchange.el" (0 0 0 0))
+;;; Generated autoloads from evil-exchange.el
+
+(autoload 'evil-exchange "evil-exchange" "\
+Exchange two regions with evil motion." t)
+
+(autoload 'evil-exchange-cancel "evil-exchange" "\
+Cancel current pending exchange.
+
+\(fn)" t nil)
+
+(autoload 'evil-exchange-install "evil-exchange" "\
+Setting evil-exchange key bindings.
+
+\(fn)" nil nil)
+
+(autoload 'evil-exchange-cx-install "evil-exchange" "\
+Setting evil-exchange key bindings in a vim-compatible way
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-exchange" '("evil-exchange")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; evil-exchange-autoloads.el ends here
---
+++ elpa/evil-exchange-20170511.259/evil-exchange-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-exchange" "20170511.259" "Exchange text more easily within Evil" '((evil "1.2.8") (cl-lib "0.3")) :commit "47691537815150715e64e6f6ec79be7746c96120" :keywords '("evil" "plugin") :authors '(("Dewdrops" . "v_v_4474@126.com")) :maintainer '("Dewdrops" . "v_v_4474@126.com") :url "http://github.com/Dewdrops/evil-exchange")
