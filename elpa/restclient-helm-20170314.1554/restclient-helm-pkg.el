b9f98b9491e16e1f43fa277dce4eb2c088e1c506
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:22 +0000

    [Adding restclient-helm version: 20170314.1554]

---
+++ elpa/restclient-helm-20170314.1554/restclient-helm-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "restclient-helm" "20170314.1554" "helm interface for restclient.el" '((restclient "0") (helm "1.9.4")) :commit "859d944796ce298b5779d9d256bd8d271d57e221" :keywords '("http" "helm") :authors '(("Pavel Kurnosov" . "pashky@gmail.com")) :maintainer '("Pavel Kurnosov" . "pashky@gmail.com"))
