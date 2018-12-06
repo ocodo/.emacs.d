4ba1d2b62a434bbee55cdab92f9fd70b328c2dce
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:54 +0000

    [Adding loop version: 20160813.1407]

---
+++ elpa/loop-20160813.1407/loop-autoloads.el
@@ -0,0 +1,22 @@
+;;; loop-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "loop" "loop.el" (0 0 0 0))
+;;; Generated autoloads from loop.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "loop" '("loop-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; loop-autoloads.el ends here
---
+++ elpa/loop-20160813.1407/loop-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "loop" "20160813.1407" "friendly imperative loop structures" 'nil :commit "e22807f83a0890dc8a904c51ee0742c34efccc6c" :keywords '("loop" "while" "for each" "break" "continue") :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk"))
