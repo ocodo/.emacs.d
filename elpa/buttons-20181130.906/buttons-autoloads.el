725a5307e6219e0414c0deeb17532b8735cbb4b1
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:54 +0000

    [Adding buttons version: 20181130.906]

---
+++ elpa/buttons-20181130.906/buttons-autoloads.el
@@ -0,0 +1,22 @@
+;;; buttons-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "buttons" "buttons.el" (0 0 0 0))
+;;; Generated autoloads from buttons.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "buttons" '("buttons-" "defbuttons")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; buttons-autoloads.el ends here
---
+++ elpa/buttons-20181130.906/buttons-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "buttons" "20181130.906" "Define and visualize hierarchies of keymaps" '((cl-lib "0.3")) :commit "da6eb4d26683cde3ef28fa726385c9697b199081" :keywords '("keymap" "template" "snippet") :authors '(("Ernesto Alfonso")) :maintainer '(nil . "(concat \"erjoalgo\" \"@\" \"gmail\" \".com\")") :url "http://github.com/erjoalgo/emacs-buttons")
