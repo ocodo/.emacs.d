648dab02e87155116acc96398325034560bd0313
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:11 +0000

    [Adding visual-regexp-steroids version: 20170222.253]

---
+++ elpa/visual-regexp-steroids-20170222.253/visual-regexp-steroids-pkg.el
@@ -0,0 +1,12 @@
+(define-package "visual-regexp-steroids" "20170222.253" "Extends visual-regexp to support other regexp engines"
+  '((visual-regexp "1.1"))
+  :keywords
+  '("external" "foreign" "regexp" "replace" "python" "visual" "feedback")
+  :authors
+  '(("Marko Bencun" . "mbencun@gmail.com"))
+  :maintainer
+  '("Marko Bencun" . "mbencun@gmail.com")
+  :url "https://github.com/benma/visual-regexp-steroids.el/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
