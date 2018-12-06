0b30df145d260973908fcfdf1fd73e748dd82d30
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:36 +0000

    [Adding flymake-coffee version: 20170723.146]

---
+++ elpa/flymake-coffee-20170723.146/flymake-coffee-autoloads.el
@@ -0,0 +1,28 @@
+;;; flymake-coffee-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-coffee" "flymake-coffee.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from flymake-coffee.el
+
+(autoload 'flymake-coffee-load "flymake-coffee" "\
+Configure flymake mode to check the current buffer's coffeescript syntax.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-coffee" '("flymake-coffee-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-coffee-autoloads.el ends here
---
+++ elpa/flymake-coffee-20170723.146/flymake-coffee-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-coffee" "20170723.146" "A flymake handler for coffee script" '((flymake-easy "0.1")) :commit "dee295acf30820ed15fe0de17137d50bc27fc80c" :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/flymake-coffee")
