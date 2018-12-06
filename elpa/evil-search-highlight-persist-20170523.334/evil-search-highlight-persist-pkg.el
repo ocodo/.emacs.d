6e2c1ad7dcb9023564ee0dd9ff2bab38ebedddda
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:12 +0000

    [Adding evil-search-highlight-persist version: 20170523.334]

---
+++ elpa/evil-search-highlight-persist-20170523.334/evil-search-highlight-persist-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-search-highlight-persist" "20170523.334" "Persistent highlights after search" '((highlight "0")) :commit "979d2dec58d3b9c5ca5fdf4bb802a0209913794e" :authors '(("Juanjo Alvarez" . "juanjo@juanjoalvarez.net")) :maintainer '("Juanjo Alvarez" . "juanjo@juanjoalvarez.net"))
