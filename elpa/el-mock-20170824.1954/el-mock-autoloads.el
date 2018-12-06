cf825e37f6f671df5a7528a1312cc9f87f3cc4a1
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:45 +0000

    [Adding el-mock version: 20170824.1954]

---
+++ elpa/el-mock-20170824.1954/el-mock-autoloads.el
@@ -0,0 +1,22 @@
+;;; el-mock-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "el-mock" "el-mock.el" (0 0 0 0))
+;;; Generated autoloads from el-mock.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "el-mock" '("stub" "mock" "not-called" "with-" "in-mocking" "-stubbed-functions" "-mocked-functions")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; el-mock-autoloads.el ends here
---
+++ elpa/el-mock-20170824.1954/el-mock-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "el-mock" "20170824.1954" "Tiny Mock and Stub framework in Emacs Lisp" 'nil :commit "5df1d3a956544f1d3ad0bcd81daf47fff33ab8cc" :keywords '("lisp" "testing" "unittest") :authors '(("rubikitch" . "rubikitch@ruby-lang.org")) :maintainer '("Johan Andersson" . "johan.rejeep@gmail.com") :url "http://github.com/rejeep/el-mock.el")
