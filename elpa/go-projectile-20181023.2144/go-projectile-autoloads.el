0dce53d39e446cc247bd2a64f14d21c0721cfffc
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:09 +0000

    [Adding go-projectile version: 20181023.2144]

---
+++ elpa/go-projectile-20181023.2144/go-projectile-autoloads.el
@@ -0,0 +1,22 @@
+;;; go-projectile-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "go-projectile" "go-projectile.el" (0 0 0 0))
+;;; Generated autoloads from go-projectile.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "go-projectile" '("go-projectile-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-projectile-autoloads.el ends here
---
+++ elpa/go-projectile-20181023.2144/go-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-projectile" "20181023.2144" "Go add-ons for Projectile" '((projectile "0.10.0") (go-mode "0") (go-eldoc "0.16") (go-rename "0") (go-guru "0")) :commit "7910884b4de560f3fc70b53752f658ef9cdc02cd" :keywords '("project" "convenience") :authors '(("Doug MacEachern" . "dougm@vmware.com")) :maintainer '("Doug MacEachern" . "dougm@vmware.com") :url "https://github.com/dougm/go-projectile")
