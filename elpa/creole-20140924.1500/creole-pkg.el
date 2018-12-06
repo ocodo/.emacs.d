64d406aebbedb6c8adfb543f1c8db160882de0f1
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:59:19 +0000

    [Adding creole version: 20140924.1500]

---
+++ elpa/creole-20140924.1500/creole-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "creole" "20140924.1500" "A parser for the Creole Wiki language" '((noflet "0.0.3") (kv "0.0.17")) :commit "7d5cffe93857f6c75ca09ac79c0e47b8d4410e53" :keywords '("lisp" "creole" "wiki") :authors '(("Nic Ferrier" . "nferrier@ferrier.me.uk")) :maintainer '("Nic Ferrier" . "nferrier@ferrier.me.uk"))
