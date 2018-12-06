9b46ca9629eb7e2dc628b4539b21c9b1e1fced5d
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:13 +0000

    [Adding google version: 20140416.1748]

---
+++ elpa/google-20140416.1748/google-autoloads.el
@@ -0,0 +1,22 @@
+;;; google-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "google" "google.el" (0 0 0 0))
+;;; Generated autoloads from google.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "google" '("google-" "define-google-search-domain")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; google-autoloads.el ends here
---
+++ elpa/google-20140416.1748/google-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "google" "20140416.1748" "Emacs interface to the Google API" 'nil :commit "3b3189a8b201c8d36fed6e61496274e530dd40bd" :keywords '("comm" "processes" "tools") :authors '(("Edward O'Connor" . "ted@oconnor.cx")) :maintainer '("Edward O'Connor" . "ted@oconnor.cx"))
