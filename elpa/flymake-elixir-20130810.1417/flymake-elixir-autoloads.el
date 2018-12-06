ea81968528582186521df1c50ec32720a04cc32a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:37 +0000

    [Adding flymake-elixir version: 20130810.1417]

---
+++ elpa/flymake-elixir-20130810.1417/flymake-elixir-autoloads.el
@@ -0,0 +1,28 @@
+;;; flymake-elixir-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-elixir" "flymake-elixir.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from flymake-elixir.el
+
+(autoload 'flymake-elixir-load "flymake-elixir" "\
+Configure flymake mode to check the current buffer's elixir syntax.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-elixir" '("flymake-elixir-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-elixir-autoloads.el ends here
---
+++ elpa/flymake-elixir-20130810.1417/flymake-elixir-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-elixir" "20130810.1417" "A flymake handler for elixir-mode .ex files." 'nil :commit "3810566cffe35d04cc3f01e27fe397d68d52f802" :authors '(("Sylvain Benner" . "syl20bnr@gmail.com")) :maintainer '("Sylvain Benner" . "syl20bnr@gmail.com"))
