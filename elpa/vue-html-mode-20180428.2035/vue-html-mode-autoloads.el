206905079ac56b71c93c08b44f9e843752216dcf
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:12 +0000

    [Adding vue-html-mode version: 20180428.2035]

---
+++ elpa/vue-html-mode-20180428.2035/vue-html-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; vue-html-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "vue-html-mode" "vue-html-mode.el" (0 0 0 0))
+;;; Generated autoloads from vue-html-mode.el
+
+(autoload 'vue-html-mode "vue-html-mode" "\
+Major mode for Vue.js templates.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "vue-html-mode" '("vue-html-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; vue-html-mode-autoloads.el ends here
---
+++ elpa/vue-html-mode-20180428.2035/vue-html-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vue-html-mode" "20180428.2035" "Major mode for editing Vue.js templates" 'nil :commit "1514939804bad558584feeb6298b38d22eadf64e" :keywords '("languages" "vue" "template") :authors '(("Adam Niederer" . "adam.niederer@gmail.com")) :maintainer '("Adam Niederer" . "adam.niederer@gmail.com") :url "http://github.com/AdamNiederer/vue-html-mode")
