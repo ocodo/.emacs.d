d93cc1a21481ebd0c1750eecc496bc9c8e1dfd87
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:55 +0000

    [Adding hemera-theme version: 20180916.924]

---
+++ elpa/hemera-theme-20180916.924/hemera-theme-autoloads.el
@@ -0,0 +1,22 @@
+;;; hemera-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "hemera-theme" "hemera-theme.el" (0 0 0 0))
+;;; Generated autoloads from hemera-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; hemera-theme-autoloads.el ends here
---
+++ elpa/hemera-theme-20180916.924/hemera-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "hemera-theme" "20180916.924" "Light theme" '((emacs "24")) :commit "b67c902b210b37b00cac68726822404543147ba8" :keywords '("themes" "light-theme") :authors '(("Guido Schmidt")) :maintainer '("Guido Schmidt" . "guido.schmidt.2912@gmail.com") :url "https://github.com/GuidoSchmidt/emacs-hemera-theme")
