f792bd866ee6b399b5822e6115c5899e8040afaf
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:27 +0000

    [Adding termbright-theme version: 20151031.235]

---
+++ elpa/termbright-theme-20151031.235/termbright-theme-autoloads.el
@@ -0,0 +1,30 @@
+;;; termbright-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "termbright-theme" "termbright-theme.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from termbright-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(autoload 'termbright-theme "termbright-theme" "\
+Load the termbright-theme.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "termbright-theme" '("termbright")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; termbright-theme-autoloads.el ends here
---
+++ elpa/termbright-theme-20151031.235/termbright-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "termbright-theme" "20151031.235" "a more usable theme for white-on-black terminals" '((emacs "24.1")) :commit "bec6ab14336c0611e85f45486276004f16d20607" :keywords '("themes") :authors '(("Brian Mastenbrook" . "brian@mastenbrook.net")) :maintainer '("Brian Mastenbrook" . "brian@mastenbrook.net") :url "https://github.com/bmastenbrook/termbright-theme-el")
