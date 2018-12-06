731becb1a682d370cbb94c3a467f0c3cd9c32a14
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:49 +0000

    [Adding helm-open-github version: 20170220.159]

---
+++ elpa/helm-open-github-20170220.159/helm-open-github-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-open-github" "20170220.159" "Utilities of Opening Github Page" '((emacs "24.4") (helm-core "1.7.7") (gh "0.8.2")) :commit "2f03d97552a1233db7694116d5f80ecde7612756" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-helm-open-github")
