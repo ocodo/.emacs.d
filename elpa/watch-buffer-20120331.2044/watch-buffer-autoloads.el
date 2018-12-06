a13a5030a3418ac6904ea61a0f201ae417d456b3
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:14 +0000

    [Adding watch-buffer version: 20120331.2044]

---
+++ elpa/watch-buffer-20120331.2044/watch-buffer-autoloads.el
@@ -0,0 +1,22 @@
+;;; watch-buffer-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "watch-buffer" "watch-buffer.el" (0 0 0 0))
+;;; Generated autoloads from watch-buffer.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "watch-buffer" '("simple-compile-" "run-compile" "apply-watch-buffer-command" "add-" "unwatch-buffer" "watch-buffer-" "build-interactive-functions" "general-watch" "this-file-argv")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; watch-buffer-autoloads.el ends here
---
+++ elpa/watch-buffer-20120331.2044/watch-buffer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "watch-buffer" "20120331.2044" "run a shell command when saving a buffer" 'nil :commit "761fd7252e6d7bf5148283c2a7ee935f087d9427" :keywords '("automation" "convenience") :authors '(("Michael Steger" . "mjsteger1@gmail.com")) :maintainer '("Michael Steger" . "mjsteger1@gmail.com") :url "https://github.com/mjsteger/watch-buffer")
