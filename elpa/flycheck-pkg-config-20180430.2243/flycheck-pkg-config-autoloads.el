4e34f8006ad8de7e149232fa0b062782584b2f53
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:33 +0000

    [Adding flycheck-pkg-config version: 20180430.2243]

---
+++ elpa/flycheck-pkg-config-20180430.2243/flycheck-pkg-config-autoloads.el
@@ -0,0 +1,29 @@
+;;; flycheck-pkg-config-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flycheck-pkg-config" "flycheck-pkg-config.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from flycheck-pkg-config.el
+
+(autoload 'flycheck-pkg-config "flycheck-pkg-config" "\
+Configure flycheck to use additional includes
+when checking the current buffer.
+
+\(fn LIB-NAME)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flycheck-pkg-config" '("flycheck-pkg-config--")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flycheck-pkg-config-autoloads.el ends here
---
+++ elpa/flycheck-pkg-config-20180430.2243/flycheck-pkg-config-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-pkg-config" "20180430.2243" "configure flycheck using pkg-config" '((dash "2.8.0") (s "1.9.0") (flycheck "29")) :commit "e72e4c1b8153611ed82695673af84096f4d52795" :keywords '("flycheck") :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk"))
