e37ea46774fe59c44da4d1a857b6dcdbc794230e
Jasonm23 <jasonm23@gmail.com>
2018-12-06 12:58:49 +0000

    [Adding boron-theme version: 20170808.1308]

---
+++ elpa/boron-theme-20170808.1308/boron-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; boron-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "boron-theme" "boron-theme.el" (0 0 0 0))
+;;; Generated autoloads from boron-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "boron-theme" '("boron")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; boron-theme-autoloads.el ends here
