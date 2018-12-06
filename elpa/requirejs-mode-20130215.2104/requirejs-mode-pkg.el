ed9e97b2a52976f1be07b214314c950db366f71a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:09:21 +0000

    [Adding requirejs-mode version: 20130215.2104]

---
+++ elpa/requirejs-mode-20130215.2104/requirejs-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "requirejs-mode" "20130215.2104" "Improved AMD module management" 'nil :commit "bbb0c09f8eb2d6a33c17319be8137f68bb16bc92" :keywords '("javascript" "amd" "requirejs") :authors '(("Marc-Olivier Ricard" . "marco.ricard@gmail.com")) :maintainer '("Marc-Olivier Ricard" . "marco.ricard@gmail.com"))
