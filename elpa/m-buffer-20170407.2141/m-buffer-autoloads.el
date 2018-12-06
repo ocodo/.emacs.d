a85bbcc7d4dc905b07038fc95afa45d7bde92898
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:02:56 +0000

    [Adding m-buffer version: 20170407.2141]

---
+++ elpa/m-buffer-20170407.2141/m-buffer-autoloads.el
@@ -0,0 +1,41 @@
+;;; m-buffer-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "m-buffer" "m-buffer.el" (0 0 0 0))
+;;; Generated autoloads from m-buffer.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "m-buffer" '("m-buffer-")))
+
+;;;***
+
+;;;### (autoloads nil "m-buffer-at" "m-buffer-at.el" (0 0 0 0))
+;;; Generated autoloads from m-buffer-at.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "m-buffer-at" '("m-buffer-at-")))
+
+;;;***
+
+;;;### (autoloads nil "m-buffer-macro" "m-buffer-macro.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from m-buffer-macro.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "m-buffer-macro" '("m-buffer-with-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("m-buffer-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; m-buffer-autoloads.el ends here
---
+++ elpa/m-buffer-20170407.2141/m-buffer-pkg.el
@@ -0,0 +1,9 @@
+(define-package "m-buffer" "20170407.2141" "List-Oriented, Functional Buffer Manipulation"
+  '((seq "2.14"))
+  :authors
+  '(("Phillip Lord" . "phillip.lord@russet.org.uk"))
+  :maintainer
+  '("Phillip Lord" . "phillip.lord@russet.rg.uk"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
