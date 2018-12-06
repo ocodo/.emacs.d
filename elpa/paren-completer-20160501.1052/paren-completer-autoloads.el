55bf0bc82bbfbcf2ea249849a56a3e47976b5af3
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:03:32 +0000

    [Adding paren-completer version: 20160501.1052]

---
+++ elpa/paren-completer-20160501.1052/paren-completer-autoloads.el
@@ -0,0 +1,43 @@
+;;; paren-completer-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "paren-completer" "paren-completer.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from paren-completer.el
+
+(autoload 'paren-completer-add-single-delimiter "paren-completer" "\
+Process buffer, then add a delimiters.
+
+\(fn)" t nil)
+
+(autoload 'paren-completer-add-all-delimiters "paren-completer" "\
+Process buffer, then add all delimiters.
+
+\(fn)" t nil)
+
+(autoload 'paren-completer-add-single-delimiter-with-newline "paren-completer" "\
+Process buffer, then add a delimiters.
+
+\(fn)" t nil)
+
+(autoload 'paren-completer-add-all-delimiters-with-newline "paren-completer" "\
+Process buffer, then add all delimiters.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "paren-completer" '("paren-completer-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; paren-completer-autoloads.el ends here
---
+++ elpa/paren-completer-20160501.1052/paren-completer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "paren-completer" "20160501.1052" "Automatically, language agnostically, fill in delimiters." '((emacs "24.3")) :commit "74183a8e13fa1266271bdcbcb4bfb29a4f915f0a" :keywords '("convenience") :authors '(("Matthew Bregg")) :maintainer '("Matthew Bregg") :url "https://github.com/MatthewBregg/paren-completer")
