4c570762d0200c95abd130aead3849512a1179e8
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:54 +0000

    [Adding helm-themes version: 20160918.545]

---
+++ elpa/helm-themes-20160918.545/helm-themes-autoloads.el
@@ -0,0 +1,27 @@
+;;; helm-themes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-themes" "helm-themes.el" (0 0 0 0))
+;;; Generated autoloads from helm-themes.el
+
+(autoload 'helm-themes "helm-themes" "\
+Theme selection with helm interface
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-themes" '("helm-themes-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-themes-autoloads.el ends here
