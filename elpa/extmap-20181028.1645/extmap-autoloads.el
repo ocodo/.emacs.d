a1e0b666674a188b90f4f4411df42f6627547504
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:18 +0000

    [Adding extmap version: 20181028.1645]

---
+++ elpa/extmap-20181028.1645/extmap-autoloads.el
@@ -0,0 +1,22 @@
+;;; extmap-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "extmap" "extmap.el" (0 0 0 0))
+;;; Generated autoloads from extmap.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "extmap" '("extmap-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; extmap-autoloads.el ends here
