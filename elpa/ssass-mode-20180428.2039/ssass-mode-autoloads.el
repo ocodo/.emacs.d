d6a0ab51b3e945d2753be341e0ed9169f35aee6a
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:13 +0000

    [Adding ssass-mode version: 20180428.2039]

---
+++ elpa/ssass-mode-20180428.2039/ssass-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; ssass-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ssass-mode" "ssass-mode.el" (0 0 0 0))
+;;; Generated autoloads from ssass-mode.el
+
+(autoload 'ssass-mode "ssass-mode" "\
+Major mode for Sass
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ssass-mode" '("ssass-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ssass-mode-autoloads.el ends here
---
+++ elpa/ssass-mode-20180428.2039/ssass-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ssass-mode" "20180428.2039" "Edit Sass without a Turing Machine" '((emacs "24.3")) :commit "da82ebb2aa7e4999c23547270d2b0b2cd9311a47" :keywords '("languages" "sass") :authors '(("Adam Niederer" . "adam.niederer@gmail.com")) :maintainer '("Adam Niederer" . "adam.niederer@gmail.com") :url "http://github.com/AdamNiederer/ssass-mode")
