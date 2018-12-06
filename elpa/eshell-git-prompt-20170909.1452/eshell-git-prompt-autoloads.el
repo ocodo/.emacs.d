37d6dae096aac12fe7947d10a0df44f9b8bfbd54
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:01 +0000

    [Adding eshell-git-prompt version: 20170909.1452]

---
+++ elpa/eshell-git-prompt-20170909.1452/eshell-git-prompt-autoloads.el
@@ -0,0 +1,33 @@
+;;; eshell-git-prompt-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "eshell-git-prompt" "eshell-git-prompt.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from eshell-git-prompt.el
+
+(autoload 'eshell-git-prompt-use-theme "eshell-git-prompt" "\
+Pick up a Eshell prompt theme from `eshell-git-prompt-themes' to use.
+
+\(fn THEME)" t nil)
+
+(autoload 'eshell/use-theme "eshell-git-prompt" "\
+List all available themes and pick one from Eshell.
+
+\(fn &optional THEME)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "eshell-git-prompt" '("eshell-git-prompt-" "with-face")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; eshell-git-prompt-autoloads.el ends here
