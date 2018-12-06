2a6e601233a7b3d8b3f1dbd0a0b6b8ed3ba45690
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:00:16 +0000

    [Adding evil-visual-mark-mode version: 20150202.1800]

---
+++ elpa/evil-visual-mark-mode-20150202.1800/evil-visual-mark-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-visual-mark-mode" "20150202.1800" "Display evil marks on buffer" '((evil "1.0.9") (dash "2.10")) :commit "094ee37599492885ff3144918fcdd9b74dadaaa0" :keywords '("evil") :authors '(("Roman Gonzalez" . "romanandreg@gmail.com")) :maintainer '("Roman Gonzalez" . "romanandreg@gmail.com"))
