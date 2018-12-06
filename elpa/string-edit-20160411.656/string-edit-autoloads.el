4cbdeb9c3e3634530b1f202114a24ae47defd947
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:11:15 +0000

    [Adding string-edit version: 20160411.656]

---
+++ elpa/string-edit-20160411.656/string-edit-autoloads.el
@@ -0,0 +1,28 @@
+;;; string-edit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "string-edit" "string-edit.el" (0 0 0 0))
+;;; Generated autoloads from string-edit.el
+
+(autoload 'string-edit-at-point "string-edit" "\
+Pop up a buffer to edit the string at point.
+This saves you from needing to manually escape characters.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "string-edit" '("string-edit-" "se/")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; string-edit-autoloads.el ends here
---
+++ elpa/string-edit-20160411.656/string-edit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "string-edit" "20160411.656" "Avoid escape nightmares by editing string in separate buffer" '((dash "1.2.0")) :commit "c44b65b4c5e9f52be9c14d88ca2f402a18d9e1dd" :authors '(("Magnar Sveen" . "magnars@gmail.com")) :maintainer '("Magnar Sveen" . "magnars@gmail.com"))
