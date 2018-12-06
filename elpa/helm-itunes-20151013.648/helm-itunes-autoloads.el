f29f71a7218dd8332b5d45de6f33b0cd1a5da62f
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:01:45 +0000

    [Adding helm-itunes version: 20151013.648]

---
+++ elpa/helm-itunes-20151013.648/helm-itunes-autoloads.el
@@ -0,0 +1,27 @@
+;;; helm-itunes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-itunes" "helm-itunes.el" (0 0 0 0))
+;;; Generated autoloads from helm-itunes.el
+
+(autoload 'helm-itunes "helm-itunes" "\
+Bring up a Spotify search interface in helm.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-itunes" '("helm-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-itunes-autoloads.el ends here
