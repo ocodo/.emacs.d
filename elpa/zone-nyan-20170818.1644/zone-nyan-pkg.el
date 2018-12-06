fb63f34dda06a752962188a1f92ce3560095314d
Jasonm23 <jasonm23@gmail.com>
2018-12-06 13:12:35 +0000

    [Adding zone-nyan version: 20170818.1644]

---
+++ elpa/zone-nyan-20170818.1644/zone-nyan-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zone-nyan" "20170818.1644" "Zone out with nyan cat" '((esxml "0.3.1")) :commit "4b1f8d95f130946718d52806489ffe2762aebfdc" :keywords '("zone") :authors '(("Vasilij Schneidermann" . "v.schneidermann@gmail.com")) :maintainer '("Vasilij Schneidermann" . "v.schneidermann@gmail.com") :url "https://github.com/wasamasa/zone-nyan")
