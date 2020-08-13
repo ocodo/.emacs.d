bd07fb690d219e3ca17aa0b9f7eb196d25eae808
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:47 +0800

    [Adding utop version: 20190715.1836]

---
+++ elpa/utop-20190715.1836/utop-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/utop-20190715.1836/utop.el  -*- no-byte-compile: t -*-
+(define-package "utop" "20190715.1836" "Universal toplevel for OCaml" '((emacs "24")) :commit "7bc5117d3449fc19f5c706a6decfdb2a30984507" :keywords '("ocaml" "languages") :authors '(("Jeremie Dimino" . "jeremie@dimino.org")) :maintainer '("Jeremie Dimino" . "jeremie@dimino.org") :url "https://github.com/diml/utop")
