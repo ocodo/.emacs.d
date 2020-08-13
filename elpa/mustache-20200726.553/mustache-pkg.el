63c34cc2bc0fdcb2ebcf4148f85daa9aba844a09
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:38:58 +0800

    [Adding mustache version: 20200726.553]

---
+++ elpa/mustache-20200726.553/mustache-pkg.el
@@ -0,0 +1,8 @@
+(define-package "mustache" "20200726.553" "a mustache templating library in emacs lisp"
+  '((ht "0.9")
+    (s "1.3.0")
+    (dash "1.2.0"))
+  :commit "4649a47340b63214c7b8f1dcc178806d96288839")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
