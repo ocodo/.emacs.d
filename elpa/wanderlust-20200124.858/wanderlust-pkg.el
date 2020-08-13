1947a559ffc6bd0b5b8f13c64860fb3b81c43e65
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:50 +0800

    [Adding wanderlust version: 20200124.858]

---
+++ elpa/wanderlust-20200124.858/wanderlust-pkg.el
@@ -0,0 +1,6 @@
+(define-package "wanderlust" "20200124.858" "Yet Another Message Interface on Emacsen"
+  '((semi "1.14.7"))
+  :commit "7af0d582cd48a37469e0606ea35887740d78c8b5")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
