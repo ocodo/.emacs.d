621c6cd70ad08127c36439ac7aa2f3036bec6821
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:40:00 +0800

    [Adding zone-nyan version: 20200506.1207]

---
+++ elpa/zone-nyan-20200506.1207/zone-nyan-autoloads.el
@@ -0,0 +1,28 @@
+;;; zone-nyan-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "zone-nyan" "zone-nyan.el" (0 0 0 0))
+;;; Generated autoloads from zone-nyan.el
+
+(autoload 'zone-nyan "zone-nyan" "\
+Zone out with nyan cat!" nil nil)
+
+(autoload 'zone-nyan-preview "zone-nyan" "\
+Preview the `zone-nyan' zone program." t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "zone-nyan" '("zone-nyan-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zone-nyan-autoloads.el ends here
---
+++ elpa/zone-nyan-20200506.1207/zone-nyan-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/zone-nyan-20200506.1207/zone-nyan.el  -*- no-byte-compile: t -*-
+(define-package "zone-nyan" "20200506.1207" "Zone out with nyan cat" '((esxml "0.3.1")) :commit "48625814f42c39521405d08009e1d44108d880ac" :keywords '("zone") :authors '(("Vasilij Schneidermann" . "mail@vasilij.de")) :maintainer '("Vasilij Schneidermann" . "mail@vasilij.de") :url "https://depp.brause.cc/zone-nyan")
