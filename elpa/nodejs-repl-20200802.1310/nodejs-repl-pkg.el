23c5d5ef17e92128c3b48def6642fe4ecf76e93f
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:01 +0800

    [Adding nodejs-repl version: 20200802.1310]

---
+++ elpa/nodejs-repl-20200802.1310/nodejs-repl-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/nodejs-repl-20200802.1310/nodejs-repl.el  -*- no-byte-compile: t -*-
+(define-package "nodejs-repl" "20200802.1310" "Run Node.js REPL" 'nil :commit "3b841055cad00f442e4a9159b1056f59411b6646" :authors '(("Takeshi Arabiki")) :maintainer '("Takeshi Arabiki"))
