dff875c43f8c5659a72ccd25e8c7dbb78aa904fe
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:34 +0800

    [Adding spacemacs-theme version: 20200808.1859]

---
+++ elpa/spacemacs-theme-20200808.1859/spacemacs-theme-pkg.el
@@ -0,0 +1,6 @@
+(define-package "spacemacs-theme" "20200808.1859" "Color theme with a dark and light versions" 'nil :commit "462ef2ac16ab8c51de21d5f3d2b1b6122b314391" :keywords
+  '("color" "theme")
+  :url "https://github.com/nashamri/spacemacs-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
