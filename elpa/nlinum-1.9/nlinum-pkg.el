a8e01964b6acf752884d1cfa33d43518bcde7a57
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:00 +0800

    [Adding nlinum version: 1.9]

---
+++ elpa/nlinum-1.9/nlinum-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/nlinum-1.9/nlinum.el  -*- no-byte-compile: t -*-
+(define-package "nlinum" "1.9" "Show line numbers in the margin" 'nil :url "http://elpa.gnu.org/packages/nlinum.html" :keywords '("convenience") :authors '(("Stefan Monnier" . "monnier@iro.umontreal.ca")) :maintainer '("Stefan Monnier" . "monnier@iro.umontreal.ca"))
