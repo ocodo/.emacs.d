89077ca852b993163a61c0b0182eb7c95672d4df
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:08 +0800

    [Adding org-vcard version: 20200720.638]

---
+++ elpa/org-vcard-20200720.638/org-vcard-pkg.el
@@ -0,0 +1,11 @@
+(define-package "org-vcard" "20200720.638" "org-mode support for vCard export and import." 'nil :commit "1ae97371b207dabfecaf6b4f7118abafe6cc5e2b" :keywords
+  '("outlines" "org" "vcard")
+  :authors
+  '(("Alexis" . "flexibeast@gmail.com")
+    ("Will Dey" . "will123dey@gmail.com"))
+  :maintainer
+  '("Alexis" . "flexibeast@gmail.com")
+  :url "https://github.com/flexibeast/org-vcard")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
