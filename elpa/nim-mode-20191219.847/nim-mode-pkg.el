942d87a3a32b1c273c6abb432a61ca898b4cfceb
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:00 +0800

    [Adding nim-mode version: 20191219.847]

---
+++ elpa/nim-mode-20191219.847/nim-mode-pkg.el
@@ -0,0 +1,15 @@
+(define-package "nim-mode" "20191219.847" "A major mode for the Nim programming language"
+  '((emacs "24.4")
+    (epc "0.1.1")
+    (let-alist "1.0.1")
+    (commenter "0.5.1")
+    (flycheck-nimsuggest "0.8.1"))
+  :commit "16a245e4974d21a6e9e7163e7fbfb50a0bd89f99" :keywords
+  '("nim" "languages")
+  :authors
+  '(("Simon Hafner"))
+  :maintainer
+  '("Simon Hafner" . "hafnersimon@gmail.com"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
