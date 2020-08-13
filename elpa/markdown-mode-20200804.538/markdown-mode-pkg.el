dca55c9fa9d4f09018e06c134000327258fc96fb
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:38:51 +0800

    [Adding markdown-mode version: 20200804.538]

---
+++ elpa/markdown-mode-20200804.538/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/markdown-mode-20200804.538/markdown-mode.el  -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20200804.538" "Major mode for Markdown-formatted text" '((emacs "25.1")) :commit "0b5386b67118767b01e69019c16ae7f1ef30bde7" :keywords '("markdown" "github flavored markdown" "itex") :authors '(("Jason R. Blevins" . "jblevins@xbeta.org")) :maintainer '("Jason R. Blevins" . "jblevins@xbeta.org") :url "https://jblevins.org/projects/markdown-mode/")
