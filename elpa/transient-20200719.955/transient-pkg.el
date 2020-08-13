4f266b97a1ac3bb8fef9f91e266aaff774176c72
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:43 +0800

    [Adding transient version: 20200719.955]

---
+++ elpa/transient-20200719.955/transient-pkg.el
@@ -0,0 +1,12 @@
+(define-package "transient" "20200719.955" "Transient commands"
+  '((emacs "25.1"))
+  :commit "4d44d08e90355a8ef36aaad1f9f79c95de1ce0e9" :keywords
+  '("bindings")
+  :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
