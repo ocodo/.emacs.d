a5ac40e434fa7c7f40a446a00e2580930358985e
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:58 +0800

    [Adding yasnippet version: 20200604.246]

---
+++ elpa/yasnippet-20200604.246/yasnippet-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/yasnippet-20200604.246/yasnippet.el  -*- no-byte-compile: t -*-
+(define-package "yasnippet" "20200604.246" "Yet another snippet extension for Emacs" '((cl-lib "0.5")) :commit "5cbdbf0d2015540c59ed8ee0fcf4788effdf75b6" :keywords '("convenience" "emulation") :maintainer '("Noam Postavsky" . "npostavs@gmail.com") :url "http://github.com/joaotavora/yasnippet")
