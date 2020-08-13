8361e059a4da3ead083d36d7e5db830ec7712e15
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:45 +0800

    [Adding typescript-mode version: 20200430.1232]

---
+++ elpa/typescript-mode-20200430.1232/typescript-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "typescript-mode" "20200430.1232" "Major mode for editing typescript"
+  '((emacs "24.3"))
+  :commit "f36fddeb549d088916ad424a8fcfb1e89a421593" :keywords
+  '("typescript" "languages")
+  :url "http://github.com/ananthakumaran/typescript.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
