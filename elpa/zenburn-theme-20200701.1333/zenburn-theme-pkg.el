6bf827c389d03906ed3f914b9a40165bd082fcf1
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:40:00 +0800

    [Adding zenburn-theme version: 20200701.1333]

---
+++ elpa/zenburn-theme-20200701.1333/zenburn-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/zenburn-theme-20200701.1333/zenburn-theme.el  -*- no-byte-compile: t -*-
+(define-package "zenburn-theme" "20200701.1333" "A low contrast color theme for Emacs." 'nil :commit "c09dbd9a36bbc0062b90be182f2b4cac64128cee" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "http://github.com/bbatsov/zenburn-emacs")
