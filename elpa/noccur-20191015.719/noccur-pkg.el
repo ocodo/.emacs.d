1b6d6cb67c0472f18b2df5e5088545256322787c
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:01 +0800

    [Adding noccur version: 20191015.719]

---
+++ elpa/noccur-20191015.719/noccur-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/noccur-20191015.719/noccur.el  -*- no-byte-compile: t -*-
+(define-package "noccur" "20191015.719" "Run multi-occur on project/dired files" 'nil :commit "fa91647a305e89561d3dbe53da002fff49abe0bb" :keywords '("convenience") :authors '(("Nicolas Petton" . "petton.nicolas@gmail.com")) :maintainer '("Nicolas Petton" . "petton.nicolas@gmail.com"))
