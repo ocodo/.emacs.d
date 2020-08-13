cddf3fe1bb7156dbb89e59e110cd76059b5d8503
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:38:52 +0800

    [Adding material-theme version: 20200620.943]

---
+++ elpa/material-theme-20200620.943/material-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "material-theme" "20200620.943" "A Theme based on the colors of the Google Material Design"
+  '((emacs "24.1"))
+  :commit "2710e61ecee1bbec943538305c26af4fe4fca68a" :keywords
+  '("themes")
+  :authors
+  '(("Christoph Paulik" . "cpaulik@gmail.com"))
+  :maintainer
+  '("Christoph Paulik" . "cpaulik@gmail.com")
+  :url "http://github.com/cpaulik/emacs-material-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
