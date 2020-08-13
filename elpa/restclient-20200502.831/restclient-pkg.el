19e2421f651d467d83cb5867be3520fc12752a93
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:23 +0800

    [Adding restclient version: 20200502.831]

---
+++ elpa/restclient-20200502.831/restclient-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/restclient-20200502.831/restclient.el  -*- no-byte-compile: t -*-
+(define-package "restclient" "20200502.831" "An interactive HTTP client for Emacs" 'nil :commit "ac8aad6c6b9e9d918062fa3c89c22c2f4ec48bc3" :keywords '("http") :authors '(("Pavel Kurnosov" . "pashky@gmail.com")) :maintainer '("Pavel Kurnosov" . "pashky@gmail.com"))
