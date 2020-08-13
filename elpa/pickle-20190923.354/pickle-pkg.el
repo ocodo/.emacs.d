bd840bc28a2c4d98e682f5cd98c3958cdb4b9567
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:13 +0800

    [Adding pickle version: 20190923.354]

---
+++ elpa/pickle-20190923.354/pickle-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/pickle-20190923.354/pickle.el  -*- no-byte-compile: t -*-
+(define-package "pickle" "20190923.354" "Major mode for editing cucumber gherkin files." '((emacs "25.1") (cl-lib "0.6.1")) :commit "3a0a717f2a24827667f34bc53830a3b81cd57460" :keywords '("ahungry" "languages" "cucumber" "gherkin") :authors '(("Matthew Carter" . "m@ahungry.com")) :maintainer '("Matthew Carter" . "m@ahungry.com") :url "https://github.com/ahungry/pickle-mode")
