a8571ed8db6ab5962e76a0b90894234a0bf12f9b
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:22 +0800

    [Adding request version: 20200517.1305]

---
+++ elpa/request-20200517.1305/request-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/request-20200517.1305/request.el  -*- no-byte-compile: t -*-
+(define-package "request" "20200517.1305" "Compatible layer for URL request in Emacs" '((emacs "24.4")) :commit "d02d1347ffdf138cffd380cbeac62ac8732036ef" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
