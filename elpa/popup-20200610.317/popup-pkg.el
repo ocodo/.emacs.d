062f8f6034da9197d9b55dc48a2db05f8f7c5d15
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:15 +0800

    [Adding popup version: 20200610.317]

---
+++ elpa/popup-20200610.317/popup-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/popup-20200610.317/popup.el  -*- no-byte-compile: t -*-
+(define-package "popup" "20200610.317" "Visual Popup User Interface" '((cl-lib "0.5")) :commit "9d104d4bbbcb37bbc9d9ce762e74d41174683f86" :keywords '("lisp") :authors '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com")) :maintainer '("Tomohiro Matsuyama" . "m2ym.pub@gmail.com"))
