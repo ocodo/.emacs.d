3bbe67b10638f5b19faa8f0dd7646c34d4f3d272
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:32 +0800

    [Adding smart-compile version: 20200727.1249]

---
+++ elpa/smart-compile-20200727.1249/smart-compile-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/smart-compile-20200727.1249/smart-compile.el  -*- no-byte-compile: t -*-
+(define-package "smart-compile" "20200727.1249" "an interface to `compile'" 'nil :commit "1f14c86af7ca5813ddc0b6dfcf2edd4a81371644" :keywords '("tools" "unix") :authors '(("Seiji Zenitani" . "zenitani@gmail.com")) :maintainer '("Seiji Zenitani" . "zenitani@gmail.com"))
