3003aa5a5f603bb084d85f45d142c1ea3a321d9b
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:55 +0800

    [Adding window-purpose version: 20190628.1827]

---
+++ elpa/window-purpose-20190628.1827/window-purpose-pkg.el
@@ -0,0 +1,14 @@
+(define-package "window-purpose" "20190628.1827" "Purpose-based window management for Emacs"
+  '((emacs "24.4")
+    (let-alist "1.0.3")
+    (imenu-list "0.1"))
+  :commit "f6421966761ad911fe8861aba2b110c5dd60d1ea" :keywords
+  '("frames")
+  :authors
+  '(("Bar Magal"))
+  :maintainer
+  '("Bar Magal")
+  :url "https://github.com/bmag/emacs-purpose")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
