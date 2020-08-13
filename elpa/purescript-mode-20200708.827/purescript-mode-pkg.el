0755ce49834a521e1408da18502cc55fc34a040a
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:20 +0800

    [Adding purescript-mode version: 20200708.827]

---
+++ elpa/purescript-mode-20200708.827/purescript-mode-pkg.el
@@ -0,0 +1,7 @@
+(define-package "purescript-mode" "20200708.827" "A PureScript editing mode"
+  '((emacs "24")
+    (cl-lib "0.6"))
+  :commit "154ad16b61fb9dec83a6c863ffaf92638278f00f")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
