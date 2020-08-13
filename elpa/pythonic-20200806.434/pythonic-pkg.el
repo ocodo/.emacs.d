33a72f3ae216239d5197e4dc0e8f2e91940276c3
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:20 +0800

    [Adding pythonic version: 20200806.434]

---
+++ elpa/pythonic-20200806.434/pythonic-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/pythonic-20200806.434/pythonic.el  -*- no-byte-compile: t -*-
+(define-package "pythonic" "20200806.434" "Utility functions for writing pythonic emacs package." '((emacs "25.1") (s "1.9") (f "0.17.2")) :commit "51233ec7ec9fbafd13e2b0479c7b2ee0930ccca5" :authors '(("Artem Malyshev" . "proofit404@gmail.com")) :maintainer '("Artem Malyshev" . "proofit404@gmail.com") :url "https://github.com/proofit404/pythonic")
