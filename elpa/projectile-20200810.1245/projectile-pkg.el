226268e62dfd24027c46e09335b33fae7a4309fe
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:18 +0800

    [Adding projectile version: 20200810.1245]

---
+++ elpa/projectile-20200810.1245/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/projectile-20200810.1245/projectile.el  -*- no-byte-compile: t -*-
+(define-package "projectile" "20200810.1245" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :commit "2eaf2c0e2d42457b55068d92659ef14195e12918" :keywords '("project" "convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/bbatsov/projectile")
