21c872efe9b9db006f07bc0d63b4e21ee68bdc08
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:52 +0800

    [Adding wgrep version: 20200217.1030]

---
+++ elpa/wgrep-20200217.1030/wgrep-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/wgrep-20200217.1030/wgrep.el  -*- no-byte-compile: t -*-
+(define-package "wgrep" "20200217.1030" "Writable grep buffer and apply the changes to files" 'nil :commit "f0ef9bfa44db503cdb2f83fcfbd2fa4e2382ef1f" :keywords '("grep" "edit" "extensions") :authors '(("Masahiro Hayashi" . "mhayashi1120@gmail.com")) :maintainer '("Masahiro Hayashi" . "mhayashi1120@gmail.com") :url "http://github.com/mhayashi1120/Emacs-wgrep/raw/master/wgrep.el")
