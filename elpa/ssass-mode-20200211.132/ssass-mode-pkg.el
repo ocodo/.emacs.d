32a9e80e4c6fe82ad5c7c0cc758b3441f106b3cf
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:35 +0800

    [Adding ssass-mode version: 20200211.132]

---
+++ elpa/ssass-mode-20200211.132/ssass-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/ssass-mode-20200211.132/ssass-mode.el  -*- no-byte-compile: t -*-
+(define-package "ssass-mode" "20200211.132" "Edit Sass without a Turing Machine" '((emacs "24.3")) :commit "96f557887ad97a0066a60c54f92b7234b8407016" :keywords '("languages" "sass") :authors '(("Adam Niederer" . "adam.niederer@gmail.com")) :maintainer '("Adam Niederer" . "adam.niederer@gmail.com") :url "http://github.com/AdamNiederer/ssass-mode")
