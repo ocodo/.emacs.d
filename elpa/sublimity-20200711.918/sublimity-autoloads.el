30e8528bf218a8ae7700ca47a850fe97d1ae42ba
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:37 +0800

    [Adding sublimity version: 20200711.918]

---
+++ elpa/sublimity-20200711.918/sublimity-autoloads.el
@@ -0,0 +1,69 @@
+;;; sublimity-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "sublimity" "sublimity.el" (0 0 0 0))
+;;; Generated autoloads from sublimity.el
+
+(defvar sublimity-mode nil "\
+Non-nil if Sublimity mode is enabled.
+See the `sublimity-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `sublimity-mode'.")
+
+(custom-autoload 'sublimity-mode "sublimity" nil)
+
+(autoload 'sublimity-mode "sublimity" "\
+smooth-scrolling and minimap, like sublime editor
+
+If called interactively, enable Sublimity mode if ARG is
+positive, and disable it if ARG is zero or negative.  If called
+from Lisp, also enable the mode if ARG is omitted or nil, and
+toggle it if ARG is `toggle'; disable the mode otherwise.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sublimity" '("sublimity-")))
+
+;;;***
+
+;;;### (autoloads nil "sublimity-attractive" "sublimity-attractive.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from sublimity-attractive.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sublimity-attractive" '("sublimity-attractive-")))
+
+;;;***
+
+;;;### (autoloads nil "sublimity-map" "sublimity-map.el" (0 0 0 0))
+;;; Generated autoloads from sublimity-map.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sublimity-map" '("sublimity-map-")))
+
+;;;***
+
+;;;### (autoloads nil "sublimity-scroll" "sublimity-scroll.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from sublimity-scroll.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sublimity-scroll" '("sublimity-scroll-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("sublimity-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sublimity-autoloads.el ends here
