aa873f465e1a1a22b51983ab813a430b4f8977de
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:40:01 +0800

    [Adding zoutline version: 20190520.1819]

---
+++ elpa/zoutline-20190520.1819/zoutline-autoloads.el
@@ -0,0 +1,22 @@
+;;; zoutline-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "zoutline" "zoutline.el" (0 0 0 0))
+;;; Generated autoloads from zoutline.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "zoutline" '("zo-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zoutline-autoloads.el ends here
---
+++ elpa/zoutline-20190520.1819/zoutline-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/zoutline-20190520.1819/zoutline.el  -*- no-byte-compile: t -*-
+(define-package "zoutline" "20190520.1819" "Simple outline library." 'nil :commit "63756846f8540b6faf89d885438186e4fe1c7d8a" :keywords '("outline") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/zoutline")
