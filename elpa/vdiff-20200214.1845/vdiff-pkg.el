c5a02632ec1d2e9727091f82d8a11ec731988946
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:47 +0800

    [Adding vdiff version: 20200214.1845]

---
+++ elpa/vdiff-20200214.1845/vdiff-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/vdiff-20200214.1845/vdiff.el  -*- no-byte-compile: t -*-
+(define-package "vdiff" "20200214.1845" "A diff tool similar to  vimdiff" '((emacs "24.4") (hydra "0.13.0")) :commit "c0541ae9c2cb878be9ab3935da058a72155a14fc" :keywords '("diff") :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-vdiff")
