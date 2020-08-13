25a37e9bf51ac8d9bc19d291f8bf58659ea10b12
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:10 +0800

    [Adding package-build version: 20200601.1939]

---
+++ elpa/package-build-20200601.1939/package-build-pkg.el
@@ -0,0 +1,13 @@
+(define-package "package-build" "20200601.1939" "Tools for assembling a package archive"
+  '((cl-lib "0.5")
+    (emacs "24.1"))
+  :commit "4cb0f98a21729f9ef0189f095384555c9d2b6fe4" :keywords
+  '("tools")
+  :authors
+  '(("Donald Ephraim Curtis" . "dcurtis@milkbox.net"))
+  :maintainer
+  '("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+  :url "https://github.com/melpa/package-build")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
