286551e38ff98e5698d8e9cdcff3230f61a8f4fb
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:48 +0800

    [Adding vdirel version: 20190430.624]

---
+++ elpa/vdirel-20190430.624/vdirel-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/vdirel-20190430.624/vdirel.el  -*- no-byte-compile: t -*-
+(define-package "vdirel" "20190430.624" "Manipulate vdir (i.e., vCard) repositories" '((emacs "24.4") (org-vcard "0.1.0") (helm "1.7.0") (seq "1.11")) :commit "a9ef32a70a1f14416e3dc5fee478ce138cc011d3" :authors '(("Damien Cassou" . "damien@cassou.me")) :maintainer '("Damien Cassou" . "damien@cassou.me") :url "https://github.com/DamienCassou/vdirel")
