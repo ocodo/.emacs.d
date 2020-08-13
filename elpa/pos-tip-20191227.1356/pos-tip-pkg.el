c7de580f63d9fa46c276d46ddff900979b219363
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:16 +0800

    [Adding pos-tip version: 20191227.1356]

---
+++ elpa/pos-tip-20191227.1356/pos-tip-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/pos-tip-20191227.1356/pos-tip.el  -*- no-byte-compile: t -*-
+(define-package "pos-tip" "20191227.1356" "Show tooltip at point" 'nil :commit "179cc126b363f72ca12fab1e0dc462ce0ee79742" :keywords '("tooltip") :authors '(("S. Irie")) :maintainer '("S. Irie"))
