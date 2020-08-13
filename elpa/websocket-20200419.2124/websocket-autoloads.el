c99f3b84f2fb7c638a1c64d30c60ba9637ed1391
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:51 +0800

    [Adding websocket version: 20200419.2124]

---
+++ elpa/websocket-20200419.2124/websocket-autoloads.el
@@ -0,0 +1,22 @@
+;;; websocket-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "websocket" "websocket.el" (0 0 0 0))
+;;; Generated autoloads from websocket.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "websocket" '("websocket-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; websocket-autoloads.el ends here
---
+++ elpa/websocket-20200419.2124/websocket-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/websocket-20200419.2124/websocket.el  -*- no-byte-compile: t -*-
+(define-package "websocket" "20200419.2124" "Emacs WebSocket client and server" '((cl-lib "0.5")) :commit "5aaf9d12068f98fb4efa772a3e5f4bb350b79a99" :keywords '("communication" "websocket" "server") :authors '(("Andrew Hyatt" . "ahyatt@gmail.com")) :maintainer '("Andrew Hyatt" . "ahyatt@gmail.com") :url "https://github.com/ahyatt/emacs-websocket")
