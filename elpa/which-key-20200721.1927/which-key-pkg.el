05ce243916db0b81b7c74d2dcc9fd09b5c2c04ee
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:54 +0800

    [Adding which-key version: 20200721.1927]

---
+++ elpa/which-key-20200721.1927/which-key-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/which-key-20200721.1927/which-key.el  -*- no-byte-compile: t -*-
+(define-package "which-key" "20200721.1927" "Display available keybindings in popup" '((emacs "24.4")) :commit "3642c11d5ef9be3c6fb9edb8fd5ec3c370abd889" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
