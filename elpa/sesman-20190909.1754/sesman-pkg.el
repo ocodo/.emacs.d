229581980b2cb888fa99571322201c19c7800bbb
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:29 +0800

    [Adding sesman version: 20190909.1754]

---
+++ elpa/sesman-20190909.1754/sesman-pkg.el
@@ -0,0 +1,12 @@
+(define-package "sesman" "20190909.1754" "Generic Session Manager"
+  '((emacs "25"))
+  :commit "edee869c209c016e5f0c5cbb8abb9f3ccd2d1e05" :keywords
+  '("process")
+  :authors
+  '(("Vitalie Spinu"))
+  :maintainer
+  '("Vitalie Spinu")
+  :url "https://github.com/vspinu/sesman")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
