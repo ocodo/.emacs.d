9b0a4b11d9a918495b227eb746fe1a063e8859cc
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:16 +0800

    [Adding powerline version: 20200105.2053]

---
+++ elpa/powerline-20200105.2053/powerline-pkg.el
@@ -0,0 +1,12 @@
+(define-package "powerline" "20200105.2053" "Rewrite of Powerline"
+  '((cl-lib "0.2"))
+  :commit "edbb464eef680efc9d408730288c716cd4cac404" :keywords
+  '("mode-line")
+  :authors
+  '(("Donald Ephraim Curtis" . "dcurtis@milkbox.net"))
+  :maintainer
+  '("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+  :url "http://github.com/milkypostman/powerline/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
