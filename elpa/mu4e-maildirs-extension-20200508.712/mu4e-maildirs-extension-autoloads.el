7ce124eec509617a0d83aaafefaa8ff6009762bc
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:38:57 +0800

    [Adding mu4e-maildirs-extension version: 20200508.712]

---
+++ elpa/mu4e-maildirs-extension-20200508.712/mu4e-maildirs-extension-autoloads.el
@@ -0,0 +1,31 @@
+;;; mu4e-maildirs-extension-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "mu4e-maildirs-extension" "mu4e-maildirs-extension.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from mu4e-maildirs-extension.el
+
+(autoload 'mu4e-maildirs-extension-load "mu4e-maildirs-extension" "\
+Initialize." nil nil)
+
+(autoload 'mu4e-maildirs-extension-unload "mu4e-maildirs-extension" "\
+Un-initialize." nil nil)
+
+(defalias 'mu4e-maildirs-extension 'mu4e-maildirs-extension-load)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "mu4e-maildirs-extension" '("mu4e-maildirs-extension-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; mu4e-maildirs-extension-autoloads.el ends here
---
+++ elpa/mu4e-maildirs-extension-20200508.712/mu4e-maildirs-extension-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/mu4e-maildirs-extension-20200508.712/mu4e-maildirs-extension.el  -*- no-byte-compile: t -*-
+(define-package "mu4e-maildirs-extension" "20200508.712" "Show mu4e maildirs summary in mu4e-main-view" '((dash "0.0.0")) :commit "4d2ece2226fa69a0e0bb23517a418145b92bd573" :authors '(("Andreu Gil Pàmies" . "agpchil@gmail.com")) :maintainer '("Andreu Gil Pàmies" . "agpchil@gmail.com") :url "http://github.com/agpchil/mu4e-maildirs-extension")
