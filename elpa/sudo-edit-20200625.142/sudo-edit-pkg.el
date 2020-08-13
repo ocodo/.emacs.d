0bc3f754cc98a593343f3c5d700915c0f4274ef0
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:38 +0800

    [Adding sudo-edit version: 20200625.142]

---
+++ elpa/sudo-edit-20200625.142/sudo-edit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/sudo-edit-20200625.142/sudo-edit.el  -*- no-byte-compile: t -*-
+(define-package "sudo-edit" "20200625.142" "Open files as another user" '((emacs "24") (cl-lib "0.5")) :commit "0e2c32b5e5242d30f8780cbe8e1b1649476cac4d" :keywords '("convenience") :authors '(("Nathaniel Flath" . "flat0103@gmail.com")) :maintainer '("Nathaniel Flath" . "flat0103@gmail.com") :url "https://github.com/nflath/sudo-edit")
