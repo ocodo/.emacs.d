4722ce50bb29593aca2c6b2fba364de1e893fdd3
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:27 +0800

    [Adding scad-mode version: 20200628.2256]

---
+++ elpa/scad-mode-20200628.2256/scad-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/scad-mode-20200628.2256/scad-mode.el  -*- no-byte-compile: t -*-
+(define-package "scad-mode" "20200628.2256" "A major mode for editing OpenSCAD code" 'nil :commit "fd94c4d568296711242e35f2a9f3823132b6bec6" :keywords '("languages") :authors '(("Len Trigg, Łukasz Stelmach")) :maintainer '("Len Trigg" . "lenbok@gmail.com") :url "https://raw.github.com/openscad/openscad/master/contrib/scad-mode.el")
