a2e7819213e721512fb803c47a1b8e42eed0d476
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:51 +0800

    [Adding web-mode version: 20200612.1038]

---
+++ elpa/web-mode-20200612.1038/web-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/web-mode-20200612.1038/web-mode.el  -*- no-byte-compile: t -*-
+(define-package "web-mode" "20200612.1038" "major mode for editing web templates" '((emacs "23.1")) :commit "60ffd878c4371644bd964f00fea38054645e3e47" :keywords '("languages") :authors '(("François-Xavier Bois <fxbois AT Google Mail Service>")) :maintainer '("François-Xavier Bois") :url "http://web-mode.org")
