0963f97d6ddbfe593e592a7da06568bce8c973f3
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:41 +0800

    [Adding tao-theme version: 20200325.344]

---
+++ elpa/tao-theme-20200325.344/tao-theme-autoloads.el
@@ -0,0 +1,45 @@
+;;; tao-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "tao-theme" "tao-theme.el" (0 0 0 0))
+;;; Generated autoloads from tao-theme.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tao-theme" '("tao-")))
+
+;;;***
+
+;;;### (autoloads nil "tao-yang-theme" "tao-yang-theme.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from tao-yang-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tao-yang-theme" '("tao-yang")))
+
+;;;***
+
+;;;### (autoloads nil "tao-yin-theme" "tao-yin-theme.el" (0 0 0 0))
+;;; Generated autoloads from tao-yin-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tao-yin-theme" '("tao-yin")))
+
+;;;***
+
+;;;### (autoloads nil nil ("tao-theme-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; tao-theme-autoloads.el ends here
---
+++ elpa/tao-theme-20200325.344/tao-theme-pkg.el
@@ -0,0 +1,4 @@
+(define-package "tao-theme" "20200325.344" "This package provides two parametrized uncoloured color themes for Emacs: tao-yin and tao-yang." 'nil :commit "34917843cde086943816d8a48977658c663a784e")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
