c635a714a649501ac5106e0b25bfbadc277765ec
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:06 +0800

    [Adding org-category-capture version: 20200329.313]

---
+++ elpa/org-category-capture-20200329.313/org-category-capture-autoloads.el
@@ -0,0 +1,23 @@
+;;; org-category-capture-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "org-category-capture" "org-category-capture.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from org-category-capture.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "org-category-capture" '("occ-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; org-category-capture-autoloads.el ends here
---
+++ elpa/org-category-capture-20200329.313/org-category-capture-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/org-category-capture-20200329.313/org-category-capture.el  -*- no-byte-compile: t -*-
+(define-package "org-category-capture" "20200329.313" "Contextualy capture of org-mode TODOs." '((org "9.0.0") (emacs "24")) :commit "96a57a43555e24e5e0d81e79f0fbb47001c41bac" :keywords '("org-mode" "todo" "tools" "outlines") :authors '(("Ivan Malison" . "IvanMalison@gmail.com")) :maintainer '("Ivan Malison" . "IvanMalison@gmail.com") :url "https://github.com/IvanMalison/org-projectile")
