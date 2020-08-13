12a3c2421421df692fde72d0fbf4df3e75148b7c
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:25 +0800

    [Adding rspec-mode version: 20200615.1443]

---
+++ elpa/rspec-mode-20200615.1443/rspec-mode-pkg.el
@@ -0,0 +1,13 @@
+(define-package "rspec-mode" "20200615.1443" "Enhance ruby-mode for RSpec"
+  '((ruby-mode "1.0")
+    (cl-lib "0.4"))
+  :commit "f1029cad2a8d9fd096e9a0f1ae9e7d7e2c8bd1ec" :keywords
+  '("rspec" "ruby")
+  :authors
+  '(("Peter Williams, et al."))
+  :maintainer
+  '("Peter Williams, et al.")
+  :url "http://github.com/pezra/rspec-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
