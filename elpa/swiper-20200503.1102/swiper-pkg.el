6d07c12ae6d4e7f17bd74f533f09b09a1018500b
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:39 +0800

    [Adding swiper version: 20200503.1102]

---
+++ elpa/swiper-20200503.1102/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/swiper-20200503.1102/swiper.el  -*- no-byte-compile: t -*-
+(define-package "swiper" "20200503.1102" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "9619e3ccc4e9179780cf48b7abb24239037791d7" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
