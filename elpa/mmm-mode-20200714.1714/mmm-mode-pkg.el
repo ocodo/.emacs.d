d408482bade0687ec9d499a2f987d0968feb4871
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:38:55 +0800

    [Adding mmm-mode version: 20200714.1714]

---
+++ elpa/mmm-mode-20200714.1714/mmm-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "mmm-mode" "20200714.1714" "Allow Multiple Major Modes in a buffer"
+  '((cl-lib "0.2"))
+  :commit "9ffe364f3a31c7a771fe3401b8924642609953e8" :keywords
+  '("convenience" "faces" "languages" "tools")
+  :authors
+  '(("Michael Abraham Shulman" . "viritrilbia@gmail.com"))
+  :maintainer
+  '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :url "https://github.com/purcell/mmm-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
