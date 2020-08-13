16a27af3dee2280020d2efa81d2d04f5027a995a
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:56 +0800

    [Adding xcode-project version: 20200810.2010]

---
+++ elpa/xcode-project-20200810.2010/xcode-project-pkg.el
@@ -0,0 +1,12 @@
+(define-package "xcode-project" "20200810.2010" "A package for reading Xcode project files."
+  '((emacs "25"))
+  :commit "90aef198df5b51dfdb9ad205aa5b412c471fd418" :keywords
+  '("languages" "tools")
+  :authors
+  '(("John Buckley" . "john@olivetoast.com"))
+  :maintainer
+  '("John Buckley" . "john@olivetoast.com")
+  :url "https://github.com/nhojb/xcode-project.git")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
