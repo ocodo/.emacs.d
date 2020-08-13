4e1c74f157b48ae39ccf77118163e1d32b82b82e
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:43 +0800

    [Adding treepy version: 20191108.2217]

---
+++ elpa/treepy-20191108.2217/treepy-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/treepy-20191108.2217/treepy.el  -*- no-byte-compile: t -*-
+(define-package "treepy" "20191108.2217" "Generic tree traversal tools" '((emacs "25.1")) :commit "3ac940e97f3d03e48ca9d7fcd74916a9b01c72f3" :keywords '("lisp" "maint" "tools") :authors '(("Daniel Barreto" . "daniel.barreto.n@gmail.com")) :maintainer '("Daniel Barreto" . "daniel.barreto.n@gmail.com") :url "https://github.com/volrath/treepy.el")
