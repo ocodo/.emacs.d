0ebef50794b31f15041a74668a3b43186cc05b62
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:49 +0800

    [Adding vue-mode version: 20190415.231]

---
+++ elpa/vue-mode-20190415.231/vue-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/vue-mode-20190415.231/vue-mode.el  -*- no-byte-compile: t -*-
+(define-package "vue-mode" "20190415.231" "Major mode for vue component based on mmm-mode" '((mmm-mode "0.5.5") (vue-html-mode "0.2") (ssass-mode "0.2") (edit-indirect "0.1.4")) :commit "031edd1f97db6e7d8d6c295c0e6d58dd128b9e71" :keywords '("languages") :authors '(("codefalling" . "code.falling@gmail.com")) :maintainer '("codefalling" . "code.falling@gmail.com"))
