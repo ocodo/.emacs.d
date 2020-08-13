eea39bd20aff84475135c39105413a775f2f0aa8
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:40:01 +0800

    [Adding zzz-to-char version: 20190713.1344]

---
+++ elpa/zzz-to-char-20190713.1344/zzz-to-char-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/zzz-to-char-20190713.1344/zzz-to-char.el  -*- no-byte-compile: t -*-
+(define-package "zzz-to-char" "20190713.1344" "Fancy version of `zap-to-char' command" '((emacs "24.4") (cl-lib "0.5") (avy "0.3.0")) :commit "b0a50b8cb6925c3184405f81be9ef9be9da69f09" :keywords '("convenience") :authors '(("Mark Karpov" . "markkarpov92@gmail.com")) :maintainer '("Mark Karpov" . "markkarpov92@gmail.com") :url "https://github.com/mrkkrp/zzz-to-char")
