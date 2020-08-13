db581b456b2ae0a9239573363d9d3d1e2943409f
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:38:52 +0800

    [Adding md-readme version: 20191112.1943]

---
+++ elpa/md-readme-20191112.1943/md-readme-autoloads.el
@@ -0,0 +1,26 @@
+;;; md-readme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "md-readme" "md-readme.el" (0 0 0 0))
+;;; Generated autoloads from md-readme.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "md-readme" '("mdr-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("md-readme-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; md-readme-autoloads.el ends here
---
+++ elpa/md-readme-20191112.1943/md-readme-pkg.el
@@ -0,0 +1,10 @@
+(define-package "md-readme" "20191112.1943" "Markdown-formatted READMEs for your ELisp" 'nil :commit "ca99f44de11fab18d1f50d4b1722f2ceee3c814d" :keywords
+  '("lisp" "help" "readme" "markdown" "header" "documentation" "github")
+  :authors
+  '(("Thomas Kappler" . "tkappler@gmail.com"))
+  :maintainer
+  '("Thomas Kappler" . "tkappler@gmail.com")
+  :url "http://github.com/thomas11/md-readme/tree/master")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
