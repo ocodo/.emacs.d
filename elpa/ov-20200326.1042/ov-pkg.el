8bf918a1eaf67cb7f3cdab76f6df198622d5fdd9
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:08 +0800

    [Adding ov version: 20200326.1042]

---
+++ elpa/ov-20200326.1042/ov-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/ov-20200326.1042/ov.el  -*- no-byte-compile: t -*-
+(define-package "ov" "20200326.1042" "Overlay library for Emacs Lisp" '((emacs "24.3")) :commit "c5b9aa4e1b00d702eb2caedd61c69a22a5fa1fab" :keywords '("convenience" "overlay") :authors '(("Shingo Fukuyama - http://fukuyama.co")) :maintainer '("Shingo Fukuyama - http://fukuyama.co") :url "https://github.com/ShingoFukuyama/ov.el")
