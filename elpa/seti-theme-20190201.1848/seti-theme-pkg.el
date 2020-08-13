88bb0f488a05fed8f0bbfb31b47ecfaaf81d14e2
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:29 +0800

    [Adding seti-theme version: 20190201.1848]

---
+++ elpa/seti-theme-20190201.1848/seti-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/seti-theme-20190201.1848/seti-theme.el  -*- no-byte-compile: t -*-
+(define-package "seti-theme" "20190201.1848" "A dark colored theme, inspired by Seti Atom Theme" 'nil :commit "9d76db0b91d4f574dd96ac80fad41da35bffa109" :keywords '("themes") :authors '(("Vlad Piersec" . "vlad.piersec@gmail.com")) :maintainer '("Vlad Piersec" . "vlad.piersec@gmail.com") :url "https://github.com/caisah/seti-theme")
