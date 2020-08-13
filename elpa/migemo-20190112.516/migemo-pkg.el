0eb7a1e14922b2bc67223bd21db04c7faf0a66f8
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:38:54 +0800

    [Adding migemo version: 20190112.516]

---
+++ elpa/migemo-20190112.516/migemo-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/migemo-20190112.516/migemo.el  -*- no-byte-compile: t -*-
+(define-package "migemo" "20190112.516" "Japanese incremental search through dynamic pattern expansion" '((cl-lib "0.5")) :commit "f42832c8ac462ecbec9a16eb781194f876fba64a" :authors '(("Satoru Takabayashi" . "satoru-t@is.aist-nara.ac.jp")) :maintainer '("Satoru Takabayashi" . "satoru-t@is.aist-nara.ac.jp") :url "https://github.com/emacs-jp/migemo")
