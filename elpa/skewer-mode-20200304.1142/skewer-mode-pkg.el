7a75eb0de078e84bb3594be388e45355b6152188
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:31 +0800

    [Adding skewer-mode version: 20200304.1142]

---
+++ elpa/skewer-mode-20200304.1142/skewer-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "skewer-mode" "20200304.1142" "live browser JavaScript, CSS, and HTML interaction"
+  '((simple-httpd "1.4.0")
+    (js2-mode "20090723")
+    (emacs "24"))
+  :commit "e5bed351939c92a1f788f78398583c2f83f1bb3c")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
