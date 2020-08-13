ce318bbdfe4bf6e68608b22507ccd9855191ce0d
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:38:59 +0800

    [Adding nav-flash version: 20191204.1427]

---
+++ elpa/nav-flash-20191204.1427/nav-flash-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/nav-flash-20191204.1427/nav-flash.el  -*- no-byte-compile: t -*-
+(define-package "nav-flash" "20191204.1427" "Briefly highlight the current line" 'nil :commit "dbb91216637e0a1e8bfd59aa883c75d45db70daf" :keywords '("extensions" "navigation" "interface") :authors '(("Roland Walker" . "walker@pobox.com")) :maintainer '("Roland Walker" . "walker@pobox.com") :url "http://github.com/rolandwalker/nav-flash")
