cb97919f786a3e331358aa541fb9f6a6c3a37529
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:38:48 +0800

    [Adding async version: 20200809.501]

---
+++ elpa/async-20200809.501/async-pkg.el
@@ -0,0 +1,8 @@
+(define-package "async" "20200809.501" "Asynchronous processing in Emacs"
+  '((emacs "24.3"))
+  :commit "36a10151e70e956e2f766ed9e65f4a9cfc8479b2" :keywords
+  '("async")
+  :url "https://github.com/jwiegley/emacs-async")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
