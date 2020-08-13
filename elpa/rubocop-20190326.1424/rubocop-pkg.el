c5bc28da535bcfdb456225254b6495a55ad72e27
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:25 +0800

    [Adding rubocop version: 20190326.1424]

---
+++ elpa/rubocop-20190326.1424/rubocop-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/rubocop-20190326.1424/rubocop.el  -*- no-byte-compile: t -*-
+(define-package "rubocop" "20190326.1424" "An Emacs interface for RuboCop" '((emacs "24")) :commit "03bf15558a6eb65e4f74000cab29412efd46660e" :keywords '("project" "convenience") :authors '(("Bozhidar Batsov")) :maintainer '("Bozhidar Batsov") :url "https://github.com/bbatsov/rubocop-emacs")
