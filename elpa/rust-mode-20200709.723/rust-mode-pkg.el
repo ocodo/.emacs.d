5be76bb8bfd4ad7e02eab2f336bda936e5c5237e
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:26 +0800

    [Adding rust-mode version: 20200709.723]

---
+++ elpa/rust-mode-20200709.723/rust-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/rust-mode-20200709.723/rust-mode.el  -*- no-byte-compile: t -*-
+(define-package "rust-mode" "20200709.723" "A major emacs mode for editing Rust source code" '((emacs "25.1")) :commit "ca7d99c6fd90fc1e636aa9d4020a2f077786a0c3" :keywords '("languages") :authors '(("Mozilla")) :maintainer '("Mozilla") :url "https://github.com/rust-lang/rust-mode")
