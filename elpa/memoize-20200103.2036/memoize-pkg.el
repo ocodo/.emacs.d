51eb3eeebfacdd1b3fe32a6fe2729e53b611997e
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:38:53 +0800

    [Adding memoize version: 20200103.2036]

---
+++ elpa/memoize-20200103.2036/memoize-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/memoize-20200103.2036/memoize.el  -*- no-byte-compile: t -*-
+(define-package "memoize" "20200103.2036" "Memoization functions" 'nil :commit "51b075935ca7070f62fae1d69fe0ff7d8fa56fdd" :authors '(("Christopher Wellons" . "mosquitopsu@gmail.com")) :maintainer '("Christopher Wellons" . "mosquitopsu@gmail.com") :url "https://github.com/skeeto/emacs-memoize")
