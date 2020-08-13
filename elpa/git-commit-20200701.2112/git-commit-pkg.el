713ac99ca6884807b00d50a345c4d1836c2ceda7
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:38:49 +0800

    [Adding git-commit version: 20200701.2112]

---
+++ elpa/git-commit-20200701.2112/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/git-commit-20200701.2112/git-commit.el  -*- no-byte-compile: t -*-
+(define-package "git-commit" "20200701.2112" "Edit Git commit messages" '((emacs "25.1") (dash "20200524") (transient "20200601") (with-editor "20200522")) :commit "4033285e04403178b2cb9191d88749c3b672e9d0" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
