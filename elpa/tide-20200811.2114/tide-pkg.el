6bcd74ce2fa4b1787ce0b815c10346f088df38d7
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:42 +0800

    [Adding tide version: 20200811.2114]

---
+++ elpa/tide-20200811.2114/tide-pkg.el
@@ -0,0 +1,16 @@
+(define-package "tide" "20200811.2114" "Typescript Interactive Development Environment"
+  '((dash "2.10.0")
+    (s "1.11.0")
+    (flycheck "27")
+    (typescript-mode "0.1")
+    (cl-lib "0.5"))
+  :commit "f0b6dac4829c3daecf02bf445a5b41b4c68f2911" :keywords
+  '("typescript")
+  :authors
+  '(("Anantha kumaran" . "ananthakumaran@gmail.com"))
+  :maintainer
+  '("Anantha kumaran" . "ananthakumaran@gmail.com")
+  :url "http://github.com/ananthakumaran/tide")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
