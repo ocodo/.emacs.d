4bcbacb2ffbeb2e728d21d371ebd5964250e52ad
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:23 +0800

    [Adding ripgrep version: 20190215.841]

---
+++ elpa/ripgrep-20190215.841/ripgrep-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/ripgrep-20190215.841/ripgrep.el  -*- no-byte-compile: t -*-
+(define-package "ripgrep" "20190215.841" "Front-end for ripgrep, a command line search tool" 'nil :commit "40e871dcc4519a70981e9f28acea304692a60978" :keywords '("ripgrep" "ack" "pt" "ag" "sift" "grep" "search") :authors '(("Nicolas Lamirault" . "nicolas.lamirault@gmail.com")) :maintainer '("Nicolas Lamirault" . "nicolas.lamirault@gmail.com") :url "https://github.com/nlamirault/ripgrep.el")
