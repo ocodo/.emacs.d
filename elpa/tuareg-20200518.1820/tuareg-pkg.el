8435158bde5e6173b1f85f542b453c9031b24416
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:45 +0800

    [Adding tuareg version: 20200518.1820]

---
+++ elpa/tuareg-20200518.1820/tuareg-pkg.el
@@ -0,0 +1,18 @@
+(define-package "tuareg" "20200518.1820" "OCaml mode for Emacs."
+  '((caml "3.12.0.1")
+    (emacs "24.4"))
+  :commit "ccde45bbc292123ec20617f1af7f7e19f7481545" :keywords
+  '("ocaml" "languages")
+  :authors
+  '(("Albert Cohen" . "Albert.Cohen@inria.fr")
+    ("Sam Steingold" . "sds@gnu.org")
+    ("Christophe Troestler" . "Christophe.Troestler@umons.ac.be")
+    ("Till Varoquaux" . "till@pps.jussieu.fr")
+    ("Sean McLaughlin" . "seanmcl@gmail.com")
+    ("Stefan Monnier" . "monnier@iro.umontreal.ca"))
+  :maintainer
+  '("Albert Cohen" . "Albert.Cohen@inria.fr")
+  :url "https://github.com/ocaml/tuareg")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
