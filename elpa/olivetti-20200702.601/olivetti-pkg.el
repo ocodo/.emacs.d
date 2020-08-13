a034e27fc1c2c0751f4fcc69e267bc99889d65e3
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:04 +0800

    [Adding olivetti version: 20200702.601]

---
+++ elpa/olivetti-20200702.601/olivetti-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/olivetti-20200702.601/olivetti.el  -*- no-byte-compile: t -*-
+(define-package "olivetti" "20200702.601" "Minor mode for a nice writing environment" '((emacs "24.5")) :commit "0bc5e98b8456493084d1bd3df35e92a12c5da3b2" :keywords '("wp" "text") :authors '(("William Rankin" . "william@bydasein.com")) :maintainer '("William Rankin" . "william@bydasein.com") :url "https://github.com/rnkn/olivetti")
