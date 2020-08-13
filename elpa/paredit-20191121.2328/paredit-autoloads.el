a59adb9d1256a85176976209596f3cf1ae302977
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:11 +0800

    [Adding paredit version: 20191121.2328]

---
+++ elpa/paredit-20191121.2328/paredit-autoloads.el
@@ -0,0 +1,41 @@
+;;; paredit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "paredit" "paredit.el" (0 0 0 0))
+;;; Generated autoloads from paredit.el
+
+(autoload 'paredit-mode "paredit" "\
+Minor mode for pseudo-structurally editing Lisp code.
+With a prefix argument, enable Paredit Mode even if there are
+  unbalanced parentheses in the buffer.
+Paredit behaves badly if parentheses are unbalanced, so exercise
+  caution when forcing Paredit Mode to be enabled, and consider
+  fixing unbalanced parentheses instead.
+\\<paredit-mode-map>
+
+If called interactively, enable Paredit mode if ARG is positive,
+and disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it if
+ARG is `toggle'; disable the mode otherwise.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'enable-paredit-mode "paredit" "\
+Turn on pseudo-structural editing of Lisp code." t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "paredit" '("?\\" "disable-paredit-mode" "paredit-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; paredit-autoloads.el ends here
---
+++ elpa/paredit-20191121.2328/paredit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/paredit-20191121.2328/paredit.el  -*- no-byte-compile: t -*-
+(define-package "paredit" "20191121.2328" "minor mode for editing parentheses" 'nil :commit "8330a41e8188fe18d3fa805bb9aa529f015318e8" :keywords '("lisp") :authors '(("Taylor R. Campbell" . "campbell+paredit@mumble.net")) :maintainer '("Taylor R. Campbell" . "campbell+paredit@mumble.net"))
