d1caa1996bb2648bc7ea09a636a067f9467447e9
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:57 +0800

    [Adding yaml-mode version: 20200725.1836]

---
+++ elpa/yaml-mode-20200725.1836/yaml-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/yaml-mode-20200725.1836/yaml-mode.el  -*- no-byte-compile: t -*-
+(define-package "yaml-mode" "20200725.1836" "Major mode for editing YAML files" '((emacs "24.1")) :commit "68fecb5f0dec712a10c8655df6881392a4613617" :keywords '("data" "yaml") :authors '(("Yoshiki Kurihara" . "clouder@gmail.com") ("Marshall T. Vandegrift" . "llasram@gmail.com")) :maintainer '("Vasilij Schneidermann" . "mail@vasilij.de"))
