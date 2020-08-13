34efb45861e1705342954aa2397aafc886fdef9f
jasonm23 <jasonm23@gmail.com>
2020-08-13 19:39:24 +0800

    [Adding robe version: 20200628.1225]

---
+++ elpa/robe-20200628.1225/robe-pkg.el
@@ -0,0 +1,13 @@
+(define-package "robe" "20200628.1225" "Code navigation, documentation lookup and completion for Ruby"
+  '((inf-ruby "2.5.1")
+    (emacs "24.4"))
+  :commit "082da38797d247f9c64568ad712a345e041d5c84" :keywords
+  '("ruby" "convenience" "rails")
+  :authors
+  '(("Dmitry Gutov"))
+  :maintainer
+  '("Dmitry Gutov")
+  :url "https://github.com/dgutov/robe")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
