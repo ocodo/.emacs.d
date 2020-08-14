9386bd08f25eeea0ca538cd4694036242a84ae22
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:50 +0800

    [Removing synonymous version: 20180325.1117]

--- elpa/synonymous-20180325.1117/synonymous-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; synonymous-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "synonymous" "synonymous.el" (23278 48011 0
-;;;;;;  0))
-;;; Generated autoloads from synonymous.el
-
-(autoload 'synonymous-synonyms "synonymous" "\
-Lookup synonyms for a word.
-
-\(fn &optional EVENT OPOINT)" t nil)
-
-(autoload 'synonymous-antonyms "synonymous" "\
-Lookup antonyms for a word.
-
-\(fn &optional EVENT OPOINT)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; synonymous-autoloads.el ends here
