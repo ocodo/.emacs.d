f6bded81318230c0cfd188828b10e1013e3f6edd
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:53 +0800

    [Removing point-stack versions: 20170808.1658,20170808.958]

--- elpa/point-stack-20170808.958/point-stack-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "point-stack" "20170808.958" "Back and forward navigation through buffer locations" 'nil :commit "76e17311e3a810314c7d31ac46dc55450ff30fa2")
