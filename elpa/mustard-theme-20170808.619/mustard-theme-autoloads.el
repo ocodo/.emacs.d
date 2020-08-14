526f96092125946c167af647357ca01ae83d1361
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:20 +0800

    [Removing mustard-theme version: 20170808.619]

--- elpa/mustard-theme-20170808.619/mustard-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; mustard-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "mustard-theme" "mustard-theme.el" (22955 53279
-;;;;;;  141445 577000))
-;;; Generated autoloads from mustard-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; mustard-theme-autoloads.el ends here
