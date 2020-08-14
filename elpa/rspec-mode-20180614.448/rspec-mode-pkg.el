c099606840016569a107a0584902f38fb6f05981
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:13 +0800

    [Removing rspec-mode versions: 20171025.1606,20180614.448,20181120.1347]

--- elpa/rspec-mode-20180614.448/rspec-mode-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "rspec-mode" "20180614.448" "Enhance ruby-mode for RSpec"
-  '((ruby-mode "1.0")
-    (cl-lib "0.4"))
-  :keywords
-  '("rspec" "ruby")
-  :url "http://github.com/pezra/rspec-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
