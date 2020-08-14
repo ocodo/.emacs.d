8161914e195b25a20afc3a6465e1ae8c29f126fe
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:29 +0800

    [Updating graphviz-dot-mode version: 20200304.432][removing old versions: 20171103.127,20181118.551]

---
+++ elpa/graphviz-dot-mode-20200304.432/graphviz-dot-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "graphviz-dot-mode" "20200304.432" "Mode for the dot-language used by graphviz (att)."
+  '((emacs "25.0"))
+  :commit "3642a0a5f41a80c8ecef7c6143d514200b80e194" :keywords
+  '("mode" "dot" "dot-language" "dotlanguage" "graphviz" "graphs" "att")
+  :maintainer
+  '("Pieter Pareit" . "pieter.pareit@gmail.com")
+  :url "https://ppareit.github.io/graphviz-dot-mode/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
