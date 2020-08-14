7dde318f652b2961609f32952aa93e9c47cd4395
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:39 +0800

    [Removing flymake-vala version: 20150325.2231]

--- elpa/flymake-vala-20150325.2231/flymake-vala-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; flymake-vala-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "flymake-vala" "flymake-vala.el" (21780 6825
-;;;;;;  883401 0))
-;;; Generated autoloads from flymake-vala.el
-
-(autoload 'flymake-vala-load "flymake-vala" "\
-Configure flymake mode to check the current buffer's vala syntax.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flymake-vala-autoloads.el ends here
--- elpa/flymake-vala-20150325.2231/flymake-vala-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-vala" "20150325.2231" "A flymake handler for vala-mode files" '((flymake-easy "0.1")) :url "https://github.com/daniellawrence/flymake-vala" :keywords '("convenience" "vala"))
