2ce1b723ce1ea4131212e60db0106aaad3b0f173
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:50 +0800

    [Removing fxrd-mode version: 20170728.1101]

--- elpa/fxrd-mode-20170728.1101/fxrd-mode-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "fxrd-mode" "20170728.1101" "Major mode for editing fixed field width files"
-  '((s "1.2"))
-  :url "https://github.com/msherry/fxrd-mode" :keywords
-  '("convenience"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
