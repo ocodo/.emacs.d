e5b1b51b16cf76895659d0f576b9537924a1e855
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:45 +0800

    [Removing buster-snippets version: 20151125.210]

--- elpa/buster-snippets-20151125.210/buster-snippets-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; buster-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "buster-snippets" "buster-snippets.el" (22108
-;;;;;;  42472 649147 292000))
-;;; Generated autoloads from buster-snippets.el
-
-(autoload 'buster-snippets-initialize "buster-snippets" "\
-
-
-\(fn)" nil nil)
-
-(eval-after-load "yasnippet" '(buster-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("buster-snippet-helpers.el" "buster-snippets-pkg.el")
-;;;;;;  (22108 42472 945063 192000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; buster-snippets-autoloads.el ends here
--- elpa/buster-snippets-20151125.210/buster-snippets-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "buster-snippets" "20151125.210" "Yasnippets for the Buster javascript testing framework"
-  '((yasnippet "0.8.0"))
-  :keywords
-  '("snippets"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
