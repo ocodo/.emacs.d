58d18254732af4020d21d9042b555d1aa761f0db
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:30 +0800

    [Removing skewer-reload-stylesheets version: 20160725.520]

--- elpa/skewer-reload-stylesheets-20160725.520/skewer-reload-stylesheets-autoloads.el
+++
@@ -1,45 +0,0 @@
-;;; skewer-reload-stylesheets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "skewer-reload-stylesheets" "skewer-reload-stylesheets.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from skewer-reload-stylesheets.el
-
-(autoload 'skewer-reload-stylesheets-start-editing "skewer-reload-stylesheets" "\
-Configure current buffer for live-editing.
-
-Add this to your stylesheet editing mode hook to make every
-stylesheet live-editable by default.
-
-\(fn)" nil nil)
-
-(autoload 'skewer-reload-stylesheets-mode "skewer-reload-stylesheets" "\
-Minor mode for interactively reloading CSS stylesheets.
-
-\(fn &optional ARG)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "skewer-reload-stylesheets" '("skewer-reload-stylesheets-")))
-
-;;;***
-
-;;;### (autoloads nil "skewer-reload-stylesheets-test" "skewer-reload-stylesheets-test.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from skewer-reload-stylesheets-test.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "skewer-reload-stylesheets-test" '("skewer-reload-stylesheets-test-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("skewer-reload-stylesheets-pkg.el") (0
-;;;;;;  0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; skewer-reload-stylesheets-autoloads.el ends here
--- elpa/skewer-reload-stylesheets-20160725.520/skewer-reload-stylesheets-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "skewer-reload-stylesheets" "20160725.520" "live-edit CSS, SCSS, Less, and friends."
-  '((skewer-mode "1.5.3")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
