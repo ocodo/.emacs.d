af8ae3016ab1e838b2531f90ed0fbc72e13838bd
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:38 +0800

    [Removing httpcode version: 20121001.2045]

--- elpa/httpcode-20121001.2045/httpcode-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; httpcode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (hc) "httpcode" "httpcode.el" (21265 60998 0 0))
-;;; Generated autoloads from httpcode.el
-
-(autoload 'hc "httpcode" "\
-Display the meaning of an HTTP status code
-
-\(fn CODE)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("httpcode-pkg.el") (21265 60998 428597
-;;;;;;  0))
-
-;;;***
-
-(provide 'httpcode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; httpcode-autoloads.el ends here
--- elpa/httpcode-20121001.2045/httpcode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "httpcode" "20121001.2045" "explains the meaning of an HTTP status code" (quote nil))
