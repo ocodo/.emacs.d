05727af5bed08795a46124f2261c840f62d66d39
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:51 +0800

    [Removing helm-commandlinefu version: 20150610.2245]

--- elpa/helm-commandlinefu-20150610.2245/helm-commandlinefu-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-commandlinefu" "20150610.2245" "Search and browse commandlinefu.com from helm" '((emacs "24.1") (helm "1.7.0") (json "1.3") (let-alist "1.0.3")) :url "https://github.com/xuchunyang/helm-commandlinefu" :keywords '("commandlinefu.com"))
