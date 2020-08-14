74073a3787a8cf318c48c9c789b79320f073bf88
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:36 +0800

    [Removing bbyac version: 20180206.641]

--- elpa/bbyac-20180206.641/bbyac-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "bbyac" "20180206.641" "Type a little Bit, and Bang! You Are Completed."
-  '((browse-kill-ring "1.3")
-    (cl-lib "0.5"))
-  :url "https://github.com/baohaojun/bbyac" :keywords
-  '("abbrev"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
