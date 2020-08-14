117bcd7d6782b7cee8497fc48e5635cf78490d23
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:21 +0800

    [Removing scad-mode versions: 20180108.1809,20180109.209]

--- elpa/scad-mode-20180108.1809/scad-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "scad-mode" "20180108.1809" "A major mode for editing OpenSCAD code" 'nil :commit "2de6815c7c2ade42e53e86751b9584ac4a5e1ef9" :url "https://raw.github.com/openscad/openscad/master/contrib/scad-mode.el" :keywords '("languages"))
