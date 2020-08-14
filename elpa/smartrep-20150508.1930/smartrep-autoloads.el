5e5b7ec635a433ebebf05a3810f8c73275c1e78e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:34 +0800

    [Removing smartrep version: 20150508.1930]

--- elpa/smartrep-20150508.1930/smartrep-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; smartrep-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("smartrep.el") (21841 22605 110804 883000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; smartrep-autoloads.el ends here
--- elpa/smartrep-20150508.1930/smartrep-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smartrep" "20150508.1930" "Support sequential operation which omitted prefix keys." 'nil :url "https://github.com/myuhe/smartrep.el" :keywords '("convenience"))
