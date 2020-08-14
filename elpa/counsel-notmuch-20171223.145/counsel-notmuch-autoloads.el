b93b476c03ba61f645b1c4e5bbe2f9bb635ac634
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:26 +0800

    [Removing counsel-notmuch version: 20171223.145]

--- elpa/counsel-notmuch-20171223.145/counsel-notmuch-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; counsel-notmuch-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "counsel-notmuch" "counsel-notmuch.el" (23360
-;;;;;;  14657 112949 739000))
-;;; Generated autoloads from counsel-notmuch.el
-
-(autoload 'counsel-notmuch "counsel-notmuch" "\
-Search for your email in notmuch with INITIAL-INPUT.
-
-\(fn &optional INITIAL-INPUT)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; counsel-notmuch-autoloads.el ends here
--- elpa/counsel-notmuch-20171223.145/counsel-notmuch-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel-notmuch" "20171223.145" "Search emails in Notmuch asynchronously with Ivy" '((emacs "24") (ivy "0.10.0") (notmuch "0.21") (s "1.12.0")))
