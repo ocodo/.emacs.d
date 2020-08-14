779d9e242e97ee11e9c0625388dbfa988e88c00f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:15 +0800

    [Removing ruby-end version: 20141215.423]

--- elpa/ruby-end-20141215.423/ruby-end-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; ruby-end-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "ruby-end" "ruby-end.el" (21664 42421 519401
-;;;;;;  0))
-;;; Generated autoloads from ruby-end.el
-
-(autoload 'ruby-end-mode "ruby-end" "\
-Automatic insertion of end blocks for Ruby.
-
-\(fn &optional ARG)" t nil)
-
-(add-hook 'ruby-mode-hook 'ruby-end-mode)
-
-(add-hook 'enh-ruby-mode-hook 'ruby-end-mode)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ruby-end-autoloads.el ends here
--- elpa/ruby-end-20141215.423/ruby-end-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ruby-end" "20141215.423" "Automatic insertion of end blocks for Ruby" 'nil :url "http://github.com/rejeep/ruby-end" :keywords '("speed" "convenience" "ruby"))
