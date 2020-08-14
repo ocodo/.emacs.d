8d66ec986d38b942b4821b88cdf39aed5313387e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:05 +0800

    [Removing color-moccur version: 20141222.1635]

--- elpa/color-moccur-20141222.1635/color-moccur-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "color-moccur" "20141222.1635" "multi-buffer occur (grep) mode" 'nil :url "http://www.bookshelf.jp/elc/color-moccur.el" :keywords '("convenience"))
