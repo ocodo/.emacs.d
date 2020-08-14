cdb2bc673c033cda90984873ca144551bcf76c2a
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:18 +0800

    [Removing multi-web-mode version: 20130824.54]

--- elpa/multi-web-mode-20130824.54/multi-web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "multi-web-mode" "20130824.54" "multiple major mode support for web editing [github]" 'nil)
-
--- elpa/multi-web-mode-20130824.54/mweb-example-config.el
+++
@@ -1,7 +0,0 @@
-(require 'multi-web-mode)
-(setq mweb-default-major-mode 'html-mode)
-(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-                  (js-mode "<script[^>]*>" "</script>")
-                  (css-mode "<style[^>]*>" "</style>")))
-(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
-(multi-web-global-mode 1)
