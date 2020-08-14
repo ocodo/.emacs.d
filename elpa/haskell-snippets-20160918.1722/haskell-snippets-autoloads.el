2f33e15b57c463b3e6fa57cfe183b3248213e4da
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:42 +0800

    [Removing haskell-snippets version: 20160918.1722]

--- elpa/haskell-snippets-20160918.1722/haskell-snippets-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; haskell-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "haskell-snippets" "haskell-snippets.el" (22507
-;;;;;;  31522 433228 829000))
-;;; Generated autoloads from haskell-snippets.el
-
-(autoload 'haskell-snippets-initialize "haskell-snippets" "\
-
-
-\(fn)" nil nil)
-
-(eval-after-load 'yasnippet '(haskell-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("haskell-snippets-pkg.el") (22507 31522
-;;;;;;  369228 828000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; haskell-snippets-autoloads.el ends here
--- elpa/haskell-snippets-20160918.1722/haskell-snippets-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "haskell-snippets" "20160918.1722" "Yasnippets for Haskell"
-  '((cl-lib "0.5")
-    (yasnippet "0.8.0"))
-  :url "https://github.com/haskell/haskell-snippets" :keywords
-  '("snippets" "haskell"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
