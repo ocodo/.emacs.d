0629fc915a6b23128f1dfff6263a60ef25808ce6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:15 +0800

    [Removing move-text version: 20170908.2030]

--- elpa/move-text-20170908.2030/move-text-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "move-text" "20170908.2030" "Move current line or region with M-up or M-down." 'nil :commit "bdaf3e3a0d33cd528cad1d10facbdf0635232e4d" :url "https://github.com/emacsfodder/move-text" :keywords '("edit"))
