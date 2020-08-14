26bb42f58c2605a6becd0245ee614040de1ddd04
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:39 +0800

    [Removing splitjoin version: 20150505.732]

--- elpa/splitjoin-20150505.732/splitjoin-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; splitjoin-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "splitjoin" "splitjoin.el" (21833 28617 251401
-;;;;;;  0))
-;;; Generated autoloads from splitjoin.el
-
-(autoload 'splitjoin "splitjoin" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; splitjoin-autoloads.el ends here
--- elpa/splitjoin-20150505.732/splitjoin-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "splitjoin" "20150505.732" "Transition between multiline and single-line code" '((cl-lib "0.5")) :url "https://github.com/syohex/emacs-splitjoin")
