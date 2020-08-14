bfe35b99e750a6b5808b2b5292b231eb8578bbd8
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:14 +0800

    [Removing web-mode versions: 20180503.452,20180620.110,20181104.2004]

--- elpa/web-mode-20180620.110/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20180620.110" "major mode for editing web templates" '((emacs "23.1")) :commit "0f4a9be46809c165f940dbd6e8e3b9f7a2dd4562" :keywords '("languages") :url "http://web-mode.org")
