904c56e2d437ee573ec21a7c359138192d83437e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:46 +0800

    [Removing format-sql version: 20150422.633]

--- elpa/format-sql-20150422.633/format-sql-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; format-sql-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "format-sql" "format-sql.el" (21818 58344 747401
-;;;;;;  0))
-;;; Generated autoloads from format-sql.el
-
-(autoload 'format-sql-region "format-sql" "\
-Uses the \"format-sql\" tool to reformat the current region.
-
-\(fn)" t nil)
-
-(autoload 'format-sql-buffer "format-sql" "\
-Uses the \"format-sql\" tool to reformat the current buffer.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; format-sql-autoloads.el ends here
--- elpa/format-sql-20150422.633/format-sql-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "format-sql" "20150422.633" "Use format-sql to make your SQL readable in directly Emacs." 'nil :url "https://github.com/paetzke/format-sql.el")
