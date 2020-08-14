117c57a101a50158618473689a7b30ee4e9596a2
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:42 +0800

    [Removing linum-relative version: 20180124.247]

--- elpa/linum-relative-20180124.247/linum-relative-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "linum-relative" "20180124.247" "display relative line number in emacs." 'nil :commit "c74a6981b688a5e1e6b8e0809363963ff558ce4d" :url "http://github.com/coldnew/linum-relative" :keywords '("converience"))
