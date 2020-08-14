ae20632f40ae5c75dd96c2ba8bf4faf41490d1b6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:50 +0800

    [Removing livid-mode version: 20131116.1340]

--- elpa/livid-mode-20131116.1340/livid-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; livid-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (livid-mode) "livid-mode" "livid-mode.el" (21131
-;;;;;;  17345 0 0))
-;;; Generated autoloads from livid-mode.el
-
-(autoload 'livid-mode "livid-mode" "\
-Minor mode for automatic evaluation of a JavaScript buffer on every change
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("livid-mode-pkg.el") (21131 17345 104617
-;;;;;;  0))
-
-;;;***
-
-(provide 'livid-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; livid-mode-autoloads.el ends here
--- elpa/livid-mode-20131116.1340/livid-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "livid-mode" "20131116.1340" "Live browser eval of JavaScript every time a buffer changes [github]" (quote ((skewer-mode "1.5.3") (s "1.8.0"))))
