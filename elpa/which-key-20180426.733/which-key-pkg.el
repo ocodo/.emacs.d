a0050257187e1ffeadbd52839ef6426beeb8191c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:17 +0800

    [Removing which-key versions: 20180426.733,20180621.1238,20181114.1432]

--- elpa/which-key-20180426.733/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "which-key" "20180426.733" "Display available keybindings in popup" '((emacs "24.4")) :commit "8a878de16db5ff83ce78f2a43d22ed2170b3abd3" :url "https://github.com/justbur/emacs-which-key")
--- elpa/which-key-20180621.1238/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "which-key" "20180621.1238" "Display available keybindings in popup" '((emacs "24.4")) :commit "ff79dfff66f880885c5893dd6fd05dc51173a476" :url "https://github.com/justbur/emacs-which-key")
