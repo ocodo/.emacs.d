4f7b177f5599eb447626f9fc5cd1857af22fbc54
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:07 +0800

    [Removing go-add-tags version: 20161123.427]

--- elpa/go-add-tags-20161123.427/go-add-tags-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-add-tags" "20161123.427" "Add field tags for struct fields" '((emacs "24.3") (s "1.11.0")) :url "https://github.com/syohex/emacs-go-add-tags")
