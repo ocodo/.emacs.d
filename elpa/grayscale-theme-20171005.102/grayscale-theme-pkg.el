b154b0409b3636c4d1a0e33bde60620e18fe6f87
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:30 +0800

    [Removing grayscale-theme version: 20171005.102]

--- elpa/grayscale-theme-20171005.102/grayscale-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "grayscale-theme" "20171005.102" "A simple grayscale theme" 'nil :commit "917d63c0effc8459502a41e0cad5822d2b200499" :url "https://github.com/belak/emacs-grayscale-theme" :keywords '("lisp"))
