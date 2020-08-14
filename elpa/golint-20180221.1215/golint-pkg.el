56f1b6e39716a1f39b5e2989b15be272e18fd62f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:19 +0800

    [Removing golint version: 20180221.1215]

--- elpa/golint-20180221.1215/golint-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "golint" "20180221.1215" "lint for the Go source code" 'nil :commit "470b6b0bb3005eda157f0275e2e4895055396a81" :url "https://github.com/golang/lint")
