066ad068e19d54689d0d48ba9aee3c54b03d1faa
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:19 +0800

    [Removing mustache-mode version: 20141024.732]

--- elpa/mustache-mode-20141024.732/mustache-mode-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; mustache-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "mustache-mode" "mustache-mode.el" (21583 44835
-;;;;;;  506547 72000))
-;;; Generated autoloads from mustache-mode.el
-
-(autoload 'mustache-mode "mustache-mode" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; mustache-mode-autoloads.el ends here
--- elpa/mustache-mode-20141024.732/mustache-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "mustache-mode" "20141024.732" "A major mode for editing Mustache files." 'nil)
