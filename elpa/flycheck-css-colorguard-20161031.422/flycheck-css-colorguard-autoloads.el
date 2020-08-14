b693456beee7a460983d5578717207738b9e8bcb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:23 +0800

    [Removing flycheck-css-colorguard version: 20161031.422]

--- elpa/flycheck-css-colorguard-20161031.422/flycheck-css-colorguard-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; flycheck-css-colorguard-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "flycheck-css-colorguard" "flycheck-css-colorguard.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from flycheck-css-colorguard.el
-
-(autoload 'flycheck-css-colorguard-setup "flycheck-css-colorguard" "\
-Setup Flycheck CSS Colorguard.
-Add `css-colorguard' to `flycheck-checkers'.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flycheck-css-colorguard-autoloads.el ends here
--- elpa/flycheck-css-colorguard-20161031.422/flycheck-css-colorguard-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flycheck-css-colorguard" "20161031.422" "Detect similar colors in CSS" '((flycheck "0.22") (emacs "24")) :url "https://github.com/Simplify/flycheck-css-colorguard/" :keywords '("flycheck" "css" "colorguard"))
