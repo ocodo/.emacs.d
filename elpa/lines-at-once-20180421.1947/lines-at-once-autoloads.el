6d6ec44994b03746173b314fed0f6b6a908952ed
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:42 +0800

    [Removing lines-at-once version: 20180421.1947]

--- elpa/lines-at-once-20180421.1947/lines-at-once-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; lines-at-once-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "lines-at-once" "lines-at-once.el" (23360 14584
-;;;;;;  311216 220000))
-;;; Generated autoloads from lines-at-once.el
-
-(autoload 'lines-at-once-insert "lines-at-once" "\
-Expand the line at point to multiple lines.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; lines-at-once-autoloads.el ends here
--- elpa/lines-at-once-20180421.1947/lines-at-once-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "lines-at-once" "20180421.1947" "Insert and edit multiple lines at once" '((emacs "25")))
