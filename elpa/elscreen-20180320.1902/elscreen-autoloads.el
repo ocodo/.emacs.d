cc358eaf9522d1760c1177f274aad45b483f6638
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:29 +0800

    [Removing elscreen version: 20180320.1902]

--- elpa/elscreen-20180320.1902/elscreen-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; elscreen-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "elscreen" "elscreen.el" (23278 48354 0 0))
-;;; Generated autoloads from elscreen.el
-
-(autoload 'elscreen-start "elscreen" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("elscreen-buffer-list.el" "elscreen-color-theme.el"
-;;;;;;  "elscreen-dired.el" "elscreen-dnd.el" "elscreen-gf.el" "elscreen-goby.el"
-;;;;;;  "elscreen-howm.el" "elscreen-pkg.el" "elscreen-server.el"
-;;;;;;  "elscreen-speedbar.el" "elscreen-w3m.el" "elscreen-wl.el")
-;;;;;;  (23278 48354 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; elscreen-autoloads.el ends here
