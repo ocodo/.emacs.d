8f450aaa6e0be4ba0d2e3b1bfb33be370275f5ca
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:59 +0800

    [Removing mag-menu version: 20150505.1150]

--- elpa/mag-menu-20150505.1150/mag-menu-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mag-menu" "20150505.1150" "Intuitive keyboard-centric menu system" '((splitter "0.1.0")) :url "https://github.com/chumpage/mag-menu" :keywords '("convenience"))
