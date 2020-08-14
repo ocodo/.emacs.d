f1713e93e287f3cdc21b182d56cd93ea6c76b494
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:01 +0800

    [Removing twilight-anti-bright-theme version: 20160622.148]

--- elpa/twilight-anti-bright-theme-20160622.148/twilight-anti-bright-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; twilight-anti-bright-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "twilight-anti-bright-theme" "twilight-anti-bright-theme.el"
-;;;;;;  (22396 28843 525361 946000))
-;;; Generated autoloads from twilight-anti-bright-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; twilight-anti-bright-theme-autoloads.el ends here
--- elpa/twilight-anti-bright-theme-20160622.148/twilight-anti-bright-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "twilight-anti-bright-theme" "20160622.148" "A soothing Emacs 24 light-on-dark theme" 'nil :url "https://github.com/jimeh/twilight-anti-bright-theme.el" :keywords '("themes"))
