02a4c703a6e7d2b7a00c4bf46f1e26923d494243
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:04 +0800

    [Removing redis version: 20150531.1248]

--- elpa/redis-20150531.1248/redis-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "redis" "20150531.1248" "Redis integration" '((emacs "24") (cl-lib "0.5")) :url "https://github.com/emacs-pe/redis.el" :keywords '("convenience"))
