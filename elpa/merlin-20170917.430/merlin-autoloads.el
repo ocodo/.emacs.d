33251a448f7378f940c9de9f01f7d3227be7a79d
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:12 +0800

    [Removing merlin versions: 20170917.430,20180816.815]

--- elpa/merlin-20170917.430/merlin-autoloads.el
+++
@@ -1,41 +0,0 @@
-;;; merlin-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "merlin" "merlin.el" (23064 61219 528787 490000))
-;;; Generated autoloads from merlin.el
-
-(autoload 'merlin-mode "merlin" "\
-Minor mode for interacting with a merlin process.
-Runs a merlin process in the background and perform queries on it.
-
-Short cuts:
-\\{merlin-mode-map}
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "merlin-company" "merlin-company.el" (23064
-;;;;;;  61219 528787 490000))
-;;; Generated autoloads from merlin-company.el
-
-(autoload 'merlin-company-backend "merlin-company" "\
-
-
-\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("merlin-ac.el" "merlin-cap.el" "merlin-iedit.el"
-;;;;;;  "merlin-imenu.el" "merlin-pkg.el") (23064 61219 532787 473000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; merlin-autoloads.el ends here
--- elpa/merlin-20170917.430/merlin-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "merlin" "20170917.430" "Mode for Merlin, an assistant for OCaml." 'nil :url "https://github.com/ocaml/merlin" :keywords
-  '("ocaml" "languages"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
