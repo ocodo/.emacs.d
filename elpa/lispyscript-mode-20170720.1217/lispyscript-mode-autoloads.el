fb2cf3c93e3c2311d9aaaa861e6d4356f93fc7b3
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:45 +0800

    [Removing lispyscript-mode version: 20170720.1217]

--- elpa/lispyscript-mode-20170720.1217/lispyscript-mode-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; lispyscript-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "lispyscript-mode" "lispyscript-mode.el" (0
-;;;;;;  0 0 0))
-;;; Generated autoloads from lispyscript-mode.el
-
-(autoload 'lispyscript-mode "lispyscript-mode" "\
-Major mode for LispyScript
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist (cons "\\.ls\\'" 'lispyscript-mode))
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "lispyscript-mode" '("lispyscript-font-lock-defaults")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; lispyscript-mode-autoloads.el ends here
