e07c199d5ff86a2314fccbea36d070d448e12594
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:00 +0800

    [Removing evil-tutor version: 20150102.2250]

--- elpa/evil-tutor-20150102.2250/evil-tutor-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; evil-tutor-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "evil-tutor" "evil-tutor.el" (21818 58358 107401
-;;;;;;  0))
-;;; Generated autoloads from evil-tutor.el
-
-(autoload 'evil-tutor-start "evil-tutor" "\
-Start a evil-tutor session.
-
-\(fn)" t nil)
-
-(defalias 'evil-tutor-resume 'evil-tutor-start)
-
-;;;***
-
-;;;### (autoloads nil nil ("evil-tutor-pkg.el") (21818 58358 118884
-;;;;;;  981000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; evil-tutor-autoloads.el ends here
