e2775128b4d19e747efdb9babc4836ec3269b098
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:31 +0800

    [Removing ocodo-svg-modelines version: 20150516.719]

--- elpa/ocodo-svg-modelines-20150516.719/ocodo-svg-modelines-autoloads.el
+++
@@ -1,41 +0,0 @@
-;;; ocodo-svg-modelines-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "ocodo-smt-overrides" "ocodo-smt-overrides.el"
-;;;;;;  (21849 28443 543401 0))
-;;; Generated autoloads from ocodo-smt-overrides.el
-
-(autoload 'ocodo-smt-string-from-file "ocodo-smt-overrides" "\
-
-
-\(fn FILE)" nil nil)
-
-;;;***
-
-;;;### (autoloads nil "ocodo-svg-modelines" "ocodo-svg-modelines.el"
-;;;;;;  (21849 28443 547401 0))
-;;; Generated autoloads from ocodo-svg-modelines.el
-
-(autoload 'ocodo-svg-modelines-init "ocodo-svg-modelines" "\
-Initialize Ocodo's SVG Modeline collection.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("ocodo-geometry-flakes-smt.el" "ocodo-kawaii-light-smt.el"
-;;;;;;  "ocodo-mesh-aqua-smt.el" "ocodo-mesh-grass-smt.el" "ocodo-mesh-retro-aqua-smt.el"
-;;;;;;  "ocodo-minimal-dark-smt.el" "ocodo-minimal-light-smt.el"
-;;;;;;  "ocodo-steps-aqua-smt.el" "ocodo-steps-grass-smt.el" "ocodo-svg-modelines-pkg.el")
-;;;;;;  (21849 28443 590352 704000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ocodo-svg-modelines-autoloads.el ends here
--- elpa/ocodo-svg-modelines-20150516.719/ocodo-svg-modelines-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "ocodo-svg-modelines" "20150516.719" "A collection of beautiful SVG modelines"
-  '((svg-mode-line-themes "0")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
