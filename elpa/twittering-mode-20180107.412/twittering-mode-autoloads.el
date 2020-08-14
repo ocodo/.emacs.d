077316398b45731fcf419a5c1711a59ef8f67650
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:01 +0800

    [Removing twittering-mode versions: 20180107.412,20180507.721]

--- elpa/twittering-mode-20180107.412/twittering-mode-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; twittering-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "twittering-mode" "twittering-mode.el" (23138
-;;;;;;  47685 776099 268000))
-;;; Generated autoloads from twittering-mode.el
-
-(autoload 'twit "twittering-mode" "\
-Start twittering-mode.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; twittering-mode-autoloads.el ends here
--- elpa/twittering-mode-20180507.721/twittering-mode-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; twittering-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "twittering-mode" "twittering-mode.el" (23344
-;;;;;;  44710 980653 963000))
-;;; Generated autoloads from twittering-mode.el
-
-(autoload 'twit "twittering-mode" "\
-Start twittering-mode.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; twittering-mode-autoloads.el ends here
