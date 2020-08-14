66ee8c3873a1aa2a92c5d995e2c2ec823361818d
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:02 +0800

    [Removing typescript-mode versions: 20180409.422,20180605.1414,20181130.830]

--- elpa/typescript-mode-20181130.830/typescript-mode-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "typescript-mode" "20181130.830" "Major mode for editing typescript" 'nil :keywords
-  '("typescript" "languages")
-  :url "http://github.com/ananthakumaran/typescript.el")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
