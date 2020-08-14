a31d6b787cc63241cbfb688317f2b883c546f2dc
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:13 +0800

    [Removing firecode-theme version: 20170808.611]

--- elpa/firecode-theme-20170808.611/firecode-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; firecode-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "firecode-theme" "firecode-theme.el" (22955
-;;;;;;  53531 201703 256000))
-;;; Generated autoloads from firecode-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; firecode-theme-autoloads.el ends here
--- elpa/firecode-theme-20170808.611/firecode-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "firecode-theme" "20170808.611" "an Emacs 24 theme based on FireCode (tmTheme)" '((emacs "24.0")) :commit "8b7b03ecdd41e70dab145b98906017e1392eaef4" :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
