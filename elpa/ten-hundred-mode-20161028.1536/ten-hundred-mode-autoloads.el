b3621ad64a17ccf46897078df1ea74f22c7c7ef4
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:53 +0800

    [Removing ten-hundred-mode version: 20161028.1536]

--- elpa/ten-hundred-mode-20161028.1536/ten-hundred-mode-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; ten-hundred-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "ten-hundred-mode" "ten-hundred-mode.el" (0
-;;;;;;  0 0 0))
-;;; Generated autoloads from ten-hundred-mode.el
-
-(autoload 'ten-hundred-mode "ten-hundred-mode" "\
-This makes you write with only the ten hundred most usual
-words that everyone uses every day. If you try to use any other
-words, the computer takes them away for you, so you can try
-again. So you always use easy words that everyone can
-understand, and no one has trouble figuring out what you want
-to say.
-
-\(fn &optional ARG)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ten-hundred-mode" '("ten-hundred-mode-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("ten-hundred-mode-pkg.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ten-hundred-mode-autoloads.el ends here
