1534a336c93874e6a7eac36e099f6edf9249c33b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:02 +0800

    [Removing codesearch version: 20180508.822]

--- elpa/codesearch-20180508.822/codesearch-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "codesearch" "20180508.822" "Core support for managing codesearch tools"
-  '((elog "0.1"))
-  :keywords
-  '("tools" "development" "search")
-  :url "https://github.com/abingham/emacs-codesearch")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
