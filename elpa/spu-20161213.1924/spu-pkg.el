1ebe8a98046c76923fbba515831d39a911d34442
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:40 +0800

    [Removing spu version: 20161213.1924]

--- elpa/spu-20161213.1924/spu-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "spu" "20161213.1924" "Silently upgrade package in the background"
-  '((emacs "24.4")
-    (signal "1.0")
-    (timp "1.2.0"))
-  :url "https://github.com/mola-T/spu" :keywords
-  '("convenience" "package"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
