26a65053312450e91151ea3700838edfb3cdfa4b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:54 +0800

    [Removing evil-rsi version: 20160221.1304]

--- elpa/evil-rsi-20160221.1304/evil-rsi-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-rsi" "20160221.1304" "Use emacs motion keys in evil, inspired by vim-rsi" '((evil "1.0.0")) :url "http://github.com/linktohack/evil-rsi" :keywords '("evil" "rsi" "evil-rsi"))
