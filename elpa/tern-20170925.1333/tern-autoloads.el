d2d06038e4aea9039107a216893f5de0e2985f51
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:54 +0800

    [Removing tern version: 20170925.1333]

--- elpa/tern-20170925.1333/tern-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; tern-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "tern" "tern.el" (23064 61003 501847 95000))
-;;; Generated autoloads from tern.el
-
-(autoload 'tern-use-server "tern" "\
-
-
-\(fn PORT SERVER)" t nil)
-
-(autoload 'tern-mode "tern" "\
-Minor mode binding to the Tern JavaScript analyzer
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; tern-autoloads.el ends here
