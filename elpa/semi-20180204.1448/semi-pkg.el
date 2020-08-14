cb5e1e60a4ef27cc208f50667a447a971c3d9304
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:23 +0800

    [Removing semi versions: 20180204.1448,20180825.901]

--- elpa/semi-20180204.1448/semi-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "semi" "20180204.1448" "A library to provide MIME features."
-  '((flim "1.14.9")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/semi-20180825.901/semi-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "semi" "20180825.901" "A library to provide MIME features."
-  '((flim "1.14.9")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
