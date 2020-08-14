152ef26437b3a4099e13d1477f1710537eb1c66d
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:43 +0800

    [Removing buffer-watcher version: 20170913.139]

--- elpa/buffer-watcher-20170913.139/buffer-watcher-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; buffer-watcher-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("buffer-watcher.el") (23064 60677 820188
-;;;;;;  221000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; buffer-watcher-autoloads.el ends here
