628eaf7fc6a84419360d136fd785f39ad3806ed0
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:56 +0800

    [Removing powerline versions: 20180321.1948,20180322.248]

--- elpa/powerline-20180321.1948/powerline-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "powerline" "20180321.1948" "Rewrite of Powerline"
-  '((cl-lib "0.2"))
-  :keywords
-  '("mode-line")
-  :url "http://github.com/milkypostman/powerline/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
