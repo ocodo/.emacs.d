2d3c926ac622e54033415086d3dcff7138848db5
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:09 +0800

    [Removing verify-url version: 20160426.528]

--- elpa/verify-url-20160426.528/verify-url-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; verify-url-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "verify-url" "verify-url.el" (22309 38817 579792
-;;;;;;  39000))
-;;; Generated autoloads from verify-url.el
-
-(autoload 'verify-url "verify-url" "\
-find out invalid urls in buffer or region
-
-\(fn &optional START END)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; verify-url-autoloads.el ends here
--- elpa/verify-url-20160426.528/verify-url-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "verify-url" "20160426.528" "find out invalid urls in the buffer or region" '((cl-lib "0.5")) :url "https://github.com/lujun9972/verify-url" :keywords '("convenience" "usability" "url"))
