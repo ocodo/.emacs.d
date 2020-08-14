af1a382db7c7c55f71f6daffd39a3282fa97faeb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:15 +0800

    [Removing el-spec version: 20121018.304]

--- elpa/el-spec-20121018.304/el-spec-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; el-spec-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("el-spec-pkg.el" "el-spec.el") (21011
-;;;;;;  20048 470427 0))
-
-;;;***
-
-(provide 'el-spec-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; el-spec-autoloads.el ends here
--- elpa/el-spec-20121018.304/el-spec-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "el-spec" "20121018.304" "ruby's rspec like syntax test frame work [github]" (quote nil))
