2dd7b7952d380f6ade8ae4f06205602c1c1963d6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:02 +0800

    [Removing pythonic versions: 20180208.214,20180624.2212,20180920.2315]

--- elpa/pythonic-20180208.214/pythonic-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pythonic" "20180208.214" "Utility functions for writing pythonic emacs package." '((emacs "24") (cl-lib "0.5") (dash "2.11") (s "1.9") (f "0.17.2")) :commit "e8102636cdd44a65638e3752fac9834a4ea87504" :url "https://github.com/proofit404/pythonic")
--- elpa/pythonic-20180624.2212/pythonic-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pythonic" "20180624.2212" "Utility functions for writing pythonic emacs package." '((emacs "25") (s "1.9") (f "0.17.2")) :commit "838eaf47c23628c5e88a59796bbe4653c57bf5c0" :url "https://github.com/proofit404/pythonic")
