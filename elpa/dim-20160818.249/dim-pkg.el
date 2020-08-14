06cf0c6189ff025a71524c01c831c6bf22c12804
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:51 +0800

    [Removing dim version: 20160818.249]

--- elpa/dim-20160818.249/dim-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dim" "20160818.249" "Change mode-line names of major/minor modes" '((emacs "24.4")) :url "https://github.com/alezost/dim.el" :keywords '("convenience"))
