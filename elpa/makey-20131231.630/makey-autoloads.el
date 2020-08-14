539fb36b28324feed0868bbf2af792f65291fb83
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:03 +0800

    [Removing makey version: 20131231.630]

--- elpa/makey-20131231.630/makey-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; makey-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (makey-initialize-key-groups) "makey" "makey.el"
-;;;;;;  (21359 36927 0 0))
-;;; Generated autoloads from makey.el
-
-(autoload 'makey-initialize-key-groups "makey" "\
-Initializes KEY-GROUP and creates all the relevant interactive commands.
-
-\(fn KEY-GROUP)" nil nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("makey-pkg.el") (21359 36927 226057 0))
-
-;;;***
-
-(provide 'makey-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; makey-autoloads.el ends here
