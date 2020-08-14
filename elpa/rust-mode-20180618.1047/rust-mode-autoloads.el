3bb498ecacf4a4f99ac41bbb39265f7bb141fdae
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:18 +0800

    [Removing rust-mode versions: 20180322.127,20180618.1047,20181008.1628]

--- elpa/rust-mode-20180618.1047/rust-mode-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; rust-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "rust-mode" "rust-mode.el" (23344 44788 646473
-;;;;;;  56000))
-;;; Generated autoloads from rust-mode.el
-
-(autoload 'rust-mode "rust-mode" "\
-Major mode for Rust code.
-
-\\{rust-mode-map}
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; rust-mode-autoloads.el ends here
