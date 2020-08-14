8fe67786b9e2e6d7e5e272ca1dd98f24b8b3d8fe
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:22 +0800

    [Removing elisp-docstring-mode version: 20170304.815]

--- elpa/elisp-docstring-mode-20170304.815/elisp-docstring-mode-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; elisp-docstring-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "elisp-docstring-mode" "elisp-docstring-mode.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from elisp-docstring-mode.el
-
-(autoload 'elisp-docstring-mode "elisp-docstring-mode" "\
-Major mode for editing Emacs Lisp docstrings.
-
-\(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "elisp-docstring-mode" '("elisp-docstring-mode-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; elisp-docstring-mode-autoloads.el ends here
--- elpa/elisp-docstring-mode-20170304.815/elisp-docstring-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "elisp-docstring-mode" "20170304.815" "Major mode for editing elisp docstrings." 'nil :keywords '("languages"))
