374d445c4d8ffc52b52570e3f742ac59dd6593f7
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:23 +0800

    [Removing xkcd version: 20160419.430]

--- elpa/xkcd-20160419.430/xkcd-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "xkcd" "20160419.430" "View xkcd from Emacs" '((json "1.3")) :url "https://github.com/vibhavp/emacs-xkcd" :keywords '("xkcd" "webcomic"))
