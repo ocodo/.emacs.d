234d943aaa6c67ec8e6a7b7d1bd3191999ac9261
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:22 +0800

    [Removing karma version: 20160220.445]

--- elpa/karma-20160220.445/karma-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "karma" "20160220.445" "Karma Test Runner Emacs Integration" '((pkg-info "0.4") (emacs "24")) :url "http://github.com/tonini/karma.el" :keywords '("language" "javascript" "js" "karma" "testing"))
