b2736e77b207d5d94bbe12d172ea970873c2129e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:24 +0800

    [Removing seoul256-theme version: 20180505.57]

--- elpa/seoul256-theme-20180505.57/seoul256-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "seoul256-theme" "20180505.57" "Low-contrast color scheme based on Seoul Colors." '((emacs "24.3")) :commit "d28a9de73a5ffb1a1c9492db75a5c1efe5e9815f" :keywords '("theme") :url "http://github.com/anandpiyer/seoul256-emacs")
