381e3a66d0c5146771776fa862526e36cfd71f45
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:06 +0800

    [Removing fancy-narrow version: 20171030.1716]

--- elpa/fancy-narrow-20171030.1716/fancy-narrow-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "fancy-narrow" "20171030.1716" "narrow-to-region with more eye candy." 'nil :commit "9f4a587f6a5a387271fb665e13f59d41fd42504c" :url "http://github.com/Bruce-Connor/fancy-narrow" :keywords '("faces" "convenience"))
