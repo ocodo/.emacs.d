804ba038ec01e16b90815869847f8045ff19d4d5
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:49 +0800

    [Removing diffview version: 20150928.2211]

--- elpa/diffview-20150928.2211/diffview-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "diffview" "20150928.2211" "View diffs in side-by-side format" 'nil :url "https://github.com/mgalgs/diffview-mode" :keywords '("convenience" "diff"))
