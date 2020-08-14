aa2bda44b284c49dee0a09d2686e442b2dab4c49
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:07 +0800

    [Removing helm-itunes version: 20151012.2348]

--- elpa/helm-itunes-20151012.2348/helm-itunes-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; helm-itunes-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "helm-itunes" "helm-itunes.el" (22045 47996
-;;;;;;  857140 0))
-;;; Generated autoloads from helm-itunes.el
-
-(autoload 'helm-itunes "helm-itunes" "\
-Bring up a Spotify search interface in helm.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-itunes-autoloads.el ends here
--- elpa/helm-itunes-20151012.2348/helm-itunes-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-itunes" "20151012.2348" "Play local iTunes and Spotify tracks" '((helm "1.6.1")) :url "https://github.com/daschwa/helm-itunes")
