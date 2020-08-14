94bfa5eb93997d4ccec0d55ef65671024550bf4f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:12 +0800

    [Removing go-impl version: 20170125.752]

--- elpa/go-impl-20170125.752/go-impl-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-impl" "20170125.752" "impl integration for go-mode" '((emacs "24.3") (go-mode "1.3.0")) :url "https://github.com/syohex/emacs-go-impl")
