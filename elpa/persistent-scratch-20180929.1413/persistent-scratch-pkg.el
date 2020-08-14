010ae27b2779ba8e2078a5204f55a009732bd09c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:47 +0800

    [Removing persistent-scratch versions: 20180425.1811,20180929.1413]

--- elpa/persistent-scratch-20180929.1413/persistent-scratch-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "persistent-scratch" "20180929.1413" "Preserve the scratch buffer across Emacs sessions" '((emacs "24")) :commit "2e6678a837db85e68da713bbd4772c7fb88d83d4" :authors '(("Fanael Linithien" . "fanael4@gmail.com")) :maintainer '("Fanael Linithien" . "fanael4@gmail.com") :url "https://github.com/Fanael/persistent-scratch")
