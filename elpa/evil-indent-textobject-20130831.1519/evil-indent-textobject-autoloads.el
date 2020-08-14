84d08900071e805b091d8940e1d7ea7f030060c3
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:47 +0800

    [Removing evil-indent-textobject version: 20130831.1519]

--- elpa/evil-indent-textobject-20130831.1519/evil-indent-textobject-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; evil-indent-textobject-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "evil-indent-textobject" "evil-indent-textobject.el"
-;;;;;;  (21439 25206 210176 999000))
-;;; Generated autoloads from evil-indent-textobject.el
-
-(eval-after-load 'evil '(progn (autoload 'evil-indent-i-indent "evil-indent-textobject" nil t) (autoload 'evil-indent-a-indent "evil-indent-textobject" nil t) (autoload 'evil-indent-a-indent-lines "evil-indent-textobject" nil t) (define-key evil-inner-text-objects-map "i" 'evil-indent-i-indent) (define-key evil-outer-text-objects-map "i" 'evil-indent-a-indent) (define-key evil-outer-text-objects-map "I" 'evil-indent-a-indent-lines)))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; evil-indent-textobject-autoloads.el ends here
--- elpa/evil-indent-textobject-20130831.1519/evil-indent-textobject-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-indent-textobject" "20130831.1519" "evil textobjects based on indentation" '((evil "0")))
