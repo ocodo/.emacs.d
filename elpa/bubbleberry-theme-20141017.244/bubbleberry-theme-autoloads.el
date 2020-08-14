708f154c3d3c2cf7b081f047e13458da621150e4
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:42 +0800

    [Removing bubbleberry-theme version: 20141017.244]

--- elpa/bubbleberry-theme-20141017.244/bubbleberry-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; bubbleberry-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "bubbleberry-theme" "bubbleberry-theme.el"
-;;;;;;  (21575 10136 914547 72000))
-;;; Generated autoloads from bubbleberry-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; bubbleberry-theme-autoloads.el ends here
--- elpa/bubbleberry-theme-20141017.244/bubbleberry-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bubbleberry-theme" "20141017.244" "A theme based on LightTable for Emacs24" '((emacs "24.1")))
