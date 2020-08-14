9265750daa441b76734fcfc648e4a1089ffbdfc0
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:39 +0800

    [Removing borland-blue-theme version: 20160117.521]

--- elpa/borland-blue-theme-20160117.521/borland-blue-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; borland-blue-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "borland-blue-theme" "borland-blue-theme.el"
-;;;;;;  (22175 12933 696901 378000))
-;;; Generated autoloads from borland-blue-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; borland-blue-theme-autoloads.el ends here
--- elpa/borland-blue-theme-20160117.521/borland-blue-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "borland-blue-theme" "20160117.521" "Blue/yellow theme based on old DOS Borland/Turbo C IDE" '((emacs "24.1")) :url "http://github.com/fourier/borland-blue-theme" :keywords '("themes"))
