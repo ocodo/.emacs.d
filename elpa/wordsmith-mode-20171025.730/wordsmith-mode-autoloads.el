6f618982d678ab6950db3580987f07cb28488319
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:19 +0800

    [Removing wordsmith-mode version: 20171025.730]

--- elpa/wordsmith-mode-20171025.730/wordsmith-mode-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; wordsmith-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "wordsmith-mode" "wordsmith-mode.el" (23064
-;;;;;;  60908 306419 794000))
-;;; Generated autoloads from wordsmith-mode.el
-
-(autoload 'wordsmith-mode "wordsmith-mode" "\
-Toggle wordsmith mode.
-Interactively with no argument, this command toggles the mode.
-to show buffer size and position in mode-line.  You can customize
-this minor mode, see option `wordsmith-mode'.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; wordsmith-mode-autoloads.el ends here
