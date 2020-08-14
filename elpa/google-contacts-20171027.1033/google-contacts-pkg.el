fa26bca97863acd46919b5088a9a4914cdce6fb2
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:20 +0800

    [Removing google-contacts version: 20171027.1033]

--- elpa/google-contacts-20171027.1033/google-contacts-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "google-contacts" "20171027.1033" "Support for Google Contacts in Emacs"
-  '((oauth2 "0.10")
-    (cl-lib "0.5"))
-  :url "http://julien.danjou.info/projects/emacs-packages#google-contacts" :keywords
-  '("comm"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
