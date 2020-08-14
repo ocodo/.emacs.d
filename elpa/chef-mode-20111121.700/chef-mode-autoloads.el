1352d97d6f16ea2feca60072b1ff9683d7f85208
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:54 +0800

    [Removing chef-mode version: 20111121.700]

--- elpa/chef-mode-20111121.700/chef-mode-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; chef-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("chef-mode.el") (21713 57893 10405 33000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; chef-mode-autoloads.el ends here
--- elpa/chef-mode-20111121.700/chef-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "chef-mode" "20111121.700" "minor mode for editing an opscode chef repository" 'nil :keywords '("chef" "knife"))
