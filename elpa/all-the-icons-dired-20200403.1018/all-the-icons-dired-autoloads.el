e2344b6a3061a587417f1009682c9a6833640135
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:19 +0800

    [Updating all-the-icons-dired version: 20200403.1018][removing old version: 20170418.2131]

--- elpa/all-the-icons-dired-20200403.1018/all-the-icons-dired-autoloads.el
+++ elpa/all-the-icons-dired-20200403.1018/all-the-icons-dired-autoloads.el
@@ -13,6 +13,11 @@
 (autoload 'all-the-icons-dired-mode "all-the-icons-dired" "\
 Display all-the-icons icon for each files in a dired buffer.
 
+If called interactively, enable All-The-Icons-Dired mode if ARG
+is positive, and disable it if ARG is zero or negative.  If
+called from Lisp, also enable the mode if ARG is omitted or nil,
+and toggle it if ARG is `toggle'; disable the mode otherwise.
+
 \(fn &optional ARG)" t nil)
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "all-the-icons-dired" '("all-the-icons-dired-")))
---
+++ elpa/all-the-icons-dired-20200403.1018/all-the-icons-dired-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/all-the-icons-dired-20200403.1018/all-the-icons-dired.el  -*- no-byte-compile: t -*-
+(define-package "all-the-icons-dired" "20200403.1018" "Shows icons for each file in dired mode" '((emacs "24.4") (all-the-icons "2.2.0")) :commit "fc2dfa1e9eb8bf1c402a675e7089638d702a27a5" :keywords '("files" "icons" "dired") :authors '(("jtbm37")) :maintainer '("jtbm37") :url "https://github.com/jtbm37/all-the-icons-dired")
