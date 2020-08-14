895d0e36df519e60cdcf580409c4e6e07f99f0fb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:55 +0800

    [Removing helm-describe-modes version: 20160211.2118]

--- elpa/helm-describe-modes-20160211.2118/helm-describe-modes-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; helm-describe-modes-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "helm-describe-modes" "helm-describe-modes.el"
-;;;;;;  (22218 26549 186328 280000))
-;;; Generated autoloads from helm-describe-modes.el
-
-(autoload 'helm-describe-modes "helm-describe-modes" "\
-A convenient Helm version of `describe-mode'.
-
-By default, it lists the major mode, active minor modes, and
-inactive minor modes.  Sources can be added or removed by
-customizing `helm-describe-modes-function-list'.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-describe-modes-autoloads.el ends here
--- elpa/helm-describe-modes-20160211.2118/helm-describe-modes-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-describe-modes" "20160211.2118" "Helm interface to major and minor modes." '((helm "1.9") (cl-lib "0.5") (emacs "24.1")) :url "https://github.com/emacs-helm/helm-describe-modes" :keywords '("docs" "convenience"))
