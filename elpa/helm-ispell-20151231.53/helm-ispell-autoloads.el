a4d11f057b057c71323b4761d1ac50d3005bce64
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:06 +0800

    [Removing helm-ispell version: 20151231.53]

--- elpa/helm-ispell-20151231.53/helm-ispell-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; helm-ispell-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "helm-ispell" "helm-ispell.el" (22160 38947
-;;;;;;  1235 167000))
-;;; Generated autoloads from helm-ispell.el
-
-(autoload 'helm-ispell "helm-ispell" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-ispell-autoloads.el ends here
--- elpa/helm-ispell-20151231.53/helm-ispell-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-ispell" "20151231.53" "ispell-complete-word with helm interface" '((helm-core "1.7.7")) :url "https://github.com/syohex/emacs-helm-ispell")
