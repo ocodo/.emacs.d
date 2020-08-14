3d3f40c46cac5de3d8901b35c8d57bcd65552f6f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:25 +0800

    [Removing flycheck-nimsuggest version: 20171027.1508]

--- elpa/flycheck-nimsuggest-20171027.1508/flycheck-nimsuggest-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flycheck-nimsuggest" "20171027.1508" "flycheck backend for Nim using nimsuggest" '((flycheck "0.23") (emacs "24.3")) :commit "dc9a5de1cb3ee05db5794d824610959a1f603bc9" :url "https://github.com/yuutayamada/flycheck-nimsuggest")
