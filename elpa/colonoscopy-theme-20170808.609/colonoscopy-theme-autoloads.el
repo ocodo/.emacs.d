b001fc62ac7c3fc0370095b0ed871324d05596df
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:04 +0800

    [Removing colonoscopy-theme version: 20170808.609]

--- elpa/colonoscopy-theme-20170808.609/colonoscopy-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; colonoscopy-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "colonoscopy-theme" "colonoscopy-theme.el"
-;;;;;;  (23064 61852 834772 626000))
-;;; Generated autoloads from colonoscopy-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; colonoscopy-theme-autoloads.el ends here
--- elpa/colonoscopy-theme-20170808.609/colonoscopy-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "colonoscopy-theme" "20170808.609" "an Emacs 24 theme based on Colonoscopy (tmTheme)" '((emacs "24.0")) :commit "64bbb322b13dae91ce9f1e3581f836f94f800ead" :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
