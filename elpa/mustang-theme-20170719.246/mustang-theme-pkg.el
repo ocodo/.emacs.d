8d5ece2379604fc7f6704215ab01e47f7c87170b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:20 +0800

    [Removing mustang-theme version: 20170719.246]

--- elpa/mustang-theme-20170719.246/mustang-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mustang-theme" "20170719.246" "port of vim's mustang theme" 'nil :commit "dda6d04803f1c9b196b620ef564e7768fee15de2" :url "http://github.com/mswift42/mustang-theme")
