9d4ff6423ebec840f2c952284fbbaad916709f41
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:19 +0800

    [Removing flx-ido version: 20180117.719]

--- elpa/flx-ido-20180117.719/flx-ido-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flx-ido" "20180117.719" "flx integration for ido" '((flx "0.1") (cl-lib "0.3")) :commit "9c5cb5de0202b4eaac9359c84ca7ce9cbd7ee835" :url "https://github.com/lewang/flx")
