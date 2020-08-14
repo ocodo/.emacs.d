951b9b8425a98139b88ad7cc59b22beea6fbfbec
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:36 +0800

    [Removing darcula-theme version: 20171227.1045]

--- elpa/darcula-theme-20171227.1045/darcula-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; darcula-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "darcula-theme" "darcula-theme.el" (23138 48820
-;;;;;;  870479 950000))
-;;; Generated autoloads from darcula-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; darcula-theme-autoloads.el ends here
--- elpa/darcula-theme-20171227.1045/darcula-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "darcula-theme" "20171227.1045" "Inspired by IntelliJ's Darcula theme" 'nil :commit "d9b82b58ded9014985be6658f4ab17e26ed9e93e" :url "https://gitlab.com/fommil/emacs-darcula-theme" :keywords '("faces"))
