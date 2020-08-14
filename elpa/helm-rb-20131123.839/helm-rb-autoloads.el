ac32631ebd31b16baaf4c9023d92158b6454ac94
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:14 +0800

    [Removing helm-rb version: 20131123.839]

--- elpa/helm-rb-20131123.839/helm-rb-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; helm-rb-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (helm-rb) "helm-rb" "helm-rb.el" (21215 24000 0
-;;;;;;  0))
-;;; Generated autoloads from helm-rb.el
-
-(autoload 'helm-rb "helm-rb" "\
-Search Ruby's method by using helm and ag.
-If you want to change methods searching program, you can specify
-the program's path to `helm-rb-get-methods-program' variable.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("helm-rb-pkg.el") (21215 24000 142606
-;;;;;;  0))
-
-;;;***
-
-(provide 'helm-rb-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; helm-rb-autoloads.el ends here
--- elpa/helm-rb-20131123.839/helm-rb-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "helm-rb" "20131123.839" "Search Ruby's method by ag and display helm"
-  '((helm "1.0")
-    (helm-ag-r "20131123")))
