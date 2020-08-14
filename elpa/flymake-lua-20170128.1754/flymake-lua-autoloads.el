52bdbb970a0b7d0c8f9ccdba78a28f3ad44cbf2c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:36 +0800

    [Removing flymake-lua version: 20170128.1754]

--- elpa/flymake-lua-20170128.1754/flymake-lua-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; flymake-lua-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "flymake-lua" "flymake-lua.el" (0 0 0 0))
-;;; Generated autoloads from flymake-lua.el
-
-(autoload 'flymake-lua-load "flymake-lua" "\
-
-
-\(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-lua" '("flymake-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flymake-lua-autoloads.el ends here
--- elpa/flymake-lua-20170128.1754/flymake-lua-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-lua" "20170128.1754" "Flymake for Lua" 'nil :keywords '("lua"))
