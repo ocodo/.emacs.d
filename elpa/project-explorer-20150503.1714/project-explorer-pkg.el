de64208edfdc18bed7705834f7f56dbece2cb984
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:58 +0800

    [Removing project-explorer version: 20150503.1714]

--- elpa/project-explorer-20150503.1714/project-explorer-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "project-explorer" "20150503.1714" "A project explorer sidebar" '((cl-lib "0.3") (es-lib "0.3") (es-windows "0.1") (emacs "24")) :url "https://github.com/sabof/project-explorer")
