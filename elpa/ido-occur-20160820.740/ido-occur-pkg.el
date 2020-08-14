f847308d120540f101dd5a703451d0d1bf6a0a86
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:47 +0800

    [Removing ido-occur version: 20160820.740]

--- elpa/ido-occur-20160820.740/ido-occur-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ido-occur" "20160820.740" "Yet another `occur' with `ido'." '((dash "2.13.0")) :url "https://github.com/danil/ido-occur" :keywords '("inner" "buffer" "search"))
