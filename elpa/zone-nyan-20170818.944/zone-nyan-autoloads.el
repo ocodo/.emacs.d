78d7e46e0301023e42605c244838c60125a36db7
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:28 +0800

    [Removing zone-nyan versions: 20170818.1644,20170818.944]

--- elpa/zone-nyan-20170818.944/zone-nyan-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; zone-nyan-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "zone-nyan" "zone-nyan.el" (22955 53062 509068
-;;;;;;  675000))
-;;; Generated autoloads from zone-nyan.el
-
-(autoload 'zone-nyan "zone-nyan" "\
-Zone out with nyan cat!
-
-\(fn)" nil nil)
-
-(autoload 'zone-nyan-preview "zone-nyan" "\
-Preview the `zone-nyan' zone program.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; zone-nyan-autoloads.el ends here
--- elpa/zone-nyan-20170818.944/zone-nyan-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zone-nyan" "20170818.944" "Zone out with nyan cat" '((esxml "0.3.1")) :commit "4b1f8d95f130946718d52806489ffe2762aebfdc" :url "https://github.com/wasamasa/zone-nyan" :keywords '("zone"))
