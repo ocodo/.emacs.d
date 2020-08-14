62781412b50e1fe8982cddf27a16706020e24297
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:24 +0800

    [Removing counsel-codesearch version: 20180527.728]

--- elpa/counsel-codesearch-20180527.728/counsel-codesearch-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; counsel-codesearch-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "counsel-codesearch" "counsel-codesearch.el"
-;;;;;;  (23360 14662 437076 644000))
-;;; Generated autoloads from counsel-codesearch.el
-
-(autoload 'counsel-codesearch "counsel-codesearch" "\
-Call the \"csearch\" shell command.
-
-INITIAL-INPUT can be given as the initial minibuffer input.
-
-\(fn &optional INITIAL-INPUT)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; counsel-codesearch-autoloads.el ends here
