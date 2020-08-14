d5bceb1182e384db969820fd0fe2f4cd3e92d867
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:15 +0800

    [Removing helm-robe version: 20151208.1955]

--- elpa/helm-robe-20151208.1955/helm-robe-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; helm-robe-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "helm-robe" "helm-robe.el" (22120 11694 532857
-;;;;;;  106000))
-;;; Generated autoloads from helm-robe.el
-
-(autoload 'helm-robe-completing-read "helm-robe" "\
-
-
-\(fn PROMPT CHOICES &optional PREDICATE REQUIRE-MATCH &rest ARGS)" nil nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-robe-autoloads.el ends here
--- elpa/helm-robe-20151208.1955/helm-robe-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-robe" "20151208.1955" "completing read function for robe" '((helm "1.7.7")) :url "https://github.com/syohex/emacs-helm-robe")
