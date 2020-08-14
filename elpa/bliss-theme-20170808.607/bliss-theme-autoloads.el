d70d6b7b93d43ff8069fa86e286dc2d90f2165cc
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:39 +0800

    [Removing bliss-theme version: 20170808.607]

--- elpa/bliss-theme-20170808.607/bliss-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; bliss-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "bliss-theme" "bliss-theme.el" (23064 61904
-;;;;;;  46648 882000))
-;;; Generated autoloads from bliss-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; bliss-theme-autoloads.el ends here
