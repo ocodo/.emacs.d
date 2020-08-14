ef76da0e5ce8bc316f32f983c6da3e502030044d
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:35 +0800

    [Removing org-projectile versions: 20171022.950,20180531.1942,20180601.242]

--- elpa/org-projectile-20171022.950/org-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-projectile" "20171022.950" "Repository todo management for org-mode" '((projectile "0.11.0") (dash "2.10.0") (emacs "24") (s "1.9.0") (org-category-capture "0.0.0")) :commit "8cec702f602b18da90b3d6207888a887b8e07750" :url "https://github.com/IvanMalison/org-projectile" :keywords '("org-mode" "projectile" "todo" "tools" "outlines"))
