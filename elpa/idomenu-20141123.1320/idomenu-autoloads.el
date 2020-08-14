952766cc515347cd6258140aa51fd54af674d207
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:50 +0800

    [Removing idomenu version: 20141123.1320]

--- elpa/idomenu-20141123.1320/idomenu-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; idomenu-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "idomenu" "idomenu.el" (21621 17243 797235
-;;;;;;  56000))
-;;; Generated autoloads from idomenu.el
-
-(autoload 'idomenu "idomenu" "\
-Switch to a buffer-local tag from Imenu via Ido.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; idomenu-autoloads.el ends here
--- elpa/idomenu-20141123.1320/idomenu-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "idomenu" "20141123.1320" "imenu tag selection a la ido" 'nil)
