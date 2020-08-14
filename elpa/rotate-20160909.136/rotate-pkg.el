894e147f80f7a308a4e6074dab230f4bfa30d974
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:13 +0800

    [Removing rotate version: 20160909.136]

--- elpa/rotate-20160909.136/rotate-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rotate" "20160909.136" "Rotate the layout of emacs" 'nil :url "https://github.com/daichirata/emacs-rotate" :keywords '("window" "layout"))
