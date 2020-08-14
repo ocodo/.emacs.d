80d0b28eb1d7f26994ed4aa7e2da67c1d5e00e06
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:21 +0800

    [Removing hemisu-theme version: 20130508.1144]

--- elpa/hemisu-theme-20130508.1144/hemisu-theme-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; hemisu-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "hemisu-theme" "hemisu-theme.el" (21783 34338
-;;;;;;  271401 0))
-;;; Generated autoloads from hemisu-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("hemisu-dark-theme.el" "hemisu-light-theme.el"
-;;;;;;  "hemisu-theme-pkg.el") (21783 34338 289030 93000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; hemisu-theme-autoloads.el ends here
--- elpa/hemisu-theme-20130508.1144/hemisu-theme-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "hemisu-theme" "20130508.1144" "Hemisu for Emacs." 'nil :url "http://github/anrzejsliwa/django-theme")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
