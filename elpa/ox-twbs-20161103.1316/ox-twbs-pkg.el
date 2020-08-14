7431f6b24db3e53b4ffd06febf022b6ce814133c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:39 +0800

    [Removing ox-twbs versions: 20161103.1316,20161103.2016]

--- elpa/ox-twbs-20161103.1316/ox-twbs-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ox-twbs" "20161103.1316" "Bootstrap compatible HTML Back-End for Org" 'nil :url "https://github.com/marsmining/ox-twbs" :keywords '("org" "html" "publish" "twitter" "bootstrap"))
