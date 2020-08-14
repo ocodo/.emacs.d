88f41134efd420aec02b35c4d5d646a64e0f1186
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:39 +0800

    [Removing package-build versions: 20180414.938,20180623.2235,20181126.220]

--- elpa/package-build-20180414.938/package-build-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "package-build" "20180414.938" "Tools for assembling a package archive"
-  '((cl-lib "0.5"))
-  :keywords
-  '("tools"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/package-build-20180623.2235/package-build-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "package-build" "20180623.2235" "Tools for assembling a package archive"
-  '((cl-lib "0.5"))
-  :keywords
-  '("tools"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
