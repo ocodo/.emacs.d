7f8d3f91688d6c43e7e72f2e978d55fecbaeb39b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:41 +0800

    [Removing ssass-mode versions: 20180428.1339,20180428.2039]

--- elpa/ssass-mode-20180428.1339/ssass-mode-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; ssass-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "ssass-mode" "ssass-mode.el" (23278 48032 0
-;;;;;;  0))
-;;; Generated autoloads from ssass-mode.el
-
-(autoload 'ssass-mode "ssass-mode" "\
-Major mode for Sass
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ssass-mode-autoloads.el ends here
--- elpa/ssass-mode-20180428.1339/ssass-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ssass-mode" "20180428.1339" "Edit Sass without a Turing Machine" '((emacs "24.3")) :commit "da82ebb2aa7e4999c23547270d2b0b2cd9311a47" :keywords '("languages" "sass") :url "http://github.com/AdamNiederer/ssass-mode")
