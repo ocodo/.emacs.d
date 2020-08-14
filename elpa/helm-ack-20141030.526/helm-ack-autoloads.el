20f92e0c5d5436b3d1e7210324e67ebe26834743
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:45 +0800

    [Removing helm-ack version: 20141030.526]

--- elpa/helm-ack-20141030.526/helm-ack-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; helm-ack-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "helm-ack" "helm-ack.el" (21588 26106 0 0))
-;;; Generated autoloads from helm-ack.el
-
-(autoload 'helm-ack-pop-stack "helm-ack" "\
-
-
-\(fn)" t nil)
-
-(autoload 'helm-ack "helm-ack" "\
-
-
-\(fn &optional DIR)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-ack-autoloads.el ends here
--- elpa/helm-ack-20141030.526/helm-ack-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-ack" "20141030.526" "Ack command with helm interface" '((helm "1.0") (cl-lib "0.5")) :url "https://github.com/syohex/emacs-helm-ack")
