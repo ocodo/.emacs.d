0daf91507c2ec4858e88ebe095ed3e35000a9581
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:10 +0800

    [Removing jenkins-watch version: 20121004.1726]

--- elpa/jenkins-watch-20121004.1726/jenkins-watch-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; jenkins-watch-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("jenkins-watch-pkg.el" "jenkins-watch.el")
-;;;;;;  (21011 19988 971685 0))
-
-;;;***
-
-(provide 'jenkins-watch-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; jenkins-watch-autoloads.el ends here
--- elpa/jenkins-watch-20121004.1726/jenkins-watch-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "jenkins-watch" "20121004.1726" "Watch continuous integration build status [github]" (quote nil))
