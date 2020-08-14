2715ad14ce1e3c5e7b1c5a3b41f4eb0aced177df
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:14 +0800

    [Removing go-playground-cli version: 20160503.214]

--- elpa/go-playground-cli-20160503.214/go-playground-cli-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-playground-cli" "20160503.214" "Go Playground client tool" '((emacs "24") (request "0.2.0") (deferred "0.3.2") (names "20151201.404") (s "1.10.0") (f "0.17.2") (let-alist "1.0.4") (cl-lib "0.5")) :url "https://github.com/kosh04/go-playground-cli")
