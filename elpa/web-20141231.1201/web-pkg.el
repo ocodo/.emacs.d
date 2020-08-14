c6a2b5f633f76e506aed66bfc285c9c683e1a95e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:13 +0800

    [Removing web version: 20141231.1201]

--- elpa/web-20141231.1201/web-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web" "20141231.1201" "useful HTTP client" '((dash "2.9.0") (s "1.5.0")) :url "http://github.com/nicferrier/emacs-web" :keywords '("lisp" "http" "hypermedia"))
