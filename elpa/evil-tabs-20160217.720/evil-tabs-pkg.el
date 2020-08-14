57abeb5b72652e7429c525bdc8e58525d346f7f1
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:58 +0800

    [Removing evil-tabs version: 20160217.720]

--- elpa/evil-tabs-20160217.720/evil-tabs-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-tabs" "20160217.720" "Integrating Vim-style tabs for Evil mode users." '((evil "0.0.0") (elscreen "0.0.0")) :url "https://github.com/krisajenkins/evil-tabs" :keywords '("evil" "tab" "tabs" "vim"))
