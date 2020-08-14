463e71fa39c17a6a86bc7f0937fc93aeb4a9ae5e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:52 +0800

    [Removing plain-theme version: 20171123.2010]

--- elpa/plain-theme-20171123.2010/plain-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "plain-theme" "20171123.2010" "Plain theme without syntax highlighting" '((emacs "24")) :commit "2609a811335d58cfb73a65d6307c156fe09037d3")
