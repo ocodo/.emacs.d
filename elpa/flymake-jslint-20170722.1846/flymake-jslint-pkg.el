c378c9f1c8d12750801eb3b6f6cc68093e42ec3c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:35 +0800

    [Removing flymake-jslint version: 20170722.1846]

--- elpa/flymake-jslint-20170722.1846/flymake-jslint-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-jslint" "20170722.1846" "A flymake handler for javascript using jslint" '((flymake-easy "0.1")) :commit "8edb82be605542b0ef62d38d818adcdde335eecb" :url "https://github.com/purcell/flymake-jslint")
