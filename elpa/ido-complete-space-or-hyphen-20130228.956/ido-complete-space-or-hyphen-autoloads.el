01dd23cdccbf6c21326207ef0365e44e84b1bae1
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:41 +0800

    [Removing ido-complete-space-or-hyphen version: 20130228.956]

--- elpa/ido-complete-space-or-hyphen-20130228.956/ido-complete-space-or-hyphen-autoloads.el
+++
@@ -1,35 +0,0 @@
-;;; ido-complete-space-or-hyphen-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (ido-complete-space-or-hyphen-disable ido-complete-space-or-hyphen-enable)
-;;;;;;  "ido-complete-space-or-hyphen" "ido-complete-space-or-hyphen.el"
-;;;;;;  (20799 54526 0 0))
-;;; Generated autoloads from ido-complete-space-or-hyphen.el
-
-(autoload 'ido-complete-space-or-hyphen-enable "ido-complete-space-or-hyphen" "\
-Enable ido-complete-space-or-hyphen
-
-\(fn)" t nil)
-
-(autoload 'ido-complete-space-or-hyphen-disable "ido-complete-space-or-hyphen" "\
-Disable ido-complete-space-or-hyphen
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("ido-complete-space-or-hyphen-pkg.el")
-;;;;;;  (20799 54526 755783 0))
-
-;;;***
-
-(provide 'ido-complete-space-or-hyphen-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ido-complete-space-or-hyphen-autoloads.el ends here
--- elpa/ido-complete-space-or-hyphen-20130228.956/ido-complete-space-or-hyphen-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ido-complete-space-or-hyphen" "20130228.956" "Complete SPACE or HYPHEN when type SPACE in ido [source: github]" (quote nil))
