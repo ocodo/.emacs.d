65811c2ee924c738865e15faa52413dcaaa3d1dc
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:41 +0800

    [Removing bpr version: 20180220.1044]

--- elpa/bpr-20180220.1044/bpr-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; bpr-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "bpr" "bpr.el" (23278 48438 0 0))
-;;; Generated autoloads from bpr.el
-
-(autoload 'bpr-spawn "bpr" "\
-Executes string CMD asynchronously in background.
-
-\(fn CMD)" t nil)
-
-(autoload 'bpr-open-last-buffer "bpr" "\
-Opens the buffer of the last spawned process.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("bpr-pkg.el" "test-bpr.el") (23278 48438
-;;;;;;  0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; bpr-autoloads.el ends here
--- elpa/bpr-20180220.1044/bpr-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "bpr" "20180220.1044" "Background Process Runner"
-  '((emacs "24"))
-  :keywords
-  '("background" "async" "process" "management")
-  :url "https://github.com/ilya-babanov/emacs-bpr")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
