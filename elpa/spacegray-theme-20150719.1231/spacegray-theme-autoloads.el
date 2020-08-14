656d57115722e3df69f5f05b8db9eeca8561dcd3
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:38 +0800

    [Removing spacegray-theme version: 20150719.1231]

--- elpa/spacegray-theme-20150719.1231/spacegray-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; spacegray-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "spacegray-theme" "spacegray-theme.el" (21937
-;;;;;;  40976 687401 0))
-;;; Generated autoloads from spacegray-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; spacegray-theme-autoloads.el ends here
--- elpa/spacegray-theme-20150719.1231/spacegray-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "spacegray-theme" "20150719.1231" "A Hyperminimal UI Theme" '((emacs "24.1")) :url "http://github.com/bruce/emacs-spacegray-theme" :keywords '("themes"))
