9d4b31c4aac6054fe3af9246c597baed9c78a144
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:05 +0800

    [Adding reformatter version: 20200813.342]

---
+++ elpa/reformatter-20200813.342/reformatter-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/reformatter-20200813.342/reformatter.el  -*- no-byte-compile: t -*-
+(define-package "reformatter" "20200813.342" "Define commands which run reformatters on the current buffer" '((emacs "24.3")) :commit "9d277200c16a3c6160ce1825b3024c3ef8caf2fd" :keywords '("convenience" "tools") :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/reformatter.el")
