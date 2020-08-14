93c296c191db21496a62784c16153b674a29e92d
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:07 +0800

    [Removing jasminejs-mode version: 20150526.1705]

--- elpa/jasminejs-mode-20150526.1705/jasminejs-mode-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; jasminejs-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "jasminejs-mode" "jasminejs-mode.el" (22089
-;;;;;;  55018 0 0))
-;;; Generated autoloads from jasminejs-mode.el
-
-(autoload 'jasminejs-mode "jasminejs-mode" "\
-To better edit your files
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("jasminejs-mode-pkg.el") (22089 55018
-;;;;;;  519422 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; jasminejs-mode-autoloads.el ends here
