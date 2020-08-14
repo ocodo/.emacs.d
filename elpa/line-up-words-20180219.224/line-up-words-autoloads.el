bf22f998e1fe9a7e91c4fe22dcbe6d89bb31d2ed
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:41 +0800

    [Removing line-up-words version: 20180219.224]

--- elpa/line-up-words-20180219.224/line-up-words-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; line-up-words-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "line-up-words" "line-up-words.el" (23360 14586
-;;;;;;  103258 838000))
-;;; Generated autoloads from line-up-words.el
-
-(autoload 'line-up-words "line-up-words" "\
-Try to align words in the region in an intelligent way.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; line-up-words-autoloads.el ends here
--- elpa/line-up-words-20180219.224/line-up-words-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "line-up-words" "20180219.224" "Align words in an intelligent way" 'nil)
