4c26dedd599436ae3733c04a02ff2890c1b70536
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:09 +0800

    [Removing md-readme versions: 20160811.1646,20160811.946]

--- elpa/md-readme-20160811.946/md-readme-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; md-readme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("md-readme-pkg.el" "md-readme.el") (22452
-;;;;;;  31673 916660 152000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; md-readme-autoloads.el ends here
--- elpa/md-readme-20160811.946/md-readme-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "md-readme" "20160811.946" "Markdown-formatted READMEs for your ELisp" 'nil :url "http://github.com/thomas11/md-readme/tree/master" :keywords
-  '("lisp" "help" "readme" "markdown" "header" "documentation" "github"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
