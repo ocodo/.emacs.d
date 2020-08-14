752a9738dba79c2c1ad887f9b20cb98e4ef43f68
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:04 +0800

    [Updating ivy-todo version: 20200323.2005][removing old versions: 20171208.1609,20171208.809]

---
+++ elpa/ivy-todo-20200323.2005/ivy-todo-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/ivy-todo-20200323.2005/ivy-todo.el  -*- no-byte-compile: t -*-
+(define-package "ivy-todo" "20200323.2005" "Manage org-mode TODOs with ivy" '((ivy "0.8.0") (emacs "25")) :commit "d74501cd334b7d709659946c5e02b21cfd5507de" :keywords '("convenience") :authors '(("Erik Sjöstrand" . "sjostrand.erik@gmail.com")) :maintainer '("Erik Sjöstrand" . "sjostrand.erik@gmail.com") :url "https://github.com/Kungsgeten/ivy-todo")
--- elpa/ivy-todo-20200323.2005/ivy-todo.el
+++ elpa/ivy-todo-20200323.2005/ivy-todo.el
@@ -1,19 +1,20 @@
 ;;; ivy-todo.el --- Manage org-mode TODOs with ivy   -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2017  Erik Sjöstrand
+;; Copyright (C) 2017--2020  Erik Sjöstrand
 ;; MIT License
 
 ;; Author: Erik Sjöstrand <sjostrand.erik@gmail.com>
-;; URL: http://github.com/Kungsgeten/ivy-todo
-;; Package-Version: 20171208.809
+;; URL: https://github.com/Kungsgeten/ivy-todo
+;; Package-Version: 20200323.2005
+;; Package-Commit: d74501cd334b7d709659946c5e02b21cfd5507de
 ;; Version: 1.00
 ;; Keywords: convenience
-;; Package-Requires: ((ivy "0.8.0") (emacs "24.3"))
+;; Package-Requires: ((ivy "0.8.0") (emacs "25"))
 
 ;;; Commentary:
 
 ;; The file `ivy-todo-file' should be an `org-mode' file containing TODO lists.
-;; These lists can be modified using `ivy-todo'. The last used TODO list while
+;; These lists can be modified using `ivy-todo'. The last used TODO list will
 ;; be buffer-locally saved. ivy-todo will try to guess the TODO list (based on
 ;; the project you're working on) if `ivy-todo-guess-list' is t.
 
@@ -25,10 +26,9 @@
   "The `org-mode' file upon which ivy-todo operates.
 Level 1 headlines are recognized as TODO lists.")
 
-(defvar ivy-todo-headline nil
+(defvar-local ivy-todo-headline nil
   "The level 1 headline in `ivy-todo-file` which contain the active TODO-list.
 Normally use `ivy-todo--buffer-headline-name' instead of accessing this variable.")
-(make-variable-buffer-local 'ivy-todo-headline)
 
 (defvar ivy-todo-guess-list t
   "Whether to guess TODO list based on the current project.")
@@ -41,29 +41,27 @@ If nil, no tags will be applied.")
   "Return an list of level 1 headlines in `ivy-todo-file'."
   (org-element-map (ivy-todo--ast) 'headline
     (lambda (h1)
-      (when (= (org-element-property :level h1) 1)
-        (org-element-property :raw-value h1)))))
+      (and (= (org-element-property :level h1) 1)
+           (org-element-property :raw-value h1)))))
 
 (defun ivy-todo--get-headline (headline)
-  "Return HEADLINE if HEADLINE exists in `ivy-todo-file'.
-Ask if the user want to add it if it doesn't exist."
+  "Return HEADLINE and its position if HEADLINE exists in `ivy-todo-file'.
+If it doesn't exist, create it."
   (when headline
     (or
      (org-element-map (ivy-todo--ast) 'headline
        (lambda (h1)
-         (when (and (= (org-element-property :level h1) 1)
-                    (equal (org-element-property :raw-value h1) headline))
-           (cons headline (org-element-property :begin h1))))
+         (and (= (org-element-property :level h1) 1)
+              (equal (org-element-property :raw-value h1) headline)
+              (cons headline (org-element-property :begin h1))))
        nil t)
-     (if (y-or-n-p (concat headline " doesn't exist. Would you like to created it?"))
-         (progn
-           (ivy-todo--replace-ast
-            (org-element-adopt-elements
-             (ivy-todo--ast)
-             (org-element-create 'headline `(:level 1 ,:title (,headline)
-                                                    :tags ,ivy-todo-default-tags))))
-           (ivy-todo--get-headline headline))
-       nil))))
+     (progn
+       (ivy-todo--replace-ast
+        (org-element-adopt-elements
+            (ivy-todo--ast)
+          (org-element-create 'headline `(:level 1 ,:title (,headline)
+                                          :tags ,ivy-todo-default-tags))))
+       (ivy-todo--get-headline headline)))))
 
 (defun ivy-todo--guess-headline-name ()
   "Guess the headline name associated with the current buffer.
@@ -71,64 +69,54 @@ Return nil if no headline is found, else a string."
   (cond ((and (require 'projectile nil t)
               (projectile-project-name)
               (not (equal (projectile-project-name) "-")))
-         (ivy-todo--get-headline (projectile-project-name)))
+         (projectile-project-name))
         ((require 'find-file-in-project nil t)
-         (ivy-todo--get-headline (file-name-base (directory-file-name (ffip-get-project-root-directory)))))
+         (file-name-base (directory-file-name (ffip-get-project-root-directory))))
         ((and (version<= "25" emacs-version)
               (require 'vc)
               (vc-root-dir))
-         (ivy-todo--get-headline (file-name-base (directory-file-name (vc-root-dir)))))
+         (file-name-base (directory-file-name (vc-root-dir))))
         (t nil)))
 
 (defun ivy-todo--buffer-headline-name ()
   "Get the name of the headline associated with the current buffer.
 Set `ivy-todo-headline` to the headline name."
-  (if ivy-todo-headline
-      ivy-todo-headline
-    (setq ivy-todo-headline
-          (let ((headlines (ivy-todo--headlines)))
-            (if ivy-todo-guess-list
-                (or (ivy-todo--guess-headline-name)
-                    (ivy-todo--get-headline (completing-read "TODO list: " headlines)))
-              (ivy-todo--get-headline (completing-read "TODO list: " headlines)))))))
+  (or ivy-todo-headline
+      (setq ivy-todo-headline
+            (ivy-todo--get-headline
+             (completing-read "TODO list: " (ivy-todo--headlines)
+                              nil nil (if ivy-todo-guess-list
+                                          (ivy-todo--guess-headline-name)))))))
 
 (defun ivy-todo--list-items ()
   "Return alist of todo items of `ivy-todo-headline'.
 The car is the name and the cdr is the position in `ivy-todo-file'."
-  (org-element-map (ivy-todo--ast) 'headline
-    (lambda (h1)
-      (when (and (= (org-element-property :level h1) 1)
-                 (string-equal (car (ivy-todo--buffer-headline-name))
-                               (org-element-property :raw-value h1)))
-        (org-element-map (org-element-contents h1) 'headline
-          (lambda (todo-item)
-            (when (org-element-property :todo-type todo-item)
-              (cons (car (split-string (org-element-interpret-data todo-item) "\n"))
-                    (org-element-property :begin todo-item)))))))
-    nil t))
+  (or
+   (org-element-map (ivy-todo--ast) 'headline
+     (lambda (h1)
+       (when (and (= (org-element-property :level h1) 1)
+                  (string-equal (car (ivy-todo--buffer-headline-name))
+                                (org-element-property :raw-value h1)))
+         (org-element-map (org-element-contents h1) 'headline
+           (lambda (todo-item)
+             (when (org-element-property :todo-type todo-item)
+               (cons (car (split-string (org-element-interpret-data todo-item) "\n"))
+                     (org-element-property :begin todo-item)))))))
+     nil t)
+   (and (ivy-todo--buffer-headline-name) nil)))
 
 (defun ivy-todo--ast ()
   "Get the AST for the TODO file."
   (delay-mode-hooks
-    (with-current-buffer (find-file-noselect ivy-todo-file)
+    (with-temp-buffer
+      (insert-file-contents ivy-todo-file)
+      (org-mode)
       (org-element-parse-buffer))))
 
 (defun ivy-todo--replace-ast (ast)
   "Replace AST of `ivy-todo-file' and save the file."
-  (with-temp-buffer
-    (insert (org-element-interpret-data ast))
-    (write-file ivy-todo-file)))
-
-(defun ivy-todo--old-or-new-item (item pos)
-  "If ITEM exist in `ivy-todo-file' goto its position, else insert it after POS.
-Meant to be used with `ivy-todo-file' as `current-buffer'."
-  (if (stringp item)
-      (progn
-        (goto-char pos)
-        (goto-char (line-end-position))
-        (org-insert-todo-subheading 1)
-        (insert item))
-    (goto-char (cdr item))))
+  (with-temp-file ivy-todo-file
+    (insert (org-element-interpret-data ast))))
 
 ;;;###autoload
 (defun ivy-todo (&optional arg)
@@ -142,24 +130,39 @@ With a `\\[universal-argument] \\[universal-argument]' ARG, change `ivy-todo-fil
   (when (> arg 1)
     (setq ivy-todo-headline
           (ivy-todo--get-headline (completing-read "TODO list: " (ivy-todo--headlines)))))
+  (when ivy-todo-headline
+    (setq ivy-todo-headline (ivy-todo--get-headline (car ivy-todo-headline))))
   (let ((items (ivy-todo--list-items))
         (pos (cdr ivy-todo-headline)))
     (ivy-read
      (concat "\"" (car ivy-todo-headline) "\" items: ")
      items
      :action
-      (lambda (x)
-        (delay-mode-hooks
-          (with-current-buffer (find-file-noselect ivy-todo-file)
-            (if (stringp x)
-                (progn
-                  (goto-char pos)
-                  (goto-char (line-end-position))
-                  (org-insert-todo-subheading 1)
-                  (insert x))
-              (goto-char (cdr x))
-              (org-todo))
-            (save-buffer)))))))
+     (lambda (x)
+       (delay-mode-hooks
+         (with-current-buffer (find-file-noselect ivy-todo-file)
+           (if (stringp x)
+               (progn
+                 (goto-char pos)
+                 (goto-char (line-end-position))
+                 (org-insert-todo-subheading 1)
+                 (insert x))
+             (goto-char (cdr x))
+             (org-todo))
+           (save-buffer)))))))
+
+;;; Ivy Actions
+
+(defun ivy-todo--old-or-new-item (item pos)
+  "If ITEM exist in `ivy-todo-file' goto its position, else insert it after POS.
+Meant to be used with `ivy-todo-file' as `current-buffer'."
+  (if (stringp item)
+      (progn
+        (goto-char pos)
+        (goto-char (line-end-position))
+        (org-insert-todo-subheading 1)
+        (insert item))
+    (goto-char (cdr item))))
 
 (defun ivy-todo-archive (headline)
   "Goto HEADLINE in `ivy-todo-file' and archive it.
@@ -198,7 +201,7 @@ HEADLINE is a string or a cons (\"headline\" . buffer-pos)."
     (delay-mode-hooks
       (with-current-buffer (find-file-noselect ivy-todo-file)
         (ivy-todo--old-or-new-item headline headline-pos)
-        (org-set-tags)
+        (org-set-tags-command)
         (save-buffer)))))
 
 (defun ivy-todo-set-effort (headline)
