306514b3d7ab4e5326a776f6928136dfecfecc47
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:35 +0800

    [Removing basic-mode version: 20171204.1217]

--- elpa/basic-mode-20171204.1217/basic-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "basic-mode" "20171204.1217" "major mode for editing BASIC code" '((seq "2.20") (emacs "24.3")) :commit "b7e851f844e9a5264e44936d1675133b4c3ed39c" :url "https://github.com/dykstrom/basic-mode" :keywords '("basic" "languages"))
