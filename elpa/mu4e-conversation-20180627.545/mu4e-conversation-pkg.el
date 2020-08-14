ddfdaf8c0e57e8f3926fbe409e40f4ad04a387f6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:15 +0800

    [Removing mu4e-conversation versions: 20180627.545,20181203.2200]

--- elpa/mu4e-conversation-20180627.545/mu4e-conversation-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mu4e-conversation" "20180627.545" "Show a complete thread in a single buffer" '((emacs "25.1")))
--- elpa/mu4e-conversation-20181203.2200/mu4e-conversation-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mu4e-conversation" "20181203.2200" "Show a complete thread in a single buffer" '((emacs "25.1")) :commit "ae5ad4beed8aa69d1400f6f733f84f440ccfc1a7" :keywords '("mail" "convenience" "mu4e") :authors '(("Pierre Neidhardt" . "mail@ambrevar.xyz")) :maintainer '("Pierre Neidhardt" . "mail@ambrevar.xyz") :url "https://gitlab.com/Ambrevar/mu4e-conversation")
