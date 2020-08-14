7f85bead03186e4d18026a7dc10ea4e6a55434ab
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:59 +0800

    [Removing clojure-cheatsheet version: 20180201.4]

--- elpa/clojure-cheatsheet-20180201.4/clojure-cheatsheet-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; clojure-cheatsheet-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "clojure-cheatsheet" "clojure-cheatsheet.el"
-;;;;;;  (23360 14664 825133 579000))
-;;; Generated autoloads from clojure-cheatsheet.el
-
-(autoload 'clojure-cheatsheet "clojure-cheatsheet" "\
-Use helm to show a Clojure cheatsheet.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; clojure-cheatsheet-autoloads.el ends here
--- elpa/clojure-cheatsheet-20180201.4/clojure-cheatsheet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "clojure-cheatsheet" "20180201.4" "The Clojure Cheatsheet for Emacs" '((helm "1.7.7") (cider "0.9.0")))
