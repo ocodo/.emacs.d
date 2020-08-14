52bb8a020774bdf4e866d7137b5674c64062c0c6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:48 +0800

    [Removing swiper versions: 20180402.1857,20180615.1400,20181120.1832]

--- elpa/swiper-20180402.1857/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20180402.1857" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "b11deda9b8fe6d3959807f1224fa720741ab8eb3" :keywords '("matching") :url "https://github.com/abo-abo/swiper")
