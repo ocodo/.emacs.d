9fe6503b36645e74ed020e103002da016fef6316
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:27 +0800

    [Removing yoshi-theme versions: 20170330.0,20170330.700]

--- elpa/yoshi-theme-20170330.0/yoshi-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yoshi-theme" "20170330.0" "Theme named after my cat" 'nil :url "http://projects.ryuslash.org/yoshi-theme/" :keywords '("faces"))
--- elpa/yoshi-theme-20170330.700/yoshi-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yoshi-theme" "20170330.700" "Theme named after my cat" 'nil :commit "eab4fb67e8fec47c25c4c1393ac1211f043dbd44" :keywords '("faces") :authors '(("Tom Willemse" . "tom@ryuslash.org")) :maintainer '("Tom Willemse" . "tom@ryuslash.org") :url "http://projects.ryuslash.org/yoshi-theme/")
