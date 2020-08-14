32a55c026f1b615dfb7e2489e9c67873569b66b2
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:32 +0800

    [Removing operate-on-number version: 20150706.2323]

--- elpa/operate-on-number-20150706.2323/operate-on-number-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "operate-on-number" "20150706.2323" "Operate on number at point with arithmetic functions" 'nil :url "https://github.com/knu/operate-on-number.el" :keywords '("editing"))
