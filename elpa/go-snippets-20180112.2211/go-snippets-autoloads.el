00fd8c7ec6bedc35f9d60ea408a1c9f496852cbe
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:17 +0800

    [Removing go-snippets version: 20180112.2211]

--- elpa/go-snippets-20180112.2211/go-snippets-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; go-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "go-snippets" "go-snippets.el" (23138 48621
-;;;;;;  574252 649000))
-;;; Generated autoloads from go-snippets.el
-
-(autoload 'go-snippets-initialize "go-snippets" "\
-
-
-\(fn)" nil nil)
-
-(eval-after-load 'yasnippet '(go-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("go-snippets-pkg.el") (23138 48621 578252
-;;;;;;  654000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; go-snippets-autoloads.el ends here
--- elpa/go-snippets-20180112.2211/go-snippets-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "go-snippets" "20180112.2211" "Yasnippets for go"
-  '((yasnippet "0.8.0")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
