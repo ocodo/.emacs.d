1fe44c4803c63b72b4dc641f8faf02b39c137c85
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:05 +0800

    [Removing markdown-toc versions: 20170711.1249,20170711.1949]

--- elpa/markdown-toc-20170711.1949/markdown-toc-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "markdown-toc" "20170711.1949" "A simple TOC generator for markdown file"
-  '((s "1.9.0")
-    (dash "2.11.0")
-    (markdown-mode "2.1")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
