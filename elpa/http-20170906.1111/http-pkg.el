abbd0ff48b79d4b852237498389a3b6d751b7bb9
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:36 +0800

    [Removing http version: 20170906.1111]

--- elpa/http-20170906.1111/http-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "http" "20170906.1111" "Yet another HTTP client" '((emacs "24.4") (request "0.2.0") (edit-indirect "0.1.4")) :commit "193a7bf843dd6b6805c7b18dab31f50c8325d710" :url "https://github.com/emacs-pe/http.el" :keywords '("convenience"))
