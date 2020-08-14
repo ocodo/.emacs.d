868623ce9add00ddc9b7c6dece8fed220db71337
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:40 +0800

    [Removing flyparens version: 20140723.1146]

--- elpa/flyparens-20140723.1146/flyparens-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; flyparens-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "flyparens" "flyparens.el" (21757 46960 39401
-;;;;;;  0))
-;;; Generated autoloads from flyparens.el
-
-(autoload 'flyparens-mode "flyparens" "\
- FlyParens
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("flyparens-config-example-1.el" "flyparens-pkg.el")
-;;;;;;  (21757 46960 52229 215000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flyparens-autoloads.el ends here
--- elpa/flyparens-20140723.1146/flyparens-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "flyparens" "20140723.1146" "Check for unbalanced parens on the fly" 'nil :keywords
-  '("faces" "convenience" "lisp" "matching" "parentheses" "parens"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
