6b4b8ed4aabe936143ad3b1186771acb3ef0ad21
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:04 +0800

    [Removing markdown-mode+ version: 20170320.1404]

--- elpa/markdown-mode+-20170320.1404/markdown-mode+-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "markdown-mode+" "20170320.1404" "extra functions for markdown-mode"
-  '((markdown-mode "20111229"))
-  :url "http://github.com/milkypostman/markdown-mode-plus" :keywords
-  '("markdown" "latex" "osx" "rtf"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
