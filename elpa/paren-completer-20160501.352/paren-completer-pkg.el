6130baeb857568c5af50ac5cbcabdc8a4072bb04
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:41 +0800

    [Removing paren-completer version: 20160501.352]

--- elpa/paren-completer-20160501.352/paren-completer-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "paren-completer" "20160501.352" "Automatically, language agnostically, fill in delimiters." '((emacs "24.3")) :url "https://github.com/MatthewBregg/paren-completer" :keywords '("convenience"))
