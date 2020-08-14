2563731346bb2a7b494c4ed5910590ea12c19644
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:39 +0800

    [Removing handlebars-mode version: 20150211.949]

--- elpa/handlebars-mode-20150211.949/handlebars-mode-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; handlebars-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "handlebars-mode" "handlebars-mode.el" (21729
-;;;;;;  61872 959401 0))
-;;; Generated autoloads from handlebars-mode.el
-
-(autoload 'handlebars-mode "handlebars-mode" "\
-
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.handlebars$" . handlebars-mode))
-
-(add-to-list 'auto-mode-alist '("\\.hbs$" . handlebars-mode))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; handlebars-mode-autoloads.el ends here
--- elpa/handlebars-mode-20150211.949/handlebars-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "handlebars-mode" "20150211.949" "A major mode for editing Handlebars files." 'nil)
