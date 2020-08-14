342b4cd2b28521fe526bb5904a4e29170e0ca92a
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:56 +0800

    [Removing git-gutter-fringe+ version: 20140729.403]

--- elpa/git-gutter-fringe+-20140729.403/git-gutter-fringe+-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; git-gutter-fringe+-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("git-gutter-fringe+.el") (21464 21538
-;;;;;;  462906 107000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; git-gutter-fringe+-autoloads.el ends here
--- elpa/git-gutter-fringe+-20140729.403/git-gutter-fringe+-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "git-gutter-fringe+" "20140729.403" "Fringe version of git-gutter+.el" '((git-gutter+ "0.1") (fringe-helper "1.0.1")))
