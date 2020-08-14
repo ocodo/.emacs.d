f81286f4953cc26fceae99e55eb136f2c7013cec
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:49 +0800

    [Removing sws-mode version: 20150317.1245]

--- elpa/sws-mode-20150317.1245/sws-mode-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; sws-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "sws-mode" "sws-mode.el" (21771 53348 395401
-;;;;;;  0))
-;;; Generated autoloads from sws-mode.el
-
-(autoload 'sws-mode "sws-mode" "\
-Major mode for editing significant whitespace files
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; sws-mode-autoloads.el ends here
--- elpa/sws-mode-20150317.1245/sws-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "sws-mode" "20150317.1245" "(S)ignificant (W)hite(S)pace mode" 'nil :url "https://github.com/brianc/jade-mode")
