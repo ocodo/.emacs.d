70498e3d4eaa396a0c98ef295916245b9ab30463
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:29 +0800

    [Removing skewer-less version: 20160828.1321]

--- elpa/skewer-less-20160828.1321/skewer-less-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "skewer-less" "20160828.1321" "Skewer support for live LESS stylesheet updates" '((skewer-mode "1.5.3")) :keywords '("languages" "tools"))
