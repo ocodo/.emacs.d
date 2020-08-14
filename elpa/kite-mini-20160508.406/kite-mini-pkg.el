dee171cb22c6563dfc858ba6e6aa6ef5b2c93b4c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:30 +0800

    [Removing kite-mini version: 20160508.406]

--- elpa/kite-mini-20160508.406/kite-mini-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "kite-mini" "20160508.406" "Remotely evaluate JavaScript in the WebKit debugger"
-  '((dash "2.11.0")
-    (websocket "1.5"))
-  :url "https://github.com/tungd/kite-mini.el" :keywords
-  '("webkit"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
