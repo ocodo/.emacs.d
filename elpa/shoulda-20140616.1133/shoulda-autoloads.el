5835315fd91881e8dc9a71dfc4a15d26f7d5b142
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:26 +0800

    [Removing shoulda version: 20140616.1133]

--- elpa/shoulda-20140616.1133/shoulda-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; shoulda-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "shoulda" "shoulda.el" (21833 28630 263401
-;;;;;;  0))
-;;; Generated autoloads from shoulda.el
-
-(autoload 'shoulda-run-should-at-point "shoulda" "\
-Run Shoulda should test at point.
-
-\(fn)" t nil)
-
-(autoload 'shoulda-run-context-at-point "shoulda" "\
-Run Shoulda context test at point.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; shoulda-autoloads.el ends here
--- elpa/shoulda-20140616.1133/shoulda-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "shoulda" "20140616.1133" "Shoulda test support for ruby" '((cl-lib "0.5")) :keywords '("ruby" "tests" "shoulda"))
