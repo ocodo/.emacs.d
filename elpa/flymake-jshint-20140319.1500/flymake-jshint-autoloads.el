472982d0b04a331cc2a8ce47d79a110ca5a2eeb9
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:34 +0800

    [Removing flymake-jshint version: 20140319.1500]

--- elpa/flymake-jshint-20140319.1500/flymake-jshint-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; flymake-jshint-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (flymake-jshint-load) "flymake-jshint" "flymake-jshint.el"
-;;;;;;  (21304 53764 0 0))
-;;; Generated autoloads from flymake-jshint.el
-
-(autoload 'flymake-jshint-load "flymake-jshint" "\
-Configure flymake mode to check the current buffer's JavaScript syntax.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("flymake-jshint-pkg.el") (21304 53764
-;;;;;;  779696 0))
-
-;;;***
-
-(provide 'flymake-jshint-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; flymake-jshint-autoloads.el ends here
--- elpa/flymake-jshint-20140319.1500/flymake-jshint-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flymake-jshint" "20140319.1500" "making flymake work with JSHint" (quote ((flymake-easy "0.8"))))
