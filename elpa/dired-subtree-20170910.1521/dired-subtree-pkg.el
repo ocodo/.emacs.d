705c0f26adc033c4d6a7f830b347f152ecc9477f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:58 +0800

    [Removing dired-subtree version: 20170910.1521]

--- elpa/dired-subtree-20170910.1521/dired-subtree-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dired-subtree" "20170910.1521" "Insert subdirectories in a tree-like fashion" '((dash "2.5.0") (dired-hacks-utils "0.0.1")) :commit "7fbaaa2de73bd571de8eee86205f779340b13c69" :keywords '("files"))
