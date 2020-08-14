0c17e9e50feb4189d6173e1e56e4ccff56c79751
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:38 +0800

    [Removing ox-hugo versions: 20180423.937,20180620.1102,20181106.2350]

--- elpa/ox-hugo-20180620.1102/ox-hugo-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "ox-hugo" "20180620.1102" "Hugo Markdown Back-End for Org Export Engine"
-  '((emacs "24.4")
-    (org "9.0"))
-  :keywords
-  '("org" "markdown" "docs")
-  :url "https://ox-hugo.scripter.co")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/ox-hugo-20181106.2350/ox-hugo-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "ox-hugo" "20181106.2350" "Hugo Markdown Back-End for Org Export Engine"
-  '((emacs "24.4")
-    (org "9.0"))
-  :keywords
-  '("org" "markdown" "docs")
-  :url "https://ox-hugo.scripter.co")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
