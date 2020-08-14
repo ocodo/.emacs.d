f39379264e84e921d0b57565dde7e25f92e7f495
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:51 +0800

    [Removing pippel version: 20170730.623]

--- elpa/pippel-20170730.623/pippel-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "pippel" "20170730.623" "Frontend to python package manager pip"
-  '((emacs "25.1")
-    (s "1.11.0")
-    (dash "2.12.0"))
-  :url "https://github.com/brotzeitmacher/pippel")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
