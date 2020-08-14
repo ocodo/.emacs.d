3141b565e4598f078b8d83ab8a0af5ec428911da
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:42 +0800

    [Removing status version: 20151230.608]

--- elpa/status-20151230.608/status-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; status-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "status" "status.el" (22148 36054 661065 328000))
-;;; Generated autoloads from status.el
-
-(autoload 'status-new "status" "\
-Create a new status icon and return it.
-
-\(fn)" nil nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("emms-status.el" "erc-status.el" "status-pkg.el")
-;;;;;;  (22148 36054 680295 18000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; status-autoloads.el ends here
--- elpa/status-20151230.608/status-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "status" "20151230.608" "This package adds support for status icons to Emacs." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
