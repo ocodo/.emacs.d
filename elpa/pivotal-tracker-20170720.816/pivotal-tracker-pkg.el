767e2766ecb2538fa635010b3810bbf0ec9b563b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:51 +0800

    [Removing pivotal-tracker version: 20170720.816]

--- elpa/pivotal-tracker-20170720.816/pivotal-tracker-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pivotal-tracker" "20170720.816" "Interact with Pivotal Tracker through its API" 'nil :commit "0311d117037c74512149a4a78b269c2e46d7dfba" :url "http://github.com/jxa/pivotal-tracker")
