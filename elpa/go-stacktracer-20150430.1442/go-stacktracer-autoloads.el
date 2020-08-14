58bd2fa7e4ed8f5a234647c398a2a11ee9c80990
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:17 +0800

    [Removing go-stacktracer version: 20150430.1442]

--- elpa/go-stacktracer-20150430.1442/go-stacktracer-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; go-stacktracer-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "go-stacktracer" "go-stacktracer.el" (22474
-;;;;;;  20576 0 0))
-;;; Generated autoloads from go-stacktracer.el
-
-(autoload 'go-stacktracer-region "go-stacktracer" "\
-Parse a Go stacktrace from START to END.
-
-\(fn START END)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; go-stacktracer-autoloads.el ends here
--- elpa/go-stacktracer-20150430.1442/go-stacktracer-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-stacktracer" "20150430.1442" "parse Go stack traces" 'nil :url "https://github.com/samertm/go-stacktracer.el" :keywords '("tools"))
