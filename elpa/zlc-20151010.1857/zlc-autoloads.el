859fee8d46f5321779d910fbd54be9164a313dea
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:28 +0800

    [Removing zlc version: 20151010.1857]

--- elpa/zlc-20151010.1857/zlc-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; zlc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "zlc" "zlc.el" (22045 47803 557140 0))
-;;; Generated autoloads from zlc.el
-
-(autoload 'zlc-mode "zlc" "\
-Toggle zlc (zsh like completion) on or off.
-With ARG, turn zlc on if arg is positive, off otherwise.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; zlc-autoloads.el ends here
--- elpa/zlc-20151010.1857/zlc-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zlc" "20151010.1857" "Provides zsh like completion system to Emacs" 'nil :keywords '("matching" "convenience"))
