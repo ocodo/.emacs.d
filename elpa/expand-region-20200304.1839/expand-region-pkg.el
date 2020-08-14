3865fc9370b6c958b69e973f2047df05347fc48c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:03 +0800

    [Updating expand-region version: 20200304.1839][removing old versions: 20171217.2125,20180817.1134]

---
+++ elpa/expand-region-20200304.1839/expand-region-pkg.el
@@ -0,0 +1,4 @@
+(define-package "expand-region" "20200304.1839" "Increase selected region by semantic units." 'nil :commit "ea6b4cbb9985ddae532bd2faf9bb00570c9f2781")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
