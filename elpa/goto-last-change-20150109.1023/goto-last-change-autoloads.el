82ecb16da653b8e25b27891274d1e98e515d4f55
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:26 +0800

    [Removing goto-last-change version: 20150109.1023]

--- elpa/goto-last-change-20150109.1023/goto-last-change-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; goto-last-change-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "goto-last-change" "goto-last-change.el" (21818
-;;;;;;  58337 123401 0))
-;;; Generated autoloads from goto-last-change.el
-
-(autoload 'goto-last-change "goto-last-change" "\
-Set point to the position of the last change.
-Consecutive calls set point to the position of the previous change.
-With a prefix arg (optional arg MARK-POINT non-nil), set mark so \\[exchange-point-and-mark]
-will return point to the current position.
-
-\(fn &optional MARK-POINT MINIMAL-LINE-DISTANCE)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; goto-last-change-autoloads.el ends here
