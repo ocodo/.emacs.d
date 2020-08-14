6776aefac917f12fe6829309d122dc79228d5470
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:32 +0800

    [Removing groovy-imports version: 20161003.151]

--- elpa/groovy-imports-20161003.151/groovy-imports-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "groovy-imports" "20161003.151" "Code for dealing with Groovy imports" '((emacs "24.4") (s "1.10.0") (pcache "0.3.2")) :url "http://www.github.com/mbezjak/emacs-groovy-imports" :keywords '("groovy"))
