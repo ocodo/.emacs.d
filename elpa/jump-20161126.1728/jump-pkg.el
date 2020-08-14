395aded2e027b3def2eea13a76d5095220f56858
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:21 +0800

    [Removing jump version: 20161126.1728]

--- elpa/jump-20161126.1728/jump-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "jump" "20161126.1728" "build functions which contextually jump between files" '((findr "0.7") (inflections "2.4") (cl-lib "0.5")) :url "http://github.com/eschulte/jump.el" :keywords '("project" "convenience" "navigation"))
