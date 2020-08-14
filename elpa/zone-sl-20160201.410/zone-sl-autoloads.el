f78c62a98e59b9893d80e473a883a145f0d8fd83
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:29 +0800

    [Removing zone-sl version: 20160201.410]

--- elpa/zone-sl-20160201.410/zone-sl-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; zone-sl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "zone-sl" "zone-sl.el" (22200 34265 752435
-;;;;;;  186000))
-;;; Generated autoloads from zone-sl.el
-
-(autoload 'zone-pgm-sl "zone-sl" "\
-Zone out with steam locomotive.
-
-\(fn)" nil nil)
-
-(autoload 'zone-sl "zone-sl" "\
-Zone out with steame locomotive.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; zone-sl-autoloads.el ends here
--- elpa/zone-sl-20160201.410/zone-sl-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "zone-sl" "20160201.410" "Zone out with steam locomotives." '((emacs "24.3")) :url "https://github.com/kawabata/zone-sl" :keywords '("games"))
