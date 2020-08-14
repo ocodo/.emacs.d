fd5e511283034c905dd45ac61febf00cb7aed1c6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:49 +0800

    [Removing evil-lisp-state version: 20160403.1948]

--- elpa/evil-lisp-state-20160403.1948/evil-lisp-state-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; evil-lisp-state-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("evil-lisp-state.el") (22291 19242 409
-;;;;;;  999000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; evil-lisp-state-autoloads.el ends here
--- elpa/evil-lisp-state-20160403.1948/evil-lisp-state-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-lisp-state" "20160403.1948" "An evil state to edit Lisp code" '((evil "1.0.9") (bind-map "0") (smartparens "1.6.1")) :stars '(#("38" 0 2 (font-lock-face paradox-star-face))) :url "https://github.com/syl20bnr/evil-lisp-state" :keywords '("convenience" "editing" "evil" "smartparens" "lisp" "mnemonic"))
