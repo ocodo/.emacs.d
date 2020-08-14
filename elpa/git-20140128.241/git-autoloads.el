e2ff35e9386acfe0a39f9acd0b2e22b08e42983b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:54 +0800

    [Removing git version: 20140128.241]

--- elpa/git-20140128.241/git-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; git-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("git-pkg.el" "git.el") (21236 38965 816235
-;;;;;;  0))
-
-;;;***
-
-(provide 'git-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; git-autoloads.el ends here
