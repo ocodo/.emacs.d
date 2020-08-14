85ce76d1d437b2792687acc89ea80de32019951d
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:49 +0800

    [Removing literate-coffee-mode version: 20170211.715]

--- elpa/literate-coffee-mode-20170211.715/literate-coffee-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "literate-coffee-mode" "20170211.715" "major-mode for Literate CoffeeScript" '((coffee-mode "0.5.0")) :url "https://github.com/syohex/emacs-literate-coffee-mode")
