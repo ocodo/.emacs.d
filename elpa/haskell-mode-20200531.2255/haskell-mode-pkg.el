f1e04bbf6bc002ef14913b73054652400853f7bf
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:41 +0800

    [Updating haskell-mode version: 20200531.2255][removing old versions: 20180406.2222,20180601.143,20181122.823]

---
+++ elpa/haskell-mode-20200531.2255/haskell-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "haskell-mode" "20200531.2255" "A Haskell editing mode"
+  '((emacs "25.1"))
+  :commit "41683c0e634bb3f54eac8747919a82132e1714fe" :keywords
+  '("haskell" "cabal" "ghc" "repl")
+  :url "https://github.com/haskell/haskell-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
