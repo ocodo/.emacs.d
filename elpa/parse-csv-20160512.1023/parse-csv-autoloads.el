d17d759c4440fcef9c755a9b42ab540019720c96
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:43 +0800

    [Removing parse-csv version: 20160512.1023]

--- elpa/parse-csv-20160512.1023/parse-csv-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; parse-csv-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("parse-csv.el") (22325 5457 914465 810000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; parse-csv-autoloads.el ends here
--- elpa/parse-csv-20160512.1023/parse-csv-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "parse-csv" "20160512.1023" "Parse strings with CSV fields into s-expressions" 'nil :url "https://github.com/mrc/el-csv" :keywords '("csv"))
