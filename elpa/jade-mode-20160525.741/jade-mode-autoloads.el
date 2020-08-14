f2fc5ca39d9855d3f0fd7efe32bdaab6eba27a1d
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:07 +0800

    [Removing jade-mode version: 20160525.741]

--- elpa/jade-mode-20160525.741/jade-mode-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; jade-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "jade-mode" "jade-mode.el" (22347 49572 345188
-;;;;;;  101000))
-;;; Generated autoloads from jade-mode.el
-
-(autoload 'jade-mode "jade-mode" "\
-Major mode for editing jade node.js templates
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.jade\\'" . jade-mode))
-
-(add-to-list 'auto-mode-alist '("\\.pug\\'" . jade-mode))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; jade-mode-autoloads.el ends here
--- elpa/jade-mode-20160525.741/jade-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "jade-mode" "20160525.741" "Major mode for editing .jade files" 'nil :url "https://github.com/brianc/jade-mode")
