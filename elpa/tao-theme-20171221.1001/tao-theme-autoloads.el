6750003737230bd42167e6e0cec81539f678ab0a
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:51 +0800

    [Removing tao-theme versions: 20171221.1001,20181020.1726]

--- elpa/tao-theme-20171221.1001/tao-theme-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; tao-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "tao-yang-theme" "tao-yang-theme.el" (23138
-;;;;;;  47737 908310 523000))
-;;; Generated autoloads from tao-yang-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil "tao-yin-theme" "tao-yin-theme.el" (23138 47737
-;;;;;;  912310 538000))
-;;; Generated autoloads from tao-yin-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("tao-theme-pkg.el" "tao-theme.el") (23138
-;;;;;;  47737 912310 538000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; tao-theme-autoloads.el ends here
--- elpa/tao-theme-20181020.1726/tao-theme-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "tao-theme" "20181020.1726" "This package provides two parametrized uncoloured color themes for Emacs: tao-yin and tao-yang." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
