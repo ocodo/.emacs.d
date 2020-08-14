1801ee4df763005f2c07c151e20afece88175430
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:22 +0800

    [Removing goose-theme version: 20160828.545]

--- elpa/goose-theme-20160828.545/goose-theme-autoloads.el
+++
@@ -1,20 +0,0 @@
-;;; goose-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "goose-theme" "goose-theme.el" (0 0 0 0))
-;;; Generated autoloads from goose-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "goose-theme" '("goose")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; goose-theme-autoloads.el ends here
--- elpa/goose-theme-20160828.545/goose-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "goose-theme" "20160828.545" "A gray color theme" '((emacs "24.1")) :url "https://github.com/thwg/goose-theme")
