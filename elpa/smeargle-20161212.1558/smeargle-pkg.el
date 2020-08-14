283f2024d51e1d60337f7f3113ab530010d6ca5b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:35 +0800

    [Removing smeargle versions: 20161212.1558,20161212.2358]

--- elpa/smeargle-20161212.1558/smeargle-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smeargle" "20161212.1558" "Highlighting region by last updated time" '((emacs "24.3")) :url "https://github.com/syohex/emacs-smeargle")
