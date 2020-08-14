448007547cfe2f50b220ac9de7f1fd24af4d3033
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:35 +0800

    [Removing org-sync version: 20180221.1127]

--- elpa/org-sync-20180221.1127/org-sync-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; org-sync-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("org-sync-bb.el" "org-sync-github.el"
-;;;;;;  "org-sync-gitlab.el" "org-sync-pkg.el" "org-sync-redmine.el"
-;;;;;;  "org-sync-rtm.el" "org-sync.el") (23278 48111 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; org-sync-autoloads.el ends here
