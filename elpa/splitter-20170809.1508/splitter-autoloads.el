97f1201c0ee12390e1cb5186ce5e2b0898e1a9ef
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:40 +0800

    [Removing splitter version: 20170809.1508]

--- elpa/splitter-20170809.1508/splitter-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; splitter-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("splitter.el") (22955 53138 309221 389000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; splitter-autoloads.el ends here
