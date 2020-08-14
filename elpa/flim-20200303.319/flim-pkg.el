fd6bdae277f7e168c7fdfd4cbbb065e1227e17f9
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:17 +0800

    [Updating flim version: 20200303.319][removing old versions: 20180328.1624,20180328.2324]

---
+++ elpa/flim-20200303.319/flim-pkg.el
@@ -0,0 +1,6 @@
+(define-package "flim" "20200303.319" "A library to provide basic features about message representation or encoding."
+  '((apel "10.8"))
+  :commit "f303f2f6c124bc8635add96d3326a2209749437b")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
