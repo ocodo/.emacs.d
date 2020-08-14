de345d728d471b90d921ff01bb5e7c95dbfb4fa5
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:22 +0800

    [Removing heroku-theme version: 20150522.1919]

--- elpa/heroku-theme-20150522.1919/heroku-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; heroku-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "heroku-theme" "heroku-theme.el" (21856 5189
-;;;;;;  699401 0))
-;;; Generated autoloads from heroku-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; heroku-theme-autoloads.el ends here
--- elpa/heroku-theme-20150522.1919/heroku-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "heroku-theme" "20150522.1919" "Heroku color theme" 'nil :url "https://github.com/jonathanchu/color-theme-heroku")
