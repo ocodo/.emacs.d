0583d74950e32c5b5aa6a2fc738eeea25084e99d
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:18 +0800

    [Removing wiki-summary version: 20150408.1422]

--- elpa/wiki-summary-20150408.1422/wiki-summary-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "wiki-summary" "20150408.1422" "View Wikipedia summaries in Emacs easily." '((emacs "24")) :url "https://github.com/jozefg/wiki-summary.el" :keywords '("wikipedia" "utility"))
