369196d9a9bc55b09635a181b846e692f3d49495
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:10 +0800

    [Removing go-gopath version: 20160705.334]

--- elpa/go-gopath-20160705.334/go-gopath-autoloads.el
+++
@@ -1,21 +0,0 @@
-;;; go-gopath-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "go-gopath" "go-gopath.el" (22474 20489 0 0))
-;;; Generated autoloads from go-gopath.el
-
-(autoload 'go-gopath-set-gopath "go-gopath" "\
-
-
-\(fn GOPATH)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; go-gopath-autoloads.el ends here
--- elpa/go-gopath-20160705.334/go-gopath-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-gopath" "20160705.334" "Will guess GOPATH using gb and projectile." '((cl-lib "0.5")) :url "http://github.com/iced/go-gopath/")
