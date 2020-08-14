b8f2ec7c9e2039247d42cc574b96368a71ab1c48
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:56 +0800

    [Removing helm-dired-history version: 20170524.346]

--- elpa/helm-dired-history-20170524.346/helm-dired-history-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; helm-dired-history-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("helm-dired-history.el") (22873 40349
-;;;;;;  0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-dired-history-autoloads.el ends here
--- elpa/helm-dired-history-20170524.346/helm-dired-history-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-dired-history" "20170524.346" "Show dired history with helm.el support." '((helm "1.9.8") (cl-lib "0.5")) :commit "281523f9fc46cf00fafd670ba5cd16552a607212" :url "https://github.com/jixiuf/helm-dired-history" :keywords '("helm" "dired history"))
