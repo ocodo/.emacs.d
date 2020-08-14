e981abae94bf025a75b37196a13bc69777884a6b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:43 +0800

    [Adding parseedn version: 20200419.1124]

---
+++ elpa/parseedn-20200419.1124/parseedn-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/parseedn-20200419.1124/parseedn.el  -*- no-byte-compile: t -*-
+(define-package "parseedn" "20200419.1124" "Clojure/EDN parser" '((emacs "25") (a "0.1.0alpha4") (parseclj "0.1.0")) :commit "92bf875962e62f8c6370b56991d546f122536c6b" :keywords '("lisp" "clojure" "edn" "parser") :authors '(("Arne Brasseur" . "arne@arnebrasseur.net")) :maintainer '("Arne Brasseur" . "arne@arnebrasseur.net"))
