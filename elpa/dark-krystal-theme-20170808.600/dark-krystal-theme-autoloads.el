cdb2575fdde0f70558fcef5bde3ea7d28d065a3d
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:37 +0800

    [Removing dark-krystal-theme version: 20170808.600]

--- elpa/dark-krystal-theme-20170808.600/dark-krystal-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; dark-krystal-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "dark-krystal-theme" "dark-krystal-theme.el"
-;;;;;;  (22955 53602 5751 213000))
-;;; Generated autoloads from dark-krystal-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dark-krystal-theme-autoloads.el ends here
