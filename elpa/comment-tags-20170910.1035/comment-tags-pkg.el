dd224557e85ad3e31a8433732c57d1579d138fd0
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:12 +0800

    [Removing comment-tags version: 20170910.1035]

--- elpa/comment-tags-20170910.1035/comment-tags-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "comment-tags" "20170910.1035" "Highlight & navigate comment tags like 'TODO'." '((emacs "24.5")) :commit "7d914097f0a03484af71e621db533737fc692f58" :url "https://github.com/vincekd/comment-tags" :keywords '("convenience" "comments" "tags"))
