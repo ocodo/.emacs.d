f3245fc34737620b190225d68d29f64bd4416398
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:00 +0800

    [Removing interval-list version: 20150327.1018]

--- elpa/interval-list-20150327.1018/interval-list-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; interval-list-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("interval-list.el") (21791 22007 511014
-;;;;;;  969000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; interval-list-autoloads.el ends here
--- elpa/interval-list-20150327.1018/interval-list-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "interval-list" "20150327.1018" "Interval list data structure for 1D selections" '((dash "2.4.0") (cl-lib "0.5") (emacs "24.4")) :url "https://github.com/Fuco1/interval-list" :keywords '("extensions" "data structure"))
