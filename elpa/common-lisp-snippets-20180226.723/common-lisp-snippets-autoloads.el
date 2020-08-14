b42b9857087a29ed75e1ed1d7397cf04004d8740
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:14 +0800

    [Removing common-lisp-snippets version: 20180226.723]

--- elpa/common-lisp-snippets-20180226.723/common-lisp-snippets-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; common-lisp-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "common-lisp-snippets" "common-lisp-snippets.el"
-;;;;;;  (23278 48411 0 0))
-;;; Generated autoloads from common-lisp-snippets.el
-
-(autoload 'common-lisp-snippets-initialize "common-lisp-snippets" "\
-Initialize Common Lisp snippets, so Yasnippet can see them.
-
-\(fn)" nil nil)
-
-(eval-after-load 'yasnippet '(common-lisp-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("common-lisp-snippets-pkg.el") (23278
-;;;;;;  48411 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; common-lisp-snippets-autoloads.el ends here
--- elpa/common-lisp-snippets-20180226.723/common-lisp-snippets-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "common-lisp-snippets" "20180226.723" "Yasnippets for Common Lisp"
-  '((yasnippet "0.8.0"))
-  :keywords
-  '("snippets")
-  :url "https://github.com/mrkkrp/common-lisp-snippets")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
