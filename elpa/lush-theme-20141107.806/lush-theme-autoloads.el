fc3c80a974618fa32c8d83761cb70ae9af589e15
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:56 +0800

    [Removing lush-theme version: 20141107.806]

--- elpa/lush-theme-20141107.806/lush-theme-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; lush-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "lush-theme" "lush-theme.el" (21604 41062 373235
-;;;;;;  56000))
-;;; Generated autoloads from lush-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-(autoload 'lush-theme "lush-theme" "\
-Load lush-theme.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; lush-theme-autoloads.el ends here
--- elpa/lush-theme-20141107.806/lush-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "lush-theme" "20141107.806" "A dark theme with strong colors" '((emacs "24")))
