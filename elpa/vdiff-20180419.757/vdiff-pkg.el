68da9036c46e365c91beeec2b1e7b602f534657b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:08 +0800

    [Removing vdiff versions: 20180419.757,20180622.808,20180920.1720]

--- elpa/vdiff-20180419.757/vdiff-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "vdiff" "20180419.757" "A diff tool similar to  vimdiff" '((emacs "24.4") (hydra "0.13.0")) :commit "c803bd61c32ab3d2adf0a157a99a6b8985640f97" :keywords '("diff") :url "https://github.com/justbur/emacs-vdiff")
