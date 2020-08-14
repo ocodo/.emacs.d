5f731a554bdda9d5e472ff67891a00288099e543
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:28 +0800

    [Removing flycheck-tip version: 20171020.348]

--- elpa/flycheck-tip-20171020.348/flycheck-tip-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "flycheck-tip" "20171020.348" "Show flycheck/flymake errors by tooltip"
-  '((flycheck "29")
-    (emacs "24.1")
-    (popup "0.5.0"))
-  :url "https://github.com/yuutayamada/flycheck-tip" :keywords
-  '("flycheck"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
