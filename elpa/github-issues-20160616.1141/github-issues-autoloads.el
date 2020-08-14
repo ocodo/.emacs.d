818b7ed19b2c6b5b052df76bb73dcb42c35498ea
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:02 +0800

    [Removing github-issues version: 20160616.1141]

--- elpa/github-issues-20160616.1141/github-issues-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; github-issues-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "github-issues" "github-issues.el" (22396 29079
-;;;;;;  595732 718000))
-;;; Generated autoloads from github-issues.el
-
-(autoload 'github-issues "github-issues" "\
-Display a list of issues list for a GitHub repository.
-
-\(fn USER REPO)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; github-issues-autoloads.el ends here
--- elpa/github-issues-20160616.1141/github-issues-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "github-issues" "20160616.1141" "Functions and modes for managing GitHub projects' issues" '((emacs "24")) :url "http://inkel.github.com/github-issues.el/" :keywords '("github" "issues"))
