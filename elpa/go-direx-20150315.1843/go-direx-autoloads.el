8d7375075770b1479fe95170636164efc9964bd2
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:08 +0800

    [Removing go-direx version: 20150315.1843]

--- elpa/go-direx-20150315.1843/go-direx-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; go-direx-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "go-direx" "go-direx.el" (21771 53411 783401
-;;;;;;  0))
-;;; Generated autoloads from go-direx.el
-
-(autoload 'go-direx-pop-to-buffer "go-direx" "\
-
-
-\(fn)" t nil)
-
-(autoload 'go-direx-switch-to-buffer "go-direx" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; go-direx-autoloads.el ends here
--- elpa/go-direx-20150315.1843/go-direx-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-direx" "20150315.1843" "Tree style source code viewer for Go language" '((direx "1.0.0") (cl-lib "0.5")) :url "https://github.com/syohex/emacs-go-direx")
