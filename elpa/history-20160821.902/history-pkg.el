a29e9ac4db18e7b1b599c2def9d0ca758fb91985
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:30 +0800

    [Removing history version: 20160821.902]

--- elpa/history-20160821.902/history-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "history" "20160821.902" "History utility for source code navigation"
-  '((emacs "24.3"))
-  :url "https://github.com/boyw165/history")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
