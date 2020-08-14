c5ed807fda248180e7898c1a5fd1fce4042ee8ac
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:20 +0800

    [Removing wotd version: 20170328.1248]

--- elpa/wotd-20170328.1248/wotd-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "wotd" "20170328.1248" "Fetch word-of-the-day from multiple online sources" '((emacs "24.4") (org "8.2.10")) :keywords '("extensions"))
