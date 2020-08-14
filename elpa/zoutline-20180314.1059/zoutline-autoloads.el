31bc67ca11ae5058940c0bee9510d6655d1d1f3c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:29 +0800

    [Removing zoutline versions: 20180314.1059,20180314.1759]

--- elpa/zoutline-20180314.1059/zoutline-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; zoutline-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("zoutline.el") (23278 47939 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; zoutline-autoloads.el ends here
--- elpa/zoutline-20180314.1059/zoutline-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zoutline" "20180314.1059" "Simple outline library." 'nil :commit "b3ee0f0e0b916838c2d2c249beba74ffdb8d5699" :keywords '("outline") :url "https://github.com/abo-abo/zoutline")
