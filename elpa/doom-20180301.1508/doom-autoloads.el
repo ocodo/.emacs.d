871e0bb1c560c7bf71cc65688a558266cd8b16e7
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:05 +0800

    [Removing doom version: 20180301.1508]

--- elpa/doom-20180301.1508/doom-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; doom-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("doom.el") (23278 48377 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; doom-autoloads.el ends here
--- elpa/doom-20180301.1508/doom-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "doom" "20180301.1508" "DOM implementation and manipulation library" '((cl-lib "0.5")) :commit "e59040aefc92dd9b3134eb623624307fb9e4327b" :keywords '("xml" "dom") :url "http://www.github.com/kensanata/doom.el/")
