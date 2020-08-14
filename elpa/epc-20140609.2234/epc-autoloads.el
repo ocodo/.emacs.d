085d7cefd9348e77f7017f9dbcbc075e264cf646
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:35 +0800

    [Removing epc version: 20140609.2234]

--- elpa/epc-20140609.2234/epc-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; epc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("epc-pkg.el" "epc.el" "epcs.el") (21893
-;;;;;;  22897 91362 650000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; epc-autoloads.el ends here
