192da616fa306a202c5b040e8cc910ab1e630eeb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:56 +0800

    [Removing evil-space version: 20151208.428]

--- elpa/evil-space-20151208.428/evil-space-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-space" "20151208.428" "Repeat motion in Evil. Correct the behaviour of what SPC should do." '((evil "1.0.0")) :url "http://github.com/linktohack/evil-space" :keywords '("space" "repeat" "motion"))
