c0f4b464fd5cf9fe14e46326ff2600c7e588f532
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:22 +0800

    [Removing scad-preview version: 20160206.536]

--- elpa/scad-preview-20160206.536/scad-preview-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; scad-preview-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "scad-preview" "scad-preview.el" (22200 34349
-;;;;;;  552737 745000))
-;;; Generated autoloads from scad-preview.el
-
-(autoload 'scad-preview-mode "scad-preview" "\
-Preview SCAD models in real-time within Emacs.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; scad-preview-autoloads.el ends here
--- elpa/scad-preview-20160206.536/scad-preview-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "scad-preview" "20160206.536" "Preview SCAD models in real-time within Emacs" '((scad-mode "91.0")) :url "http://hins11.yu-yake.com/")
