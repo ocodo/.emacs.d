8fa189a2ed66cc21aa34bd4ab0459651fdd76859
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:44 +0800

    [Removing db-pg version: 20130131.1102]

--- elpa/db-pg-20130131.1102/db-pg-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; db-pg-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("db-pg.el") (21440 45247 622753 742000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; db-pg-autoloads.el ends here
--- elpa/db-pg-20130131.1102/db-pg-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "db-pg" "20130131.1102" "A PostgreSQL adapter for emacs-db" '((pg "0.12") (db "0.0.6")))
