43adde844de1fdb777c634ed6daa86573da6c00e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:55 +0800

    [Removing textmate-to-yas version: 20160409.1008]

--- elpa/textmate-to-yas-20160409.1008/textmate-to-yas-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "textmate-to-yas" "20160409.1008" "Import Textmate macros into yasnippet syntax" 'nil :url "https://github.com/mlf176f2/textmate-to-yas.el/" :keywords
-  '("yasnippet" "textmate"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
