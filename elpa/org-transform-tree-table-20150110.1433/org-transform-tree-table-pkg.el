e4331b88d5d0edca943224608a914d084ecdc30a
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:36 +0800

    [Removing org-transform-tree-table versions: 20150110.1433,20150110.633]

--- elpa/org-transform-tree-table-20150110.1433/org-transform-tree-table-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-transform-tree-table" "20150110.1433" "Transform org-mode tree with properties to a table, and the other way around" '((dash "2.10.0") (s "1.3.0")) :commit "0a9bf07f01bc5fc3b349aff64e83999a8de83b52" :keywords '("org-mode" "table" "org-table" "tree" "csv" "convert") :authors '((nil . "Johan Lindstrom <buzzwordninja not_this_bit@googlemail.com>")) :maintainer '(nil . "Johan Lindstrom <buzzwordninja not_this_bit@googlemail.com>") :url "https://github.com/jplindstrom/emacs-org-transform-tree-table")
--- elpa/org-transform-tree-table-20150110.633/org-transform-tree-table-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-transform-tree-table" "20150110.633" "Transform org-mode tree with properties to a table, and the other way around" '((dash "2.10.0") (s "1.3.0")) :url "https://github.com/jplindstrom/emacs-org-transform-tree-table" :keywords '("org-mode" "table" "org-table" "tree" "csv" "convert"))
