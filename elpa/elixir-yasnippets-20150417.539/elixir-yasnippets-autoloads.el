e3f9a15ea786754c6c21bb082f749848410de632
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:24 +0800

    [Removing elixir-yasnippets version: 20150417.539]

--- elpa/elixir-yasnippets-20150417.539/elixir-yasnippets-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; elixir-yasnippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "elixir-yasnippets" "elixir-yasnippets.el"
-;;;;;;  (21812 54703 851401 0))
-;;; Generated autoloads from elixir-yasnippets.el
-
-(autoload 'elixir-snippets-initialize "elixir-yasnippets" "\
-
-
-\(fn)" nil nil)
-
-(eval-after-load 'yasnippet '(elixir-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("elixir-yasnippets-pkg.el") (21812 54703
-;;;;;;  860456 462000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; elixir-yasnippets-autoloads.el ends here
--- elpa/elixir-yasnippets-20150417.539/elixir-yasnippets-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "elixir-yasnippets" "20150417.539" "Yasnippets for Elixir"
-  '((yasnippet "0.8.0")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
