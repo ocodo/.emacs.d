d46c4f9f4f3ac179e3e928f4e2bef543c2fca66f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:08 +0800

    [Removing colormaps version: 20171008.1524]

--- elpa/colormaps-20171008.1524/colormaps-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; colormaps-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "colormaps" "colormaps.el" (23064 60673 820224
-;;;;;;  795000))
-;;; Generated autoloads from colormaps.el
-
-(autoload 'colormaps-get-color "colormaps" "\
-Get hex color for given VALUE and CMAP-ID (identifier symbol for colormap).
-
-\(fn VALUE &optional CMAP-ID)" nil nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; colormaps-autoloads.el ends here
--- elpa/colormaps-20171008.1524/colormaps-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "colormaps" "20171008.1524" "Hex colormaps" '((emacs "25")) :commit "19fbb64a6288d505b9cf45c9b5a3eed0bfb135e2" :url "https://github.com/lepisma/colormaps.el" :keywords '("tools"))
