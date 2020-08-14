f6768dd0da8107851c85f603cbea54421fd8cdb9
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:58 +0800

    [Updating inkpot-theme version: 20200616.434][removing old versions: 20171217.144,20181119.706]

--- elpa/inkpot-theme-20171217.144/inkpot-theme-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; inkpot-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "inkpot-theme" "inkpot-theme.el" (23138 48332
-;;;;;;  93836 991000))
-;;; Generated autoloads from inkpot-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("inkpot-theme-pkg.el") (23138 48330 425834
-;;;;;;  200000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; inkpot-theme-autoloads.el ends here
---
+++ elpa/inkpot-theme-20200616.434/inkpot-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/inkpot-theme-20200616.434/inkpot-theme.el  -*- no-byte-compile: t -*-
+(define-package "inkpot-theme" "20200616.434" "port of vim's inkpot theme" 'nil :commit "0a5f50c3aa590404ae81cdb08eb869ffc04da625" :keywords '("color" "theme") :authors '(("Sarah Iovan" . "sarah@hwaetageek.com") ("Campbell Barton" . "ideasman42@gmail.com")) :maintainer '("Sarah Iovan" . "sarah@hwaetageek.com") :url "https://github.com/ideasman42/emacs-inkpot-theme")
