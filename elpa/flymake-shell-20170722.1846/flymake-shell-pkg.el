31e1752413765cbab6c054004430dd7b523687a2
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:39 +0800

    [Removing flymake-shell version: 20170722.1846]

--- elpa/flymake-shell-20170722.1846/flymake-shell-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-shell" "20170722.1846" "A flymake syntax-checker for shell scripts" '((flymake-easy "0.1")) :commit "a16cf453056b9849cc7c912bb127fb0b08fc6dab" :url "https://github.com/purcell/flymake-shell")
