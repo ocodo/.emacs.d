93af38ee01bfc4b29ac33c0cb45b40996797dd0b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:04 +0800

    [Removing markdown-mode versions: 20180124.138,20180619.1015,20181112.1529]

--- elpa/markdown-mode-20180124.138/markdown-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "markdown-mode" "20180124.138" "Major mode for Markdown-formatted text" '((emacs "24.4") (cl-lib "0.5")) :commit "668de4a965980d618637a3b5754e721b54c51e83" :url "https://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
--- elpa/markdown-mode-20181112.1529/markdown-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "markdown-mode" "20181112.1529" "Major mode for Markdown-formatted text" '((emacs "24.4") (cl-lib "0.5")) :commit "d18a8f856d19dfac8fa6e6e72b2448e262045fcc" :keywords '("markdown" "github flavored markdown" "itex") :authors '(("Jason R. Blevins" . "jblevins@xbeta.org")) :maintainer '("Jason R. Blevins" . "jblevins@xbeta.org") :url "https://jblevins.org/projects/markdown-mode/")
