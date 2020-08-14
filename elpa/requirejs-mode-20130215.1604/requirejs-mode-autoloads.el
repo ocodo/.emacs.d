2ad06cf46ad46d5fbe684bd4cd2af41a736ece77
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:07 +0800

    [Removing requirejs-mode version: 20130215.1604]

--- elpa/requirejs-mode-20130215.1604/requirejs-mode-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; requirejs-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("requirejs-mode-pkg.el" "requirejs-mode.el")
-;;;;;;  (21011 18259 561257 0))
-
-;;;***
-
-(provide 'requirejs-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; requirejs-mode-autoloads.el ends here
--- elpa/requirejs-mode-20130215.1604/requirejs-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "requirejs-mode" "20130215.1604" "Improved AMD module management [github]" (quote nil))
