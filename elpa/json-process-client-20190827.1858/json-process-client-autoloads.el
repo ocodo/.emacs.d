bcca56bdff9350545ee159a89ebe423bc04e5a7a
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:18 +0800

    [Adding json-process-client version: 20190827.1858]

---
+++ elpa/json-process-client-20190827.1858/json-process-client-autoloads.el
@@ -0,0 +1,23 @@
+;;; json-process-client-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "json-process-client" "json-process-client.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from json-process-client.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "json-process-client" '("json-process-client-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; json-process-client-autoloads.el ends here
---
+++ elpa/json-process-client-20190827.1858/json-process-client-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/json-process-client-20190827.1858/json-process-client.el  -*- no-byte-compile: t -*-
+(define-package "json-process-client" "20190827.1858" "Interact with a TCP process using JSON" '((emacs "25.1")) :commit "422606a7bf08d13646e3db4f6c2bddb69bd61dec" :authors '(("Nicolas Petton" . "nicolas@petton.fr") ("Damien Cassou" . "damien@cassou.me")) :maintainer '("Nicolas Petton" . "nicolas@petton.fr") :url "https://gitlab.petton.fr/nico/json-process-client")
