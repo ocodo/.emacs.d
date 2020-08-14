91291dc8680c65d046818b271562844789877f78
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:12 +0800

    [Removing edit-server version: 20180120.752]

--- elpa/edit-server-20180120.752/edit-server-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; edit-server-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "edit-server" "edit-server.el" (23166 23028
-;;;;;;  256859 755000))
-;;; Generated autoloads from edit-server.el
-
-(autoload 'edit-server-start "edit-server" "\
-Start the edit server.
-
-If argument VERBOSE is non-nil, logs all server activity to buffer
-`*edit-server-log*'.  When called interactivity, a prefix argument
-will cause it to be verbose.
-
-\(fn &optional VERBOSE)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; edit-server-autoloads.el ends here
--- elpa/edit-server-20180120.752/edit-server-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "edit-server" "20180120.752" "server that responds to edit requests from Chrome" 'nil :commit "f01f5775760d73a8b0975d8caf009c3b1e7b2229" :url "https://github.com/stsquad/emacs_chrome")
