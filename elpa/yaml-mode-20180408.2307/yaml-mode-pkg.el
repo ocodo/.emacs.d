51669ff7d96172db9ed2d5ec71ca69a7708d1a6c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:25 +0800

    [Removing yaml-mode versions: 20180408.2307,20180409.607]

--- elpa/yaml-mode-20180408.2307/yaml-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yaml-mode" "20180408.2307" "Major mode for editing YAML files" '((emacs "24.1")) :commit "40067a10ac1360f0b9533f0bbbb2eea128e2574d" :keywords '("data" "yaml"))
