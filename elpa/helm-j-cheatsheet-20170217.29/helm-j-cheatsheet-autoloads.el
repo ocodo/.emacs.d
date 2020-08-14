6264a75e3f0fdba18b544da35125e37cd5feadc2
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:07 +0800

    [Removing helm-j-cheatsheet version: 20170217.29]

--- elpa/helm-j-cheatsheet-20170217.29/helm-j-cheatsheet-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; helm-j-cheatsheet-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "helm-j-cheatsheet" "helm-j-cheatsheet.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from helm-j-cheatsheet.el
-
-(autoload 'helm-j-cheatsheet "helm-j-cheatsheet" "\
-Use helm to show a J cheat sheet.
-
-\(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-j-cheatsheet" '("helm-source-j-cheatsheet" "jc-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-j-cheatsheet-autoloads.el ends here
--- elpa/helm-j-cheatsheet-20170217.29/helm-j-cheatsheet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-j-cheatsheet" "20170217.29" "Quick J reference for Emacs" '((helm "1.5.3")) :url "https://github.com/abo-abo/helm-j-cheatsheet")
