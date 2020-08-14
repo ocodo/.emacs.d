f628c671b8ef101486a541cfb5fc70634cd46435
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:14 +0800

    [Removing helm-pt version: 20160214.1542]

--- elpa/helm-pt-20160214.1542/helm-pt-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; helm-pt-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "helm-pt" "helm-pt.el" (22218 26402 338681
-;;;;;;  61000))
-;;; Generated autoloads from helm-pt.el
-
-(autoload 'helm-do-pt "helm-pt" "\
-Helm source for platinum searcher.
-
-\(fn &optional BASEDIR)" t nil)
-
-(autoload 'helm-projectile-pt "helm-pt" "\
-Helm version of projectile-pt.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("helm-pt-pkg.el") (22218 26402 357471
-;;;;;;  118000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-pt-autoloads.el ends here
--- elpa/helm-pt-20160214.1542/helm-pt-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-pt" "20160214.1542" "Helm interface to the platinum searcher"
-  '((helm "1.5.6"))
-  :keywords
-  '("helm" "platinum searcher"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
