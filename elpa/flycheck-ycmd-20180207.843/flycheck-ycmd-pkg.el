89a51fb8573e87929eb17176e88f1970b2b1a663
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:28 +0800

    [Removing flycheck-ycmd version: 20180207.843]

--- elpa/flycheck-ycmd-20180207.843/flycheck-ycmd-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flycheck-ycmd" "20180207.843" "flycheck integration for ycmd" '((emacs "24") (dash "2.13.0") (flycheck "0.22") (ycmd "1.2") (let-alist "1.0.5")) :commit "e21c99de8fd2992031adaa758df0d495e55d682a" :url "https://github.com/abingham/emacs-ycmd")
