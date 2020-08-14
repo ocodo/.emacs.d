c0e56c85908bc3d23b649622b0d0818fd702d01e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:28 +0800

    [Removing highlight-leading-spaces version: 20151216.422]

--- elpa/highlight-leading-spaces-20151216.422/highlight-leading-spaces-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; highlight-leading-spaces-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "highlight-leading-spaces" "highlight-leading-spaces.el"
-;;;;;;  (22176 24846 0 0))
-;;; Generated autoloads from highlight-leading-spaces.el
-
-(autoload 'highlight-leading-spaces-mode "highlight-leading-spaces" "\
-Highlight leading spaces.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; highlight-leading-spaces-autoloads.el ends here
--- elpa/highlight-leading-spaces-20151216.422/highlight-leading-spaces-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "highlight-leading-spaces" "20151216.422" "Highlight leading spaces" '((emacs "24.4")) :url "https://github.com/mrBliss/highlight-leading-spaces")
