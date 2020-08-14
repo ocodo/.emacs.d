967b16d252317bc96b098d288aca8737ed8bf932
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:09 +0800

    [Removing easy-kill-extras version: 20161028.504]

--- elpa/easy-kill-extras-20161028.504/easy-kill-extras-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "easy-kill-extras" "20161028.504" "Extra functions for easy-kill."
-  '((easy-kill "0.9.4"))
-  :url "https://github.com/knu/easy-kill-extras.el" :keywords
-  '("killing" "convenience"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
