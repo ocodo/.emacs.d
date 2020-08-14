0e384ace63fab674e14aa911ca7694eb56483535
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:58 +0800

    [Removing togetherly version: 20170425.2316]

--- elpa/togetherly-20170425.2316/togetherly-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; togetherly-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("togetherly.el") (22873 40538 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; togetherly-autoloads.el ends here
