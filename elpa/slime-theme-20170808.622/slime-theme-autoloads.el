4f371251a75969ac5cf3364804b82b60f1fc89b1
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:32 +0800

    [Removing slime-theme version: 20170808.622]

--- elpa/slime-theme-20170808.622/slime-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; slime-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "slime-theme" "slime-theme.el" (22955 53148
-;;;;;;  209239 531000))
-;;; Generated autoloads from slime-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; slime-theme-autoloads.el ends here
