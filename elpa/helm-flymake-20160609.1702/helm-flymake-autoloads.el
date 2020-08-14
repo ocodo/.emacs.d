c6ca88bfc3248ca854e0673ab0180e7c42c40be3
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:01 +0800

    [Removing helm-flymake version: 20160609.1702]

--- elpa/helm-flymake-20160609.1702/helm-flymake-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; helm-flymake-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "helm-flymake" "helm-flymake.el" (22366 21626
-;;;;;;  321196 520000))
-;;; Generated autoloads from helm-flymake.el
-
-(autoload 'helm-flymake "helm-flymake" "\
-helm interface for flymake.
-
-\(fn ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-flymake-autoloads.el ends here
--- elpa/helm-flymake-20160609.1702/helm-flymake-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-flymake" "20160609.1702" "helm interface for flymake" '((helm "1.0")) :url "https://github.com/tam17aki")
