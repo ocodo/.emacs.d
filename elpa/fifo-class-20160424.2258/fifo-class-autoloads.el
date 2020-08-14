5bace2bee54209dc65bf9d3a91c9b8ac0c35f09c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:08 +0800

    [Removing fifo-class version: 20160424.2258]

--- elpa/fifo-class-20160424.2258/fifo-class-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; fifo-class-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "fifo-class" "fifo-class.el" (0 0 0 0))
-;;; Generated autoloads from fifo-class.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "fifo-class" '("fifo-class")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; fifo-class-autoloads.el ends here
--- elpa/fifo-class-20160424.2258/fifo-class-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "fifo-class" "20160424.2258" "First in first out abstract class" 'nil :url "https://github.com/mola-T/fifo-class" :keywords '("lisp"))
