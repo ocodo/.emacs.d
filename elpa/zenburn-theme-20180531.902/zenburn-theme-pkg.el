cbcc3815d77e52cc9a08c2b4489643d9d6c7d2e0
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:27 +0800

    [Removing zenburn-theme versions: 20180222.1757,20180531.902,20181014.1555]

--- elpa/zenburn-theme-20180531.902/zenburn-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zenburn-theme" "20180531.902" "A low contrast color theme for Emacs." 'nil :commit "eea429cb2cbde8674b2a17ccc2bda94b54575c01" :url "http://github.com/bbatsov/zenburn-emacs")
--- elpa/zenburn-theme-20181014.1555/zenburn-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zenburn-theme" "20181014.1555" "A low contrast color theme for Emacs." 'nil :commit "d71a0f0556c1db785738ab9b0c989df342705a81" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "http://github.com/bbatsov/zenburn-emacs")
