fbf0878f4ce866d3e8e83cd034e7b113212203a8
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:51 +0800

    [Removing tabula-rasa version: 20141215.2147]

--- elpa/tabula-rasa-20141215.2147/tabula-rasa-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; tabula-rasa-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("tabula-rasa.el") (21664 42411 922036
-;;;;;;  211000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; tabula-rasa-autoloads.el ends here
--- elpa/tabula-rasa-20141215.2147/tabula-rasa-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "tabula-rasa" "20141215.2147" "Distraction free writing mode" '((emacs "24.4")) :url "https://github.com/idomagal/Tabula-Rasa/blob/master/tabula-rasa.el" :keywords '("distraction free" "writing"))
