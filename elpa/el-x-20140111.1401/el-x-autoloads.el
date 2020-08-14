6719c466c7c87325412ca214e7aba6abf28227e8
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:16 +0800

    [Removing el-x version: 20140111.1401]

--- elpa/el-x-20140111.1401/el-x-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; el-x-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (adflet) "dflet" "dflet.el" (21221 16971 0 0))
-;;; Generated autoloads from dflet.el
-
-(autoload 'dflet "dflet")
-
-(autoload 'adflet "dflet" "\
-Anaphoric version of `dflet'. Binds `this-fn' to the original
-definition of the function.
-
-\(fn BINDINGS &rest BODY)" nil t)
-
-;;;***
-
-;;;### (autoloads nil nil ("el-x-pkg.el" "el-x.el" "subr-compat.el")
-;;;;;;  (21221 16971 36761 0))
-
-;;;***
-
-(provide 'el-x-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; el-x-autoloads.el ends here
--- elpa/el-x-20140111.1401/el-x-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "el-x" "20140111.1401" "main entry point for el-x package" 'nil)
