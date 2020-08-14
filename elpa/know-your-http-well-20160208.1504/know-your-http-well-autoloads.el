db03846dcba283527905b7260be8bc679f5ab584
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:30 +0800

    [Removing know-your-http-well version: 20160208.1504]

--- elpa/know-your-http-well-20160208.1504/know-your-http-well-autoloads.el
+++
@@ -1,71 +0,0 @@
-;;; know-your-http-well-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "http-headers" "http-headers.el" (22218 26374
-;;;;;;  458748 870000))
-;;; Generated autoloads from http-headers.el
-
-(autoload 'http-header "http-headers" "\
-Display the meaning of an HTTP header
-
-\(fn HEADER)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "http-methods" "http-methods.el" (22218 26374
-;;;;;;  470748 841000))
-;;; Generated autoloads from http-methods.el
-
-(autoload 'http-method "http-methods" "\
-Display the meaning of an HTTP method
-
-\(fn METHOD)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "http-relations" "http-relations.el" (22218
-;;;;;;  26374 462748 861000))
-;;; Generated autoloads from http-relations.el
-
-(autoload 'http-relation "http-relations" "\
-Display the meaning of an HTTP relation
-
-\(fn RELATION)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "http-status-codes" "http-status-codes.el"
-;;;;;;  (22218 26374 474748 832000))
-;;; Generated autoloads from http-status-codes.el
-
-(autoload 'http-status-code "http-status-codes" "\
-Display the meaning of an HTTP status code or phrase
-
-\(fn STATUS)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "media-types" "media-types.el" (22218 26374
-;;;;;;  486748 803000))
-;;; Generated autoloads from media-types.el
-
-(autoload 'media-type "media-types" "\
-Display the template of a media-type
-
-\(fn MEDIA-TYPE)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("know-your-http-well-pkg.el" "know-your-http-well.el")
-;;;;;;  (22218 26374 505110 182000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; know-your-http-well-autoloads.el ends here
--- elpa/know-your-http-well-20160208.1504/know-your-http-well-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "know-your-http-well" "20160208.1504" "Look up the meaning of HTTP headers, methods, relations, status codes" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
