207ec7589e67e3ce51374d1859e787c07637ede3
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:00 +0800

    [Removing helm-flycheck version: 20160710.129]

--- elpa/helm-flycheck-20160710.129/helm-flycheck-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; helm-flycheck-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "helm-flycheck" "helm-flycheck.el" (22405 60885
-;;;;;;  112470 732000))
-;;; Generated autoloads from helm-flycheck.el
-
-(autoload 'helm-flycheck "helm-flycheck" "\
-Show flycheck errors with `helm'.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-flycheck-autoloads.el ends here
--- elpa/helm-flycheck-20160710.129/helm-flycheck-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-flycheck" "20160710.129" "Show flycheck errors with helm" '((dash "2.12.1") (flycheck "28") (helm-core "1.9.8")) :url "https://github.com/yasuyk/helm-flycheck" :keywords '("helm" "flycheck"))
