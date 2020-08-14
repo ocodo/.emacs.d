e45ebba2d39cc44f01b1cd0cd38fa7755e69449c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:41 +0800

    [Removing haskell-emacs version: 20160904.1326]

--- elpa/haskell-emacs-20160904.1326/haskell-emacs-autoloads.el
+++
@@ -1,35 +0,0 @@
-;;; haskell-emacs-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "haskell-emacs" "haskell-emacs.el" (22479 30443
-;;;;;;  968782 250000))
-;;; Generated autoloads from haskell-emacs.el
-
-(autoload 'haskell-emacs-help "haskell-emacs" "\
-Display the documentation for haskell-emacs.
-
-\(fn)" t nil)
-
-(autoload 'haskell-emacs-init "haskell-emacs" "\
-Initialize haskell FFI or reload it to reflect changed functions.
-
-When ARG, force installation dialog.
-Call `haskell-emacs-help' to read the documentation.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("haskell-emacs-pkg.el") (22479 30443 956782
-;;;;;;  220000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; haskell-emacs-autoloads.el ends here
--- elpa/haskell-emacs-20160904.1326/haskell-emacs-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "haskell-emacs" "20160904.1326" "Write emacs extensions in haskell" 'nil :url "https://github.com/knupfer/haskell-emacs" :keywords
-  '("haskell" "emacs" "ffi"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
