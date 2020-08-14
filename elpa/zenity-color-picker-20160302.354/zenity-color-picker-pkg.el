2d7634bb407a5c911c4510cdbe59e42b3a8edf4c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:28 +0800

    [Removing zenity-color-picker version: 20160302.354]

--- elpa/zenity-color-picker-20160302.354/zenity-color-picker-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "zenity-color-picker" "20160302.354" "Insert and adjust colors using Zenity" '((emacs "24.4")) :url "https://bitbucket.org/Soft/zenity-color-picker.el" :keywords '("colors"))
