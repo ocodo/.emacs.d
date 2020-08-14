cbcafa38d24bda9b8d54be8fb672750ffca4ec45
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:55 +0800

    [Removing evil-smartparens version: 20171210.713]

--- elpa/evil-smartparens-20171210.713/evil-smartparens-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; evil-smartparens-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "evil-smartparens" "evil-smartparens.el" (23138
-;;;;;;  48714 74362 723000))
-;;; Generated autoloads from evil-smartparens.el
-
-(autoload 'evil-smartparens-mode "evil-smartparens" "\
-Toggle evil-smartparens.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; evil-smartparens-autoloads.el ends here
