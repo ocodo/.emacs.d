3037b71d41024ddba7be61c7d2b1f25c829f4f75
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:14 +0800

    [Removing mmt versions: 20171231.2219,20180101.619]

--- elpa/mmt-20171231.2219/mmt-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; mmt-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("mmt.el") (23138 48077 577335 261000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; mmt-autoloads.el ends here
--- elpa/mmt-20180101.619/mmt-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mmt" "20180101.619" "Missing macro tools for Emacs Lisp" '((emacs "24.1") (cl-lib "0.3")) :commit "e860009ce531ee05d2902309db5f804326596b45" :keywords '("macro" "emacs-lisp") :authors '(("Mark Karpov" . "markkarpov92@gmail.com")) :maintainer '("Mark Karpov" . "markkarpov92@gmail.com") :url "https://github.com/mrkkrp/mmt")
