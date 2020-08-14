bda7f995aa9cc160a01968581d1fafd071c53398
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:01 +0800

    [Removing evil-visualstar version: 20160222.1648]

--- elpa/evil-visualstar-20160222.1648/evil-visualstar-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-visualstar" "20160222.1648" "Starts a * or # search from the visual selection" '((evil "0")) :url "https://github.com/bling/evil-visualstar" :keywords '("evil" "vim" "visualstar"))
