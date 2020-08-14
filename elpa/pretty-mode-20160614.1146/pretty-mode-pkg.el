4e2de87ad7d56047d860b9526f8c5e655d33e156
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:57 +0800

    [Removing pretty-mode versions: 20160614.1146,20160614.1846]

--- elpa/pretty-mode-20160614.1146/pretty-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pretty-mode" "20160614.1146" "Redisplay parts of the buffer as pretty Unicode symbols." 'nil :url "https://github.com/akatov/pretty-mode" :keywords '("pretty" "unicode" "symbols"))
