b110161afc244f1b6dd7a3e24d5c9b52a05040bd
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:55 +0800

    [Updating cider version: 20200813.2112][removing old versions: 20180505.210,20180624.2344,20181128.1311]

---
+++ elpa/cider-20200813.2112/cider-pkg.el
@@ -0,0 +1,24 @@
+(define-package "cider" "20200813.2112" "Clojure Interactive Development Environment that Rocks"
+  '((emacs "25")
+    (clojure-mode "5.12")
+    (parseedn "0.1")
+    (pkg-info "0.4")
+    (queue "0.2")
+    (spinner "1.7")
+    (seq "2.16")
+    (sesman "0.3.2"))
+  :commit "19056352563cd9a0fb0dca3f20e1f0a1649a8df7" :keywords
+  '("languages" "clojure" "cider")
+  :authors
+  '(("Tim King" . "kingtim@gmail.com")
+    ("Phil Hagelberg" . "technomancy@gmail.com")
+    ("Bozhidar Batsov" . "bozhidar@batsov.com")
+    ("Artur Malabarba" . "bruce.connor.am@gmail.com")
+    ("Hugo Duncan" . "hugo@hugoduncan.org")
+    ("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.com")
+  :url "http://www.github.com/clojure-emacs/cider")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
