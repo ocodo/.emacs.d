cf710af5ebd9c7ae90e82cf1939a97196fca340f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:43 +0800

    [Removing db version: 20140421.1411]

--- elpa/db-20140421.1411/db-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; db-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("db.el") (21335 7910 939752 134000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; db-autoloads.el ends here
--- elpa/db-20140421.1411/db-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "db" "20140421.1411" "A database for EmacsLisp" '((kv "0.0.11")))
