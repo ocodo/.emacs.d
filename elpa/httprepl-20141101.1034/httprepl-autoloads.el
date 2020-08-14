cb65ecbe4f86742dd6d95ed971fe99e60bc9d649
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:38 +0800

    [Removing httprepl version: 20141101.1034]

--- elpa/httprepl-20141101.1034/httprepl-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; httprepl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "httprepl" "httprepl.el" (21590 19643 0 0))
-;;; Generated autoloads from httprepl.el
-
-(autoload 'httprepl "httprepl" "\
-Interactively evaluate HTTP requests at a REPL-like
-interface. Switches to the buffer specified by
-`httprepl-buffer-name', or creates it if it does not exist. See
-`httprepl-mode' for a reference on constructing requests.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; httprepl-autoloads.el ends here
--- elpa/httprepl-20141101.1034/httprepl-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "httprepl" "20141101.1034" "An HTTP REPL" '((s "1.9.0") (dash "2.5.0") (emacs "24")) :url "https://github.com/gregsexton/httprepl.el" :keywords '("http" "repl"))
