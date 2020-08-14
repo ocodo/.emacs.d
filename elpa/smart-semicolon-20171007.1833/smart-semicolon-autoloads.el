bf1898b7fcb11f72f91b1a0565d68dc71d9f686c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:33 +0800

    [Removing smart-semicolon version: 20171007.1833]

--- elpa/smart-semicolon-20171007.1833/smart-semicolon-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; smart-semicolon-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "smart-semicolon" "smart-semicolon.el" (23064
-;;;;;;  60637 220570 612000))
-;;; Generated autoloads from smart-semicolon.el
-
-(autoload 'smart-semicolon-mode "smart-semicolon" "\
-Minor mode to insert semicolon smartly.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; smart-semicolon-autoloads.el ends here
--- elpa/smart-semicolon-20171007.1833/smart-semicolon-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smart-semicolon" "20171007.1833" "Insert semicolon smartly" '((emacs "25")) :commit "94cf665aed45c5882e94afe465704fed6326e92e" :url "https://github.com/iquiw/smart-semicolon")
