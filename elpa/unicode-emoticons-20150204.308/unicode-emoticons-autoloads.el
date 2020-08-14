1431014e88d843fe7e48f8bc9e2cf84266bfb819
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:05 +0800

    [Removing unicode-emoticons version: 20150204.308]

--- elpa/unicode-emoticons-20150204.308/unicode-emoticons-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; unicode-emoticons-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "unicode-emoticons" "unicode-emoticons.el"
-;;;;;;  (21818 58240 483401 0))
-;;; Generated autoloads from unicode-emoticons.el
-
-(autoload 'unicode-emoticons-mode "unicode-emoticons" "\
-Unicode-emoticons-mode enables expansion of certain keywords to
-unicode emoticons. It is built as an abbrev table, and therefore also
-needs the abbrev-mode to be enabled for it to take affect.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; unicode-emoticons-autoloads.el ends here
--- elpa/unicode-emoticons-20150204.308/unicode-emoticons-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "unicode-emoticons" "20150204.308" "Shortcuts for common unicode emoticons" 'nil :url "https://github.com/hagleitn/unicode-emoticons" :keywords '("games" "entertainment" "comms"))
