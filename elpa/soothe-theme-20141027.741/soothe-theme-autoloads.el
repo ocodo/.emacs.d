40bfffca676b61c57327370c86fa20e51ef2fec4
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:36 +0800

    [Removing soothe-theme version: 20141027.741]

--- elpa/soothe-theme-20141027.741/soothe-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; soothe-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "soothe-theme" "soothe-theme.el" (21583 43769
-;;;;;;  906547 72000))
-;;; Generated autoloads from soothe-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; soothe-theme-autoloads.el ends here
--- elpa/soothe-theme-20141027.741/soothe-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "soothe-theme" "20141027.741" "a dark colorful theme for Emacs24." '((emacs "24.1")))
