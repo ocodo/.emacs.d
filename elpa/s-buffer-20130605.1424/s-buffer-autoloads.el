76e5f024f7569daf1d6cdeef0ad86bfc4db89447
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:20 +0800

    [Removing s-buffer version: 20130605.1424]

--- elpa/s-buffer-20130605.1424/s-buffer-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; s-buffer-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("s-buffer.el") (21440 45228 189155 671000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; s-buffer-autoloads.el ends here
--- elpa/s-buffer-20130605.1424/s-buffer-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "s-buffer" "20130605.1424" "s operations for buffers" '((s "1.6.0") (noflet "0.0.3")))
