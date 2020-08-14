80e9ab1d1e91b0f0f4a2a72f99fa64b04607c64f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:26 +0800

    [Removing counsel-osx-app version: 20160821.109]

--- elpa/counsel-osx-app-20160821.109/counsel-osx-app-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; counsel-osx-app-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "counsel-osx-app" "counsel-osx-app.el" (22459
-;;;;;;  4336 458004 283000))
-;;; Generated autoloads from counsel-osx-app.el
-
-(autoload 'counsel-osx-app "counsel-osx-app" "\
-Launch an application via ivy interface.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; counsel-osx-app-autoloads.el ends here
--- elpa/counsel-osx-app-20160821.109/counsel-osx-app-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel-osx-app" "20160821.109" "launch osx applications via ivy interface" '((ivy "0.8.0") (emacs "24.3")) :url "https://github.com/d12frosted/counsel-osx-app")
