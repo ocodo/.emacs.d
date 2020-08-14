dae9f348896c46397deea8929eee1d5b29dda0cf
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:46 +0800

    [Removing evil-god-state version: 20141116.1855]

--- elpa/evil-god-state-20141116.1855/evil-god-state-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; evil-god-state-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "evil-god-state" "evil-god-state.el" (21609
-;;;;;;  28945 577235 56000))
-;;; Generated autoloads from evil-god-state.el
-
-(autoload 'evil-execute-in-god-state "evil-god-state" "\
-Execute the next command in God state.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; evil-god-state-autoloads.el ends here
--- elpa/evil-god-state-20141116.1855/evil-god-state-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "evil-god-state" "20141116.1855" "use god-mode keybindings in evil-mode" '((evil "1.0.8") (god-mode "2.12.0")) :url "https://github.com/gridaphobe/evil-god-state" :keywords '("evil" "leader" "god-mode"))
