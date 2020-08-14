a4807e8879be992fc806bd0de8de3e1c7c60ba53
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:11 +0800

    [Removing mellow-theme version: 20170808.617]

--- elpa/mellow-theme-20170808.617/mellow-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; mellow-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "mellow-theme" "mellow-theme.el" (22955 53302
-;;;;;;  741476 688000))
-;;; Generated autoloads from mellow-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; mellow-theme-autoloads.el ends here
--- elpa/mellow-theme-20170808.617/mellow-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mellow-theme" "20170808.617" "an Emacs 24 theme based on Mellow (tmTheme)" '((emacs "24.0")) :commit "2bdf18f05f5212b6f269d9a94afe2cf201766891" :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
