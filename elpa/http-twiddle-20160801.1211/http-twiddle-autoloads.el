affd6d9d1a3c46ffbfe89e27a7f9b9d44b8a0be6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:37 +0800

    [Removing http-twiddle version: 20160801.1211]

--- elpa/http-twiddle-20160801.1211/http-twiddle-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; http-twiddle-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "http-twiddle" "http-twiddle.el" (0 0 0 0))
-;;; Generated autoloads from http-twiddle.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "http-twiddle" '("http-twiddle-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; http-twiddle-autoloads.el ends here
--- elpa/http-twiddle-20160801.1211/http-twiddle-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "http-twiddle" "20160801.1211" "send & twiddle & resend HTTP requests" 'nil :url "https://github.com/hassy/http-twiddle/blob/master/http-twiddle.el" :keywords '("http" "rest" "soap"))
