59b8e41a3a2fcd864c0ac8f8e0521126d4653a92
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:43 +0800

    [Removing stream version: 2.2.4]

--- elpa/stream-2.2.4/stream-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; stream-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("stream-pkg.el" "stream-x.el" "stream.el")
-;;;;;;  (22873 40512 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; stream-autoloads.el ends here
