e6d37c17b24fe32c6ec4599dce764437df6429ec
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:59 +0800

    [Removing projectile version: 20181204.1559]

--- elpa/projectile-20181204.1559/projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "projectile" "20181204.1559" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :commit "614e7dbf44459ff81d80b4a409bd958ea7c60cc1" :keywords '("project" "convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/bbatsov/projectile")
