df6c33dbc8277373c2aa1ee50edb0751228b22fb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:17 +0800

    [Removing wgrep-pt versions: 20140510.1531,20140510.2231]

--- elpa/wgrep-pt-20140510.1531/wgrep-pt-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; wgrep-pt-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "wgrep-pt" "wgrep-pt.el" (21783 34311 931401
-;;;;;;  0))
-;;; Generated autoloads from wgrep-pt.el
-
-(autoload 'wgrep-pt-setup "wgrep-pt" "\
-
-
-\(fn)" nil nil)
-
-(add-hook 'pt-search-mode-hook 'wgrep-pt-setup)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; wgrep-pt-autoloads.el ends here
--- elpa/wgrep-pt-20140510.1531/wgrep-pt-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "wgrep-pt" "20140510.1531" "Writable pt buffer and apply the changes to files" '((wgrep "2.1.5")) :url "http://github.com/mhayashi1120/Emacs-wgrep/raw/master/wgrep-pt.el" :keywords '("grep" "edit" "extensions"))
