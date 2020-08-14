dea3c1b4f663a536124b1ba2b0e260d2a7d642d4
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:14 +0800

    [Removing rubocop versions: 20170311.2211,20170312.611]

--- elpa/rubocop-20170311.2211/rubocop-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rubocop" "20170311.2211" "An Emacs interface for RuboCop" '((emacs "24")) :url "https://github.com/bbatsov/rubocop-emacs" :keywords '("project" "convenience"))
