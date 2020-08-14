face343ac8bc831571c1fba853cbb86169937915
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:40 +0800

    [Removing dash versions: 20180413.30,20180910.1856]

--- elpa/dash-20180413.30/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash.el") (23278 48390 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
