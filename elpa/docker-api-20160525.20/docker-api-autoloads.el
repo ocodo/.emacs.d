c7ec024d10d4381af23a643f1d2563b7bd672736
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:03 +0800

    [Removing docker-api version: 20160525.20]

--- elpa/docker-api-20160525.20/docker-api-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; docker-api-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("docker-api-connection.el" "docker-api-containers.el"
-;;;;;;  "docker-api-images.el" "docker-api-networks.el" "docker-api-pkg.el"
-;;;;;;  "docker-api-volumes.el" "docker-api.el") (22347 49619 771722
-;;;;;;  557000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; docker-api-autoloads.el ends here
--- elpa/docker-api-20160525.20/docker-api-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "docker-api" "20160525.20" "Emacs interface to the Docker API"
-  '((dash "2.12.1")
-    (request "0.2.0")
-    (s "1.11.0"))
-  :url "https://github.com/Silex/docker-api.el")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
