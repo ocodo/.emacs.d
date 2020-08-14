3be29ca9b2ea168f7d451e30218a62ffa7990c4e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:59 +0800

    [Removing evil-textobj-column version: 20170905.1205]

--- elpa/evil-textobj-column-20170905.1205/evil-textobj-column-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; evil-textobj-column-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "evil-textobj-column" "evil-textobj-column.el"
-;;;;;;  (23064 61696 923174 710000))
-;;; Generated autoloads from evil-textobj-column.el
- (autoload 'evil-textobj-column-word "evil-textobj-column" nil t)
- (autoload 'evil-textobj-column-WORD "evil-textobj-column" nil t)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; evil-textobj-column-autoloads.el ends here
--- elpa/evil-textobj-column-20170905.1205/evil-textobj-column-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "evil-textobj-column" "20170905.1205" "Provides column text objects." '((names "0.5") (emacs "24") (evil "0")) :commit "835d7036d0bc9a6e44fc9b7c54ccf2a7c01428cd" :url "https://github.com/noctuid/evil-textobj-column" :keywords '("evil" "column" "text-object"))
