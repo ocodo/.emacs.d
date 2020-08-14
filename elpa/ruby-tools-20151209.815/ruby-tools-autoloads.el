7c071077016251cbc45d03bee8aa469ce29e8be1
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:18 +0800

    [Removing ruby-tools version: 20151209.815]

--- elpa/ruby-tools-20151209.815/ruby-tools-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; ruby-tools-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "ruby-tools" "ruby-tools.el" (22122 19303 23677
-;;;;;;  188000))
-;;; Generated autoloads from ruby-tools.el
-
-(autoload 'ruby-tools-mode "ruby-tools" "\
-Collection of handy functions for ruby-mode.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("ruby-tools-pkg.el") (22122 19303 31033
-;;;;;;  388000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ruby-tools-autoloads.el ends here
