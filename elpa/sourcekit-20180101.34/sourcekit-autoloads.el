f75aea7720ef866e8d910dc5f57fe1ba1b9e8be8
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:37 +0800

    [Removing sourcekit version: 20180101.34]

--- elpa/sourcekit-20180101.34/sourcekit-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; sourcekit-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("sourcekit.el") (23138 47794 308519 854000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; sourcekit-autoloads.el ends here
