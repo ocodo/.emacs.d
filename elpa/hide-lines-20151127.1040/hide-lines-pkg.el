2a52fc07ac3e2d8fb89c1e5185422ef692a44a26
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:24 +0800

    [Removing hide-lines version: 20151127.1040]

--- elpa/hide-lines-20151127.1040/hide-lines-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "hide-lines" "20151127.1040" "Commands for hiding lines based on a regexp" 'nil :url "https://github.com/vapniks/hide-lines" :keywords '("convenience"))
