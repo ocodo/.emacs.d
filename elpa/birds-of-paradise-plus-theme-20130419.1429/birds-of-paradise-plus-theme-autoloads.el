33e3fa92620a5f1824ab4a750c30993f04408980
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:38 +0800

    [Removing birds-of-paradise-plus-theme version: 20130419.1429]

--- elpa/birds-of-paradise-plus-theme-20130419.1429/birds-of-paradise-plus-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; birds-of-paradise-plus-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "birds-of-paradise-plus-theme" "birds-of-paradise-plus-theme.el"
-;;;;;;  (21422 2355 0 0))
-;;; Generated autoloads from birds-of-paradise-plus-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("birds-of-paradise-plus-theme-pkg.el")
-;;;;;;  (21422 2355 671544 0))
-
-;;;***
-
-(provide 'birds-of-paradise-plus-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; birds-of-paradise-plus-theme-autoloads.el ends here
