bd2c61a86215f05e4168ba8f78ad2bf3e7fdc50c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:47 +0800

    [Removing describe-number version: 20151031.1755]

--- elpa/describe-number-20151031.1755/describe-number-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; describe-number-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "describe-number" "describe-number.el" (22309
-;;;;;;  46726 0 0))
-;;; Generated autoloads from describe-number.el
-
-(autoload 'describe-number "describe-number" "\
-Describe information about VALUE, which can be a number or a string.
-
-\(fn VALUE)" t nil)
-
-(autoload 'describe-number-at-point "describe-number" "\
-Describe number at point or region by using `describe-number'.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; describe-number-autoloads.el ends here
--- elpa/describe-number-20151031.1755/describe-number-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "describe-number" "20151031.1755" "Describe arbitrarily large number at point." '((yabin "1.1")) :url "https://github.com/netromdk/describe-number" :keywords '("describe" "value" "help"))
