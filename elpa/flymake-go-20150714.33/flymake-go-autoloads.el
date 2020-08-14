b60ac52f3dc0b6979e7cce7dee8f2411f4d1232c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:32 +0800

    [Removing flymake-go version: 20150714.33]

--- elpa/flymake-go-20150714.33/flymake-go-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; flymake-go-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("flymake-go.el") (21937 41122 697254 883000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flymake-go-autoloads.el ends here
--- elpa/flymake-go-20150714.33/flymake-go-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-go" "20150714.33" "A flymake handler for go-mode files" 'nil :url "https://github.com/robert-zaremba/flymake-go" :keywords '("go" "flymake"))
