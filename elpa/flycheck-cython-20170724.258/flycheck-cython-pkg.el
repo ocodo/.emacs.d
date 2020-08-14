0b61aa85f836c4ff0919e285fb04a2f8913d6d19
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:23 +0800

    [Removing flycheck-cython version: 20170724.258]

--- elpa/flycheck-cython-20170724.258/flycheck-cython-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flycheck-cython" "20170724.258" "Support Cython in flycheck" '((flycheck "0.25")) :commit "ecc4454d35ab5317ab66a04406f36f0c1dbc0b76")
