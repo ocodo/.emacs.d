a1a4e82b85ec99273c9242ffdb11e41814a2e3e9
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:29 +0800

    [Removing zossima version: 20121123.1635]

--- elpa/zossima-20121123.1635/zossima-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; zossima-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (zossima-mode) "zossima" "zossima.el" (21163 221
-;;;;;;  0 0))
-;;; Generated autoloads from zossima.el
-
-(autoload 'zossima-mode "zossima" "\
-Improved navigation for Ruby
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("zossima-pkg.el") (21163 221 496493 0))
-
-;;;***
-
-(provide 'zossima-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; zossima-autoloads.el ends here
--- elpa/zossima-20121123.1635/zossima-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "zossima" "20121123.1635" "Ruby from Emacs"
-  '((inf-ruby "2.2.3")))
