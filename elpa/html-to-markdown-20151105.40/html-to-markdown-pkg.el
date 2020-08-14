2eade1c12a4c509b03783c4f0c3882b7a3267d24
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:34 +0800

    [Removing html-to-markdown version: 20151105.40]

--- elpa/html-to-markdown-20151105.40/html-to-markdown-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "html-to-markdown" "20151105.40" "HTML to Markdown converter written in Emacs-lisp." '((cl-lib "0.5")) :url "http://github.com/Bruce-Connor/html-to-markdown" :keywords '("tools" "wp" "languages"))
