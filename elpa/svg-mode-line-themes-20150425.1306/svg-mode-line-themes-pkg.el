02b34b4c733292d7d5c91b492e1b1c202b88c2dd
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:47 +0800

    [Removing svg-mode-line-themes version: 20150425.1306]

--- elpa/svg-mode-line-themes-20150425.1306/svg-mode-line-themes-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "svg-mode-line-themes" "20150425.1306" "SVG-based themes for mode-line"
-  '((xmlgen "0.4"))
-  :url "https://github.com/sabof/svg-mode-line-themes")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
