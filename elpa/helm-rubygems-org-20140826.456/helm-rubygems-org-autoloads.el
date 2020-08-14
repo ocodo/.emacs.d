a1ab60c17bf2789351468ec2f304d9ad25c47e04
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:16 +0800

    [Removing helm-rubygems-org version: 20140826.456]

--- elpa/helm-rubygems-org-20140826.456/helm-rubygems-org-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; helm-rubygems-org-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "helm-rubygems-org" "helm-rubygems-org.el"
-;;;;;;  (21575 14627 0 0))
-;;; Generated autoloads from helm-rubygems-org.el
-
-(defvar helm-rubygems-org-search-source '((name . "Rubygems.org") (candidates . helm-rubygems-org-search) (volatile) (delayed) (requires-pattern . 2) (action ("Copy Gemfile require" . helm-rubygems-org-candidate-to-kill-ring) ("View Description" . helm-rubygems-org-candidate-view) ("Browse source code project" . helm-rubygems-org-candidate-browse-to-source) ("Browse on rubygems.org" . helm-rubygems-org-candidate-browse-to-project))))
-
-(autoload 'helm-rubygems-org "helm-rubygems-org" "\
-List Rubygems.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-rubygems-org-autoloads.el ends here
--- elpa/helm-rubygems-org-20140826.456/helm-rubygems-org-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-rubygems-org" "20140826.456" "Use helm to search rubygems.org" '((emacs "24") (helm "1.6.3") (cl-lib "0.5")) :url "https://github.com/neomantic/helm-rubygems-org" :keywords '("ruby" "rubygems" "gemfile" "helm"))
