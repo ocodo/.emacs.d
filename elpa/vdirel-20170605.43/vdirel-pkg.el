62a9ff5e5e62b0d68297cba4ef750820aa5065b4
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:08 +0800

    [Removing vdirel versions: 20170605.43,20181001.1135]

--- elpa/vdirel-20170605.43/vdirel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "vdirel" "20170605.43" "Manipulate vdir (i.e., vCard) repositories" '((emacs "24.4") (org-vcard "0.1.0") (helm "1.7.0") (seq "1.11")) :commit "a1e6ea3ed5faaf70667c62bc5591dc810331cb1a")
