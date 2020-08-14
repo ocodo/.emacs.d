fffb01052d218b48b38d2243a13daee04cf711bc
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:40 +0800

    [Removing harvest version: 20170822.1046]

--- elpa/harvest-20170822.1046/harvest-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "harvest" "20170822.1046" "Harvest integration" '((swiper "0.7.0") (hydra "0.13.0") (s "1.11.0")) :commit "7acbc0564b250521b67131ee2a0a92720239454f" :url "https://github.com/kostajh/harvest.el" :keywords '("harvest"))
