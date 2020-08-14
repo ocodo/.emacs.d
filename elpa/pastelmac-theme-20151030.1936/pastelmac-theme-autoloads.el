b1b8fdd5a764907873013bbc781c6009b68ad94a
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:44 +0800

    [Removing pastelmac-theme version: 20151030.1936]

--- elpa/pastelmac-theme-20151030.1936/pastelmac-theme-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; pastelmac-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "pastelmac-theme" "pastelmac-theme.el" (22069
-;;;;;;  45703 0 0))
-;;; Generated autoloads from pastelmac-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-(autoload 'pastelmac-theme "pastelmac-theme" "\
-Load the pastelmac-theme.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; pastelmac-theme-autoloads.el ends here
--- elpa/pastelmac-theme-20151030.1936/pastelmac-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pastelmac-theme" "20151030.1936" "a soothing theme with a pastel color palette" '((emacs "24.1")) :url "https://github.com/bmastenbrook/pastelmac-theme-el" :keywords '("themes"))
