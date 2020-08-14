29061a25c8103a4f596daab459bda37b02706295
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:44 +0800

    [Removing stripe-buffer version: 20141208.708]

--- elpa/stripe-buffer-20141208.708/stripe-buffer-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "stripe-buffer" "20141208.708" "Use a different background for even and odd lines" '((cl-lib "1.0")) :url "https://github.com/sabof/stripe-buffer")
