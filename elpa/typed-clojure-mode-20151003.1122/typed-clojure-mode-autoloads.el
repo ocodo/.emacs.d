8694987ca6c9020b2ce32a590763709a856081fb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:02 +0800

    [Removing typed-clojure-mode version: 20151003.1122]

--- elpa/typed-clojure-mode-20151003.1122/typed-clojure-mode-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; typed-clojure-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "typed-clojure-mode" "typed-clojure-mode.el"
-;;;;;;  (22045 47849 369140 0))
-;;; Generated autoloads from typed-clojure-mode.el
-
-(autoload 'typed-clojure-mode "typed-clojure-mode" "\
-The official minor mode for editing Typed Clojure. Provides
-namespace typechecking, error navigation, display of type data,
-and annotation snippets.
-
-\\{typed-clojure-mode-map}
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("typed-clojure-error-mode.el" "typed-clojure-mode-pkg.el")
-;;;;;;  (22045 47849 398045 392000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; typed-clojure-mode-autoloads.el ends here
--- elpa/typed-clojure-mode-20151003.1122/typed-clojure-mode-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "typed-clojure-mode" "20151003.1122" "Typed Clojure minor mode for Emacs"
-  '((clojure-mode "2.1.1")
-    (cider "0.10.0alpha"))
-  :url "https://github.com/typedclojure/typed-clojure-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
