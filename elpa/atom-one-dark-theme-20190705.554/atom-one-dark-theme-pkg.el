7d8e7e205819840a009d77c9224dd21735ef0359
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:28 +0800

    [Updating atom-one-dark-theme version: 20190705.554][removing old version: 20181010.1348]

---
+++ elpa/atom-one-dark-theme-20190705.554/atom-one-dark-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/atom-one-dark-theme-20190705.554/atom-one-dark-theme.el  -*- no-byte-compile: t -*-
+(define-package "atom-one-dark-theme" "20190705.554" "Atom One Dark color theme" 'nil :commit "623fc08252e30174401750a09168279571288c7f" :authors '(("Jonathan Chu" . "me@jonathanchu.is")) :maintainer '("Jonathan Chu" . "me@jonathanchu.is") :url "https://github.com/jonathanchu/atom-one-dark-theme")
