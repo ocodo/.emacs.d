094d79b65d6cef03c97b98cab660913e630694d9
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:08 +0800

    [Removing restart-emacs versions: 20170609.905,20180601.331]

--- elpa/restart-emacs-20180601.331/restart-emacs-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "restart-emacs" "20180601.331" "Restart emacs from within emacs" 'nil :commit "9aa90d3df9e08bc420e1c9845ee3ff568e911bd9" :keywords '("convenience") :url "https://github.com/iqbalansari/restart-emacs")
