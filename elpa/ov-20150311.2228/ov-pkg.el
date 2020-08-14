b0951cce731433e26010e3c8166d40621a9b10c3
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:37 +0800

    [Removing ov versions: 20150311.2228,20150312.528]

--- elpa/ov-20150311.2228/ov-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ov" "20150311.2228" "Overlay library for Emacs Lisp" '((emacs "24.3")))
--- elpa/ov-20150312.528/ov-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ov" "20150312.528" "Overlay library for Emacs Lisp" '((emacs "24.3")) :commit "fae7215b3dedba2a9ced145284332e4609bfdc38" :keywords '("overlay") :authors '(("Shingo Fukuyama - http://fukuyama.co")) :maintainer '("Shingo Fukuyama - http://fukuyama.co") :url "https://github.com/ShingoFukuyama/ov.el")
