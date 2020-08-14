44ba9984ded5838e74bb206d1ae5cc7717eb7f2f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:36 +0800

    [Removing smyx-theme version: 20141127.28]

--- elpa/smyx-theme-20141127.28/smyx-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; smyx-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "smyx-theme" "smyx-theme.el" (21783 34321 719401
-;;;;;;  0))
-;;; Generated autoloads from smyx-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; smyx-theme-autoloads.el ends here
--- elpa/smyx-theme-20141127.28/smyx-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smyx-theme" "20141127.28" "smyx Color Theme" 'nil :keywords '("color" "theme" "smyx"))
