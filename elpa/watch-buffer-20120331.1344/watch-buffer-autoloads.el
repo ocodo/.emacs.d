4ce5a0a484ced74e96e07620405e239ce039c3a5
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:13 +0800

    [Removing watch-buffer version: 20120331.1344]

--- elpa/watch-buffer-20120331.1344/watch-buffer-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; watch-buffer-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("watch-buffer.el") (21440 46582 548922
-;;;;;;  250000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; watch-buffer-autoloads.el ends here
--- elpa/watch-buffer-20120331.1344/watch-buffer-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "watch-buffer" "20120331.1344" "run a shell command when saving a buffer" 'nil)
