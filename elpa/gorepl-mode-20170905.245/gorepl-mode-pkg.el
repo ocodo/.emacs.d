3fe4c85b976dfda01450d0d6718490262089f6e0
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:23 +0800

    [Removing gorepl-mode version: 20170905.245]

--- elpa/gorepl-mode-20170905.245/gorepl-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gorepl-mode" "20170905.245" "Go REPL Interactive Development in top of Gore" '((emacs "24") (s "1.11.0") (f "0.19.0") (hydra "0.13.0")) :commit "bbd27f6a0a77f484e2a3f082d70dc69da63ae52a" :url "http://www.github.com/manute/gorepl-mode" :keywords '("languages" "go" "golang" "gorepl"))
