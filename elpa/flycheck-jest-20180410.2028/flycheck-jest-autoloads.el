87949a4e18f057bec616179cd58a919053028e03
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:24 +0800

    [Removing flycheck-jest version: 20180410.2028]

--- elpa/flycheck-jest-20180410.2028/flycheck-jest-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; flycheck-jest-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "flycheck-jest" "flycheck-jest.el" (23360 15312
-;;;;;;  912672 518000))
-;;; Generated autoloads from flycheck-jest.el
-
-(autoload 'flycheck-jest-setup "flycheck-jest" "\
-Setup Flycheck for Jest.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flycheck-jest-autoloads.el ends here
