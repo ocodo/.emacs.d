6f0af8766de92d77c0c9562f91b54b80e5da4117
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:28 +0800

    [Removing sicp versions: 20171028.1523,20180823.1222]

--- elpa/sicp-20171028.1523/sicp-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "sicp" "20171028.1523" "Structure and Interpretation of Computer Programs in info format" 'nil :url "https://mitpress.mit.edu/sicp")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/sicp-20180823.1222/sicp-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; sicp-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; sicp-autoloads.el ends here
