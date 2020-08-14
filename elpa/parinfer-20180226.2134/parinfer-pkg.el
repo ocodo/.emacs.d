d0f1eec7441e5dff979161a44335534650295fe6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:42 +0800

    [Removing parinfer version: 20180226.2134]

--- elpa/parinfer-20180226.2134/parinfer-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "parinfer" "20180226.2134" "Simpler Lisp editing"
-  '((dash "2.13.0")
-    (cl-lib "0.5"))
-  :keywords
-  '("parinfer")
-  :url "https://github.com/DogLooksGood/parinfer-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
