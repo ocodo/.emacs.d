85e205e40bbaf82e3f22c156f402932f2a51f61c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:47 +0800

    [Removing helm-aws versions: 20171227.132,20180514.332]

--- elpa/helm-aws-20171227.132/helm-aws-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; helm-aws-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "helm-aws" "helm-aws.el" (23138 48466 562045
-;;;;;;  725000))
-;;; Generated autoloads from helm-aws.el
-
-(autoload 'helm-aws "helm-aws" "\
-Show helm with a table of aws information.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-aws-autoloads.el ends here
--- elpa/helm-aws-20180514.332/helm-aws-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-aws" "20180514.332" "Manage AWS EC2 server instances directly from Emacs" '((helm "1.5.3") (cl-lib "0.5") (s "1.9.0")) :commit "b36c744b3f00f458635a91d1f5158fccbb5baef6" :url "https://github.com/istib/helm-aws")
