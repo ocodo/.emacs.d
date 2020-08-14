dab3fd5361626d5709e219c4a433a2e6b2f1648f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:44 +0800

    [Updating helm version: 20200813.627][removing old versions: 20180505.444,20180624.2223,20181127.518]

--- elpa/helm-20180624.2223/helm-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "helm" "20180624.2223" "Helm is an Emacs incremental and narrowing framework"
-  '((emacs "24.4")
-    (async "1.9.3")
-    (popup "0.5.3")
-    (helm-core "2.9.6"))
-  :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/helm-20200813.627/helm-pkg.el
@@ -0,0 +1,9 @@
+(define-package "helm" "20200813.627" "Helm is an Emacs incremental and narrowing framework"
+  '((emacs "25.1")
+    (async "1.9.4")
+    (popup "0.5.3")
+    (helm-core "3.6.2"))
+  :commit "477582f79480dd299b9d84dfd2ad9a592f3e5e8b" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
