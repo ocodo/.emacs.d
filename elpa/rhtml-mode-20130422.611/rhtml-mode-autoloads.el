1b6f4a9deaf5ed619b1ae8bea42ddf064ce22fe8
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:10 +0800

    [Removing rhtml-mode version: 20130422.611]

--- elpa/rhtml-mode-20130422.611/rhtml-mode-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; rhtml-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (rhtml-mode) "rhtml-mode" "rhtml-mode.el" (21163
-;;;;;;  893 0 0))
-;;; Generated autoloads from rhtml-mode.el
-
-(autoload 'rhtml-mode "rhtml-mode" "\
-Embedded Ruby Mode (RHTML)
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.html\\.erb$" . rhtml-mode))
-
-;;;***
-
-;;;### (autoloads nil nil ("rhtml-erb.el" "rhtml-fonts.el" "rhtml-mode-pkg.el"
-;;;;;;  "rhtml-navigation.el" "rhtml-ruby-hook.el" "rhtml-sgml-hacks.el")
-;;;;;;  (21163 893 193779 0))
-
-;;;***
-
-(provide 'rhtml-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; rhtml-mode-autoloads.el ends here
--- elpa/rhtml-mode-20130422.611/rhtml-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rhtml-mode" "20130422.611" "major mode for editing RHTML files" 'nil)
