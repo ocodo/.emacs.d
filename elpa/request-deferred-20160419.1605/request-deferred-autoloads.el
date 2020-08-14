cce6a50188ee77f5681bec92b97d77630670ffbb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:07 +0800

    [Removing request-deferred version: 20160419.1605]

--- elpa/request-deferred-20160419.1605/request-deferred-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; request-deferred-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("request-deferred.el") (22309 38797 89019
-;;;;;;  511000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; request-deferred-autoloads.el ends here
--- elpa/request-deferred-20160419.1605/request-deferred-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "request-deferred" "20160419.1605" "Wrap request.el by deferred" '((deferred "0.3.1") (request "0.2.0")))
