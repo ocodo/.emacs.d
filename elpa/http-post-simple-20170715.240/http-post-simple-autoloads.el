39a93031052632d7fb914194f84942772bb2b62f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:36 +0800

    [Removing http-post-simple version: 20170715.240]

--- elpa/http-post-simple-20170715.240/http-post-simple-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; http-post-simple-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "http-post-simple" "http-post-simple.el" (0
-;;;;;;  0 0 0))
-;;; Generated autoloads from http-post-simple.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "http-post-simple" '("http-post-" "url-http-response-status")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; http-post-simple-autoloads.el ends here
