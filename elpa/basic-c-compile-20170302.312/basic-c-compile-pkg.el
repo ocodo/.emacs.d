cb1983ada8e134016414126193b0b84de80d3664
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:35 +0800

    [Removing basic-c-compile version: 20170302.312]

--- elpa/basic-c-compile-20170302.312/basic-c-compile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "basic-c-compile" "20170302.312" "Quickly create a Makefile, compile and run C." '((cl-lib "0.5") (f "0.19.0")) :url "https://github.com/nick96/basic-c-compile" :keywords '("c" "makefile" "compilation" "convenience"))
