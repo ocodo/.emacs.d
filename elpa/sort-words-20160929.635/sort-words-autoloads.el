dc3f629d214a8fe820e73d08bd46d5c2f3eafa47
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:37 +0800

    [Removing sort-words version: 20160929.635]

--- elpa/sort-words-20160929.635/sort-words-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; sort-words-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "sort-words" "sort-words.el" (22519 51627 120214
-;;;;;;  21000))
-;;; Generated autoloads from sort-words.el
-
-(autoload 'sort-words "sort-words" "\
-Sort words in region alphabetically.
-Then insert them replacing the existing region.
-START and END are boundries of the selected region.
-
-\(fn START END)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; sort-words-autoloads.el ends here
