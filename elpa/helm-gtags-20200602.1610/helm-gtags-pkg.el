d0a21c0fa536e9288e1082cb7ba28d03c9c70fd9
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:05 +0800

    [Updating helm-gtags version: 20200602.1610][removing old versions: 20170115.2129,20170116.529]

---
+++ elpa/helm-gtags-20200602.1610/helm-gtags-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/helm-gtags-20200602.1610/helm-gtags.el  -*- no-byte-compile: t -*-
+(define-package "helm-gtags" "20200602.1610" "GNU GLOBAL helm interface" '((emacs "24.4") (helm "2.0")) :commit "a15fe1dd272d252ad933d8129db1dce02fd41adb" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-helm-gtags")
