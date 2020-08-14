176a0e28ae828cd15465ce27f5fc8f9802ee9ff3
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:53 +0800

    [Removing evil-paredit version: 20150413.1348]

--- elpa/evil-paredit-20150413.1348/evil-paredit-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; evil-paredit-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "evil-paredit" "evil-paredit.el" (21812 54697
-;;;;;;  699401 0))
-;;; Generated autoloads from evil-paredit.el
-
-(autoload 'evil-paredit-mode "evil-paredit" "\
-Minor mode for setting up Evil with paredit in a single buffer
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("evil-paredit-pkg.el") (21812 54697 706632
-;;;;;;  371000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; evil-paredit-autoloads.el ends here
--- elpa/evil-paredit-20150413.1348/evil-paredit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "evil-paredit" "20150413.1348" "Paredit support for evil keybindings" '((evil "1.0.9") (paredit "25beta")) :url "https://github.com/roman/evil-paredit" :keywords '("paredit" "evil"))
