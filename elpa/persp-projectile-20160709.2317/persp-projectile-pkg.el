6cdc6e730bf101e285ba34182843b472a265cceb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:48 +0800

    [Removing persp-projectile versions: 20160709.2317,20180616.1244]

--- elpa/persp-projectile-20160709.2317/persp-projectile-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "persp-projectile" "20160709.2317" "Perspective integration with Projectile" '((perspective "1.9") (projectile "0.11.0") (cl-lib "0.3")) :keywords '("project" "convenience"))
--- elpa/persp-projectile-20180616.1244/persp-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "persp-projectile" "20180616.1244" "Perspective integration with Projectile" '((perspective "1.9") (projectile "0.11.0") (cl-lib "0.3")) :commit "3a79cb26e290b478e83aa7795146fb1759092d14" :keywords '("project" "convenience"))
