2459402a63dc9820208c087660f3babdd50e8eed
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:16 +0800

    [Removing go-scratch version: 20150809.2140]

--- elpa/go-scratch-20150809.2140/go-scratch-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; go-scratch-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "go-scratch" "go-scratch.el" (22474 20496 0
-;;;;;;  0))
-;;; Generated autoloads from go-scratch.el
-
-(autoload 'go-scratch "go-scratch" "\
-Go to the Go scratch buffer.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; go-scratch-autoloads.el ends here
--- elpa/go-scratch-20150809.2140/go-scratch-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-scratch" "20150809.2140" "*scratch* buffer for Go" '((go-mode "1.3.1") (emacs "24")) :keywords '("languages" "go"))
