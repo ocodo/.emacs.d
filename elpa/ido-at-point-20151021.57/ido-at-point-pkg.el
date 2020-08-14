b9cf211357f261b018b05292a1cff66b9871cb48
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:41 +0800

    [Removing ido-at-point version: 20151021.57]

--- elpa/ido-at-point-20151021.57/ido-at-point-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ido-at-point" "20151021.57" "ido-style completion-at-point" '((emacs "24")) :url "https://github.com/katspaugh/ido-at-point" :keywords '("convenience" "abbrev"))
