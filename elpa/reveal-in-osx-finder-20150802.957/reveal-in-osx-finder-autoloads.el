c9b08c6dbe6036dab25296c37db68f01b18a7d7b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:09 +0800

    [Removing reveal-in-osx-finder version: 20150802.957]

--- elpa/reveal-in-osx-finder-20150802.957/reveal-in-osx-finder-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; reveal-in-osx-finder-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "reveal-in-osx-finder" "reveal-in-osx-finder.el"
-;;;;;;  (21954 40807 851401 0))
-;;; Generated autoloads from reveal-in-osx-finder.el
-
-(autoload 'reveal-in-osx-finder "reveal-in-osx-finder" "\
-Reveal the file associated with the current buffer in the OS X Finder.
-In a dired buffer, it will open the current directory.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; reveal-in-osx-finder-autoloads.el ends here
