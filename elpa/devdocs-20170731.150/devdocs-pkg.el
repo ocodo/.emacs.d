940dd95888be617c6f72f9cfa98e66c9c36187d0
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:48 +0800

    [Removing devdocs version: 20170731.150]

--- elpa/devdocs-20170731.150/devdocs-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "devdocs" "20170731.150" "Launch DevDocs search" 'nil :commit "a2d51e824f0cc48a9dd611cc740bc8b86143e611" :url "https://github.com/xuchunyang/DevDocs.el")
