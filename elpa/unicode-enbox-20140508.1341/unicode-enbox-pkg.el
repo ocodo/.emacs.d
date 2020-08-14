c2772a0acfe453ac67f883362a78efbe9c2e5a50
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:05 +0800

    [Removing unicode-enbox version: 20140508.1341]

--- elpa/unicode-enbox-20140508.1341/unicode-enbox-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "unicode-enbox" "20140508.1341" "Surround a string with box-drawing characters" '((string-utils "0.3.2") (ucs-utils "0.7.6") (list-utils "0.4.2") (persistent-soft "0.8.8") (pcache "0.2.3")) :url "http://github.com/rolandwalker/unicode-enbox" :keywords '("extensions" "interface"))
