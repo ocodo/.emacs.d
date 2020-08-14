346f26cc5b5116aec6cbdd483bc306d0965d069b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:48 +0800

    [Removing persp-mode versions: 20180410.1252,20180604.1018,20180930.1720]

--- elpa/persp-mode-20180604.1018/persp-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "persp-mode" "20180604.1018" "windows/buffers sets shared among frames + save/load." 'nil :commit "cc1d16aeb17f45d7141fcdc45f8bbffa03b3127f" :keywords '("perspectives" "session" "workspace" "persistence" "windows" "buffers" "convenience") :url "https://github.com/Bad-ptr/persp-mode.el")
