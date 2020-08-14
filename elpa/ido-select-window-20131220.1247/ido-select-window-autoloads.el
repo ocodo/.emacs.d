a389c6b0caa6c6dd2d4d0ea5f0f44480de498ead
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:48 +0800

    [Removing ido-select-window version: 20131220.1247]

--- elpa/ido-select-window-20131220.1247/ido-select-window-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; ido-select-window-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (ido-select-window) "ido-select-window" "ido-select-window.el"
-;;;;;;  (21215 23833 0 0))
-;;; Generated autoloads from ido-select-window.el
-
-(autoload 'ido-select-window "ido-select-window" "\
-Switch the focus to another window.
-
-Choose a buffer name in the mini-buffer using ido.  The currently
-selected window is excluded from the list of windows.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("ido-select-window-pkg.el") (21215 23833
-;;;;;;  797161 0))
-
-;;;***
-
-(provide 'ido-select-window-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ido-select-window-autoloads.el ends here
--- elpa/ido-select-window-20131220.1247/ido-select-window-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ido-select-window" "20131220.1247" "Select a window using ido and buffer names" (quote ((emacs "24.1"))))
