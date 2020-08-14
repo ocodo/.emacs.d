26e194c503196192e9cbd49d072fb46df37903e2
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:00 +0800

    [Removing discover-my-major versions: 20170113.2306,20180605.2211]

--- elpa/discover-my-major-20180605.2211/discover-my-major-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "discover-my-major" "20180605.2211" "Discover key bindings and their meaning for the current Emacs major mode" '((makey "0.2")) :commit "c592e5e67454f0d1b68669ac0c270073164b16b3" :keywords '("discover" "help" "major-mode" "keys") :url "https://framagit.org/steckerhalter/discover-my-major")
