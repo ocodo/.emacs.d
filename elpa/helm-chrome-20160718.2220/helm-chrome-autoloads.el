c14bc4bc7fbc88a7660ccf3127e12870f51fc82e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:50 +0800

    [Removing helm-chrome version: 20160718.2220]

--- elpa/helm-chrome-20160718.2220/helm-chrome-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; helm-chrome-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "helm-chrome" "helm-chrome.el" (0 0 0 0))
-;;; Generated autoloads from helm-chrome.el
-
-(autoload 'helm-chrome-bookmarks "helm-chrome" "\
-Search Chrome Bookmark using `helm'.
-
-\(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-chrome" '("helm-chrome-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-chrome-autoloads.el ends here
--- elpa/helm-chrome-20160718.2220/helm-chrome-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-chrome" "20160718.2220" "Helm interface for Chrome bookmarks" '((helm "1.5") (cl-lib "0.3") (emacs "24")) :url "https://github.com/kawabata/helm-chrome" :keywords '("tools"))
