7528443d141f36fadd895703312f561817ba0afb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:42 +0800

    [Updating evil version: 20200718.842][removing old versions: 20180428.1409,20180517.1300,20181107.1016]

--- elpa/evil-20180517.1300/evil-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; evil-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "evil-core" "evil-core.el" (23344 45036 880298
-;;;;;;  269000))
-;;; Generated autoloads from evil-core.el
- (autoload 'evil-mode "evil" nil t)
-
-;;;***
-
-;;;### (autoloads nil nil ("evil-command-window.el" "evil-commands.el"
-;;;;;;  "evil-common.el" "evil-development.el" "evil-digraphs.el"
-;;;;;;  "evil-ex.el" "evil-integration.el" "evil-jumps.el" "evil-macros.el"
-;;;;;;  "evil-maps.el" "evil-pkg.el" "evil-repeat.el" "evil-search.el"
-;;;;;;  "evil-states.el" "evil-types.el" "evil-vars.el" "evil.el")
-;;;;;;  (23344 45036 888298 458000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; evil-autoloads.el ends here
---
+++ elpa/evil-20200718.842/evil-pkg.el
@@ -0,0 +1,9 @@
+(define-package "evil" "20200718.842" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (undo-tree "0.7.4")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "2bc6ca3587502fde67b93e411e329fc8224c916a")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
