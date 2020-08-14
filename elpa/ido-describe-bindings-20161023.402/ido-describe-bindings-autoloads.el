47f02cf5886b4e8e72952b6e53d4cb819ce60f44
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:43 +0800

    [Removing ido-describe-bindings version: 20161023.402]

--- elpa/ido-describe-bindings-20161023.402/ido-describe-bindings-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; ido-describe-bindings-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "ido-describe-bindings" "ido-describe-bindings.el"
-;;;;;;  (22541 57979 0 0))
-;;; Generated autoloads from ido-describe-bindings.el
-
-(autoload 'ido-describe-bindings "ido-describe-bindings" "\
-Yet another `describe-bindings' with `ido'.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ido-describe-bindings-autoloads.el ends here
