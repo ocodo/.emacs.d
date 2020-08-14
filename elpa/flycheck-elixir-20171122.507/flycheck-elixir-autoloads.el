39b779a1b681a43982801edbc1494de52a77660c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:24 +0800

    [Removing flycheck-elixir version: 20171122.507]

--- elpa/flycheck-elixir-20171122.507/flycheck-elixir-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; flycheck-elixir-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("flycheck-elixir.el") (23064 61677 295228
-;;;;;;  441000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flycheck-elixir-autoloads.el ends here
--- elpa/flycheck-elixir-20171122.507/flycheck-elixir-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flycheck-elixir" "20171122.507" "Support Elixir in flycheck" '((flycheck "0.25")) :commit "1f19a739cdb93851c6b01b1e188e215d035cd4a1")
