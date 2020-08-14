19110b046b3a8e4481ce5570dae33fa908456835
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:13 +0800

    [Removing commenter version: 20160219.827]

--- elpa/commenter-20160219.827/commenter-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; commenter-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "commenter" "commenter.el" (22291 19268 144780
-;;;;;;  449000))
-;;; Generated autoloads from commenter.el
-
-(autoload 'commenter-setup "commenter" "\
-Add advises to ‘comment-normalize-vars’.
-
-\(fn)" nil nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; commenter-autoloads.el ends here
--- elpa/commenter-20160219.827/commenter-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "commenter" "20160219.827" "multiline-comment support package" '((emacs "24.4") (let-alist "1.0.4")) :stars '(#("1" 0 1 (font-lock-face paradox-star-face))) :url "https://github.com/yuutayamada/commenter" :keywords '("comment"))
