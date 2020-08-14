46c9a5f0a8881bb1317c9c8f5253d579ed27d586
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:14 +0800

    [Removing websocket versions: 20180422.1716,20180423.16]

--- elpa/websocket-20180422.1716/websocket-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; websocket-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("websocket.el") (23278 47953 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; websocket-autoloads.el ends here
--- elpa/websocket-20180423.16/websocket-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "websocket" "20180423.16" "Emacs WebSocket client and server" '((cl-lib "0.5")) :commit "0d96ba2ff5a25c6cd6c66f417cc9b5f38a4308ba" :keywords '("communication" "websocket" "server") :authors '(("Andrew Hyatt" . "ahyatt@gmail.com")) :maintainer '("Andrew Hyatt" . "ahyatt@gmail.com"))
