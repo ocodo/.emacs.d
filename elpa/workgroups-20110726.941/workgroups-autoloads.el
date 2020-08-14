6a3aafc803e1fa6c3b774fbb66846ea6c49fa1cb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:20 +0800

    [Removing workgroups version: 20110726.941]

--- elpa/workgroups-20110726.941/workgroups-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; workgroups-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("workgroups-pkg.el" "workgroups.el") (21420
-;;;;;;  26450 484910 0))
-
-;;;***
-
-(provide 'workgroups-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; workgroups-autoloads.el ends here
