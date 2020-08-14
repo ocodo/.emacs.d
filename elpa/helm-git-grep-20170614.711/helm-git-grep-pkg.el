bad84a2d6f5b18c1a9952e74a034065f88feae9c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:02 +0800

    [Removing helm-git-grep version: 20170614.711]

--- elpa/helm-git-grep-20170614.711/helm-git-grep-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-git-grep" "20170614.711" "helm for git grep, an incremental git-grep(1)" '((helm-core "2.2.0")) :commit "744cea07dba6e6a5effbdba83f1b786c78fd86d3" :url "https://github.com/yasuyk/helm-git-grep")
