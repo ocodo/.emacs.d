d19c97f1f14a108668820cd6b087d2f99be0ac7b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:12 +0800

    [Removing waher-theme version: 20141115.430]

--- elpa/waher-theme-20141115.430/waher-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; waher-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "waher-theme" "waher-theme.el" (21609 28928
-;;;;;;  41235 56000))
-;;; Generated autoloads from waher-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; waher-theme-autoloads.el ends here
--- elpa/waher-theme-20141115.430/waher-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "waher-theme" "20141115.430" "Emacs 24 theme based on waher for st2 by dduckster" '((emacs "24.1")) :url "https://github.com/jasonm23/emacs-waher-theme")
