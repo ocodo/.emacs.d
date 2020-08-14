f3d8890a586bc1194d08daf8f561a33cafa70251
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:22 +0800

    [Removing company-ycmd versions: 20171010.2258,20180520.353]

--- elpa/company-ycmd-20171010.2258/company-ycmd-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; company-ycmd-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "company-ycmd" "company-ycmd.el" (23064 61829
-;;;;;;  294830 773000))
-;;; Generated autoloads from company-ycmd.el
-
-(autoload 'company-ycmd-setup "company-ycmd" "\
-Add company-ycmd to the front of company-backends.
-
-\(fn)" nil nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; company-ycmd-autoloads.el ends here
--- elpa/company-ycmd-20180520.353/company-ycmd-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "company-ycmd" "20180520.353" "company-mode backend for ycmd" '((ycmd "1.3") (company "0.9.3") (deferred "0.5.1") (s "1.11.0") (dash "2.13.0") (let-alist "1.0.5") (f "0.19.0")) :commit "1806b6a9d00946f06b42bb218b3839a392b8a95e" :url "https://github.com/abingham/emacs-ycmd")
