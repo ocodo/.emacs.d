2b278849913e1b15d6e961c10e28e79a8826b092
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:17 +0800

    [Removing multi version: 20131013.844]

--- elpa/multi-20131013.844/multi-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; multi-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("multi.el") (21713 57857 694031 723000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; multi-autoloads.el ends here
--- elpa/multi-20131013.844/multi-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "multi" "20131013.844" "Clojure-style multi-methods for emacs lisp" '((emacs "24")) :url "http://github.com/kurisuwhyte/emacs-multi" :keywords '("multimethod" "generic" "predicate" "dispatch"))
