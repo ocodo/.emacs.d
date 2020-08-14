01360b31d513520691f4797da5a025c76f10112f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:05 +0800

    [Removing fakir version: 20140729.952]

--- elpa/fakir-20140729.952/fakir-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; fakir-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("fakir.el") (21464 21505 761981 639000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; fakir-autoloads.el ends here
--- elpa/fakir-20140729.952/fakir-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "fakir" "20140729.952" "fakeing bits of Emacs" '((noflet "0.0.8") (dash "1.3.2") (kv "0.0.19")))
