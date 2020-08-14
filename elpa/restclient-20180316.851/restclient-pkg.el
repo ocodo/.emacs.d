f5e74f3728b4b55a01a18601ccc3ef07da99cdf3
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:08 +0800

    [Removing restclient versions: 20180316.1551,20180316.851]

--- elpa/restclient-20180316.851/restclient-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "restclient" "20180316.851" "An interactive HTTP client for Emacs" 'nil :commit "859d944796ce298b5779d9d256bd8d271d57e221" :keywords '("http"))
