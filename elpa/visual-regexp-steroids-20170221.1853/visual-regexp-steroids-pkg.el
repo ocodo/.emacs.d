ef46254b805b6cf437a144b20e72322226e5d81b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:10 +0800

    [Removing visual-regexp-steroids version: 20170221.1853]

--- elpa/visual-regexp-steroids-20170221.1853/visual-regexp-steroids-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "visual-regexp-steroids" "20170221.1853" "Extends visual-regexp to support other regexp engines"
-  '((visual-regexp "1.1"))
-  :url "https://github.com/benma/visual-regexp-steroids.el/" :keywords
-  '("external" "foreign" "regexp" "replace" "python" "visual" "feedback"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
