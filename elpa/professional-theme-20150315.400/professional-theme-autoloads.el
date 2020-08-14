95df86cbbee341722a665d62ec2424e93c065ab9
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:58 +0800

    [Removing professional-theme version: 20150315.400]

--- elpa/professional-theme-20150315.400/professional-theme-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; professional-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "professional-theme" "professional-theme.el"
-;;;;;;  (21783 34325 59401 0))
-;;; Generated autoloads from professional-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-(autoload 'professional-theme "professional-theme" "\
-Apply the professional-theme.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; professional-theme-autoloads.el ends here
--- elpa/professional-theme-20150315.400/professional-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "professional-theme" "20150315.400" "Emacs port of Vim's professional theme" 'nil :url "https://github.com/juanjux/emacs-professional-theme" :keywords '("theme" "light" "professional"))
