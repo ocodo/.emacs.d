d474bf50958a73e94e43b2b31318e2949ffff38b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:09 +0800

    [Removing jekyll-modes version: 20141117.514]

--- elpa/jekyll-modes-20141117.514/jekyll-modes-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; jekyll-modes-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "jekyll-modes" "jekyll-modes.el" (22506 20053
-;;;;;;  0 0))
-;;; Generated autoloads from jekyll-modes.el
-
-(autoload 'liquid-tags-mode "jekyll-modes" "\
-Major mode for Liquid Tags
-
-\(fn)" t nil)
- (autoload 'jekyll-markdown-mode "jekyll-modes")
- (autoload 'jekyll-html-mode "jekyll-modes")
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; jekyll-modes-autoloads.el ends here
--- elpa/jekyll-modes-20141117.514/jekyll-modes-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "jekyll-modes" "20141117.514" "Major modes (markdown and HTML) for authoring Jekyll content" '((polymode "0.2")) :url "https://github.com/fred-o/jekyll-modes" :keywords '("docs"))
