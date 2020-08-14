842ee0d19a3cc78b7751dc5ae7eef50f51bcfd10
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:35 +0800

    [Removing flymake-json versions: 20170722.1846,20180511.211]

--- elpa/flymake-json-20180511.211/flymake-json-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-json" "20180511.211" "A flymake handler for json using jsonlint" '((flymake-easy "0.1")) :commit "ae58795f948402e987cda4c15f10354f8ec2d0fd" :url "https://github.com/purcell/flymake-json")
