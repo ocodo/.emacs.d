d13fea782adf2599c18a81fc993cae3403166ea7
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:30 +0800

    [Removing hippie-exp-ext version: 20160502.1626]

--- elpa/hippie-exp-ext-20160502.1626/hippie-exp-ext-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; hippie-exp-ext-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("hippie-exp-ext.el") (22338 19840 261923
-;;;;;;  174000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; hippie-exp-ext-autoloads.el ends here
--- elpa/hippie-exp-ext-20160502.1626/hippie-exp-ext-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "hippie-exp-ext" "20160502.1626" "Extension of hippie-expand" 'nil :url "http://www.emacswiki.org/emacs/download/hippie-exp-ext.el" :keywords '("abbrev" "convenience" "completions" "hippie-expand"))
