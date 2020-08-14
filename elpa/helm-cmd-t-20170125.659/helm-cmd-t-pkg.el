9dc0b77f2a954a09e10c1878ecf755c866ae61d7
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:50 +0800

    [Removing helm-cmd-t version: 20170125.659]

--- elpa/helm-cmd-t-20170125.659/helm-cmd-t-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "helm-cmd-t" "20170125.659" "cmd-t style completion" 'nil :url "https://github.com/lewang/helm-cmd-t" :keywords
-  '("helm" "project-management" "completion" "convenience" "cmd-t" "textmate"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
