20c1bae012f7566b4b6acdada2fb8b5ea674c229
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:44 +0800

    [Removing evil-commentary version: 20170413.751]

--- elpa/evil-commentary-20170413.751/evil-commentary-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "evil-commentary" "20170413.751" "Comment stuff out. A port of vim-commentary."
-  '((evil "1.0.0"))
-  :url "http://github.com/linktohack/evil-commentary" :keywords
-  '("evil" "comment" "commentary" "evil-commentary"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
