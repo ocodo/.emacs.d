b7390ff6318ad97cfebe570e8dd8b76a4a8dde7e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:06 +0800

    [Removing unicode-progress-reporter version: 20140508.1341]

--- elpa/unicode-progress-reporter-20140508.1341/unicode-progress-reporter-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "unicode-progress-reporter" "20140508.1341" "Progress-reporter with fancy characters" '((emacs "24.1.0") (ucs-utils "0.7.6") (list-utils "0.4.2") (persistent-soft "0.8.8") (pcache "0.2.3")) :url "http://github.com/rolandwalker/unicode-progress-reporter" :keywords '("interface"))
