1696c544d83924b8ba7e9f9c5de0113a1d66d73a
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:17 +0800

    [Removing company-inf-ruby version: 20140805.1354]

--- elpa/company-inf-ruby-20140805.1354/company-inf-ruby-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; company-inf-ruby-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("company-inf-ruby.el") (21487 3811 727533
-;;;;;;  863000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; company-inf-ruby-autoloads.el ends here
