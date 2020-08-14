a9e82edb5fb9524e5fdc7684dc3bd4977bffbc53
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:00 +0800

    [Updating magit version: 20200813.957][removing old versions: 20180505.1620,20180620.1224,20181204.2004]

---
+++ elpa/magit-20200813.957/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "20200813.957" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (async "20200113")
+    (dash "20200524")
+    (git-commit "20200516")
+    (transient "20200601")
+    (with-editor "20200522"))
+  :commit "81a5f5668a7886dcb30c57bac8caf5cb29fbd7fb" :keywords
+  '("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
