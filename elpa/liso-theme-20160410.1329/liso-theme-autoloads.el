b00602a884e4055c17f31f7e59b2e0abb0b9949b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:43 +0800

    [Removing liso-theme version: 20160410.1329]

--- elpa/liso-theme-20160410.1329/liso-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; liso-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "liso-theme" "liso-theme.el" (22919 54815 0
-;;;;;;  0))
-;;; Generated autoloads from liso-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; liso-theme-autoloads.el ends here
