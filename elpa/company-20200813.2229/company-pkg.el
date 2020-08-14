add05a80f26a2b2ac6cfe5a7f4df19f3dbac3ff3
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:15 +0800

    [Updating company version: 20200813.2229][removing old versions: 20180501.11,20180615.1508,20181105.2312]

---
+++ elpa/company-20200813.2229/company-pkg.el
@@ -0,0 +1,12 @@
+(define-package "company" "20200813.2229" "Modular text completion framework"
+  '((emacs "24.3"))
+  :commit "57a6554db750567917f1192463e2503787f9b883" :keywords
+  '("abbrev" "convenience" "matching")
+  :authors
+  '(("Nikolaj Schumacher"))
+  :maintainer
+  '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
