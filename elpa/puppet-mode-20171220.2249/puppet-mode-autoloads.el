9910a162f76898c85e7c9adf29890bc55ef75e0d
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:00 +0800

    [Removing puppet-mode versions: 20171220.2249,20180813.1947]

--- elpa/puppet-mode-20171220.2249/puppet-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; puppet-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "puppet-mode" "puppet-mode.el" (23138 47931
-;;;;;;  964959 277000))
-;;; Generated autoloads from puppet-mode.el
-
-(autoload 'puppet-mode "puppet-mode" "\
-
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
-
-;;;***
-
-;;;### (autoloads nil nil ("puppet-mode-pkg.el") (23138 47929 640952
-;;;;;;  601000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; puppet-mode-autoloads.el ends here
--- elpa/puppet-mode-20171220.2249/puppet-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "puppet-mode" "20171220.2249" "Major mode for Puppet manifests" '((emacs "24.1") (pkg-info "0.4")) :commit "b3ed5057166a4f49dfa9be638523a348b55a2fd2" :url "https://github.com/voxpupuli/puppet-mode" :keywords '("languages"))
