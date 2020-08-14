0f3735642b65f21d4541df140030bb7753ce8809
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:10 +0800

    [Removing mediawiki versions: 20170812.2255,20170813.555]

--- elpa/mediawiki-20170812.2255/mediawiki-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mediawiki" "20170812.2255" "mediawiki frontend" 'nil :commit "8473e12d1839f5287a4227586bf117dad820f867" :url "https://github.com/hexmode/mediawiki-el" :keywords '("mediawiki" "wikipedia" "network" "wiki"))
