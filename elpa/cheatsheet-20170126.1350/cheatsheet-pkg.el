c74bd25bba17278fa35dc071e37d4160b6b1d070
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:53 +0800

    [Removing cheatsheet version: 20170126.1350]

--- elpa/cheatsheet-20170126.1350/cheatsheet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cheatsheet" "20170126.1350" "create your own cheatsheet" '((emacs "24") (cl-lib "0.5")) :url "http://github.com/darksmile/cheatsheet/" :keywords '("convenience" "usability"))
