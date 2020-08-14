f6cb634881fcf22828ccc337dfe790665520614c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:16 +0800

    [Removing el2markdown version: 20170630.1158]

--- elpa/el2markdown-20170630.1158/el2markdown-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "el2markdown" "20170630.1158" "Convert commentary section of elisp files to markdown." 'nil :commit "368d99313683cd943c99feaffca356be60bdb636" :url "https://github.com/Lindydancer/el2markdown")
