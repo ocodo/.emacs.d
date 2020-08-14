217a4a777c1b1f0d12ad728d4deb4e411e00d7c3
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:58 +0800

    [Removing tree-mode version: 20151104.531]

--- elpa/tree-mode-20151104.531/tree-mode-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; tree-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "tree-mode" "tree-mode.el" (22084 1809 108917
-;;;;;;  586000))
-;;; Generated autoloads from tree-mode.el
-
-(autoload 'tree-minor-mode "tree-mode" "\
-More keybindings for tree-widget.
-
-\\{tree-mode-map}
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; tree-mode-autoloads.el ends here
--- elpa/tree-mode-20151104.531/tree-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "tree-mode" "20151104.531" "A mode to manage tree widgets" 'nil :keywords '("help" "convenience" "widget"))
