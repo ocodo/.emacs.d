c2e0ce8f794287b536996eaabcdd4033a3b8e6bc
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:45 +0800

    [Removing ido-grid-mode version: 20160122.339]

--- elpa/ido-grid-mode-20160122.339/ido-grid-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ido-grid-mode" "20160122.339" "Display ido-prospects in the minibuffer in a grid." '((emacs "24.4")) :url "https://github.com/larkery/ido-grid-mode.el" :keywords '("convenience"))
