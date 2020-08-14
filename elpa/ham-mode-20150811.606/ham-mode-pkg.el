17b97eae9eea692571eab97b9c4fd48cf33ae004
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:38 +0800

    [Removing ham-mode version: 20150811.606]

--- elpa/ham-mode-20150811.606/ham-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ham-mode" "20150811.606" "Html As Markdown. Transparently edit an html file using markdown" '((html-to-markdown "1.2") (markdown-mode "2.0")) :url "http://github.com/Bruce-Connor/ham-mode" :keywords '("convenience" "emulation" "wp"))
