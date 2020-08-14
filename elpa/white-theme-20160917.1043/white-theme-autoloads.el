671029a6384b55fd33e1aea90416c9a8667eb4fa
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:17 +0800

    [Removing white-theme version: 20160917.1043]

--- elpa/white-theme-20160917.1043/white-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; white-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "white-theme" "white-theme.el" (22507 31277
-;;;;;;  597225 150000))
-;;; Generated autoloads from white-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; white-theme-autoloads.el ends here
--- elpa/white-theme-20160917.1043/white-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "white-theme" "20160917.1043" "Minimalistic light color theme inspired by basic-theme" '((emacs "24")) :url "http://github.com/anler/white-theme.el" :keywords '("color" "theme" "minimal" "basic" "simple" "white"))
