b2d623ef052b6365bef6598637919f26f4f6552f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:57 +0800

    [Adding lv version: 20200507.1518]

---
+++ elpa/lv-20200507.1518/lv-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/lv-20200507.1518/lv.el  -*- no-byte-compile: t -*-
+(define-package "lv" "20200507.1518" "Other echo area" 'nil :commit "112e689f75b59398d8eca79ac6562dca12b8a959" :authors '(("Oleh Krehel")) :maintainer '("Oleh Krehel"))
