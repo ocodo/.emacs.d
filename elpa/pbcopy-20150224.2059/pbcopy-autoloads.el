a877809fcfa9c303196bccecbc68900bd63c77bd
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:44 +0800

    [Removing pbcopy version: 20150224.2059]

--- elpa/pbcopy-20150224.2059/pbcopy-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; pbcopy-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "pbcopy" "pbcopy.el" (21741 24547 487401 0))
-;;; Generated autoloads from pbcopy.el
-
-(autoload 'turn-on-pbcopy "pbcopy" "\
-
-
-\(fn)" t nil)
-
-(autoload 'turn-off-pbcopy "pbcopy" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; pbcopy-autoloads.el ends here
--- elpa/pbcopy-20150224.2059/pbcopy-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pbcopy" "20150224.2059" "Emacs Interface to pbcopy" 'nil :url "https://github.com/jkp/pbcopy.el" :keywords '("mac" "osx" "pbcopy"))
