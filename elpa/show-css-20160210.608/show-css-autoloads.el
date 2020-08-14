22ca790c14c852a7bde03dc4528ef0d6d690cc2b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:27 +0800

    [Removing show-css version: 20160210.608]

--- elpa/show-css-20160210.608/show-css-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; show-css-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "show-css" "show-css.el" (22218 26245 983065
-;;;;;;  531000))
-;;; Generated autoloads from show-css.el
-
-(autoload 'showcss-mode "show-css" "\
-Display the css of the class or id the cursor is at.
-Visit https://github.com/smmcg/showcss-mode to view the
-git repository
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("buffer-combine.el" "show-css-pkg.el")
-;;;;;;  (22218 26246 6897 501000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; show-css-autoloads.el ends here
--- elpa/show-css-20160210.608/show-css-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "show-css" "20160210.608" "Show the css of the html attribute the cursor is on"
-  '((doom "1.3")
-    (s "1.10.0"))
-  :url "https://github.com/smmcg/showcss-mode" :keywords
-  '("hypermedia"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
