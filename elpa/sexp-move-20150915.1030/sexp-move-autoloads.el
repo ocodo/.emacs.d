f4a5b7563941af8fab3a06f28476e3dc3974a364
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:25 +0800

    [Removing sexp-move version: 20150915.1030]

--- elpa/sexp-move-20150915.1030/sexp-move-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; sexp-move-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("sexp-move.el") (22021 61750 481174 836000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; sexp-move-autoloads.el ends here
--- elpa/sexp-move-20150915.1030/sexp-move-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "sexp-move" "20150915.1030" "Improved S-Expression Movement" 'nil :url "https://gitlab.com/elzair/sexp-move" :keywords '("sexp"))
