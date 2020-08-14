ef254baf628ae181370fdf6f412711df0b96a28e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:37 +0800

    [Removing darkburn-theme version: 20170423.952]

--- elpa/darkburn-theme-20170423.952/darkburn-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; darkburn-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "darkburn-theme" "darkburn-theme.el" (22873
-;;;;;;  40232 0 0))
-;;; Generated autoloads from darkburn-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; darkburn-theme-autoloads.el ends here
