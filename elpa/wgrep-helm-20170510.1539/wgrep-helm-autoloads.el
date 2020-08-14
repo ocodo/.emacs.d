6a56fb71a05f46a7379a8167f7c9f4c16de9775e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:16 +0800

    [Removing wgrep-helm versions: 20170510.1539,20170510.2239]

--- elpa/wgrep-helm-20170510.1539/wgrep-helm-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; wgrep-helm-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "wgrep-helm" "wgrep-helm.el" (22873 40587 0
-;;;;;;  0))
-;;; Generated autoloads from wgrep-helm.el
-
-(autoload 'wgrep-helm-setup "wgrep-helm" "\
-
-
-\(fn)" nil nil)
-
-(add-hook 'helm-grep-mode-hook 'wgrep-helm-setup)
-
-(add-hook 'helm-moccur-mode-hook 'wgrep-helm-setup)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; wgrep-helm-autoloads.el ends here
--- elpa/wgrep-helm-20170510.2239/wgrep-helm-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "wgrep-helm" "20170510.2239" "Writable helm-grep-mode buffer and apply the changes to files" '((wgrep "2.1.1")) :commit "414be70bd313e482cd9f0b70fd2daad4ee23497c" :keywords '("grep" "edit" "extensions") :authors '(("Masahiro Hayashi" . "mhayashi1120@gmail.com")) :maintainer '("Masahiro Hayashi" . "mhayashi1120@gmail.com") :url "http://github.com/mhayashi1120/Emacs-wgrep/raw/master/wgrep-helm.el")
