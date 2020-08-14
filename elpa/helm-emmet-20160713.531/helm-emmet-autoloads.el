6f306b0cc01c970eb4e35cdddcfb22714e7a4cea
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:57 +0800

    [Removing helm-emmet version: 20160713.531]

--- elpa/helm-emmet-20160713.531/helm-emmet-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; helm-emmet-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "helm-emmet" "helm-emmet.el" (0 0 0 0))
-;;; Generated autoloads from helm-emmet.el
-
-(autoload 'helm-emmet "helm-emmet" "\
-Helm to preview or expand emmet-mode's snippets.
-
-\(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-emmet" '("helm-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-emmet-autoloads.el ends here
--- elpa/helm-emmet-20160713.531/helm-emmet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-emmet" "20160713.531" "helm sources for emmet-mode's snippets" '((helm "1.0") (emmet-mode "1.0.2")) :url "https://github.com/yasuyk/helm-emmet" :keywords '("convenience" "helm" "emmet"))
