bdff7c27242eb63f1566ce6700e7289963f47bc4
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:17 +0800

    [Removing helm-spotify version: 20160905.1447]

--- elpa/helm-spotify-20160905.1447/helm-spotify-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; helm-spotify-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "helm-spotify" "helm-spotify.el" (22479 30396
-;;;;;;  844663 263000))
-;;; Generated autoloads from helm-spotify.el
-
-(defvar helm-source-spotify-track-search '((name . "Spotify") (volatile) (delayed) (multiline) (requires-pattern . 2) (candidates-process . helm-spotify-search) (action-transformer . helm-spotify-actions-for-track)))
-
-(autoload 'helm-spotify "helm-spotify" "\
-Bring up a Spotify search interface in helm.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-spotify-autoloads.el ends here
--- elpa/helm-spotify-20160905.1447/helm-spotify-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-spotify" "20160905.1447" "Control Spotify with Helm." '((helm "0.0.0") (multi "2.0.0")) :url "https://github.com/krisajenkins/helm-spotify" :keywords '("helm" "spotify"))
