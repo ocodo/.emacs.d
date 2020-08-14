340da2a8a0706da867648c92d012cf138bf0cdf5
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:47 +0800

    [Removing super-save versions: 20171008.3,20180929.727]

--- elpa/super-save-20171008.3/super-save-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "super-save" "20171008.3" "Auto-save buffers, based on your activity." '((emacs "24.4")) :commit "1c8fbd5e18277e4af0ada2678a854b1c9072db38" :url "https://github.com/bbatsov/super-save" :keywords '("convenience"))
--- elpa/super-save-20180929.727/super-save-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "super-save" "20180929.727" "Auto-save buffers, based on your activity." '((emacs "24.4")) :commit "2a905b8bdfc93bee16e2d62a61c6211bbe009331" :keywords '("convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/bbatsov/super-save")
