a201b87ae6596e31bd002b8543cc29a3baebc114
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:32 +0800

    [Removing flymake-elixir version: 20130810.717]

--- elpa/flymake-elixir-20130810.717/flymake-elixir-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; flymake-elixir-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "flymake-elixir" "flymake-elixir.el" (21440
-;;;;;;  45239 66176 999000))
-;;; Generated autoloads from flymake-elixir.el
-
-(autoload 'flymake-elixir-load "flymake-elixir" "\
-Configure flymake mode to check the current buffer's elixir syntax.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flymake-elixir-autoloads.el ends here
--- elpa/flymake-elixir-20130810.717/flymake-elixir-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flymake-elixir" "20130810.717" "A flymake handler for elixir-mode .ex files." 'nil)
