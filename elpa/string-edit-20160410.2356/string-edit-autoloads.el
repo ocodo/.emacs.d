28d2304fcb89e3eafd4f20cf72db7c1193ae175c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:43 +0800

    [Removing string-edit version: 20160410.2356]

--- elpa/string-edit-20160410.2356/string-edit-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; string-edit-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "string-edit" "string-edit.el" (22291 19067
-;;;;;;  80216 788000))
-;;; Generated autoloads from string-edit.el
-
-(autoload 'string-edit-at-point "string-edit" "\
-Pop up a buffer to edit the string at point.
-This saves you from needing to manually escape characters.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; string-edit-autoloads.el ends here
--- elpa/string-edit-20160410.2356/string-edit-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "string-edit" "20160410.2356" "Avoid escape nightmares by editing string in separate buffer" '((dash "1.2.0")) :stars '(#("43" 0 2 (font-lock-face paradox-star-face))))
