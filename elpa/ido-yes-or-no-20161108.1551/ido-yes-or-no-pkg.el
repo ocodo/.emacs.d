e12ebb66214c972f4f9cf59e52e7dc59e20e428b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:49 +0800

    [Removing ido-yes-or-no version: 20161108.1551]

--- elpa/ido-yes-or-no-20161108.1551/ido-yes-or-no-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ido-yes-or-no" "20161108.1551" "Use Ido to answer yes-or-no questions" '((ido-completing-read+ "0")) :url "https://github.com/DarwinAwardWinner/ido-yes-or-no" :keywords '("convenience" "completion" "ido"))
