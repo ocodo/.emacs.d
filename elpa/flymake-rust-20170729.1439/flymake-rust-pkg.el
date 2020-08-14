48ac19ad9ee4c2426379cb73242e94c3f0e2bb53
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:38 +0800

    [Removing flymake-rust version: 20170729.1439]

--- elpa/flymake-rust-20170729.1439/flymake-rust-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-rust" "20170729.1439" "A flymake handler for rust-mode files" '((flymake-easy "0.1")) :commit "2f42d1f2dad73ec9de460eda6176e3ab25c446f0" :url "https://github.com/joaoxsouls/flymake-rust")
