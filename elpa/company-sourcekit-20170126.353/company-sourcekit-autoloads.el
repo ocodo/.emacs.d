1dc96eb0ca836d4211aaf20bf2436cce21dd17cd
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:20 +0800

    [Removing company-sourcekit version: 20170126.353]

--- elpa/company-sourcekit-20170126.353/company-sourcekit-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; company-sourcekit-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "company-sourcekit" "company-sourcekit.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from company-sourcekit.el
-
-(autoload 'company-sourcekit "company-sourcekit" "\
-Company backend for swift using sourcekitten.
-
-\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "company-sourcekit" '("company-sourcekit-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; company-sourcekit-autoloads.el ends here
