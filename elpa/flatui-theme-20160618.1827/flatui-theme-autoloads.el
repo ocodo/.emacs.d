406d5423cea5dbb234e589deddcd1e89a74ed73e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:16 +0800

    [Removing flatui-theme version: 20160618.1827]

--- elpa/flatui-theme-20160618.1827/flatui-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; flatui-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "flatui-theme" "flatui-theme.el" (22396 29105
-;;;;;;  630533 511000))
-;;; Generated autoloads from flatui-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flatui-theme-autoloads.el ends here
--- elpa/flatui-theme-20160618.1827/flatui-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flatui-theme" "20160618.1827" "A color theme for Emacs based on flatuicolors.com" 'nil :url "https://github.com/john2x/flatui-theme.el")
