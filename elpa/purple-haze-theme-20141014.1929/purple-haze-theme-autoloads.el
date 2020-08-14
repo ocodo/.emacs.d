13cc1c8c47ac1c5f526a772cc966245a4051bf78
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:01 +0800

    [Removing purple-haze-theme version: 20141014.1929]

--- elpa/purple-haze-theme-20141014.1929/purple-haze-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; purple-haze-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "purple-haze-theme" "purple-haze-theme.el"
-;;;;;;  (21575 7784 838547 72000))
-;;; Generated autoloads from purple-haze-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; purple-haze-theme-autoloads.el ends here
--- elpa/purple-haze-theme-20141014.1929/purple-haze-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "purple-haze-theme" "20141014.1929" "an overtly purple color theme for Emacs24." '((emacs "24.0")))
