25d0ca5cdbe55afe977e9b40c987b81dc19573db
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:54 +0800

    [Removing git-commit versions: 20180411.1649,20180607.906,20181124.1111]

--- elpa/git-commit-20180411.1649/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20180411.1649" "Edit Git commit messages" '((emacs "24.4") (dash "20170810") (with-editor "20180318")) :commit "31835d1108c951c5ee2744b4ab1b0d8a9e119636" :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")
--- elpa/git-commit-20181124.1111/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20181124.1111" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "bf226698b769ba4cc12d3dad12d15a03319d36a5" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
