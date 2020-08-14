303a937348b6c99a86788fa6843c374d69fdd005
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:13 +0800

    [Removing migemo versions: 20160924.1441,20160924.741]

--- elpa/migemo-20160924.1441/migemo-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "migemo" "20160924.1441" "Japanese incremental search through dynamic pattern expansion" '((cl-lib "0.5")) :commit "e4744efae1b2fdea2bbd2ceaff0f6ea0bb739f5a" :authors '(("Satoru Takabayashi" . "satoru-t@is.aist-nara.ac.jp")) :maintainer '("Satoru Takabayashi" . "satoru-t@is.aist-nara.ac.jp") :url "https://github.com/emacs-jp/migemo")
--- elpa/migemo-20160924.741/migemo-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "migemo" "20160924.741" "Japanese incremental search through dynamic pattern expansion" '((cl-lib "0.5")) :url "https://github.com/emacs-jp/migemo")
