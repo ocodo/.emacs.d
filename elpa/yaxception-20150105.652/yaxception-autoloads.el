a19fae0208cf7b7e2d5668ae1e90c153336f5107
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:26 +0800

    [Removing yaxception version: 20150105.652]

--- elpa/yaxception-20150105.652/yaxception-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; yaxception-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("yaxception.el") (21975 62421 426079 977000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; yaxception-autoloads.el ends here
