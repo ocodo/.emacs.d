bc611b789af06f96061eebc143a1a8cdcf553eed
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:22 +0800

    [Removing xcode-project versions: 20170922.612,20180509.1218,20181025.1244]

--- elpa/xcode-project-20170922.612/xcode-project-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "xcode-project" "20170922.612" "A package for reading Xcode project files."
-  '((emacs "25"))
-  :url "https://github.com/nhojb/xcode-project.git" :keywords
-  '("languages" "tools"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/xcode-project-20181025.1244/xcode-project-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; xcode-project-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "xcode-parser" "xcode-parser.el" (0 0 0 0))
-;;; Generated autoloads from xcode-parser.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "xcode-parser" '("xcode-parser-")))
-
-;;;***
-
-;;;### (autoloads nil "xcode-project" "xcode-project.el" (0 0 0 0))
-;;; Generated autoloads from xcode-project.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "xcode-project" '("xcode-project-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("xcode-project-pkg.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; xcode-project-autoloads.el ends here
