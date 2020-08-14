24929a0955ab9b97a252a2eed4e4a6deb6f2c7eb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:01 +0800

    [Removing magit-org-todos version: 20180412.1542]

--- elpa/magit-org-todos-20180412.1542/magit-org-todos-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; magit-org-todos-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "magit-org-todos" "magit-org-todos.el" (23360
-;;;;;;  15301 12386 138000))
-;;; Generated autoloads from magit-org-todos.el
-
-(autoload 'magit-org-todos-insert-org-todos "magit-org-todos" "\
-Insert org todos from the local todo.org.
-
-\(fn)" nil nil)
-
-(autoload 'magit-org-todos-autoinsert "magit-org-todos" "\
-Automatically insert todo section into magit status buffer.
-
-\(fn)" nil nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magit-org-todos-autoloads.el ends here
--- elpa/magit-org-todos-20180412.1542/magit-org-todos-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "magit-org-todos" "20180412.1542" "Add local todo items to the magit status buffer" '((magit "2.0.0") (emacs "24")))
