9a297ca8193c15fb81c2144f8a47d3f0a29ea5ce
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:06 +0800

    [Removing markup-faces version: 20141110.17]

--- elpa/markup-faces-20141110.17/markup-faces-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; markup-faces-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("markup-faces.el") (22010 16047 231825
-;;;;;;  536000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; markup-faces-autoloads.el ends here
--- elpa/markup-faces-20141110.17/markup-faces-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "markup-faces" "20141110.17" "collection of faces for markup language modes" 'nil :url "https://github.com/sensorflo/markup-faces" :keywords '("wp" "faces"))
