d9ac01297278ea9710a3866c01510d508d9f3ee7
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:39 +0800

    [Removing speed-type versions: 20171230.1647,20171230.847]

--- elpa/speed-type-20171230.1647/speed-type-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "speed-type" "20171230.1647" "Practice touch and speed typing" '((emacs "24.3") (cl-lib "0.3")) :commit "7a67fcd7bf825eee890097bd4a1b3c4f531a1135" :keywords '("games") :authors '(("Gunther Hagleitner")) :maintainer '("Julien Pagès" . "j.parkouss@gmail.com") :url "https://github.com/parkouss/speed-type")
