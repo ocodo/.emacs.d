142ab14f4347aa326687170af6bef218dd9c8733
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:37 +0800

    [Removing es-lib version: 20141111.1030]

--- elpa/es-lib-20141111.1030/es-lib-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "es-lib" "20141111.1030" "A collection of emacs utilities"
-  '((cl-lib "0.3"))
-  :url "https://github.com/sabof/es-lib")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
