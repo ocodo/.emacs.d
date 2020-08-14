5a85fc622322ece9e9b0fed18a7ed84262f3f4a6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:29 +0800

    [Removing creamsody-theme version: 20170222.258]

--- elpa/creamsody-theme-20170222.258/creamsody-theme-autoloads.el
+++
@@ -1,21 +0,0 @@
-;;; creamsody-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "creamsody-theme" "creamsody-theme.el" (0 0
-;;;;;;  0 0))
-;;; Generated autoloads from creamsody-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "creamsody-theme" '("creamsody-modeline")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; creamsody-theme-autoloads.el ends here
