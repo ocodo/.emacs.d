1e567db88f98bc2a894d03c99887a71a85ea8d3c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:48 +0800

    [Removing helm-c-moccur version: 20151230.124]

--- elpa/helm-c-moccur-20151230.124/helm-c-moccur-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; helm-c-moccur-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("helm-c-moccur.el") (22160 38959 607596
-;;;;;;  775000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-c-moccur-autoloads.el ends here
--- elpa/helm-c-moccur-20151230.124/helm-c-moccur-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-c-moccur" "20151230.124" "helm source for color-moccur.el" '((helm "20120811") (color-moccur "2.71")) :keywords '("convenience" "emulation"))
