5ce7241ae2545747802f3144352bc0aa40ee4d2c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:06 +0800

    [Removing j-mode version: 20171224.1056]

--- elpa/j-mode-20171224.1056/j-mode-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "j-mode" "20171224.1056" "Major mode for editing J programs" 'nil :url "http://github.com/zellio/j-mode" :keywords
-  '("j" "languages"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
