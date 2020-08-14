56193c4d22fac900384d6c5f31f4fc2e2e994e21
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:04 +0800

    [Removing helm-google versions: 20180212.240,20180605.2220]

--- elpa/helm-google-20180212.240/helm-google-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-google" "20180212.240" "Emacs Helm Interface for quick Google searches" '((helm "0")) :commit "f9b5f0dfd51e16278eba70d8fc94a599563dab61" :keywords '("helm" "google" "search" "browse" "searx") :url "https://github.com/steckerhalter/helm-google")
--- elpa/helm-google-20180605.2220/helm-google-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-google" "20180605.2220" "Emacs Helm Interface for quick Google searches" '((helm "0")) :commit "48e91a73d5f48c39d7a219022a24440cff548e1a" :keywords '("helm" "google" "search" "browse" "searx") :url "https://framagit.org/steckerhalter/helm-google")
