e2f1ae8b8391d65a9e246836730aaea6a2fcbfb8
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:20 +0800

    [Removing hemera-theme version: 20170910.603]

--- elpa/hemera-theme-20170910.603/hemera-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; hemera-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "hemera-theme" "hemera-theme.el" (23064 60660
-;;;;;;  848345 69000))
-;;; Generated autoloads from hemera-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; hemera-theme-autoloads.el ends here
--- elpa/hemera-theme-20170910.603/hemera-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "hemera-theme" "20170910.603" "Light theme" '((emacs "24")) :commit "354ae3b788c11ac08e6e2fe7c86adc621e2b16fd" :url "https://github.com/GuidoSchmidt/emacs-hemera-theme" :keywords '("themes" "light-theme"))
