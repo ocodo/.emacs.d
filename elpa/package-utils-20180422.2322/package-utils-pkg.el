21e502798dd469de5d9c9e374449a5b097d05092
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:40 +0800

    [Removing package-utils versions: 20180422.2322,20180514.715]

--- elpa/package-utils-20180422.2322/package-utils-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "package-utils" "20180422.2322" "Extensions for package.el" '((restart-emacs "0.1.1")) :commit "67ed5d2da9f1c4f2683eff6e48c55f441065d3d6" :keywords '("package" "convenience") :url "https://github.com/Silex/package-utils")
--- elpa/package-utils-20180514.715/package-utils-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "package-utils" "20180514.715" "Extensions for package.el" '((restart-emacs "0.1.1")) :commit "5621b95c56b55499f0463fd8b29501da25d861bd" :keywords '("package" "convenience") :url "https://github.com/Silex/package-utils")
