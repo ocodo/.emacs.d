bccff1bda4195d5fdac3205741d90e0b29b227a6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:29 +0800

    [Removing highlight-parentheses version: 20170809.1151]

--- elpa/highlight-parentheses-20170809.1151/highlight-parentheses-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "highlight-parentheses" "20170809.1151" "highlight surrounding parentheses" 'nil :commit "c38885bba4f174c0a2cad3a60fe12b7cf8699aa1" :url "https://github.com/tsdh/highlight-parentheses.el" :keywords '("faces" "matching"))
