d034f99b049ef7f357f98b8b333e59067826be07
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:11 +0800

    [Removing edit-list version: 20100930.743]

--- elpa/edit-list-20100930.743/edit-list-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; edit-list-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("edit-list.el") (22519 51988 612724 648000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; edit-list-autoloads.el ends here
--- elpa/edit-list-20100930.743/edit-list-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "edit-list" "20100930.743" "edit a single list" 'nil :url "http://mwolson.org/static/dist/elisp/edit-list.el")
