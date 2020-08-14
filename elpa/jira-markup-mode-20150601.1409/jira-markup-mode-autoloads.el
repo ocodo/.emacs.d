d57501b29c71ee80be0a951d52ad773f38e5308c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:11 +0800

    [Removing jira-markup-mode version: 20150601.1409]

--- elpa/jira-markup-mode-20150601.1409/jira-markup-mode-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; jira-markup-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "jira-markup-mode" "jira-markup-mode.el" (22089
-;;;;;;  54979 0 0))
-;;; Generated autoloads from jira-markup-mode.el
-
-(autoload 'jira-markup-mode "jira-markup-mode" "\
-Major mode for editing Jira-Markup files.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; jira-markup-mode-autoloads.el ends here
--- elpa/jira-markup-mode-20150601.1409/jira-markup-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "jira-markup-mode" "20150601.1409" "Emacs Major mode for JIRA-markup-formatted text files" 'nil :url "https://github.com/mnuessler/jira-markup-mode" :keywords '("jira" "markup"))
