0b8838ebbd2167e92dc556f291e3cb0e370cabb1
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:33 +0800

    [Removing smart-cursor-color version: 20141124.919]

--- elpa/smart-cursor-color-20141124.919/smart-cursor-color-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smart-cursor-color" "20141124.919" "Change cursor color dynamically" 'nil :url "https://github.com/7696122/smart-cursor-color/" :keywords '("cursor" "color" "face"))
