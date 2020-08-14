29f54781080b870f3754fb9b12ec0c9d71b21279
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:19 +0800

    [Removing with-editor versions: 20180414.757,20180618.1602,20181113.1845]

--- elpa/with-editor-20180414.757/with-editor-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "with-editor" "20180414.757" "Use the Emacsclient as $EDITOR"
-  '((emacs "24.4")
-    (async "1.9"))
-  :keywords
-  '("tools")
-  :url "https://github.com/magit/with-editor")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/with-editor-20180618.1602/with-editor-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "with-editor" "20180618.1602" "Use the Emacsclient as $EDITOR"
-  '((emacs "24.4")
-    (async "1.9"))
-  :keywords
-  '("tools")
-  :url "https://github.com/magit/with-editor")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
