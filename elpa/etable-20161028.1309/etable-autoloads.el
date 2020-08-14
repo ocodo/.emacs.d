dbcfac269dd4d967da8007746c700e2034002f65
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:41 +0800

    [Removing etable version: 20161028.1309]

--- elpa/etable-20161028.1309/etable-autoloads.el
+++
@@ -1,62 +0,0 @@
-;;; etable-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "etable" "etable.el" (0 0 0 0))
-;;; Generated autoloads from etable.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "etable" '("etable")))
-
-;;;***
-
-;;;### (autoloads nil "etable-cell-renderer" "etable-cell-renderer.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from etable-cell-renderer.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "etable-cell-renderer" '("etable-")))
-
-;;;***
-
-;;;### (autoloads nil "etable-selection-model" "etable-selection-model.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from etable-selection-model.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "etable-selection-model" '("etable-")))
-
-;;;***
-
-;;;### (autoloads nil "etable-table-column" "etable-table-column.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from etable-table-column.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "etable-table-column" '("etable-")))
-
-;;;***
-
-;;;### (autoloads nil "etable-table-column-model" "etable-table-column-model.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from etable-table-column-model.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "etable-table-column-model" '("etable-")))
-
-;;;***
-
-;;;### (autoloads nil "etable-table-model" "etable-table-model.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from etable-table-model.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "etable-table-model" '("etable-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("etable-pkg.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; etable-autoloads.el ends here
--- elpa/etable-20161028.1309/etable-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "etable" "20161028.1309" "Implementation of javax.swing.JTable for Emacs."
-  '((dash "2.9.0")
-    (interval-list "0.1")
-    (emacs "24.4")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
