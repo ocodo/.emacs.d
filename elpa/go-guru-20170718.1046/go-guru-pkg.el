d17a32a4d5b8dd0b7fef90fa1d7f3a2b8a92d0cb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:11 +0800

    [Removing go-guru version: 20170718.1046]

--- elpa/go-guru-20170718.1046/go-guru-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-guru" "20170718.1046" "Integration of the Go 'guru' analysis tool into Emacs." '((go-mode "1.3.1") (cl-lib "0.5")) :commit "3f02a165dba38295ae4b961482fe50e012a2112a" :keywords '("tools"))
