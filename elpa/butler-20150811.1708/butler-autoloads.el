faf85c4ccc6453a240f4e9167ee14b89125b5dc1
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:47 +0800

    [Removing butler version: 20150811.1708]

--- elpa/butler-20150811.1708/butler-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; butler-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "butler" "butler.el" (21965 50259 447401 0))
-;;; Generated autoloads from butler.el
-
-(autoload 'butler-status "butler" "\
-Shows the butler status buffer which displays the status of the configured CI server
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("butler-pkg.el" "butler-servers.el" "butler-util.el")
-;;;;;;  (21965 50259 457389 700000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; butler-autoloads.el ends here
--- elpa/butler-20150811.1708/butler-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "butler" "20150811.1708" "Emacs client for Jenkins"
-  '((deferred "0.3.2")
-    (json "1.2")
-    (emacs "24")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
