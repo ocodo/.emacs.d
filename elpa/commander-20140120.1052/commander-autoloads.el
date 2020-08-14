9b5d3507f0708e1e0aaafebe309ae7a30d72f3f6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:11 +0800

    [Removing commander version: 20140120.1052]

--- elpa/commander-20140120.1052/commander-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; commander-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("commander.el") (22507 31241 857224 201000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; commander-autoloads.el ends here
