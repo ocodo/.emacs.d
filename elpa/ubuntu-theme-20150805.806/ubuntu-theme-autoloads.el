775abdc5d16a3d9b0401d06f0f0d5ac96b7b2e28
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:04 +0800

    [Removing ubuntu-theme version: 20150805.806]

--- elpa/ubuntu-theme-20150805.806/ubuntu-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; ubuntu-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "ubuntu-theme" "ubuntu-theme.el" (21954 40858
-;;;;;;  443401 0))
-;;; Generated autoloads from ubuntu-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ubuntu-theme-autoloads.el ends here
--- elpa/ubuntu-theme-20150805.806/ubuntu-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ubuntu-theme" "20150805.806" "A theme inspired by the default terminal colors in Ubuntu" 'nil :url "http://github.com/rocher/ubuntu-theme")
