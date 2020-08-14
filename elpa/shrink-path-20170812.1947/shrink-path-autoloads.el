66e3691724db88ff0722fda8b42c595747b1907e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:27 +0800

    [Removing shrink-path versions: 20170812.1947,20170813.247]

--- elpa/shrink-path-20170812.1947/shrink-path-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; shrink-path-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("shrink-path.el") (23360 14564 934755
-;;;;;;  455000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; shrink-path-autoloads.el ends here
