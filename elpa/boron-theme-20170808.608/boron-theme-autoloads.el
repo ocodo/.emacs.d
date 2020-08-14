4e642209420d2eda7246017c2a6ce1d004726fe1
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:40 +0800

    [Removing boron-theme version: 20170808.608]

--- elpa/boron-theme-20170808.608/boron-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; boron-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "boron-theme" "boron-theme.el" (23064 61902
-;;;;;;  502652 563000))
-;;; Generated autoloads from boron-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; boron-theme-autoloads.el ends here
