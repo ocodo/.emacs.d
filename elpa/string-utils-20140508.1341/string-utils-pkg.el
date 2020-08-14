0fb8844a4cf967ff8a73a48e5792d2f6b2f11cc3
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:44 +0800

    [Removing string-utils version: 20140508.1341]

--- elpa/string-utils-20140508.1341/string-utils-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "string-utils" "20140508.1341" "String-manipulation utilities" '((list-utils "0.4.2")) :url "http://github.com/rolandwalker/string-utils" :keywords '("extensions"))
