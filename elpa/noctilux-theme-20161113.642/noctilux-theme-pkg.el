1dfdf44c02156284515f70ce8a6dcf8f18ae0751
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:26 +0800

    [Removing noctilux-theme version: 20161113.642]

--- elpa/noctilux-theme-20161113.642/noctilux-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "noctilux-theme" "20161113.642" "Dark theme inspired by LightTable" '((emacs "24")) :url "https://github.com/sjrmanning/noctilux-theme")
