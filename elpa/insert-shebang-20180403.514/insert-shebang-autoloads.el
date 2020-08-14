cd73c2654e9562b039e11d929c4138ce8d0caa7d
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:59 +0800

    [Removing insert-shebang version: 20180403.514]

--- elpa/insert-shebang-20180403.514/insert-shebang-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; insert-shebang-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "insert-shebang" "insert-shebang.el" (23278
-;;;;;;  48193 0 0))
-;;; Generated autoloads from insert-shebang.el
-
-(autoload 'insert-shebang "insert-shebang" "\
-Insert shebang line automatically.
-Calls function `insert-shebang-get-extension-and-insert'.  With argument as
-`buffer-name'.
-
-\(fn)" t nil)
-(add-hook 'find-file-hook 'insert-shebang)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; insert-shebang-autoloads.el ends here
