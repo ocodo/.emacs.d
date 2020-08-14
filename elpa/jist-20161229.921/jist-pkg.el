d4c782c3785ebcdfbe6ef26ecd7abbb9fb7e0e9c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:11 +0800

    [Removing jist version: 20161229.921]

--- elpa/jist-20161229.921/jist-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "jist" "20161229.921" "Gist integration" '((emacs "24.4") (dash "2.12.0") (seq "1.11") (let-alist "1.0.4") (magit "2.1.0") (request "0.2.0")) :url "https://github.com/emacs-pe/jist.el" :keywords '("convenience"))
