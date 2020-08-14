d9f847d25c0a9ca4b32904d67b131b3cb16e5703
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:17 +0800

    [Removing ruby-refactor version: 20160214.850]

--- elpa/ruby-refactor-20160214.850/ruby-refactor-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ruby-refactor" "20160214.850" "A minor mode which presents various Ruby refactoring helpers." '((ruby-mode "1.2")) :url "https://github.com/ajvargo/ruby-refactor" :keywords '("refactor" "ruby"))
