9ea2eee0476178a884e2b694da3198c501139128
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:33 +0800

    [Removing flymake-haskell-multi version: 20170722.1846]

--- elpa/flymake-haskell-multi-20170722.1846/flymake-haskell-multi-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; flymake-haskell-multi-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "flymake-haskell-multi" "flymake-haskell-multi.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from flymake-haskell-multi.el
-
-(autoload 'flymake-haskell-multi-load "flymake-haskell-multi" "\
-Configure flymake mode to check the current buffer's hlint syntax.
-
-\(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-haskell-multi" '("flymake-haskell-multi-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("flymake-haskell-multi-pkg.el") (0 0 0
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flymake-haskell-multi-autoloads.el ends here
