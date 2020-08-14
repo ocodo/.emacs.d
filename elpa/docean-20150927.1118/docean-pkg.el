0cf0039f671c0b3a98dd11161403ebdaca20dabb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:01 +0800

    [Removing docean versions: 20150927.1118,20180605.1044]

--- elpa/docean-20150927.1118/docean-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "docean" "20150927.1118" "Interact with DigitalOcean from Emacs." '((emacs "24") (cl-lib "0.5") (request "0.2.0")) :url "https://github.com/emacs-pe/docean.el" :keywords '("convenience"))
