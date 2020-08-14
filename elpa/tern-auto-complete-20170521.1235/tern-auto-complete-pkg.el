368e8bb974beeb9d6f1a3a2f529316d58a0f01b5
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:54 +0800

    [Removing tern-auto-complete version: 20170521.1235]

--- elpa/tern-auto-complete-20170521.1235/tern-auto-complete-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "tern-auto-complete" "20170521.1235" "Tern Completion by auto-complete.el" '((tern "0.0.1") (auto-complete "1.4") (cl-lib "0.5") (emacs "24")) :commit "21fd14f052252095a4a6238fb4d8916cb34d73ed")
