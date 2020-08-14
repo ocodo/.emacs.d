77507e5a3912106547bee5d133a0e261c2db8b41
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:38 +0800

    [Removing ox-gfm version: 20170628.1402]

--- elpa/ox-gfm-20170628.1402/ox-gfm-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ox-gfm" "20170628.1402" "Github Flavored Markdown Back-End for Org Export Engine" 'nil :commit "99f93011b069e02b37c9660b8fcb45dab086a07f" :keywords '("org" "wp" "markdown" "github"))
