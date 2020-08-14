a11f904283908f639f5f9808214c899ded0e3ff1
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:31 +0800

    [Removing embrace version: 20171031.1133]

--- elpa/embrace-20171031.1133/embrace-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "embrace" "20171031.1133" "Add/Change/Delete pairs based on `expand-region'" '((cl-lib "0.5") (expand-region "0.10.0")) :commit "dd5da196e5bcc5e6d87e1937eca0c21da4334ef2" :keywords '("extensions"))
