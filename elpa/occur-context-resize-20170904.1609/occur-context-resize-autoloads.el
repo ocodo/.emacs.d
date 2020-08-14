aae1d8a8a69a1c91b4363dc48bd5e84260620dbc
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:30 +0800

    [Removing occur-context-resize version: 20170904.1609]

--- elpa/occur-context-resize-20170904.1609/occur-context-resize-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; occur-context-resize-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "occur-context-resize" "occur-context-resize.el"
-;;;;;;  (23064 61184 212941 737000))
-;;; Generated autoloads from occur-context-resize.el
-
-(autoload 'occur-context-resize-mode "occur-context-resize" "\
-Dynamically resize context around matches in occur-mode.
-
-\\{occur-context-resize-mode-map}
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; occur-context-resize-autoloads.el ends here
