5c659b0dee00104dad7ae2edfa1e78be15f3d0c0
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:26 +0800

    [Removing shift-number version: 20170301.659]

--- elpa/shift-number-20170301.659/shift-number-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "shift-number" "20170301.659" "Increase/decrease the number at point" 'nil :url "https://github.com/alezost/shift-number.el" :keywords '("convenience"))
