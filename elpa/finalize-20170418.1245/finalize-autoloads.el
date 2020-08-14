e901bb0a0c699a487d9e36abe06ca243e769f271
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:10 +0800

    [Removing finalize version: 20170418.1245]

--- elpa/finalize-20170418.1245/finalize-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; finalize-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "finalizable" "finalizable.el" (0 0 0 0))
-;;; Generated autoloads from finalizable.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "finalizable" '("finalizable")))
-
-;;;***
-
-;;;### (autoloads nil "finalize" "finalize.el" (0 0 0 0))
-;;; Generated autoloads from finalize.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "finalize" '("finalize-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("finalize-pkg.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; finalize-autoloads.el ends here
--- elpa/finalize-20170418.1245/finalize-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "finalize" "20170418.1245" "finalizers for Emacs Lisp"
-  '((emacs "24.1")
-    (cl-generic "0.3")
-    (cl-lib "0.3")
-    (eieio "1.4")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
