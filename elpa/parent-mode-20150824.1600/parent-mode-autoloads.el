61569a4046a39d18264e2f711d3445e5e5c8341e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:42 +0800

    [Removing parent-mode version: 20150824.1600]

--- elpa/parent-mode-20150824.1600/parent-mode-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; parent-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("parent-mode.el") (21998 34650 545826
-;;;;;;  1000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; parent-mode-autoloads.el ends here
