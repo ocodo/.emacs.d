36591b3d3900d00438415922a27e82a71325f0c8
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:07 +0800

    [Removing utop versions: 20180228.305,20181010.2155]

--- elpa/utop-20180228.305/utop-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "utop" "20180228.305" "Universal toplevel for OCaml" '((emacs "24")) :commit "1b4bd052c6513d38f6cc626c24665637d25fdc07" :keywords '("ocaml" "languages") :url "https://github.com/diml/utop")
--- elpa/utop-20181010.2155/utop-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "utop" "20181010.2155" "Universal toplevel for OCaml" '((emacs "24")) :commit "ea38850e606dd18c94e2ccabc28485fec1c8f91f" :keywords '("ocaml" "languages") :authors '(("Jeremie Dimino" . "jeremie@dimino.org")) :maintainer '("Jeremie Dimino" . "jeremie@dimino.org") :url "https://github.com/diml/utop")
