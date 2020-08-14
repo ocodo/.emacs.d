1bdda6702e48650793617b1c7a2a4ce51f30bda1
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:49 +0800

    [Removing c-c-combo version: 20151223.1855]

--- elpa/c-c-combo-20151223.1855/c-c-combo-autoloads.el
+++
@@ -1,36 +0,0 @@
-;;; c-c-combo-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "c-c-combo" "c-c-combo.el" (22145 57683 334882
-;;;;;;  365000))
-;;; Generated autoloads from c-c-combo.el
-
-(defvar c-c-combo-mode nil "\
-Non-nil if C-C-Combo mode is enabled.
-See the command `c-c-combo-mode' for a description of this minor mode.
-Setting this variable directly does not take effect;
-either customize it (see the info node `Easy Customization')
-or call the function `c-c-combo-mode'.")
-
-(custom-autoload 'c-c-combo-mode "c-c-combo" nil)
-
-(autoload 'c-c-combo-mode "c-c-combo" "\
-c-c-combo
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("c-c-combo-pkg.el") (22145 57683 385516
-;;;;;;  292000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; c-c-combo-autoloads.el ends here
--- elpa/c-c-combo-20151223.1855/c-c-combo-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "c-c-combo" "20151223.1855" "Make stuff happen when you reach a target wpm" 'nil :url "https://www.github.com/CestDiego/c-c-combo.el")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
