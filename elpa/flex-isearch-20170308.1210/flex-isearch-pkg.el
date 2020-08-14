9bb90113a2a9b46ec07409251c08b147efc1011f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:16 +0800

    [Removing flex-isearch version: 20170308.1210]

--- elpa/flex-isearch-20170308.1210/flex-isearch-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flex-isearch" "20170308.1210" "Flex matching (like ido) in isearch." 'nil :url "https://bitbucket.org/jpkotta/flex-isearch" :keywords '("convenience" "search"))
