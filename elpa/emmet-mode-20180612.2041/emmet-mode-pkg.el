f661f3765d13501681720f4346577637d4a742ea
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:32 +0800

    [Removing emmet-mode versions: 20180504.1944,20180612.2041]

--- elpa/emmet-mode-20180612.2041/emmet-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "emmet-mode" "20180612.2041" "Unofficial Emmet's support for emacs" 'nil :commit "1acb821e0142136344ccf40c1e5fb664d7db2e70" :keywords '("convenience") :url "https://github.com/smihica/emmet-mode")
