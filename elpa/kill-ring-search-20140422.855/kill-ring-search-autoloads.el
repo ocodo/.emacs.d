c01a0ec43360fd9e3b49ea72b540d5a441e0017b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:27 +0800

    [Removing kill-ring-search version: 20140422.855]

--- elpa/kill-ring-search-20140422.855/kill-ring-search-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; kill-ring-search-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "kill-ring-search" "kill-ring-search.el" (22089
-;;;;;;  54942 0 0))
-;;; Generated autoloads from kill-ring-search.el
-
-(autoload 'kill-ring-search "kill-ring-search" "\
-Search the kill ring in the minibuffer.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; kill-ring-search-autoloads.el ends here
--- elpa/kill-ring-search-20140422.855/kill-ring-search-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "kill-ring-search" "20140422.855" "incremental search for the kill ring" 'nil :url "http://nschum.de/src/emacs/kill-ring-search/" :keywords '("convenience" "matching"))
