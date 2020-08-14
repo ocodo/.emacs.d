2e256deba4ee08f8cfc58d23c6ab8d1d60e7e2ef
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:59 +0800

    [Removing gitconfig-mode version: 20180318.1256]

--- elpa/gitconfig-mode-20180318.1256/gitconfig-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gitconfig-mode" "20180318.1256" "Major mode for editing .gitconfig files" 'nil :commit "b08ac12f09a6cfe967ef70dad3f22430da4fbdab" :keywords '("convenience" "vc" "git") :url "https://github.com/magit/git-modes")
