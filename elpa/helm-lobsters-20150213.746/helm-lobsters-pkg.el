d546982c4095d6d1a6486ffa9a9e964cb6b970bd
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:08 +0800

    [Removing helm-lobsters version: 20150213.746]

--- elpa/helm-lobsters-20150213.746/helm-lobsters-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-lobsters" "20150213.746" "helm front-end for lobste.rs" '((helm "1.0") (cl-lib "0.5")) :url "https://github.com/julienXX/helm-lobste.rs")
