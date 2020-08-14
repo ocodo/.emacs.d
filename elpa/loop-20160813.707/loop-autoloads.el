0aaa57e501bad722fd30df85bd31b2d64f03b165
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:53 +0800

    [Removing loop version: 20160813.707]

--- elpa/loop-20160813.707/loop-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; loop-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("loop.el") (22452 31695 152729 386000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; loop-autoloads.el ends here
--- elpa/loop-20160813.707/loop-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "loop" "20160813.707" "friendly imperative loop structures" 'nil :keywords '("loop" "while" "for each" "break" "continue"))
