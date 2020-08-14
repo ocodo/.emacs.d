0feb7802585d20c0a37926bc540ec313feb79c25
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:53 +0800

    [Removing checkbox version: 20141116.1658]

--- elpa/checkbox-20141116.1658/checkbox-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; checkbox-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "checkbox" "checkbox.el" (21612 9810 409235
-;;;;;;  56000))
-;;; Generated autoloads from checkbox.el
-
-(autoload 'checkbox-toggle "checkbox" "\
-Toggle checkbox (\"[ ]\" or \"[x]\") on the current line.
-If checkbox does not exist, an empty checkbox will be inserted
-before the first word constituent.
-In programming modes, checkboxes will be inserted in comments.
-With prefix ARG, delete checkbox.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; checkbox-autoloads.el ends here
--- elpa/checkbox-20141116.1658/checkbox-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "checkbox" "20141116.1658" "Quick manipulation of textual checkboxes" '((emacs "24") (cl-lib "0.5")) :url "http://github.com/camdez/checkbox.el" :keywords '("convenience"))
