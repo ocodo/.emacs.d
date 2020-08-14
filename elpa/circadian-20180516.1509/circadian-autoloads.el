f921672c95bb146f0733c9c8e84fd29e5d945a9a
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:56 +0800

    [Removing circadian versions: 20180307.954,20180516.1509]

--- elpa/circadian-20180516.1509/circadian-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; circadian-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "circadian" "circadian.el" (23344 45124 82347
-;;;;;;  672000))
-;;; Generated autoloads from circadian.el
-
-(autoload 'circadian-setup "circadian" "\
-Setup circadian based on `circadian-themes'.
-
-\(fn)" nil nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; circadian-autoloads.el ends here
