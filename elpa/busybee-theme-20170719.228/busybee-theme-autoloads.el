8308b51f81efaf705c56d8a07825fbed0fe840ef
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:46 +0800

    [Removing busybee-theme version: 20170719.228]

--- elpa/busybee-theme-20170719.228/busybee-theme-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; busybee-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "busybee-theme" "busybee-theme.el" (0 0 0 0))
-;;; Generated autoloads from busybee-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; busybee-theme-autoloads.el ends here
--- elpa/busybee-theme-20170719.228/busybee-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "busybee-theme" "20170719.228" "port of vim's mustang theme" 'nil :commit "66b2315b030582d0ebee605cf455d386d8c30fcd" :url "http://github.com/mswift42/busybee-theme")
