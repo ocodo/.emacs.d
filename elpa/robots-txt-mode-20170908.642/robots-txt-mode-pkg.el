ba11b245fc37c8549028f246db7a7992a0042fe0
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:12 +0800

    [Removing robots-txt-mode versions: 20170908.642,20180919.1541]

--- elpa/robots-txt-mode-20170908.642/robots-txt-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "robots-txt-mode" "20170908.642" "Major mode for editing robots.txt" 'nil :commit "4a77674ab2963b829d3b751741c4ce1169e87f6b" :url "https://github.com/emacs-php/robots-txt-mode" :keywords '("languages" "comm" "web"))
--- elpa/robots-txt-mode-20180919.1541/robots-txt-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "robots-txt-mode" "20180919.1541" "Major mode for editing robots.txt" 'nil :commit "f8fc7ee50a3d5d7a2838772ed298fb69b9051c5c" :keywords '("languages" "comm" "web") :authors '(("USAMI Kenta" . "tadsan@zonu.me")) :maintainer '("USAMI Kenta" . "tadsan@zonu.me") :url "https://github.com/emacs-php/robots-txt-mode")
