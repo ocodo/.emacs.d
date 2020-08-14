445e0e4782435c81246fdfedd445d4a6ccf71339
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:02 +0800

    [Updating docker version: 20200610.715][removing old versions: 20180422.2320,20180524.506,20181101.504]

---
+++ elpa/docker-20200610.715/docker-pkg.el
@@ -0,0 +1,18 @@
+(define-package "docker" "20200610.715" "Emacs interface to Docker"
+  '((dash "2.14.1")
+    (docker-tramp "0.1")
+    (emacs "24.5")
+    (json-mode "1.7.0")
+    (s "1.12.0")
+    (tablist "0.70")
+    (transient "0.2.0"))
+  :commit "3773112eea3fc99704b5ca50c1e9a3db2cb8e4f3" :keywords
+  '("filename" "convenience")
+  :authors
+  '(("Philippe Vaucher" . "philippe.vaucher@gmail.com"))
+  :maintainer
+  '("Philippe Vaucher" . "philippe.vaucher@gmail.com")
+  :url "https://github.com/Silex/docker.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
