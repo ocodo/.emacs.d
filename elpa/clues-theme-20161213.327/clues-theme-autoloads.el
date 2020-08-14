855d112f14d5c61b303809753ddf45f3d71f3790
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:01 +0800

    [Removing clues-theme version: 20161213.327]

--- elpa/clues-theme-20161213.327/clues-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; clues-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "clues-theme" "clues-theme.el" (22613 6668
-;;;;;;  393034 973000))
-;;; Generated autoloads from clues-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; clues-theme-autoloads.el ends here
