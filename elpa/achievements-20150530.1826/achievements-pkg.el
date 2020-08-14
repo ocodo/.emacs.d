694dfdbb35c654b2cb1a63e082139f8a194312f0
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:11 +0800

    [Updating achievements version: 20150531.1317][removing old version: 20150530.1826]

--- elpa/achievements-20150530.1826/achievements-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "achievements" "20150530.1826" "Achievements for emacs usage."
-  '((keyfreq "0.0.3")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/achievements-20150531.1317/achievements-pkg.el
@@ -0,0 +1,6 @@
+(define-package "achievements" "20150531.1317" "Achievements for emacs usage."
+  '((keyfreq "0.0.3"))
+  :commit "c8275ee492d56255999d58f2988129ab29145182")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
