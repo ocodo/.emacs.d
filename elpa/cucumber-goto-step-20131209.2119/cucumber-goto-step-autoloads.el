1a3aab2b40df426a553f7e3f86da4e26ef701b0e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:35 +0800

    [Removing cucumber-goto-step version: 20131209.2119]

--- elpa/cucumber-goto-step-20131209.2119/cucumber-goto-step-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; cucumber-goto-step-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (jump-to-cucumber-step) "cucumber-goto-step" "cucumber-goto-step.el"
-;;;;;;  (21163 37436 0 0))
-;;; Generated autoloads from cucumber-goto-step.el
-
-(autoload 'jump-to-cucumber-step "cucumber-goto-step" "\
-Jumps to a step definition.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("cucumber-goto-step-pkg.el") (21163 37436
-;;;;;;  403739 0))
-
-;;;***
-
-(provide 'cucumber-goto-step-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; cucumber-goto-step-autoloads.el ends here
