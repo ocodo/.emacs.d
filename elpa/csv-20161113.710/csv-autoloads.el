902c1e79aa190360cfe50d1e0ed7af778936d8da
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:35:33 +0800

    [Removing csv version: 20161113.710]

--- elpa/csv-20161113.710/csv-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; csv-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("csv.el") (22955 52836 8433 318000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; csv-autoloads.el ends here
--- elpa/csv-20161113.710/csv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "csv" "20161113.710" "Functions for reading and parsing CSV files." 'nil :commit "aa1dfa1263565d5fac3879c21d8ddf5f8915e411" :keywords '("extensions" "data" "csv"))
