6eed68ac0f2a02c0c75825a637ddb9ecb9de82fb
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:00 +0800

    [Removing pt version: 20161226.1159]

--- elpa/pt-20161226.1159/pt-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pt" "20161226.1159" "A front-end for pt, The Platinum Searcher." 'nil :url "https://github.com/bling/pt.el" :keywords '("pt" "ack" "ag" "grep" "search"))
