7c2462501e8839295c254408622c3658e98fd761
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:25 +0800

    [Removing yasnippet versions: 20180412.1548,20180620.1750,20181015.1212]

--- elpa/yasnippet-20180412.1548/yasnippet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yasnippet" "20180412.1548" "Yet another snippet extension for Emacs." '((cl-lib "0.5")) :commit "f9231b0e3d43cfcf69198f72af3a048ca60aec6e" :keywords '("convenience" "emulation") :url "http://github.com/joaotavora/yasnippet")
