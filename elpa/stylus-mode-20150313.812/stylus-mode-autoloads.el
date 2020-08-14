bc0e2433e5da49353e91ad48c323c030f02d072b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:45 +0800

    [Removing stylus-mode version: 20150313.812]

--- elpa/stylus-mode-20150313.812/stylus-mode-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; stylus-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "stylus-mode" "stylus-mode.el" (21893 22855
-;;;;;;  895401 0))
-;;; Generated autoloads from stylus-mode.el
-
-(autoload 'stylus-mode "stylus-mode" "\
-Major mode for editing stylus node.js templates
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.styl\\'" . stylus-mode))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; stylus-mode-autoloads.el ends here
--- elpa/stylus-mode-20150313.812/stylus-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "stylus-mode" "20150313.812" "Major mode for editing .jade files" '((sws-mode "0")) :url "https://github.com/brianc/jade-mode")
