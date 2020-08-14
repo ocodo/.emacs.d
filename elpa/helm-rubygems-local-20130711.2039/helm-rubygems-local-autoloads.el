5d8d424bab4b7d7835209ccc3d0bbafe756f6b0a
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:39:16 +0800

    [Removing helm-rubygems-local version: 20130711.2039]

--- elpa/helm-rubygems-local-20130711.2039/helm-rubygems-local-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; helm-rubygems-local-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (helm-rubygems-local) "helm-rubygems-local" "helm-rubygems-local.el"
-;;;;;;  (20960 49073 0 0))
-;;; Generated autoloads from helm-rubygems-local.el
-
-(autoload 'helm-rubygems-local "helm-rubygems-local" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("helm-rubygems-local-pkg.el") (20960 49073
-;;;;;;  803758 0))
-
-;;;***
-
-(provide 'helm-rubygems-local-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; helm-rubygems-local-autoloads.el ends here
--- elpa/helm-rubygems-local-20130711.2039/helm-rubygems-local-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-rubygems-local" "20130711.2039" "Installed local rubygems find-file for helm [github]" (quote ((helm "1.5.3"))))
