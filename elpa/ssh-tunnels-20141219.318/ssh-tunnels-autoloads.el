1fad521adf4119de14f5c4f5051041c1d8b90b4c
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:42 +0800

    [Removing ssh-tunnels versions: 20141219.318,20181129.1536]

--- elpa/ssh-tunnels-20141219.318/ssh-tunnels-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; ssh-tunnels-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "ssh-tunnels" "ssh-tunnels.el" (21664 42687
-;;;;;;  659401 0))
-;;; Generated autoloads from ssh-tunnels.el
-
-(autoload 'ssh-tunnels "ssh-tunnels" "\
-View and manipulate SSH tunnels.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ssh-tunnels-autoloads.el ends here
--- elpa/ssh-tunnels-20141219.318/ssh-tunnels-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ssh-tunnels" "20141219.318" "Manage SSH tunnels" '((cl-lib "0.5") (emacs "24")) :url "http://github.com/death/ssh-tunnels" :keywords '("tools" "convenience"))
