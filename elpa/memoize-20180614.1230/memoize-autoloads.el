a1009fb09cc576198f28f58e0a5226f25ef7072e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:11 +0800

    [Removing memoize versions: 20170720.1802,20180614.1230,20180614.1930]

--- elpa/memoize-20180614.1230/memoize-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; memoize-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("memoize.el") (23344 44844 551783 618000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; memoize-autoloads.el ends here
--- elpa/memoize-20180614.1230/memoize-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "memoize" "20180614.1230" "Memoization functions" 'nil :commit "9a561268ffb550b257a08710489a95cd087998b6" :url "https://github.com/skeeto/emacs-memoize")
