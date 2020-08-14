d27a86a11dde8014808ec6fc5b250a8e6c04185f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:07 +0800

    [Removing vagrant version: 20170301.1406]

--- elpa/vagrant-20170301.1406/vagrant-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "vagrant" "20170301.1406" "Manage a vagrant box from emacs" 'nil :url "https://github.com/ottbot/vagrant.el" :keywords '("vagrant" "chef"))
