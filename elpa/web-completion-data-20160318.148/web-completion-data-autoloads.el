76890c09a8555d92ac47894bf810c690041c2765
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:14 +0800

    [Removing web-completion-data version: 20160318.148]

--- elpa/web-completion-data-20160318.148/web-completion-data-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; web-completion-data-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("web-completion-data-pkg.el" "web-completion-data.el")
-;;;;;;  (22253 3177 295190 559000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; web-completion-data-autoloads.el ends here
--- elpa/web-completion-data-20160318.148/web-completion-data-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "web-completion-data" "20160318.148" "Shared completion data for ac-html and company-web" 'nil :url "https://github.com/osv/web-completion-data" :keywords
-  '("html" "auto-complete" "company"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
