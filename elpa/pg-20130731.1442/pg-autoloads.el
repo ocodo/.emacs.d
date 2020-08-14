df6e0554100221201779e312bada18433c2d9952
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:49 +0800

    [Removing pg version: 20130731.1442]

--- elpa/pg-20130731.1442/pg-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; pg-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("pg.el") (21440 45247 325234 852000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; pg-autoloads.el ends here
--- elpa/pg-20130731.1442/pg-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pg" "20130731.1442" "Emacs Lisp interface to the PostgreSQL RDBMS" 'nil)
