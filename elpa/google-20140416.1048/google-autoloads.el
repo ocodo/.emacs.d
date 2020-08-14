b57055773220d2ae84ff53a32f6c5ceea8235aed
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:20 +0800

    [Removing google version: 20140416.1048]

--- elpa/google-20140416.1048/google-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; google-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("google.el") (22253 5333 753382 727000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; google-autoloads.el ends here
--- elpa/google-20140416.1048/google-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "google" "20140416.1048" "Emacs interface to the Google API" 'nil :stars '(#("7" 0 1 (font-lock-face paradox-star-face))) :keywords '("comm" "processes" "tools"))
