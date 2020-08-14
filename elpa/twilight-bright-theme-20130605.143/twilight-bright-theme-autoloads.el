e0a4df6d474ab081e1e3bb3fb0c725054c4fb939
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:01 +0800

    [Removing twilight-bright-theme version: 20130605.143]

--- elpa/twilight-bright-theme-20130605.143/twilight-bright-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; twilight-bright-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "twilight-bright-theme" "twilight-bright-theme.el"
-;;;;;;  (21412 9265 0 0))
-;;; Generated autoloads from twilight-bright-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("twilight-bright-theme-pkg.el") (21412
-;;;;;;  9265 168861 0))
-
-;;;***
-
-(provide 'twilight-bright-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; twilight-bright-theme-autoloads.el ends here
--- elpa/twilight-bright-theme-20130605.143/twilight-bright-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "twilight-bright-theme" "20130605.143" "A Emacs 24 faces port of the TextMate theme" (quote nil))
