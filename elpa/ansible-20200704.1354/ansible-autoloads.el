6b6367b025d33cb2b3f9b63a76d10a23ad38107e
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:34:24 +0800

    [Updating ansible version: 20200704.1354][removing old version: 20180813.114]

--- elpa/ansible-20200704.1354/ansible-autoloads.el
+++ elpa/ansible-20200704.1354/ansible-autoloads.el
@@ -9,20 +9,23 @@
 ;;;### (autoloads nil "ansible" "ansible.el" (0 0 0 0))
 ;;; Generated autoloads from ansible.el
 
-(defvar ansible::key-map (make-sparse-keymap) "\
+(defvar ansible-key-map (make-sparse-keymap) "\
 Keymap for Ansible.")
 
 (autoload 'ansible "ansible" "\
 Ansible minor mode.
 
+If called interactively, enable Ansible mode if ARG is positive,
+and disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it if
+ARG is `toggle'; disable the mode otherwise.
+
 \(fn &optional ARG)" t nil)
 
-(autoload 'ansible::dict-initialize "ansible" "\
-Initialize Ansible auto-complete.
+(autoload 'ansible-dict-initialize "ansible" "\
+Initialize Ansible auto-complete." nil nil)
 
-\(fn)" nil nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ansible" '("ansible::")))
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ansible" '("ansible-")))
 
 ;;;***
 
--- elpa/ansible-20200704.1354/ansible-pkg.el
+++ elpa/ansible-20200704.1354/ansible-pkg.el
@@ -1,7 +1,7 @@
-(define-package "ansible" "20180813.114" "Ansible minor mode"
+(define-package "ansible" "20200704.1354" "Ansible minor mode"
   '((s "1.9.0")
     (f "0.16.2"))
-  :authors
+  :commit "b5ef59406604bc5027f4d816d90e633feef0149c" :authors
   '(("k1LoW (Kenichirou Oyama), <k1lowxb [at] gmail [dot] com> <k1low [at] 101000lab [dot] org>"))
   :maintainer
   '("k1LoW (Kenichirou Oyama), <k1lowxb [at] gmail [dot] com> <k1low [at] 101000lab [dot] org>")
