c66a5646a93285a7c8bb11f12cb1c416a987fe5a
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:38 +0800

    [Removing flymake-sass version: 20170722.1846]

--- elpa/flymake-sass-20170722.1846/flymake-sass-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-sass" "20170722.1846" "Flymake handler for sass and scss files" '((flymake-easy "0.1")) :commit "2de28148e92deb93bff3d55fe14e7c67ac476056" :url "https://github.com/purcell/flymake-sass")
