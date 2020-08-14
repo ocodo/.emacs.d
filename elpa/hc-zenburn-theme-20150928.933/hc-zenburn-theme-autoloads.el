1d117b3449dff04b5ad835866e31a12da84ecca0
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:38:43 +0800

    [Removing hc-zenburn-theme version: 20150928.933]

--- elpa/hc-zenburn-theme-20150928.933/hc-zenburn-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; hc-zenburn-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "hc-zenburn-theme" "hc-zenburn-theme.el" (22055
-;;;;;;  13779 0 0))
-;;; Generated autoloads from hc-zenburn-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; hc-zenburn-theme-autoloads.el ends here
--- elpa/hc-zenburn-theme-20150928.933/hc-zenburn-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "hc-zenburn-theme" "20150928.933" "An higher contrast version of the Zenburn theme." 'nil :url "https:github.com/edran/hc-zenburn-emacs")
