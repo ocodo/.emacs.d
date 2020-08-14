afbae6a43091a9d4e4332fe13833f3320c56fa01
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:35 +0800

    [Adding language-id version: 20200726.1813]

---
+++ elpa/language-id-20200726.1813/language-id-autoloads.el
@@ -0,0 +1,22 @@
+;;; language-id-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "language-id" "language-id.el" (0 0 0 0))
+;;; Generated autoloads from language-id.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "language-id" '("language-id-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; language-id-autoloads.el ends here
---
+++ elpa/language-id-20200726.1813/language-id-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/language-id-20200726.1813/language-id.el  -*- no-byte-compile: t -*-
+(define-package "language-id" "20200726.1813" "Library to work with programming language identifiers" '((emacs "24") (cl-lib "0.5")) :commit "aa541a4461a07add17374fd56aef6e2fd1a61c60" :keywords '("languages" "util") :authors '(("Lassi Kortela" . "lassi@lassi.io")) :maintainer '("Lassi Kortela" . "lassi@lassi.io") :url "https://github.com/lassik/emacs-language-id")
