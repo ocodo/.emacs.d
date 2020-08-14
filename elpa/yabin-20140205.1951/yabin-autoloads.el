bf99ef6d91d79b8e5b77222c72514795211166f1
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:43:24 +0800

    [Removing yabin version: 20140205.1951]

--- elpa/yabin-20140205.1951/yabin-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; yabin-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("yabin.el") (22309 46725 790761 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; yabin-autoloads.el ends here
--- elpa/yabin-20140205.1951/yabin-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "yabin" "20140205.1951" "Yet Another Bignum package (A thin wrapper of calc.el)." 'nil :keywords '("data"))
