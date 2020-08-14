ee71bc495c46e394b07e31aaa7a45f50252d1c8b
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:28 +0800

    [Removing signal version: 20160816.738]

--- elpa/signal-20160816.738/signal-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; signal-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "signal" "signal.el" (0 0 0 0))
-;;; Generated autoloads from signal.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "signal" '("undefsignal" "defsignal")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; signal-autoloads.el ends here
--- elpa/signal-20160816.738/signal-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "signal" "20160816.738" "Advanced hook" '((emacs "24") (cl-lib "0.5")) :url "https://github.com/mola-T/signal" :keywords '("internal" "lisp" "processes" "tools"))
