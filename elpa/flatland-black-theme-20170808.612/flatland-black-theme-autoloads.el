e8510e1841c80a9f5f6ee9a97af090c80d75f9bd
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:37:15 +0800

    [Removing flatland-black-theme version: 20170808.612]

--- elpa/flatland-black-theme-20170808.612/flatland-black-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; flatland-black-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "flatland-black-theme" "flatland-black-theme.el"
-;;;;;;  (22955 53529 501702 7000))
-;;; Generated autoloads from flatland-black-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flatland-black-theme-autoloads.el ends here
