d3824cab2da3948af88506f2a3d39bc700fd70a6
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:41 +0800

    [Removing sr-speedbar version: 20161025.131]

--- elpa/sr-speedbar-20161025.131/sr-speedbar-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "sr-speedbar" "20161025.131" "Same frame speedbar" 'nil :url "http://www.emacswiki.org/emacs/download/sr-speedbar.el" :keywords '("speedbar" "sr-speedbar.el"))
