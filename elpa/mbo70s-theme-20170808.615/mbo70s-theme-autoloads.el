8a7b6b408760852f238c060927148e38609f8940
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:08 +0800

    [Removing mbo70s-theme version: 20170808.615]

--- elpa/mbo70s-theme-20170808.615/mbo70s-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; mbo70s-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "mbo70s-theme" "mbo70s-theme.el" (22955 53311
-;;;;;;  601487 932000))
-;;; Generated autoloads from mbo70s-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; mbo70s-theme-autoloads.el ends here
