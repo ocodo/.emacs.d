a7d8a1f08e9c0c2176f999e4f282f81d8aa1fc2f
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:40:16 +0800

    [Removing js3-mode version: 20160515.850]

--- elpa/js3-mode-20160515.850/js3-mode-autoloads.el
+++
@@ -1,43 +0,0 @@
-;;; js3-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "js3" "js3.el" (22338 19797 467451 89000))
-;;; Generated autoloads from js3.el
- (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
-
-(autoload 'js3-mode "js3" "\
-Major mode for editing JavaScript code.
-
-\\{js3-mode-map}
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "js3-mode" "js3-mode.el" (22338 19797 459451
-;;;;;;  102000))
-;;; Generated autoloads from js3-mode.el
- (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
-
-(autoload 'js3-mode "js3-mode" "\
-Major mode for editing JavaScript code.
-
-\\{js3-mode-map}
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("js3-mode-pkg.el") (22338 19797 484498
-;;;;;;  196000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; js3-mode-autoloads.el ends here
--- elpa/js3-mode-20160515.850/js3-mode-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "js3-mode" "20160515.850" "An improved JavaScript editing mode" 'nil :keywords
-  '("javascript" "languages"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
