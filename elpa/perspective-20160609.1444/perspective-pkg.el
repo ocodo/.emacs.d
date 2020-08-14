f2232e3f1c905229651a712b085f4374c501af05
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:49 +0800

    [Removing perspective versions: 20160609.1444,20180614.1533,20181120.114]

--- elpa/perspective-20160609.1444/perspective-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "perspective" "20160609.1444" "switch between named \"perspectives\" of the editor" '((cl-lib "0.5")) :url "http://github.com/nex3/perspective-el" :keywords '("workspace" "convenience" "frames"))
