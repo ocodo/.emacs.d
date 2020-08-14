dfa3d5723ec486dbf383261229bc8d38b6d495fc
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:41:09 +0800

    [Removing mc-extras versions: 20170824.2239,20180519.2139]

--- elpa/mc-extras-20170824.2239/mc-extras-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "mc-extras" "20170824.2239" "Extra functions for multiple-cursors mode."
-  '((multiple-cursors "1.2.1"))
-  :url "https://github.com/knu/mc-extras.el" :keywords
-  '("editing" "cursors"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
