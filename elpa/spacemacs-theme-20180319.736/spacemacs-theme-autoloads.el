74f434b47b5f2e0971964b5bcc469de842dd6299
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:42:38 +0800

    [Removing spacemacs-theme versions: 20180319.736,20180618.1129,20181107.1725]

--- elpa/spacemacs-theme-20180319.736/spacemacs-theme-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; spacemacs-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "spacemacs-common" "spacemacs-common.el" (23278
-;;;;;;  48033 0 0))
-;;; Generated autoloads from spacemacs-common.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("spacemacs-dark-theme.el" "spacemacs-light-theme.el"
-;;;;;;  "spacemacs-theme-pkg.el") (23278 48033 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; spacemacs-theme-autoloads.el ends here
--- elpa/spacemacs-theme-20180618.1129/spacemacs-theme-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; spacemacs-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "spacemacs-common" "spacemacs-common.el" (23344
-;;;;;;  44752 149617 974000))
-;;; Generated autoloads from spacemacs-common.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("spacemacs-dark-theme.el" "spacemacs-light-theme.el"
-;;;;;;  "spacemacs-theme-pkg.el") (23344 44752 149617 974000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; spacemacs-theme-autoloads.el ends here
