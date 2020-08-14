9ed86fb836db0e31665033325abf96d0b75b07d4
jasonm23 <jasonm23@gmail.com>
2020-08-14 11:36:40 +0800

    [Removing esxml version: 20171129.7]

--- elpa/esxml-20171129.7/esxml-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; esxml-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("esxml-pkg.el" "esxml-query.el" "esxml.el")
-;;;;;;  (23138 48748 978402 109000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; esxml-autoloads.el ends here
--- elpa/esxml-20171129.7/esxml-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "esxml" "20171129.7" "Library for working with xml via esxml and sxml" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
