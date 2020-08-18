f757e3b8c2fe9b8d844c3925710c7a9ad9e9f7fd
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:11:43 +0800

    [Adding ivy-clojuredocs version: 20200714.1111]

---
+++ elpa/ivy-clojuredocs-20200714.1111/ivy-clojuredocs-autoloads.el
@@ -0,0 +1,29 @@
+;;; ivy-clojuredocs-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ivy-clojuredocs" "ivy-clojuredocs.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from ivy-clojuredocs.el
+
+(autoload 'ivy-clojuredocs "ivy-clojuredocs" "\
+Search for help at ClojureDocs API." t nil)
+
+(autoload 'ivy-clojuredocs-at-point "ivy-clojuredocs" "\
+Search for help using word at point at ClojureDocs API." t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ivy-clojuredocs" '("ivy-clojuredocs-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ivy-clojuredocs-autoloads.el ends here
