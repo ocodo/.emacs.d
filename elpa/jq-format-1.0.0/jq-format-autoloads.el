35bab25ade99a041e8f9fbc4449476eb187a87c8
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:12:58 +0800

    [Adding jq-format version: 1.0.0]

---
+++ elpa/jq-format-1.0.0/jq-format-autoloads.el
@@ -0,0 +1,28 @@
+;;; jq-format-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "jq-format" "jq-format.el" (0 0 0 0))
+;;; Generated autoloads from jq-format.el
+ (autoload 'jq-format-json-buffer "jq-format" nil t)
+ (autoload 'jq-format-json-region "jq-format" nil t)
+ (autoload 'jq-format-json-on-save-mode "jq-format" nil t)
+ (autoload 'jq-format-jsonlines-buffer "jq-format" nil t)
+ (autoload 'jq-format-jsonlines-region "jq-format" nil t)
+ (autoload 'jq-format-jsonlines-on-save-mode "jq-format" nil t)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "jq-format" '("jq-format-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; jq-format-autoloads.el ends here
