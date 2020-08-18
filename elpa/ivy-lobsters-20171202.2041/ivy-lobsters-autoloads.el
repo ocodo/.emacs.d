3fcd769417757dc5847df80faccbb65b4f873d6d
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:12:53 +0800

    [Adding ivy-lobsters version: 20171202.2041]

---
+++ elpa/ivy-lobsters-20171202.2041/ivy-lobsters-autoloads.el
@@ -0,0 +1,25 @@
+;;; ivy-lobsters-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ivy-lobsters" "ivy-lobsters.el" (0 0 0 0))
+;;; Generated autoloads from ivy-lobsters.el
+
+(autoload 'ivy-lobsters "ivy-lobsters" "\
+Show latest lobste.rs stories." t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ivy-lobsters" '("ivy-lobsters-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ivy-lobsters-autoloads.el ends here
