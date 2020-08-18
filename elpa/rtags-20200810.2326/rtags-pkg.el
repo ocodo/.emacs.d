3c0a4764cc65f631a54ef7897697a8e48c97520c
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:13:01 +0800

    [Adding rtags version: 20200810.2326]

---
+++ elpa/rtags-20200810.2326/rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/rtags-20200810.2326/rtags.el  -*- no-byte-compile: t -*-
+(define-package "rtags" "20200810.2326" "A front-end for rtags" '((emacs "24.3")) :commit "b57b36039f6411f23009c4ec0315ca5a7adb6824" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "https://github.com/Andersbakken/rtags")
