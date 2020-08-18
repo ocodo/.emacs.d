ce26a39460c100b1e68909995f456d61dbb29764
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:12:55 +0800

    [Adding ivy-rich version: 20200601.104]

---
+++ elpa/ivy-rich-20200601.104/ivy-rich-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/ivy-rich-20200601.104/ivy-rich.el  -*- no-byte-compile: t -*-
+(define-package "ivy-rich" "20200601.104" "More friendly display transformer for ivy" '((emacs "25.1") (ivy "0.13.0")) :commit "10970130b41c6ef9570893cdab8dfbe720e2b1a9" :keywords '("convenience" "ivy") :authors '(("Yevgnen Koh" . "wherejoystarts@gmail.com")) :maintainer '("Yevgnen Koh" . "wherejoystarts@gmail.com") :url "https://github.com/Yevgnen/ivy-rich")
