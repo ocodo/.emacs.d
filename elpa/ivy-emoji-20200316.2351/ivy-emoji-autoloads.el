e31d557f88ec8dfe56c282eb1089b9cbd07f0462
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:11:56 +0800

    [Adding ivy-emoji version: 20200316.2351]

---
+++ elpa/ivy-emoji-20200316.2351/ivy-emoji-autoloads.el
@@ -0,0 +1,25 @@
+;;; ivy-emoji-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ivy-emoji" "ivy-emoji.el" (0 0 0 0))
+;;; Generated autoloads from ivy-emoji.el
+
+(autoload 'ivy-emoji "ivy-emoji" "\
+Select an emoji and insert it." t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ivy-emoji" '("ivy-emoji-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ivy-emoji-autoloads.el ends here
---
+++ elpa/ivy-emoji-20200316.2351/ivy-emoji-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/ivy-emoji-20200316.2351/ivy-emoji.el  -*- no-byte-compile: t -*-
+(define-package "ivy-emoji" "20200316.2351" "Insert emojis with ivy" '((emacs "26.1") (ivy "0.13.0")) :commit "a1b7d32048278afd9b06536a8af96f533639d146" :keywords '("emoji" "ivy" "convenience") :authors '(("Gabriele Bozzola" . "sbozzolator@gmail.com")) :maintainer '("Gabriele Bozzola" . "sbozzolator@gmail.com") :url "https://github.com/sbozzolo/ivy-emoji.git")
