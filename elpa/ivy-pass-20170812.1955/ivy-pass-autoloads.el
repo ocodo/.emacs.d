152ff677cae5ec0ca755ea1ba430088ebf61e523
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:12:54 +0800

    [Adding ivy-pass version: 20170812.1955]

---
+++ elpa/ivy-pass-20170812.1955/ivy-pass-autoloads.el
@@ -0,0 +1,25 @@
+;;; ivy-pass-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ivy-pass" "ivy-pass.el" (0 0 0 0))
+;;; Generated autoloads from ivy-pass.el
+
+(autoload 'ivy-pass "ivy-pass" "\
+Select an entry and copy its password to the kill ring." t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ivy-pass" '("ivy-pass-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ivy-pass-autoloads.el ends here
