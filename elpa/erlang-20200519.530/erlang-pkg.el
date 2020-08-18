6d950373601099fe43e940461b1642c3c7965b09
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:11:03 +0800

    [Adding erlang version: 20200519.530]

---
+++ elpa/erlang-20200519.530/erlang-pkg.el
@@ -0,0 +1,6 @@
+(define-package "erlang" "20200519.530" "Erlang major mode"
+  '((emacs "24.1"))
+  :commit "4ca912b859f779d6d9b235ea0cf6fb7662edcc59")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
