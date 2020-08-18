e936df26e135b8dd1e9b79c6601ebcdf0f408d71
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:12:51 +0800

    [Adding ivy-erlang-complete version: 20191112.1137]

---
+++ elpa/ivy-erlang-complete-20191112.1137/ivy-erlang-complete-pkg.el
@@ -0,0 +1,10 @@
+(define-package "ivy-erlang-complete" "20191112.1137" "Erlang context sensitive completion at point using ivy. It also support xref and eldoc."
+  '((async "1.9")
+    (counsel "0.11.0")
+    (ivy "0.11.0")
+    (erlang "19.2")
+    (emacs "25.1"))
+  :commit "c443dba0c466d36bef01a8985474f5da0a5a65fe")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
