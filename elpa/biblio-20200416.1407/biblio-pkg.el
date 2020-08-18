e21045fc45b872af47da147fabf5b4dab2d9179c
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:11:01 +0800

    [Adding biblio version: 20200416.1407]

---
+++ elpa/biblio-20200416.1407/biblio-pkg.el
@@ -0,0 +1,7 @@
+(define-package "biblio" "20200416.1407" "Browse and import bibliographic references from CrossRef, arXiv, DBLP, HAL, Dissemin, and doi.org"
+  '((emacs "24.3")
+    (biblio-core "0.2"))
+  :commit "eb9baf1d2bf6a073d24ccb717025baa693e98f3e")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
