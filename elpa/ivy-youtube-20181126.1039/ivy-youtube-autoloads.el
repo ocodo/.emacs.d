348e29eeb02e90729857e2aa1c33c77d27d3cca0
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:12:57 +0800

    [Adding ivy-youtube version: 20181126.1039]

---
+++ elpa/ivy-youtube-20181126.1039/ivy-youtube-autoloads.el
@@ -0,0 +1,24 @@
+;;; ivy-youtube-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ivy-youtube" "ivy-youtube.el" (0 0 0 0))
+;;; Generated autoloads from ivy-youtube.el
+
+(autoload 'ivy-youtube "ivy-youtube" nil t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ivy-youtube" '("ivy-youtube-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ivy-youtube-autoloads.el ends here
