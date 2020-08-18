2e26deeca2f154ee979b52f3c08c500f14165b4f
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:11:02 +0800

    [Adding emms version: 20200716.1815]

---
+++ elpa/emms-20200716.1815/emms-pkg.el
@@ -0,0 +1,12 @@
+(define-package "emms" "20200716.1815" "The Emacs Multimedia System"
+  '((cl-lib "0.5"))
+  :commit "36d760e2bd7e5376aff3f03456fa6445833242ad" :keywords
+  '("emms" "mp3" "ogg" "flac" "music" "mpeg" "video" "multimedia")
+  :authors
+  '(("Jorgen Schäfer" . "forcer@forcix.cx"))
+  :maintainer
+  '("Yoni Rabkin" . "yrk@gnu.org")
+  :url "https://www.gnu.org/software/emms/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
