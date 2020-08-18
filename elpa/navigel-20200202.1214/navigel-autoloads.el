ae3fd3b631bf3c2f0c925e58320db56bdf451ee5
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:12:59 +0800

    [Adding navigel version: 20200202.1214]

---
+++ elpa/navigel-20200202.1214/navigel-autoloads.el
@@ -0,0 +1,22 @@
+;;; navigel-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "navigel" "navigel.el" (0 0 0 0))
+;;; Generated autoloads from navigel.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "navigel" '("navigel-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; navigel-autoloads.el ends here
---
+++ elpa/navigel-20200202.1214/navigel-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/navigel-20200202.1214/navigel.el  -*- no-byte-compile: t -*-
+(define-package "navigel" "20200202.1214" "Facilitate the creation of tabulated-list based UIs" '((emacs "25.1") (tablist "1.0")) :commit "0a2d624d6b49f8363badc5ba8699b7028ef85632" :authors '(("Damien Cassou" . "damien@cassou.me")) :maintainer '("Damien Cassou" . "damien@cassou.me") :url "https://gitlab.petton.fr/DamienCassou/navigel")
