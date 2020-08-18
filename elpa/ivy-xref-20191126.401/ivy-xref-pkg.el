9383665bed3f6a2d90a90414ffa8572730f2f93e
jasonm23 <jasonm23@gmail.com>
2020-08-18 12:12:57 +0800

    [Adding ivy-xref version: 20191126.401]

---
+++ elpa/ivy-xref-20191126.401/ivy-xref-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/ivy-xref-20191126.401/ivy-xref.el  -*- no-byte-compile: t -*-
+(define-package "ivy-xref" "20191126.401" "Ivy interface for xref results" '((emacs "25.1") (ivy "0.10.0")) :commit "3d4c35fe2b243d948d8fe02a1f0d76a249d63de9" :authors '(("Alex Murray" . "murray.alex@gmail.com")) :maintainer '("Alex Murray" . "murray.alex@gmail.com") :url "https://github.com/alexmurray/ivy-xref")
