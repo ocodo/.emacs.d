35d28a46ae26d4d822d29268a9df6a6ee87146be
Jason Milkins <jasonm23@gmail.com>
2020-08-26 17:38:31 +0800

    Add magit-20200825.1251

---
+++ elpa/magit-20200825.1251/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "20200825.1251" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (async "20200113")
+    (dash "20200524")
+    (git-commit "20200516")
+    (transient "20200601")
+    (with-editor "20200522"))
+  :commit "99adeff9fcea17eb0f2066f2c22ef938aec10401" :keywords
+  '("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
