605739cbcc5836dd5868d0802095afb7dca148ec
jasonm23 <jasonm23@gmail.com>
2020-08-26 13:49:57 +0800

    [Adding flycheck-crystal version: 20200805.2344]

---
+++ elpa/flycheck-crystal-20200805.2344/flycheck-crystal-autoloads.el
@@ -0,0 +1,23 @@
+;;; flycheck-crystal-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flycheck-crystal" "flycheck-crystal.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from flycheck-crystal.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flycheck-crystal" '("flycheck-crystal-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flycheck-crystal-autoloads.el ends here
---
+++ elpa/flycheck-crystal-20200805.2344/flycheck-crystal-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/flycheck-crystal-20200805.2344/flycheck-crystal.el  -*- no-byte-compile: t -*-
+(define-package "flycheck-crystal" "20200805.2344" "Add support for Crystal to Flycheck" '((flycheck "30")) :commit "f9e4db16ff9fdc6a296363aa35d19cfb4926e472" :keywords '("tools" "crystal") :url "https://github.com/crystal-lang-tools/emacs-crystal-mode")
