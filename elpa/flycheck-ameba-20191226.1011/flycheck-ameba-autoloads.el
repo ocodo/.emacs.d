14a859aa38cd775d01f9c536bb09e435bf1cd6a8
jasonm23 <jasonm23@gmail.com>
2020-09-02 13:40:04 +0800

    [Adding flycheck-ameba version: 20191226.1011]

---
+++ elpa/flycheck-ameba-20191226.1011/flycheck-ameba-autoloads.el
@@ -0,0 +1,26 @@
+;;; flycheck-ameba-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flycheck-ameba" "flycheck-ameba.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from flycheck-ameba.el
+
+(autoload 'flycheck-ameba-setup "flycheck-ameba" "\
+Setup Flycheck Ameba." t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flycheck-ameba" '("flycheck-ameba--find-project-root")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flycheck-ameba-autoloads.el ends here
---
+++ elpa/flycheck-ameba-20191226.1011/flycheck-ameba-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/flycheck-ameba-20191226.1011/flycheck-ameba.el  -*- no-byte-compile: t -*-
+(define-package "flycheck-ameba" "20191226.1011" "Add support for Ameba to Flycheck" '((emacs "24.4") (flycheck "30")) :commit "0c4925ae0e998818326adcb47ed27ddf9761c7dc" :keywords '("tools" "crystal" "ameba") :url "https://github.com/crystal-ameba/ameba.el")
