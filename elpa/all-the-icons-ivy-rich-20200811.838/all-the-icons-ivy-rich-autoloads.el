d2e544dbff938488d5c181f3cff4d7c306889a18
jasonm23 <jasonm23@gmail.com>
2020-09-02 13:40:03 +0800

    [Adding all-the-icons-ivy-rich version: 20200811.838]

---
+++ elpa/all-the-icons-ivy-rich-20200811.838/all-the-icons-ivy-rich-autoloads.el
@@ -0,0 +1,46 @@
+;;; all-the-icons-ivy-rich-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "all-the-icons-ivy-rich" "all-the-icons-ivy-rich.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from all-the-icons-ivy-rich.el
+
+(defvar all-the-icons-ivy-rich-mode nil "\
+Non-nil if All-The-Icons-Ivy-Rich mode is enabled.
+See the `all-the-icons-ivy-rich-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `all-the-icons-ivy-rich-mode'.")
+
+(custom-autoload 'all-the-icons-ivy-rich-mode "all-the-icons-ivy-rich" nil)
+
+(autoload 'all-the-icons-ivy-rich-mode "all-the-icons-ivy-rich" "\
+Better experience with icons for ivy.
+
+If called interactively, enable All-The-Icons-Ivy-Rich mode if
+ARG is positive, and disable it if ARG is zero or negative.  If
+called from Lisp, also enable the mode if ARG is omitted or nil,
+and toggle it if ARG is `toggle'; disable the mode otherwise.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'all-the-icons-ivy-rich-reload "all-the-icons-ivy-rich" "\
+Reload `all-the-icons-ivy-rich'." t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "all-the-icons-ivy-rich" '("all-the-icons-ivy-rich-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; all-the-icons-ivy-rich-autoloads.el ends here
---
+++ elpa/all-the-icons-ivy-rich-20200811.838/all-the-icons-ivy-rich-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/all-the-icons-ivy-rich-20200811.838/all-the-icons-ivy-rich.el  -*- no-byte-compile: t -*-
+(define-package "all-the-icons-ivy-rich" "20200811.838" "Better experience with icons for ivy" '((emacs "25.1") (ivy-rich "0.1.0") (all-the-icons "2.2.0")) :commit "6428cb39729b7290368c5b95f563a320c98f972d" :keywords '("convenience" "icons" "ivy") :authors '(("Vincent Zhang" . "seagle0128@gmail.com")) :maintainer '("Vincent Zhang" . "seagle0128@gmail.com") :url "https://github.com/seagle0128/all-the-icons-ivy-rich")
