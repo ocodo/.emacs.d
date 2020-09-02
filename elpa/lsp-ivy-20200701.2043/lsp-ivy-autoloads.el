809b39ee5a257d67b47b2e1ca7609cb59e6e9639
jasonm23 <jasonm23@gmail.com>
2020-09-02 13:40:04 +0800

    [Adding lsp-ivy version: 20200701.2043]

---
+++ elpa/lsp-ivy-20200701.2043/lsp-ivy-autoloads.el
@@ -0,0 +1,34 @@
+;;; lsp-ivy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "lsp-ivy" "lsp-ivy.el" (0 0 0 0))
+;;; Generated autoloads from lsp-ivy.el
+
+(autoload 'lsp-ivy-workspace-symbol "lsp-ivy" "\
+`ivy' for lsp workspace/symbol.
+When called with prefix ARG the default selection will be symbol at point.
+
+\(fn ARG)" t nil)
+
+(autoload 'lsp-ivy-global-workspace-symbol "lsp-ivy" "\
+`ivy' for lsp workspace/symbol for all of the current workspaces.
+When called with prefix ARG the default selection will be symbol at point.
+
+\(fn ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "lsp-ivy" '("lsp-ivy-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lsp-ivy-autoloads.el ends here
---
+++ elpa/lsp-ivy-20200701.2043/lsp-ivy-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jason/.emacs.d/elpa/lsp-ivy-20200701.2043/lsp-ivy.el  -*- no-byte-compile: t -*-
+(define-package "lsp-ivy" "20200701.2043" "LSP ivy integration" '((emacs "25.1") (dash "2.14.1") (lsp-mode "6.2.1") (ivy "0.13.0")) :commit "4cdb739fc2bc47f7d4dcad824f9240c70c4cb37d" :keywords '("languages" "debug") :url "https://github.com/emacs-lsp/lsp-ivy")
