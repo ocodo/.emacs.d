392cef51bc4d292e9ef4c964b5e9b1b348839e46
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:15:38 +0700

    [Adding ido-completing-read+ version: ]

---
+++ elpa/request-20210816.200/request-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from request.el  -*- no-byte-compile: t -*-
+(define-package "request" "20210816.200" "Compatible layer for URL request" '((emacs "24.4")) :commit "2b68b3ca543f1dfbebb43a44f20601c3947bd729" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
---
+++ elpa/request-deferred-20210214.37/request-deferred-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from request-deferred.el  -*- no-byte-compile: t -*-
+(define-package "request-deferred" "20210214.37" "Wrap request.el by deferred" '((emacs "24.1") (deferred "0.3.1") (request "0.3")) :commit "2b68b3ca543f1dfbebb43a44f20601c3947bd729" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
