aba02c878bbd33fc9eb65c6e7d723841782024b1
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:13:02 +0700

    [Updating go-impl version: 20210621.743][removing old version: 20170125.1552]

---
+++ elpa/go-impl-20210621.743/go-impl-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from go-impl.el  -*- no-byte-compile: t -*-
+(define-package "go-impl" "20210621.743" "impl integration for go-mode" '((emacs "24.3") (go-mode "1.3.0")) :commit "1eebba6ccd02d11a5a82ad4540a8d562797bc3b3" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-go-impl")
