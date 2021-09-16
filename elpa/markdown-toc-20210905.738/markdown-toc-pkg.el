3d3d368e7abb5b6dc87aaa7d31c8c2b114e402b2
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:14:36 +0700

    [Updating markdown-toc version: 20210905.738][removing old version: 20200517.1233]

---
+++ elpa/markdown-toc-20210905.738/markdown-toc-pkg.el
@@ -0,0 +1,14 @@
+(define-package "markdown-toc" "20210905.738" "A simple TOC generator for markdown file"
+  '((s "1.9.0")
+    (dash "2.11.0")
+    (markdown-mode "2.1"))
+  :commit "3d724e518a897343b5ede0b976d6fb46c46bcc01" :authors
+  '((nil . "Antoine R. Dumont (@ardumont)"))
+  :maintainer
+  '(nil . "Antoine R. Dumont (@ardumont)")
+  :keywords
+  '("markdown" "toc" "tools")
+  :url "http://github.com/ardumont/markdown-toc")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
