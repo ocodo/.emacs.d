c9c6ef5f458238c09da39e86a205a16e409e4327
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:12:07 +0700

    [Updating eshell-git-prompt version: 20210817.553][removing old version: 20200109.2250]

---
+++ elpa/eshell-git-prompt-20210817.553/eshell-git-prompt-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from eshell-git-prompt.el  -*- no-byte-compile: t -*-
+(define-package "eshell-git-prompt" "20210817.553" "Some Eshell prompt for Git users" '((emacs "24.1") (cl-lib "0.5") (dash "2.11.0")) :commit "f638ba32b661d15895e767305f59f41eb01197ae" :authors '(("Chunyang Xu" . "mail@xuchunyang.me")) :maintainer '("Chunyang Xu" . "mail@xuchunyang.me") :keywords '("eshell" "git") :url "https://github.com/xuchunyang/eshell-git-prompt")
