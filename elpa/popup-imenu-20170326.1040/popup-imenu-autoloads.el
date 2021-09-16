6218080967fe987ebd89937afad941e05825aa0b
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:15:15 +0700

    [Removing popup versions: 20200610.317,imenu-20170326.1040]

--- elpa/popup-imenu-20170326.1040/popup-imenu-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; popup-imenu-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "popup-imenu" "popup-imenu.el" (0 0 0 0))
-;;; Generated autoloads from popup-imenu.el
-
-(autoload 'popup-imenu "popup-imenu" "\
-Open the popup window with imenu items.
-
-\(fn)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "popup-imenu" '("popup-imenu-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; popup-imenu-autoloads.el ends here
