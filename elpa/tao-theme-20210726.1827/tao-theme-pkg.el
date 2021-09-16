08f4851f83c2f9383caabea0940cdc758c7960d3
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:15:58 +0700

    [Updating tao-theme version: 20210726.1827][removing old version: 20200325.344]

---
+++ elpa/tao-theme-20210726.1827/tao-theme-pkg.el
@@ -0,0 +1,8 @@
+(define-package "tao-theme" "20210726.1827" "This package provides two parametrized uncoloured color themes for Emacs: tao-yin and tao-yang." 'nil :commit "f35b97823f27e8d0f378bbd18b79a61f9e34cc55" :authors
+  '(("Peter Kosov" . "11111000000@email.com"))
+  :maintainer
+  '("Peter Kosov" . "11111000000@email.com")
+  :url "http://github.com/11111000000/tao-theme-emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
