c2aac5cc7a8b8be669dcde99b1896bbd74f34249
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:12:23 +0700

    [Updating expand-region version: 20210708.1952][removing old version: 20200304.1839]

---
+++ elpa/expand-region-20210708.1952/expand-region-pkg.el
@@ -0,0 +1,9 @@
+(define-package "expand-region" "20210708.1952" "Increase selected region by semantic units." 'nil :commit "95a773bd8f557cbd43d3b2dab2fa4417ec5927ab" :authors
+  '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainer
+  '("Magnar Sveen" . "magnars@gmail.com")
+  :keywords
+  '("marking" "region"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
