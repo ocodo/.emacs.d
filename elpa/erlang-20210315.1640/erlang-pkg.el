77c99be610e661c1fc12c3f06aed1874d8d472f3
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:12:06 +0700

    [Updating erlang version: 20210315.1640][removing old version: 20200519.530]

---
+++ elpa/erlang-20210315.1640/erlang-pkg.el
@@ -0,0 +1,11 @@
+(define-package "erlang" "20210315.1640" "Erlang major mode"
+  '((emacs "24.1"))
+  :commit "187aca095bba1b700ba7c11ae439356c8e9f922b" :authors
+  '(("Anders Lindgren"))
+  :maintainer
+  '("Anders Lindgren")
+  :keywords
+  '("erlang" "languages" "processes"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
