2ad4df93da21a415ba0f5be72fdff5022c64d0e9
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:15:30 +0700

    [Updating reformatter version: 20210831.1405][removing old version: 20200813.342]

---
+++ elpa/reformatter-20210831.1405/reformatter-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from reformatter.el  -*- no-byte-compile: t -*-
+(define-package "reformatter" "20210831.1405" "Define commands which run reformatters on the current buffer" '((emacs "24.3")) :commit "b57f5d480003ab7b0880e0059dcc51747fb2e088" :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :keywords '("convenience" "tools") :url "https://github.com/purcell/emacs-reformatter")
