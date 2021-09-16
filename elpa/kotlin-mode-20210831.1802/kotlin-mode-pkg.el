2b4520b916a6176f8d5516e6320e9f53a28b38fc
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:14:09 +0700

    [Updating kotlin-mode version: 20210831.1802][removing old version: 20191102.1510]

---
+++ elpa/kotlin-mode-20210831.1802/kotlin-mode-pkg.el
@@ -0,0 +1,11 @@
+(define-package "kotlin-mode" "20210831.1802" "Major mode for kotlin"
+  '((emacs "24.3"))
+  :commit "876cc27dc105979a0b59782141785f8e172891e8" :authors
+  '(("Shodai Yokoyama" . "quantumcars@gmail.com"))
+  :maintainer
+  '("Shodai Yokoyama" . "quantumcars@gmail.com")
+  :keywords
+  '("languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
