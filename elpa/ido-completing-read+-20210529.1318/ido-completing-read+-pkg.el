d96884570b72e154ca332be9d8fa541eb25997a7
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:18:06 +0700

    Updating ido-completing-read+-20210529.1318 <- 20200520.1535

---
+++ elpa/ido-completing-read+-20210529.1318/ido-completing-read+-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ido-completing-read+.el  -*- no-byte-compile: t -*-
+(define-package "ido-completing-read+" "20210529.1318" "A completing-read-function using ido" '((emacs "24.4") (seq "0.5") (memoize "1.1")) :commit "49e7967ea8c0ab0a206b40d70fc19be115083fa1" :authors '(("Ryan C. Thompson" . "rct@thompsonclan.org")) :maintainer '("Ryan C. Thompson" . "rct@thompsonclan.org") :keywords '("ido" "completion" "convenience") :url "https://github.com/DarwinAwardWinner/ido-completing-read-plus")
