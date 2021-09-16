c04e955258344ad86d2b7b3f04b96608d3a2f545
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:16:24 +0700

    [Adding xref version: 1.2.2]

---
+++ elpa/xref-1.2.2/xref-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from xref.el  -*- no-byte-compile: t -*-
+(define-package "xref" "1.2.2" "Cross-referencing commands" '((emacs "26.1")) :url "https://elpa.gnu.org/packages/xref.html")
