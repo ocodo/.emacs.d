eeabdf02b65ff932a163b61bcc84b8593c1c01a5
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:13:05 +0700

    [Updating google-translate version: 20210406.1138][removing old version: 20200809.1430]

---
+++ elpa/google-translate-20210406.1138/google-translate-pkg.el
@@ -0,0 +1,10 @@
+(define-package "google-translate" "20210406.1138" "Emacs interface to Google Translate." 'nil :commit "0f7f48a09bca064999ecea03102a7c96f52cbd1b" :authors
+  '(("Oleksandr Manzyuk" . "manzyuk@gmail.com"))
+  :maintainer
+  '("Andrey Tykhonov" . "atykhonov@gmail.com")
+  :keywords
+  '("convenience")
+  :url "https://github.com/atykhonov/google-translate")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
