9ebd88a589b3ced725ec29301a4a335ce00ceb0c
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:10:19 +0700

    [Updating avk-emacs-themes version: 20210521.1051][removing old version: 20191220.625]

---
+++ elpa/avk-emacs-themes-20210521.1051/avk-emacs-themes-pkg.el
@@ -0,0 +1,10 @@
+(define-package "avk-emacs-themes" "20210521.1051" "Collection of avk themes" 'nil :commit "7b9b6517873c4d4d73e6e34ca56c54062db60759" :authors
+  '(("Alex V. Koval" . "alex@koval.kharkov.ua"))
+  :maintainer
+  '("Alex V. Koval" . "alex@koval.kharkov.ua")
+  :keywords
+  '("theme")
+  :url "https://github.com/avkoval/avk-emacs-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
