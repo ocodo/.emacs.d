53093c628b567f5a4d52e16971593d7ca28d2968
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:10:17 +0700

    [Updating atom-one-dark-theme version: 20210128.1640][removing old version: 20190705.554]

---
+++ elpa/atom-one-dark-theme-20210128.1640/atom-one-dark-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from atom-one-dark-theme.el  -*- no-byte-compile: t -*-
+(define-package "atom-one-dark-theme" "20210128.1640" "Atom One Dark color theme" 'nil :commit "b34b62e85593812b55ee552a1cb0eecfb04767bb" :authors '(("Jonathan Chu" . "me@jonathanchu.is")) :maintainer '("Jonathan Chu" . "me@jonathanchu.is") :url "https://github.com/jonathanchu/atom-one-dark-theme")
