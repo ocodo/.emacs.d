0d96610135bd456ffe3c6a2bb56e115f28f9a319
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:14:34 +0700

    [Adding ido-completing-read+ version: ]

---
+++ elpa/magit-section-20210829.1849/magit-section-pkg.el
@@ -0,0 +1,13 @@
+(define-package "magit-section" "20210829.1849" "Sections for read-only buffers"
+  '((emacs "25.1")
+    (dash "2.18.1"))
+  :commit "1e40d0021790707f6e88debda04f6b14d9429586" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
