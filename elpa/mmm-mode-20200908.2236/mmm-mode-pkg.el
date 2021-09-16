d00997577eb9590d09b99541b9863f2aec896897
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:14:39 +0700

    [Updating mmm-mode version: 20200908.2236][removing old version: 20200714.1714]

--- elpa/mmm-mode-20200908.2236/mmm-mode-pkg.el
+++ elpa/mmm-mode-20200908.2236/mmm-mode-pkg.el
@@ -1,11 +1,11 @@
-(define-package "mmm-mode" "20200714.1714" "Allow Multiple Major Modes in a buffer"
+(define-package "mmm-mode" "20200908.2236" "Allow Multiple Major Modes in a buffer"
   '((cl-lib "0.2"))
-  :commit "9ffe364f3a31c7a771fe3401b8924642609953e8" :keywords
-  '("convenience" "faces" "languages" "tools")
-  :authors
+  :commit "0d00cdf4d02cc166304f6967a20fa22e2eaf208b" :authors
   '(("Michael Abraham Shulman" . "viritrilbia@gmail.com"))
   :maintainer
   '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :keywords
+  '("convenience" "faces" "languages" "tools")
   :url "https://github.com/purcell/mmm-mode")
 ;; Local Variables:
 ;; no-byte-compile: t
