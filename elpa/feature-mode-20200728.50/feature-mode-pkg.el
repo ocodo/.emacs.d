5aac829e4c633c7ec4a7aa0556380de249f002e8
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:12:24 +0700

    [Updating feature-mode version: 20200815.1617][removing old versions: 20200728.50,20200728.50/snippets/feature-mode,20200728.50/support]

--- elpa/feature-mode-20200728.50/feature-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "feature-mode" "20200728.50" "Major mode for editing Gherkin (i.e. Cucumber) user stories" 'nil :commit "823c5d33ab37a6fbb9786ac6365821f6e5c954d2")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
