02af79984771d51d28f68b2f67db596b8a47b426
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:14:03 +0700

    [Updating js2-mode version: 20210906.2337][removing old version: 20200725.112]

--- elpa/js2-mode-20210906.2337/js2-mode-pkg.el
+++ elpa/js2-mode-20210906.2337/js2-mode-pkg.el
@@ -1,14 +1,14 @@
-(define-package "js2-mode" "20200725.112" "Improved JavaScript editing mode"
+(define-package "js2-mode" "20210906.2337" "Improved JavaScript editing mode"
   '((emacs "24.1")
     (cl-lib "0.5"))
-  :commit "40aab27581279d0fdbfeb9afeb85f39d401a927f" :keywords
-  '("languages" "javascript")
-  :authors
+  :commit "e6a9059fc823a17496e1a5114652d92a9071a78f" :authors
   '(("Steve Yegge" . "steve.yegge@gmail.com")
     ("mooz" . "stillpedant@gmail.com")
     ("Dmitry Gutov" . "dgutov@yandex.ru"))
   :maintainer
   '("Steve Yegge" . "steve.yegge@gmail.com")
+  :keywords
+  '("languages" "javascript")
   :url "https://github.com/mooz/js2-mode/")
 ;; Local Variables:
 ;; no-byte-compile: t
