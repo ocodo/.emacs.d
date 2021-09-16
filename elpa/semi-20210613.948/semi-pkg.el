94ddbf8c27573459d7b8f503ac31f62d7de783e8
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:15:46 +0700

    [Updating semi version: 20210613.948][removing old version: 20200429.641]

---
+++ elpa/semi-20210613.948/semi-pkg.el
@@ -0,0 +1,8 @@
+(define-package "semi" "20210613.948" "A library to provide MIME features."
+  '((emacs "24.5")
+    (apel "10.8")
+    (flim "1.14.9"))
+  :commit "509f6f0bc2f5d020c63e47d9ad89410dc20bcb6f")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
