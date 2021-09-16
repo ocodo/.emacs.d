7d06dc053c7a785d0e55316be7582ac5b1b560c1
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:16:05 +0700

    [Updating visual-regexp version: 20210502.2019][removing old version: 20190414.814]

--- elpa/visual-regexp-20210502.2019/visual-regexp-autoloads.el
+++ elpa/visual-regexp-20210502.2019/visual-regexp-autoloads.el
@@ -12,7 +12,7 @@
 (autoload 'vr/mc-mark "visual-regexp" "\
 Convert regexp selection to multiple cursors.
 
-\(fn REGEXP START END)" nil nil)
+\(fn REGEXP START END)" t nil)
 
 (autoload 'vr/replace "visual-regexp" "\
 Regexp-replace with live visual feedback.
@@ -20,7 +20,7 @@ Regexp-replace with live visual feedback.
 \(fn REGEXP REPLACE START END)" t nil)
 
 (autoload 'vr/query-replace "visual-regexp" "\
-Use vr/query-replace like you would use query-replace-regexp.
+Use `vr/query-replace' like you would use `query-replace-regexp'.
 
 \(fn REGEXP REPLACE START END)" t nil)
 
---
+++ elpa/visual-regexp-20210502.2019/visual-regexp-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from visual-regexp.el  -*- no-byte-compile: t -*-
+(define-package "visual-regexp" "20210502.2019" "A regexp/replace command for Emacs with interactive visual feedback" '((cl-lib "0.2")) :commit "48457d42a5e0fe10fa3a9c15854f1f127ade09b5" :authors '(("Marko Bencun" . "mbencun@gmail.com")) :maintainer '("Marko Bencun" . "mbencun@gmail.com") :keywords '("regexp" "replace" "visual" "feedback") :url "https://github.com/benma/visual-regexp.el/")
