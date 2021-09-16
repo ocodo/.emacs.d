bdd92a939bf0c05b050f9f43856fea432d15a985
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:11:15 +0700

    [Removing company versions: 20200813.2229,quickhelp-20200714.1611,sourcekit-20170126.1153]

---
+++ elpa/company-20210909.1505/company-pkg.el
@@ -0,0 +1,12 @@
+(define-package "company" "20210909.1505" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "1887974e7ad50cb4884fbec27985078bba754b60" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainer
+  '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/company-sourcekit-20210430.2155/company-sourcekit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from company-sourcekit.el  -*- no-byte-compile: t -*-
+(define-package "company-sourcekit" "20210430.2155" "company-mode completion backend for SourceKit" '((emacs "24.3") (company "0.8.12") (dash "2.18.0") (sourcekit "0.2.0")) :commit "a1860ad4dd3a542acd2fa0dfac2a388cbdf4af0c" :authors '(("Nathan Kot" . "nk@nathankot.com")) :maintainer '("Nathan Kot" . "nk@nathankot.com") :keywords '("abbrev") :url "https://github.com/nathankot/company-sourcekit")
