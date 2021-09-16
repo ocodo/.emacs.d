a716b22ba36be0ecd67646a41d81f2b401c68042
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:11:26 +0700

    [Updating csharp-mode version: 20210826.421][removing old version: 20200728.1113]

---
+++ elpa/csharp-mode-20210826.421/csharp-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "csharp-mode" "20210826.421" "C# mode derived mode"
+  '((emacs "26.1"))
+  :commit "fe8a68e9849fc7617e0c870cacd6599b8a797638" :authors
+  '(("Theodor Thornhill" . "theo@thornhill.no"))
+  :maintainer
+  '("Jostein Kjønigsen" . "jostein@gmail.com")
+  :keywords
+  '("c#" "languages" "oop" "mode")
+  :url "https://github.com/emacs-csharp/csharp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
