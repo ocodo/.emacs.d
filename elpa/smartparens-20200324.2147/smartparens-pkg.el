55cac49984962300a06c4a6f7acc4335ebdc46be
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:15:52 +0700

    [Updating smartparens version: 20210904.1621][removing old version: 20200324.2147]

--- elpa/smartparens-20200324.2147/smartparens-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "smartparens" "20200324.2147" "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
-  '((dash "2.13.0")
-    (cl-lib "0.3"))
-  :commit "555626a43f9bb1985aa9a0eb675f2b88b29702c8")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/smartparens-20210904.1621/smartparens-pkg.el
@@ -0,0 +1,13 @@
+(define-package "smartparens" "20210904.1621" "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
+  '((dash "2.13.0")
+    (cl-lib "0.3"))
+  :commit "2834c66c4f09778d0c57e99886c329188eed591a" :authors
+  '(("Matus Goljer" . "matus.goljer@gmail.com"))
+  :maintainer
+  '("Matus Goljer" . "matus.goljer@gmail.com")
+  :keywords
+  '("abbrev" "convenience" "editing")
+  :url "https://github.com/Fuco1/smartparens")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
