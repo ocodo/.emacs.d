4397aee458703c412af8a342a092709486b39b9b
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:10:14 +0700

    [Updating apache-mode version: 20210519.1931][removing old version: 20190711.1111]

---
+++ elpa/apache-mode-20210519.1931/apache-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from apache-mode.el  -*- no-byte-compile: t -*-
+(define-package "apache-mode" "20210519.1931" "Major mode for editing Apache httpd configuration files" 'nil :commit "f2c11aac2f5fc598123e04f4604bea248689a117" :authors '(("Karl Chen" . "quarl@nospam.quarl.org")) :maintainer '("USAMI Kenta" . "tadsan@zonu.me") :keywords '("languages" "faces") :url "https://github.com/emacs-php/apache-mode")
