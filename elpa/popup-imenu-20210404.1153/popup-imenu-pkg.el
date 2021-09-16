a76821cc7d66a8e5b739c59a42f5c2eed61071fc
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:15:15 +0700

    [Adding ido-completing-read+ version: ]

---
+++ elpa/popup-imenu-20210404.1153/popup-imenu-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from popup-imenu.el  -*- no-byte-compile: t -*-
+(define-package "popup-imenu" "20210404.1153" "imenu index popup" '((dash "2.12.1") (popup "0.5.3") (flx-ido "0.6.1")) :commit "b00c4d503cbbaf01c136b1647329e6a6257d012c" :authors '(("Igor Shymko" . "igor.shimko@gmail.com")) :maintainer '("Igor Shymko" . "igor.shimko@gmail.com") :keywords '("popup" "imenu") :url "https://github.com/ancane/popup-imenu")
