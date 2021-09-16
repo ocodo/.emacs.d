58b60620790f466511d493f3e95c0b27a95784bd
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:14:23 +0700

    [Updating logito version: 20201226.534][removing old version: 20120225.2155]

--- elpa/logito-20120225.2155/logito-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; logito-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("logito-pkg.el" "logito.el") (20729 12545
-;;;;;;  120780))
-
-;;;***
-
-(provide 'logito-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; logito-autoloads.el ends here
