22855611bf79752be18392b2d6fd711c78965204
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:12:55 +0700

    [Updating fuzzy version: 20210317.140][removing old version: 20150730.337]

---
+++ elpa/fuzzy-20210317.140/fuzzy-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from fuzzy.el  -*- no-byte-compile: t -*-
+(define-package "fuzzy" "20210317.140" "Fuzzy Matching" '((emacs "24.3")) :commit "edce468a3077d03ca63e1559f7ffebfa09c8effd" :authors '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com")) :maintainer '("Tomohiro Matsuyama" . "m2ym.pub@gmail.com") :keywords '("convenience") :url "https://github.com/auto-complete/fuzzy-el")
