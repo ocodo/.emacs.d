4e099dfe5d9fcd84303da402e0c1e7912d412275
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:15:03 +0700

    [Updating parseedn version: 20210729.1657][removing old version: 20200419.1124]

---
+++ elpa/parseedn-20210729.1657/parseedn-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from parseedn.el  -*- no-byte-compile: t -*-
+(define-package "parseedn" "20210729.1657" "Clojure/EDN parser" '((emacs "25") (a "0.1.0alpha4") (parseclj "0.1.0")) :commit "7b9ca20b398ca0ca0e3005e84c16f23aab49b667" :authors '(("Arne Brasseur" . "arne@arnebrasseur.net")) :maintainer '("Arne Brasseur" . "arne@arnebrasseur.net") :keywords '("lisp" "clojure" "edn" "parser"))
