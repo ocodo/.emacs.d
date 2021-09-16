871284262115fd17964218f0de54c6d083f56ab4
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:12:01 +0700

    [Updating elixir-mode version: 20210509.2353][removing old version: 20200723.1750]

--- elpa/elixir-mode-20200723.1750/elixir-mode-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "elixir-mode" "20200723.1750" "Major mode for editing Elixir files"
-  '((emacs "24")
-    (pkg-info "0.4"))
-  :commit "01b332495d3f44addeb236428041c4ffa0c2ca3b" :keywords
-  '("languages" "elixir")
-  :url "https://github.com/elixir-editors/emacs-elixir")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/elixir-mode-20210509.2353/elixir-mode-pkg.el
@@ -0,0 +1,9 @@
+(define-package "elixir-mode" "20210509.2353" "Major mode for editing Elixir files"
+  '((emacs "25")
+    (pkg-info "0.6"))
+  :commit "6bbc1e5ac46064613c982cedc60566ed077e7a58" :keywords
+  '("languages" "elixir")
+  :url "https://github.com/elixir-editors/emacs-elixir")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
