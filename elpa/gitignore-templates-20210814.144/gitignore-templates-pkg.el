50164f87d0ae09e159a19257c5c3b9701615bdbe
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:13:01 +0700

    [Updating gitignore-templates version: 20210814.144][removing old version: 20200228.1419]

---
+++ elpa/gitignore-templates-20210814.144/gitignore-templates-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from gitignore-templates.el  -*- no-byte-compile: t -*-
+(define-package "gitignore-templates" "20210814.144" "Create .gitignore using GitHub or gitignore.io API" '((emacs "24.3")) :commit "d28cd1cec00242b688861648d36d086818b06099" :authors '(("Xu Chunyang")) :maintainer '("Xu Chunyang") :keywords '("tools") :url "https://github.com/xuchunyang/gitignore-templates.el")
