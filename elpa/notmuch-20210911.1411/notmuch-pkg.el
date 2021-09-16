add78681f8d2c618736862012d2093a6408f1fd0
Jason Milkins <jasonm23@gmail.com>
2021-09-16 14:14:44 +0700

    [Updating notmuch version: 20210911.1411][removing old version: 20200810.17]

---
+++ elpa/notmuch-20210911.1411/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "20210911.1411" "run notmuch within emacs" 'nil :commit "21e365f51a7ff17e5154cf06396aeafe3f7d9bd7" :url "https://notmuchmail.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
