a771a1d0570658491476ebdf12b8788ce562ac08
Jason Milkins <jasonm23@gmail.com>
2021-09-30 15:44:15 +0700

    [Updating spacemacs-theme version: 20210924.1220][removing old version: 20210706.1210]

---
+++ elpa/spacemacs-theme-20210924.1220/spacemacs-theme-pkg.el
@@ -0,0 +1,4 @@
+(define-package "spacemacs-theme" "20210924.1220" "Color theme with a dark and light versions" 'nil :commit "e5ed346b9c31f0b43eb359614efd9aa439e1d18d")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
