7e5bb5787bc70b804c74b9b28f87f6a9c6ed89c3
Jason Milkins <jasonm23@gmail.com>
2021-09-30 15:44:12 +0700

    [Updating org version: 20210920][removing old versions: 20210913,20210913/etc,20210913/etc/styles]

--- elpa/org-20210913/org-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "org"
-  "20210913" "Outline-based notes management and organizer" ( ))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/org-20210920/org-pkg.el
@@ -0,0 +1,5 @@
+(define-package "org"
+  "20210920" "Outline-based notes management and organizer" ( ))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
