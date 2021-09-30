4bb418d7fab83a155f0a3843c12fcdebf6c6d570
Jason Milkins <jasonm23@gmail.com>
2021-09-30 15:44:06 +0700

    [Updating magit version: 20210928.1325][removing old version: 20210913.1931]

---
+++ elpa/magit-20210928.1325/magit-pkg.el
@@ -0,0 +1,18 @@
+(define-package "magit" "20210928.1325" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (dash "20210330")
+    (git-commit "20210806")
+    (magit-section "20210806")
+    (transient "20210701")
+    (with-editor "20210524"))
+  :commit "f53148a569191bdbfb78d76f28481b91c60cb846" :authors
+  '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+    ("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
