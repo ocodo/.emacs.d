3eff88b2d147a2fac8276cb6a42fbb99870f9721
Jason Milkins <jasonm23@gmail.com>
2021-09-30 15:37:13 +0700

    [Updating diff-hl version: 20210928.139][removing old version: 20210909.207]

--- elpa/diff-hl-20210928.139/diff-hl-pkg.el
+++ elpa/diff-hl-20210928.139/diff-hl-pkg.el
@@ -1,7 +1,7 @@
-(define-package "diff-hl" "20210909.207" "Highlight uncommitted changes using VC"
+(define-package "diff-hl" "20210928.139" "Highlight uncommitted changes using VC"
   '((cl-lib "0.2")
     (emacs "25.1"))
-  :commit "40c89a7b0d2590b1e094128f3f1d6dc87bf14ce9" :authors
+  :commit "6b7ca8c310ec1c1a83990c8d1c013c68f61d9d51" :authors
   '(("Dmitry Gutov" . "dgutov@yandex.ru"))
   :maintainer
   '("Dmitry Gutov" . "dgutov@yandex.ru")
