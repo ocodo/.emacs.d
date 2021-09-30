98727c689c621354f045e6442922c4f5e00b55d1
Jason Milkins <jasonm23@gmail.com>
2021-09-30 15:44:03 +0700

    [Updating language-id version: 20210916.831][removing old version: 20210822.412]

---
+++ elpa/language-id-20210916.831/language-id-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from language-id.el  -*- no-byte-compile: t -*-
+(define-package "language-id" "20210916.831" "Library to work with programming language identifiers" '((emacs "24") (cl-lib "0.5")) :commit "906fac7d91994d02120cfb5f547c1d06cea1ad69" :authors '(("Lassi Kortela" . "lassi@lassi.io")) :maintainer '("Lassi Kortela" . "lassi@lassi.io") :keywords '("languages" "util") :url "https://github.com/lassik/emacs-language-id")
